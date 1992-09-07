70e8c438eefa329276f346df07e5cf86aff969ae
Malcolm J. Currie <mjc@star.rl.ac.uk>
1992-09-07 20:14:43 +0000

    Converts Interim integer format code to its character form.
    svn path=/trunk/; revision=37

---
+++ applications/convert/libcon/con_btypc.f
@@ -0,0 +1,101 @@
+      SUBROUTINE CON_BTYPC( ITCODE, FORMAT, STATUS )
+*+
+*  Name:
+*     CON_BTYPC
+
+*  Purpose:
+*     Converts Interim integer format code to its character form.
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Type of Module:
+*     ADAM A-task
+
+*  Invocation:
+*     CALL CON_BTYPC( ITCODE, FORMAT, STATUS )
+
+*  Description:
+*     Converts an Interim code, e.g. 102, to its character form, e.g.
+*     SW.
+
+*  Arguments:
+*     ITCODE = INTEGER (Given)
+*        The Interim integer code for a data type.
+*     FORMAT = CHARACTER * 2 (Returned)
+*        The Interim data type.
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
+
+*  Authors:
+*     MJC: Malcolm J. Currie (STARLINK)
+*     {enter_new_authors_here}
+
+*  History:
+*     1991 October 30 (MJC):
+*        Original version.
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+      
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+
+*  Arguments Given:
+      INTEGER
+     :  ITCODE
+
+*  Arguments Returned:
+      CHARACTER * ( * )
+     :  FORMAT
+
+*  Status:
+      INTEGER STATUS             ! Global status
+
+*  Local Constants:
+      INTEGER MAXTYP
+      PARAMETER( MAXTYP=7 )
+
+*  Local Variables:
+      INTEGER
+     :  I,                       ! Loop counter
+     :  TYPNUM( MAXTYP )         ! The table of Interim numeric data
+                                 ! types
+
+      CHARACTER * 2
+     :  TYPES( MAXTYP )          ! The table of Interim numeric data
+                                 ! types
+
+*  Local Data:
+      DATA TYPES/'SB', 'SW', 'SL',  'R', 'DP', 'UB', 'UW' /
+      DATA TYPNUM/101, 102,  104,  204,  208,  301,  302/
+
+*.
+
+*  Check the inherited global status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  Loop until a match is found.  Copy the type string to the output
+*  argument. If none is found an error report will be made, otherwise
+*  the routine exits.
+      FORMAT = ' '
+      DO I = 1, MAXTYP
+         IF ( ITCODE .EQ. TYPNUM( I ) ) THEN
+            FORMAT = TYPES( I )
+            GOTO 999
+         END IF
+      END DO
+
+*  If an error occurred, then report a contextual message.
+      STATUS = SAI__ERROR
+      CALL ERR_REP( 'CON_BTYPC_ERR',
+     :  'Unable to convert Interim type code into a string.', STATUS )
+
+  999 CONTINUE
+      END
