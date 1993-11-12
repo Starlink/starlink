aca64d2bbf047d90f08e472211f583e9f92adb97
Malcolm J. Currie <mjc@star.rl.ac.uk>
1993-11-12 21:35:02 +0000

    Ensured that the SIMPLE card always appears first in the FITS header.
    svn path=/trunk/; revision=356

--- applications/convert/ndf2unf.f
+++ applications/convert/ndf2unf.f
@@ -198,6 +198,8 @@
                                  ! terminating the FITS extension
       INTEGER FD                 ! File descriptor
       INTEGER FIOSTA             ! Fortran I/O status
+      LOGICAL FIRST              ! True if the FITS mandatory headers
+                                 ! have yet to be written
       CHARACTER * ( DAT__SZLOC ) FLOC ! Locator to FITS extension
       CHARACTER FITSTR *( SZFITS ) ! A FITS-header card
       LOGICAL HEADER             ! If true there will be FITS-like
@@ -398,6 +400,10 @@
 *  be added to the header.
          END = .FALSE.
 
+*  Initialise the flag to indicate whether or not the mandatory headers
+*  have been written.
+         FIRST = .TRUE.
+
 *  Loop for each card.
          DO  I = 1, NCARD
 
@@ -430,23 +436,34 @@
      :        ( INDEX( DESCR, 'BUNITS') .EQ. 0 .OR. .NOT. UNTFND ) .AND.
      :        ( INDEX( DESCR, 'TITLE') .EQ. 0 .OR. .NOT. TITFND ) ) THEN
 
-*  Write the FITS card to the unformatted file.
-*   Write this header to the unformatted file.
-               WRITE( LUN, IOSTAT=FIOSTA ) FITSTR
-               CALL FIO_REP( LUN, ' ', FIOSTA, 'Error writing a '/
-     :           /'header record to file ^FNAME.  Reason was ^IOSTAT.',
-     :           STATUS )
-
 *  This code assumes that the FITS header will begin with the mandatory
 *  headers in the mandatory order.  It is not worth the great effort to
-*  validate the FITS descriptors of a defunct format.  Thus keyword one
-*  is SIMPLE.  Want to write after this.
-               IF ( I .EQ. 1 ) THEN
+*  validate the FITS descriptors of a defunct format.  However, since
+*  some applications validate the headers, insure the first header is
+*  SIMPLE.  Want to write after this.
+               IF ( FIRST ) THEN
+                  IF ( INDEX( DESCR, 'SIMPLE' ) .EQ. 0 ) THEN
+
+*  Create the SIMPLE card.
+                     FITSTR = ' '
+                     FITSTR( 1:9 ) = 'SIMPLE  ='
+                     FITSTR( 30: ) = 'T / File is simple'
+                  END IF
+
+*  Write the SIMPLE card header to the unformatted file.
+                  WRITE( LUN, IOSTAT=FIOSTA ) FITSTR
+                  CALL FIO_REP( LUN, ' ', FIOSTA, 'Error writing a '/
+     :              /'header record to file ^FNAME.  Reason was '/
+     :              /'^IOSTAT.', STATUS )
 
 *  Insert NAXIS, AXISn, and optional keywords to the header if the
 *  appropriate special objects are present in the NDF.
                   CALL CON_SPHEA( NDF, FD, TYPE, 'UNFORMATTED', NFLAGS,
      :                            CMPFND, STATUS )
+
+*  Record that the mandatory headers have been written to the output
+*  file.
+                  FIRST = .FALSE.
 
 *  Use more obvious flags to indicate the certain items have been
 *  written to the descriptors already.
@@ -456,6 +473,28 @@
                   TITFND = CMPFND( 4 )
                   LABFND = CMPFND( 5 )
                   UNTFND = CMPFND( 6 )
+
+                  IF ( INDEX( DESCR, 'SIMPLE' ) .EQ. 0 ) THEN
+
+*  Obtain its value which was overwritten by the SIMPLE card.
+                     CALL DAT_GET0C( CLOC, FITSTR, STATUS )
+
+
+*  Write the original first header, now it appears after the mandatory
+*  headers.
+                     WRITE( LUN, IOSTAT=FIOSTA ) FITSTR
+                     CALL FIO_REP( LUN, ' ', FIOSTA, 'Error writing a '/
+     :                 /'header record to file ^FNAME.  Reason was '/
+     :                 /'^IOSTAT.', STATUS )
+                  END IF
+
+               ELSE
+
+*  Write the ordinary FITS card to the unformatted file.
+                  WRITE( LUN, IOSTAT=FIOSTA ) FITSTR
+                  CALL FIO_REP( LUN, ' ', FIOSTA, 'Error writing a '/
+     :              /'header record to file ^FNAME.  Reason was '/
+     :              /'^IOSTAT.', STATUS )
                END IF
             END IF
 
