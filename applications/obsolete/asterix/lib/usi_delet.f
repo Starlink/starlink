14390125a957103c12653934819d67e21874f2dd
Asterix Developer <asterix@star.sr.bham.ac.uk>
1994-11-24 13:43:31 +0000

    Initial revision

---
+++ applications/obsolete/asterix/lib/usi_delet.f
@@ -0,0 +1,113 @@
+*+  USI_DELET - Delete environment value
+      SUBROUTINE USI_DELET( PAR, STATUS )
+*    Description :
+*     <description of what the subroutine does - for user info>
+*    Method :
+*     <description of how the subroutine works - for programmer info>
+*    Deficiencies :
+*     <description of any deficiencies>
+*    Bugs :
+*     <description of any "bugs" which have not been fixed>
+*    Authors :
+*
+*     David J. Allan (JET-X, University of Birmingham)
+*
+*    History :
+*
+*     21 May 93 : Original (DJA)
+*
+*    Type definitions :
+*
+      IMPLICIT NONE
+*
+*    Global constants :
+*
+      INCLUDE 'SAE_PAR'
+      INCLUDE 'DAT_PAR'
+      INCLUDE 'USI0_PAR'
+*
+*    Global variables :
+*
+      INCLUDE 'USI_CMN'
+*
+*    Import :
+*
+      CHARACTER*(*)		PAR			! Parameter name
+*
+*    Status :
+*
+      INTEGER 			STATUS
+*
+*    External references :
+*
+      EXTERNAL			USI_BLK
+*
+*    Local variables :
+*
+      INTEGER			RPTR			! Facility routine
+*-
+
+*    New error context
+      CALL ERR_MARK
+
+*    Check USI initialised
+      IF ( .NOT .USI_SYINIT ) THEN
+        CALL USI_INIT( STATUS )
+      END IF
+
+*    Locate the DELET facility in this context
+      CALL USI0_LOCRTN( USI__F_DELET, RPTR, STATUS )
+
+*    Invoke it
+      CALL USI_DELET_E( %VAL(RPTR), PAR, STATUS )
+
+*    Restore error context
+      CALL ERR_RLSE
+
+      END
+
+
+
+*+  USI_DELET_E - Invoke parameter deleting routine
+      SUBROUTINE USI_DELET_E( DELET_RTN, PAR, STATUS )
+*    Description :
+*     <description of what the subroutine does - for user info>
+*    Method :
+*     <description of how the subroutine works - for programmer info>
+*    Deficiencies :
+*     <description of any deficiencies>
+*    Bugs :
+*     <description of any "bugs" which have not been fixed>
+*    Authors :
+*
+*     David J. Allan (JET-X, University of Birmingham)
+*
+*    History :
+*
+*     21 May 93 : Original (DJA)
+*
+*    Type definitions :
+*
+      IMPLICIT NONE
+*
+*    Global constants :
+*
+      INCLUDE 'SAE_PAR'
+*
+*    Import :
+*
+      EXTERNAL			DELET_RTN		! Cancel routine
+      CHARACTER*(*)		PAR			! Parameter name
+*
+*    Status :
+*
+      INTEGER 			STATUS
+*-
+
+*    Check status
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*    Invoke the DELET routine
+      CALL DELET_RTN( PAR, STATUS )
+
+      END
