0b1a5b2c1b4c1c297db8ae0559f37db8c77c93c1
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-01-05 16:20:17 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/arx_write.f
+++ applications/obsolete/asterix/lib/arx_write.f
@@ -21,16 +21,31 @@
       INTEGER STATUS
 *    External references :
 *    Functions :
+      INTEGER CHR_LEN
 *    Local Constants :
 *    Local variables :
       CHARACTER*132 ARDOUT
+      INTEGER FID
+      INTEGER L
+      INTEGER I
+      INTEGER SIZE
 *-
       IF (STATUS.EQ.SAI__OK) THEN
 
 *  get output file name
         CALL USI_GET0C(PAR,ARDOUT,STATUS)
 
-        CALL GRP_LISTF(ARDOUT,0,0,'ARD text',ID,STATUS)
+        CALL FIO_OPEN(ARDOUT,'WRITE','NONE',0,FID,STATUS)
+
+        CALL GRP_GRPSZ(ID,SIZE,STATUS)
+
+        DO I=1,SIZE
+          CALL GRP_GET(ID,I,1,ARDOUT,STATUS)
+          L=CHR_LEN(ARDOUT)
+          CALL FIO_WRITE(FID,ARDOUT(:L),STATUS)
+        ENDDO
+
+        CALL FIO_CLOSE(FID,STATUS)
 
         IF (STATUS.NE.SAI__OK) THEN
           CALL ERR_REP(' ','from ARX_WRITE',STATUS)
