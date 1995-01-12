858812f9f3898cc67d1395248dd19528ee440985
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-01-12 10:03:52 +0000

    Initial revision

---
+++ applications/obsolete/asterix/grf/gcb_fsave.f
@@ -0,0 +1,113 @@
+      SUBROUTINE GCB_FSAVE( FID, STATUS )
+*+
+*  Name:
+*     GCB_FSAVE
+
+*  Purpose:
+*     Saves Grafix Control Block to file object
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Invocation:
+*     CALL GCB_FSAVE( FID, STATUS )
+
+*  Description:
+*     Saves Grafix Control Block to file object. Currently just calls the
+*     HDS version.
+
+*  Arguments:
+*     FID = INTEGER (given)
+*        ADI identifier of file object
+*     STATUS = INTEGER (given and returned)
+*        The global status.
+
+*  Examples:
+*     {routine_example_text}
+*        {routine_example_description}
+
+*  Pitfalls:
+*     {pitfall_description}...
+
+*  Notes:
+*     {routine_notes}...
+
+*  Prior Requirements:
+*     {routine_prior_requirements}...
+
+*  Side Effects:
+*     {routine_side_effects}...
+
+*  Algorithm:
+*     {algorithm_description}...
+
+*  Accuracy:
+*     {routine_accuracy}
+
+*  Timing:
+*     {routine_timing}
+
+*  External Routines Used:
+*     {name_of_facility_or_package}:
+*        {routine_used}...
+
+*  Implementation Deficiencies:
+*     {routine_deficiencies}...
+
+*  {machine}-specific features used:
+*     {routine_machine_specifics}...
+
+*  {DIY_prologue_heading}:
+*     {DIY_prologue_text}
+
+*  References:
+*     {routine_references}...
+
+*  Keywords:
+*     {routine_keywords}...
+
+*  Copyright:
+*     {routine_copyright}
+
+*  Authors:
+*     DJA: David J. Allan (Jet-X, University of Birmingham)
+*     {enter_new_authors_here}
+
+*  History:
+*     12 Jan 1995 (DJA):
+*        Original version.
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'DAT_PAR'
+
+*  Arguments Given:
+      INTEGER			FID			! File object id
+
+*  Status:
+      INTEGER 			STATUS             	! Global status
+
+*  Local Variables:
+      CHARACTER*(DAT__SZLOC)	LOC			! HDS file handle
+*.
+
+*  Check inherited global status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  Extract locator and call HDS routine
+      CALL ADI_CGET0C( FID, 'LOCATOR', LOC, STATUS )
+      CALL GCB_SAVE( LOC, STATUS )
+
+*  Report any errors
+      IF ( STATUS .NE. SAI__OK ) CALL AST_REXIT( 'GCB_FSAVE', STATUS )
+
+      END
