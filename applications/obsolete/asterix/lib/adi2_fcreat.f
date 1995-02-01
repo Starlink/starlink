bd82362e16cfeb04e609469f6f5d63037677bbf2
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-02-01 14:32:41 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/adi2_fcreat.f
+++ applications/obsolete/asterix/lib/adi2_fcreat.f
@@ -4,13 +4,13 @@
 *     ADI2_FCREAT
 
 *  Purpose:
-*     {routine_purpose}
+*     Create a new FITS file, or FITS file component
 
 *  Language:
-*     {routine_language}
+*     Starlink Fortran 77
 
 *  Invocation:
-*     CALL ADI2_FCREAT( [p]... )
+*     CALL ADI2_FCREAT( FILE, ID, FID, STATUS )
 
 *  Description:
 *     {routine_description}
@@ -107,22 +107,58 @@ c      {data_type} {constant_name} ! [constant_description]
 C      PARAMETER ( {constant_name} = {cons} )
 
 *  Local Variables:
-c      {data_type} {name}[dimensions] ! [local_variable_description]
+      CHARACTER*132		FNAME			! File name
+      CHARACTER*8 		KEYWRD			! Keyword name
 
-*  Internal References:
-c      {data_type} {internal_name} ! [internal_description]
-c      [internal_definition_statement]...
-
-*  Local Data:
-c      DATA {data_elm} / {data_values}... /
-c      [data_stmt]...
+      INTEGER			BSIZE			! Block size
+      INTEGER			FSTAT			! FITSIO status
+      INTEGER			HDU			! HDU number
+      INTEGER			LFILEC			! Last filename char
+      INTEGER			LUN			! Logical unit number
 
 *.
 
 *  Check inherited global status.
       IF ( STATUS .NE. SAI__OK ) RETURN
 
+*  Extract filename
+      CALL ADI_GET0C( FILE, FNAME, STATUS )
+
+*  Parse the file specification into file, hdu and keyword names
+      CALL ADI2_PARSE( FNAME, LFILEC, HDU, KEYWRD, STATUS )
+
+*  Grab logical unit from system
+      CALL FIO_GUNIT( LUN, STATUS )
+
 	CALL ADI_PRINT( ID, STATUS )
+
+*  Try to create file
+      BSIZE = 1
+      FSTAT = 0
+      CALL FTINIT( LUN, FNAME(:LFILEC), BSIZE, FSTAT )
+
+*  Created ok?
+      IF ( FSTAT .EQ. 0 ) THEN
+
+*    Create new instance of a FITSfile object
+        CALL ADI_NEW0( 'FITSfile', FID, STATUS )
+
+*    Write HDU number
+        CALL ADI_CPUT0I( FID, '.HDU', HDU, STATUS )
+
+*    Write extra info into the file handle object
+        CALL ADI_CPUT0I( FID, '.LUN', LUN, STATUS )
+        CALL ADI_CPUT0I( FID, '.BLOCK_SIZE', BSIZE, STATUS )
+
+      ELSE
+
+*    Return unit to system
+        CALL FIO_PUNIT( LUN, STATUS )
+
+*    Report an error
+        CALL ADI2_FITERP( FSTAT, STATUS )
+
+      END IF
 
 *  Report any errors
       IF ( STATUS .NE. SAI__OK ) CALL AST_REXIT( 'ADI2_FCREAT', STATUS )
