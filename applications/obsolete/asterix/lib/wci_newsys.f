629e2c50a270e6b5ed7c0cfe650f56293884f251
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-02-13 13:40:51 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/wci_newsys.f
+++ applications/obsolete/asterix/lib/wci_newsys.f
@@ -56,7 +56,7 @@
 *     {routine_deficiencies}...
 
 *  References:
-*     WCI Subroutine Guide : http://www.sr.star.ac.uk:8080/asterix-docs/Programmer/Guides/wci.html
+*     WCI Subroutine Guide : http://www.sr.bham.ac.uk:8080/asterix-docs/Programmer/Guides/wci.html
 
 *  Keywords:
 *     package:wci, usage:public
