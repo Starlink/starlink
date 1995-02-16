20daf0d43d3a44521f3b0c782f9f8f0bf0cb487f
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-02-16 10:10:45 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/util/syserr.f
+++ applications/obsolete/asterix/util/syserr.f
@@ -33,6 +33,7 @@
 *                        ( dja )
 *     12 Dec 88 : V1.0-1 USIs now used for user interface. (DJA)
 *     24 Nov 94 : V1.8-0 Now use USI for user interface (DJA)
+*     15 Feb 95 : V1.8-1 Use new data interface routines (DJA)
 *
 *   Type Definitions :
 *
@@ -41,7 +42,7 @@
 *   Global constants :
 *
       INCLUDE 'SAE_PAR'
-      INCLUDE 'DAT_PAR'
+      INCLUDE 'ADI_PAR'
 *
 *   Status :
 *
@@ -54,19 +55,19 @@
 *
 *   Local variables :
 *
-      CHARACTER*(DAT__SZLOC)	ILOC		! Locator to data object
-      CHARACTER*(DAT__SZLOC)	OLOC		! Locator to data object
-      CHARACTER*(DAT__SZTYP)	OBTYPE		! Type of data object
+      CHARACTER*(ADI__SZTYP)	OBTYPE		! Type of data object
       CHARACTER*80 		ACTION(MAXLINES)! History file entries
 
       INTEGER                   ACTLEN          ! Length of ACTION sting
-      INTEGER 			DIMS(DAT__MXDIM)! Dimensions of data
-      INTEGER 			DIMSV(DAT__MXDIM)!Dimensions of variance
+      INTEGER 			DIMS(ADI__MXDIM)! Dimensions of data
+      INTEGER 			DIMSV(ADI__MXDIM)!Dimensions of variance
       INTEGER 			DPTR		! Pointer to data array
+      INTEGER		  	IFID			! Input dataset
       INTEGER		 	NDIM	        ! Dimensionality of data
       INTEGER		 	NDIMV	        ! Dimensionality of variance
       INTEGER			NELM		! No of data/variance values
       INTEGER                   NREC            ! No. of history records
+      INTEGER		  	OFID			! Output dataset
       INTEGER                   USE             ! History records no.
       INTEGER 			VPTR		! Pointer to variance array
 
@@ -81,7 +82,7 @@
 *    Version id :
 *
       CHARACTER*(22)		VERSION
-	 PARAMETER              (VERSION='SYSERR Version 1.8-0')
+	 PARAMETER              ( VERSION='SYSERR Version 1.8-1' )
 *-
 
 *    Check status
@@ -91,40 +92,41 @@
       CALL MSG_PRNT( VERSION )
 
 *    Initialise
-      CALL AST_INIT
+      CALL AST_INIT()
 
 *    See whether we're going to overwrite
       CALL USI_GET0L( 'OVER', OVERWRITE, STATUS )
 
 *    Obtain data object, access and check it
       IF ( OVERWRITE ) THEN
-         CALL USI_ASSOCI( 'INP', 'UPDATE', OLOC, PRIM, STATUS )
-
+         CALL USI_TASSOCI( 'INP', '*', 'UPDATE', OFID, STATUS )
+         CALL BDI_PRIM( OFID, PRIM, STATUS )
       ELSE
-         CALL USI_ASSOC2(  'INP', 'OUT', 'READ', ILOC, OLOC, PRIM,
-     :                                                    STATUS )
+         CALL USI_TASSOC2(  'INP', 'OUT', 'READ', IFID, OFID, STATUS )
+         CALL BDI_PRIM( IFID, PRIM, STATUS )
          CALL USI_SHOW( 'Output dataset {OUT}', STATUS )
-         CALL HDX_COPY( ILOC, OLOC, STATUS )
+         CALL ADI_FCOPY( IFID, OFID, STATUS )
 
       END IF
       IF ( STATUS .NE. SAI__OK ) GOTO 99
-      CALL DAT_TYPE( OLOC, OBTYPE, STATUS )
+      CALL BDI_TYPE( OFID, OBTYPE, STATUS )
       CALL MSG_SETC( 'OTYPE', OBTYPE )
-      CALL MSG_PRNT( 'Data object is of type ^OTYPE.' )
+      CALL MSG_PRNT( 'Data object is of type ^OTYPE' )
 
       IF ( PRIM ) THEN
-	 CALL MSG_PRNT( 'ERROR : Cannot use primitive object.' )
 	 STATUS = SAI__ERROR
+	 CALL ERR_REP( ' ', 'ERROR : Cannot use primitive object.',
+     :                 STATUS )
 
       ELSE
-	 CALL BDA_CHKDATA( OLOC, OK, NDIM, DIMS, STATUS )
+	 CALL BDI_CHKDATA( OFID, OK, NDIM, DIMS, STATUS )
 	 IF ( OK ) THEN
-	    CALL BDA_MAPDATA( OLOC, 'READ', DPTR, STATUS )
+	    CALL BDI_MAPDATA( OFID, 'READ', DPTR, STATUS )
 	    CALL ARR_SUMDIM( NDIM, DIMS, NELM )
 
 	 ELSE
-	    CALL MSG_PRNT( 'ERROR: No data object found.' )
 	    STATUS = SAI__ERROR
+	    CALL ERR_REP( ' ', 'ERROR: No data object found.', STATUS )
 
 	 END IF
 
@@ -137,15 +139,15 @@
 
 *    See if VARIANCE is present. If it is check it and map it ; otherwise
 *    create a new VARIANCE structure.
-      CALL BDA_CHKVAR( OLOC, OK, NDIMV, DIMSV, STATUS )
+      CALL BDI_CHKVAR( OFID, OK, NDIMV, DIMSV, STATUS )
 
       IF ( OK ) THEN
-         CALL BDA_MAPVAR( OLOC, 'UPDATE', VPTR, STATUS )
+         CALL BDI_MAPVAR( OFID, 'UPDATE', VPTR, STATUS )
 
       ELSE IF ( STATUS .EQ. SAI__OK ) THEN
-	 CALL BDA_CREVAR( OLOC, NDIM, DIMS, STATUS )
-         CALL MSG_PRNT( 'No variance present - new array created.' )
-         CALL BDA_MAPVAR( OLOC, 'WRITE', VPTR, STATUS )
+	 CALL BDI_CREVAR( OFID, NDIM, DIMS, STATUS )
+         CALL MSG_PRNT( 'No variance present - new array created' )
+         CALL BDI_MAPVAR( OFID, 'WRITE', VPTR, STATUS )
 
       END IF
       IF ( STATUS .NE. SAI__OK ) GOTO 99
@@ -155,7 +157,7 @@
       CALL SYSERR_ERRSUB( SERR, NELM, NEWVARA, %VAL(DPTR), %VAL(VPTR))
 
 *    History file entry
-      CALL HIST_ADD( OLOC, VERSION, STATUS )
+      CALL HSI_ADD( OFID, VERSION, STATUS )
       IF ( STATUS .NE. SAI__OK ) GOTO 99
 
       CALL MSG_SETR( 'PCERR', PERCERR )
@@ -170,7 +172,7 @@
 
       NREC=MAXLINES
       CALL USI_TEXT( USE, ACTION, NREC, STATUS )
-      CALL HIST_PTXT( OLOC, NREC, ACTION, STATUS )
+      CALL HSI_PTXT( OFID, NREC, ACTION, STATUS )
 
 *    Tidy up
  99   CALL AST_CLOSE()
