a6075a962b544174d1d3950d313d351cfa805a43
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-02-22 09:55:56 +0000

    Initial revision

---
+++ applications/obsolete/asterix/lib/adi1_putloc.f
@@ -0,0 +1,114 @@
+      SUBROUTINE ADI1_PUTLOC( LOC, ID, STATUS )
+*+
+*  Name:
+*     ADI1_PUTLOC
+
+*  Purpose:
+*     Wrap an HDS locator in an ADI object
+
+*  Language:
+*     Starlink Fortran
+
+*  Invocation:
+*     CALL ADI1_PUTLOC( LOC, ID, STATUS )
+
+*  Description:
+*     Store an HDS locator in an ADI object
+
+*  Arguments:
+*     LOC = CHARACTER*(DAT__SZLOC) (given)
+*        HDS locator
+*     ID = INTEGER (returned)
+*        ADI identifier
+*     STATUS = INTEGER (given and returned)
+*        The global status.
+
+*  Examples:
+*     {routine_example_text}
+*        {routine_example_description}
+
+*  Pitfalls:
+*     {pitfall_description}...
+
+*  Notes:
+*     {routine_notes}...
+
+*  Prior Requirements:
+*     {routine_prior_requirements}...
+
+*  Side Effects:
+*     {routine_side_effects}...
+
+*  Algorithm:
+*     {algorithm_description}...
+
+*  Accuracy:
+*     {routine_accuracy}
+
+*  Timing:
+*     {routine_timing}
+
+*  External Routines Used:
+*     {name_of_facility_or_package}:
+*        {routine_used}...
+
+*  Implementation Deficiencies:
+*     {routine_deficiencies}...
+
+*  {machine}-specific features used:
+*     {routine_machine_specifics}...
+
+*  {DIY_prologue_heading}:
+*     {DIY_prologue_text}
+
+*  References:
+*     {routine_references}...
+
+*  Keywords:
+*     {routine_keywords}...
+
+*  Copyright:
+*     {routine_copyright}
+
+*  Authors:
+*     DJA: David J. Allan (Jet-X, University of Birmingham)
+*     {enter_new_authors_here}
+
+*  History:
+*     12 Jan 1995 (DJA):
+*        Original version.
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'DAT_PAR'
+
+*  Arguments Given:
+      CHARACTER*(DAT__SZLOC)	LOC			! Locator to store
+
+*  Arguments Returned:
+      INTEGER			ID			! ADI identifier
+
+*  Status:
+      INTEGER 			STATUS             	! Global status
+*.
+
+*  Check inherited global status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  Create ADI object to handle the locator
+      CALL ADI_NEW0( 'HDSfile', ID, STATUS )
+      CALL ADI_CPUT0C( ID, 'Locator', LOC, STATUS )
+
+*  Report any errors
+      IF ( STATUS .NE. SAI__OK ) CALL AST_REXIT( 'ADI1_PUTLOC', STATUS )
+
+      END
