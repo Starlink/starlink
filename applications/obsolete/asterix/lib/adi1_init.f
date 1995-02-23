fa9169a803adc85c90baecacd2abeaad2690eb6f
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-02-23 15:35:24 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/adi1_init.f
+++ applications/obsolete/asterix/lib/adi1_init.f
@@ -2,11 +2,13 @@
 
         INTEGER         STATUS
 	INTEGER ID,DID
+        EXTERNAL        ADI1_FCREAT
         EXTERNAL        ADI1_OPEN
         EXTERNAL        ADI1_NATRL
 
         CALL ADI_DEFREP( 'HDS', ID, STATUS )
         CALL ADI_DEFRCB( ID, 'OPEN_RTN', ADI1_OPEN, STATUS )
+        CALL ADI_DEFRCB( ID, 'CREAT_RTN', ADI1_FCREAT, STATUS )
 
 	CALL ADI_DEFCLS( 'HDSlocator', ' ', 'Locator', DID,
      :                   STATUS )
