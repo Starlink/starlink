56b5ebded30e0f03752c5f05200c77a4e7b32877
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-02-28 13:04:42 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/adi2_fcomit.f
+++ applications/obsolete/asterix/lib/adi2_fcomit.f
@@ -91,6 +91,143 @@
       INTEGER 			STATUS             	! Global status
 
 *  Local Variables:
+      CHARACTER*8		HDU			! HDU name
+
+      INTEGER			EID			! EXTENSIONS object
+      INTEGER			HID			! HDU object
+      INTEGER			IHDU			! Loop over HDUs
+      INTEGER			NHDU			! HDU count
+
+      LOGICAL			THERE			! Object exists?
+*.
+
+*  Check inherited global status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  Write the other keywords
+      CALL ADI2_LOCHDU( FID, ' ', HID, STATUS )
+      CALL ADI2_FCOMIT_HDU( FID, HID, ' ', STATUS )
+      CALL ADI_ERASE( HID, STATUS )
+
+*  Loop over extensions
+      CALL ADI_THERE( FID, 'EXTENSIONS', THERE, STATUS )
+      IF ( THERE ) THEN
+        CALL ADI_FIND( FID, 'EXTENSIONS', EID, STATUS )
+        CALL ADI_NCMP( EID, NHDU, STATUS )
+
+        DO IHDU = 1, NHDU
+
+          CALL ADI_INDCMP( EID, IHDU, HID, STATUS )
+          CALL ADI_NAME( HID, HDU, STATUS )
+          CALL ADI2_FCOMIT_HDU( FID, HID, HDU, STATUS )
+          CALL ADI_ERASE( HID, STATUS )
+        END DO
+
+        CALL ADI_ERASE( EID, STATUS )
+
+      END IF
+
+*  Report any errors
+      IF ( STATUS .NE. SAI__OK ) CALL AST_REXIT( 'ADI2_FCOMIT', STATUS )
+
+      END
+
+
+      SUBROUTINE ADI2_FCOMIT_HDU( FID, HID, HDU, STATUS )
+*+
+*  Name:
+*     ADI2_FCOMIT_HDU
+
+*  Purpose:
+*     Commit buffer changes to a FITSfile object
+
+*  Language:
+*     Starlink Fortran
+
+*  Invocation:
+*     CALL ADI2_FCOMIT_HDU( FID, HID, HDU, STATUS )
+
+*  Description:
+*     Commit any changes to keywords or data to the FITS file on disk. The
+*     file is not closed.
+
+*  Arguments:
+*     FID = INTEGER (given)
+*        ADI identifier of the FITSfile object
+*     STATUS = INTEGER (given and returned)
+*        The global status.
+
+*  Examples:
+*     {routine_example_text}
+*        {routine_example_description}
+
+*  Pitfalls:
+*     {pitfall_description}...
+
+*  Notes:
+*     {routine_notes}...
+
+*  Prior Requirements:
+*     {routine_prior_requirements}...
+
+*  Side Effects:
+*     {routine_side_effects}...
+
+*  Algorithm:
+*     {algorithm_description}...
+
+*  Accuracy:
+*     {routine_accuracy}
+
+*  Timing:
+*     {routine_timing}
+
+*  External Routines Used:
+*     {name_of_facility_or_package}:
+*        {routine_used}...
+
+*  Implementation Deficiencies:
+*     {routine_deficiencies}...
+
+*  References:
+*     ADI Subroutine Guide : http://www.sr.bham.ac.uk:8080/asterix-docs/Programmer/Guides/adi.html
+
+*  Keywords:
+*     package:adi, usage:private
+
+*  Copyright:
+*     Copyright (C) University of Birmingham, 1995
+
+*  Authors:
+*     DJA: David J. Allan (Jet-X, University of Birmingham)
+*     {enter_new_authors_here}
+
+*  History:
+*     2 Feb 1995 (DJA):
+*        Original version.
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'ADI_PAR'
+
+*  Arguments Given:
+      INTEGER			FID			! FITSfile identifier
+      INTEGER			HID			! HDU identifier
+      CHARACTER*(*)		HDU			! HDU name
+
+*  Status:
+      INTEGER 			STATUS             	! Global status
+
+*  Local Variables:
       CHARACTER*132		CMT			! Keyword comment
       CHARACTER*20		CLASS			! Keyword value class
       CHARACTER*132		CVALUE			! Keyword value
@@ -118,7 +255,7 @@
       CALL ADI_CGET0I( FID, '.LUN', LUN, STATUS )
 
 *  Primary HDU
-      CALL ADI2_GKEY0I( FID, ' ', 'NAXIS', .TRUE., .FALSE.,
+      CALL ADI2_GKEY0I( FID, HDU, 'NAXIS', .TRUE., .FALSE.,
      :                  NAXIS, CMT, STATUS )
 
 *  If NAXIS not present then write a null header
@@ -132,8 +269,7 @@
       CALL FTPHPR( LUN, .TRUE., 8, NAXIS, NAXES, 0, 1, .TRUE., FSTAT )
       CALL FTRDEF( LUN, FSTAT )
 
-*  Write the other keywords
-      CALL ADI2_LOCHDU( FID, ' ', HID, STATUS )
+*  Get number of keywords
       CALL ADI_NCMP( HID, NKEY, STATUS )
       DO IKEY = 1, NKEY
         CALL ADI_INDCMP( HID, IKEY, KID, STATUS )
@@ -158,14 +294,10 @@
         END IF
         CALL ADI_ERASE( KID, STATUS )
       END DO
-      CALL ADI_ERASE( HID, STATUS )
-
-*  Trap bad close status
-      IF ( FSTAT .NE. 0 ) THEN
-        CALL ADI2_FITERP( FSTAT, STATUS )
-      END IF
 
 *  Report any errors
-      IF ( STATUS .NE. SAI__OK ) CALL AST_REXIT( 'ADI2_FCOMIT', STATUS )
+      IF ( STATUS .NE. SAI__OK ) THEN
+        CALL AST_REXIT( 'ADI2_FCOMIT_HDU', STATUS )
+      END IF
 
       END
