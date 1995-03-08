dda81325c694e39bd82b85f1c99d3641dee32fd8
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-03-08 09:57:02 +0000

    Initial revision

---
+++ applications/obsolete/asterix/lib/adi1_locmore.f
@@ -0,0 +1,133 @@
+      SUBROUTINE ADI1_LOCMORE( ID, CREATE, MLOC, STATUS )
+*+
+*  Name:
+*     ADI1_LOCMORE
+
+*  Purpose:
+*     Locate MORE structure given HDS object
+
+*  Language:
+*     Starlink Fortran
+
+*  Invocation:
+*     CALL ADI1_LOCMORE( ID, CREATE, MLOC, STATUS )
+
+*  Description:
+*     Locate MORE structure given HDS object. The routine first
+*     checks that the object has not already been found, but only creates
+*     it if CREATE is specified true.
+
+*  Arguments:
+*     ID = INTEGER (given)
+*        ADI identifier referencing HDS object
+*     CREATE = LOGICAL (given)
+*        Create component if it doesn't exist?
+*     MLOC = CHARACTER*(DAT__SZLOC) (returned)
+*        Locate to MORE object
+*     STATUS = INTEGER (given and returned)
+*        The global status.
+
+*  Examples:
+*     {routine_example_text}
+*        {routine_example_description}
+
+*  Pitfalls:
+*     {pitfall_description}...
+
+*  Notes:
+*     {routine_notes}...
+
+*  Prior Requirements:
+*     {routine_prior_requirements}...
+
+*  Side Effects:
+*     {routine_side_effects}...
+
+*  Algorithm:
+*     {algorithm_description}...
+
+*  Accuracy:
+*     {routine_accuracy}
+
+*  Timing:
+*     {routine_timing}
+
+*  External Routines Used:
+*     {name_of_facility_or_package}:
+*        {routine_used}...
+
+*  Implementation Deficiencies:
+*     {routine_deficiencies}...
+
+*  References:
+*     ADI Subroutine Guide : http://www.sr.bham.ac.uk:8080/asterix-docs/Programmer/Guides/adi.html
+
+*  Keywords:
+*     package:adi, usage:private
+
+*  Copyright:
+*     Copyright (C) University of Birmingham, 1995
+
+*  Authors:
+*     DJA: David J. Allan (Jet-X, University of Birmingham)
+*     {enter_new_authors_here}
+
+*  History:
+*     24 Feb 1995 (DJA):
+*        Original version.
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'DAT_PAR'
+
+*  Arguments Given:
+      INTEGER			ID			! ADI identifier
+      LOGICAL			CREATE			! Create if not there?
+
+*  Arguments Returned:
+      CHARACTER*(DAT__SZLOC)	MLOC			! HEADER locator
+
+*  Status:
+      INTEGER 			STATUS             	! Global status
+
+*  Local Constants:
+      CHARACTER*10		MPROPN			! Name of property
+        PARAMETER		( MPROPN = '.MORE_LOC' )
+
+*  Local Variables:
+      CHARACTER*(DAT__SZLOC)	LOC			! Object locator
+
+      LOGICAL			THERE			! Property exists?
+*.
+
+*  Check inherited global status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  Located already?
+      CALL ADI_THERE( ID, MPROPN, THERE, STATUS )
+      IF ( THERE ) THEN
+        CALL ADI_CGET0C( ID, MPROPN, MLOC, STATUS )
+      ELSE
+        CALL DAT_THERE( LOC, 'MORE', THERE, STATUS )
+        IF ( CREATE .AND. .NOT. THERE ) THEN
+          CALL DAT_NEW( LOC, 'MORE', 'EXTENSION', 0, 0, STATUS )
+        END IF
+        CALL ADI1_FIND( LOC, 'MORE', MLOC, STATUS )
+        CALL ADI_CPUT0C( ID, MPROPN, MLOC, STATUS )
+      END IF
+
+*  Report any errors
+      IF ( STATUS .NE. SAI__OK ) THEN
+        CALL AST_REXIT( 'ADI1_LOCMORE', STATUS )
+      END IF
+
+      END
