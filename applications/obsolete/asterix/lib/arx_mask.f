c5a9084eb21dab71b670ada1f8be2eaf5b14d72b
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-03-08 14:18:07 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/arx_mask.f
+++ applications/obsolete/asterix/lib/arx_mask.f
@@ -13,6 +13,7 @@
       INCLUDE 'DAT_PAR'
       INCLUDE 'PAR_ERR'
       INCLUDE 'GRP_PAR'
+      INCLUDE 'PRM_PAR'
 *    Global Variables :
 *    Import :
       INTEGER ID
@@ -42,17 +43,25 @@
         LBND(2)=1
         UBND(1)=DIMS(1)
         UBND(2)=DIMS(2)
-*  set pixel to world transformation - see Sec.4 of ARD manual
+
+*  pixel coords.
+        IF (UNITS(1)(:3).EQ.'PIX') THEN
+          TR(1)=VAL__BADR
+*  otherwise set pixel to world transformation - see Sec.4 of ARD manual
 *  note that ARD assumes the pixel coord of first pixel to range
 *  from 0..1 whereas ASTERIX assumes a range 0.5 to 1.5 with 1
 *  at centre
-        TR(1)=0.5-BASE(1)/SCALE(1)
-        TR(2)=1.0/SCALE(1)
-        TR(3)=0.0
-        TR(4)=0.5-BASE(2)/SCALE(2)
-        TR(5)=0.0
-        TR(6)=1.0/SCALE(2)
+        ELSE
+          TR(1)=0.5-BASE(1)/SCALE(1)
+          TR(2)=1.0/SCALE(1)
+          TR(3)=0.0
+          TR(4)=0.5-BASE(2)/SCALE(2)
+          TR(5)=0.0
+          TR(6)=1.0/SCALE(2)
+        ENDIF
+
         REGVAL=2
+
 *  interpret ARD
         CALL ARD_WORK(ID,2,LBND,UBND,TR,.FALSE.,REGVAL,MASK,
      :                         LBNDI,UBNDI,LBNDE,UBNDE,STATUS)
