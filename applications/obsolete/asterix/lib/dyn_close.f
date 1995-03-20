ba503b1563366bfe35e9d7105962f3d659016b26
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-03-20 11:42:04 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/dyn_close.f
+++ applications/obsolete/asterix/lib/dyn_close.f
@@ -1,57 +1,118 @@
-*+  DYN_CLOSE - close down DYN_ system
-      SUBROUTINE DYN_CLOSE()
-*    Description :
-*    Method :
-*    Deficiencies :
-*    Bugs :
-*    Authors :
-*     BHVAD::RJV
-*    History :
-*
-*     29 Apr 92 : Use ERR_ to provide error protection. Removed unnecessary
-*                 HDS_CLOSE calls (DJA)
-*     16 Jun 92 : Replaced LIB$ calls with PSX ones (DJA)
-*
-*    Type Definitions :
-      IMPLICIT NONE
-*    Global constants :
-      INCLUDE 'SAE_PAR'
-      INCLUDE 'DAT_PAR'
-*    Status :
-      INTEGER STATUS
-*    Import :
-*    Import/Export :
-*    Export :
-*    External references :
-*    Global variables :
-      INCLUDE 'ASTLIB(DYN_CMN)'
-*    Local Constants :
-      INTEGER NULL
-      PARAMETER (NULL=0)
-*    Local variables :
-      INTEGER IPTR
+      SUBROUTINE DYN_CLOSE(  )
+*+
+*  Name:
+*     DYN_CLOSE
+
+*  Purpose:
+*     {routine_purpose}
+
+*  Language:
+*     Starlink Fortran
+
+*  Invocation:
+*     CALL DYN_CLOSE( )
+
+*  Description:
+*     {routine_description}
+
+*  Examples:
+*     {routine_example_text}
+*        {routine_example_description}
+
+*  Pitfalls:
+*     {pitfall_description}...
+
+*  Notes:
+*     {routine_notes}...
+
+*  Prior Requirements:
+*     {routine_prior_requirements}...
+
+*  Side Effects:
+*     {routine_side_effects}...
+
+*  Algorithm:
+*     {algorithm_description}...
+
+*  Accuracy:
+*     {routine_accuracy}
+
+*  Timing:
+*     {routine_timing}
+
+*  External Routines Used:
+*     {name_of_facility_or_package}:
+*        {routine_used}...
+
+*  Implementation Deficiencies:
+*     {routine_deficiencies}...
+
+*  References:
+*     DYN Subroutine Guide : http://www.sr.bham.ac.uk:8080/asterix-docs/Programmer/Guides/dyn.html
+
+*  Keywords:
+*     package:dyn, usage:public
+
+*  Copyright:
+*     Copyright (C) University of Birmingham, 1995
+
+*  Authors:
+*     DJA: David J. Allan (Jet-X, University of Birmingham)
+*     {enter_new_authors_here}
+
+*  History:
+*     20 Mar 1995 (DJA):
+*        Original version.
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
 *-
 
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+
+*  Global Variables:
+      INCLUDE 'DYN_CMN'                                 ! DYN common block
+*       DYN_ISINIT = LOGICAL (given)
+*         DYN class definitions loaded?
+*       DYS_PTR[] = INTEGER (given)
+*         Dynamic memory addresses
+
+*  External References:
+      EXTERNAL			DYN0_BLK		! Ensures inclusion
+
+*  Local Variables:
+      INTEGER			I			! Loop over slots
+      INTEGER 			STATUS             	! Local status
+*.
+
+*  Check initialised
+      IF ( DYN_ISINIT ) THEN
+
 *    Start new error context
-      CALL ERR_BEGIN( STATUS )
+        CALL ERR_BEGIN( STATUS )
 
-      DO IPTR=1,NPTR
-        IF (LIST(IPTR).SECTION) THEN
-          CALL HDS_ERASE(LIST(IPTR).LOC,STATUS)
-          IF ( STATUS .NE. SAI__OK ) THEN
-            CALL ERR_ANNUL( STATUS )
+*    Loop over sections
+        DO I = 1, DYN__NMAX
+
+*      Active section?
+          IF ( DYS_PTR(I) .NE. 0 ) THEN
+
+*        Unmap the section
+            CALL DYN0_UNMAP( I, STATUS )
+
           END IF
-        ELSE
-          CALL PSX_FREE( LIST(IPTR).PTR, STATUS )
-        ENDIF
-        LIST(IPTR).PTR=NULL
-        LIST(IPTR).NPAGE=NULL
-        LIST(IPTR).NBYTE=NULL
-        LIST(IPTR).SECTION=.FALSE.
-      ENDDO
-      NPTR=NULL
 
-*    Restore error context
-      CALL ERR_END( STATUS )
+        END DO
+
+*      Restore error context
+        CALL ERR_END( STATUS )
+
+      END IF
 
       END
