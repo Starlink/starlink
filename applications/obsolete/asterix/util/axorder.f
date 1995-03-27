65120a9555b02e64a36b1baf4901792c9c49bd84
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-03-27 17:28:51 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/util/axorder.f
+++ applications/obsolete/asterix/util/axorder.f
@@ -26,6 +26,7 @@
 *      8 May 91 : V1.4-0  Re-named from AXSWAP (DJA)
 *      6 Mar 92 : V1.6-0  OUT prompt moved back to pre-processing (DJA)
 *     24 Nov 94 : V1.8-0  Now use USI for user interface (DJA)
+*     26 Mar 95 : V1.8-1  Use new data interface (DJA)(
 *
 *    Type definitions :
 *
@@ -34,7 +35,7 @@
 *    Global constants :
 *
       INCLUDE 'SAE_PAR'
-      INCLUDE 'DAT_PAR'
+      INCLUDE 'ADI_PAR'
 *
 *    Status :
 *
@@ -50,23 +51,22 @@
       CHARACTER*80               UNITS             !
       CHARACTER*10               NSTR              !
 
-      CHARACTER*(DAT__SZLOC)     ILOC              ! Input dataset
-      CHARACTER*(DAT__SZLOC)     OLOC              ! Output dataset
-
-      INTEGER                    DIMS(DAT__MXDIM)  ! Input dimensions
+      INTEGER                    DIMS(ADI__MXDIM)  ! Input dimensions
       INTEGER                    IDPTR,IVPTR,IQPTR ! Input data pointers
-      INTEGER                    ODIMS(DAT__MXDIM) ! Output dimensions
-      INTEGER                    QDIMS(DAT__MXDIM) ! Input quality dimensions
-      INTEGER                    SELAX(DAT__MXDIM) ! New axis order
-      INTEGER                    VDIMS(DAT__MXDIM) ! Input variance dimensions
+      INTEGER                    ODIMS(ADI__MXDIM) ! Output dimensions
+      INTEGER                    QDIMS(ADI__MXDIM) ! Input quality dimensions
+      INTEGER                    SELAX(ADI__MXDIM) ! New axis order
+      INTEGER                    VDIMS(ADI__MXDIM) ! Input variance dimensions
 
       INTEGER                    HLEN, NDIGIT      ! String lengths
       INTEGER                    I                 ! Loop over dimensions
       INTEGER                    IAPTR, OAPTR      ! Input & output axis data
+      INTEGER			IFID			! Input dataset id
       INTEGER                    IWPTR             ! Input axis widths
       INTEGER                    NDIM              ! Dimensionality
       INTEGER                    NLINES            ! Number of history text bits
       INTEGER                    ODPTR,OVPTR,OQPTR ! Output data pointers
+      INTEGER			OFID			! Output dataset id
       INTEGER                    OWPTR             ! Output axis widths
       INTEGER                    QNDIM             ! Quality dimensionality
       INTEGER                    VNDIM             ! Variance dimensionality
@@ -78,14 +78,14 @@
       LOGICAL                    INPRIM            ! Input primitive?
       LOGICAL                    IWOK              ! Input widths ok?
       LOGICAL                    OK, VOK, QOK      ! Input objects ok?
-      LOGICAL                    SPEC(DAT__MXDIM)  ! Axis specified in output?
+      LOGICAL                    SPEC(ADI__MXDIM)  ! Axis specified in output?
       LOGICAL                    SWAP_1D           ! Special 1D swap?
       LOGICAL                    UNIF              ! Uniform axis widths?
 *
 *    Version :
 *
       CHARACTER*30 VERSION
-        PARAMETER  (VERSION = 'AXORDER Version 1.8-0')
+        PARAMETER  (VERSION = 'AXORDER Version 1.8-1')
 *-
 
 *    Check status
@@ -98,8 +98,8 @@
       CALL MSG_PRNT( VERSION )
 
 *    Get input object
-      CALL USI_ASSOC2( 'INP', 'OUT', 'READ', ILOC, OLOC, INPRIM,
-     :                                                  STATUS )
+      CALL USI_TASSOC2( 'INP', 'OUT', 'READ', IFID, OFID, STATUS )
+      CALL BDI_PRIM( IFID, INPRIM, STATUS )
       IF ( INPRIM ) THEN
         CALL MSG_PRNT( 'Primitive input object - not allowed' )
         STATUS = SAI__OK
@@ -108,30 +108,31 @@
 
 *    Check and map the data
       SWAP_1D = .FALSE.
-      CALL BDA_CHKDATA( ILOC, OK, NDIM, DIMS, STATUS )
+      CALL BDI_CHKDATA( IFID, OK, NDIM, DIMS, STATUS )
       IF ( .NOT. OK ) THEN
-        CALL MSG_PRNT( 'FATAL ERROR : Invalid data' )
         STATUS = SAI__ERROR
+        CALL ERR_REP( ' ', 'FATAL ERROR : Invalid data', STATUS )
       ELSE IF ( NDIM .EQ. 0 ) THEN
-        CALL MSG_PRNT( 'Object is scalar - no axes to swap!' )
         STATUS = SAI__ERROR
+        CALL ERR_REP( ' ', 'Object is scalar - no axes to swap!',
+     :                STATUS )
       ELSE IF ( NDIM .EQ. 1 ) THEN
         SWAP_1D = .TRUE.
         CALL MSG_PRNT( 'Swapping data and axis values' )
       END IF
       IF ( STATUS .NE. SAI__OK ) GOTO 99
-      CALL BDA_MAPDATA( ILOC, 'READ', IDPTR, STATUS )
+      CALL BDI_MAPDATA( IFID, 'READ', IDPTR, STATUS )
 
 *    Variance
-      CALL BDA_CHKVAR( ILOC, VOK, VNDIM, VDIMS, STATUS )
+      CALL BDI_CHKVAR( IFID, VOK, VNDIM, VDIMS, STATUS )
       IF ( VOK ) THEN
-        CALL BDA_MAPVAR( ILOC, 'READ', IVPTR, STATUS )
+        CALL BDI_MAPVAR( IFID, 'READ', IVPTR, STATUS )
       END IF
 
 *    Quality
-      CALL BDA_CHKQUAL( ILOC, QOK, QNDIM, QDIMS, STATUS )
+      CALL BDI_CHKQUAL( IFID, QOK, QNDIM, QDIMS, STATUS )
       IF ( QOK .AND. .NOT. SWAP_1D ) THEN
-        CALL BDA_MAPQUAL( ILOC, 'READ', IQPTR, STATUS )
+        CALL BDI_MAPQUAL( IFID, 'READ', IQPTR, STATUS )
       END IF
 
 *    Get new axis order if NDIM > 2
@@ -140,7 +141,7 @@
 *      Tell user about different axes
         CALL MSG_PRNT('Axes in input object are :')
         CALL MSG_PRNT(' ')
-        CALL AXIS_LIST( ILOC, NDIM, STATUS )
+        CALL AXIS_TLIST( IFID, NDIM, STATUS )
 
         NSEL = 0
         CALL USI_GET1I( 'SELAX', NDIM, SELAX, NSEL, STATUS )
@@ -166,19 +167,19 @@
       ELSE
 
 *      Donor dimensions not set yet
-        CALL ARR_INIT1L( .FALSE., DAT__MXDIM, SPEC, STATUS )
+        CALL ARR_INIT1L( .FALSE., ADI__MXDIM, SPEC, STATUS )
 
 *      Loop over input and create output dimensions
         DO I =1, NSEL
           IF ( ( SELAX(I) .GT. NDIM ) .OR. ( SELAX(I) .LT. 0 )  ) THEN
-            CALL MSG_PRNT( 'FATAL ERROR : No such axis in input'/
-     :                                                /' file!' )
             STATUS = SAI__ERROR
+            CALL ERR_REP( ' ', 'FATAL ERROR : No such axis in input'/
+     :                                            /' file!', STATUS )
           ELSE IF ( SPEC(I) ) THEN
             CALL MSG_SETI( 'AX', I )
-            CALL MSG_PRNT( 'FATAL ERROR : Axis ^AX is multiply'/
-     :                                 /' specified in output' )
             STATUS = SAI__ERROR
+            CALL ERR_REP( ' ', 'FATAL ERROR : Axis ^AX is multiply'/
+     :                             /' specified in output', STATUS )
           ELSE
             SPEC(I) = .TRUE.
             ODIMS(I) = DIMS(SELAX(I))
@@ -198,22 +199,22 @@
       END IF
 
 *    Create axis structure
-      CALL BDA_CREAXES( OLOC, NDIM, STATUS )
+      CALL BDI_CREAXES( OFID, NDIM, STATUS )
 
 *    Create and map output data
-      CALL BDA_CREDATA( OLOC, NDIM, ODIMS, STATUS )
-      CALL BDA_MAPDATA( OLOC, 'WRITE', ODPTR, STATUS )
+      CALL BDI_CREDATA( OFID, NDIM, ODIMS, STATUS )
+      CALL BDI_MAPDATA( OFID, 'WRITE', ODPTR, STATUS )
       IF ( STATUS .NE. SAI__OK ) GOTO 99
 
 *    One dimensional swap
       IF ( SWAP_1D ) THEN
 
 *      Map input axis
-        CALL BDA_MAPAXVAL( ILOC, 'READ', 1, IAPTR, STATUS )
+        CALL BDI_MAPAXVAL( IFID, 'READ', 1, IAPTR, STATUS )
 
 *      Create irregular output axis and map
-        CALL BDA_CREAXVAL( OLOC, 1, .FALSE., ODIMS(1), STATUS )
-        CALL BDA_MAPAXVAL( OLOC, 'WRITE', 1, OAPTR, STATUS )
+        CALL BDI_CREAXVAL( OFID, 1, .FALSE., ODIMS(1), STATUS )
+        CALL BDI_MAPAXVAL( OFID, 'WRITE', 1, OAPTR, STATUS )
 
 *      Copy input data to output axis, and input axis to output data
         CALL ARR_COP1R( ODIMS(1), %VAL(IDPTR), %VAL(OAPTR), STATUS )
@@ -221,20 +222,20 @@
 
 *      Variances become axis widths and vice versa
         IF ( VOK ) THEN
-          CALL BDA_CREAXWID( OLOC, 1, .FALSE., ODIMS(1), STATUS )
-          CALL BDA_MAPAXWID( OLOC, 'WRITE', 1, OWPTR, STATUS )
+          CALL BDI_CREAXWID( OFID, 1, .FALSE., ODIMS(1), STATUS )
+          CALL BDI_MAPAXWID( OFID, 'WRITE', 1, OWPTR, STATUS )
 
 *        Copy and square root
           CALL ARR_COP1R( ODIMS(1), %VAL(IVPTR), %VAL(OWPTR), STATUS )
           CALL ARR_SQRT1R( %VAL(OWPTR), ODIMS(1), STATUS )
 
         END IF
-        CALL BDA_CHKAXWID( ILOC, 1, IWOK, UNIF, NWID, STATUS )
+        CALL BDI_CHKAXWID( IFID, 1, IWOK, UNIF, NWID, STATUS )
         IF ( IWOK ) THEN
 
 *        Create variance array, and map it
-          CALL BDA_CREVAR( OLOC, 1, ODIMS, STATUS )
-          CALL BDA_MAPVAR( OLOC, 'WRITE', OVPTR, STATUS )
+          CALL BDI_CREVAR( OFID, 1, ODIMS, STATUS )
+          CALL BDI_MAPVAR( OFID, 'WRITE', OVPTR, STATUS )
 
 *        Copy widths and square
           CALL ARR_COP1R( ODIMS(1), %VAL(IWPTR), %VAL(OVPTR), STATUS )
@@ -244,52 +245,50 @@
 
 *      Just copy quality
         IF ( QOK ) THEN
-          CALL BDA_COPQUAL( ILOC, OLOC, STATUS )
+          CALL BDI_COPQUAL( IFID, OFID, STATUS )
         END IF
 
 *      Copy text strings
-        CALL BDA_GETLABEL( ILOC, LABEL, STATUS )
-        CALL BDA_GETUNITS( ILOC, UNITS, STATUS )
-        CALL BDA_PUTAXLABEL( OLOC, 1, LABEL, STATUS )
-        CALL BDA_PUTAXUNITS( OLOC, 1, UNITS, STATUS )
-        CALL BDA_GETAXUNITS( ILOC, 1, UNITS, STATUS )
-        CALL BDA_GETAXLABEL( ILOC, 1, LABEL, STATUS )
-        CALL BDA_PUTLABEL( OLOC, LABEL, STATUS )
-        CALL BDA_PUTUNITS( OLOC, UNITS, STATUS )
+        CALL BDI_GETLABEL( IFID, LABEL, STATUS )
+        CALL BDI_GETUNITS( IFID, UNITS, STATUS )
+        CALL BDI_PUTAXTEXT( OFID, 1, LABEL, UNITS, STATUS )
+        CALL BDI_GETAXTEXT( IFID, 1, LABEL, UNITS, STATUS )
+        CALL BDI_PUTLABEL( OFID, LABEL, STATUS )
+        CALL BDI_PUTUNITS( OFID, UNITS, STATUS )
 
 *    normal swap
       ELSE
 
 *      Copy axes
         DO I = 1, NDIM
-          CALL BDA_COPAXIS( ILOC, OLOC, SELAX(I), I, STATUS )
+          CALL BDI_COPAXIS( IFID, OFID, SELAX(I), I, STATUS )
         END DO
 
 *      Copy top-level text
-        CALL BDA_COPTEXT( ILOC, OLOC, STATUS )
+        CALL BDI_COPTEXT( IFID, OFID, STATUS )
 
 *      Create output variance and quality
         IF ( VOK ) THEN
-          CALL BDA_CREVAR( OLOC, NDIM, ODIMS, STATUS )
+          CALL BDI_CREVAR( OFID, NDIM, ODIMS, STATUS )
         END IF
         IF ( QOK ) THEN
-          CALL BDA_CREQUAL( OLOC, NDIM, ODIMS, STATUS )
-          CALL BDA_GETMASK( ILOC, MASK, STATUS )
-          CALL BDA_PUTMASK( OLOC, MASK, STATUS )
+          CALL BDI_CREQUAL( OFID, NDIM, ODIMS, STATUS )
+          CALL BDI_GETMASK( IFID, MASK, STATUS )
+          CALL BDI_PUTMASK( OFID, MASK, STATUS )
         END IF
 
 *      Map output variance and quality
         IF ( VOK ) THEN
-          CALL BDA_MAPVAR( OLOC, 'WRITE', OVPTR, STATUS )
+          CALL BDI_MAPVAR( OFID, 'WRITE', OVPTR, STATUS )
         END IF
         IF ( QOK ) THEN
-          CALL BDA_MAPQUAL( OLOC, 'WRITE', OQPTR, STATUS )
+          CALL BDI_MAPQUAL( OFID, 'WRITE', OQPTR, STATUS )
         END IF
         IF ( STATUS .NE. SAI__OK ) GOTO 99
 
 *      Pad out dimensions for 7D
-        IF ( NDIM .LT. DAT__MXDIM ) THEN
-          DO I = NDIM + 1, DAT__MXDIM
+        IF ( NDIM .LT. ADI__MXDIM ) THEN
+          DO I = NDIM + 1, ADI__MXDIM
             DIMS(I) = 1
             ODIMS(I) = 1
             SELAX(I) = I
@@ -311,19 +310,17 @@
       END IF
 
 *    Copy ancillary stuff
-      CALL BDA_COPMORE( ILOC, OLOC, STATUS )
+      CALL BDI_COPMORE( IFID, OFID, STATUS )
 
 *    History
-      CALL HIST_COPY( ILOC, OLOC, STATUS )
-      CALL HIST_ADD( OLOC, VERSION, STATUS )
+      CALL HSI_COPY( IFID, OFID, STATUS )
+      CALL HSI_ADD( OFID, VERSION, STATUS )
       CALL USI_NAMEI( NLINES, HTXT(2), STATUS )
-      CALL HIST_PTXT( OLOC, NLINES + 1, HTXT, STATUS )
+      CALL HSI_PTXT( OFID, NLINES + 1, HTXT, STATUS )
 
 *    Release datasets
-      CALL BDA_RELEASE( ILOC, STATUS )
-      CALL BDA_RELEASE( OLOC, STATUS )
-      CALL USI_ANNUL( ILOC, STATUS )
-      CALL USI_ANNUL( OLOC, STATUS )
+      CALL BDI_RELEASE( IFID, STATUS )
+      CALL BDI_RELEASE( OFID, STATUS )
 
 *    Tidy up
  99   CALL AST_CLOSE()
