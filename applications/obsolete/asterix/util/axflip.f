a9fe46f6bba872ada97d61522d26bee3441dde35
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-03-29 08:35:26 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/util/axflip.f
+++ applications/obsolete/asterix/util/axflip.f
@@ -26,6 +26,7 @@
 *     18 Jun 90 : V1.2-0  Bug with irregular axes fixed (DJA)
 *     10 Apr 91 : V1.4-0  Copes with primitives and datasets with no axes (DJA)
 *     24 Nov 94 : V1.8-0  Now use USI for user interface (DJA)
+*     28 Mar 95 : V1.8-1  New data interface (DJA)
 *
 *    Type definitions :
 *
@@ -34,7 +35,7 @@
 *    Global constants :
 *
       INCLUDE 'SAE_PAR'
-      INCLUDE 'DAT_PAR'
+      INCLUDE 'ADI_PAR'
 *
 *    Status :
 *
@@ -49,28 +50,28 @@
       CHARACTER*80            	HTXT(5)    		! History text
       CHARACTER*10              NSTR              	!
 
-      CHARACTER*(DAT__SZLOC)    ILOC              	! Input dataset
-      CHARACTER*(DAT__SZLOC)    OLOC              	! Output dataset
-      CHARACTER*(DAT__SZTYP)    OBTYPE            	! Object type
+      CHARACTER*(ADI__SZTYP)    OBTYPE            	! Object type
 
-      INTEGER                   DIMS(DAT__MXDIM)  	! Input dimensions
+      INTEGER                   DIMS(ADI__MXDIM)  	! Input dimensions
       INTEGER                   IDPTR,IVPTR,IQPTR 	! Input data pointers
-      INTEGER                   QDIMS(DAT__MXDIM) 	! Input quality dimensions
-      INTEGER                   SELAX(DAT__MXDIM) 	! Axes to flip
-      INTEGER                   VDIMS(DAT__MXDIM) 	! Input variance dimensions
+      INTEGER                   QDIMS(ADI__MXDIM) 	! Input quality dimensions
+      INTEGER                   SELAX(ADI__MXDIM) 	! Axes to flip
+      INTEGER                   VDIMS(ADI__MXDIM) 	! Input variance dimensions
 
       INTEGER                   HLEN              	! Length of history text
       INTEGER                   I                 	! Loop over dimensions
+      INTEGER			IFID			! Input dataset id
       INTEGER                   NAX               	! # of AXIS objects
       INTEGER                   NDIGIT            	! Length of number in char
       INTEGER                   NDIM              	! Dimensionality
       INTEGER                   NLINES            	! History lines used
       INTEGER                   ODPTR,OVPTR,OQPTR 	! Output data pointers
+      INTEGER			OFID			! Output dataset id
       INTEGER                   QNDIM             	! Quality dimensionality
       INTEGER                   VNDIM             	! Variance dimensionality
       INTEGER                   NSEL              	! Number of axes to flip
 
-      LOGICAL                   FLIP(DAT__MXDIM)  	! Flip these axes?
+      LOGICAL                   FLIP(ADI__MXDIM)  	! Flip these axes?
       LOGICAL                   FLIP_OK           	! Carry on with flip?
       LOGICAL                   INPRIM            	! Input primitive?
       LOGICAL                   OK, VOK, QOK      	! Input objects ok?
@@ -78,7 +79,7 @@
 *    Version :
 *
       CHARACTER*30 		VERSION
-        PARAMETER 		( VERSION = 'AXFLIP Version 1.8-0' )
+        PARAMETER 		( VERSION = 'AXFLIP Version 1.8-1' )
 *-
 
 *    Check status
@@ -91,39 +92,41 @@
       CALL MSG_PRNT( VERSION )
 
 *    Get input object
-      CALL USI_ASSOCI( 'INP', 'READ', ILOC, INPRIM, STATUS )
+      CALL USI_TASSOCI( 'INP', '*', 'READ', IFID, STATUS )
       IF ( STATUS .NE. SAI__OK ) GOTO 99
 
 *    Check and map the data
-      CALL BDA_CHKDATA( ILOC, OK, NDIM, DIMS, STATUS )
+      CALL BDI_CHKDATA( IFID, OK, NDIM, DIMS, STATUS )
       IF ( .NOT. OK ) THEN
-         CALL MSG_PRNT( 'FATAL ERROR : Invalid data' )
          STATUS = SAI__ERROR
+         CALL ERR_REP( ' ', 'FATAL ERROR : Invalid data', STATUS )
       ELSE IF ( NDIM .EQ. 0 ) THEN
-         CALL MSG_PRNT( 'Object is scalar - no axes to flip!' )
          STATUS = SAI__ERROR
+         CALL ERR_REP( ' ', 'Object is scalar - no axes to flip!',
+     :                 STATUS )
       ELSE
-         CALL BDA_MAPDATA( ILOC, 'READ', IDPTR, STATUS )
+         CALL BDI_MAPDATA( IFID, 'READ', IDPTR, STATUS )
       END IF
       IF ( STATUS .NE. SAI__OK ) GOTO 99
 
 *    Variance
-      CALL BDA_CHKVAR( ILOC, VOK, VNDIM, VDIMS, STATUS )
+      CALL BDI_CHKVAR( IFID, VOK, VNDIM, VDIMS, STATUS )
       IF ( VOK ) THEN
-        CALL BDA_MAPVAR( ILOC, 'READ', IVPTR, STATUS )
+        CALL BDI_MAPVAR( IFID, 'READ', IVPTR, STATUS )
       END IF
 
 *    Quality
-      CALL BDA_CHKQUAL( ILOC, QOK, QNDIM, QDIMS, STATUS )
+      CALL BDI_CHKQUAL( IFID, QOK, QNDIM, QDIMS, STATUS )
       IF ( QOK ) THEN
-        CALL BDA_MAPQUAL( ILOC, 'READ', IQPTR, STATUS )
+        CALL BDI_MAPQUAL( IFID, 'READ', IQPTR, STATUS )
       END IF
 
 *    Get number of input axes (ie axis structures)
+      CALL BDI_PRIM( IFID, INPRIM, STATUS )
       IF ( INPRIM ) THEN
         NAX = 0
       ELSE
-        CALL BDA_CHKAXES( ILOC, NAX, STATUS )
+        CALL BDI_CHKAXES( IFID, NAX, STATUS )
       END IF
 
 *    Tell user about different axes
@@ -136,7 +139,7 @@
       ELSE
         CALL MSG_PRNT( 'Axes present in dataset are:' )
         CALL MSG_PRNT( ' ' )
-        CALL AXIS_LIST( ILOC, NDIM, STATUS )
+        CALL AXIS_TLIST( IFID, NDIM, STATUS )
       END IF
 
 *    Select axes to flip if NDIM > 1
@@ -150,7 +153,7 @@
       END IF
 
 *    Set up the FLIP array
-      CALL ARR_INIT1L( .FALSE., DAT__MXDIM, FLIP, STATUS )
+      CALL ARR_INIT1L( .FALSE., ADI__MXDIM, FLIP, STATUS )
       FLIP_OK = .FALSE.
       IF ( ( NDIM .EQ. 1 ) .OR. ( NSEL .EQ. 1 ) ) THEN
         HTXT(1) = 'Swapped axis : '
@@ -172,57 +175,52 @@
       END DO
 
 *    Output dataset
-      IF ( INPRIM ) THEN
-        OBTYPE = 'BINDS'
-      ELSE
-        CALL DAT_TYPE( ILOC, OBTYPE, STATUS )
-      END IF
-      CALL USI_ASSOCO( 'OUT', OBTYPE, OLOC, STATUS )
+      CALL USI_TASSOCO( 'OUT', 'BINDS', OFID, STATUS )
       IF ( STATUS .NE. SAI__OK ) GOTO 99
 
 *    Copy axes
       IF ( NAX .GT. 0 ) THEN
 
 *      Create axis structure
-        CALL BDA_CREAXES( OLOC, NDIM, STATUS )
+        CALL BDI_CREAXES( OFID, NDIM, STATUS )
 
         DO I = 1, NDIM
           IF ( FLIP(I) ) THEN
 
 *          Copy with flip
-            CALL AXFLIP_COPAXIS( ILOC, OLOC, I, STATUS )
+            CALL AXFLIP_COPAXIS( IFID, OFID, I, STATUS )
 
           ELSE
 
 *          Simple axis copy
-            CALL BDA_COPAXIS( ILOC, OLOC, I, I, STATUS )
+            CALL BDI_COPAXIS( IFID, OFID, I, I, STATUS )
 
           END IF
         END DO
       END IF
 
 *    Copy output data, variance and quality
-      CALL BDA_COPDATA( ILOC, OLOC, STATUS )
+      CALL BDI_COPDATA( IFID, OFID, STATUS )
       IF ( VOK ) THEN
-        CALL BDA_COPVAR( ILOC, OLOC, STATUS )
+        CALL BDI_COPVAR( IFID, OFID, STATUS )
       END IF
       IF ( QOK ) THEN
-        CALL BDA_COPQUAL( ILOC, OLOC, STATUS )
+        CALL BDI_COPQUAL( IFID, OFID, STATUS )
       END IF
 
 *    Map output data
-      CALL BDA_MAPDATA( OLOC, 'WRITE', ODPTR, STATUS )
+      CALL BDI_MAPDATA( OFID, 'WRITE', ODPTR, STATUS )
       IF ( VOK ) THEN
-        CALL BDA_MAPVAR( OLOC, 'WRITE', OVPTR, STATUS )
+        CALL BDI_MAPVAR( OFID, 'WRITE', OVPTR, STATUS )
       END IF
       IF ( QOK ) THEN
-        CALL BDA_MAPQUAL( OLOC, 'WRITE', OQPTR, STATUS )
+        CALL BDI_MAPQUAL( OFID, 'WRITE', OQPTR, STATUS )
       END IF
       IF ( STATUS .NE. SAI__OK ) GOTO 99
 
 *    Pad out dimensions for 7D
-      IF ( NDIM .LT. DAT__MXDIM ) THEN
-        DO I = NDIM + 1, DAT__MXDIM
+      IF ( NDIM .LT. ADI__MXDIM ) THEN
+        DO I = NDIM + 1, ADI__MXDIM
           DIMS(I) = 1
         END DO
       END IF
@@ -240,27 +238,25 @@
 
 *    Copy ancillary stuff
       IF ( .NOT. INPRIM ) THEN
-        CALL BDA_COPMORE( ILOC, OLOC, STATUS )
-        CALL BDA_COPTEXT( ILOC, OLOC, STATUS )
+        CALL BDI_COPMORE( IFID, OFID, STATUS )
+        CALL BDI_COPTEXT( IFID, OFID, STATUS )
       END IF
 
 *    History
       IF ( INPRIM ) THEN
-        CALL HIST_NEW( OLOC, STATUS )
+        CALL HSI_NEW( OFID, STATUS )
       ELSE
-        CALL HIST_COPY( ILOC, OLOC, STATUS )
+        CALL HSI_COPY( IFID, OFID, STATUS )
       END IF
-      CALL HIST_ADD( OLOC, VERSION, STATUS )
+      CALL HSI_ADD( OFID, VERSION, STATUS )
 
 *    Get input file spec
       CALL USI_NAMEI( NLINES, HTXT(2), STATUS )
-      CALL HIST_PTXT( OLOC, NLINES+1, HTXT, STATUS )
+      CALL HSI_PTXT( OFID, NLINES+1, HTXT, STATUS )
 
 *    Release files
-      CALL BDA_RELEASE( ILOC, STATUS )
-      CALL BDA_RELEASE( OLOC, STATUS )
-      CALL USI_ANNUL( ILOC, STATUS )
-      CALL USI_ANNUL( OLOC, STATUS )
+      CALL BDI_RELEASE( IFID, STATUS )
+      CALL BDI_RELEASE( OFID, STATUS )
 
 *    Tidy up
  99   CALL AST_CLOSE()
@@ -271,7 +267,7 @@
 
 
 *+  AXFLIP_COPAXIS - Copy axis and flip direction
-      SUBROUTINE AXFLIP_COPAXIS( ILOC, OLOC, AXN, STATUS )
+      SUBROUTINE AXFLIP_COPAXIS( IFID, OFID, AXN, STATUS )
 *    Description :
 *    Method :
 *    Deficiencies :
@@ -291,16 +287,12 @@
 *    Global constants :
 *
       INCLUDE 'SAE_PAR'
-      INCLUDE 'DAT_PAR'
 *
 *    Import :
 *
-      CHARACTER*(DAT__SZLOC)  	ILOC              	! Input dataset
+      INTEGER			IFID			! Input dataset
       INTEGER                 	AXN               	! Axis to flip
-*
-*    Import-Export :
-*
-      CHARACTER*(DAT__SZLOC)  	OLOC              	! Output dataset
+      INTEGER			OFID			! Output dataset
 *
 *    Status :
 *
@@ -323,41 +315,41 @@
 
 *    Validate input axis
       OK = .FALSE.
-      CALL BDA_CHKAXVAL( ILOC, AXN, OK, REG, SIZE, STATUS )
+      CALL BDI_CHKAXVAL( IFID, AXN, OK, REG, SIZE, STATUS )
       IF ( OK ) THEN
-         CALL BDA_CREAXVAL( OLOC, AXN, REG, SIZE, STATUS )
+         CALL BDI_CREAXVAL( OFID, AXN, REG, SIZE, STATUS )
          IF ( REG ) THEN
-            CALL BDA_GETAXVAL( ILOC, AXN, BASE, SCALE, SIZE, STATUS )
-            CALL BDA_PUTAXVAL( OLOC, AXN, BASE+(SIZE-1)*SCALE, -SCALE,
+            CALL BDI_GETAXVAL( IFID, AXN, BASE, SCALE, SIZE, STATUS )
+            CALL BDI_PUTAXVAL( OFID, AXN, BASE+(SIZE-1)*SCALE, -SCALE,
      :                                                  SIZE, STATUS )
          ELSE
-            CALL BDA_MAPAXVAL( ILOC, 'READ', AXN, IAPTR, STATUS )
-            CALL BDA_MAPAXVAL( OLOC, 'WRITE', AXN, OAPTR, STATUS )
+            CALL BDI_MAPAXVAL( IFID, 'READ', AXN, IAPTR, STATUS )
+            CALL BDI_MAPAXVAL( OFID, 'WRITE', AXN, OAPTR, STATUS )
             CALL ARR_FLIP1R( SIZE, %VAL(IAPTR), %VAL(OAPTR), STATUS )
-            CALL BDA_UNMAPAXVAL( ILOC, AXN, STATUS )
-            CALL BDA_UNMAPAXVAL( OLOC, AXN, STATUS )
+            CALL BDI_UNMAPAXVAL( IFID, AXN, STATUS )
+            CALL BDI_UNMAPAXVAL( OFID, AXN, STATUS )
          END IF
       END IF
 
 *    now the widths
       OK = .FALSE.
-      CALL BDA_CHKAXWID( ILOC, AXN, OK, REG, SIZE, STATUS )
+      CALL BDI_CHKAXWID( IFID, AXN, OK, REG, SIZE, STATUS )
       IF ( OK ) THEN
          IF ( REG ) THEN
-            CALL BDA_GETAXWID( ILOC, AXN, BASE, SCALE, SIZE, STATUS )
-            CALL BDA_PUTAXWID( OLOC, AXN, BASE+(SIZE-1)*SCALE, -SCALE,
+            CALL BDI_GETAXWID( IFID, AXN, BASE, SCALE, SIZE, STATUS )
+            CALL BDI_PUTAXWID( OFID, AXN, BASE+(SIZE-1)*SCALE, -SCALE,
      :                                                  SIZE, STATUS )
          ELSE
-            CALL BDA_MAPAXWID( ILOC, 'READ', AXN, IAPTR, STATUS )
-            CALL BDA_CREAXWID( OLOC, AXN, .FALSE., SIZE, STATUS )
-            CALL BDA_MAPAXWID( OLOC, 'WRITE', AXN, OAPTR, STATUS )
+            CALL BDI_MAPAXWID( IFID, 'READ', AXN, IAPTR, STATUS )
+            CALL BDI_CREAXWID( OFID, AXN, .FALSE., SIZE, STATUS )
+            CALL BDI_MAPAXWID( OFID, 'WRITE', AXN, OAPTR, STATUS )
             CALL ARR_FLIP1R( SIZE, %VAL(IAPTR), %VAL(OAPTR), STATUS )
-            CALL BDA_UNMAPAXWID( ILOC, AXN, STATUS )
-            CALL BDA_UNMAPAXWID( OLOC, AXN, STATUS )
+            CALL BDI_UNMAPAXWID( IFID, AXN, STATUS )
+            CALL BDI_UNMAPAXWID( OFID, AXN, STATUS )
          END IF
       END IF
 
 *    and copy the text
-      CALL BDA_COPAXTEXT( ILOC, OLOC, AXN, AXN, STATUS )
+      CALL BDI_COPAXTEXT( IFID, OFID, AXN, AXN, STATUS )
 
       END
