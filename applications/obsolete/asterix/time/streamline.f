d64373c43693f1468a1fb6c5303a8fae351c8c71
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-04-11 14:32:16 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/time/streamline.f
+++ applications/obsolete/asterix/time/streamline.f
@@ -20,112 +20,76 @@
       IMPLICIT NONE
 *    Global constants :
       INCLUDE 'SAE_PAR'
-      INCLUDE 'DAT_PAR'
       INCLUDE 'PAR_ERR'
 *    Status :
       INTEGER STATUS
-*    Global variables :
-*    Functions :
-*    Local Constants :
 *    Local variables :
-      CHARACTER*30 VERSION                      !Version of the software
-      CHARACTER*(DAT__SZLOC) ILOC               !Locator to the input datafile
-      CHARACTER*(DAT__SZLOC) OLOC               !Locator to the output datafile
-      CHARACTER*(DAT__SZLOC) MLOC               !Locator to the MORE structure
-      CHARACTER*(DAT__SZLOC) ALOC               !Locator to the axis structure
-      CHARACTER*(DAT__SZTYP) INTYPE             !Type of input datafile
-      CHARACTER*80 TITLE                        !Title of file
-      CHARACTER*40 UNITS                        !Units of data
       CHARACTER*80 PTEXT(4)                     !History text
       LOGICAL LVAR                              !Is the variance array present
+      INTEGER			IFID, OFID
       INTEGER DPNTR,TPNTR,QPNTR,VPNTR           !Pointers to arrays.
       INTEGER NTOT                              !Total no. of data points
       INTEGER NGOOD                             !No. of good quality data points
       INTEGER NLINES                            !No of history list items
+      INTEGER			ODPTR,OVPTR,OAPTR	! Output arrays
 *
-* Check status :
+*  Version:
+*
+      CHARACTER*30		VERSION
+        PARAMETER	 	( VERSION = 'STREAMLINE Version 1.8-0')
+*-
+
+*  Check status :
       IF (STATUS .NE. SAI__OK) RETURN
-*
-      VERSION='STREAMLINE Version 1.5-1'
-      CALL MSG_OUT(' ',VERSION, STATUS)
-*
-* Initialise the Asterix common blocks.
-      CALL AST_INIT
-*
-* Get input data into an array of GOOD values only.
-      CALL TIM_GETDATA(ILOC, NTOT, NGOOD, TPNTR, DPNTR, LVAR,
-     &                  VPNTR, STATUS)
-*
-* Test that bad quality points were found
+
+*  Version
+      CALL MSG_PRNT( VERSION )
+
+*  Initialise ASTERIX
+      CALL AST_INIT()
+
+*  Get input data into an array of GOOD values only.
+      CALL TIM_GETDAT( IFID, NTOT, NGOOD, TPNTR, DPNTR, LVAR,
+     :                  VPNTR, STATUS)
+
+*  Test that bad quality points were found
       IF (NTOT .EQ. NGOOD) THEN
          CALL MSG_PRNT('**No bad quality data points were found**')
          GOTO 999
       ENDIF
-*
-      CALL DAT_TYPE(ILOC, INTYPE, STATUS)
-*
-* Create output file
-      CALL USI_ASSOCO('OUT', INTYPE, OLOC, STATUS)
-*
-      IF (STATUS .NE. SAI__OK) GOTO 999
-*
-* Copy MORE box from old file into new file
-      CALL DAT_FIND(ILOC, 'MORE', MLOC, STATUS)
 
-      CALL DAT_COPY(MLOC, OLOC, 'MORE', STATUS)
-*
-* Copy units and title
-      CALL BDA_GETTITLE(ILOC, TITLE, STATUS)
-      CALL BDA_PUTTITLE(OLOC, TITLE, STATUS)
-*
-      CALL BDA_GETUNITS(ILOC, UNITS, STATUS)
-      CALL BDA_PUTUNITS(OLOC, UNITS, STATUS)
-*
-      IF (STATUS .NE. SAI__OK) THEN
-         CALL MSG_PRNT('WARNING: Error copying MORE box')
-         STATUS = SAI__OK
-      ENDIF
-*
-* Write data array into output file
-      CALL HDX_PUTR(OLOC, 'DATA_ARRAY', NGOOD, %val(DPNTR), STATUS)
-*
-* Write variance into output array if required
-      CALL HDX_PUTR(OLOC, 'VARIANCE', NGOOD, %val(VPNTR), STATUS)
-*
-* Write axis into output file
-      CALL BDA_CREAXES(OLOC, 1, STATUS)
-      CALL BDA_CREAXVAL(OLOC, 1, .FALSE., NGOOD, STATUS)
-*
-* Find axis array - and write in primitive values
-      CALL BDA_LOCAXVAL(OLOC, 1, ALOC, STATUS)
-*
-      CALL DAT_PUT1R(ALOC, NGOOD, %val(TPNTR), STATUS)
-*
-      IF (STATUS .NE. SAI__OK) THEN
-         CALL MSG_PRNT('Error writing axis values')
-         GOTO 999
-      ENDIF
-*
-* Copy and update history
-      CALL HIST_COPY(ILOC, OLOC, STATUS)
-*
-      CALL HIST_ADD(OLOC, VERSION, STATUS)
-*
-      CALL USI_NAMEI(NLINES, PTEXT, STATUS)
-*
-      CALL HIST_PTXT(OLOC, NLINES, PTEXT, STATUS)
-*
-      IF (STATUS .NE. SAI__OK) THEN
-         CALL MSG_PRNT('Error writing history')
-         GOTO 999
-      ENDIF
-*
-999   CONTINUE
-*
-      CALL AST_CLOSE(STATUS)
-*
-      IF (STATUS .NE. SAI__OK) THEN
-         CALL ERR_REP(' ','from STREAMLINE',STATUS)
-      ENDIF
-*
+*  Create output file
+      CALL USI_TASSOCO('OUT', 'BINDS', OFID, STATUS)
+      IF (STATUS .NE. SAI__OK) GOTO 999
+
+*  Propogate from input...
+      CALL BDI_COPMORE( IFID, OFID, STATUS )
+      CALL BDI_COPTEXT( IFID, OFID, STATUS )
+
+*  Write data array into output file
+      CALL BDI_CREDATA( OFID, 1, NGOOD, STATUS )
+      CALL BDA_MAPDATA( OFID, 'WRITE', ODPTR, STATUS )
+      CALL ARR_COP1R( NGOOD, %VAL(DPNTR), %VAL(ODPTR), STATUS )
+
+*  Write variance into output array if required
+      CALL BDI_CREVAR( OFID, 1, NGOOD, STATUS )
+      CALL BDA_MAPVAR( OFID, 'WRITE', OVPTR, STATUS )
+      CALL ARR_COP1R( NGOOD, %VAL(VPNTR), %VAL(OVPTR), STATUS )
+
+*  Write axis into output file
+      CALL BDI_CREAXES( OFID, 1, STATUS )
+      CALL BDI_CREAXVAL( OFID, 1, .FALSE., NGOOD, STATUS )
+      CALL BDI_MAPAXVAL( OFID, 'WRITE', 1, OAPTR, STATUS )
+      CALL ARR_COP1R( NGOOD, %VAL(TPNTR), %VAL(OAPTR), STATUS )
+
+*  Copy and update history
+      CALL HSI_COPY( IFID, OFID, STATUS )
+      CALL HSI_ADD( OFID, VERSION, STATUS )
+      CALL USI_NAMEI( NLINES, PTEXT, STATUS )
+      CALL HSI_PTXT( OFID, NLINES, PTEXT, STATUS )
+
+*  Tidy up
+ 99   CALL AST_CLOSE()
+      CALL AST_ERR( STATUS )
+
       END
