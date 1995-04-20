75df247fde68a024d11777e0722169d97f56e97f
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-04-20 13:59:26 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/time/evphase.f
+++ applications/obsolete/asterix/time/evphase.f
@@ -1,5 +1,5 @@
 *+  EVPHASE - Adds a PHASE lists to input event dataset
-      SUBROUTINE EVPHASE(STATUS)
+      SUBROUTINE EVPHASE( STATUS )
 *
 *    Description :
 *
@@ -36,6 +36,7 @@
 *     24 Jul 91 : V1.5-1 Updated TAI definition (DJA)
 *      7 Oct 92 : V1.7-0 Minor changes for UNIX port (DJA)
 *     24 Nov 94 : V1.8-0 Now use USI for user interface (DJA)
+*     20 Apr 95 : V1.8-1 No longer uses BDA routines (DJA)
 *
 *    Type Definitions :
 *
@@ -52,30 +53,31 @@
 *
 *    Local variables :
 *
-      CHARACTER*(DAT__SZLOC) ILOC                        ! Input dataset
-      CHARACTER*(DAT__SZLOC) OLOC                        ! Output dataset
-      CHARACTER*(DAT__SZLOC) HLOC                        ! Input HEADER
-      CHARACTER*(DAT__SZLOC) PLOC                        ! Phase object
-      CHARACTER*80           EPHEMERIS(3)                ! History entry
+      CHARACTER*(DAT__SZLOC) 	ILOC                    ! Input dataset
+      CHARACTER*(DAT__SZLOC) 	OLOC                    ! Output dataset
+      CHARACTER*(DAT__SZLOC) 	PLOC                    ! Phase object
+      CHARACTER*80           	EPHEMERIS(3)            ! History entry
 
-      DOUBLE PRECISION       BASE_TAI                    ! Start time of observation (days after 1st Jan 1972)
-      DOUBLE PRECISION       EPHEM_TAI                   ! Ephemeris base time
-                                                         ! in TAI
-      DOUBLE PRECISION       COEFF(3)                    ! Ephemeris coefficients
+      DOUBLE PRECISION       	BASETAI                 ! Start time of observation (days after 1st Jan 1972)
+      DOUBLE PRECISION       	EPHEM_TAI               ! Ephemeris base time
+                                                        ! in TAI
+      DOUBLE PRECISION       	COEFF(3)                ! Ephemeris coefficients
 
-      INTEGER                C                           ! Loop over COEFF
-      INTEGER                NELM                        ! No of data poits
-      INTEGER                PPTR                        ! Phase list data
-      INTEGER                TPTR                        ! Time list data
+      INTEGER                	C                       ! Loop over COEFF
+      INTEGER			IFID			! Input dataset id
+      INTEGER                	NELM                    ! No of data poits
+      INTEGER			OFID			! Output dataset id
+      INTEGER                	PPTR                    ! Phase list data
+      INTEGER			TIMID			! Timing info
+      INTEGER                	TPTR                    ! Time list data
 
-      LOGICAL                OK                          ! Input data ok?
-      LOGICAL                INPRIM                      ! Input primitive?
-      LOGICAL                PHASE_THERE                 ! Phase exists already?
+      LOGICAL                	OK                      ! Input data ok?
+      LOGICAL                	PHASE_THERE             ! Phase exists already?
 *
 *    Version :
 *
-      CHARACTER*25           VERSION
-         PARAMETER         ( VERSION = 'EVPHASE Version 1.8-0' )
+      CHARACTER*25 		VERSION
+        PARAMETER         	( VERSION = 'EVPHASE Version 1.8-1' )
 *-
 
 *    Version
@@ -85,26 +87,27 @@
       CALL AST_INIT()
 
 *    Associate input and output datasets
-      CALL USI_ASSOC2('INP', 'OUT', 'READ', ILOC, OLOC, INPRIM, STATUS)
-      IF ( INPRIM ) THEN
-        CALL MSG_PRNT('FATAL ERROR: Input object must be a dataset')
-        STATUS = SAI__ERROR
-      END IF
+      CALL USI_TASSOC2('INP', 'OUT', 'READ', IFID, OFID, STATUS )
       IF ( STATUS .NE. SAI__OK ) GOTO 99
 
+*    Extract locators
+      CALL ADI1_GETLOC( IFID, ILOC, STATUS )
+      CALL ADI1_GETLOC( OFID, OLOC, STATUS )
+
 *    Get observation start time
-      CALL BDA_LOCHEAD( ILOC, HLOC, STATUS )
-      CALL HDX_OK( HLOC, 'BASE_TAI', OK, STATUS )
+      CALL TCI_GETID( IFID, TIMID, STATUS )
+      CALL ADI_THERE( TIMID, 'TAIObs', OK, STATUS )
       IF ( OK ) THEN
-        CALL CMP_GET0D( HLOC, 'BASE_TAI', BASE_TAI, STATUS )
+        CALL ADI_CGET0D( TIMID, 'TAIObs', BASETAI, STATUS )
       ELSE
-        CALL MSG_PRNT( 'FATAL ERROR: No BASE_TAI in file header' )
         STATUS = SAI__ERROR
+        CALL ERR_REP( ' ', 'FATAL ERROR: No base atomic time'/
+     :                /' in file header', STATUS )
       END IF
       IF ( STATUS .NE. SAI__OK ) GOTO 99
 
 *    Copy input to output
-      CALL HDX_COPY( ILOC, OLOC, STATUS )
+      CALL ADI_FCOPY( IFID, OFID, STATUS )
 
 *    Find TIME list and its length
       CALL LIST_OK( ILOC, 'RAW_TIMETAG', OK, STATUS )
@@ -140,7 +143,7 @@
 
 *    Write ephemeris values to EPHEMERIS
       DO C = 1, 3
-        WRITE(EPHEMERIS(C), '(15X,A,I1,A,G15.5)')
+        WRITE(EPHEMERIS(C), '(A,I1,A,G15.5)')
      :                            'Coefficient ',C,' = ',COEFF(C)
       END DO
 
@@ -152,7 +155,7 @@
 
 *    Find difference from dataset reference time in seconds, and other
 *    coefficents in seconds
-      COEFF(1) = (EPHEM_TAI - BASE_TAI ) * 86400.0D0
+      COEFF(1) = (EPHEM_TAI - BASETAI) * 86400.0D0
       COEFF(2) = COEFF(2) * 86400.0D0
       COEFF(3) = COEFF(3) * 86400.0D0
 
@@ -160,11 +163,8 @@
       CALL EVPHASE_INT( NELM, %VAL(TPTR), COEFF, %VAL(PPTR), STATUS )
 
 *    History
-      CALL HIST_ADD( OLOC, VERSION, STATUS )
-      CALL HIST_PTXT( OLOC, 3, EPHEMERIS, STATUS )
-
-*    Free dataset
-      CALL BDA_RELEASE( ILOC, STATUS )
+      CALL HSI_ADD( OFID, VERSION, STATUS )
+      CALL HSI_PTXT( OFID, 3, EPHEMERIS, STATUS )
 
 *    Tidy up
  99   CALL AST_CLOSE
