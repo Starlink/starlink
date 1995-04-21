cd7f1d13910bc0f5604755c083ce1e432222a577
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-04-21 14:06:40 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/spec/ssim.f
+++ applications/obsolete/asterix/spec/ssim.f
@@ -1,12 +1,16 @@
 *+  SSIM - Generates simulated spectral dataset
       SUBROUTINE SSIM( STATUS )
+*
 *    Description :
+*
 *     A composite spectral model is folded through an instrument response
 *     (for a given observation duration) and a background spectrum and Poisson
 *     noise are optionally added, to give a simulated spectral observation.
 *     Since results are given in count/chan/sec the observation time need
 *     only be specified where Poisson errors are to be included.
+*
 *    Environment parameters :
+*
 *     INP=UNIV(R)
 *            dataset containing instrument response
 *     MODEL=UNIV(R)
@@ -21,7 +25,9 @@
 *            redshift
 *     OUT=UNIV(W)
 *            output spectrum
+*
 *    Method :
+*
 *     The program will now work from any object containing a suitable INSTR_RESP
 *     component. Axis data are generated from the information in the response
 *     matrix itself. The OBDAT structure is only used to pass the number of
@@ -52,11 +58,13 @@
 *      8 Sep 93 : V1.7-0 Added SPEC_INIT call and removed reference to
 *                        SPEC_CMN_RZ (DJA)
 *     24 Nov 94 : V1.8-0 Now use USI for user interface (DJA)
+*     21 Apr 95 : V1.8-1 Updated data interface (DJA)
 *
 *    Type definitions :
 	IMPLICIT NONE
 *    Global constants :
 	INCLUDE 'SAE_PAR'
+	INCLUDE 'ADI_PAR'
 	INCLUDE 'DAT_PAR'
 	INCLUDE 'PAR_ERR'
 	INCLUDE 'FIT_PAR'
@@ -71,27 +79,13 @@
 	RECORD/PREDICTION/ PREDDAT	! Data predicted by model
 	RECORD/INSTR_RESP/ INSTR	! Instrument response
 	RECORD /MODEL_SPEC/ MODEL	! Model specification
-	CHARACTER*(DAT__SZLOC) ILOC	! Locator to input data (or ref object)
-	CHARACTER*(DAT__SZLOC) OLOC	! Output spectrum dataset
-	CHARACTER*(DAT__SZLOC) BGLOC	! Background spectrum
-	CHARACTER*(DAT__SZLOC) MLOC	! Locator to model_spec object
-	CHARACTER*(DAT__SZLOC) CHLOC	! Locator to CHANNEL compt of response
-	CHARACTER*(DAT__SZLOC) ASTLOC	! Locator to ASTERIX box
-	CHARACTER*(DAT__SZLOC) HLOC	! Locator to HEADER
-	CHARACTER*(DAT__SZLOC) LOC	! Locator
+
 	CHARACTER*100 BGOBJ		! B/g dataset name
 	CHARACTER*100 FILE		! File name
 	CHARACTER*80 TELES		! Telescope name
 	CHARACTER*80 INSTRUM		! Instrument name
-	CHARACTER*80 CTOBS		! Character conversion of TOBS
-	CHARACTER*80 MODSPEC		! Spectral model specification
 	CHARACTER*80 LEGEND(3)		! Legend for graphics
-	LOGICAL FROZEN(NPAMAX)		! Frozen parameter flag
-	LOGICAL POISS			! Poisson errors required?
-	LOGICAL BG			! B/g spectrum to be added?
-	LOGICAL THERE			! Component present?
-	LOGICAL FOUND			! Instrument response found?
-	LOGICAL OK			! Data OK?
+
 	INTEGER DPTR			! Pointer to data array
 	INTEGER VPTR			! Pointer to variance array
 	INTEGER BPTR			! Pointer to b/g array
@@ -99,10 +93,9 @@
 	INTEGER AXPTR			! Pointer to o/p axis array
 	INTEGER I			!
 	INTEGER NPAR			! No of parameters
-	INTEGER NCHAN			! No of channels in spectrum
-	INTEGER NCHAR			! No of characters in string
 	INTEGER NDIM			! Dimensionality
-	INTEGER DIMS(DAT__MXDIM)	! Dimensions
+	INTEGER DIMS(ADI__MXDIM)	! Dimensions
+
 	REAL TOBS			! Observation time (secs)
 	REAL TOTAL			! Total count/sec
 	REAL PARAM(NPAMAX)		! Model parameters
@@ -111,213 +104,229 @@
 	REAL LE(NPAMAX)			! Lower parameter errors
 	REAL UE(NPAMAX)			! Upper parameter errors
 	REAL Z				! Redshift [ Eobs/Esource=1/(1+z) ]
-*    Local data :
-*    Version :
-	CHARACTER*30 VERSION
-	PARAMETER		(VERSION='SSIM VERSION 1.8-0')
+
+      INTEGER			BGID			! Bgnd file
+      INTEGER			DETID			! Detector config info
+      INTEGER			IFID			! I/p containing resp
+      INTEGER			MFID			! Model spec id
+      INTEGER			NCH			! # chars used in string
+      INTEGER 			NCHAN			! Channels in spectrum
+      INTEGER			OFID			! Output dataset id
+      INTEGER			TIMID			! Timing info
+
+      LOGICAL 			BG			! B/g spectrum to be added?
+      LOGICAL 			FOUND			! Instrument response found?
+      LOGICAL 			FROZEN(NPAMAX)		! Frozen parameter flag
+      LOGICAL 			OK			! Data OK?
+      LOGICAL 			POISS			! Poisson errors required?
+      LOGICAL 			THERE			! Component present?
+
+*  Version :
+      CHARACTER*30 		VERSION
+	PARAMETER		( VERSION = 'SSIM VERSION 1.8-1' )
 *-
 
-* Version
-	CALL MSG_PRNT( VERSION )
+*  Version
+      CALL MSG_PRNT( VERSION )
 
-* Initialise
-	CALL AST_INIT
-        CALL SPEC_INIT( STATUS )
+*  Initialise
+      CALL AST_INIT()
+      CALL SPEC_INIT( STATUS )
 
-* Model GENUS
-	MODEL.GENUS='SPEC'
+*  Model GENUS
+      MODEL.GENUS = 'SPEC'
 
-* Get instrument response
-	FOUND=.FALSE.
-	CALL USI_DASSOC('INP','READ',ILOC,STATUS)
-	CALL FIT_INSGET(ILOC,0,FOUND,INSTR,STATUS)
-	IF(STATUS.NE.SAI__OK) GO TO 9000
-	IF(.NOT.FOUND)THEN
-	  CALL MSG_PRNT( 'No instrument response component found' )
-	  GO TO 9000
-	ENDIF
+*  Get instrument response
+      FOUND = .FALSE.
+
+*  Read input file
+      CALL USI_TASSOCI( 'INP', '*', 'READ', IFID, STATUS )
+      CALL FIT_GETINS( IFID, 0, FOUND, INSTR, STATUS )
+      IF ( STATUS .NE. SAI__OK ) GOTO 99
+      IF(.NOT.FOUND)THEN
+	CALL MSG_PRNT( 'No instrument response component found' )
+	GOTO 99
+      END IF
 
 * Get model specification
-	CALL USI_DASSOC('MODEL','READ',MLOC,STATUS)
-	CALL FIT_MODGET(MLOC,MODEL,NPAR,PARAM,LB,UB,LE,UE,FROZEN,STATUS)
-	IF(STATUS.NE.SAI__OK) GO TO 9000
+      CALL USI_TASSOCI( 'MODEL', '*', 'READ', MFID, STATUS )
+      CALL FIT_TMODGET( MFID, MODEL, NPAR, PARAM, LB, UB, LE, UE,
+     :                  FROZEN, STATUS )
+      IF(STATUS.NE.SAI__OK) GOTO 99
 
-* User input
-	CALL USI_DASSOC('BG','READ',BGLOC,STATUS)
-	IF(STATUS.EQ.SAI__OK)THEN
-	  BG=.TRUE.
-	ELSE IF(STATUS.EQ.PAR__NULL)THEN
-	  CALL ERR_ANNUL(STATUS)
+* Background spectrum
+      CALL USI_TASSOCI( 'BG', '*', 'READ', BGID, STATUS )
+      IF ( STATUS .EQ. SAI__OK ) THEN
+	BG = .TRUE.
+      ELSE IF ( STATUS .EQ. PAR__NULL ) THEN
+	CALL ERR_ANNUL(STATUS)
+	BG = .FALSE.
+      ELSE
+	GOTO 99
+      END IF
+
+* Poisson deviate?
+      CALL USI_GET0L( 'POISS', POISS, STATUS )
+      IF ( POISS ) THEN
+	CALL USI_GET0R( 'OBLEN', TOBS, STATUS )
+      END IF
+      IF ( STATUS .NE. SAI__OK ) GOTO 99
+
+*  Get red-shift
+      CALL SFIT_GETZ( Z, STATUS )
+
+*  Output dataset
+      CALL USI_TASSOCO( 'OUT', 'SPECTRUM', OFID, STATUS )
+      IF ( STATUS .NE. SAI__OK ) GOTO 99
+
+*  Set up the only OBDAT component needed (no. of channels, taken from response)
+      CALL ADI_CGET0I( INSTR.R_ID, 'NCHAN', NCHAN, STATUS )
+      OBDAT.NDAT = NCHAN
+
+*  Set up PREDDAT (no workspace required)
+      PREDDAT.CONVOLVE = .TRUE.
+      CALL FIT_TPREDSET( IFID, 1, .FALSE., OBDAT, PREDDAT, STATUS )
+      IF ( STATUS .NE. SAI__OK ) GOTO 99
+
+*  Apply redshift to model space energy bounds
+      CALL SFIT_APPRED( Z, PREDDAT.NMBOUND, %VAL(PREDDAT.MLBNDPTR),
+     :                  %VAL(PREDDAT.MUBNDPTR), STATUS )
+
+*  Copy ancillary stuff
+      CALL BDI_COPMORE( IFID, OFID, STATUS )
+
+*  Read timing info from input, adjust and write to output
+      CALL TCI_GETID( IFID, TIMID, STATUS )
+      CALL ADI_CPUT0R( TIMID, 'ObsLength', TOBS, STATUS )
+      CALL ADI_CPUT0R( TIMID, 'Exposure', TOBS, STATUS )
+      CALL TCI_PUTID( OFID, TIMID, STATUS )
+
+*  Change target
+      CALL DCI_GETID( IFID, DETID, STATUS )
+      CALL ADI_CPUT0C( DETID, 'Target', 'Simulated data', STATUS )
+      CALL DCI_PUTID( OFID, DETID, STATUS )
+
+*  Set up output components
+      CALL BDI_CREDATA( OFID, 1, NCHAN, STATUS )
+      CALL BDI_CREAXES( OFID, 1, STATUS )
+      CALL BDI_CREAXVAL( OFID, 1, .FALSE., NCHAN, STATUS )
+      IF ( POISS ) THEN
+	CALL BDI_CREVAR( OFID, 1, NCHAN, STATUS )
+      END IF
+      IF ( STATUS .NE. SAI__OK ) GOTO 99
+
+*  Copy ChannelSpec from response matrix to output axis
+      CALL ADI_CMAPR( INSTR.R_ID, 'ChannelSpec', 'READ', CHPTR, STATUS )
+      CALL BDI_MAPAXVAL( OFID, 'WRITE', 1, AXPTR, STATUS )
+      CALL ARR_COP1R( NCHAN, %VAL(CHPTR), %VAL(AXPTR), STATUS )
+      CALL BDI_UNMAPAXVAL( OFID, 1, STATUS )
+      CALL ADI_CUNMAP( INSTR.R_ID, 'ChannelSpec', CHPTR, STATUS )
+      IF ( STATUS .NE. SAI__OK ) CALL ERR_FLUSH(STATUS)
+
+*  Map data etc and write labels
+      CALL BDI_MAPDATA( OFID, 'WRITE', DPTR, STATUS )
+      IF ( POISS ) THEN
+	CALL BDI_MAPVAR( OFID, 'WRITE', VPTR, STATUS )
+      END IF
+      IF ( STATUS .NE. SAI__OK ) GOTO 99
+      CALL BDI_PUTLABEL( OFID, 'Intensity', STATUS )
+      CALL BDI_PUTUNITS( OFID, 'count/sec', STATUS )
+      CALL BDI_PUTAXLABEL( OFID, 1, 'Channel', STATUS )
+      IF(STATUS.NE.SAI__OK) CALL ERR_FLUSH( STATUS )
+
+*  Copy spectral model into ASTERIX.SPEC_MODEL auxilliary data
+      CALL AUI_PUTID( OFID, 'ASTERIX.SPEC_MODEL', MFID, STATUS )
+      IF ( STATUS .NE. SAI__OK ) GOTO 99
+
+*  Workspace for model stack
+      CALL SFIT_MAPMODSTK( 1, PREDDAT, MODEL.STACKPTR, STATUS )
+
+*  Calculate predicted data
+      CALL FIT_PREDDAT( 1, 1, OBDAT, INSTR, PREDDAT, MODEL, PARAM,
+     :                  1, %VAL(DPTR), STATUS )
+
+*  Map background data and ...
+      IF ( BG ) THEN
+	CALL BDI_CHKDATA( BGID,OK,NDIM,DIMS,STATUS)
+	IF ( .NOT. OK ) THEN
+	  CALL MSG_PRNT( 'No background spectrum available' )
 	  BG=.FALSE.
-	ELSE
-	  GO TO 9000
-	ENDIF
-	CALL USI_GET0L('POISS',POISS,STATUS)
-	IF(POISS)THEN
-	  CALL USI_GET0R('OBLEN',TOBS,STATUS)
-	ENDIF
-        CALL SFIT_GETZ( Z, STATUS )
-
-* Output dataset
-        CALL USI_ASSOCO( 'OUT', 'SPECTRUM', OLOC, STATUS )
-	IF(STATUS.NE.SAI__OK) GO TO 9000
-
-* Set up the only OBDAT component needed (no. of channels, taken from response)
-	CALL DAT_FIND(INSTR.ELOC,'CHANNEL',CHLOC,STATUS)
-	CALL CMP_SIZE(CHLOC,'CHANNEL_SPEC',NCHAN,STATUS)
-	OBDAT.NDAT=NCHAN
-
-* Set up PREDDAT (no workspace required)
-	PREDDAT.CONVOLVE=.TRUE.
-	CALL FIT_PREDSET(ILOC,1,.FALSE.,OBDAT,PREDDAT,STATUS)
-	IF(STATUS.NE.SAI__OK) GO TO 9000
-
-* Apply redshift to model space energy bounds
-        CALL SFIT_APPRED( Z, PREDDAT.NMBOUND,
-     :                    %VAL(PREDDAT.MLBNDPTR),
-     :                    %VAL(PREDDAT.MUBNDPTR), STATUS )
-
-* Copy MORE box to output file, amend HEADER and write exposure time
-	CALL BDA_COPMORE(ILOC,OLOC,STATUS)
-	CALL BDA_LOCHEAD(OLOC,HLOC,STATUS)
-	CALL CMP_PUT0C(HLOC,'TARGET','Simulated data',STATUS)
-	CALL CMP_PUT0R(HLOC,'OBS_LENGTH',TOBS,STATUS)
-	CALL CMP_PUT0R(HLOC,'EXPOSURE_TIME',TOBS,STATUS)
-	IF(STATUS.NE.SAI__OK) CALL ERR_ANNUL(STATUS)
-
-* Set up output components
-	CALL BDA_CREDATA(OLOC,1,NCHAN,STATUS)
-	CALL BDA_CREAXES(OLOC,1,STATUS)
-	CALL BDA_CREAXVAL(OLOC,1,.FALSE.,NCHAN,STATUS)
-	IF(POISS)THEN
-	  CALL BDA_CREVAR(OLOC,1,NCHAN,STATUS)
-	ENDIF
-	IF(STATUS.NE.SAI__OK) GO TO 9000
-
-	CALL DAT_FIND(CHLOC,'CHANNEL_SPEC',LOC,STATUS)
-	CALL DAT_MAPV(LOC,'_REAL','READ',CHPTR,NCHAN,STATUS)
-	CALL BDA_MAPAXVAL(OLOC,'WRITE',1,AXPTR,STATUS)
-	CALL ARR_COP1R(NCHAN,%VAL(CHPTR),%VAL(AXPTR),STATUS)
-	CALL BDA_UNMAPAXVAL(OLOC,1,STATUS)
-	CALL DAT_ANNUL(CHLOC,STATUS)
-	CALL DAT_ANNUL(LOC,STATUS)
-	IF(STATUS.NE.SAI__OK) CALL ERR_FLUSH(STATUS)
-
-* Map DATA_ARRAY etc, write labels
-	CALL BDA_MAPDATA(OLOC,'WRITE',DPTR,STATUS)
-	IF(POISS)THEN
-	  CALL BDA_MAPVAR(OLOC,'WRITE',VPTR,STATUS)
-	ENDIF
-	IF(STATUS.NE.SAI__OK) GO TO 9000
-	CALL BDA_PUTLABEL(OLOC,'Intensity',STATUS)
-	CALL BDA_PUTUNITS(OLOC,'count/sec',STATUS)
-	CALL BDA_PUTAXLABEL(OLOC,1,'Channel',STATUS)
-	IF(STATUS.NE.SAI__OK) CALL ERR_FLUSH(STATUS)
-
-* Copy spectral model into MORE.ASTERIX.SPEC_MODEL
-	CALL BDA_LOCAST(OLOC,ASTLOC,STATUS)
-	CALL DAT_THERE(ASTLOC,'SPEC_MODEL',OK,STATUS)
-	IF(OK)THEN
-	  CALL DAT_ERASE(ASTLOC,'SPEC_MODEL',STATUS)
-	ENDIF
-	CALL DAT_COPY(MLOC,ASTLOC,'SPEC_MODEL',STATUS)
-	IF(STATUS.NE.SAI__OK) GO TO 9000
-
-* Workspace for model stack
-	CALL DYN_MAPR(1,PREDDAT.NMDAT*MAXSTACK,MODEL.STACKPTR,
-     :  STATUS)
-
-* Calculate predicted data
-	CALL FIT_PREDDAT(1,1,OBDAT,INSTR,PREDDAT,MODEL,PARAM,1,%VAL(DPTR),
-     :  STATUS)
-
-* Map background data and ...
-	IF(BG)THEN
-	  CALL BDA_CHKDATA(BGLOC,OK,NDIM,DIMS,STATUS)
-	  IF(.NOT.OK)THEN
-	    CALL MSG_PRNT( 'No background spectrum available' )
-	    BG=.FALSE.
-	    GO TO 100
-	  ENDIF
-	  IF(DIMS(1).NE.NCHAN)THEN
-	    CALL MSG_PRNT( 'Background spectrum has wrong number of'/
+	  GOTO 10
+	END IF
+	IF ( DIMS(1) .NE. NCHAN ) THEN
+	  CALL MSG_PRNT( 'Background spectrum has wrong number of'/
      :        /' channels - not used' )
-	    BG=.FALSE.
-	    GO TO 100
-	  ENDIF
-	  CALL BDA_MAPDATA(BGLOC,'READ',BPTR,STATUS)
+	  BG=.FALSE.
+	  GOTO 10
+	END IF
+	CALL BDI_MAPDATA(BGID,'READ',BPTR,STATUS)
 
 * ...write b/g dataset name to output file (ASTERIX box)
-	  CALL HDS_TRACE(BGLOC,I,BGOBJ,FILE,STATUS)
-	  CALL DAT_THERE(BGLOC,'BGFILE',OK,STATUS)
-	  IF(.NOT.THERE)THEN
-	    CALL DAT_NEWC(ASTLOC,'BGFILE',80,0,0,STATUS)
-	  ENDIF
-	  CALL CMP_PUT0C(ASTLOC,'BGFILE',BGOBJ,STATUS)
-	  IF(STATUS.NE.SAI__OK) CALL ERR_FLUSH(STATUS)
-	ENDIF
- 100	CONTINUE
+	CALL ADI_FTRACE(BGID,I,BGOBJ,FILE,STATUS)
+        CALL AUI_PUT0C( OFID, 'ASTERIX.BGFILE', BGOBJ, STATUS )
+	IF(STATUS.NE.SAI__OK) CALL ERR_FLUSH(STATUS)
 
-* If necessary pass to subroutine to add b/g and calculate variances
-	IF(POISS.OR.BG)THEN
-	  CALL SSIM_BGERR(NCHAN,POISS,TOBS,BG,%VAL(BPTR),%VAL(DPTR),
+      END IF
+ 10   CONTINUE
+
+*  If necessary pass to subroutine to add b/g and calculate variances
+      IF ( POISS .OR. BG ) THEN
+	CALL SSIM_BGERR(NCHAN,POISS,TOBS,BG,%VAL(BPTR),%VAL(DPTR),
      :    %VAL(VPTR))
-	ENDIF
+      END IF
 
-* Report total count rate
-	CALL ARR_SUM1R( NCHAN, %VAL(DPTR), TOTAL, STATUS )
-	CALL MSG_BLNK
-	CALL MSG_SETR('TOT',TOTAL)
-	CALL MSG_PRNT('Total count rate = ^TOT')
+*  Report total count rate
+      CALL ARR_SUM1R( NCHAN, %VAL(DPTR), TOTAL, STATUS )
+      CALL MSG_BLNK
+      CALL MSG_SETR('TOT',TOTAL)
+      CALL MSG_PRNT('Total count rate = ^TOT')
 
-* Graph legend
-	CALL CMP_GET0C(HLOC,'OBSERVATORY',TELES,STATUS)
-	CALL CMP_GET0C(HLOC,'INSTRUMENT',INSTRUM,STATUS)
-	IF(STATUS.NE.SAI__OK) CALL ERR_FLUSH(STATUS)
-	NCHAR=0
-	IF(CHR_LEN(TELES).GT.0)THEN
-	  NCHAR=CHR_LEN(TELES)
-	  LEGEND(1)=TELES(1:NCHAR)//' : '
-	  NCHAR=NCHAR+3
-	ENDIF
-	IF(CHR_LEN(INSTRUM).GT.0)THEN
-	  LEGEND(1)=LEGEND(1)(1:NCHAR)//
-     :    INSTRUM(1:CHR_LEN(INSTRUM))//' : '
-	  NCHAR=NCHAR+CHR_LEN(INSTRUM)+3
-	ENDIF
-	LEGEND(1)=LEGEND(1)(1:NCHAR)//'Simulated data'
-	IF(POISS)THEN
-	  CALL CHR_RTOC(TOBS,CTOBS,I)
-	  LEGEND(2)='Observation length : '//CTOBS(1:CHR_LEN(CTOBS))/
-     :    /' sec'
-	ELSE
-	  LEGEND(2)='No statistical noise'
-	ENDIF
-	CALL CMP_GET0C(MLOC,'SPEC',MODSPEC,STATUS)
-	LEGEND(3)='Spectral model : '//MODSPEC(1:CHR_LEN(MODSPEC))
+*  Construct graph legend strings
+      CALL ADI_CGET0C( DETID, 'Mission', TELES, STATUS )
+      CALL ADI_CGET0C( DETID, 'Instrument', INSTRUM, STATUS )
+      IF(STATUS.NE.SAI__OK) CALL ERR_FLUSH(STATUS)
+      NCH = CHR_LEN(TELES)
+      IF ( NCH .GT. 0 ) THEN
+	LEGEND(1)=TELES(1:NCH)//' : '
+	NCH = NCH + 3
+      END IF
+      IF(CHR_LEN(INSTRUM).GT.0)THEN
+	LEGEND(1)=LEGEND(1)(1:NCH)//INSTRUM(1:CHR_LEN(INSTRUM))//' : '
+	NCH=NCH+CHR_LEN(INSTRUM)+3
+      END IF
+      LEGEND(1)=LEGEND(1)(1:NCH)//'Simulated data'
+      IF ( POISS ) THEN
+        CALL MSG_SETR( 'TOBS', TOBS )
+        CALL MSG_MAKE( 'Observation length : ^TOBS sec', LEGEND(2),
+     :                 NCH )
+      ELSE
+	LEGEND(2) = 'No statistical noise'
+      END IF
+      CALL MSG_SETC( 'SPEC', MODEL.SPEC )
+      CALL MSG_MAKE( 'Spectral model : ^SPEC', LEGEND(3), NCH )
 
-        CALL GCB_ATTACH('LOCAL',STATUS)
-	DO I=1,3
-          CALL GCB_SET1C('TITLE_TEXT',I,1,LEGEND(I),STATUS)
-	ENDDO
-        CALL GCB_SETI('TITLE_N',3,STATUS)
-        CALL GCB_SAVE(OLOC,STATUS)
-        CALL GCB_DETACH(STATUS)
+*  Attach GCB and write attributes
+      CALL GCB_LCONNECT( STATUS )
+      DO I = 1, 3
+        CALL GCB_SET1C('TITLE_TEXT',I,1,LEGEND(I),STATUS)
+      END DO
+      CALL GCB_SETI('TITLE_N',3,STATUS)
+      CALL GCB_FSAVE( OFID, STATUS )
+      CALL GCB_DETACH(STATUS)
+      IF ( STATUS .NE. SAI__OK ) CALL ERR_FLUSH(STATUS)
 
-	IF(STATUS.NE.SAI__OK) CALL ERR_FLUSH(STATUS)
+*  History
+      CALL HSI_ADD( OFID, VERSION, STATUS )
 
-* History
-	CALL HIST_ADD(OLOC,VERSION,STATUS)
+*  Tidy up
+ 99   CALL AST_CLOSE()
+      CALL AST_ERR( STATUS )
 
-* Tidy up
- 9000	CALL AST_CLOSE
-        CALL AST_ERR( STATUS )
-
-	END
+      END
 
 
 *+  SSIM_BGERR - Adds b/g and calculates poisson errors
-	SUBROUTINE SSIM_BGERR(NDAT,POISS,TOBS,BG,BGVAL,DVAL,VVAL)
+	SUBROUTINE SSIM_BGERR(NDAT,POISS,TOBS,BG,BGVAL,DVAL,VVAL,STATUS)
 *    Description :
 *     Adds b/g to data array if BG is .TRUE.
 *     Calculates poisson variances if POISS=.TRUE.
@@ -328,6 +337,10 @@
 *     14 Mar 91: Variances replace errors (TJP)
 *    Type definitions :
       IMPLICIT NONE
+*    Global constants :
+	INCLUDE 'SAE_PAR'
+*    Status :
+	INTEGER STATUS
 *    Import :
 	INTEGER NDAT		! No of data values
 	LOGICAL POISS		! Poisson errors required?
@@ -342,7 +355,10 @@
 *    Local variables :
 	INTEGER I
 	INTEGER MATH_POISS
-*--------------------------------------------------------------
+*-
+
+*  Check inherited global status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
 
 	DO I=1,NDAT
 	  IF(BG)THEN
