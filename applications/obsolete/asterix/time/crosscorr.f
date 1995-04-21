25359a275950866b0b12bc0b46e0191e8c23b50b
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-04-21 11:35:00 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/time/crosscorr.f
+++ applications/obsolete/asterix/time/crosscorr.f
@@ -54,41 +54,26 @@
       IMPLICIT NONE
 *    Global constants :
       INCLUDE 'SAE_PAR'
-      INCLUDE 'DAT_PAR'
-      INCLUDE 'PAR_ERR'
-*    Global variables :
-*    Structure definitions :
+      INCLUDE 'ADI_PAR'
 *    Status :
       INTEGER STATUS
 *    Function declarations :
       INTEGER CHR_LEN
-*    Local constants :
 *    Local variables :
-      CHARACTER*(DAT__SZLOC) YLOC            ! First data object locator
-      CHARACTER*(DAT__SZLOC) ZLOC            ! Second data object locator
-      CHARACTER*(DAT__SZLOC) ILOC            ! Locator of 1st non-prim input
-      CHARACTER*(DAT__SZLOC) XCLOC           ! Locator to output object
-      CHARACTER*(DAT__SZLOC) HRLOC           ! Locator to o/p history record
-      CHARACTER*(DAT__SZTYP) YOBTYPE         ! Type of first data object
-      CHARACTER*(DAT__SZTYP) ZOBTYPE         ! Type of second data object
       CHARACTER*80           LUNITS          ! Lag units
       CHARACTER*80           YFILE,ZFILE     ! Input files
       CHARACTER*80           YPATH,ZPATH     ! Input paths (within file)
       CHARACTER*80           STRING          ! Text string
       CHARACTER*80           TEXT(10)        ! History text
 
-      LOGICAL                ANYBAD          ! Any bad quality points at all?
-      LOGICAL                DENOISE         ! Noise variance removed?
-      LOGICAL                LOG
-      LOGICAL                OK              ! Present & correct?
-      LOGICAL                VARS            ! Variances available?
-      LOGICAL                WEIGHT          ! Weighted cross-correlation?
-      LOGICAL                YPRIM           ! Primitive input array 1?
-      LOGICAL                YREG            ! Y spacing known to be regular?
-      LOGICAL                ZPRIM           ! Primitive input array 2?
-      LOGICAL                ZREG            ! Z spacing known to be regular?
+      REAL                   BASE            ! Axis base value
+      REAL                   SCALE           ! Axis scale value
+      REAL                   YSCALE          ! Axis scale value for Y
+      REAL                   ZSCALE          ! Axis scale value for Z
+      REAL                   VMIN,VMAX       ! Min and max variance values
 
       INTEGER                I
+      INTEGER			IFID			! I/p used as base
       INTEGER                YDPTR           ! Pointer to Y data array
       INTEGER                ZDPTR           ! Pointer to Z data array
       INTEGER                YVPTR           ! Pointer to Y variance array
@@ -112,106 +97,101 @@
       INTEGER                NLINE           ! Line no. of HISTORY text
       INTEGER                TEXTLEN         ! Length of text string
       INTEGER                IDIM(DAT__MXDIM)! Size of each dimension
+      INTEGER			XCID			! Output dataset id
+      INTEGER			YFID			! 1st input id
+      INTEGER			ZFID			! 2nd input id
 
-      REAL                   BASE            ! Axis base value
-      REAL                   SCALE           ! Axis scale value
-      REAL                   YSCALE          ! Axis scale value for Y
-      REAL                   ZSCALE          ! Axis scale value for Z
-      REAL                   VMIN,VMAX       ! Min and max variance values
-*    Local data :
-*    Version :
-      CHARACTER*30 VERSION
-         PARAMETER (VERSION = 'CROSSCORR Version 1.2-0')
+      LOGICAL                ANYBAD          ! Any bad quality points at all?
+      LOGICAL                DENOISE         ! Noise variance removed?
+      LOGICAL                LOG
+      LOGICAL                OK              ! Present & correct?
+      LOGICAL                VARS            ! Variances available?
+      LOGICAL                WEIGHT          ! Weighted cross-correlation?
+      LOGICAL                YPRIM           ! Primitive input array 1?
+      LOGICAL                YREG            ! Y spacing known to be regular?
+      LOGICAL                ZPRIM           ! Primitive input array 2?
+      LOGICAL                ZREG            ! Z spacing known to be regular?
+
+*  Version :
+      CHARACTER*30		VERSION
+        PARAMETER 		( VERSION = 'CROSSCORR Version 1.8-0' )
 *-
 
 *    Announce version
       CALL MSG_PRNT(VERSION)
 
-*    Initialise ASTERIX packages
-      CALL AST_INIT
+*    Initialise ASTERIX
+      CALL AST_INIT()
 
 *    Obtain data objects
-      CALL USI_ASSOCI( 'INP1', 'READ' , YLOC , YPRIM , STATUS )
-      CALL USI_ASSOCI( 'INP2', 'READ' , ZLOC , ZPRIM , STATUS )
-      IF(STATUS.NE.SAI__OK) GO TO 9000
+      CALL USI_TASSOCI( 'INP1', '*', 'READ' , YFID, STATUS )
+      CALL USI_TASSOCI( 'INP2', '*', 'READ' , ZFID, STATUS )
+      IF(STATUS.NE.SAI__OK) GOTO 99
 
-*    Find types and inform user
-      CALL DAT_TYPE( YLOC, YOBTYPE, STATUS )
-      IF(STATUS.EQ.SAI__OK)THEN
-        CALL MSG_SETC( 'OTYPE1', YOBTYPE )
-        CALL DAT_TYPE( ZLOC, ZOBTYPE, STATUS )
-
-        IF ( YOBTYPE .EQ. ZOBTYPE ) THEN
-          CALL MSG_PRNT( 'Both objects are of type ^OTYPE1')
-        ELSE
-          CALL MSG_PRNT( '$OBJ1 is of type ^OTYPE1')
-          CALL MSG_SETC( 'OTYPE2', ZOBTYPE )
-          CALL MSG_PRNT( '$OBJ2 is of type ^OTYPE2')
-        ENDIF
-      ELSE
-        CALL ERR_FLUSH(STATUS)
-      ENDIF
+*    Test whether primitive
+      CALL BDI_PRIM( YFID, YPRIM, STATUS )
+      CALL BDI_PRIM( ZFID, ZPRIM, STATUS )
 
 *    Check data, map if present and 1-dimensional
-      CALL BDA_CHKDATA(YLOC,OK,NDIM,IDIM,STATUS)
+      CALL BDI_CHKDATA(YFID,OK,NDIM,IDIM,STATUS)
 D     print *,'y;ok,ndim,idim,status:',ok,ndim,idim,status
       IF(.NOT.OK)THEN
-        CALL ERR_REP('BADDAT','Invalid data in dataset 1',STATUS)
         STATUS=SAI__ERROR
+        CALL ERR_REP('BADDAT','Invalid data in dataset 1',STATUS)
       ELSE
         IF(NDIM.NE.1)THEN
-          CALL ERR_REP( 'NOT_1DY', '$OBJ1 data are not one-dimensional',
-     :    STATUS )
           STATUS=SAI__ERROR
+          CALL ERR_REP( 'NOT_1DY', '$INP1 data are not one-dimensional',
+     :    STATUS )
         ELSE
-          CALL BDA_MAPDATA(YLOC,'READ',YDPTR,STATUS)
+          CALL BDI_MAPDATA(YFID,'READ',YDPTR,STATUS)
           NY=IDIM(1)
         ENDIF
       ENDIF
-      IF(STATUS.NE.SAI__OK) GO TO 9000
+      IF(STATUS.NE.SAI__OK) GOTO 99
 
-      CALL BDA_CHKDATA(ZLOC,OK,NDIM,IDIM,STATUS)
+      CALL BDI_CHKDATA(ZFID,OK,NDIM,IDIM,STATUS)
 D     print *,'z;ok,ndim,idim,status:',ok,ndim,idim,status
       IF(.NOT.OK)THEN
         CALL ERR_REP('BADDAT','Invalid data in dataset 2',STATUS)
         STATUS=SAI__ERROR
       ELSE
         IF(NDIM.NE.1)THEN
-          CALL ERR_REP( 'NOT_1DZ', '$OBJ2 data are not one-dimensional',
+          CALL ERR_REP( 'NOT_1DZ', '$INP2 data are not one-dimensional',
      :    STATUS )
           STATUS=SAI__ERROR
         ELSE
-          CALL BDA_MAPDATA(ZLOC,'READ',ZDPTR,STATUS)
+          CALL BDI_MAPDATA(ZFID,'READ',ZDPTR,STATUS)
           NZ=IDIM(1)
         ENDIF
       ENDIF
-      IF(STATUS.NE.SAI__OK) GO TO 9000
+      IF(STATUS.NE.SAI__OK) GOTO 99
 
 *    Check for regular axis spacing
       YREG=.FALSE.
       IF(.NOT.YPRIM)THEN
-        CALL BDA_CHKAXVAL(YLOC,1,OK,YREG,NVAL,STATUS)
+        CALL BDI_CHKAXVAL(YFID,1,OK,YREG,NVAL,STATUS)
       ENDIF
       IF (.NOT.YREG) THEN
-        CALL MSG_PRNT( 'Assuming $OBJ1 regularly spaced')
+        CALL MSG_PRNT( 'Assuming $INP1 regularly spaced')
       END IF
       IF(STATUS.NE.SAI__OK) CALL ERR_FLUSH(STATUS)
 
       ZREG=.FALSE.
       IF(.NOT.ZPRIM)THEN
-        CALL BDA_CHKAXVAL(ZLOC,1,OK,ZREG,NVAL,STATUS)
+        CALL BDI_CHKAXVAL(ZFID,1,OK,ZREG,NVAL,STATUS)
       ENDIF
       IF(.NOT.ZREG) THEN
-        CALL MSG_PRNT( 'Assuming $OBJ2 regularly spaced')
+        CALL MSG_PRNT( 'Assuming $INP2 regularly spaced')
       END IF
       IF(STATUS.NE.SAI__OK) CALL ERR_FLUSH(STATUS)
 
 *    Get axis spacing if available
       IF(YREG)THEN
-        CALL BDA_GETAXVAL(YLOC,1,BASE,YSCALE,I,STATUS)
+        CALL BDI_GETAXVAL(YFID,1,BASE,YSCALE,I,STATUS)
       ENDIF
       IF(ZREG)THEN
-        CALL BDA_GETAXVAL(ZLOC,1,BASE,ZSCALE,I,STATUS)
+        CALL BDI_GETAXVAL(ZFID,1,BASE,ZSCALE,I,STATUS)
       ENDIF
       IF(YREG.AND.ZREG)THEN
         IF(ABS(YSCALE-ZSCALE)/YSCALE.LT.0.0001)THEN
@@ -232,10 +212,10 @@ D     print *,'z;ok,ndim,idim,status:',ok,ndim,idim,status
 
 *    If one array is larger than the other then truncate it
       IF (NY.GT.NZ) THEN
-        CALL MSG_PRNT('$OBJ1 array truncated for use')
+        CALL MSG_PRNT('$INP1 array truncated for use')
         ND = NZ
       ELSE IF (NZ.GT.NY) THEN
-        CALL MSG_PRNT('$OBJ2 array truncated for use')
+        CALL MSG_PRNT('$INP2 array truncated for use')
         ND=NY
       ELSE
         ND=NY
@@ -247,13 +227,13 @@ D     print *,'z;ok,ndim,idim,status:',ok,ndim,idim,status
         CALL ERR_REP('TOO_SHORT','Insufficient data to cross-correlate',
      :  STATUS)
         STATUS=SAI__ERROR
-        GO TO 9000
+        GOTO 99
       ENDIF
 
 *    User input
       CALL USI_DEF0I( 'LAG',ND-1,STATUS)
       CALL USI_GET0I( 'LAG', LMAX, STATUS )
-      IF(STATUS.NE.SAI__OK) GO TO 9000
+      IF(STATUS.NE.SAI__OK) GOTO 99
       IF(LMAX.GT.ND-1)THEN
         LMAX=ND-1
         CALL MSG_SETI('LMAX',LMAX)
@@ -262,17 +242,17 @@ D     print *,'z;ok,ndim,idim,status:',ok,ndim,idim,status
 
 *    Check data variances - only allow weighting if all variances are available
       IF((.NOT.YPRIM).AND.(.NOT.ZPRIM))THEN
-        CALL BDA_CHKVAR(YLOC,VARS,NDIM,IDIM,STATUS)
+        CALL BDI_CHKVAR(YFID,VARS,NDIM,IDIM,STATUS)
         IF(VARS)THEN
           IF(NDIM.NE.1.OR.IDIM(1).NE.NY)THEN
-            CALL MSG_PRNT('WARNING: $OBJ1 variance array is'
+            CALL MSG_PRNT('WARNING: $INP1 variance array is'
      :      //' wrong size. Will proceed without weighting.')
             VARS=.FALSE.
           ELSE
-            CALL BDA_CHKVAR(ZLOC,OK,NDIM,IDIM,STATUS)
+            CALL BDI_CHKVAR(ZFID,OK,NDIM,IDIM,STATUS)
             IF(OK)THEN
               IF(NDIM.NE.1.OR.IDIM(1).NE.NZ)THEN
-                CALL MSG_PRNT('WARNING: $OBJ2 variance array'
+                CALL MSG_PRNT('WARNING: $INP2 variance array'
      :          //' is wrong size. Will proceed without weighting.')
                 VARS=.FALSE.
               ENDIF
@@ -285,12 +265,12 @@ D     print *,'z;ok,ndim,idim,status:',ok,ndim,idim,status
           CALL USI_GET0L( 'WEIGHTED', WEIGHT, STATUS )
           CALL USI_GET0L( 'NOISE', DENOISE, STATUS )
         ENDIF
-        IF(STATUS.NE.SAI__OK) GO TO 9000
+        IF(STATUS.NE.SAI__OK) GOTO 99
 
 *      Map variances
         IF(WEIGHT.OR.DENOISE)THEN
-          CALL BDA_MAPVAR(YLOC,'READ',YVPTR,STATUS)
-          CALL BDA_MAPVAR(ZLOC,'READ',ZVPTR,STATUS)
+          CALL BDI_MAPVAR(YFID,'READ',YVPTR,STATUS)
+          CALL BDI_MAPVAR(ZFID,'READ',ZVPTR,STATUS)
         ENDIF
         IF(STATUS.NE.SAI__OK)THEN
           CALL ERR_REP('BAD_VAR','Failed to map variances - no '//
@@ -301,27 +281,27 @@ D     print *,'z;ok,ndim,idim,status:',ok,ndim,idim,status
         ENDIF
 
 *      Check data quality
-        CALL BDA_CHKQUAL( YLOC, OK, QNDIM, QDIM, STATUS )
+        CALL BDI_CHKQUAL( YFID, OK, QNDIM, QDIM, STATUS )
         IF ( OK ) THEN
-           CALL BDA_MAPLQUAL( YLOC, 'READ', ANYBAD, YQPTR, STATUS )
+           CALL BDI_MAPLQUAL( YFID, 'READ', ANYBAD, YQPTR, STATUS )
            IF ( ANYBAD ) THEN
               CALL ARR_NBAD( ND, %VAL(YQPTR), NBAD, STATUS )
               CALL MSG_SETI( 'NBAD', NBAD )
               CALL MSG_PRNT('WARNING: ^NBAD bad data points present'/
-     :                                                  /' in $OBJ1')
+     :                                                  /' in $INP1')
            END IF
-           CALL BDA_UNMAPLQUAL( YLOC, STATUS )
+           CALL BDI_UNMAPLQUAL( YFID, STATUS )
         END IF
-        CALL BDA_CHKQUAL( ZLOC, OK, QNDIM, QDIM, STATUS )
+        CALL BDI_CHKQUAL( ZFID, OK, QNDIM, QDIM, STATUS )
         IF ( OK ) THEN
-           CALL BDA_MAPLQUAL( ZLOC, 'READ', ANYBAD, ZQPTR, STATUS )
+           CALL BDI_MAPLQUAL( ZFID, 'READ', ANYBAD, ZQPTR, STATUS )
            IF ( ANYBAD ) THEN
               CALL ARR_NBAD( ND, %VAL(ZQPTR), NBAD, STATUS )
               CALL MSG_SETI( 'NBAD', NBAD )
               CALL MSG_PRNT('WARNING: ^NBAD bad data points present'/
-     :                                                  /' in $OBJ2')
+     :                                                  /' in $INP2')
            END IF
-           CALL BDA_UNMAPLQUAL( ZLOC, STATUS )
+           CALL BDI_UNMAPLQUAL( ZFID, STATUS )
         END IF
       END IF
 
@@ -330,14 +310,14 @@ D     print *,'z;ok,ndim,idim,status:',ok,ndim,idim,status
         CALL ARR_RANG1R(ND,%VAL(YVPTR),VMIN,VMAX,STATUS)
         IF(VMIN.LE.0.0)THEN
           CALL MSG_PRNT('Non-positive variance values in '//
-     :    '$OBJ1 - proceeding without weighting')
+     :    '$INP1 - proceeding without weighting')
           WEIGHT=.FALSE.
           DENOISE=.FALSE.
         ELSE
           CALL ARR_RANG1R(ND,%VAL(ZVPTR),VMIN,VMAX,STATUS)
           IF(VMIN.LE.0.0)THEN
             CALL MSG_PRNT('Non-positive variance values in '//
-     :      '$OBJ2 - proceeding without weighting')
+     :      '$INP2 - proceeding without weighting')
             WEIGHT=.FALSE.
             DENOISE=.FALSE.
           ENDIF
@@ -345,68 +325,64 @@ D     print *,'z;ok,ndim,idim,status:',ok,ndim,idim,status
       ENDIF
 
 *    Create a cross-correlation object
-      CALL USI_DCREAT( 'OUT', 'CROSS_CORR', 0, 0, STATUS )
-      CALL USI_DASSOC( 'OUT', 'WRITE', XCLOC, STATUS )
-      IF(STATUS.NE.SAI__OK) GO TO 9000
+      CALL USI_TASSOCO( 'OUT', 'CROSS_CORR', XCID, STATUS )
+      IF(STATUS.NE.SAI__OK) GOTO 99
 
 *    Create principal arrays
       NL = 2*LMAX + 1
       NDIM=1
       IDIM(1)=NL
-      CALL BDA_CREBDS(XCLOC,NDIM,IDIM,.TRUE.,.FALSE.,.FALSE.,STATUS)
+      CALL BDI_CREBDS( XCID,NDIM,IDIM,.TRUE.,.FALSE.,.FALSE.,STATUS)
 
 *    Map output data
-      CALL BDA_MAPDATA(XCLOC,'WRITE',XCPTR,STATUS)
-      IF(STATUS.NE.SAI__OK) GO TO 9000
+      CALL BDI_MAPDATA( XCID,'WRITE',XCPTR,STATUS)
+      IF(STATUS.NE.SAI__OK) GOTO 99
 
 *    Compute cross-correlation
       CALL TIM_XCOR(ND,%VAL(YDPTR),%VAL(ZDPTR),%VAL(YVPTR),%VAL(ZVPTR),
      :WEIGHT,DENOISE,NL,%VAL(XCPTR))
 
 *    Enter lag values
-      CALL BDA_PUTAXVAL(XCLOC,1,-LMAX*SCALE,SCALE,NL,STATUS)
+      CALL BDI_PUTAXVAL( XCID,1,-LMAX*SCALE,SCALE,NL,STATUS)
 
 *    Copy ancillary information from first non-primitive reg. spaced input file
       IF(YREG)THEN
-        CALL DAT_CLONE(YLOC,ILOC,STATUS)
+        CALL ADI_CLONE(YFID,IFID,STATUS)
         BASEFILE=1
       ELSE IF(ZREG)THEN
-        CALL DAT_CLONE(ZLOC,ILOC,STATUS)
+        CALL ADI_CLONE(ZFID,IFID,STATUS)
         BASEFILE=2
       ELSE IF(.NOT.YPRIM)THEN
-        CALL DAT_CLONE(YLOC,ILOC,STATUS)
+        CALL ADI_CLONE(YFID,IFID,STATUS)
         BASEFILE=1
       ELSE IF(.NOT.ZPRIM)THEN
-        CALL DAT_CLONE(ZLOC,ILOC,STATUS)
+        CALL ADI_CLONE(ZFID,IFID,STATUS)
         BASEFILE=2
       ENDIF
-D     print *,'yprim,zprim,yreg,zreg: ',yprim,zprim,yreg,zreg
-D     print *,'basefile: ',basefile
 
 *    Create or copy data and axis ancillaries
-      CALL BDA_PUTLABEL(XCLOC,'Cross-correlation',STATUS)
-      CALL BDA_PUTAXLABEL(XCLOC,1,'Lag',STATUS)
-      CALL BDA_PUTAXWID(XCLOC,1,SCALE,STATUS)
+      CALL BDI_PUTLABEL( XCID,'Cross-correlation',STATUS)
+      CALL BDI_PUTAXLABEL( XCID,1,'Lag',STATUS)
+      CALL BDI_PUTAXWID( XCID,1,SCALE,STATUS)
       IF(STATUS.NE.SAI__OK) CALL ERR_FLUSH(STATUS)
       IF(.NOT.(YPRIM.AND.ZPRIM))THEN
-        CALL BDA_GETAXUNITS(ILOC,1,STRING,STATUS)
-        CALL BDA_PUTAXUNITS(XCLOC,1,STRING,STATUS)
+        CALL BDI_GETAXUNITS(IFID,1,STRING,STATUS)
+        CALL BDI_PUTAXUNITS( XCID,1,STRING,STATUS)
         IF(STATUS.NE.SAI__OK) CALL ERR_ANNUL(STATUS)
 
 *    Copy MORE
-        CALL HDX_CCOPY(ILOC,XCLOC,'MORE',STATUS)
-        IF(STATUS.NE.SAI__OK) CALL ERR_ANNUL(STATUS)
+        CALL BDI_COPMORE( IFID, XCID, STATUS )
 
-* History entry (copy history file from ILOC, if available)
-        CALL HIST_OK(ILOC,OK,STATUS)
+* History entry (copy history file from IFID, if available)
+        CALL HSI_OK(IFID,OK,STATUS)
         IF(OK)THEN
-          CALL HIST_COPY(ILOC,XCLOC,STATUS)
+          CALL HSI_COPY(IFID,XCID,STATUS)
         ELSE
           BASEFILE=0
         ENDIF
         IF(STATUS.NE.SAI__OK) CALL ERR_FLUSH(STATUS)
       ENDIF
-      CALL HIST_ADD(XCLOC,VERSION,STATUS)
+      CALL HSI_ADD(XCID,VERSION,STATUS)
       CALL USI_NAMEI(NLINE,TEXT,STATUS)
       IF(BASEFILE.GT.0)THEN
         NLINE=NLINE+1
@@ -426,13 +402,11 @@ D     print *,'basefile: ',basefile
       ELSE
         TEXT(NLINE)=STRING(1:CHR_LEN(STRING))//'    No noise correction'
       ENDIF
-D     print *,'History text: ',text
-      CALL HIST_PTXT(XCLOC,NLINE,TEXT,STATUS)
 
-* Tidy up
-      CALL AST_CLOSE(STATUS)
+      CALL HSI_PTXT(XCID,NLINE,TEXT,STATUS)
 
 * Exit
- 9000 CONTINUE
+ 99   CALL AST_CLOSE()
+      CALL AST_ERR( STATUS )
 
       END
