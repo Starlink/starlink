ac37f4d947db2ef4164b38b4fc6dda6bbb4272a3
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-04-28 10:11:25 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/src/imsim.f
+++ applications/obsolete/asterix/src/imsim.f
@@ -549,7 +549,7 @@
 
 *        Associate psf if first time through
           IF ( NSRC .GT. 0 ) THEN
-            CALL PSF_TASSOCO( OFID, PSLOT, STATUS )
+            CALL PSF_TASSOC( OFID, PSLOT, STATUS )
             IF ( STATUS .NE. SAI__OK ) GOTO 99
           END IF
 
