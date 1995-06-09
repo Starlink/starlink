b7f825acc999d2c12452851c291209d1bfb1fb6c
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-06-09 11:46:28 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/grf/igui.f
+++ applications/obsolete/asterix/grf/igui.f
@@ -33,13 +33,63 @@
       CALL USI_GET0C('ACTION',ACTION,STATUS)
       CALL CHR_UCASE(ACTION)
 
-*  the GUI has just started up
-      IF (ACTION.EQ.'START') THEN
+      IF (ACTION.EQ.'INIT') THEN
+
+        CALL USI_GET0C('VALUE',VALUE,STATUS)
+        CALL CHR_UCASE(VALUE)
+
+        IF (VALUE.EQ.'GCB') THEN
+
+*  initialise GCB
+          CALL GCB_RDDSCF(STATUS)
+
+        ENDIF
+
+      ELSEIF (ACTION.EQ.'CREATE') THEN
 
 *  get the name of the noticeboard that will pass information to GUI
         CALL USI_GET0C('VALUE',VALUE,STATUS)
 
 *  create noticeboard
+        CALL IGUI_CRENB(VALUE,STATUS)
+
+        I_GUI=(STATUS.EQ.SAI__OK)
+
+      ENDIF
+
+      CALL USI_CLOSE()
+
+      END
+
+
+*+  IGUI_CRENB - create noticeboard
+      SUBROUTINE IGUI_CRENB(NAME,STATUS)
+*    Description :
+*    Deficiencies :
+*    Bugs :
+*    Authors :
+*        rjv@star.sr.bham.ac.uk
+*    History :
+*    Type definitions :
+      IMPLICIT NONE
+*    Global constants :
+      INCLUDE 'SAE_PAR'
+      INCLUDE 'DAT_PAR'
+      INCLUDE 'PAR_ERR'
+      INCLUDE 'PRM_PAR'
+*    Import :
+      CHARACTER*(*) NAME
+*    Global variables :
+      INCLUDE 'IMG_CMN'
+*    Status :
+      INTEGER STATUS
+*    Function declarations :
+*    Local constants :
+*    Local variables :
+      INTEGER ID,SID,ITEMID
+*-
+      IF (STATUS.EQ.SAI__OK) THEN
+
         CALL NBS_BEGIN_DEFINITION(ID,STATUS)
         CALL NBS_DEFINE_PRIMITIVE(ID,'X','_REAL',0,VAL__NBR,SID,STATUS)
         CALL NBS_DEFINE_PRIMITIVE(ID,'Y','_REAL',0,VAL__NBR,SID,STATUS)
@@ -66,10 +116,6 @@
         CALL NBS_FIND_ITEM(I_NBID,'MAX',ITEMID,STATUS)
         CALL NBS_PUT_VALUE(ITEMID,0,VAL__NBR,0.0,STATUS)
 
-        I_GUI=(STATUS.EQ.SAI__OK)
-
       ENDIF
-
-      CALL USI_CLOSE()
 
       END
