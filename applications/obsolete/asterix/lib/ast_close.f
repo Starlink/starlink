06a90b76cf0e028e67bcdf12424aad93e0042435
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-06-09 09:23:47 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/ast_close.f
+++ applications/obsolete/asterix/lib/ast_close.f
@@ -1,18 +1,94 @@
-*+  AST_CLOSE - shutdown ASTERIX sub-systems
       SUBROUTINE AST_CLOSE()
-*    Description :
-*      Closes BDA_, DYN_, HIST_, USI_ systems
-*    History :
-*    Type definitions :
-      IMPLICIT NONE
-*    Global variables :
-      INCLUDE 'SAE_PAR'
-*    Local Constants :
-*    Local variables :
+*+
+*  Name:
+*     AST_CLOSE
+
+*  Purpose:
+*     Shut down ASTERIX sub-systems started by AST_INIT
+
+*  Language:
+*     Starlink Fortran
+
+*  Invocation:
+*     CALL AST_CLOSE()
+
+*  Description:
+*     Close down the file system, dynamic memory management and the user
+*     interface. IEEE inexact and underflow exceptions are cleared.
+
+*  Examples:
+*     {routine_example_text}
+*        {routine_example_description}
+
+*  Pitfalls:
+*     {pitfall_description}...
+
+*  Notes:
+*     {routine_notes}...
+
+*  Prior Requirements:
+*     {routine_prior_requirements}...
+
+*  Side Effects:
+*     {routine_side_effects}...
+
+*  Algorithm:
+*     {algorithm_description}...
+
+*  Accuracy:
+*     {routine_accuracy}
+
+*  Timing:
+*     {routine_timing}
+
+*  External Routines Used:
+*     {name_of_facility_or_package}:
+*        {routine_used}...
+
+*  Implementation Deficiencies:
+*     {routine_deficiencies}...
+
+*  References:
+*     AST Subroutine Guide : http://www.sr.bham.ac.uk:8080/asterix-docs/Programmer/Guides/ast.html
+
+*  Keywords:
+*     package:ast, usage:public
+
+*  Copyright:
+*     Copyright (C) University of Birmingham, 1995
+
+*  Authors:
+*     DJA: David J. Allan (Jet-X, University of Birmingham)
+*     {enter_new_authors_here}
+
+*  History:
+*     9 Jun 1995 (DJA):
+*        Original version.
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
 *-
+
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*.
+
+*  File system
       CALL BDA_CLOSE()
+
+*  Dynamic memory management
       CALL DYN_CLOSE()
+
+*  History (should go soon)
       CALL HIST_CLOSE()
+
+*  User interface
       CALL USI_CLOSE()
+
+*  Clear floating exceptions
+      CALL AST_CLIEEE()
 
       END
