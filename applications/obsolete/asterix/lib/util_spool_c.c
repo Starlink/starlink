9301eac252e610f22de05b705058809e45253e57
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-06-30 14:55:12 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/util_spool_c.c
+++ applications/obsolete/asterix/lib/util_spool_c.c
@@ -1,41 +1,100 @@
-/*+
- *  Name:
- *     util_spool_c.c
- *
- *  Purpose:
- *     Spool a file to the printer using a command defined by the ASTERIX
- *     environment variable AST_<TYPE>_SPOOL. Errors by the spooling process
- *     are reported and flushed to the environment.
- *
- *  Invokation :
- *
- *     From FORTRAN use as follows,
- *
- *       CALL UTIL_SPOOL( FILE, TYPE, DELETE, STATUS )
- *
- *     where FILE is the file to be spooled, and TYPE is one of the file
- *     types for which a spool command is defined, eg. TEXT or PS.
- *
- *  Language:
- *
- *     Starlink ANSI C
- *
- *  Description:
- *
- *  Authors:
- *
- *     David J. Allan (BHVAD::DJA)
- *
- *  History:
- *
- *    29-JUL-1993 (DJA):
- *        Original version.
- *- */
+/*
+*+
+*  Name:
+*     UTIL_SPOOL
 
-/* Include Statements: */
+*  Purpose:
+*     Spool a file to a printer
+
+*  Language:
+*     Starlink ANSI C
+
+*  Invocation:
+*     CALL UTIL_SPOOL( FILE, TYPE, DELETE, STATUS )
+
+*  Description:
+*     Spool a file to the printer using a command defined by the ASTERIX
+*     environment variable AST_<TYPE>_SPOOL. Errors by the spooling process
+*     are reported and flushed to the environment. The value of TYPE can
+*     be anything as long as the appropriate environment variables set.
+*     Examples might be FORTRAN, PS or TEXT.
+*     {routine_description}
+
+*  Arguments:
+*     FILE = CHARACTER*(*) (given)
+*        The name of the file to be spooled
+*     TYPE = CHARACTER*(*) (given)
+*        The type describing the contents or format of the file
+*     DELETE = LOGICAL (given)
+*        Delete the file after it is spooled?
+*     STATUS = INTEGER (given and returned)
+*        The global status.
+
+*  Examples:
+*     {routine_example_text}
+*        {routine_example_description}
+
+*  Pitfalls:
+*     {pitfall_description}...
+
+*  Notes:
+*     {routine_notes}...
+
+*  Prior Requirements:
+*     {routine_prior_requirements}...
+
+*  Side Effects:
+*     {routine_side_effects}...
+
+*  Algorithm:
+*     {algorithm_description}...
+
+*  Accuracy:
+*     {routine_accuracy}
+
+*  Timing:
+*     {routine_timing}
+
+*  External Routines Used:
+*     {name_of_facility_or_package}:
+*        {routine_used}...
+
+*  Implementation Deficiencies:
+*     {routine_deficiencies}...
+
+*  {machine}-specific features used:
+*     {routine_machine_specifics}...
+
+*  References:
+*     util Subroutine Guide : http://www.sr.bham.ac.uk:8080/asterix-docs/Programmer/Guides/util.html
+
+*  Keywords:
+*     package:util, usage:public
+
+*  Copyright:
+*     Copyright (C) University of Birmingham, 1995
+
+*  Authors:
+*     DJA: David J. Allan (Jet-X, University of Birmingham)
+*     {enter_new_authors_here}
+
+*  History:
+*     29 Jul 1993 (DJA):
+*        Original version.
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+*/
+
+/*
+ *  Include files
+ */
 #include "sae_par.h"
-#include "f77.h"                       /* c <-> FORTRAN interfacing */
-#include "cnf.h"                       /* c <-> FORTRAN strings */
+#include "f77.h"
+#include "cnf.h"
 #include "ems.h"                       /* Error handling */
 #include <stdio.h>                     /* i/o handling */
 #include <stdlib.h>
@@ -48,8 +107,12 @@
 #include <unistd.h>
 #endif
 
-F77_SUBROUTINE(util_spool)( CHARACTER(file), CHARACTER(type),
-      LOGICAL(del), INTEGER(status) TRAIL(file) TRAIL(type) )
+
+/*
+ *  Body of code
+ */
+F77_SUBROUTINE(util_spool)( CHARACTER(file), CHARACTER(type), LOGICAL(del),
+                            INTEGER(status) TRAIL(file) TRAIL(type) )
   {
   GENPTR_CHARACTER(file)
   GENPTR_CHARACTER(type)
@@ -65,22 +128,27 @@ F77_SUBROUTINE(util_spool)( CHARACTER(file), CHARACTER(type),
   int   splen;				/* Length of *spcmd */
   DECLARE_CHARACTER(cstr,132);          /* VMS command line */
 
-  if ( (*status) != SAI__OK ) 		/* Check status */
+/* Check inherited global stratus on entry */
+  if ( *status != SAI__OK )
     return;
 
-  esym = (char *)                       /* Make space for new string */
-         malloc(type_length+1+10);
-  strncpy( esym, "AST_", 4 );           /* Construct environment variable */
+/* Make space for new string */
+  esym = (char *) malloc(type_length+1+10);
+
+/* Construct environment variable name */
+  strncpy( esym, "AST_", 4 );
   strncpy( esym+4, type, type_length );
   strcpy( esym+4+type_length, "_SPOOL" );
 
-  spcmd = getenv( esym );		/* Get spool command */
+/* Get spool command */
+  spcmd = getenv( esym );
   free( esym );
 
-  if ( spcmd )                          /* Spool command defined? */
-    {
-    fname = cnf_creim( file,            /* Import filename to C string */
-              file_length );
+/* Spool command defined? */
+  if ( spcmd ) {
+
+/* Import filename to C string */
+    fname = cnf_creim( file, file_length );
 
     splen = strlen(spcmd);
     strcpy( cstr, spcmd );
@@ -88,7 +156,9 @@ F77_SUBROUTINE(util_spool)( CHARACTER(file), CHARACTER(type),
     strcpy( cstr + splen + 1, fname );
 
 #ifdef VAX
-    if ( F77_ISTRUE(*del) )		/* Deletion required? */
+
+/* Deletion required? */
+    if ( F77_ISTRUE(*del) )
       if ( strncmp(cstr,"PRINT",5) == 0 )	/* Simple print command? */
         {
         strcpy( cstr + splen + 		/* Tack on a delete qualifier */
@@ -98,32 +168,39 @@ F77_SUBROUTINE(util_spool)( CHARACTER(file), CHARACTER(type),
     lib$spawn( CHARACTER_ARG(cstr) TRAIL(cstr) );
 
 #else
-    if ( (cstatus = vfork()) == 0 )	/* Successful child creation returns */
-					/* zero to the child process and the */
-					/* +ve PID to the parent */
-      {
-      if ( cstatus = execlp( spcmd, 	/* Child ok, so try to run command */
-                      spcmd, fname,
-                        (char *)0 ) == -1 )
-        {
+
+/* Successful child creation returns zero to the child process and the */
+/* +ve PID to the parent */
+    if ( (cstatus = vfork()) == 0 ) {
+
+/* Child ok, so try to run command */
+      if ( cstatus = execlp( spcmd, spcmd, fname, (char *)0 ) == -1 ) {
+
         printf( "Failed to execute command %s %s\n", spcmd, fname );
 
         _exit(1);			/* Kills the child process */
         }
       }
-    else if ( cstatus <= -1 )		/* Parent only, failed to create child */
-      {
+
+/* Parent only, failed to create child */
+    else if ( cstatus <= -1 ) {
+
       *status = SAI__ERROR;
       ems_rep_c( " ", "Failed to create sub-process", status );
       }
-    else				/* Parent, successful child creation */
-      {
-      wstatus = wait( &cstatus );	/* Wait for child to finish */
 
-      if ( F77_ISTRUE(*del) )		/* Delete the file? */
-	{
-	if ( remove( fname ) )		/* Try to delete it */
-          {
+/* Parent, successful child creation */
+    else {
+
+/* Wait for child to finish */
+      wstatus = wait( &cstatus );
+
+/* Delete the file? */
+      if ( F77_ISTRUE(*del) ) {
+
+/* Try to delete it */
+	if ( remove( fname ) ) {
+
           *status = SAI__ERROR;
           ems_setc_c( "FILE", file, file_length );
           ems_rep_c( " ", "Unable to delete ^FILE after spooling", status );
@@ -132,10 +209,11 @@ F77_SUBROUTINE(util_spool)( CHARACTER(file), CHARACTER(type),
       }
 #endif
 
-    cnf_free( fname );			/* Free space */
+/* Free space */
+    cnf_free( fname );
     }
-  else
-    {
+
+  else {
     *status = SAI__ERROR;
     ems_setc_c( "FILE", file, file_length );
     ems_rep_c( " ", "File ^FILE not spooled", status );
