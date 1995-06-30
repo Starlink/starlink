0cf2cb22ba103b36dc4925aabf981a55843d98d6
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-06-30 14:33:07 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/util_ploc_c.c
+++ applications/obsolete/asterix/lib/util_ploc_c.c
@@ -1,38 +1,102 @@
-/*+
- *  Name:
- *     util_ploc_c.c
- *
- *  Purpose:
- *     Returns the address of an argument when used as a Fortran function.
- *     This routine is direct replacement for %LOC on all platforms - it
- *     is needed becausse the SUN version doesn't always work.
- *
- *  Language:
- *
- *     Starlink ANSI C
- *
- *  Description:
- *
- *     This routine is direct replacement for %LOC on all platforms - it
- *     is needed becausse the SUN version doesn't always work.
- *
- *  Authors:
- *
- *     David J. Allan (BHVAD::DJA)
- *
- *  History:
- *
- *     9-DEC-1992 (DJA):
- *        Original version.
- *- */
+/*
+*+
+*  Name:
+*     UTIL_PLOC
 
-/* Include Statements: */
-#include "f77.h"                       /* c <-> FORTRAN interfacing */
-#include "cnf.h"                       /* c <-> FORTRAN strings */
+*  Purpose:
+*     Returns address of its argument
 
-F77_INTEGER_FUNCTION(util_ploc)( INTEGER(arg) )
+*  Language:
+*     Starlink ANSI C
+
+*  Invocation:
+*     INTEGER ADDR
+*     ADDR = UTIL_PLOC( ARG )
+
+*  Description:
+*     Returns the address of its argument. Used by Fortran programs which
+*     want to construct pointers from variables. Function is identical to
+*     the intrinsic %LOC supported on many architectures, but can be used
+*     with EXTERNAL data types and on some machines which don't support
+*     %LOC.
+
+*  Arguments:
+*     ARG = anytype (given)
+*        The address of the Fortran item whose address is required
+
+*  Examples:
+*     {routine_example_text}
+*        {routine_example_description}
+
+*  Pitfalls:
+*     {pitfall_description}...
+
+*  Notes:
+*     {routine_notes}...
+
+*  Prior Requirements:
+*     {routine_prior_requirements}...
+
+*  Side Effects:
+*     {routine_side_effects}...
+
+*  Algorithm:
+*     {algorithm_description}...
+
+*  Accuracy:
+*     {routine_accuracy}
+
+*  Timing:
+*     {routine_timing}
+
+*  External Routines Used:
+*     {name_of_facility_or_package}:
+*        {routine_used}...
+
+*  Implementation Deficiencies:
+*     {routine_deficiencies}...
+
+*  {machine}-specific features used:
+*     {routine_machine_specifics}...
+
+*  {DIY_prologue_heading}:
+*     {DIY_prologue_text}
+
+*  References:
+*     util Subroutine Guide : http://www.sr.bham.ac.uk:8080/asterix-docs/Programmer/Guides/util.html
+
+*  Keywords:
+*     package:util, usage:public
+
+*  Copyright:
+*     Copyright (C) University of Birmingham, 1995
+
+*  Authors:
+*     DJA: David J. Allan (Jet-X, University of Birmingham)
+*     {enter_new_authors_here}
+
+*  History:
+*      9 Dec 1992 (DJA):
+*        Original version.
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+*/
+
+/*
+ *  Include files
+ */
+#include "f77.h"
+
+/*
+ *  Body of code
+ */
+F77_POINTER_TYPE F77_EXTERNAL_NAME(util_ploc)( INTEGER(arg )
   {
-  GENPTR_INTEGER(arg)			/* Any old argument */
+  GENPTR_INTEGER(arg)
 
-  return (int) arg;
+  return (F77_POINTER_TYPE) arg;
   }
