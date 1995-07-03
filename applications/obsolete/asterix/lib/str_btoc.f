c5f4c78f115b650448958ef76ec31ca4ab183d5f
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-07-03 09:39:49 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/str_btoc.f
+++ applications/obsolete/asterix/lib/str_btoc.f
@@ -1,57 +1,136 @@
-*+  STR_BTOC - Converts 8-bit binary number from  BYTE to CHAR
-      SUBROUTINE STR_BTOC(NUM,STRING,STATUS)
-*    Description :
+      SUBROUTINE STR_BTOC( NUM, STRING, STATUS )
+*+
+*  Name:
+*     STR_BTOC
+
+*  Purpose:
+*     Converts 8-bit binary number from BYTE to CHAR
+
+*  Language:
+*     Starlink Fortran
+
+*  Invocation:
+*     CALL STR_BTOC( NUM, STRING, STATUS )
+
+*  Description:
 *     Returns the binary representation of an 8-bit number in character form
-*    Method :
-*    Deficiencies :
-*    Bugs :
-*    Authors :
-*     (BHVAD::RJV)
-*    History :
-*    Type Definitions :
-      IMPLICIT NONE
-*    Global constants :
-      INCLUDE 'SAE_PAR'
-*    Import :
-      BYTE NUM
-*    Import-Export :
-*    Export :
-      CHARACTER*(*) STRING
-*    Status :
-      INTEGER STATUS
-*    External references :
-      BYTE BIT_ANDUB
-*    Local constants :
-      INTEGER IBIT,JBIT
-      BYTE BIT
-*    Local variables :
+
+*  Arguments:
+*     NUM = BYTE (given)
+*        The byte value to convert
+*     STRING = CHARACTER*(*)
+*        The string representation of NUM
+*     STATUS = INTEGER (given and returned)
+*        The global status.
+
+*  Examples:
+*     {routine_example_text}
+*        {routine_example_description}
+
+*  Pitfalls:
+*     {pitfall_description}...
+
+*  Notes:
+*     {routine_notes}...
+
+*  Prior Requirements:
+*     {routine_prior_requirements}...
+
+*  Side Effects:
+*     {routine_side_effects}...
+
+*  Algorithm:
+*     {algorithm_description}...
+
+*  Accuracy:
+*     {routine_accuracy}
+
+*  Timing:
+*     {routine_timing}
+
+*  External Routines Used:
+*     {name_of_facility_or_package}:
+*        {routine_used}...
+
+*  Implementation Deficiencies:
+*     {routine_deficiencies}...
+
+*  References:
+*     STR Subroutine Guide : http://www.sr.bham.ac.uk:8080/asterix-docs/Programmer/Guides/str.html
+
+*  Keywords:
+*     package:str, usage:public, bit operations
+
+*  Copyright:
+*     Copyright (C) University of Birmingham, 1995
+
+*  Authors:
+*     RJV: Bob Vallance (ROSAT, University of Birmingham)
+*     {enter_new_authors_here}
+
+*  History:
+*     3 Jul 1995 (DJA):
+*        Original version.
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
 *-
-      IF (STATUS.EQ.SAI__OK) THEN
-        IF (LEN(STRING).LT.8) THEN
-          STATUS=SAI__ERROR
-          CALL ERR_REP('TOOSHORT','Character string too short',STATUS)
-        ELSE
-          STRING='00000000'
 
-* scan number bit by bit
-          IBIT=0
-          DO WHILE (IBIT.LE.7.AND.STATUS.EQ.SAI__OK)
-            IF (IBIT.LT.7) THEN
-              BIT=2**IBIT
-            ELSE
-              BIT=-128
-            ENDIF
-            JBIT=8-IBIT
-            IF (BIT_ANDUB(NUM,BIT)/BIT) THEN
-              STRING(JBIT:JBIT)='1'
-            ENDIF
-            IBIT=IBIT+1
-          ENDDO
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
 
-        ENDIF
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
 
-        IF (STATUS.NE.SAI__OK) THEN
-          CALL ERR_REP('STR_BTOC','from STR_BTOC',STATUS)
-        ENDIF
-      ENDIF
+*  Arguments Given:
+      BYTE			NUM
+
+*  Arguments Returned:
+      CHARACTER*(*)		STRING
+
+*  Status:
+      INTEGER			STATUS             	! Global status
+
+*  External References:
+      EXTERNAL			BIT_ANDUB
+        BYTE			BIT_ANDUB
+
+*  Local Variables:
+      INTEGER 			IBIT,JBIT
+
+      BYTE 			BIT
+*.
+
+*  Check inherited global status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+      IF (LEN(STRING).LT.8) THEN
+        STATUS = SAI__ERROR
+        CALL ERR_REP( 'TOOSHORT', 'Character string too short', STATUS )
+
+      ELSE
+        STRING = '00000000'
+
+*    Scan number bit by bit
+        IBIT=0
+        DO WHILE (IBIT.LE.7)
+          IF (IBIT.LT.7) THEN
+            BIT=2**IBIT
+          ELSE
+            BIT=-128
+          END IF
+          JBIT=8-IBIT
+          IF (BIT_ANDUB(NUM,BIT)/BIT) THEN
+            STRING(JBIT:JBIT)='1'
+          ENDIF
+          IBIT = IBIT + 1
+        END DO
+
+      END IF
+
+*  Report any errors
+      IF ( STATUS .NE. SAI__OK ) CALL AST_REXIT( 'STR_BTOC', STATUS )
+
       END
