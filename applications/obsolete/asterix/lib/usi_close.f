232154d8d907220a414c39a762eee7cab24dd74d
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-08-17 11:08:15 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/usi_close.f
+++ applications/obsolete/asterix/lib/usi_close.f
@@ -5,6 +5,7 @@
 *    Type definitions :
       IMPLICIT NONE
       INCLUDE  'SAE_PAR'
+      INCLUDE  'ADI_PAR'
       INCLUDE  'DAT_PAR'
       INCLUDE  'USI_CMN'
 *    Local constants :
@@ -21,15 +22,21 @@
       ISTAT=SAI__OK
       DO N=1,USI__NMAX
         IF (DS(N).USED) THEN
-          IF ( DS(N).LOC .NE. DAT__NOLOC ) THEN
+          IF ( DS(N).ADIFPN ) THEN
+            CALL ADI_FCLOSE( DS(N).ADI_ID, STATUS )
+
+          ELSE IF ( DS(N).LOC .NE. DAT__NOLOC ) THEN
             CALL DAT_VALID(DS(N).LOC,VALID,ISTAT)
             IF (VALID) THEN
               CALL DAT_ANNUL(DS(N).LOC,ISTAT)
             ENDIF
-            DS(N).LOC=DAT__NOLOC
-            DS(N).USED=.FALSE.
-            DS(N).IO=BLANK
           ENDIF
+
+          DS(N).ADI_ID = ADI__NULLID
+          DS(N).LOC=DAT__NOLOC
+          DS(N).USED=.FALSE.
+          DS(N).IO=BLANK
+
         ENDIF
       ENDDO
       USI_SYINIT = .FALSE.
