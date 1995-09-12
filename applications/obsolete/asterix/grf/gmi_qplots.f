5707b370ae3974efa4ff136bf00a89fe6871730a
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-09-12 09:13:34 +0000

    Initial revision

---
+++ applications/obsolete/asterix/grf/gmi_qplots.f
@@ -0,0 +1,110 @@
+      SUBROUTINE GMI_QPLOTS( ID, NPLOT, STATUS )
+*+
+*  Name:
+*     GMI_QPLOTS
+
+*  Purpose:
+*     Get number of defined plots in a multi-graph dataset
+
+*  Language:
+*     Starlink Fortran
+
+*  Invocation:
+*     CALL GMI_QPLOTS( ID, NPLOT, STATUS )
+
+*  Description:
+*     {routine_description}
+
+*  Arguments:
+*     ID = INTEGER (given)
+*        ADI identifier of multi-graph dataset
+*     NPLOT = INTEGER (returned)
+*        Number of plots defined
+*     STATUS = INTEGER (given and returned)
+*        The global status.
+
+*  Examples:
+*     {routine_example_text}
+*        {routine_example_description}
+
+*  Pitfalls:
+*     {pitfall_description}...
+
+*  Notes:
+*     {routine_notes}...
+
+*  Prior Requirements:
+*     {routine_prior_requirements}...
+
+*  Side Effects:
+*     {routine_side_effects}...
+
+*  Algorithm:
+*     {algorithm_description}...
+
+*  Accuracy:
+*     {routine_accuracy}
+
+*  Timing:
+*     {routine_timing}
+
+*  External Routines Used:
+*     {name_of_facility_or_package}:
+*        {routine_used}...
+
+*  Implementation Deficiencies:
+*     {routine_deficiencies}...
+
+*  References:
+*     GMI Subroutine Guide : http://www.sr.bham.ac.uk/asterix-docs/Programmer/Guides/gmi.html
+
+*  Keywords:
+*     package:gmi, usage:public
+
+*  Copyright:
+*     Copyright (C) University of Birmingham, 1995
+
+*  Authors:
+*     DJA: David J. Allan (Jet-X, University of Birmingham)
+*     {enter_new_authors_here}
+
+*  History:
+*     12 Sep 1995 (DJA):
+*        Original version.
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+
+*  Arguments Given:
+      INTEGER			ID
+
+*  Arguments Returned:
+      INTEGER			NPLOT
+
+*  Status:
+      INTEGER 			STATUS             	! Global status
+
+*  Local Variables:
+      CHARACTER*(DAT__SZLOC)    LOC
+*.
+
+*  Check inherited global status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  Just invoke HDS for the moment
+      CALL ADI1_GETLOC( ID, LOC, STATUS )
+      CALL GMD_QPLOTS( LOC, NPLOT, STATUS )
+
+*  Report any errors
+      IF ( STATUS .NE. SAI__OK ) CALL AST_REXIT( 'GMI_QPLOTS', STATUS )
+
+      END
