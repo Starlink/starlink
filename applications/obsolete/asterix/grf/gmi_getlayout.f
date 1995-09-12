19ba1a0e197d4a51760d7fba26606f4686f4a2cd
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-09-12 09:15:09 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/grf/gmi_getlayout.f
+++ applications/obsolete/asterix/grf/gmi_getlayout.f
@@ -89,6 +89,7 @@
 
 *  Global Constants:
       INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'DAT_PAR'
 
 *  Arguments Given:
       INTEGER			ID
--- applications/obsolete/asterix/grf/gmi_getplot.f
+++ applications/obsolete/asterix/grf/gmi_getplot.f
@@ -87,6 +87,7 @@
 
 *  Global Constants:
       INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'DAT_PAR'
 
 *  Arguments Given:
       INTEGER			ID, PLOT
