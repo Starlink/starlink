4cbcc7af3ae8d221913892f07a2d09c3ac4c7921
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-10-10 10:29:11 +0000

    Initial revision

---
+++ applications/obsolete/asterix/grf/gcb1_csave.f
@@ -0,0 +1,137 @@
+      SUBROUTINE GCB1_CSAVE( NARG, ARGS, OARG, STATUS )
+*+
+*  Name:
+*     GCB1_CSAVE
+
+*  Purpose:
+*     Save a cached GCB
+
+*  Language:
+*     Starlink Fortran
+
+*  Invocation:
+*     CALL GCB1_CSAVE( NARG, ARGS, OARG, STATUS )
+
+*  Description:
+*     {routine_description}
+
+*  Arguments:
+*     NARG = INTEGER (given)
+*        Number of method arguments
+*     ARGS(*) = INTEGER (given)
+*        ADI identifier of method arguments
+*     OARG = INTEGER (returned)
+*        Output data
+*     STATUS = INTEGER (given and returned)
+*        The global status.
+
+*  Examples:
+*     {routine_example_text}
+*        {routine_example_description}
+
+*  Pitfalls:
+*     {pitfall_description}...
+
+*  Notes:
+*     {routine_notes}...
+
+*  Prior Requirements:
+*     {routine_prior_requirements}...
+
+*  Side Effects:
+*     {routine_side_effects}...
+
+*  Algorithm:
+*     {algorithm_description}...
+
+*  Accuracy:
+*     {routine_accuracy}
+
+*  Timing:
+*     {routine_timing}
+
+*  External Routines Used:
+*     {name_of_facility_or_package}:
+*        {routine_used}...
+
+*  Implementation Deficiencies:
+*     {routine_deficiencies}...
+
+*  References:
+*     GCB Subroutine Guide : http://www.sr.bham.ac.uk/asterix-docs/Programmer/Guides/gcb.html
+
+*  Keywords:
+*     package:gcb, usage:private
+
+*  Copyright:
+*     Copyright (C) University of Birmingham, 1995
+
+*  Authors:
+*     DJA: David J. Allan (Jet-X, University of Birmingham)
+*     {enter_new_authors_here}
+
+*  History:
+*     10 Oct 1995 (DJA):
+*        Original version.
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'DAT_PAR'
+      INCLUDE 'GCB_PAR'
+
+*  Arguments Given:
+      INTEGER                   NARG, ARGS(*)
+
+*  Arguments Returned:
+      INTEGER                   OARG
+
+*  Status:
+      INTEGER 			STATUS             	! Global status
+
+*  Local Variables:
+      CHARACTER*(DAT__SZLOC)	GCBLOC			! Locator to GCB
+
+      INTEGER			CACHE			! Cache address
+      INTEGER			GCBPTR			! Mapped file GCB
+      INTEGER			NBYTE			! Bytes to store GCB
+      INTEGER			NSCAL, NSTRUC		! # cmps in GCB
+*.
+
+*  Check inherited global status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  Extract cache address
+      CALL ADI_GET0I( ARGS(1), CACHE, STATUS )
+
+*  Find size required
+      DISP = GCB__SZPTR + 1
+      CALL GCB_GETI_SUB( %VAL(CACHE), DISP, GCB__SZPTR, GCB__PTRFMT,
+     :                                               NSCAL, STATUS )
+      DISP = DISP + GCB__SZPTR
+      CALL GCB_GETI_SUB( %VAL(CACHE), DISP, GCB__SZPTR, GCB__PTRFMT,
+     :                                              NSTRUC, STATUS )
+      NBYTE = GCB__NHDBLK * GCB__SZPTR + NSCAL + NSTRUC
+
+*  Locate and map the file GCB
+      CALL GCB1_SAVE_MAPGCB( ARGS(2), NBYTE, GCBLOC, GCBPTR, STATUS )
+
+*  Copy cache to output
+      CALL ARR_COP1B( NBYTE, %VAL(CACHE), %VAL(GCBPTR), STATUS )
+
+*  Release output
+      CALL DAT_UNMAP( GCBLOC, STATUS )
+      CALL DAT_ANNUL( GCBLOC, STATUS )
+
+*  Report any errors
+      IF ( STATUS .NE. SAI__OK ) CALL AST_REXIT( 'GCB1_CSAVE', STATUS )
+
+      END
