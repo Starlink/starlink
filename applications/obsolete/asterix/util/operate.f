c289b44a0878afdd9979a2d92a407346ce1cca34
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-10-19 10:23:23 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/util/operate.f
+++ applications/obsolete/asterix/util/operate.f
@@ -328,7 +328,8 @@
       IF ( (NBAD.GT.0) .AND. .NOT. QOK ) THEN
         CALL BDI_PUT( OFID, 'Quality', 'UBYTE', 1, NELM, %VAL(QPTR),
      :                STATUS )
-        CALL BDI_PUT( OFID, 'QualityMask', 'UBYTE', 0, 0, MASK, STATUS )
+        IMASK = MASK
+        CALL BDI_PUT0I( OFID, 'QualityMask', IMASK, STATUS )
       END IF
 
 *  Amend data label if present
