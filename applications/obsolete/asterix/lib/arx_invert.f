5aeb4e9ea7bd4314236c8c7245f12ecd8353dd09
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-11-07 12:34:09 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/arx_invert.f
+++ applications/obsolete/asterix/lib/arx_invert.f
@@ -101,7 +101,8 @@
       REAL X1,X2,Y1,Y2,R1,R2
       REAL XW1,XW2,YW1,YW2,XR1,XR2,YR1,YR2,A1,A2
       INTEGER IL,NL
-      INTEGER C1,C2,IC
+      INTEGER C1,C2
+      INTEGER SUB
       INTEGER NCIRC,NELL,NBOX,NAND,NNOT
 *-
 
@@ -120,8 +121,6 @@
           CALL ARX_GET(ARDID,IL,TEXT,STATUS)
           CALL CHR_UCASE(TEXT)
           CALL CHR_RMBLK(TEXT)
-	print *,text
-        call flush(6)
           NCIRC=NCIRC+STR_OCCUR('CIRCLE',TEXT)
           NELL=NELL+STR_OCCUR('ELLIPSE',TEXT)
           NBOX=NBOX+STR_OCCUR('BOX',TEXT)
@@ -129,31 +128,20 @@
           NNOT=NNOT+STR_OCCUR('NOT',TEXT)
         ENDDO
 
-	print *,ncirc
-        call flush(6)
 
 *  simple circle
         IF (NCIRC.EQ.1) THEN
           SHAPE='CIRCLE'
           C1=INDEX(TEXT,'(')+1
-          C2=INDEX(TEXT(C1:),')')-1
-          DO IC=C1,C2
-            IF (TEXT(IC:IC).EQ.',') THEN
-              TEXT(IC:IC)=' '
-            ENDIF
-          ENDDO
-	print *,text(c1:c2)
-        call flush(6)
+          C2=INDEX(TEXT,')')-1
           READ(TEXT(C1:C2),*) PAR(1),PAR(2),PAR(3)
 
-	print *,par(1),par(2),par(3)
-        call flush(6)
 *  simple ellipse
         ELSEIF (NELL.EQ.1) THEN
           SHAPE='ELLIPSE'
-          C1=INDEX(TEXT,'(')
-          C2=INDEX(TEXT(C1:),')')
-          READ(TEXT(C1+1:C2-1),*) X1,Y1,XR1,YR1,A1
+          C1=INDEX(TEXT,'(')+1
+          C2=INDEX(TEXT,')')-1
+          READ(TEXT(C1:C2),*) X1,Y1,XR1,YR1,A1
           PAR(1)=X1
           PAR(2)=Y1
           PAR(3)=A1
@@ -163,9 +151,9 @@
 *  simple rectangular box
         ELSEIF (NBOX.EQ.1) THEN
           SHAPE='BOX'
-          C1=INDEX(TEXT,'(')
-          C2=INDEX(TEXT(C1:),')')
-          READ(TEXT(C1+1:C2-1),*) PAR(1),PAR(2),PAR(3),PAR(4)
+          C1=INDEX(TEXT,'(')+1
+          C2=INDEX(TEXT,')')-1
+          READ(TEXT(C1:C2),*) PAR(1),PAR(2),PAR(3),PAR(4)
 
 *  circular annulus
         ELSEIF (NCIRC.EQ.2.AND.NAND.EQ.1.AND.NNOT.EQ.1) THEN
@@ -175,12 +163,13 @@
             CALL CHR_RMBLK(TEXT(C1:))
             C1=CHR_LEN(TEXT)+1
           ENDDO
-          C1=INDEX(TEXT,'(')
-          C2=INDEX(TEXT(C1:),')')
-          READ(TEXT(C1+1:C2-1),*) X1,Y1,R1
-          C1=INDEX(TEXT(C2:),'(')
-          C2=INDEX(TEXT(C1:),')')
-          READ(TEXT(C1+1:C2-1),*) X2,Y2,R2
+          C1=INDEX(TEXT,'(')+1
+          C2=INDEX(TEXT,')')-1
+          READ(TEXT(C1:C2),*) X1,Y1,R1
+          SUB=C2+2
+          C1=INDEX(TEXT(SUB:),'(')+SUB
+          C2=INDEX(TEXT(SUB:),')')+SUB-2
+          READ(TEXT(C1:C2),*) X2,Y2,R2
           IF (R2.LT.R1.AND.X1.EQ.X2.AND.Y1.EQ.Y2) THEN
             SHAPE='ANNULUS'
             PAR(1)=X1
@@ -199,11 +188,12 @@
             CALL CHR_RMBLK(TEXT(C1:))
             C1=CHR_LEN(TEXT)+1
           ENDDO
-          C1=INDEX(TEXT,'(')
-          C2=INDEX(TEXT(C1:),')')
+          C1=INDEX(TEXT,'(')+1
+          C2=INDEX(TEXT,')')-1
           READ(TEXT(C1+1:C2-1),*) X1,Y1,XW1,YW1
-          C1=INDEX(TEXT(C2:),'(')
-          C2=INDEX(TEXT(C1:),')')
+          SUB=C2+2
+          C1=INDEX(TEXT(SUB:),'(')+SUB
+          C2=INDEX(TEXT(SUB:),')')+SUB-2
           READ(TEXT(C1+1:C2-1),*) X2,Y2,XW2,YW2
           IF (XW2.LT.XW1.AND.YW1.LT.YW2.AND.X1.EQ.X2.AND.Y1.EQ.Y2)
      :                                                         THEN
@@ -226,11 +216,12 @@
             CALL CHR_RMBLK(TEXT(C1:))
             C1=CHR_LEN(TEXT)+1
           ENDDO
-          C1=INDEX(TEXT,'(')
-          C2=INDEX(TEXT(C1:),')')
+          C1=INDEX(TEXT,'(')+1
+          C2=INDEX(TEXT,')')-1
           READ(TEXT(C1+1:C2-1),*) X1,Y1,XR1,YR1,A1
-          C1=INDEX(TEXT(C2:),'(')
-          C2=INDEX(TEXT(C1:),')')
+          SUB=C2+2
+          C1=INDEX(TEXT(SUB:),'(')+SUB
+          C2=INDEX(TEXT(SUB:),')')+SUB-2
           READ(TEXT(C1+1:C2-1),*) X2,Y2,XR2,YR2,A2
           IF (XR2.LT.XR1.AND.YR1.LT.YR2.AND.X1.EQ.X2.AND.Y1.EQ.Y2.AND.
      :                                                  A1.EQ.A2) THEN
