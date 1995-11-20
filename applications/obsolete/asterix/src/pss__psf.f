da01f23b8ebc32031965dac20f795057133af92c
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-11-20 11:30:30 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/src/pss__psf.f
+++ applications/obsolete/asterix/src/pss__psf.f
@@ -376,7 +376,7 @@
               GOTO 99
             END IF
             DO IPOS = 1, NIPOS
-              PSF_PPS(IPOS) = ABS(PSF_PIXL(ILEV,IPOS)
+              PSF_PPS(IPOS) = ABS(PSF_PIXL(ILEV,IPOS))
             END DO
             NUR = NIPOS
 
