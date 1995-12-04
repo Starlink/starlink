3b75dedcf3398f7b841996ceb82d0c577aa413c6
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-12-04 11:05:00 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/spec/sbin.f
+++ applications/obsolete/asterix/spec/sbin.f
@@ -1,141 +1,236 @@
-*+  SBIN - Rebin spectrum
       SUBROUTINE SBIN( STATUS )
-*    Description :
-*     rebins a spectrum to give approximately equal population
-*     in each bin
-*    Authors :
-*     BHVAD::RJV
-*    History :
-*
-*      6 Nov 92 : V1.5-1 Max bin content changed to min (RJV)
-*     18 Nov 93 : V1.7-0 Added missing AST_INIT call (DJA)
-*     24 Nov 94 : V1.8-0 Now use USI for user interface (DJA)
-*     27 Mar 95 : V1.8-1 BIT_ used (RJV)
-*     21 Apr 95 : V1.8-2 Updated data interface (DJA)
-*
-*    Type definitions :
-      IMPLICIT NONE
-*    Global constants :
-      INCLUDE 'SAE_PAR'
-      INCLUDE 'ADI_PAR'
-*    Global variables :
-*    Status :
-      INTEGER STATUS
-*    Function declarations :
-*    Local constants :
-*    Local variables :
-      CHARACTER*80 TEXT
-      REAL SUM
-      REAL MINVAL
-      INTEGER NDIM,DIMS(ADI__MXDIM)
-      INTEGER INVAL,ONVAL
-      INTEGER IDPTR,IAPTR,IWPTR,IVPTR,IQPTR
-      INTEGER ODPTR,OAPTR,OWPTR,OVPTR
-      INTEGER TDPTR,TAPTR,TWPTR,TVPTR
-      INTEGER OPT
-      INTEGER			IFID			! Input dataset id
-      INTEGER			OFID			! Output dataset id
+*+
+*  Name:
+*     SBIN
 
-      BYTE 			MASK
+*  Purpose:
+*     Rebin a spectrum
 
-      LOGICAL DOK,VOK,QOK
+*  Language:
+*     Starlink Fortran
 
-*  Version :
-      CHARACTER*30 VERSION
-      PARAMETER (VERSION = 'SBIN Version 1.8-2')
+*  Type of Module:
+*     ASTERIX task
+
+*  Invocation:
+*     CALL SBIN( STATUS )
+
+*  Arguments:
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
+
+*  Description:
+*     Rebins a spectrum to give approximately equal population in each bin.
+
+*  Usage:
+*     sbin {parameter_usage}
+
+*  Environment Parameters:
+*     INP = CHAR (read)
+*        Input spectrum name
+*     OPT = INTEGER (read)
+*        Rebinning option
+*     NBIN = INTEGER (read)
+*        Number of bins for option 1
+*     MINVAL = REAL (read)
+*        Minimum bin value for option 2
+*     OUT = CHAR (read)
+*        Output spectrum name
+
+*  Examples:
+*     {routine_example_text}
+*        {routine_example_description}
+
+*  Pitfalls:
+*     {pitfall_description}...
+
+*  Notes:
+*     {routine_notes}...
+
+*  Prior Requirements:
+*     {routine_prior_requirements}...
+
+*  Side Effects:
+*     {routine_side_effects}...
+
+*  Algorithm:
+*     {algorithm_description}...
+
+*  Accuracy:
+*     {routine_accuracy}
+
+*  Timing:
+*     {routine_timing}
+
+*  Implementation Status:
+*     {routine_implementation_status}
+
+*  External Routines Used:
+*     {name_of_facility_or_package}:
+*        {routine_used}...
+
+*  Implementation Deficiencies:
+*     {routine_deficiencies}...
+
+*  References:
+*     {task_references}...
+
+*  Keywords:
+*     sbin, usage:public
+
+*  Copyright:
+*     Copyright (C) University of Birmingham, 1995
+
+*  Authors:
+*     RJV: Robert Vallance (ROSAT,University of Birmingham)
+*     DJA: David J. Allan (Jet-X, University of Birmingham)
+*     {enter_new_authors_here}
+
+*  History:
+*        Nov 1992 V1.5-0 (RJV):
+*        Original version.
+*      6 Nov 1992 V1.5-1 (RJV):
+*        Max bin content changed to min (RJV)
+*     18 Nov 1993 V1.7-0 (DJA):
+*        Added missing AST_INIT call (DJA)
+*     24 Nov 1994 V1.8-0 (DJA):
+*        Now use USI for user interface (DJA)
+*     27 Mar 1995 V1.8-1 (RJV):
+*        BIT_ used
+*     21 Apr 1995 V1.8-2 (DJA):
+*        Updated data interface
+*      4 Dec 1995 V2.0-0 (DJA):
+*        ADI port
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
 *-
 
-*    Version id
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'ADI_PAR'
+
+*  Status:
+      INTEGER			STATUS             	! Global status
+
+*  Local Constants:
+      CHARACTER*30		VERSION
+        PARAMETER		( VERSION = 'SBIN Version V2.0-0' )
+
+*  Local Variables:
+      REAL 			SUM			! Data sum
+      REAL 			MINVAL			! Min bin value
+
+      INTEGER 			NDIM,DIMS(ADI__MXDIM)	! I/p dimensions
+      INTEGER 			INVAL,ONVAL		! I/p & o/p sizes
+      INTEGER 			IDPTR,IAPTR,IWPTR,	! I/p data, axis, widths
+     :                            IVPTR,IQPTR		! variance and quality
+      INTEGER 			TDPTR,TAPTR,TWPTR,TVPTR ! Temp data,axis,widths
+							! and variance
+      INTEGER 			OPT			! Rebinning option
+      INTEGER			IFID			! Input dataset id
+      INTEGER			OFID			! Output dataset id
+      INTEGER			SPID			! Interface object
+
+      BYTE 			MASK			! I/p quality mask
+
+      LOGICAL 			DOK,VOK,QOK		! Input data bits ok?
+*.
+
+*  Check inherited global status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  Version id
       CALL MSG_PRNT( VERSION )
 
-*    Initialise Asterix
+*  Initialise ASTERIX
       CALL AST_INIT()
 
 *  Get input & output files
-      CALL USI_TASSOC2( 'INP', 'OUT', 'READ', IFID, OFID, STATUS )
+      CALL USI_ASSOC( 'INP', 'Spectrum', 'READ', IFID, STATUS )
+      CALL USI_CREAT( 'OUT', ADI__NULLID, OFID, STATUS )
 
-      CALL BDI_CHKDATA(IFID,DOK,NDIM,DIMS,STATUS)
+*  Check input
+      CALL BDI_CHK( IFID, 'Data', DOK, STATUS )
+      CALL BDI_GETSHP( IFID, ADI__MXDIM, DIMS, NDIM, STATUS )
       IF (DOK.AND.NDIM.EQ.1) THEN
 
-*  get pointers to input components
-        INVAL=DIMS(1)
-        CALL BDI_MAPDATA(IFID,'READ',IDPTR,STATUS)
-        CALL BDI_CHKVAR(IFID,VOK,NDIM,DIMS,STATUS)
-        IF (VOK) THEN
-          CALL BDI_MAPVAR(IFID,'READ',IVPTR,STATUS)
-        ENDIF
-        CALL BDI_MAPAXVAL(IFID,'READ',1,IAPTR,STATUS)
-        CALL BDI_MAPAXWID(IFID,'READ',1,IWPTR,STATUS)
+*    Get pointers to input components
+        INVAL = DIMS(1)
+        CALL BDI_MAPR( IFID, 'Data', 'READ', IDPTR, STATUS )
+        CALL BDI_CHK( IFID, 'Variance', VOK, STATUS )
+        IF ( VOK ) THEN
+          CALL BDI_MAPR( IFID, 'Variance', 'READ', IVPTR, STATUS )
+        END IF
+        CALL BDI_AXMAPR( IFID, 1, 'Data', 'READ', IAPTR, STATUS )
+        CALL BDI_AXMAPR( IFID, 1, 'Width', 'READ', IWPTR, STATUS )
 
-*  get total counts taking account of any QUALITY
-        CALL BDI_CHKQUAL(IFID,QOK,NDIM,DIMS,STATUS)
-        IF (QOK) THEN
-          CALL BDI_MAPQUAL(IFID,'READ',IQPTR,STATUS)
-          CALL BDI_GETMASK(IFID,MASK,STATUS)
-          CALL ARR_SUM1RQ(INVAL,%VAL(IDPTR),%VAL(IQPTR),MASK,SUM,STATUS)
+*    Get total counts taking account of any QUALITY
+        CALL BDI_CHK( IFID, 'Quality', QOK, STATUS )
+        IF ( QOK ) THEN
+          CALL BDI_MAP( IFID, 'Data', 'UBYTE', 'READ', IQPTR, STATUS )
+          CALL BDI_GET( IFID, 'QualityMask', 'UBYTE', 0, 0, MASK,
+     :                  IDUM, STATUS )
+          CALL ARR_SUM1RQ( INVAL, %VAL(IDPTR), %VAL(IQPTR), MASK, SUM,
+     :                     STATUS )
         ELSE
-          CALL ARR_SUM1R(INVAL,%VAL(IDPTR),SUM,STATUS)
-        ENDIF
+          CALL ARR_SUM1R( INVAL, %VAL(IDPTR), SUM, STATUS )
+        END IF
 
-*  get some temporary storage for rebined data
-        CALL DYN_MAPR(1,INVAL,TDPTR,STATUS)
-        CALL DYN_MAPR(1,INVAL,TAPTR,STATUS)
-        CALL DYN_MAPR(1,INVAL,TWPTR,STATUS)
-        IF (VOK) THEN
-          CALL DYN_MAPR(1,INVAL,TVPTR,STATUS)
-        ENDIF
+*    Get some temporary storage for rebined data
+        CALL DYN_MAPR( 1, INVAL, TDPTR, STATUS )
+        CALL DYN_MAPR( 1, INVAL, TAPTR, STATUS )
+        CALL DYN_MAPR( 1, INVAL, TWPTR, STATUS )
+        IF ( VOK ) THEN
+          CALL DYN_MAPR( 1, INVAL, TVPTR, STATUS )
+        END IF
 
-*  which mode of rebinning
-        CALL USI_GET0I('OPT',OPT,STATUS)
-        IF (OPT.EQ.1) THEN
-          CALL USI_GET0I('NBIN',ONVAL,STATUS)
+*    Which mode of rebinning
+        CALL USI_GET0I( 'OPT', OPT, STATUS )
+        IF ( OPT .EQ. 1 ) THEN
+          CALL USI_GET0I( 'NBIN', ONVAL, STATUS )
           MINVAL=SUM/REAL(ONVAL)
-        ELSEIF (OPT.EQ.2) THEN
-          CALL USI_GET0R('MIN',MINVAL,STATUS)
+        ELSE IF ( OPT .EQ. 2 ) THEN
+          CALL USI_GET0R( 'MIN', MINVAL, STATUS )
         ELSE
-          CALL MSG_PRNT('AST_ERR: invalid mode option')
-          STATUS=SAI__ERROR
-        ENDIF
+          STATUS = SAI__ERROR
+          CALL ERR_REP(' ','AST_ERR: invalid mode option',STATUS)
+        END IF
 
-*  rebin
+*    Rebin
         CALL SBIN_DOIT(INVAL,%VAL(IDPTR),%VAL(IAPTR),%VAL(IWPTR),
      :                   VOK,%VAL(IVPTR),QOK,%VAL(IQPTR),MASK,
      :                   MINVAL,ONVAL,%VAL(TDPTR),%VAL(TAPTR),
      :                   %VAL(TWPTR),%VAL(TVPTR),STATUS)
 
-*  copy data from temporary storage to output file
-        CALL BDI_CREDATA(OFID,1,ONVAL,STATUS)
-        CALL BDI_MAPDATA(OFID,'WRITE',ODPTR,STATUS)
-        CALL ARR_COP1R(ONVAL,%VAL(TDPTR),%VAL(ODPTR),STATUS)
-        CALL BDI_UNMAPDATA(OFID,STATUS)
-        IF (VOK) THEN
-          CALL BDI_CREVAR(OFID,1,ONVAL,STATUS)
-          CALL BDI_MAPVAR(OFID,'WRITE',OVPTR,STATUS)
-          CALL ARR_COP1R(ONVAL,%VAL(TVPTR),%VAL(OVPTR),STATUS)
-          CALL BDI_UNMAPVAR(OFID,STATUS)
+*    Create output object
+        CALL BDI_NEW( 'Spectrum', 1, ONVAL, 'REAL', SPID, STATUS )
+        CALL ADI_SETLNK( SPID, OFID, STATUS )
+        OFID = SPID
+
+*    Copy data from temporary storage to output file
+        CALL BDI_PUT1R( OFID, 'Data', ONVAL, %val(TDPTR), STATUS )
+        IF ( VOK ) THEN
+          CALL BDI_PUT1R( OFID, 'Data', ONVAL, %val(TVPTR), STATUS )
         ENDIF
-        CALL BDI_CREAXVAL(OFID,1,.FALSE.,ONVAL,STATUS)
-        CALL BDI_MAPAXVAL(OFID,'WRITE',1,OAPTR,STATUS)
-        CALL ARR_COP1R(ONVAL,%VAL(TAPTR),%VAL(OAPTR),STATUS)
-        CALL BDI_UNMAPAXVAL(OFID,1,STATUS)
-        CALL BDI_CREAXWID(OFID,1,.FALSE.,ONVAL,STATUS)
-        CALL BDI_MAPAXWID(OFID,'WRITE',1,OWPTR,STATUS)
-        CALL ARR_COP1R(ONVAL,%VAL(TWPTR),%VAL(OWPTR),STATUS)
-        CALL BDI_UNMAPAXWID(OFID,1,STATUS)
-        CALL BDI_GETAXLABEL(IFID,1,TEXT,STATUS)
-        CALL BDI_PUTAXLABEL(OFID,1,TEXT,STATUS)
-        CALL BDI_GETAXUNITS(IFID,1,TEXT,STATUS)
-        CALL BDI_PUTAXUNITS(OFID,1,TEXT,STATUS)
+        CALL BDI_AXPUT1R( OFID, 1, 'Data', ONVAL, %val(TAPTR), STATUS )
+        CALL BDI_AXPUT1R( OFID, 1, 'Width', ONVAL, %val(TWPTR), STATUS )
+        CALL BDI_AXCOPY( IFID, 1, 'Label,Units', OFID, 1, STATUS )
 
-*  update history
-        CALL HSI_COPY(IFID,OFID,STATUS)
-        CALL HSI_ADD(OFID,VERSION,STATUS)
+*    Update history
+        CALL HSI_COPY( IFID, OFID, STATUS )
+        CALL HSI_ADD( OFID, VERSION, STATUS )
 
-*  copy ancilliary stuff
-        CALL BDI_COPTEXT(IFID,OFID,STATUS)
-        CALL BDI_COPMORE(IFID,OFID,STATUS)
+*    Copy ancilliary stuff
+        CALL BDI_COPY( IFID, 'Title,Label,Units', OFID, ' ', STATUS )
+        CALL UDI_COPANC( IFID, ' ', OFID, STATUS )
 
-*  release temporary storage
+*    Release temporary storage
         CALL DYN_UNMAP(TDPTR,STATUS)
         CALL DYN_UNMAP(TAPTR,STATUS)
         CALL DYN_UNMAP(TWPTR,STATUS)
@@ -145,8 +240,7 @@
 
       ENDIF
 
-      CALL BDI_RELEASE(IFID,STATUS)
-      CALL BDI_RELEASE(OFID,STATUS)
+*  Tidy up
       CALL USI_ANNUL('INP',STATUS)
       CALL USI_ANNUL('OUT',STATUS)
       CALL AST_CLOSE()
