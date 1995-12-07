b2d3490d928a63f5575da0545ef8c8a5e6e98cb6
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-12-07 09:32:15 +0000

    Initial revision

---
+++ applications/obsolete/asterix/grf/iload1d.f
@@ -0,0 +1,76 @@
+*+  ILOAD1D - load external 1D data into image processing system
+      SUBROUTINE ILOAD1D(STATUS)
+*    Description :
+*    Deficiencies :
+*    Bugs :
+*    Authors :
+*     rjv@star.sr.bham.ac.uk
+*    History :
+*    Type definitions :
+      IMPLICIT NONE
+*    Global constants :
+      INCLUDE 'SAE_PAR'
+      INCLUDE 'DAT_PAR'
+      INCLUDE 'PAR_ERR'
+*    Global variables :
+      INCLUDE 'IMG_CMN'
+*    Status :
+      INTEGER STATUS
+*    Function declarations :
+*    Local constants :
+*    Local variables :
+      CHARACTER*132		IFILE			! Input file name
+      INTEGER IFID
+      LOGICAL GCB
+*    Version :
+      CHARACTER*30 VERSION
+      PARAMETER (VERSION = 'ILOAD1D Version 2.0-0')
+*-
+      CALL USI_INIT()
+
+      CALL MSG_PRNT(VERSION)
+
+      IF (.NOT.I_OPEN) THEN
+        CALL MSG_PRNT('AST_ERR: image processing not active')
+      ELSE
+
+
+*  get input image
+        CALL USI_GET0C( 'INP', IFILE, STATUS )
+        CALL ADI_FOPEN( IFILE, 'BinDS', 'READ', IFID, STATUS )
+
+*  see if GCB wanted
+        CALL USI_GET0L('GCB',GCB,STATUS)
+
+        IF (STATUS.EQ.SAI__OK) THEN
+
+          CALL MSG_PRNT(' ')
+          CALL MSG_PRNT('Checking data...')
+          CALL IMG_CHECK1D(IFID,STATUS)
+
+          IF (STATUS.EQ.SAI__OK) THEN
+            CALL MSG_PRNT('Loading data....')
+            CALL IMG_LOAD1D(IFID,STATUS)
+
+
+            CALL GCB_ATTACH('IMAGE',STATUS)
+            CALL IMG_1DGCB(STATUS)
+
+            IF (GCB) THEN
+              CALL GCB_FLOAD(IFID,STATUS)
+            ELSE
+              CALL GCB_CLEAR(STATUS)
+            ENDIF
+
+          ENDIF
+
+        ENDIF
+
+        CALL ADI_FCLOSE(IFID,STATUS)
+        CALL USI_ANNUL('INP',STATUS)
+
+      ENDIF
+
+      CALL USI_CLOSE()
+
+      END
