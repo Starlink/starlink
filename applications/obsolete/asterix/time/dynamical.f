c6ecc38fd089835a8c86418bb32229ab827f98da
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-12-11 09:51:56 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/time/dynamical.f
+++ applications/obsolete/asterix/time/dynamical.f
@@ -225,7 +225,7 @@
          ELSE
            CALL BDI_AXCHK( IFID, 1, 'Data', OK, STATUS )
            IF ( OK ) THEN
-             CALL BDI_AXMAPR( IFID, 1, 'Data', APTR, STATUS )
+             CALL BDI_AXMAPR( IFID, 1, 'Data', 'READ', APTR, STATUS )
              CALL ARR_CHKREG( %VAL(APTR), DIMS(1), REG, BASE, DX,
      :                        STATUS )
              IF ( .NOT. REG ) THEN
