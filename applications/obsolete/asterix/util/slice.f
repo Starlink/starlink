cc1abeef1cf353f3bc7d1b372cda37b9aa127f86
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-12-12 10:15:08 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/util/slice.f
+++ applications/obsolete/asterix/util/slice.f
@@ -40,6 +40,7 @@
 *                        use standard system. (DJA)
 *     24 Nov 94 : V1.8-0 Now use USI for user interface (DJA)
 *     21 Apr 95 : V1.8-1 Updated data interface (DJA)
+*     12 Dec 1995 : V2.0-0 ADI port (DJA)
 *
 *   Type Definitions :
 *
@@ -61,12 +62,11 @@
 
       REAL		      	RMIN,RMAX		! Min & max values of slice
       REAL		      	D			! Data value
-      REAL 		      DQMIN,DQMAX	! Data min & max with quality
-      REAL 		      DMIN,DMAX		! Data min & max without quality
+      REAL 		      	DQMIN,DQMAX		! Data min & max with quality
+      REAL 		      	DMIN,DMAX	        ! Data min & max without quality
 
-      INTEGER		      DIMSD(ADI__MXDIM) ! Sizes of data dimensions
-      INTEGER		      DIMSQ(ADI__MXDIM) ! Sizes of quality dimensions
-      INTEGER			DPTR		! Pointer to input data array
+      INTEGER		      	DIMSD(ADI__MXDIM) 	! I/p dimensions
+      INTEGER			DPTR			! I/p data
       INTEGER			I, K			! Loop counters
       INTEGER			IFID			! Input dataset id
       INTEGER		      	INDICES(ADI__MXDIM) 	! Array indices of point
@@ -74,88 +74,77 @@
       INTEGER			NELM			! # data points
       INTEGER			NDIMD		! Dimensionality of data
       INTEGER			NDIMDR		! Dimensionality of data - 1
-      INTEGER			NDIMQ		! Dimensionality of quality
-      INTEGER			NSLICE		! No.of good points in slice
-      INTEGER			NBAD		! No.of bad points in slice
-      INTEGER			QPTR		! Pointer to data quality
+      INTEGER			NSLICE			! # good points in slice
+      INTEGER			NBAD			! # bad points in slice
+      INTEGER			QPTR			! Pointer to data quality
       INTEGER			WIDTH			! Width of o/p stream
 
-      LOGICAL                   OK              ! General validity test
-      LOGICAL			PRIM		! Input object primitive?
-      LOGICAL			Q		! Quality value
+      LOGICAL                   OK              	! General validity test
+      LOGICAL			Q			! Quality value
       LOGICAL			QUAL_OK		! Quality array available?
-      LOGICAL		        SOMEBAD         ! Any bad quality points?
 *
 *    Version id:
 *
       CHARACTER*21		VERSION
-	PARAMETER	        ( VERSION='SLICE Version 1.8-1' )
+	PARAMETER	        ( VERSION='SLICE Version 2.0-0' )
 *-
 
 *    Check status
       IF ( STATUS .NE. SAI__OK ) RETURN
 
-*    Version
+*  Version
       CALL MSG_PRNT( VERSION )
 
-*    Initialise Asterix common blocks
+*  Initialise Asterix common blocks
       CALL AST_INIT()
 
-*    Obtain data object, access and check it
-      CALL USI_TASSOCI( 'INP', '*', 'READ', IFID, STATUS )
+*  Obtain data object, access and check it
+      CALL USI_TASSOCI( 'INP', 'BinDS|Array', 'READ', IFID, STATUS )
       CALL USI_SHOW( 'Input dataset {INP}', STATUS )
 
-*    Is the input object the array required?
-      CALL BDI_PRIM( IFID, PRIM, STATUS )
-      CALL BDI_CHKDATA( IFID, OK, NDIMD, DIMSD, STATUS )
+*  Is the input object the array required?
+      CALL BDI_CHK( IFID, 'Data', OK, STATUS )
+      CALL BDI_GETSHP( IFID, ADI__MXDIM, DIMSD, NDIMD, STATUS )
       IF ( .NOT. OK ) THEN
-	 CALL MSG_PRNT( 'ERROR : No data object found.' )
-         STATUS = SAI__ERROR
-
+	CALL MSG_PRNT( 'ERROR : No data object found.' )
+        STATUS = SAI__ERROR
       END IF
       IF ( STATUS .NE. SAI__OK ) GOTO 99
 
-*    Map in data. and quality - find range
-      CALL BDI_MAPDATA( IFID, 'READ', DPTR, STATUS )
+*  Map in data. and quality - find range
+      CALL BDI_MAPR( IFID, 'Data', 'READ', DPTR, STATUS )
       CALL ARR_SUMDIM( NDIMD, DIMSD, NELM )
-
-*    Check status
       IF ( STATUS .NE. SAI__OK ) GOTO 99
 
-      CALL BDI_CHKQUAL( IFID, QUAL_OK, NDIMQ, DIMSQ, STATUS )
-
+      CALL BDI_CHK( IFID, 'Quality', QUAL_OK, STATUS )
       IF ( QUAL_OK .AND. ( STATUS .EQ. SAI__OK ) ) THEN
-	 CALL BDI_MAPLQUAL( IFID, 'READ', SOMEBAD, QPTR, STATUS )
+        CALL BDI_MAPL( IFID, 'LogicalQuality', 'READ', QPTR, STATUS )
       END IF
-
       IF ( STATUS .NE. SAI__OK ) THEN
-         CALL ERR_FLUSH( STATUS )
-	 QUAL_OK = .FALSE.
+        CALL ERR_FLUSH( STATUS )
+        QUAL_OK = .FALSE.
       END IF
 
       CALL ARR_RANG1R( NELM, %VAL(DPTR), DMIN, DMAX, STATUS )
 
-      IF ( QUAL_OK .AND. SOMEBAD ) THEN
-	 CALL ARR_RANG1RLQ( NELM, %VAL(DPTR), %VAL(QPTR), DQMIN, DQMAX,
+      IF ( QUAL_OK ) THEN
+	CALL ARR_RANG1RLQ( NELM, %VAL(DPTR), %VAL(QPTR), DQMIN, DQMAX,
      :                      STATUS )
       ELSE
-         IF ( .NOT. QUAL_OK ) THEN
-            CALL MSG_PRNT( 'No data quality available.' )
-	 END IF
-         QUAL_OK = .FALSE.
+        IF ( .NOT. QUAL_OK ) THEN
+          CALL MSG_PRNT( 'No data quality available.' )
+	END IF
+        QUAL_OK = .FALSE.
       END IF
 
-*    Try to get data units
-      UNITS = 'data units'
-      IF (.NOT. PRIM ) THEN
-         CALL BDI_GETUNITS( IFID, UNITS, STATUS )
-         IF ( (UNITS .GT. ' ') .OR. (STATUS.NE.SAI__OK)) THEN
-            STATUS = SAI__OK
-            UNITS = 'data units'
-         END IF
+*  Try to get data units
+      CALL BDI_GET0C( IFID, 'Units', UNITS, STATUS )
+      IF ( (UNITS .GT. ' ') .OR. (STATUS.NE.SAI__OK)) THEN
+        IF ( STATUS .NE. SAI__OK ) CALL ERR_ANNUL( STATUS )
+        UNITS = 'data units'
       END IF
 
-*    Inform user about length of data set and data range
+*  Inform user about length of data set and data range
       CALL MSG_SETI( 'NELM', NELM )
       CALL MSG_PRNT ( '^NELM data points entered' )
       CALL MSG_SETR( 'MIN', DMIN )
@@ -176,10 +165,9 @@
         CALL USI_DEF0R( 'MAX', DMAX, STATUS )
       END IF
 
-*    Get range of data
+*  Get range of data
       CALL USI_GET0R( 'MIN', RMIN, STATUS )
       CALL USI_GET0R( 'MAX', RMAX, STATUS )
-
       IF ( STATUS .NE. SAI__OK ) GOTO 99
 
       IF ( RMIN .LT. RMAX ) THEN
@@ -205,21 +193,21 @@
          END IF
 
       ELSE
-         STATUS = SAI__ERROR
-         CALL ERR_REP( ' ', 'Lower bound must be less than upper'
-     :                                      //' bound.', STATUS )
-         GOTO 99
+        STATUS = SAI__ERROR
+        CALL ERR_REP( ' ', 'Lower bound must be less than upper'
+     :                                     //' bound.', STATUS )
+        GOTO 99
 
       END IF
 
-*    Open the output device
+*  Open the output device
       CALL AIO_ASSOCO( 'DEV', 'LIST', OCI, WIDTH, STATUS )
       IF ( STATUS .NE. SAI__OK ) GOTO 99
 
-*    Output header
+*  Output header
       CALL AIO_TITLE( OCI, VERSION, STATUS )
 
-*    Report file name
+*  Report file name
       CALL ADI_FTRACE( IFID, I, PATH, FILE, STATUS )
       IF ( STATUS .NE. SAI__OK ) THEN
         CALL ERR_ANNUL( STATUS )
@@ -246,29 +234,29 @@
       NDIMDR = NDIMD - 1
       DO I = 1, NELM
 
-*      Get data value
+*    Get data value
         CALL ARR_ELEM1R( DPTR, NELM, I, D, STATUS )
 
-*      In user supplied range?
+*    In user supplied range?
 	IF ( (D .GE. RMIN) .AND. (D.LE.RMAX))THEN
 
-*        Increment counter
+*      Increment counter
 	  NSLICE = NSLICE + 1
 
-*        Is this a good quality point?
+*      Is this a good quality point?
 	  IF ( QUAL_OK ) THEN
             CALL ARR_ELEM1L( QPTR, NELM, I, Q, STATUS )
           ELSE
             Q = .TRUE.
 	  END IF
 
-*        If its a good point...
+*      If its a good point...
           IF ( Q ) THEN
 
-*          Recover array indices
+*        Recover array indices
             CALL UTIL_INDEX( NDIMD, DIMSD, I, INDICES )
 
-*          Write line containing value and indices
+*        Write line containing value and indices
 	    IF ( NDIMD .EQ. 1 ) THEN
 	      WRITE( TBUF, 15 ) D, INDICES(1)
  15           FORMAT( 1PG14.6, 3X, I7 )
@@ -285,23 +273,23 @@
 	END IF
       END DO
 
-*    Number of points
+*  Number of points
       CALL AIO_BLNK( OCI, STATUS )
       CALL AIO_BLNK( OCI, STATUS )
       CALL MSG_SETI( 'NP', NSLICE )
       CALL AIO_WRITE( OCI, 'Total of ^NP points', STATUS )
 
-*    Number of bad points (if any)
+*  Number of bad points (if any)
       IF ( QUAL_OK ) THEN
         CALL MSG_SETI( 'NB', NBAD )
         CALL AIO_WRITE( OCI, '^NB bad quality points omitted from'/
      :                                          /' slice', STATUS )
       END IF
 
-*    Close file and spool
+*  Close file and spool
       CALL AIO_CANCL( 'DEV', STATUS )
 
-*    Tidy up & exit
+*  Tidy up & exit
  99   CALL AST_CLOSE()
       CALL AST_ERR( STATUS )
 
