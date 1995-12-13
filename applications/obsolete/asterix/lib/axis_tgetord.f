73b3380b6e9ab91043bdcebf8f616fff8bc0a12e
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-12-13 13:17:18 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/axis_tgetord.f
+++ applications/obsolete/asterix/lib/axis_tgetord.f
@@ -52,85 +52,45 @@
 *
 *    Local variables :
 *
-      INTEGER                         BEG, END             ! Character ptrs
-      INTEGER                         CSTAT                ! Local CHR status
       INTEGER                         IAX                  ! Loop over axes
       INTEGER                         IDIMS(ADI__MXDIM)    ! Input dimensions
       INTEGER                         NAX                  ! # of axes
       INTEGER                         MIAX                 ! Moved axis index
 
       LOGICAL                         DOMOVES              ! Move axes?
-      LOGICAL                         OK                   ! Validity check
       LOGICAL                         USED(ADI__MXDIM)     ! Dimension used?
 *-
 
       IF ( STATUS .EQ. SAI__OK ) THEN
 
-*      Get input dimensions
-        CALL BDI_CHKDATA( FID, OK, NDIM, IDIMS, STATUS )
+*    Get input dimensions
+        CALL BDI_GETSHP( FID, ADI__MXDIM, IDIMS, NDIM, STATUS )
 
-*      Abort if bad, otherwise...
-        IF ( ( STATUS .EQ. SAI__OK ) .AND. OK ) THEN
+*    Abort if bad, otherwise...
+        IF ( STATUS .EQ. SAI__OK ) THEN
 
-*        Set axes unused
+*      Set axes unused
           DO IAX = 1, NDIM
             USED(IAX) = .FALSE.
           END DO
 
-*        Axes present?
-          CALL BDI_CHKAXES( FID, NAX, STATUS )
-          IF ( ( AXORD .GT. ' ' ) .AND. ( NAX .LT. 1 ) ) THEN
-            CALL MSG_PRNT( '! No axis data in dataset - assuming'/
-     :                                    /' axis order '//AXORD )
-            DOMOVES = .FALSE.
-          ELSE
-            DOMOVES = ( AXORD .GT. ' ' )
-          END IF
-
-*        Locate each axis whose order is specified
+*      Locate each axis whose order is specified
           IF ( DOMOVES ) THEN
             IAX = 1
-            BEG = 1
-            END = 0
-            CSTAT = SAI__OK
-            DO WHILE ( CSTAT .EQ. SAI__OK )
+            DO IAX = 1, LEN(AXORD)
 
-*            Find next axis word
-              CALL CHR_FIWE( AXORD, END, CSTAT )
-
-*            Is this a numeric token?
-              CALL CHR_CTOI( AXORD(BEG:END), MIAX, STATUS )
-              IF ( STATUS .NE. SAI__OK ) THEN
-                CALL ERR_ANNUL( STATUS )
-                CALL AXIS_TFIND( FID, AXORD(BEG:END), NDIM, MIAX,
-     :                                                   STATUS )
-              END IF
-
-*            Check returned value
-              IF ( MIAX .EQ. 0 ) THEN
-                STATUS = SAI__ERROR
-                CALL MSG_SETC( 'AX', AXORD(BEG:END) )
-                CALL ERR_REP( ' ', 'Unable to find axis ^AX', STATUS )
-
-              ELSE IF ( MIAX .LT. 0 ) THEN
-                CALL MSG_PRNT( 'WARNING : Ambiguous axis name '/
-     :                                         /AXORD(BEG:END) )
-              END IF
+*          Try to locate next axis quantity
+              CALL BDI0_FNDAXC( FID, AXORD(IAX:IAX), MIAX, STATUS )
               IF ( STATUS .NE. SAI__OK ) GOTO 99
 
-*            Store axis no
+*          Store axis no
               MAXOR(IAX) = MIAX
               MDIMS(IAX) = IDIMS(MIAX)
               USED(MIAX) = .TRUE.
 
-*            Next axis
-              IF ( CSTAT .EQ. SAI__OK ) IAX = IAX + 1
-              BEG = END + 2
-              END = END + 1
-
             END DO
 
-*          Any dimensions not covered by specification
+*        Any dimensions not covered by specification
             IF ( IAX .LT. NDIM ) THEN
               MIAX = IAX + 1
               DO IAX = 1, NDIM
@@ -150,7 +110,7 @@
 
           END IF
 
-*        Pad output dimensions to 7D
+*      Pad output dimensions to 7D
           IF ( NDIM .LT. ADI__MXDIM ) THEN
             DO IAX = NDIM + 1, ADI__MXDIM
               MAXOR(IAX) = IAX
@@ -158,7 +118,7 @@
             END DO
           END IF
 
-*        Any axis moves required?
+*      Any axis moves required?
           NEED_MOVES = .FALSE.
           IF ( DOMOVES ) THEN
             DO IAX = 1, NDIM
@@ -166,13 +126,9 @@
             END DO
           END IF
 
-        ELSE IF ( STATUS .EQ. SAI__OK ) THEN
-          CALL MSG_PRNT( '! Invalid data' )
-          STATUS = SAI__OK
-
         END IF
 
-*      Report error
+*    Report error
   99    IF ( STATUS .NE. SAI__OK ) THEN
           CALL AST_REXIT( 'AXIS_TGETORD', STATUS )
         END IF
