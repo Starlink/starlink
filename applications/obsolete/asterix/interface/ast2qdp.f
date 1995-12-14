dd87bc7034dc42a9bff844d57a29b7cc4970466a
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-12-14 11:41:24 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/interface/ast2qdp.f
+++ applications/obsolete/asterix/interface/ast2qdp.f
@@ -26,7 +26,7 @@
 *     ast2qdp {parameter_usage}
 
 *  Environment Parameters:
-*     INP = UNIV (read)
+*     INP = CHAR (read)
 *       The input binned dataset
 *     OUT = CHAR (read)
 *       Output text file name
@@ -70,7 +70,7 @@
 *     {task_references}...
 
 *  Keywords:
-*     astqdp, usage:public
+*     ast2qdp, usage:public
 
 *  Copyright:
 *     Copyright (C) University of Birmingham, 1995
@@ -95,6 +95,8 @@
 *        Use BDI for data interface, and AIO for output.
 *      1 Aug 1995 V1.8-3 (DJA):
 *        New prologues.
+*     14 Dec 1995 V2.0-0 (DJA):
+*        ADI port
 *     {enter_changes_here}
 
 *  Bugs:
@@ -115,18 +117,17 @@
 
 *  Local Constants:
       CHARACTER*30		VERSION
-        PARAMETER		( VERSION = 'AST2QDP Version 1.8-3' )
+        PARAMETER		( VERSION = 'AST2QDP Version V2.0-0' )
 
 *  Local Variables:
-      CHARACTER*80                     LABEL              !
-      CHARACTER*80                     TITLE              !
-      CHARACTER*40                     UNITS              !
+      CHARACTER*80              LABEL              	!
+      CHARACTER*80              TITLE              	!
+      CHARACTER*40              UNITS              	!
 
-      REAL                             LO, HI             ! Range bounds
-      REAL                             SIZE               ! Character size
+      REAL                      LO, HI             	! Range bounds
+      REAL                      SIZE               	! Character size
 
       INTEGER                   APTR, AWPTR        	! Axis data/width
-      INTEGER                   DIMS(ADI__MXDIM)   	! Dimensions
       INTEGER                   DPTR               	! Data values
       INTEGER                   EPTR               	! Error values
       INTEGER			IFID			! Input dataset
@@ -142,7 +143,6 @@
       LOGICAL                   ANYBAD             	! Any bad points?
       LOGICAL                   AWOK               	! Axis widths valid?
       LOGICAL                   DOK                	! Data valid?
-      LOGICAL                   GOK                	! GRAFIX there?
       LOGICAL                   LSET, HSET         	! Range bounds set?
       LOGICAL                   OK                 	! General validity?
       LOGICAL                   QOK                	! Quality ok?
@@ -166,24 +166,17 @@
       CALL AST_INIT()
 
 *  Associate input file
-      CALL USI_TASSOCI( 'INP', '*', 'READ', IFID, STATUS )
-      CALL BDI_PRIM( IFID, PRIM, STATUS )
+      CALL USI_ASSOC( 'INP', 'BinDS|Array', 'READ', IFID, STATUS )
+      CALL ADI_DERVD( IFID, 'Array', PRIM, STATUS )
       IF ( STATUS .NE. SAI__OK ) GOTO 99
 
 *  Get output text file
       CALL AIO_ASSOCO( 'OUT', 'LIST', OID, WIDTH, STATUS )
 
 *  Check data
-      CALL BDI_CHKDATA( IFID, DOK, NDIM, DIMS, STATUS )
-      IF ( DOK ) THEN
-        IF ( NDIM .GT. 1 ) THEN
-          STATUS = SAI__ERROR
-          CALL ERR_REP( ' ', 'Data must be 1-dimensional', STATUS )
-          GOTO 99
-        ELSE
-          NELM = DIMS(1)
-        END IF
-      ELSE
+      CALL BDI_CHK( IFID, 'Data', DOK, STATUS )
+      CALL BDI_GETSHP( IFID, 1, NELM, NDIM, STATUS )
+      IF ( .NOT. DOK ) THEN
         STATUS = SAI__ERROR
         CALL ERR_REP( ' ', 'Invalid data', STATUS )
         GOTO 99
@@ -196,13 +189,13 @@
       END IF
 
 *  Quality and variance present?
-      CALL BDI_CHKQUAL( IFID, QOK, NDIM, DIMS, STATUS )
-      CALL BDI_CHKVAR( IFID, VOK, NDIM, DIMS, STATUS )
+      CALL BDI_CHK( IFID, 'Quality', QOK, STATUS )
+      CALL BDI_CHK( IFID, 'Variance', VOK, STATUS )
 
 *  Check axis data and widths
       IF ( .NOT. PRIM ) THEN
-        CALL BDI_CHKAXVAL( IFID, 1, AOK, REG, DIMS, STATUS )
-        CALL BDI_CHKAXWID( IFID, 1, AWOK, REGWID, DIMS, STATUS )
+        CALL BDI_AXCHK( IFID, 1, 'Data', AOK, STATUS )
+        CALL BDI_AXCHK( IFID, 1, 'Width', AWOK, STATUS )
         IF ( .NOT. AOK ) THEN
           CALL MSG_PRNT( 'No axis values present in input, will use'/
      :                   /' pixel number instead' )
@@ -216,26 +209,22 @@
 
 *  Axis data
       IF ( AOK ) THEN
-        CALL BDI_MAPAXVAL( IFID, 'READ', 1, APTR, STATUS )
+        CALL BDI_AXMAPR( IFID, 1, 'Data', 'READ', APTR, STATUS )
       END IF
 
 *  Widths
       IF ( AWOK ) THEN
-        CALL BDI_MAPAXWID( IFID, 'READ', 1, AWPTR, STATUS )
+        CALL BDI_AXMAPR( IFID, 1, 'Width', 'READ', AWPTR, STATUS )
       END IF
 
 *  Map quality
       IF ( QOK ) THEN
-        CALL BDI_MAPLQUAL( IFID, 'READ', ANYBAD, QPTR, STATUS )
-        IF ( .NOT. ANYBAD ) THEN
-          CALL BDI_UNMAPQUAL( IFID, STATUS )
-          QOK = .FALSE.
-        END IF
+        CALL BDI_MAPL( IFID, 'LogicalQuality', 'READ', QPTR, STATUS )
       END IF
 
 *  Copy data array
-      CALL BDI_MAPDATA( IFID, 'READ', DPTR, STATUS )
-      IF ( VOK ) CALL BDI_MAPERR( IFID, 'READ', EPTR, STATUS )
+      CALL BDI_MAPR( IFID, 'Data', 'READ', DPTR, STATUS )
+      IF ( VOK ) CALL BDI_MAPR( IFID, 'Error', 'READ', EPTR, STATUS )
 
 *  Write a command telling QDP how to read the errors
       IF ( AWOK .AND. VOK ) THEN
@@ -247,17 +236,17 @@
       END IF
 
 *  Dataset title
-      CALL BDI_GETTITLE( IFID, TITLE, STATUS )
+      CALL BDI_GET0C( IFID, 'Title', TITLE, STATUS )
       IF ( TITLE .GT. ' ' ) THEN
         CALL MSG_SETC( 'LAB', TITLE )
         CALL AIO_WRITE( OID, 'LABEL T "^LAB"', STATUS )
       END IF
 
 *  X axis label and units
-      CALL BDI_GETAXLABEL( IFID, 1, LABEL, STATUS )
+      CALL BDI_AXGET0C( IFID, 1, 'Label', LABEL, STATUS )
       IF ( LABEL .GT. ' ' ) THEN
         CALL MSG_SETC( 'LAB', LABEL )
-        CALL BDI_GETAXUNITS( IFID, 1, UNITS, STATUS )
+        CALL BDI_AXGET0C( IFID, 1, 'Units', UNITS, STATUS )
         IF ( UNITS .GT. ' ' ) THEN
           CALL MSG_SETC( 'UNIT', UNITS )
           CALL AIO_WRITE( OID, 'LAB X "^LAB (^UNIT)"', STATUS )
@@ -267,10 +256,10 @@
       END IF
 
 *  The data (=Y) axis label and units
-      CALL BDI_GETLABEL( IFID, LABEL, STATUS )
+      CALL BDI_GET0C( IFID, 'Label', LABEL, STATUS )
       IF ( LABEL .GT. ' ' ) THEN
         CALL MSG_SETC( 'LAB', LABEL )
-        CALL BDI_GETUNITS( IFID, UNITS, STATUS )
+        CALL BDI_GET0C( IFID, 'Units', UNITS, STATUS )
         IF ( UNITS .GT. ' ' ) THEN
           CALL MSG_SETC( 'UNIT', UNITS )
           CALL AIO_WRITE( OID, 'LABEL Y "^LAB (^UNIT)"', STATUS )
@@ -279,84 +268,78 @@
         END IF
       END IF
 
-*  Test ASTERIX plotting attributes if present
-      CALL BDI_CHKGRAF( IFID, GOK, STATUS )
-      IF ( GOK ) THEN
+*  Connect to graphics system
+      CALL GCB_LCONNECT( STATUS )
+      CALL GCB_FLOAD( IFID, STATUS )
 
-*    Connect to graphics system
-        CALL GCB_LCONNECT( STATUS )
-        CALL GCB_FLOAD( IFID, STATUS )
-
-*    X range?
-        CALL GCB_GETR( 'XAXIS_LO', LSET, LO, STATUS )
-        CALL GCB_GETR( 'XAXIS_HI', HSET, HI, STATUS )
-        IF ( LSET .AND. HSET ) THEN
-          CALL MSG_SETR( 'LO', LO )
-          CALL MSG_SETR( 'HI', HI )
-          CALL AIO_WRITE( OID, 'RESCALE X ^LO ^HI', STATUS )
-        END IF
-
-*    Log X axis?
-        CALL GCB_GETL( 'XAXIS_LOG', OK, XLOG, STATUS )
-        IF ( OK .AND. XLOG ) THEN
-          CALL AIO_WRITE( OID, 'LOG X', STATUS )
-        END IF
-
-*    Y range?
-        CALL GCB_GETR( 'YAXIS_LO', LSET, LO, STATUS )
-        CALL GCB_GETR( 'YAXIS_HI', HSET, HI, STATUS )
-        IF ( LSET .AND. HSET ) THEN
-          CALL MSG_SETR( 'LO', LO )
-          CALL MSG_SETR( 'HI', HI )
-          CALL AIO_WRITE( OID, 'RESCALE Y ^LO ^HI', STATUS )
-        END IF
-
-*    Log Y axis?
-        CALL GCB_GETL( 'XAXIS_LOG', OK, YLOG, STATUS )
-        IF ( OK .AND. YLOG ) THEN
-          CALL AIO_WRITE( OID, 'LOG Y', STATUS )
-        END IF
-
-*    Markers? Only size and symbol available in PLT command
-        CALL GCB_GETL( 'POINT_FLAG', OK, SET, STATUS )
-        IF ( OK .AND. SET ) THEN
-          CALL GCB_GETI( 'POINT_SYMBOL', SYM_SET, SYM, STATUS )
-          CALL GCB_GETR( 'POINT_SIZE', SIZ_SET, SIZE, STATUS )
-          IF ( .NOT. SYM_SET ) SYM = 2
-          IF ( .NOT. SIZ_SET ) SIZE = 1.0
-          CALL MSG_SETI( 'SYM', SYM )
-          CALL MSG_SETR( 'SIZ', SIZE )
-          CALL AIO_WRITE( OID, 'MARK ^SYM ON SIZE ^SIZ', STATUS )
-        END IF
-
-*    Polyline?
-        CALL GCB_GETL( 'POLY_FLAG', OK, SET, STATUS )
-        IF ( OK .AND. SET ) THEN
-          CALL GCB_GETI( 'POLY_STYLE', STY_SET, STYLE, STATUS )
-          IF ( STY_SET ) THEN
-            CALL MSG_SETI( 'STY', STYLE )
-            CALL AIO_WRITE( OID, 'LSTYLE ^STY ON', STATUS )
-          END IF
-          CALL AIO_WRITE( OID, 'LINE ON', STATUS )
-        END IF
-
-*    Stepline?
-        CALL GCB_GETL( 'STEP_FLAG', OK, SET, STATUS )
-        IF ( OK .AND. SET ) THEN
-          CALL GCB_GETI( 'STEP_STYLE', STY_SET, STYLE, STATUS )
-          IF ( STY_SET ) THEN
-            CALL MSG_SETI( 'STY', STYLE )
-            CALL AIO_WRITE( OID, 'LSTYLE ^STY ON', STATUS )
-          END IF
-          CALL AIO_WRITE( OID, 'LINE STEPPED ON', STATUS )
-        ELSE
-          CALL AIO_WRITE( OID, 'LINE OFF', STATUS )
-        END IF
-
-*    Detach from graphics
-        CALL GCB_DETACH( STATUS )
-
+*  X range?
+      CALL GCB_GETR( 'XAXIS_LO', LSET, LO, STATUS )
+      CALL GCB_GETR( 'XAXIS_HI', HSET, HI, STATUS )
+      IF ( LSET .AND. HSET ) THEN
+        CALL MSG_SETR( 'LO', LO )
+        CALL MSG_SETR( 'HI', HI )
+        CALL AIO_WRITE( OID, 'RESCALE X ^LO ^HI', STATUS )
       END IF
+
+*  Log X axis?
+      CALL GCB_GETL( 'XAXIS_LOG', OK, XLOG, STATUS )
+      IF ( OK .AND. XLOG ) THEN
+        CALL AIO_WRITE( OID, 'LOG X', STATUS )
+      END IF
+
+*  Y range?
+      CALL GCB_GETR( 'YAXIS_LO', LSET, LO, STATUS )
+      CALL GCB_GETR( 'YAXIS_HI', HSET, HI, STATUS )
+      IF ( LSET .AND. HSET ) THEN
+        CALL MSG_SETR( 'LO', LO )
+        CALL MSG_SETR( 'HI', HI )
+        CALL AIO_WRITE( OID, 'RESCALE Y ^LO ^HI', STATUS )
+      END IF
+
+*  Log Y axis?
+      CALL GCB_GETL( 'XAXIS_LOG', OK, YLOG, STATUS )
+      IF ( OK .AND. YLOG ) THEN
+        CALL AIO_WRITE( OID, 'LOG Y', STATUS )
+      END IF
+
+*  Markers? Only size and symbol available in PLT command
+      CALL GCB_GETL( 'POINT_FLAG', OK, SET, STATUS )
+      IF ( OK .AND. SET ) THEN
+        CALL GCB_GETI( 'POINT_SYMBOL', SYM_SET, SYM, STATUS )
+        CALL GCB_GETR( 'POINT_SIZE', SIZ_SET, SIZE, STATUS )
+        IF ( .NOT. SYM_SET ) SYM = 2
+        IF ( .NOT. SIZ_SET ) SIZE = 1.0
+        CALL MSG_SETI( 'SYM', SYM )
+        CALL MSG_SETR( 'SIZ', SIZE )
+        CALL AIO_WRITE( OID, 'MARK ^SYM ON SIZE ^SIZ', STATUS )
+      END IF
+
+*  Polyline?
+      CALL GCB_GETL( 'POLY_FLAG', OK, SET, STATUS )
+      IF ( OK .AND. SET ) THEN
+        CALL GCB_GETI( 'POLY_STYLE', STY_SET, STYLE, STATUS )
+        IF ( STY_SET ) THEN
+          CALL MSG_SETI( 'STY', STYLE )
+          CALL AIO_WRITE( OID, 'LSTYLE ^STY ON', STATUS )
+        END IF
+        CALL AIO_WRITE( OID, 'LINE ON', STATUS )
+      END IF
+
+*  Stepline?
+      CALL GCB_GETL( 'STEP_FLAG', OK, SET, STATUS )
+      IF ( OK .AND. SET ) THEN
+        CALL GCB_GETI( 'STEP_STYLE', STY_SET, STYLE, STATUS )
+        IF ( STY_SET ) THEN
+          CALL MSG_SETI( 'STY', STYLE )
+          CALL AIO_WRITE( OID, 'LSTYLE ^STY ON', STATUS )
+        END IF
+        CALL AIO_WRITE( OID, 'LINE STEPPED ON', STATUS )
+      ELSE
+        CALL AIO_WRITE( OID, 'LINE OFF', STATUS )
+      END IF
+
+*  Detach from graphics
+      CALL GCB_DETACH( STATUS )
 
 *  Write the data
       CALL AST2QDP_WDATA( OID, NELM, %VAL(DPTR), QOK, %VAL(QPTR),
@@ -457,11 +440,8 @@
 *  Implementation Deficiencies:
 *     {routine_deficiencies}...
 
-*  References:
-*     {task_references}...
-
 *  Keywords:
-*     astqdp, usage:private
+*     ast2qdp, usage:private
 
 *  Copyright:
 *     Copyright (C) University of Birmingham, 1995
