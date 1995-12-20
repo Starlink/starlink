3008fe748ed5a95cd7ce07be3cc3e9c1db800f95
Asterix Developer <asterix@star.sr.bham.ac.uk>
1995-12-20 16:40:20 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/bdi1_arymap.f
+++ applications/obsolete/asterix/lib/bdi1_arymap.f
@@ -185,7 +185,7 @@
         END IF
 
 *    Fill mapped array with copies of scalar data
-        IF ( ENELM .GT. NELM ) THEN
+        IF ( (ENELM .GT. NELM) .OR. (NDIM.EQ.0) ) THEN
           CALL BDI1_ARYMAP_REP( SSIZE, DBUF, ENELM, %VAL(PTR), STATUS )
         END IF
 
