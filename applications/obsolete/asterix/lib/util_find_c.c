b2689f27130128f6dea6015c627df4de5beec7f7
Asterix Developer <asterix@star.sr.bham.ac.uk>
1996-01-22 14:36:30 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/util_find_c.c
+++ applications/obsolete/asterix/lib/util_find_c.c
@@ -194,10 +194,6 @@ F77_SUBROUTINE(util_findfile_int)( CHARACTER(deft), CHARACTER(wspec),
         {
         d_name = dent->d_name;
 
-#ifdef sun4_Solaris
-  d_name -= 2;
-#endif
-
         if ( !strcmp(d_name,".") ||		/* Skip these entries */
              !strcmp(d_name,"..") ) continue;
           {
