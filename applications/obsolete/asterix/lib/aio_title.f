de3824ea5a3219a833acb72289e66e08556fb304
Asterix Developer <asterix@star.sr.bham.ac.uk>
1996-02-08 14:15:44 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/aio_title.f
+++ applications/obsolete/asterix/lib/aio_title.f
@@ -43,12 +43,7 @@
 
 *  Local Variables:
       CHARACTER*50              B50			! A line of blanks
-      CHARACTER*9 		DAT			! Date string
-      CHARACTER*24              PT                      ! PSX time string
-      CHARACTER*8 		TIM			! Time string
-
-      INTEGER                   FSTAT                   ! i/o status
-      INTEGER                   NTICKS                  ! System time
+      CHARACTER*18		TSTR			! Time string
 
       INTEGER 			LENGTH			! Length of prog name
       INTEGER 			NX			! Spacing to centre title
@@ -57,20 +52,11 @@
 *  Check inherited global status.
       IF ( STATUS .NE. SAI__OK ) RETURN
 
-*    Get time
-      CALL PSX_TIME( NTICKS, STATUS )
-      CALL PSX_CTIME( NTICKS, PT, STATUS )
-
-*    Format sensibly
- 10   FORMAT( A2, '-', A3, '-', A2 )
-      WRITE( DAT, 10, IOSTAT=FSTAT ) PT(9:10), PT(5:7), PT(23:24)
-      TIM = PT(12:19)
-
-*    Centre title for 80 char page width
+*  Centre title for 80 char page width
       LENGTH = CHR_LEN(NAME)
       NX = (80-LENGTH)/2
       CALL AIO_IWRITE( OCH, NX, NAME(:LENGTH), STATUS )
       CALL CHR_FILL( ' ', B50 )
-      CALL AIO_IWRITE( OCH, 4, DAT//B50//TIM, STATUS )
+      CALL AIO_IWRITE( OCH, 4, TSTR(1:9)//B50//TSTR(11:18), STATUS )
 
       END
