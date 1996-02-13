30b05756cbedcc3cf4d804c07d32a65c8ed37a5e
Asterix Developer <asterix@star.sr.bham.ac.uk>
1996-02-13 08:07:52 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/hsi1_add.f
+++ applications/obsolete/asterix/lib/hsi1_add.f
@@ -76,6 +76,8 @@
 *  History:
 *     14 Feb 1995 (DJA):
 *        Original version.
+*     13 Feb 1996 (DJA):
+*        Don't try to write history to primitives
 *     {enter_changes_here}
 
 *  Bugs:
@@ -116,7 +118,6 @@
       CHARACTER*(DAT__SZLOC)	LOC			! Output HDS object
       CHARACTER*(DAT__SZLOC)	RLOC			! RECORDS structure
 
-      CHARACTER*80		NAME			! Creator name
       CHARACTER*12           	SYSNAME, NODENAME,
      :                       	RELEASE, VERSION, MACHINE
       CHARACTER*18		TSTR			! Time string
@@ -124,7 +125,6 @@
       INTEGER			CURREC			! Current rec number
       INTEGER			ESIZE			! Extend size
       INTEGER			IVERB			! Verbosity
-      INTEGER			NLEN			! Command length
       INTEGER			SIZE			! Number of records
 
       LOGICAL			THERE			! Exists already?
@@ -139,75 +139,79 @@
 *  Extract locator
       CALL ADI1_GETLOC( ARGS(1), LOC, STATUS )
 
-*  Ensure history exists
-      CALL DAT_THERE( LOC, 'HISTORY', THERE, STATUS )
-      IF ( .NOT. THERE ) THEN
-        CALL HSI_NEW( ARGS(1), STATUS )
-      END IF
+*  Can't add history to primitives
+      CALL DAT_PRIM( LOC, PRIM, STATUS )
+      IF ( .NOT. PRIM ) THEN
 
-*  Locate HISTORY component
-      CALL DAT_FIND( LOC, 'HISTORY', HLOC, STATUS )
-
-*  Get verbosity
-      CALL HSI1_GETVRB( HLOC, IVERB, STATUS )
-
-*  Only write text if verbosity is QUIET or greater
-      IF ( IVERB .GE. HSI__QUIET ) THEN
-
-*    Locate RECORDS structure, and find its size
-        CALL DAT_FIND( HLOC, 'RECORDS', RLOC, STATUS )
-        CALL DAT_SIZE( RLOC, SIZE, STATUS )
-        IF ( STATUS .NE. SAI__OK ) GOTO 99
-
-*    Get current record number
-        CALL CMP_GET0I( HLOC, 'CURRENT_RECORD', CURREC, STATUS )
-
-*    Extend file if necessary
-        CURREC = CURREC + 1
-        IF ( CURREC .GT. SIZE ) THEN
-          CALL CMP_GET0I( HLOC, 'EXTEND_SIZE', ESIZE, STATUS )
-          CALL DAT_ALTER( RLOC, 1, SIZE + ESIZE, STATUS )
+*    Ensure history exists
+        CALL DAT_THERE( LOC, 'HISTORY', THERE, STATUS )
+        IF ( .NOT. THERE ) THEN
+          CALL HSI_NEW( ARGS(1), STATUS )
         END IF
-        IF ( STATUS .NE. SAI__OK ) GOTO 99
 
-*    Increment current record component
-        CALL CMP_PUT0I( HLOC, 'CURRENT_RECORD', CURREC, STATUS )
+*    Locate HISTORY component
+        CALL DAT_FIND( LOC, 'HISTORY', HLOC, STATUS )
 
-*    Get locator to current record
-        CALL DAT_CELL( RLOC, 1, CURREC, CRLOC, STATUS )
+*    Get verbosity
+        CALL HSI1_GETVRB( HLOC, IVERB, STATUS )
 
-*    Get system stuff including node name
-        CALL PSX_UNAME( SYSNAME, NODENAME, RELEASE, VERSION, MACHINE,
-     :                                                     STATUS )
-        IF ( STATUS .NE. SAI__OK ) THEN
-          CALL ERR_ANNUL( STATUS )
-          NODENAME = 'unknown'
+*    Only write text if verbosity is QUIET or greater
+        IF ( IVERB .GE. HSI__QUIET ) THEN
+
+*      Locate RECORDS structure, and find its size
+          CALL DAT_FIND( HLOC, 'RECORDS', RLOC, STATUS )
+          CALL DAT_SIZE( RLOC, SIZE, STATUS )
+          IF ( STATUS .NE. SAI__OK ) GOTO 99
+
+*      Get current record number
+          CALL CMP_GET0I( HLOC, 'CURRENT_RECORD', CURREC, STATUS )
+
+*      Extend file if necessary
+          CURREC = CURREC + 1
+          IF ( CURREC .GT. SIZE ) THEN
+            CALL CMP_GET0I( HLOC, 'EXTEND_SIZE', ESIZE, STATUS )
+            CALL DAT_ALTER( RLOC, 1, SIZE + ESIZE, STATUS )
+          END IF
+          IF ( STATUS .NE. SAI__OK ) GOTO 99
+
+*      Increment current record component
+          CALL CMP_PUT0I( HLOC, 'CURRENT_RECORD', CURREC, STATUS )
+
+*      Get locator to current record
+          CALL DAT_CELL( RLOC, 1, CURREC, CRLOC, STATUS )
+
+*      Get system stuff including node name
+          CALL PSX_UNAME( SYSNAME, NODENAME, RELEASE, VERSION,
+     :                                       MACHINE, STATUS )
+          IF ( STATUS .NE. SAI__OK ) THEN
+            CALL ERR_ANNUL( STATUS )
+            NODENAME = 'unknown'
+          END IF
+          CALL DAT_NEW0C( CRLOC, 'HOST', 30, STATUS )
+          CALL CMP_PUT0C( CRLOC, 'HOST', NODENAME, STATUS )
+
+*      Get date and time and write it
+          CALL DAT_NEW0C( CRLOC, 'DATE', 18, STATUS )
+          CALL TCI_CTIME( TSTR, STATUS )
+          CALL CMP_PUT0C( CRLOC, 'DATE', TSTR, STATUS )
+
+*      Write the command name
+          CALL ADI1_CCA2HC( ARGS(2), ' ', CRLOC, COMMAND, STATUS )
+
+*      Release current record
+          CALL DAT_ANNUL( CRLOC, STATUS )
+
+*      Release RECORDS
+          CALL DAT_ANNUL( RLOC, STATUS )
+
+*    End of verbosity test
         END IF
-        CALL DAT_NEW0C( CRLOC, 'HOST', 30, STATUS )
-        CALL CMP_PUT0C( CRLOC, 'HOST', NODENAME, STATUS )
 
-*    Get date and time and write it
-        CALL DAT_NEW0C( CRLOC, 'DATE', 18, STATUS )
-        CALL TCI_CTIME( TSTR, STATUS )
-        CALL CMP_PUT0C( CRLOC, 'DATE', TSTR, STATUS )
+*    Release HISTORY component
+        CALL DAT_ANNUL( HLOC, STATUS )
 
-*    Write the command name
-        CALL ADI_GET0C( ARGS(2), NAME, STATUS )
-        NLEN = MAX( 1, CHR_LEN(NAME) )
-        CALL DAT_NEW0C( CRLOC, 'COMMAND', NLEN, STATUS )
-        CALL CMP_PUT0C( CRLOC, 'COMMAND', NAME(:NLEN), STATUS )
-
-*    Release current record
-        CALL DAT_ANNUL( CRLOC, STATUS )
-
-*    Release RECORDS
-        CALL DAT_ANNUL( RLOC, STATUS )
-
-*  End of verbosity test
+*  End if primitive test
       END IF
-
-*  Release HISTORY component
-      CALL DAT_ANNUL( HLOC, STATUS )
 
 *  Report any errors
  99   IF ( STATUS .NE. SAI__OK ) THEN
