f8d7654320bc5565dbb68a2da8dfad1c5df6ba62
Malcolm J. Currie <mjc@star.rl.ac.uk>
1996-02-13 15:55:00 +0000

    Access C open from Fortran.
    svn path=/trunk/; revision=440

---
+++ applications/convert/libcon/con_cclos.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "cnf.h"
+#include "f77.h"
+
+FILE *fd;   /*  A pointer to the FILE structure identifying the open file */
+
+
+
+F77_INTEGER_FUNCTION(con_cclos)( void  ){
+/*
+ *  Name:
+ *    con_cclose
+ 
+ *  Purpose:
+ *    Provides access to the C "fclose" function from Fortran.
+
+ *  Invocation:
+ *    ISTAT = CON_CCLOS()
+
+ *  Function Value:
+ *    ISTAT = INTEGER (Returned)
+ *       Status: 0 = failure, 1 = success
+
+
+ */   
+
+      if( fclose( fd ) ){
+         return( 0 );
+
+      } else {
+         return( 1 );
+
+      }
+
+} 
