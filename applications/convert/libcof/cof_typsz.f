2289711343b552425c699ab116e92fd1b8bb052a
Malcolm J. Currie <mjc@star.rl.ac.uk>
1996-04-04 16:44:53 +0000

    Returns the number of bytes used to store an item of any of the HDS primitive data types.
    svn path=/trunk/; revision=505

---
+++ applications/convert/libcof/cof_typsz.f
@@ -0,0 +1,106 @@
+      SUBROUTINE COF_TYPSZ( TYPE, NBYTES, STATUS )         
+*+
+*  Name:
+*     COF_TYPSZ
+
+*  Purpose:
+*     Returns the number of bytes used to store an item of any of the
+*     HDS primitive data types.
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Invocation:
+*     CALL COF_TYPSZ( TYPE, NBYTES, STATUS )         
+
+*  Description:
+*     If the input TYPE is one of the HDS primitive numeric data types,
+*     i.e. one of _REAL, _DOUBLE, _INTEGER, _WORD, _UWORD, _BYTE or 
+*      _UBYTE, then the number of bytes used by that data type is
+*     returned as NBYTES. The values are those stored as the symbolic
+*     constants VAL__NBx in the PRM_PAR include file. (See SUN/39.)
+*     If the TYPE is not one of the above, an error results and
+*     STATUS is set.
+
+*  Arguments:
+*     TYPE = CHARACTER (Given)
+*        The data type.
+*     NBYTES = INTEGER (Returned)
+*        The number of bytes used for this data type.
+*     STATUS = INTEGER (Given and returned)
+*        Global status.
+
+*  Author:
+*     JM: Jo Murray (STARLINK)
+*     MJC: Malcolm J. Currie (STARLINK)
+*     {enter_new_authors_here}
+
+*  History:
+*     1994 May 31 (MJC):
+*        Original version based on JM's CONVERT routine, but using a
+*        standard prologue and style.
+*     {enter_any_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+
+*  Type Definitions:
+      IMPLICIT  NONE               ! No implicit typing
+
+*  Global Constants:
+      INCLUDE  'SAE_PAR'           ! SSE global definitions
+      INCLUDE  'PRM_PAR'           ! PRIMDAT symbolic constants
+
+*  Arguments Given:
+      CHARACTER * ( * ) TYPE       ! Data type
+
+*  Arguments Returned:
+      INTEGER   NBYTES             ! No. bytes required
+
+*  Status:
+      INTEGER   STATUS             ! Global status
+
+*  External References:
+      LOGICAL CHR_SIMLR            ! Compare character strings
+
+*.
+
+*  Check the inherited global status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  Assign the number of bytes appropriate for the HDS data type using
+*  the PRIMDAT constants.
+      IF ( CHR_SIMLR( TYPE, '_REAL' ) ) THEN
+         NBYTES = VAL__NBR
+      
+      ELSE IF ( CHR_SIMLR( TYPE, '_DOUBLE' ) ) THEN
+         NBYTES = VAL__NBD
+
+      ELSE IF ( CHR_SIMLR( TYPE, '_INTEGER' ) ) THEN
+         NBYTES = VAL__NBI
+
+      ELSE IF ( CHR_SIMLR( TYPE, '_WORD' ) ) THEN
+         NBYTES = VAL__NBW
+
+      ELSE IF ( CHR_SIMLR( TYPE, '_UWORD' ) ) THEN
+         NBYTES = VAL__NBUW
+
+      ELSE IF ( CHR_SIMLR( TYPE, '_BYTE' ) ) THEN
+         NBYTES = VAL__NBB
+
+      ELSE IF ( CHR_SIMLR( TYPE, '_UBYTE' ) ) THEN
+         NBYTES = VAL__NBUB
+
+      ELSE
+
+*  If the type is not recognised, report the error.
+         STATUS = SAI__ERROR
+         CALL MSG_SETC( 'TYPE', TYPE )
+         CALL ERR_REP ( 'COF_TYPSZ_INVTYP',
+     :     '^TYPE is not a primitive numeric type.', STATUS )
+      END IF
+
+      END
+
