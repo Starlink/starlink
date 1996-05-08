3a9f2e6a23e9c7a0d3619473c2c958861f4e6b9d
Asterix Developer <asterix@star.sr.bham.ac.uk>
1996-05-08 11:29:28 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/psf_slotinit.f
+++ applications/obsolete/asterix/lib/psf_slotinit.f
@@ -1,5 +1,5 @@
-*+  PSF_SLOTINIT - Initialise slot pointed to by SLOT
-      SUBROUTINE PSF_SLOTINIT( SLOT, STATUS )
+*+  PSF_SLOTINIT - Initialise slot pointed to by PSID
+      SUBROUTINE PSF_SLOTINIT( PSID, STATUS )
 *
 *    Author :
 *
@@ -36,30 +36,33 @@
       INCLUDE 'SAE_PAR'
       INCLUDE 'PSF_PAR'
 *
-*    Global variables :
-*
-      INCLUDE 'PSF_CMN'
-*
 *    Import :
 *
-      INTEGER          		SLOT                   	! Psf slot
+      INTEGER          		PSID                   	! Psf slot
 *
 *    Local variables :
 *
       CHARACTER*15		TAG			! Psf tag name
 
+      REAL			DR, DX, DY		! Bin sizes
       REAL             		MAXR                   	! Max radius of image corner
       REAL             		X_BR, Y_BR	      	! Axis bases
       REAL             		X_DR, Y_DR	      	! Axis bin widths
       REAL             		X_TOR, Y_TOR	      	! Axis conversion factors
       REAL             		XLO, XHI, YLO, YHI     	! Axis extrema
 
+      INTEGER			BPTR			! Bin pointers
       INTEGER          		I                      	! Loop over radial model bins
+      INTEGER			NR, NX, NY		! Number of model bins
+      INTEGER			NTOT			! Total # model bins
       INTEGER			RTNPTR			! Init routine ptr
+      INTEGER			SMTYPE			! Spatial model type
       INTEGER          		X_AX,Y_AX,E_AX,T_AX    	! Axis identifiers
       INTEGER          		X_DIM, Y_DIM           	! Axis dimensions
       INTEGER          		X_PTR, Y_PTR           	! Axis data pointers
 
+      LOGICAL			ISMODEL			! Model psf?
+      LOGICAL			SMREG			! Spatial mode regular?
       LOGICAL          		X_REG, Y_REG	      	! Axes regular
 *-
 
@@ -67,28 +70,35 @@
       IF ( STATUS .NE. SAI__OK ) RETURN
 
 *  Get axis data from dataset
-      CALL PSF_CHKAXES( SLOT, STATUS )
+      CALL PSF_CHKAXES( PSID, STATUS )
 
 *  Identify axes
-      CALL PSF_QAXES( SLOT, X_AX, Y_AX, E_AX, T_AX, STATUS )
+      CALL PSF_QAXES( PSID, X_AX, Y_AX, E_AX, T_AX, STATUS )
 
 *  Update model arguments if needed
-      IF ( P_MODEL(SLOT) ) THEN
+      CALL ADI_CGET0L( PSID, 'IsModel', ISMODEL, STATUS )
+      IF ( ISMODEL ) THEN
 
 *    Extract axis data from internal storage
-        CALL PSF1_GETAXVAL( P_INST(SLOT), X_AX, X_DIM, X_REG, X_PTR,
+        CALL PSF1_GETAXVAL( PSID, X_AX, X_DIM, X_REG, X_PTR,
      :                      X_BR, X_DR, X_TOR, STATUS )
-        CALL PSF1_GETAXVAL( P_INST(SLOT), Y_AX, Y_DIM, Y_REG, Y_PTR,
+        CALL PSF1_GETAXVAL( PSID, Y_AX, Y_DIM, Y_REG, Y_PTR,
      :                      Y_BR, Y_DR, Y_TOR, STATUS )
 
+*    Extract model info
+        CALL ADI_CGET0I( PSID, 'ModelType', SMTYPE, STATUS )
+
 *    Polar model?
-        IF ( SM_TYPE(SLOT) .EQ. PSF_PGRID ) THEN
+        IF ( SMTYPE .EQ. PSF_PGRID ) THEN
 
 *      Regular radial bins?
-          IF ( SM_P_REG(SLOT) ) THEN
+          CALL ADI_CGET0L( PSID, 'ModelReg', SMREG, STATUS )
+          IF ( SMREG ) THEN
 
 *        Convert model args to radians
-            SM_P_DR(SLOT) = ABS(SM_P_DR(SLOT) * X_TOR)
+            CALL ADI_CGET0R( PSID, 'ModelDr', DR, STATUS )
+            DR = ABS( DR * X_TOR )
+            CALL ADI_CPUT0R( PSID, 'ModelDr', DR, STATUS )
 
 *        Find maximum radius of image corners (with a half-pixel margin),
 *        in radians from the field centre
@@ -100,35 +110,47 @@
      :                    XHI*XHI + YLO*YLO, XHI*XHI + YHI*YHI ) )
 
 *        Find number of radial bins
-            SM_P_NR(SLOT) = INT( MAXR/SM_P_DR(SLOT) ) + 1
+            NR = INT( MAXR/DR ) + 1
+            CALL ADI_CPUT0I( PSID, 'ModelNr', NR, STATUS )
 
           ELSE
 
 *        Convert to radian**2
-            DO I = 1, SM_P_NR(SLOT)
-              SM_P_RUP(I,SLOT) = SM_P_RUP(I,SLOT) * (X_TOR**2)
-            END DO
+            CALL ADI_CGET0I( PSID, 'ModelNr', NR, STATUS )
+            CALL ADI_CMAPR( PSID, 'ModelRup', 'UPDATE', BPTR, STATUS )
+            CALL ARR_MULTRR( X_TOR**2, NR, %VAL(BPTR) )
+            CALL ADI_CUNMAP( PSID, 'ModelRup', BPTR, STATUS )
 
           END IF
 
-*      Hence number of polar bins
-          SM_NMOD(SLOT) = SM_P_NR(SLOT) * SM_P_NA(SLOT)
+*      Find total number of bins
+          CALL ADI_CGET0I( PSID, 'ModelNaz', NA, STATUS )
+          NTOT = NR * NA
 
 *    Assume rectangular
         ELSE
 
 *      Convert bin widths to radians
-          SM_R_DX(SLOT) = SM_R_DX(SLOT) * X_TOR
-          SM_R_DY(SLOT) = SM_R_DY(SLOT) * Y_TOR
+          CALL ADI_CGET0R( PSID, 'ModelDx', DX, STATUS )
+          CALL ADI_CGET0R( PSID, 'ModelDy', DY, STATUS )
+          DX = DX * X_TOR
+          DY = DY * Y_TOR
+          CALL ADI_CPUT0R( PSID, 'ModelDx', DX, STATUS )
+          CALL ADI_CPUT0R( PSID, 'ModelDy', DY, STATUS )
 
 *      Numbers of bins in X and Y axes
-          SM_R_NX(SLOT) = INT( X_DIM * X_DR/ SM_R_DX(SLOT) )
-          SM_R_NY(SLOT) = INT( Y_DIM * Y_DR/ SM_R_DY(SLOT) )
+          NX = INT( X_DIM * X_DR/ SM_R_DX(SLOT) )
+          NY = INT( Y_DIM * Y_DR/ SM_R_DY(SLOT) )
+          CALL ADI_CPUT0I( PSID, 'ModelNx', NX, STATUS )
+          CALL ADI_CPUT0I( PSID, 'ModelNy', NY, STATUS )
 
 *      Hence number of rectangular bins
-          SM_NMOD(SLOT)  = MAX(1,SM_R_NX(SLOT)*SM_R_NY(SLOT) )
+          NTOT = MAX(1,NX*NY)
 
         END IF
+
+*    Write total number of bins
+        CALL ADI_CPUT0I( PSID, 'ModelNtot', NTOT, STATUS )
 
       END IF
 
@@ -136,17 +158,15 @@
 *  acts as flag to library routines that energy information will be passed
 *  from above, and so prevents prompts like "mean photon energy" from
 *  appearing
-      IF ( EM_OK(SLOT) .OR. (E_AX .GT. 0) ) THEN
-        CALL PSF_DEF( SLOT, 0.0D0, 0.0D0, 1, 1, 0, 0, STATUS )
+      CALL ADI_CGET0L( PSID, 'IsEnergyModel', ISMODEL, STATUS )
+      IF ( ISMODEL .OR. (E_AX .GT. 0) ) THEN
+        CALL PSF_DEF( PSID, 0.0D0, 0.0D0, 1, 1, 0, 0, STATUS )
       END IF
 
 *  Call the initialisation routine depending on the tag
-      CALL ADI_CGET0C( P_PSID(SLOT), 'Tag', TAG, STATUS )
+      CALL ADI_CGET0C( PSID, 'Tag', TAG, STATUS )
       CALL ADI_CGET0I( P_PLIST, TAG, RTNPTR, STATUS )
-      CALL PSF_INIT_EXEC( %VAL(RTNPTR), P_PSID(SLOT), STATUS )
-
-*  Abort point
- 99   CONTINUE
+      CALL PSF_INIT_EXEC( %VAL(RTNPTR), PSID, STATUS )
 
       END
 
