eff7132abb0f8d086fc83a91bba305b1550f47f7
Phil Daly <pnd@jach.hawaii.edu>
1996-05-29 00:07:02 +0000

    Add Start ADAMnet
    svn path=/trunk/; revision=585

--- applications/obsolete/cgs4dr/tcl/cgs4drLoadTask.tcl
+++ applications/obsolete/cgs4dr/tcl/cgs4drLoadTask.tcl
@@ -9,6 +9,7 @@ proc cgs4drLoadTask {task} {
 # Cred4
    if {$ltask == "cred4"} {
      exec /usr/bin/cp $env(CGS4DR_ROOT)/standard.cred4 $env(CGS4_CONFIG)/standard.cred4
+     exec /usr/bin/cp $env(CGS4DR_ROOT)/standard.cred4 $env(CGS4_CONFIG)/STANDARD.cred4
      if {[file exists $env(CGS4_CONFIG)/default.cred4]==0} {
        exec /usr/bin/cp $env(CGS4DR_ROOT)/default.cred4 $env(CGS4_CONFIG)/default.cred4
      }
@@ -22,14 +23,15 @@ proc cgs4drLoadTask {task} {
 
 #  Need to dynamically edit standard.p4 to use proper PID
      set fid [open $env(CGS4DR_ROOT)/standard.p4 r]
-     if {[file exists $env(CGS4_CONFIG)/standard.p4] == 1} {exec /usr/bin/rm -f $env(CGS4_CONFIG)/standard.p4}
-     set fid2 [open $env(CGS4_CONFIG)/standard.p4 w]
+     if {[file exists $env(P4_CONFIG)/standard.p4] == 1} {exec /usr/bin/rm -f $env(CGS4_CONFIG)/standard.p4}
+     set fid2 [open $env(P4_CONFIG)/standard.p4 w]
      while {[gets $fid line] >=0 } {
        regsub -nocase "xwindows" ${line} "xwindows;$env(PID)xwin" line
        puts $fid2 $line  
      }
      close $fid
      close $fid2
+     exec /usr/bin/cp $env(P4_CONFIG)/standard.p4 $env(P4_CONFIG)/STANDARD.p4
      if {[file exists $env(P4_CONFIG)/default.p4]==0} {
        exec /usr/bin/cp $env(CGS4DR_ROOT)/default.p4 $env(P4_CONFIG)/default.p4
      }
