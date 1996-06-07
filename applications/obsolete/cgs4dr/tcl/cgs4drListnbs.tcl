6880325c0a4537314dd6bb3aa9425f3f5cb60557
Phil Daly <pnd@jach.hawaii.edu>
1996-06-07 22:17:41 +0000

    Tabulate output
    svn path=/trunk/; revision=605

--- applications/obsolete/cgs4dr/tcl/cgs4drListnbs.tcl
+++ applications/obsolete/cgs4dr/tcl/cgs4drListnbs.tcl
@@ -4,7 +4,10 @@ proc cgs4drListnbs {taskname item} {
 #-
     set info [nbs info $item]
     if {[lindex $info 0]} {
-        set message "$item: ([lindex $info 1]*[lindex $info 2]) [lindex $info 3] [nbs get $item]"
+        set item  "[string tolower $item]"
+        set type  "([lindex $info 1]*[lindex $info 2]) [lindex $info 3]"
+        set value "[string trim [nbs get $item]]"
+        set message [format "%40s:\t %s\t %s" $item $type $value]
         cgs4drInform $taskname $message
     } {
         for {set i 1} {$i < [llength $info]} {incr i} {
