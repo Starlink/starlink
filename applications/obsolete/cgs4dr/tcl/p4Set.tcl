10be33472c36f195c1b5eca5f7126fd62c67ddb4
Phil Daly <pnd@jach.hawaii.edu>
1996-06-07 22:13:34 +0000

    Use p4SetNbs.tcl
    svn path=/trunk/; revision=601

--- applications/obsolete/cgs4dr/tcl/p4Set.tcl
+++ applications/obsolete/cgs4dr/tcl/p4Set.tcl
@@ -8,8 +8,7 @@ proc p4Set {taskname} {
     global P4NoticeBoard
     global P4Widgets
     global cgs4drHtml
-    set port $P4Widgets(PORT_NO)
-    set root ${P4NoticeBoard}.port_${port}.
+    set nbsroot ${P4NoticeBoard}.port_$P4Widgets(PORT_NO)
 
 # Create dialog box.
     if {[winfo exists .p4Dialogue]} {destroy .p4Dialogue}
@@ -54,9 +53,9 @@ proc p4Set {taskname} {
     bind $P4Widgets(TITLE) <Button-3> "cgs4drHelpDialog .helpDialog $cgs4drHtml/p4SetBox1.html"
 
     $P4Widgets(DEVICE) delete 0 end
-    $P4Widgets(DEVICE) insert end [string trim [nbs get ${root}device_name]]
+    $P4Widgets(DEVICE) insert end [string trim [nbs get ${nbsroot}.device_name]]
     $P4Widgets(TITLE) delete 0 end
-    $P4Widgets(TITLE) insert end [string trim [nbs get ${root}title]]
+    $P4Widgets(TITLE) insert end [string trim [nbs get ${nbsroot}.title]]
 
 # Middle frame contains axis options
     set top3    [frame $middle.top]
@@ -92,17 +91,17 @@ proc p4Set {taskname} {
     bind $pe <Button-3> "cgs4drHelpDialog .helpDialog $cgs4drHtml/p4SetBox1.html"
 
     $P4Widgets(XOPT) delete 0 end
-    $P4Widgets(XOPT) insert end [string trim [nbs get ${root}device_xopt]]
+    $P4Widgets(XOPT) insert end [string trim [nbs get ${nbsroot}.device_xopt]]
     $P4Widgets(YOPT) delete 0 end
-    $P4Widgets(YOPT) insert end [string trim [nbs get ${root}device_yopt]]
-    if {[nbs get ${root}plot_axes]} {
+    $P4Widgets(YOPT) insert end [string trim [nbs get ${nbsroot}.device_yopt]]
+    if {[nbs get ${nbsroot}.plot_axes]} {
 	set P4Widgets(PLOT_AXES) 1
-    } {
+    } else {
 	set P4Widgets(PLOT_AXES) 0
     }
-    if {[nbs get ${root}pre_erase_plot]} {
+    if {[nbs get ${nbsroot}.pre_erase_plot]} {
 	set P4Widgets(PRE_ERASE_PLOT) 1
-    } {
+    } else {
 	set P4Widgets(PRE_ERASE_PLOT) 0
     }
 
@@ -164,16 +163,16 @@ proc p4Set {taskname} {
     bind $P4Widgets(JEND) <Button-3> "cgs4drHelpDialog .helpDialog $cgs4drHtml/p4SetBox1.html"
 
     $P4Widgets(CHAR_HEIGHT) delete 0 end
-    $P4Widgets(CHAR_HEIGHT) insert end [string trim [nbs get ${root}char_height]]
-    set P4Widgets(CSTYLE) [string trim [nbs get ${root}colour_style]]
+    $P4Widgets(CHAR_HEIGHT) insert end [string trim [nbs get ${nbsroot}.char_height]]
+    set P4Widgets(CSTYLE) [string trim [nbs get ${nbsroot}.colour_style]]
     $P4Widgets(ISTART) delete 0 end
-    $P4Widgets(ISTART) insert 0 [nbs get ${root}istart]
+    $P4Widgets(ISTART) insert 0 [nbs get ${nbsroot}.istart]
     $P4Widgets(IEND) delete 0 end
-    $P4Widgets(IEND) insert 0 [nbs get ${root}iend]
+    $P4Widgets(IEND) insert 0 [nbs get ${nbsroot}.iend]
     $P4Widgets(JSTART) delete 0 end
-    $P4Widgets(JSTART) insert 0 [nbs get ${root}jstart]
+    $P4Widgets(JSTART) insert 0 [nbs get ${nbsroot}.jstart]
     $P4Widgets(JEND) delete 0 end
-    $P4Widgets(JEND) insert 0 [nbs get ${root}jend]
+    $P4Widgets(JEND) insert 0 [nbs get ${nbsroot}.jend]
 
 # Show the dialog box
     set bv [dialogShow .p4Dialogue .p4Dialogue]
@@ -181,99 +180,7 @@ proc p4Set {taskname} {
 # Do something in response to OK button
     if {$bv == 0} {
       cgs4drCursor watch red white
-
-      set device [string trim [$P4Widgets(DEVICE) get]]
-      nbs put ${root}device_name $device
-      set xopt [string trim [$P4Widgets(XOPT) get]]
-      nbs put ${root}device_xopt $xopt
-      set yopt [string trim [$P4Widgets(YOPT) get]]
-      nbs put ${root}device_yopt $yopt
-      set display_plane $P4Widgets(DISPLAY_PLANE)
-      nbs put ${root}display_plane $display_plane
-      set display_type $P4Widgets(DISPLAY_TYPE)
-      nbs put ${root}display_type $display_type
-      nbs put ${root}colour_style $P4Widgets(CSTYLE)
-      set title [string trim [$P4Widgets(TITLE) get]]
-      nbs put ${root}title $title
-
-      set autoscale $P4Widgets(AUTOSCALE)
-      nbs put ${root}autoscale $autoscale
-      if {$autoscale == 0} {
-        set high [$P4Widgets(HIGH) get]
-        nbs put ${root}high $high
-        set low [$P4Widgets(LOW) get]
-        nbs put ${root}low $low
-      }
-
-      set plot_whole $P4Widgets(PLOT_WHOLE)
-      nbs put ${root}plot_whole $plot_whole
-      if {$plot_whole == 0} {
-        set xstart [$P4Widgets(XSTART) get]
-        nbs put ${root}xstart $xstart
-        set xend [$P4Widgets(XEND) get]
-        nbs put ${root}xend $xend
-        set ystart [$P4Widgets(YSTART) get]
-        nbs put ${root}ystart $ystart
-        set yend [$P4Widgets(YEND) get]
-        nbs put ${root}yend $yend
-      }
-
-      nbs put ${root}device_lut $P4Widgets(LUT)
-      nbs put ${root}display_data $P4Widgets(DATA)
-      nbs put ${root}plot_axes $P4Widgets(PLOT_AXES)
-
-      switch [string toupper $display_type] {
-	HISTOGRAM {
-	  set histogram_xstep [$P4Widgets(HISTOGRAM_XSTEP) get]
-	  nbs put ${root}histogram_xstep $histogram_xstep
-	  set histogram_ystep [$P4Widgets(HISTOGRAM_YSTEP) get]
-	  nbs put ${root}histogram_ystep $histogram_ystep
-	  set histogram_bins [$P4Widgets(HISTOGRAM_BINS) get]
-	  nbs put ${root}histogram_bins $histogram_bins
-	  set hist_smooth [$P4Widgets(HIST_SMOOTH) get]
-	  nbs put ${root}hist_smooth $hist_smooth
-	}
-	CONTOUR {
-	  set contour_levels [$P4Widgets(CONTOUR_LEVELS) get]
-	  nbs put ${root}contour_levels $contour_levels
-	  set contour_type $P4Widgets(CONTOUR_TYPE)
-	  nbs put ${root}contour_type $contour_type
-	}
-	OVERGRAPH {
-	  set plot_errors $P4Widgets(PLOT_ERRORS)
-	  nbs put ${root}plot_errors $plot_errors
-	  set cut_direction $P4Widgets(CUT_DIRECTION)
-	  nbs put ${root}cut_direction $cut_direction
-	  set slice_start [$P4Widgets(SLICE_START) get]
-	  nbs put ${root}slice_start $slice_start
-	  set slice_end [$P4Widgets(SLICE_END) get]
-	  nbs put ${root}slice_end $slice_end
-	  set overcolour $P4Widgets(OVERCOLOUR)
-	  nbs put ${root}overcolour $overcolour
-	}
-	GRAPH {
-	  set plot_errors $P4Widgets(PLOT_ERRORS)
-	  nbs put ${root}plot_errors $plot_errors
-	  set cut_direction $P4Widgets(CUT_DIRECTION)
-	  nbs put ${root}cut_direction $cut_direction
-	  set slice_start [$P4Widgets(SLICE_START) get]
-	  nbs put ${root}slice_start $slice_start
-	  set slice_end [$P4Widgets(SLICE_END) get]
-	  nbs put ${root}slice_end $slice_end
-	}
-      }
-
-      nbs put ${root}pre_erase_plot $P4Widgets(PRE_ERASE_PLOT)
-      set cheight [string trim [$P4Widgets(CHAR_HEIGHT) get]]
-      nbs put ${root}char_height $cheight
-      set istart [string trim [$P4Widgets(ISTART) get]]
-      nbs put ${root}istart $istart
-      set iend [string trim [$P4Widgets(IEND) get]]
-      nbs put ${root}iend $iend
-      set jstart [string trim [$P4Widgets(JSTART) get]]
-      nbs put ${root}jstart $jstart
-      set jend [string trim [$P4Widgets(JEND) get]]
-      nbs put ${root}jend $jend
+      p4SetNbs $nbsroot
     }
 
 # Remove the dialog box
