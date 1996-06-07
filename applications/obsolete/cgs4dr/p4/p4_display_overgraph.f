0ff34e0a326f993b50f783940452eb4ce99d1bad
Phil Daly <pnd@jach.hawaii.edu>
1996-06-07 22:10:21 +0000

    Save old SPOS, EPOS values
    svn path=/trunk/; revision=599

--- applications/obsolete/cgs4dr/p4/p4_display_overgraph.f
+++ applications/obsolete/cgs4dr/p4/p4_display_overgraph.f
@@ -20,7 +20,10 @@
       INTEGER PORT                        ! Port to plot in
       INTEGER ACTBYTES, NBS_STATUS
       CHARACTER*( NBS_FLEN ) OLD_TYPE     ! Previous display type
+      CHARACTER*( NBS_FLEN ) OLD_CUT      ! Previous cut direction
+      CHARACTER*( NBS_FLEN ) OLD_COL      ! Previous overcolour
       LOGICAL OLD_ERASE                   ! Old value of pre_erase_plot
+      REAL OLD_SSTART, OLD_SEND           ! Old values of slice start and end
 *-
 
 *    Return if status on entry is bad
@@ -30,9 +33,16 @@
       NBS_STATUS = SAI__OK
       CALL NBS_PUT_VALUE( TASK_BUSY_ID, 0, VAL__NBI, .TRUE., NBS_STATUS )
 
-*    Get the port number, read noticeboard then get other parameters
+*    Get the port number, read noticeboard and save old values
       CALL PAR_GET0I( 'PORT', PORT, STATUS )
       CALL P4_READ_NB( PORT, STATUS )
+      OLD_TYPE = DISPLAY_TYPE( PORT )
+      OLD_SSTART = SLICE_START( PORT )
+      OLD_SEND = SLICE_END( PORT )
+      OLD_CUT = CUT_DIRECTION( PORT )
+      OLD_COL = OVERCOLOUR( PORT )
+
+*    Get new values
       CALL PAR_GET0C( 'DATA', DISPLAY_DATA( PORT ), STATUS )
       CALL CHR_UCASE( DISPLAY_PLANE(PORT) )
       CALL PAR_GET0C( 'CUT', CUT_DIRECTION(PORT), STATUS )
@@ -42,9 +52,7 @@
       CALL PAR_GET0R( 'SPOS', SLICE_START(PORT), STATUS )
       CALL PAR_GET0R( 'EPOS', SLICE_END(PORT), STATUS )
 
-
 *    Reset some values
-      OLD_TYPE = DISPLAY_TYPE( PORT )
       DISPLAY_TYPE( PORT ) = 'OVERGRAPH'
       CALL NBS_GET_VALUE( PRE_ERASE_PLOT_ID(PORT), 0, VAL__NBI, OLD_ERASE, ACTBYTES, STATUS )
       CALL NBS_PUT_VALUE( PRE_ERASE_PLOT_ID(PORT), 0, VAL__NBI, .FALSE., STATUS )
@@ -73,6 +81,10 @@
       JEND(PORT)   = -1
       TITLE(PORT)  = 'A_U_T_O'
       DISPLAY_TYPE( PORT ) = OLD_TYPE
+      SLICE_START( PORT ) = OLD_SSTART
+      SLICE_END( PORT ) = OLD_SEND
+      CUT_DIRECTION( PORT ) = OLD_CUT 
+      OVERCOLOUR( PORT ) = OLD_COL 
       CALL NBS_PUT_VALUE( PRE_ERASE_PLOT_ID(PORT), 0, VAL__NBI, OLD_ERASE, STATUS )
       CALL P4_WRITE_NB( PORT, STATUS )
 
