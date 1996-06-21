ead3080a1653b62fd2f0e2b866e7932d357b016f
Asterix Developer <asterix@star.sr.bham.ac.uk>
1996-06-21 08:06:16 +0000

    Auto-comment by replace

--- applications/obsolete/asterix/lib/psf0_usegrid.f
+++ applications/obsolete/asterix/lib/psf0_usegrid.f
@@ -200,8 +200,9 @@
 
 *    See if the next energy is actually closer
         IF ( EBIN .LT. DIMS(NDIM) ) THEN
-          CALL ARR_ELEM1R( EPTR, DIMS(NDIM), EBIN-1, E1, STATUS )
-          CALL ARR_ELEM1R( EPTR, DIMS(NDIM), EBIN, E2, STATUS )
+          CALL ARR_ELEM1R( EPTR, DIMS(NDIM), EBIN, E1, STATUS )
+          CALL ARR_ELEM1R( EPTR, DIMS(NDIM), EBIN+1, E2, STATUS )
+	print *,energy,e1,e2
           IF ( (E2-ENERGY) .LT. (ENERGY-E1) ) EBIN = EBIN + 1
         END IF
 
