f8ab0864d065891db9a3a54eeea75733eff8a2bb
Tim Jenness <t.jenness@jach.hawaii.edu>
1996-09-04 20:48:09 +0000

    Pass in RES and DIAMETER
    svn path=/trunk/; revision=710

--- applications/surf/sculib/sculib_wtfn_regrid_3.f
+++ applications/surf/sculib/sculib_wtfn_regrid_3.f
@@ -1,6 +1,7 @@
-      SUBROUTINE SCULIB_BESSEL_REGRID_3 (PIXSPACE, NI, NJ, ICEN, 
-     :  JCEN, TOT_WEIGHT_IN, WAVELENGTH, CONV_DATA_SUM, 
-     :  CONV_VARIANCE_SUM, CONV_QUALITY_SUM, CONV_WEIGHT, STATUS)
+      SUBROUTINE SCULIB_WTFN_REGRID_3 (DIAMETER, RES, PIXSPACE, NI,NJ,
+     :     ICEN, JCEN, TOT_WEIGHT_IN, WAVELENGTH, CONV_DATA_SUM, 
+     :     CONV_VARIANCE_SUM, CONV_QUALITY_SUM, CONV_WEIGHT, 
+     :     WEIGHTSIZE, WTFN, STATUS)
 *+
 *  Name:
 *     SCULIB_BESSEL_REGRID_3
@@ -11,13 +12,17 @@
 *     Starlink Fortran 77
 
 *  Invocation:
-*     CALL SCULIB_BESSEL_REGRID_3 (PIXSPACE, NI, NJ, ICEN, JCEN, 
+*     CALL SCULIB_BESSEL_REGRID_3 (DIAMETER, RES,PIXSPACE, NI, NJ, ICEN, JCEN, 
 *    :  TOT_WEIGHT_IN, WAVELENGTH, CONV_DATA_SUM, CONV_VARIANCE_SUM, 
-*    :  CONV_QUALITY_SUM, CONV_WEIGHT, STATUS)
+*    :  CONV_QUALITY_SUM, CONV_WEIGHT, WEIGHTSIZE, WTFN, STATUS)
 
 *  Description:
 
 *  Arguments:
+*     DIAMETER                       = REAL (given)
+*        size of dish
+*     RES                            = INTEGER (Given)
+*        number of resolution elements per scale length
 *     PIXSPACE                       = REAL (Given)
 *        the pixel spacing of the output pixels (radians).
 *     NI                             = INTEGER (Given)
@@ -40,6 +45,10 @@
 *        the quality on the convolution sum for each output pixel.
 *     CONV_WEIGHT (NI,NJ)            = REAL (Given and returned)
 *        the convolution weight for each output pixel.
+*     WEIGHTSIZE                       = INTEGER (Given)
+*        radius of weight function in scale units (SCUIP__FILTRAD for BESSEL, 1 for LINEAR)
+*     WTFN (RES * RES * WEIGHTSIZE * WEIGHTSIZE) = REAL (Given)
+*        convolution weighting function
 *     STATUS = INTEGER (Given and Returned)
 *        The global status.
 *  Authors:
@@ -57,8 +66,11 @@
 
 *  Global Constants:
       INCLUDE 'SAE_PAR'                          ! Standard SAE constants
+      INCLUDE 'PRM_PAR'
 
 *  Arguments Given:
+      REAL DIAMETER
+      REAL RES
       REAL PIXSPACE
       INTEGER NI
       INTEGER NJ
@@ -66,6 +78,8 @@
       INTEGER JCEN
       REAL TOT_WEIGHT_IN (NI, NJ)
       REAL WAVELENGTH
+      INTEGER WEIGHTSIZE
+      REAL WTFN(RES * RES * WEIGHTSIZE * WEIGHTSIZE)
 
 *  Arguments Returned:
       REAL CONV_DATA_SUM (NI, NJ)
@@ -76,27 +90,13 @@
 *  Status:
       INTEGER STATUS                             ! Global status
 
-*  External Functions:
-      REAL SCULIB_BESSJ1                         ! J1(x) Bessel function
 *  Local Constants:
-      REAL    DIAMETER                           ! diameter of JCMT mirror
-      PARAMETER (DIAMETER = 15.0)
-      INTEGER MAX_RES_ELEMENT                    ! radius of convolution
-      PARAMETER (MAX_RES_ELEMENT = 7)            ! function in resolution 
-                                                 ! elements
-      REAL    PI                                 !
-      PARAMETER (PI = 3.14159265359)
-      INTEGER WTRESOL                            ! number of sub resolution 
-      PARAMETER (WTRESOL = 200)                  ! element interpolations of the
-                                                 ! weighting function
 
 *  Local Variables:
-      INTEGER I                                  ! loop counter
       INTEGER ICONV                              ! I index in convolution
       INTEGER ICPIX                              ! index in convolution function
                                                  ! corresponding to RPIX
       INTEGER IOUT                               ! I pixel index in output array
-      INTEGER ISTART                             !
       INTEGER JCONV                              ! J index in convolution
       INTEGER JOUT                               ! J pixel index in output array
       INTEGER PIX_RANGE                          ! radius over which convolution
@@ -117,7 +117,14 @@
       REAL    YPIX                               ! y coord of output pixel
       REAL    WT                                 ! value of weighting function
                                                  ! at output pixel
-      REAL    WTFN (MAX_RES_ELEMENT * WTRESOL)   ! weighting function
+      REAL FILTER1_SQ
+      REAL FILTER_RAD_SQ
+      REAL SCALE
+      REAL SCALESQ
+      REAL RES_SCAL
+      REAL RAD_OV_SCAL
+      REAL SMALL
+      REAL SMALLRT
 *   local data
 *.
 
@@ -125,46 +132,42 @@
 
       IF (STATUS .NE. SAI__OK) RETURN
 
+* Set up small to prevent comparing REAL to 0.0
+
+      SMALL = VAL__SMLR * 10.0
+      SMALLRT = SQRT(SMALL)
+
+
 *  set x and y axis pixel increments, x increases to left hence -ve.
 
       XINC = -PIXSPACE
       YINC = PIXSPACE
 
-*  Weighting function used is the function (I/I0 = 2*J1(x)/x), with
-*  a cosine apodization to zero over the last third of its extent.
-*  It is tabulated up to MAX_RES_ELEMENT resolution elements out at a 
-*  resolution of 1/WTRESOL of a resolution element. The angular size
-*  of a resolution element is equal to WAVELENGTH / 2 * DIAMETER. 
+* Some time saving squares
 
-*  ..basic function
-
-      WTFN(1) = 1.0
-      DO I = 2, MAX_RES_ELEMENT * WTRESOL
-         RTEMP = REAL(I-1) * PI / REAL(WTRESOL)
-         WTFN (I) = 2.0 * SCULIB_BESSJ1(RTEMP, STATUS) / RTEMP
-      END DO
-
-*  ..apodization
-
-      ISTART = NINT (REAL(MAX_RES_ELEMENT * WTRESOL) * 0.66)
-      DO I = ISTART, MAX_RES_ELEMENT * WTRESOL
-         WTFN (I) = WTFN (I) *
-     :     COS (REAL(I-ISTART)*PI/
-     :     (2.0*REAL(MAX_RES_ELEMENT*WTRESOL-ISTART)))
-      END DO
+      FILTER1_SQ = RES * RES
+      FILTER_RAD_SQ = WEIGHTSIZE * WEIGHTSIZE
 
 *  ..extent of convolution function in units of output pixels
     
       RES_ELEMENT = WAVELENGTH * 1.0E-6 / (2.0 * DIAMETER)
-      RTEMP = REAL (MAX_RES_ELEMENT) * RES_ELEMENT / PIXSPACE
+      SCALE = 1.0 / RES_ELEMENT
+
+      SCALESQ = SCALE * SCALE
+      
+      RTEMP = REAL(WEIGHTSIZE) * RES_ELEMENT / PIXSPACE
       PIX_RANGE = INT (RTEMP) + 1
+
+      RAD_OV_SCAL = FILTER_RAD_SQ / SCALESQ
+      RES_SCAL = FILTER1_SQ * SCALESQ
+
 
 *  now do the convolution
 
       DO JOUT = 1, NJ
          DO IOUT = 1, NI
 
-            IF (TOT_WEIGHT_IN (IOUT,JOUT) .NE. 0.0) THEN
+            IF (TOT_WEIGHT_IN (IOUT,JOUT) .GT. SMALL) THEN
 
 *  OK, good map point, loop through other map points in convolution
 *  function range ...
@@ -179,19 +182,17 @@
 *  and if they have zero weight, add them into the convolution function 
 *  weight as if they were measured to be zero
 
-                     IF (TOT_WEIGHT_IN(ICONV,JCONV) .EQ. 0.0) THEN
+                     IF (TOT_WEIGHT_IN(ICONV,JCONV) .LT. SMALL) THEN
                         YCONV = REAL (JCONV-JCEN) * YINC     
                         XCONV = REAL (ICONV-ICEN) * XINC 
-                        RPIX = SQRT ((YPIX-YCONV)**2 + (XPIX-XCONV)**2)
-                        RPIX = RPIX / RES_ELEMENT         
+                        RPIX = (YPIX-YCONV)**2 + (XPIX-XCONV)**2
 
-                        ICPIX = NINT (RPIX * WTRESOL) + 1
-                        IF ((ICPIX.GE.1).AND.
-     :                      (ICPIX.LE.MAX_RES_ELEMENT*WTRESOL)) THEN
-                           WT = WTFN (ICPIX)
-                        ELSE
-                           WT = 0.0
-                        END IF
+                        WT = 0.0
+* Same method as used in REGRID_2
+                        IF (RPIX .LT. RAD_OV_SCAL) THEN
+                           ICPIX = NINT(RES_SCAL * RPIX)
+                           WT = WTFN(ICPIX+1)
+                        ENDIF
 
                         CONV_WEIGHT(IOUT,JOUT) = CONV_WEIGHT(IOUT,JOUT) 
      :                     + WT
@@ -211,12 +212,12 @@
 
       DO JOUT = 1, NJ
          DO IOUT = 1, NI
-            IF (TOT_WEIGHT_IN (IOUT,JOUT) .EQ. 0.0) THEN
+            IF (TOT_WEIGHT_IN (IOUT,JOUT) .LT. SMALL) THEN
                CONV_DATA_SUM (IOUT,JOUT) = 0.0
                CONV_VARIANCE_SUM (IOUT,JOUT) = 0.0
                CONV_QUALITY_SUM (IOUT,JOUT) = 1
             ELSE
-               IF (CONV_WEIGHT(IOUT,JOUT)**2 .NE. 0.0) THEN
+               IF (CONV_WEIGHT(IOUT,JOUT) .GT. SMALLRT) THEN
                   CONV_DATA_SUM (IOUT,JOUT) = 
      :              CONV_DATA_SUM (IOUT,JOUT) /
      :               CONV_WEIGHT (IOUT,JOUT)
