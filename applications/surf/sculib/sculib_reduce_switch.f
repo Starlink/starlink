6ffe4a072e90615ac4954e1af6de4f1a3a451206
Tim Jenness <t.jenness@jach.hawaii.edu>
1996-10-31 18:13:41 +0000

    Add support for QUALITY bytes. Add SCUBAWAVE and RAMPWAVE as alternatives to SQUARE.
    svn path=/trunk/; revision=771

--- applications/surf/sculib/sculib_reduce_switch.f
+++ applications/surf/sculib/sculib_reduce_switch.f
@@ -33,7 +33,7 @@
 *     bolometers are assumed to be in.
 *
 *     SWITCH_PER_EXP = 1
-*        CHOP_FUN = SQUARE
+*        CHOP_FUN = SQUARE (SCUBAWAVE or RAMPWAVE)
 *           left bolometer = BAD (was never looking at source), weight =  0.0
 *         middle bolometer = switch 1, weight = 1.0
 *          right bolometer = - switch 1, weight = 1.0
@@ -67,7 +67,7 @@
 *     and the routine will return with bad status.
 *    Invocation :
 *     CALL SCULIB_REDUCE_SWITCH (CHOP_FUN, SWITCH_PER_EXP,
-*    :  N_DATA, 
+*    :  N_DATA,
 *    :  SWITCH_1_DATA, SWITCH_1_VARIANCE, SWITCH_1_QUALITY,
 *    :  SWITCH_2_DATA, SWITCH_2_VARIANCE, SWITCH_2_QUALITY,
 *    :  SWITCH_3_DATA, SWITCH_3_VARIANCE, SWITCH_3_QUALITY,
@@ -84,16 +84,16 @@
 *           data for switch 1
 *     SWITCH_1_VARIANCE (N_DATA)            = REAL (Given)
 *           variance for switch 1
-*     SWITCH_1_QUALITY (N_DATA)             = INTEGER (Given)
+*     SWITCH_1_QUALITY (N_DATA)             = BYTE (Given)
 *           quality for switch 1
 *     SWITCH_2_DATA (N_DATA)                = REAL (Given)
 *           likewise for switch 2
 *     SWITCH_2_VARIANCE (N_DATA)            = REAL (Given)
-*     SWITCH_2_QUALITY (N_DATA)             = INTEGER (Given)
+*     SWITCH_2_QUALITY (N_DATA)             = BYTE (Given)
 *     SWITCH_3_DATA (N_DATA)                = REAL (Given)
 *           and switch 3
 *     SWITCH_3_VARIANCE (N_DATA)            = REAL (Given)
-*     SWITCH_3_QUALITY (N_DATA)             = INTEGER (Given)
+*     SWITCH_3_QUALITY (N_DATA)             = BYTE (Given)
 *     BEAM                                  = INTEGER (Given)
 *           the projected beam assumed for the bolometers; 1 = LEFT
 *           2 = MIDDLE, 3 = RIGHT
@@ -101,7 +101,7 @@
 *           the exposure result
 *     EXP_VARIANCE (N_DATA)                 = REAL (Returned)
 *           the variance on the result
-*     EXP_QUALITY (N_DATA)                  = INTEGER (Returned)
+*     EXP_QUALITY (N_DATA)                  = BYTE (Returned)
 *           the quality on the result
 *     WEIGHT                                = REAL (Returned)
 *           the relative weight to assign to this projected-beam compared
@@ -113,6 +113,7 @@
 *    Bugs :
 *    Authors :
 *     J.Lightfoot (REVAD::JFL)
+*     TIMJ: T. Jenness (JACH)
 *    History :
 *     $Id$
 *     5-AUG-1993: Original version.
@@ -128,23 +129,24 @@
       INTEGER N_DATA
       REAL SWITCH_1_DATA (N_DATA)
       REAL SWITCH_1_VARIANCE (N_DATA)
-      INTEGER SWITCH_1_QUALITY (N_DATA)
+      BYTE SWITCH_1_QUALITY (N_DATA)
       REAL SWITCH_2_DATA (N_DATA)
       REAL SWITCH_2_VARIANCE (N_DATA)
-      INTEGER SWITCH_2_QUALITY (N_DATA)
+      BYTE SWITCH_2_QUALITY (N_DATA)
       REAL SWITCH_3_DATA (N_DATA)
       REAL SWITCH_3_VARIANCE (N_DATA)
-      INTEGER SWITCH_3_QUALITY (N_DATA)
+      BYTE SWITCH_3_QUALITY (N_DATA)
       INTEGER BEAM
 *    Import-Export :
 *    Export :
       REAL EXP_DATA (N_DATA)
       REAL EXP_VARIANCE (N_DATA)
-      INTEGER EXP_QUALITY (N_DATA)
+      BYTE EXP_QUALITY (N_DATA)
       REAL WEIGHT
 *    Status :
       INTEGER STATUS
 *    External references :
+      BYTE SCULIB_BITOR
 *    Global variables :
 *    Local Constants :
 *    Local variables :
@@ -160,13 +162,14 @@
 
       IF (SWITCH_PER_EXP .EQ. 1) THEN
 
-         IF (CHOP_FUN .EQ. 'SQUARE') THEN
+         IF (CHOP_FUN .EQ. 'SQUARE' .OR. CHOP_FUN.EQ.'RAMPWAVE'
+     :        .OR. CHOP_FUN .EQ. 'SCUBAWAVE') THEN
 
             IF (BEAM .EQ. 1) THEN
 
                DO I = 1, N_DATA
                   WEIGHT = 0.0
-                  EXP_QUALITY (I) = 1
+                  EXP_QUALITY (I) = 1      ! Switch on INVALID QUAL flag
                END DO
 
             ELSE IF (BEAM .EQ. 2) THEN
@@ -174,13 +177,10 @@
                DO I = 1, N_DATA
                   WEIGHT = 1.0
 
-                  IF (SWITCH_1_QUALITY(I) .EQ. 0) THEN
-                     EXP_DATA (I) = SWITCH_1_DATA (I)
-                     EXP_VARIANCE (I) = SWITCH_1_VARIANCE (I) 
-                     EXP_QUALITY (I) = 0
-                  ELSE
-                     EXP_QUALITY (I) = 1
-                  END IF
+                  EXP_DATA (I) = SWITCH_1_DATA (I)
+                  EXP_VARIANCE (I) = SWITCH_1_VARIANCE (I) 
+                  EXP_QUALITY (I) = SWITCH_1_QUALITY(I)
+
                END DO
 
             ELSE IF (BEAM .EQ. 3) THEN
@@ -188,13 +188,10 @@
                DO I = 1, N_DATA
                   WEIGHT = 1.0
 
-                  IF (SWITCH_1_QUALITY(I) .EQ. 0) THEN
-                     EXP_DATA (I) = - SWITCH_1_DATA (I)
-                     EXP_VARIANCE (I) = SWITCH_1_VARIANCE (I) 
-                     EXP_QUALITY (I) = 0
-                  ELSE
-                     EXP_QUALITY (I) = 1
-                  END IF
+                  EXP_DATA (I) = - SWITCH_1_DATA (I)
+                  EXP_VARIANCE (I) = SWITCH_1_VARIANCE (I) 
+                  EXP_QUALITY (I) = SWITCH_1_QUALITY(I)
+
                END DO
 
             ELSE
@@ -213,13 +210,10 @@
                DO I = 1, N_DATA
                   WEIGHT = 0.5
 
-                  IF (SWITCH_1_QUALITY(I) .EQ. 0) THEN
-                     EXP_DATA (I) = -2.0 * SWITCH_1_DATA (I)
-                     EXP_VARIANCE (I) = SWITCH_1_VARIANCE (I) 
-                     EXP_QUALITY (I) = 0
-                  ELSE
-                     EXP_QUALITY (I) = 1
-                  END IF
+                  EXP_DATA (I) = -2.0 * SWITCH_1_DATA (I)
+                  EXP_VARIANCE (I) = SWITCH_1_VARIANCE (I) 
+                  EXP_QUALITY (I) = SWITCH_1_QUALITY(I)
+
                END DO
 
             ELSE IF (BEAM .EQ. 2) THEN
@@ -227,13 +221,10 @@
                DO I = 1, N_DATA
                   WEIGHT = 1.0
 
-                  IF (SWITCH_1_QUALITY(I) .EQ. 0) THEN
-                     EXP_DATA (I) = SWITCH_1_DATA (I)
-                     EXP_VARIANCE (I) = SWITCH_1_VARIANCE (I) 
-                     EXP_QUALITY (I) = 0
-                  ELSE
-                     EXP_QUALITY (I) = 1
-                  END IF
+                  EXP_DATA (I) = SWITCH_1_DATA (I)
+                  EXP_VARIANCE (I) = SWITCH_1_VARIANCE (I) 
+                  EXP_QUALITY (I) = SWITCH_1_QUALITY(I)
+
                END DO
 
             ELSE
@@ -256,40 +247,35 @@
 
       ELSE IF (SWITCH_PER_EXP .EQ. 2) THEN
 
-         IF (CHOP_FUN .EQ. 'SQUARE') THEN
+         IF (CHOP_FUN .EQ. 'SQUARE' .OR. CHOP_FUN.EQ.'RAMPWAVE'
+     :        .OR. CHOP_FUN .EQ. 'SCUBAWAVE') THEN
 
             IF ((BEAM .EQ. 1) .OR. (BEAM .EQ. 3)) THEN
                WEIGHT = 0.5
 
                DO I = 1, N_DATA
-                  IF ((SWITCH_1_QUALITY(I) .EQ. 0) .AND.
-     :                (SWITCH_2_QUALITY(I) .EQ. 0)) THEN
-                     EXP_DATA (I) = SWITCH_2_DATA (I) -
-     :                 SWITCH_1_DATA (I)
-                     EXP_VARIANCE (I) = 
+
+                  EXP_DATA (I) = SWITCH_2_DATA (I) - SWITCH_1_DATA (I)
+                  EXP_VARIANCE (I) = 
      :                 SWITCH_2_VARIANCE (I) +
      :                 SWITCH_1_VARIANCE (I)
-                     EXP_QUALITY (I) = 0
-                  ELSE
-                     EXP_QUALITY (I) = 1
-                  END IF
+                  EXP_QUALITY (I) = SCULIB_BITOR(SWITCH_1_QUALITY(I),
+     :                 SWITCH_2_QUALITY(I))
+
                END DO
 
             ELSE IF (BEAM .EQ. 2) THEN
                WEIGHT = 1.0
 
                DO I = 1, N_DATA
-                  IF ((SWITCH_1_QUALITY(I) .EQ. 0) .AND.
-     :                (SWITCH_2_QUALITY(I) .EQ. 0)) THEN
-                     EXP_DATA (I) = (SWITCH_1_DATA (I) -
+                  EXP_DATA (I) = (SWITCH_1_DATA (I) -
      :                 SWITCH_2_DATA (I)) / 2.0
-                     EXP_VARIANCE (I) = 
+                  EXP_VARIANCE (I) = 
      :                 (SWITCH_2_VARIANCE (I) +
      :                 SWITCH_1_VARIANCE (I)) / 4.0
-                     EXP_QUALITY (I) = 0
-                  ELSE
-                     EXP_QUALITY (I) = 1
-                  END IF
+                  EXP_QUALITY (I) = SCULIB_BITOR(SWITCH_1_QUALITY(I),
+     :                 SWITCH_2_QUALITY(I))
+
                END DO
 
             ELSE
@@ -307,49 +293,40 @@
                WEIGHT = 0.5
 
                DO I = 1, N_DATA
-                  IF ((SWITCH_1_QUALITY(I) .EQ. 0) .AND.
-     :                (SWITCH_2_QUALITY(I) .EQ. 0)) THEN
-                     EXP_DATA (I) = - 2.0 * (SWITCH_1_DATA (I) -
+
+                  EXP_DATA (I) = - 2.0 * (SWITCH_1_DATA (I) -
      :                 SWITCH_2_DATA (I))
-                     EXP_VARIANCE (I) = 4.0 * (SWITCH_2_VARIANCE (I) +
+                  EXP_VARIANCE (I) = 4.0 * (SWITCH_2_VARIANCE (I) +
      :                 SWITCH_1_VARIANCE (I))
-                     EXP_QUALITY (I) = 0
-                  ELSE
-                     EXP_QUALITY (I) = 1
-                  END IF
+                  EXP_QUALITY (I) = SCULIB_BITOR(SWITCH_1_QUALITY(I),
+     :                 SWITCH_2_QUALITY(I))
+
                END DO
 
             ELSE IF (BEAM .EQ. 2) THEN
                WEIGHT = 1.5
 
                DO I = 1, N_DATA
-                  IF ((SWITCH_1_QUALITY(I) .EQ. 0) .AND.
-     :                (SWITCH_2_QUALITY(I) .EQ. 0)) THEN
-                     EXP_DATA (I) = 2.0 * (SWITCH_1_DATA (I) -
+                  EXP_DATA (I) = 2.0 * (SWITCH_1_DATA (I) -
      :                 SWITCH_2_DATA (I)) / 3.0
-                     EXP_VARIANCE (I) = 4.0 *
+                  EXP_VARIANCE (I) = 4.0 *
      :                 (SWITCH_2_VARIANCE (I) +
      :                 SWITCH_1_VARIANCE (I)) / 9.0
-                     EXP_QUALITY (I) = 0
-                  ELSE
-                     EXP_QUALITY (I) = 1
-                  END IF
+                  EXP_QUALITY (I) = SCULIB_BITOR(SWITCH_1_QUALITY(I),
+     :                 SWITCH_2_QUALITY(I))
                END DO
 
             ELSE IF (BEAM .EQ. 3) THEN
                WEIGHT = 1.5
 
                DO I = 1, N_DATA
-                  IF ((SWITCH_1_QUALITY(I) .EQ. 0) .AND.
-     :                (SWITCH_2_QUALITY(I) .EQ. 0)) THEN
-                     EXP_DATA (I) = - 2.0 * (SWITCH_1_DATA (I) -
+                  EXP_DATA (I) = - 2.0 * (SWITCH_1_DATA (I) -
      :                 SWITCH_2_DATA (I)) / 3.0
-                     EXP_VARIANCE (I) = 4.0 * (SWITCH_2_VARIANCE (I) +
+                  EXP_VARIANCE (I) = 4.0 * (SWITCH_2_VARIANCE (I) +
      :                 SWITCH_1_VARIANCE (I)) / 9.0
-                     EXP_QUALITY (I) = 0
-                  ELSE
-                     EXP_QUALITY (I) = 1
-                  END IF
+                  EXP_QUALITY (I) = SCULIB_BITOR(SWITCH_1_QUALITY(I),
+     :                 SWITCH_2_QUALITY(I))
+
                END DO
 
             ELSE
@@ -372,7 +349,9 @@
 
       ELSE IF (SWITCH_PER_EXP .EQ. 3) THEN
 
-         IF (CHOP_FUN .EQ. 'SQUARE') THEN
+         IF (CHOP_FUN .EQ. 'SQUARE' .OR. CHOP_FUN.EQ.'RAMPWAVE'
+     :        .OR. CHOP_FUN .EQ. 'SCUBAWAVE') THEN
+
 
             STATUS = SAI__ERROR
             CALL ERR_REP (' ', 'SCULIB_REDUCE_SWITCH: 3 switches '//
@@ -384,38 +363,36 @@
                WEIGHT = 2.0 / 3.0
 
                DO I = 1, N_DATA
-                  IF ((SWITCH_1_QUALITY(I) .EQ. 0) .AND.
-     :                (SWITCH_2_QUALITY(I) .EQ. 0) .AND.
-     :                (SWITCH_3_QUALITY(I) .EQ. 0)) THEN
-                     EXP_DATA (I) = - (2.0 * SWITCH_1_DATA (I) -
+
+                  EXP_DATA (I) = - (2.0 * SWITCH_1_DATA (I) -
      :                 (SWITCH_2_DATA (I) + SWITCH_3_DATA(I))) / 2.0
-                     EXP_VARIANCE (I) = 
+                  EXP_VARIANCE (I) = 
      :                 (4.0 * SWITCH_1_VARIANCE (I) +
      :                 (SWITCH_2_VARIANCE (I) +
      :                 SWITCH_3_VARIANCE(I))) / 4.0
-                     EXP_QUALITY (I) = 0
-                  ELSE
-                     EXP_QUALITY (I) = 1
-                  END IF
+                  EXP_QUALITY (I) = SCULIB_BITOR(SWITCH_1_QUALITY(I),
+     :                 SWITCH_2_QUALITY(I))
+                  EXP_QUALITY (I) = SCULIB_BITOR(EXP_QUALITY(I),
+     :                 SWITCH_3_QUALITY(I))
+
                END DO
 
             ELSE IF (BEAM .EQ. 2) THEN
                WEIGHT = 1.0
 
                DO I = 1, N_DATA
-                  IF ((SWITCH_1_QUALITY(I) .EQ. 0) .AND.
-     :                (SWITCH_2_QUALITY(I) .EQ. 0) .AND.
-     :                (SWITCH_3_QUALITY(I) .EQ. 0)) THEN
-                     EXP_DATA (I) = - (2.0 * SWITCH_1_DATA (I) -
+
+                  EXP_DATA (I) = - (2.0 * SWITCH_1_DATA (I) -
      :                 (SWITCH_2_DATA (I) + SWITCH_3_DATA(I))) / 3.0
-                     EXP_VARIANCE (I) = 
+                  EXP_VARIANCE (I) = 
      :                 (4.0 * SWITCH_1_VARIANCE (I) +
      :                 (SWITCH_2_VARIANCE (I) +
      :                 SWITCH_3_VARIANCE(I))) / 9.0
-                     EXP_QUALITY (I) = 0
-                  ELSE
-                     EXP_QUALITY (I) = 1
-                  END IF
+                  EXP_QUALITY (I) = SCULIB_BITOR(SWITCH_1_QUALITY(I),
+     :                 SWITCH_2_QUALITY(I))
+                  EXP_QUALITY (I) = SCULIB_BITOR(EXP_QUALITY(I),
+     :                 SWITCH_3_QUALITY(I))
+
                END DO
 
             ELSE
