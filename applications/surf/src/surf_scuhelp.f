4034fb00ef02a6f26336baf9f17d0d5a738f137c
Tim Jenness <t.jenness@jach.hawaii.edu>
1996-11-01 21:12:06 +0000

    Initial revision
    svn path=/trunk/; revision=774

---
+++ applications/surf/src/surf_scuhelp.f
@@ -0,0 +1,236 @@
+      SUBROUTINE SCUHELP( STATUS )
+*+
+*  Name:
+*     SCUHELP
+
+*  Purpose:
+*     Gives help about SCUBA software
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Type of Module:
+*     ADAM A-task
+
+*  Invocation:
+*     CALL KAPHELP( STATUS )
+
+*  Arguments:
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
+
+*  Description:
+*     Displays help about SCUBA.  The help information has classified
+*     and alphabetical lists of commands, general information about
+*     SCUBA and related material; it describes individual commands in
+*     detail.
+*
+*     Here are some of the main options.
+*        scuhelp
+*           No parameter is given so the introduction and the top-level
+*           help index is displayed.
+*        scuhelp application/topic
+*           This gives help about the specified application or topic.
+*        scuhelp application/topic subtopic
+*           This lists help about a subtopic of the specified
+*           application or topic. The hierarchy of topics has a maximum
+*           of four levels.
+*        scuhelp Hints
+*           This gives hints for new and intermediate users.
+*        scuhelp summary
+*           This shows a one-line summary of each application.
+*        scuhelp classified classification
+*           This lists a one-line summary of each application in the
+*           given functionality classification.
+
+*     See the Section "Navigating the Help Library" for details how to
+*     move around the help information, and to select the topics you
+*     want to view.
+
+*  Usage:
+*     scuhelp [topic] [subtopic] [subsubtopic] [subsubsubtopic]
+
+*  ADAM Parameters:
+*     TOPIC = LITERAL (Read)
+*        Topic for which help is to be given. [" "]
+*     SUBTOPIC = LITERAL (Read)
+*        Subtopic for which help is to be given. [" "]
+*     SUBSUBTOPIC = LITERAL (Read)
+*        Subsubtopic for which help is to be given. [" "]
+*     SUBSUBSUBTOPIC = LITERAL (Read)
+*        Subsubsubtopic for which help is to be given. [" "]
+
+*  Algorithm:
+*     -  Check for error on entry; return if not o.k.
+*     -  Obtain topic and subtopics required.  Concatenate them
+*        separated by spaces.
+*     -  If an error has occurred set all topics to be null.
+*     -  Get help on required topic.
+
+*  Navigating the Help Library:
+*     The help information is arranged hierarchically.  You can move
+*     around the help information whenever SCUHELP prompts.  This
+*     occurs when it has either presented a screen's worth of text or
+*     has completed displaying the previously requested help.  The
+*     information displayed by SCUHELP on a particular topic includes a
+*     description of the topic and a list of subtopics that further
+*     describe the topic.
+*
+*     At a prompt you may enter:
+*        o  a topic and/or subtopic name(s) to display the help for that
+*           topic or subtopic, so for example, "block parameters box"
+*           gives help on BOX, which is a subtopic of Parameters, which
+*           in turn is a subtopic of BLOCK;
+*
+*        o  a <CR> to see more text at a "Press RETURN to continue ..."
+*           request;
+*
+*        o  a <CR>} at topic and subtopic prompts to move up one level
+*           in the hierarchy, and if you are at the top level it will
+*           terminate the help session;
+*
+*        o  a CTRL/D (pressing the CTRL and D keys simultaneously) in
+*           response to any prompt will terminate the help session;
+*
+*        o  a question mark "?" to redisplay the text for the current
+*           topic, including the list of topic or subtopic names; or
+* 
+*        o  an ellipsis "..." to display all the text below the
+*           current point in the hierarchy.  For example, "BLOCK..."
+*           displays information on the BLOCK topic as well as
+*           information on all the subtopics under BLOCK.
+*
+*     You can abbreviate any topic or subtopic using the following
+*     rules.
+*
+*        o  Just give the first few characters, e.g. "PARA" for
+*           Parameters.
+* 
+*        o  Some topics are composed of several words separated by
+*           underscores.  Each word of the keyword may be abbreviated,
+*           e.g. "Colour_Set" can be shortened to "C_S".
+* 
+*        o  The characters "%" and "*" act as wildcards, where the
+*           percent sign matches any single character, and asterisk
+*           matches any sequence of characters.  Thus to display
+*           information on all available topics, type an asterisk in
+*           reply to a prompt.
+*
+*        o  If a word contains, but does end with an asterisk wildcard,
+*           it must not be truncated.
+* 
+*        o  The entered string must not contain leading or embedded
+*           spaces.
+*
+*     Ambiguous abbreviations result in all matches being displayed.
+
+*  Implementation Status:
+*     -  Uses the portable help system.
+
+*  Authors:
+*     MJC: Malcolm J. Currie (STARLINK)
+*     TIMJ: Tim Jenness (JACH)
+*     {enter_new_authors_here}
+
+*  History:
+*     1986 November 14 (MJC):
+*        Original.
+*     1988 October 11 (MJC):
+*        Fixed bug to enable subtopics to be accessed from the command
+*        line.
+*     1991 September 25 (MJC):
+*        Corrected the description for the new layout in the new help
+*        library, KAPPA.
+*     1992 June 17 (MJC):
+*        UNIX version using portable HELP.
+*     1992 August 19 (MJC):
+*        Rewrote the description, added usage and implementation status.
+*     1995 November 9 (MJC):
+*        Modified for UNIX and added the Topic on Navigation.
+*     1996 November 1 (TIMJ)
+*        Modified for SCUBA software
+*     {enter_further_changes_here}
+
+*  Bugs:
+*     {note_new_bugs_here}
+
+*-
+
+*  Type Definitions:
+      IMPLICIT NONE
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'        ! SSE global definitions
+      INCLUDE 'PAR_ERR'        ! Parameter-system errors
+
+*  Status:
+      INTEGER STATUS
+
+*  External References:
+      INTEGER
+     :  CHR_LEN                ! Length of character strings ignoring
+                               ! trailing blanks
+
+
+*  Local Constants:
+      CHARACTER*10 LIBNAM      ! Name of the KAPPA help library
+      PARAMETER ( LIBNAM = 'SCUBA_HELP' )
+      INTEGER MAXLEV           ! Maximum number of help levels
+      PARAMETER ( MAXLEV = 4 )
+
+*  Local Variables:
+      CHARACTER*19
+     :  HLPTXT*80,             ! Composite command
+     :  LIBRAY*132,            ! Library name and path
+     :  PATH*122,              ! Library path
+     :  TOPIC( MAXLEV )        ! Name of the topic required
+
+      INTEGER
+     :  I,                     ! Loop counter
+     :  NC                     ! Number of characters in the help string
+                               ! less trailing blanks
+*.
+
+*  Check the inherited status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  Translate the environment variable/logical name.
+      CALL PSX_GETENV( LIBNAM, PATH, STATUS )
+      IF ( STATUS .EQ. SAI__OK ) THEN
+
+*  Form the full library name.
+         NC = CHR_LEN( PATH )
+         LIBRAY = PATH( :NC )//'.shl'
+
+*  Get topic and subtopics.
+         CALL PAR_GET0C( 'TOPIC', TOPIC(1), STATUS )
+         CALL PAR_CANCL( 'TOPIC', STATUS )
+
+         CALL PAR_GET0C( 'SUBTOPIC', TOPIC(2), STATUS )
+         CALL PAR_CANCL( 'SUBTOPIC', STATUS )
+
+         CALL PAR_GET0C( 'SUBSUBTOPIC', TOPIC(3), STATUS )
+         CALL PAR_CANCL( 'SUBSUBTOPIC', STATUS )
+
+         CALL PAR_GET0C( 'SUBSUBSUBTOPIC', TOPIC(4), STATUS )
+         CALL PAR_CANCL( 'SUBSUBSUBTOPIC', STATUS )
+
+*  Concatenate the help topics into a single string
+         HLPTXT = TOPIC( 1 )
+         NC = CHR_LEN( TOPIC( 1 ) ) + 1
+         DO I = 2, MAXLEV
+            CALL CHR_APPND( ' '//TOPIC( I ), HLPTXT, NC )
+         END DO
+
+*  Use a null string when something has gone wrong obtaining the
+*  topics and sub-topics.
+         IF ( STATUS .NE. SAI__OK ) THEN
+            CALL ERR_ANNUL( STATUS )
+            HLPTXT = '         '
+         END IF
+
+*  Get help text.
+         CALL GETHLP( LIBRAY, HLPTXT, STATUS )
+      END IF
+
+      END
