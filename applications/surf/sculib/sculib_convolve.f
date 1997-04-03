c15e52cc2a6a25946197ac046c44c3ab07937e20
Tim Jenness <t.jenness@jach.hawaii.edu>
1997-04-03 21:05:45 +0000

    QUALITY as UBYTES
    svn path=/trunk/; revision=903

--- applications/surf/sculib/sculib_convolve.f
+++ applications/surf/sculib/sculib_convolve.f
@@ -1,7 +1,7 @@
 *+  SCULIB_CONVOLVE - convolve array A with array B to give result in R
       SUBROUTINE SCULIB_CONVOLVE (A_DATA, A_VARIANCE, A_QUALITY,
      :  B, N_A, N_B, N_MIDDLE, NORM, R_DATA, R_VARIANCE, R_QUALITY,
-     :  STATUS)
+     :  BADBIT, STATUS)
 *    Description :
 *     This routine convolves array A with array B using multiplication
 *     only (i.e. no FFTs are used). The routine assumes that the A arrays
@@ -22,7 +22,7 @@
 *           input data array
 *     A_VARIANCE (N_A)              = REAL (Given)
 *           variance on A_DATA
-*     A_QUALITY (N_A)               = INTEGER (Given)
+*     A_QUALITY (N_A)               = BYTE (Given)
 *           quality on A_DATA
 *     B (N_B)                       = REAL (Given)
 *           convolution function
@@ -38,8 +38,10 @@
 *           result of the convolution
 *     R_VARIANCE (N_A)              = REAL (Returned)
 *           variance on R_DATA
-*     R_QUALITY (N_A)               = INTEGER (Returned)
+*     R_QUALITY (N_A)               = QUALITY (Returned)
 *           quality on R_DATA
+*     BADBIT                        = BYTE (Given)
+*           badbit mask
 *     STATUS                        = INTEGER (Given and returned)
 *           global status
 *    Method :
@@ -55,33 +57,39 @@
       IMPLICIT NONE
 *    Global constants :
       INCLUDE 'SAE_PAR'
+      INCLUDE 'PRM_PAR'
 *    Import :
       INTEGER N_A
       INTEGER N_B
       REAL    A_DATA (N_A)
       REAL    A_VARIANCE (N_A)
-      INTEGER A_QUALITY (N_A)
+      BYTE    A_QUALITY (N_A)
       REAL    B (N_B)
       INTEGER N_MIDDLE
       REAL    NORM
+      BYTE    BADBIT
 *    Import-Export :
 *    Export :
       REAL    R_DATA (N_A)
       REAL    R_VARIANCE (N_A)
-      INTEGER R_QUALITY (N_A)
+      BYTE    R_QUALITY (N_A)
 *    Status :
       INTEGER STATUS
 *    External references :
+      BYTE SCULIB_BITAND
 *    Global variables :
 *    Local Constants :
 *    Local variables :
+      LOGICAL DONE
       INTEGER JK                           ! DO loop index
       INTEGER JK1                          ! index in convolution array
       INTEGER KL                           ! DO loop index
+      BYTE    QUAL
       REAL    SUM                          ! convolution data sum
       REAL    SUM_VAR                      ! convolution variance sum
 *    Internal References :
 *    Local data :
+      INCLUDE 'NDF_FUNC'
 *-
 
       IF (STATUS .NE. SAI__OK) RETURN
@@ -92,19 +100,28 @@
          SUM = 0.0
          SUM_VAR = 0.0
          R_QUALITY (JK) = 1
-
+         QUAL = 0
+         DONE = .FALSE.
+         
          DO KL = MIN(1,1+N_MIDDLE-JK), N_B
             JK1 = JK - N_MIDDLE + KL
 
             IF (JK1.LE.N_A .AND. JK1.GE.1) THEN
-               IF (A_QUALITY(JK1) .EQ. 0) THEN
+               IF (NDF_QMASK(A_QUALITY(JK1), BADBIT) .AND.
+     :              A_DATA(JK1) .NE. VAL__BADR) THEN
+
                   SUM = SUM + A_DATA (JK1) * B (KL)
                   SUM_VAR = SUM_VAR + A_VARIANCE (JK1) * B (KL) **2
-                  R_QUALITY (JK) = 0
+                  QUAL = SCULIB_BITAND(QUAL, A_QUALITY(JK1))
+                  DONE = .TRUE.
+
+                  R_QUALITY (JK) = A_QUALITY(JK1)
+
                END IF
             END IF
          END DO
 
+         IF (DONE) R_QUALITY(JK) = QUAL
          R_DATA (JK) = SUM / NORM
          R_VARIANCE (JK) = SUM_VAR / NORM**2
       END DO
