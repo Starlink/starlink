1d50d04d7744051d5d7c8f6a64a7c38afe3ef329
Tim Jenness <t.jenness@jach.hawaii.edu>
1997-05-22 03:33:49 +0000

    Initial revision
    svn path=/trunk/; revision=983

---
+++ applications/surf/sculib/sculib_search_datadir.f
@@ -0,0 +1,115 @@
+      SUBROUTINE SCULIB_SEARCH_DATADIR(PARAM, INDF, STATUS)
+*+
+*  Name:
+*     SCULIB_SEARCH_DATADIR
+
+*  Purpose:
+*     Open an NDF using the parameter system whilst searching DATADIR
+
+*  Invocation:
+*     CALL SCULIB_SEARCH_DATADIR(PARAM, INDF, STATUS)
+
+*  Description:
+*     This routine reads a parameter and attempt to open an NDF.
+*     If it fails it searches the directory DATADIR and then opens
+*     an NDF there. If that fails it asks again.
+
+
+*  Arguments:
+*     PARAM = CHAR (Given)
+*        Name of parameter associated with NDF
+*     INDF = INTEGER (Returned)
+*        NDF identifier of successfully opened file
+*     STATUS = INTEGER (Given and Returned)
+*        Global Status value
+
+*  Implementation Status:
+
+*  Authors:
+*     TIMJ: Tim Jenness (JACH)
+*     {enter_new_authors_here}
+ 
+*  History:
+*     1997 May 21 (TIMJ):
+*       Original version
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+ 
+*-
+
+*  Type Definitions:
+      IMPLICIT NONE
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'               ! SSE global definitions
+      INCLUDE 'MSG_PAR'               ! MSG__ constants
+      INCLUDE 'NDF_PAR'               ! NDF__ constants
+
+*  Arguments Given:
+      CHARACTER * (*) PARAM
+
+*  Arguments Returned:
+      INTEGER INDF
+
+*  Status:
+      INTEGER STATUS                 ! Global status
+
+*  External References:
+      INTEGER GETCWD
+      INTEGER CHDIR
+
+*  Local constants:
+      CHARACTER * 15 TSKNAME           ! Name of task
+      PARAMETER (TSKNAME = 'SEARCH_DATADIR')
+
+
+*  Local Variables:
+      CHARACTER*128 CWD                ! Current directory
+      CHARACTER*128 DATA_DIR           ! Data directory
+      INTEGER ISTAT                    ! Status from CHDIR
+      LOGICAL TRYING                   ! Looping logical
+
+
+*.
+
+      IF (STATUS .NE. SAI__OK) RETURN
+
+      TRYING = .TRUE.
+
+      DO WHILE (TRYING)
+ 
+         CALL NDF_EXIST('IN', 'READ', INDF, STATUS)
+ 
+*  The file could be in DATADIR
+ 
+         IF (INDF.EQ.NDF__NOID) THEN
+            CALL PSX_GETENV('DATADIR', DATA_DIR, STATUS)
+ 
+            ISTAT = GETCWD(CWD)
+            IF (ISTAT .EQ. 0) THEN
+               ISTAT = CHDIR(DATA_DIR)
+               IF (ISTAT .EQ. 0) THEN
+                  CALL NDF_EXIST('IN', 'READ', INDF, STATUS)
+                  ISTAT = CHDIR(CWD)
+                  IF (INDF .NE. NDF__NOID) TRYING = .FALSE.
+               END IF
+            END IF
+         ELSE
+            TRYING = .FALSE.
+         END IF
+ 
+        IF (TRYING) THEN
+            CALL ERR_ANNUL(STATUS)
+            CALL MSG_SETC('TASK', TSKNAME)
+            CALL MSG_OUTIF(MSG__QUIET, ' ','^TASK: Failed to'//
+     :           ' find requested file in CWD and DATADIR',
+     :           STATUS)
+            CALL PAR_CANCL('IN', STATUS)
+         END IF
+         
+      END DO
+
+      END 
+
