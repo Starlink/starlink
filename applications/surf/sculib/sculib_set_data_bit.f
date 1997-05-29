eded56b0beb1e974a745ed8be49e556374762796
Tim Jenness <t.jenness@jach.hawaii.edu>
1997-05-29 00:35:15 +0000

    Initial revision
    svn path=/trunk/; revision=1009

---
+++ applications/surf/sculib/sculib_set_data_bit.f
@@ -0,0 +1,170 @@
+      SUBROUTINE SCULIB_SET_DATA_BIT (USE_THIS, N_BOLS, N_POS, 
+     :     N_BEAM, MASK, BITNUM, STATE, IN_DATA, STATUS)
+*+
+*  Name:
+*     SCULIB_SET_DATA_BIT
+
+*  Purpose:
+*     set a bit in data given a byte mask 
+
+*  Language:
+*     Starlink Fortran 77
+*  
+
+*    Invocation :
+*     CALL SCULIB_SET_DATA_BIT(USE_THIS, N_BOLS, N_POS, N_BEAM, 
+*    :  MASK, VALUE, IN_DATA, STATUS)
+
+
+*    Description :
+*       This routine uses a byte mask (N_BOLS * N_POS) to set a data
+*       bit in the output. A mask value of 1 indicates that a value
+*       should be changed. This routine does not distinguish 'beams'
+*       A bit can be set or unset.
+
+*    Parameters :
+*     USE_THIS   = LOGICAL (Given)
+*           Describes whether the mask should be set to the value (TRUE)
+*           or whether the rest of the data should be set (FALSE)
+*     N_BOLS                  = INTEGER (Given)
+*           number of bolometers measured
+*     N_POS                   = INTEGER (Given)
+*           number of positions measured
+*     N_BEAM                  = INTEGER (Given)
+*           number of beams used
+*     MASK( N_BOLS, N_POS)    = BYTE (Given)
+*     BITNUM                  = INTEGER (Given)
+*           Bit number to affect
+*     STATE                   = LOGICAL (Given)
+*           Set the bits if true. Otherwise clear them
+*     IN_DATA (N_BOLS, N_POS, N_BEAM)
+*                             = BYTE (Given and returned)
+*           the data to be masked
+
+*     STATUS                  = INTEGER (Given and returned)
+*           global status
+*    Method :
+*    Deficiencies :
+*    Bugs :
+*    Authors :
+*       Tim Jenness (JACH)
+*    History :
+*    endhistory
+*    Type Definitions :
+      IMPLICIT NONE
+*    Global constants :
+      INCLUDE 'SAE_PAR'
+      INCLUDE 'PRM_PAR'
+
+*    Import :
+      INTEGER N_BOLS
+      INTEGER N_POS
+      INTEGER N_BEAM
+      BYTE    MASK(N_BOLS, N_POS)
+      LOGICAL USE_THIS
+      INTEGER BITNUM
+      LOGICAL STATE
+
+*    Import-Export :
+      BYTE    IN_DATA (N_BOLS, N_POS, N_BEAM)
+*    Export :
+*    Status :
+      INTEGER STATUS
+*    External references :
+      BYTE SCULIB_BITON
+      BYTE SCULIB_BITOFF
+
+*    Global variables :
+*    Local Constants :
+*    Local variables :
+      INTEGER BEAM                      ! beam index in DO loop
+      INTEGER BOL                       ! bolometer index in DO loop
+      INTEGER POS                       ! measured position index in DO loop
+*    Internal References :
+*    Local data :
+*-
+
+      IF (STATUS .NE. SAI__OK) RETURN
+
+*     Do this if we are changing the section
+      IF (USE_THIS) THEN
+
+         IF (STATE) THEN
+            DO BOL = 1, N_BOLS
+
+               DO POS = 1, N_POS
+
+                  IF (MASK(BOL,POS) .NE. 0) THEN
+
+                     DO BEAM = 1, N_BEAM
+
+                        IN_DATA(BOL, POS, BEAM) = SCULIB_BITON(
+     :                       IN_DATA(BOL, POS, BEAM), BITNUM)
+
+                     END DO
+                  END IF
+               END DO
+            END DO
+
+         ELSE
+
+*       Turn the bits off
+            DO BOL = 1, N_BOLS
+                  
+               DO POS = 1, N_POS
+
+                  IF (MASK(BOL,POS) .NE. 0) THEN
+
+                     DO BEAM = 1, N_BEAM
+
+                        IN_DATA(BOL, POS, BEAM) = SCULIB_BITOFF(
+     :                       IN_DATA(BOL, POS, BEAM), BITNUM)
+
+                     END DO
+                  END IF
+               END DO
+            END DO
+            
+         END IF
+
+*     Set unmasked data to value
+      ELSE
+
+         IF (STATE) THEN
+
+            DO BOL = 1, N_BOLS
+               DO POS = 1, N_POS
+                  
+                  IF (MASK(BOL, POS) .EQ. 0) THEN
+
+                     DO BEAM = 1, N_BEAM
+
+                        IN_DATA(BOL, POS, BEAM) = SCULIB_BITON(
+     :                       IN_DATA(BOL, POS, BEAM), BITNUM)
+                        
+                     END DO
+                  END IF
+               END DO
+            END DO
+
+         ELSE
+
+            DO BOL = 1, N_BOLS
+               DO POS = 1, N_POS
+                  
+                  IF (MASK(BOL, POS) .EQ. 0) THEN
+
+                     DO BEAM = 1, N_BEAM
+
+                        IN_DATA(BOL, POS, BEAM) = SCULIB_BITOFF(
+     :                       IN_DATA(BOL, POS, BEAM), BITNUM)
+
+                     END DO
+                  END IF
+               END DO
+            END DO
+
+         END IF
+      END IF
+
+      END
