830ef6b9599248fe45ac313f3db085fc91c00a61
Tim Jenness <t.jenness@jach.hawaii.edu>
1997-05-29 21:25:34 +0000

    Initial revision
    svn path=/trunk/; revision=1011

---
+++ applications/surf/sculib/sculib_remove_linear_baseline.f
@@ -0,0 +1,365 @@
+      SUBROUTINE SCULIB_REMOVE_LINEAR_BASELINE(N_EXPOSURES,
+     :     N_INTEGRATIONS, N_MEASUREMENTS, DEMOD_POINTER, N_BOL, N_POS,
+     :     IN_DATA, IN_VARIANCE, IN_QUALITY, NSTART, NEND,
+     :     OUT_DATA, OUT_VARIANCE, OUT_QUALITY, BADBIT, STATUS)
+*+
+*  Name:
+*     SCULIB_REMOVE_LINEAR_BASELINE
+
+*  Purpose:
+*     Remove linear baseline from each exposure.
+
+*  Invocation:
+*     CALL SCULIB_REMOVE_LINEAR_BASELINE(N_EXPOSURES,
+*    :     N_INTEGRATIONS, N_MEASUREMENTS, DEM_PNTR, N_BOL, N_POS,
+*    :     IN_DATA, IN_QUALITY, SAMPLE_DX, CHOP_THROW, OUT_DATA,
+*    :     OUT_QUALITY, BADBIT, STATUS)
+
+*  Description:
+*     This routine takes a data array. It then removes a linear baseline
+*     from each scan. The ends of the scan are the size of the chop throw
+*     (should be no source at the ends of a scan)
+
+*  Arguments:
+*     N_EXPOSURES                 = INTEGER (Given)
+*           maximum number of exposures per integration
+*     N_INTEGRATIONS              = INTEGER (Given)
+*           number of integrations in the observation
+*     N_MEASUREMENTS              = INTEGER (Given)
+*           number of measurements in the observation
+*     DEMOD_POINTER (N_EXPOSURES, N_INTEGRATIONS, N_MEASUREMENTS)
+*                                 = INTEGER (Given)
+*           array pointing to start and finish of scans in IN_DATA
+*     N_BOL                       = INTEGER (Given)
+*           the number of bolometers for which data was taken
+*     N_POS                       = INTEGER (Given)
+*           the number of positions measured in the scan
+*     IN_DATA (N_BOL, N_POS)      = REAL (Given)
+*           the measured data
+*     IN_VARIANCE (N_BOL, N_POS)      = REAL (Given)
+*           the measured variance
+*     IN_QUALITY (N_BOL, N_POS)   = BYTE (Given)
+*           the quality on IN_DATA
+*     NSTART                      = INTEGER (Given)
+*           Number of points used for fit at start of scan
+*     NEND                        = INTEGER (Given)
+*           Number of points from end of scan
+*     OUT_DATA (N_BOL, N_POS)     = REAL (Returned)
+*           the data with baseline removed
+*     OUT_VARIANCE (N_BOL, N_POS)      = REAL (Given)
+*           the output variance
+*     OUT_QUALITY (N_BOL, N_POS)  = BYTE (Returned)
+*           the quality on OUT_DATA
+*     BADBIT                      = BYTE (Given)
+*           bad bit mask
+*     STATUS = INTEGER (Given and Returned)
+*        Global Status value
+
+*  Implementation Status:
+
+*  Authors:
+*     TIMJ: Tim Jenness (JACH)
+*     JFL: John Lightfoot (RoE)
+*     {enter_new_authors_here}
+ 
+*  History:
+*     1997 May 22 (TIMJ):
+*       Original version
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+ 
+*-
+
+*  Type Definitions:
+      IMPLICIT NONE
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'               ! SSE global definitions
+      INCLUDE 'PRM_PAR'               ! VAL__ constants
+      INCLUDE 'MSG_PAR'               ! MSG__ constants
+
+*  Arguments Given:
+      BYTE    BADBIT
+      INTEGER N_BOL
+      INTEGER N_EXPOSURES
+      INTEGER N_INTEGRATIONS
+      INTEGER N_MEASUREMENTS
+      INTEGER N_POS
+      INTEGER DEMOD_POINTER(N_EXPOSURES, N_INTEGRATIONS, N_MEASUREMENTS)
+      REAL    IN_DATA(N_BOL, N_POS)
+      BYTE    IN_QUALITY(N_BOL, N_POS)
+      REAL    IN_VARIANCE(N_BOL, N_POS)
+      INTEGER NEND
+      INTEGER NSTART
+
+*  Arguments Returned:
+      REAL    OUT_DATA(N_BOL, N_POS)
+      BYTE    OUT_QUALITY(N_BOL, N_POS)
+      REAL    OUT_VARIANCE(N_BOL, N_POS)
+
+*  Status:
+      INTEGER STATUS                 ! Global status
+
+*  External references:
+
+*  Local Constants:
+      INTEGER MAXDEG                 ! Maximum degree of polynomial
+      PARAMETER (MAXDEG = 1)         ! A straight line!
+      INTEGER NDER                   ! Number of derivatives from fit
+      PARAMETER (NDER = 0)
+      REAL    MIN_VAR                 ! Minimum variance
+      PARAMETER (MIN_VAR = 1.0E-4)
+
+*  Local Variables:
+      INTEGER A_END                  ! End of A_PTR
+      INTEGER A_PTR                  ! Coefficients from fit      
+      INTEGER BOL                    ! Loop counter
+      INTEGER CHUNK                  ! Loop counter
+      INTEGER COUNT                  ! Number of good points for fit
+      INTEGER ENDSC                  ! End of baseline copy
+      DOUBLE PRECISION EPS           ! Fit tolerance
+      INTEGER EXPOSURE               ! Exposure counter
+      INTEGER FITDATA_END            ! End of FITDATA_PTR
+      INTEGER FITDATA_PTR            ! Data values to be fitted
+      INTEGER IERR                   ! Position of VEC_ error
+      INTEGER IFAIL                  ! Error status from fit
+      INTEGER INTEGRATION            ! Integration counter
+      INTEGER MEASUREMENT            ! Measurement counter
+      INTEGER NSCRATCH               ! Size of work arrays
+      INTEGER NCHUNKS                ! Number of baseline sections
+      INTEGER NDEG                   ! Number of degr of freedom used for fit
+      INTEGER NERR                   ! Number of errors in VEC_
+      INTEGER N_SCAN                 ! Number of samples in a scan
+      INTEGER POS                    ! Loop counter
+      INTEGER R_END                  ! End of R_PTR
+      INTEGER R_PTR                  ! R coefficient for fit
+      INTEGER SCAN_END               ! End of scan in array
+      INTEGER SCAN_START             ! Start of scan in array
+      INTEGER START                  ! Start of baseline copy
+      INTEGER SZ_CHUNK               ! Size of first chunk
+      REAL    WEIGHT                 ! Weight variable
+      INTEGER WEIGHT_END             ! End of WEIGHT_PTR
+      INTEGER WEIGHT_PTR             ! Weight of each point
+      DOUBLE PRECISION XPOS          ! X position to calculate polynomial
+      INTEGER X_END                  ! End of X_PTR
+      INTEGER X_PTR                  ! Pixel X positions of each point
+      DOUBLE PRECISION YFIT          ! Value of derivatives
+      DOUBLE PRECISION YP            ! Value of the polynomial at X
+
+
+*     Local References:
+      INCLUDE 'NDF_FUNC'
+
+*.
+
+      IF (STATUS .NE. SAI__OK) RETURN
+
+*     Calculate size of workspace
+*     Assume baseline region is the chop throw at each end
+
+      NSCRATCH = NSTART + NEND
+
+*     Allocate work space for data and coordinates
+      CALL SCULIB_MALLOC(NSCRATCH * VAL__NBD, FITDATA_PTR,FITDATA_END,
+     :     STATUS)
+      CALL SCULIB_MALLOC(NSCRATCH * VAL__NBD, X_PTR, X_END,
+     :     STATUS)
+      CALL SCULIB_MALLOC(NSCRATCH * VAL__NBD, WEIGHT_PTR, WEIGHT_END,
+     :     STATUS)
+      CALL SCULIB_MALLOC(NSCRATCH * VAL__NBD, R_PTR, R_END,
+     :     STATUS)
+      CALL SCULIB_MALLOC((3 * (NSCRATCH + MAXDEG + 1)) * VAL__NBD, 
+     :     A_PTR, A_END, STATUS)
+
+
+*     First need to select a scan
+
+      DO MEASUREMENT = 1, N_MEASUREMENTS
+         DO INTEGRATION = 1, N_INTEGRATIONS
+            DO EXPOSURE = 1, N_EXPOSURES
+               
+               CALL SCULIB_FIND_SWITCH (DEMOD_POINTER, 1,
+     :           N_EXPOSURES, N_INTEGRATIONS, N_MEASUREMENTS, N_POS,
+     :           1, EXPOSURE, INTEGRATION, MEASUREMENT, SCAN_START,
+     :           SCAN_END, STATUS)
+ 
+               IF ((SCAN_START .EQ. VAL__BADI) .OR.
+     :             (SCAN_START .EQ. 0) ) THEN
+                  CALL MSG_SETI ('E', EXPOSURE)
+                  CALL MSG_SETI ('I', INTEGRATION)
+                  CALL MSG_SETI ('M', MEASUREMENT)
+                  CALL MSG_OUTIF (MSG__NORM, ' ', 
+     :                 'REMOVE_LINEAR_BASELINE: no data '//
+     :                 'for exp ^E in int ^I, meas ^M', STATUS)
+               ELSE
+*  OK, there is some data for the scan
+ 
+                  CALL MSG_SETI('NI', INTEGRATION)
+                  CALL MSG_SETI('NE', EXPOSURE)
+                  CALL MSG_OUTIF(MSG__NORM,' ',
+     :                 'REMOVE_LINEAR_BASELINE: Processing exposure'//
+     :                 ' ^NE  of integration ^NI', STATUS)
+ 
+ 
+                  N_SCAN = SCAN_END - SCAN_START + 1
+
+                  DO BOL = 1, N_BOL
+
+*     Copy the data into the work array
+*     If the scan is shorter than NSTART+NEND, then copy all of it
+
+                     IF (STATUS .EQ. SAI__OK) THEN
+
+                        NCHUNKS = 2
+                        SZ_CHUNK = NSTART
+
+                        IF (N_SCAN .LT. NSTART + NEND) THEN
+                           NCHUNKS = 1
+                           SZ_CHUNK = N_SCAN
+                        END IF
+
+*     Copy the baseline pixels from each end into the scratch array
+*     Do this in two chunks (start of scan then end). If the scan is too
+*     short then do it all in one chunk
+
+                        COUNT = 0
+
+                        DO CHUNK = 1, NCHUNKS
+
+                           IF (CHUNK .EQ. 1) THEN
+                              START = SCAN_START
+                              ENDSC = SCAN_START + SZ_CHUNK - 1
+                           ELSE
+                              START = SCAN_END - NEND + 1
+                              ENDSC = SCAN_END
+                           END IF
+                           
+                           DO POS = START, ENDSC
+                              IF ((NDF_QMASK(IN_QUALITY(BOL, POS), 
+     :                             BADBIT)) .AND.
+     :                             IN_DATA(BOL, POS) .NE. VAL__BADR) 
+     :                             THEN
+                                 
+                                 CALL VEC_RTOD(.FALSE., 1, 
+     :                                IN_DATA(BOL,POS), 
+     :                                %VAL(FITDATA_PTR + 
+     :                                COUNT * VAL__NBD),
+     :                                IERR, NERR, STATUS)
+
+*       Check that variance is okay
+                                 IF (IN_VARIANCE(BOL, POS) .LT. 
+     :                                MIN_VAR) THEN
+                                    WEIGHT = 1.0 / MIN_VAR
+                                 ELSE
+                                    WEIGHT = 1.0 / IN_VARIANCE(BOL, POS)
+                                 END IF
+                                    
+
+                                 CALL VEC_RTOD(.FALSE., 1, 
+     :                                WEIGHT,
+     :                                %VAL(WEIGHT_PTR + 
+     :                                COUNT * VAL__NBD),
+     :                                IERR, NERR, STATUS)
+
+
+                                 CALL VEC_ITOD(.FALSE., 1, 
+     :                                POS - SCAN_START + 1, 
+     :                                %VAL(X_PTR + COUNT * VAL__NBD),
+     :                                IERR, NERR, STATUS)
+
+                                 COUNT = COUNT + 1
+
+
+                              END IF
+                           END DO
+                        END DO
+
+                        IF (COUNT .GT. NSCRATCH) THEN
+                           print *,'COUNT is too high!!',COUNT
+                        END IF
+
+*     We now have some data with COUNT pixels in it and an array
+*     containing pixel distance (X pixel values relative to start of scan)
+*     Simply need to fit this with a least squares polynomial fit.
+*     Use first order polynomial.
+
+                        EPS = 0.0D0
+                        IFAIL = 0
+
+                        CALL PDA_DPOLFT (COUNT, %VAL(X_PTR), 
+     :                       %VAL(FITDATA_PTR), %VAL(WEIGHT_PTR), 
+     :                       MAXDEG, NDEG, EPS, 
+     :                       %VAL(R_PTR), IFAIL, %VAL(A_PTR), STATUS)
+
+                        IF (IFAIL .EQ. 1) THEN
+
+*     Now use the coefficients to remove the baseline from the data
+
+                           IF (STATUS .EQ. SAI__OK) THEN
+                              DO POS = SCAN_START, SCAN_END
+
+                                 XPOS = DBLE(POS - SCAN_START + 1)
+                                 
+                                 CALL PDA_DP1VLU(NDEG, NDER, XPOS, YFIT, 
+     :                                YP, %VAL(A_PTR), STATUS)
+
+*     Simply remove this value (write out even if quality is bad)
+                                 IF (IN_DATA(BOL,POS) .NE. VAL__BADR) 
+     :                                THEN
+
+                                    OUT_DATA(BOL,POS) = IN_DATA(BOL,POS)
+     :                                   - REAL(YFIT)
+
+                                 ELSE
+                                    OUT_DATA(BOL,POS) = IN_DATA(BOL,POS)
+                                 END IF
+                              
+                                 OUT_QUALITY(BOL, POS) = 
+     :                                IN_QUALITY(BOL, POS)
+                                 OUT_VARIANCE(BOL, POS) = 
+     :                                IN_VARIANCE(BOL,POS)
+                                 
+                              END DO
+                           END IF
+
+                        ELSE IF (STATUS .NE. SAI__OK) THEN
+
+                           CALL MSG_SETI('IFAIL', IFAIL)
+                           CALL ERR_REP(' ','REMOVE_LINEAR_BASELINE:'//
+     :                          ' Error whilst removing basline'//
+     :                          ' (IFAIL=^IFAIL)', STATUS)
+
+                        ELSE
+
+                           CALL MSG_SETI('IFAIL', IFAIL)
+                           CALL MSG_OUTIF(MSG__QUIET,' ',
+     :                          'REMOVE_LINEAR_BASELINE: Warning '//
+     :                          'IFAIL = ^IFAIL in PDA_DPOLFT',
+     :                          STATUS)
+
+                        END IF
+
+
+
+                     END IF
+                  END DO
+
+
+               END IF
+
+            END DO
+         END DO
+      END DO
+
+*     Tidy up
+
+      CALL SCULIB_FREE('FIT_DATA', FITDATA_PTR, FITDATA_END, STATUS)
+      CALL SCULIB_FREE('X_DATA', X_PTR, X_END, STATUS)
+      CALL SCULIB_FREE('X_DATA', R_PTR, R_END, STATUS)
+      CALL SCULIB_FREE('X_DATA', A_PTR, A_END, STATUS)
+      CALL SCULIB_FREE('WEIGHT', WEIGHT_PTR, WEIGHT_END, STATUS)
+
+      END 
+
