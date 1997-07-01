9378be756d10f662438b91b1e7602eff439dbe9b
Tim Jenness <t.jenness@jach.hawaii.edu>
1997-07-01 03:35:15 +0000

    Process '-' flag of sections to check for inversion
    svn path=/trunk/; revision=1139

--- applications/surf/sculib/sculib_split_file_spec.f
+++ applications/surf/sculib/sculib_split_file_spec.f
@@ -1,5 +1,5 @@
       SUBROUTINE SCULIB_SPLIT_FILE_SPEC( NAME, MAX_SPEC, FILENAME,
-     :     NSPEC, DATA_SPEC, STATUS)
+     :     NSPEC, DATA_SPEC, USE_SECTION, STATUS)
 *+
 *  Name:
 *     SCULIB_SPLIT_FILE_SPEC
@@ -15,6 +15,8 @@
 *     This routine takes a string and returns the filename (ie text
 *     before a {) and the data specifications. Each specification
 *     is indicated by a {}. More than one spec can be returned.
+*     If the string contains a '!' then we will use the invers of the 
+*     specified section.
 
 *  Arguments:
 *     NAME = CHAR (Given)
@@ -27,6 +29,10 @@
 *        Number of specs found/returned (can be 0)
 *     DATA_SPEC(NSPEC) = CHAR (Returned)
 *        The actual specification for the SCUBA section
+*     USE_SECTION = LOGICAL (Returned)
+*        Set to true by default and false if a '-' is found 
+*        at the end of the section (ie after last closing brace) -- 
+*        a '-' indicates that we are using the inverse of the section. 
 *     STATUS = INTEGER (Given and Returned)
 *        Global Status value
 
@@ -61,6 +67,7 @@
       CHARACTER*(*) DATA_SPEC(MAX_SPEC)
       CHARACTER*(*) FILENAME
       INTEGER NSPEC
+      LOGICAL USE_SECTION
 
 *  Status:
       INTEGER STATUS                 ! Global status
@@ -68,16 +75,27 @@
 *  External references:
       INTEGER CHR_LEN
 
+*  Local Constants
+      CHARACTER*(1) CCURLY           ! Character used to close a section
+      PARAMETER (CCURLY = '}')
+      CHARACTER*(1) NEGCHAR          ! Character used to indicate negation
+      PARAMETER (NEGCHAR = '-')
+      CHARACTER*(1) OCURLY           ! Character used to start a section
+      PARAMETER (OCURLY = '{')
+
+
 *  Local Variables:
-      CHARACTER*(1) CCURLY          ! Different search string
       INTEGER IPOSN                  ! Current position in string
-      CHARACTER*(1) OCURLY           ! Search string
+      INTEGER NEGPOS                 ! Position of '!'
       INTEGER SLENGTH                ! Length of input string
       INTEGER START                  ! Pos of opening brace
 
 *.
 
       IF (STATUS .NE. SAI__OK) RETURN
+
+*     Remove all blanks from the string
+      CALL CHR_RMBLK(NAME)
 
 *     Find length of input string
       SLENGTH = CHR_LEN(NAME)
@@ -86,10 +104,6 @@
       IPOSN = 1
       NSPEC = 0
 
-*     Set search string
-      OCURLY = '{'
-      CCURLY = '}'
-      
 *     First extract the filename from the section
 
       CALL CHR_FIND(NAME, OCURLY, .TRUE., IPOSN)
@@ -113,6 +127,37 @@
 *     Extract the name
          FILENAME = NAME(:IPOSN - 1)
          
+      END IF
+
+*     Now I would like to look for a '-' in the string.
+*     The presence of a '-' signifies negation regardless of where
+*     it appears - hopefully most people will put it in the end
+*     It can not appear before the first '{' (otherwise it would
+*     be part of the name.
+
+      USE_SECTION = .TRUE.
+
+*     Store the start position in the string
+
+      NEGPOS = IPOSN
+      
+*     Try to find if we are not past the end of the string already
+      IF (NEGPOS .LE. SLENGTH) THEN
+         
+         CALL CHR_FIND(NAME, NEGCHAR, .TRUE., NEGPOS)
+
+*     Have I found the string
+         IF (NEGPOS .LE. SLENGTH) THEN
+
+            USE_SECTION = .FALSE.   ! I have found a !
+
+*     Set the string NEGCHAR to a blank character so that it does 
+*     not confuse SCULIB_DECODE_SPEC
+
+            NAME(NEGPOS:NEGPOS) = ' '
+
+         END IF
+
       END IF
 
 *     Now loop through all the specifications
