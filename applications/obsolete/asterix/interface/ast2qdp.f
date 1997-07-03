010d6c3f9d16879597779aecd80a00eda0491663
Asterix Developer <asterix@star.sr.bham.ac.uk>
1997-07-03 09:39:15 +0000

    *** empty log message ***

--- applications/obsolete/asterix/interface/ast2qdp.f
+++ applications/obsolete/asterix/interface/ast2qdp.f
@@ -120,7 +120,7 @@
 
 *  Local Constants:
       CHARACTER*30		VERSION
-        PARAMETER		( VERSION = 'AST2QDP Version rb test' )
+        PARAMETER		( VERSION = 'AST2QDP Version 2.1-0b' )
 
 *  Local Variables:
       CHARACTER*80              LABEL              	!
