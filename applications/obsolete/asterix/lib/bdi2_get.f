9fcecb44dbb8f8a8289f323353773ad7720bcff1
Asterix Developer <asterix@star.sr.bham.ac.uk>
1997-07-14 11:28:06 +0000

    New scheme for BinDSAxis copying (RB)

--- applications/obsolete/asterix/lib/bdi2_get.f
+++ applications/obsolete/asterix/lib/bdi2_get.f
@@ -70,6 +70,7 @@
 
 *  Authors:
 *     DJA: David J. Allan (Jet-X, University of Birmingham)
+*     RB: Richard Beard (ROSAT, University of Birmingham)
 *     {enter_new_authors_here}
 
 *  History:
@@ -79,6 +80,8 @@
 *        Handle GET operation for whole axes
 *     6 Dec 1995 (DJA):
 *       Unified invention scheme
+*    14 Jul 1997 (RB):
+*       Add BinDSAxis scheme for axis copying
 *     {enter_changes_here}
 
 *  Bugs:
@@ -104,13 +107,26 @@
 
 *  Local Variables:
       CHARACTER*20		ITEM			! Item to get
+      CHARACTER*10		AXITEM(7)
+      CHARACTER*7		AXTYPE(7)
+      CHARACTER*40		CVAL
+
+      REAL			RVAL
 
       INTEGER			CACHEID			! File cache object
       INTEGER			ITID			! Invented item id
       INTEGER			NDIM, DIMS(ADI__MXDIM)	! Model object dims
       INTEGER			WBPTR			! Write back address
+      INTEGER			I
 
       LOGICAL			OK			! Data is valid?
+      LOGICAL			LVAL
+
+*  Local data:
+      DATA AXITEM		/'Label', 'Units', 'Normalised', 'Data',
+     :				 'Width', 'LoWidth', 'HiWidth'/
+      DATA AXTYPE		/'CHAR',  'CHAR',  'LOGICAL',    'REAL',
+     :                           'REAL',  'REAL',    'REAL'/
 *.
 
 *  Check inherited global status.
@@ -122,63 +138,75 @@
 *  Extract the arguments
       CALL ADI_GET0C( ARGS(3), ITEM, STATUS )
 
-*  Locate object to be got
-      CALL BDI2_CFIND( ARGS(1), ARGS(2), ITEM, .FALSE., .FALSE.,
-     :                 CACHEID, NDIM, DIMS, STATUS )
+*  Whole axis?
+      IF ( (ITEM(:5) .EQ. 'Axis_') .AND.
+     :     (INDEX(ITEM(6:),'_').EQ.0) ) THEN
 
-*  Everything ok?
-      IF ( (STATUS .EQ. SAI__OK) .AND. (CACHEID.NE.ADI__NULLID) ) THEN
-
-*    Whole axis?
-        IF ( (ITEM(:5) .EQ. 'Axis_') .AND.
-     :       (INDEX(ITEM(6:),'_').EQ.0) ) THEN
-
-*      Create ADI axis description object
-          CALL ADI_NEW0( 'BinDSAxis', OARG, STATUS )
+*    Create ADI axis description object
+        CALL ADI_NEW0( 'BinDSAxis', OARG, STATUS )
 
 *      Copy each component of the axis structure
-c          CALL ADI1_CCH2AC( CLOC, 'LABEL', OARG, 'Label', STATUS )
-c          CALL ADI1_CCH2AC( CLOC, 'UNITS', OARG, 'Units', STATUS )
-c          CALL ADI1_CCH2AL( CLOC, 'NORMALISED', OARG,
-c     :                      'Normalised', STATUS )
-c          CALL ADI1_CCH2AT( CLOC, 'DATA_ARRAY', OARG, 'Data', STATUS )
-c          CALL ADI1_CCH2AT( CLOC, 'WIDTH', OARG, 'Width', STATUS )
-c          CALL ADI1_CCH2AT( CLOC, 'LOWIDTH', OARG, 'LoWidth', STATUS )
-c          CALL ADI1_CCH2AT( CLOC, 'HIWIDTH', OARG, 'HiWidth', STATUS )
+        DO I = 1, 7
+          CALL BDI2_CFIND( ARGS(1), ARGS(2), ITEM(1:6)//'_'//AXITEM(I),
+     :                     .FALSE., .FALSE., CACHEID, NDIM, DIMS,
+     :                     STATUS )
+          IF ( CACHEID .NE. ADI__NULLID ) THEN
+            CALL ADI_CNEW0( OARG, AXITEM(I), AXTYPE(I), STATUS )
+            IF ( AXTYPE(I) .EQ. 'REAL' ) THEN
+              CALL ADI_CGET0R( CACHEID, 'Value', RVAL, STATUS )
+              CALL ADI_CPUT0R( OARG, AXITEM(I), RVAL, STATUS )
+            ELSE IF ( AXTYPE(I) .EQ. 'CHAR' )  THEN
+              CALL ADI_CGET0C( CACHEID, 'Value', CVAL, STATUS )
+              CALL ADI_CPUT0C( OARG, AXITEM(I), CVAL, STATUS )
+            ELSE IF ( AXTYPE(I) .EQ. 'LOGICAL' )  THEN
+              CALL ADI_CGET0L( CACHEID, 'Value', LVAL, STATUS )
+              CALL ADI_CPUT0L( OARG, AXITEM(I), LVAL, STATUS )
+            END IF
+          ELSE
+            CALL ERR_ANNUL( STATUS )
+          END IF
+        END DO
+      ELSE
+
+*    Locate object to be got
+        CALL BDI2_CFIND( ARGS(1), ARGS(2), ITEM, .FALSE., .FALSE.,
+     :                   CACHEID, NDIM, DIMS, STATUS )
+
+*    Everything ok?
+        IF ( (STATUS .EQ. SAI__OK) .AND. (CACHEID.NE.ADI__NULLID) ) THEN
 
 *    Just locate the data in the cache object
 *    Special fix for no AUNIT keyword in ROSAT files (RB)
-        ELSE
           CALL ADI_FIND( CACHEID, 'Value', OARG, STATUS )
           OK = (STATUS .NE. SAI__OK)
 
 *      Free the cache object
           CALL ADI_ERASE( CACHEID, STATUS )
 
-        END IF
-
-      ELSE
-
-*    Try to invent data
-        CALL ERR_BEGIN( STATUS )
-        CALL BDI2_INVNT( ARGS(1), ARGS(2), ITEM, 'REAL', 'READ',
-     :                   ITID, NDIM, DIMS, WBPTR, STATUS )
-        CALL ERR_END( STATUS )
-
-*    Invented ok?
-        IF ( ITID .NE. ADI__NULLID ) THEN
-
-*      We should store this and re-use it
-          CALL ERR_ANNUL( STATUS )
-
-*      Return to user
-          OARG = ITID
-
         ELSE
-          STATUS = SAI__ERROR
-          CALL MSG_SETC( 'IT', ITEM )
-          CALL ERR_REP( ' ', 'Unable to get item ^IT data from '/
-     :                  /'HDS file', STATUS )
+
+*      Try to invent data
+          CALL ERR_BEGIN( STATUS )
+          CALL BDI2_INVNT( ARGS(1), ARGS(2), ITEM, 'REAL', 'READ',
+     :                     ITID, NDIM, DIMS, WBPTR, STATUS )
+          CALL ERR_END( STATUS )
+
+*      Invented ok?
+          IF ( ITID .NE. ADI__NULLID ) THEN
+
+*        We should store this and re-use it
+            CALL ERR_ANNUL( STATUS )
+
+*        Return to user
+            OARG = ITID
+
+          ELSE
+            STATUS = SAI__ERROR
+            CALL MSG_SETC( 'IT', ITEM )
+            CALL ERR_REP( ' ', 'Unable to get item ^IT data from '/
+     :                    /'HDS file', STATUS )
+          END IF
+
         END IF
 
       END IF
