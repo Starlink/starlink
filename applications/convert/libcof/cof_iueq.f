62f348ca339e9bf1ca24e5bcc2321d333b9bfffb
Malcolm J. Currie <mjc@star.rl.ac.uk>
1997-08-07 20:51:24 +0000

    Converts IUEFA 16-bit quality to NDF quality.
    svn path=/trunk/; revision=1290

---
+++ applications/convert/libcof/cof_iueq.f
@@ -0,0 +1,77 @@
+      SUBROUTINE COF_IUEQ( EL, IUEQUA, QUALIT, STATUS )
+*+
+*  Name:
+*     COF_IUEQ
+
+*  Purpose:
+*     Converts IUEFA 16-bit quality to NDF quality.
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Invocation:
+*     CALL COF_IUEQ( EL, IUEQUA, QUALIT, STATUS )
+
+*  Description:
+*     This routines takes IUE Final Archive quality information
+*     stored at 2's complement 16-bit integers, into unsigned-byte
+*     quality as required by the NDF.  The most significant 8 bits of
+*     the 14 actually used are transferred.  In practice this means a
+*     division by -128.
+
+*  Arguments:
+*     EL = INTEGER (Given)
+*        The number of elements in the quality arrays.
+*     IUEQUA( EL ) = INTEGER * 2 (Given)
+*        The IUE Final Archive quality array.
+*     QUALIT( EL ) = BYTE (Returned)
+*        The NDF quality array.
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
+
+*  [optional_subroutine_items]...
+*  Authors:
+*     MJC: Malcolm J. Currie (STARLINK)
+*     {enter_new_authors_here}
+
+*  History:
+*     1996 June 29 (MJC):
+*        Original version.
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+      
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+
+*  Arguments Given:
+      INTEGER EL
+      INTEGER * 2 IUEQUA( EL )
+
+*  Arguments Returned:
+      BYTE QUALIT( EL )
+
+*  Status:
+      INTEGER STATUS             ! Global status
+
+*  Local Variables:
+      INTEGER I                  ! Loop counter
+
+*.
+
+*  Check inherited global status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  Loop for all pixels, dividing by -128 to get values in the range
+*  0 to 256.
+      DO I = 1, EL
+         QUALIT( I ) = -IUEQUA( I ) / 128
+      END DO
+
+      END
