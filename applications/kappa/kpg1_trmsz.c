3d2668e9e7d3472c4960025cba503d91f87e9651
David S. Berry <d.berry@jach.hawaii.edu>
1998-04-01 09:21:12 +0000

    *** empty log message ***
    svn path=/trunk/; revision=1852

--- applications/kappa/kpg1_trmsz.c
+++ applications/kappa/kpg1_trmsz.c
@@ -1,79 +1,67 @@
 /*
-*+
 *  Name:
 *     KPG1_TRMSZ
 
 *  Purpose:
-*     Obtains the width and height of the terminal screen or window for
-*     UNIX.
+*     A Fortran callable function to obtain the size of the output screen.
 
 *  Language:
-*     ANSI C
+*     C
 
 *  Invocation:
-*     CALL KPG1_TRMSZ( WIDTH, HEIGHT )
+*     ISTAT = KPG1_TRMSZ( WIDTH, HEIGHT )
 
 *  Description:
-*     This routine obtains the width and height of the terminal screen
-*     or window for UNIX.
+*     The routine uses the ioctl system to obtain the window size for
+*     stdout.
+*     Compile with -I/star/include (use gcc on Sun)
+
+*     {routine_description}
 
 *  Arguments:
 *     WIDTH = INTEGER (Returned)
-*        The width of the screen in characters.
+*        The width of the screen (characters)
 *     HEIGHT = INTEGER (Returned)
-*        The height of the screen in lines.
+*        The height of the screen (lines)
 
-*  Building:
-*     -  On SUNs this should be compiled with gcc, but with cc on
-*     DECstations.  Use the switch "-I/star/include" on both systems.
+*  Returned Value:
+*     KPG1_TRMSZ = INTEGER
+*        Returns 1 on success; 0 on failure.
 
-*  [optional_subroutine_items]...
 *  Authors:
-*     AJC: Alan Chipperfield (STARLINK)
-*     MJC: Malcolm J. Currie (STARLINK)
+*     AJC: A J Chipperfield (STARLINK)
 *     {enter_new_authors_here}
 
 *  History:
-*     1992 June 17 (AJC):
-*        Original version.
-*     1992 June 22 (MJC):
-*        Renamed and modified for KAPPA use.  Converted to SST prologue.
-*     1993 March 15 (MJC):
-*        Cast initscr to int and added endwin call.
-*     1993 August 1 (MJC):
-*        New version that does not require curses.
-*     {enter_further_changes_here}
+*     10-FEB-1998 (AJC):
+*        Re-written using termios (based on SUBPAR_TRMSZ).
+*     {enter_changes_here}
 
 *  Bugs:
 *     {note_any_bugs_here}
 
 *- */
-
-/* Global Constants: */
-#include <sys/termio.h>
+#include <termios.h>
+#ifndef TIOCGWINSZ
+#include <sys/ioctl.h>
+#endif
+#include <unistd.h>
 #include "f77.h"
-
-/*
-*.  */
 
 F77_INTEGER_FUNCTION(kpg1_trmsz)(INTEGER(width),INTEGER(height))
 {
 
-/* Pointers to Arguments:						    */
+/* Pointers to Arguments:
+*/
    GENPTR_INTEGER(width)
    GENPTR_INTEGER(height)
 
-#ifdef TIOCGWINSZ
 	struct winsize s;
 
-/* this 1 is the file descriptor eqivalent of 'SYS$OUTPUT' on VMS.	    */
-	if (ioctl (1, TIOCGWINSZ, &s))
+	if (ioctl (STDOUT_FILENO, TIOCGWINSZ, (char *) &s) < 0)
 		return 0;
+
 	*height = s.ws_row;
 	*width = s.ws_col;
-	
 	return 1;
-#else
-	return 0;
-#endif
 }
