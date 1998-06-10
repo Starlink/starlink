fd9a7c2284aed2422c30a455c463e3c20471d7c4
Tim Jenness <t.jenness@jach.hawaii.edu>
1998-06-10 22:44:02 +0000

    Initial revision
    svn path=/trunk/; revision=2141

---
+++ applications/surf/scripts/change_nacentre.pl
@@ -0,0 +1,130 @@
+#!/star/bin/ndfperl
+
+# Perl program to add an offset to the bolometer positions
+# stored in a file.
+
+# Command line options
+use Getopt::Long;
+
+
+# Use the NDF module
+use NDF;
+
+# Use READLINE for I/O
+use Term::ReadLine;
+
+# Read the standard options from the command line
+$result = GetOptions("help"    => \$h,
+                     "version" => \$v,
+                     "out=s"   => \$outfile
+                     );
+
+$h = 1 unless $result == 1;  # Print help info if unknown option
+ 
+ 
+# Print a help message
+ 
+ 
+($h) && do {
+  print qq/
+Usage:
+  change_nacentre [-h] [-v] infile dx dy
+Options:
+\t-h[elp]   \tThis message.
+\t-v[ersion]\tPrint version number of program.
+\tinfile    \tInput file name (no .sdf)
+\tdx        \tChange in Nasmyth X direction
+\tdy        \tChange in Nasmyth Y direction
+Description:
+\tShift the Nasmyth centre position of the array.
+\tCan be used to take out the difference between the
+\tcentres of the LONG and SHORT wave arrays.
+\tThis command is designed to be run after EXTINCTION
+\tcorrection.
+/;
+  exit;
+};
+
+($v) && do {
+  print qq/change_nacent: version 0.10 (1998)
+/;
+  exit;
+};
+
+# Create a Term object
+$term = new Term::ReadLine 'nacent';
+
+
+# Now read the command line arguments one at a time.
+
+if ($#ARGV > -1) {
+  $in = shift;
+} else {
+  $prompt = "Please enter input file name: ";
+  $in = $term->readline($prompt);
+}
+$in =~ s/\.sdf$//;  # Strip trailing sdf
+
+
+if ($#ARGV > -1) {
+  $du3 = shift;
+} else {
+  $prompt = "Please enter NA shift in X direction: ";
+  $du3 = $term->readline($prompt);
+}
+
+if ($#ARGV > -1) {
+  $du4 = shift;
+} else {
+  $prompt = "Please enter NA shift in Y direction: ";
+  $du4 = $term->readline($prompt);
+}
+
+
+# Now we have all the required parameters
+
+# Start NDF
+ndf_begin;
+$status = &NDF::SAI__OK;
+
+# Open the file
+ndf_open(&NDF::DAT__ROOT, $in, 'UPDATE', 'OLD', $indf, $place, $status);
+
+# Find the SCUBA extension
+ndf_xloc($indf, 'SCUBA', 'UPDATE', $xloc, $status);
+
+# Read the BOL_DU3 extension if dx is non zero
+# Can simply add this value to all members of the BOL_DU* array
+# even if the bolometers arent being used
+
+
+foreach $coord ('du3', 'du4') {
+
+
+  $du = $$coord;
+  $comp = 'BOL_'.uc($coord);
+
+  print "Adding $du arcsec to $coord\n";
+
+  if ($du != 0.0) {
+    # Read
+    cmp_getvd($xloc, $comp, 200, @offsets, $el, $status);
+
+    # Add offset
+    map { $_ += $du } @offsets;
+
+    # Write
+    cmp_putvr($xloc, $comp, $el, @offsets, $status);
+
+  }
+}
+
+# Annul locator
+dat_annul($xloc, $status);
+
+# Close NDF
+ndf_annul($indf, $status);
+
+# Shut down ndf
+ndf_end($status);
+
