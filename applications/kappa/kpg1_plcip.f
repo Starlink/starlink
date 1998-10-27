b57bdde9ec0168fd6dba0f6df3f7f6024f68f34e
David S. Berry <d.berry@jach.hawaii.edu>
1998-10-27 12:22:25 +0000

    *** empty log message ***
    svn path=/trunk/; revision=2587

--- applications/kappa/kpg1_plcip.f
+++ applications/kappa/kpg1_plcip.f
@@ -77,6 +77,9 @@
       PARAMETER ( COPREC = 0.001 )
 
 *  Local Variables:
+      INTEGER CI1                ! Lowest available colour index
+      INTEGER CI2                ! Highest available colour index
+      INTEGER HI                 ! Highest palette colour index
       INTEGER I                  ! Loop counter
       LOGICAL MATCH              ! A colour match found?
       REAL B                     ! Blue intensity
@@ -85,7 +88,6 @@
       REAL METRIC                ! Metric for a colour in the colour set
       REAL PALETT( NPRICL, 0:CTM__RSVPN - 1 ) ! Palette colours
       REAL R                     ! Red intensity
-
 *.
 
 *  Check the inherited global status.
@@ -94,10 +96,31 @@
 *  Find the RGB intensities of the named colour.
       CALL KPG1_NMCOL( COLOUR, R, G, B, STATUS )
 
+*  Find the range of colour indices available on the cureent graphics
+*  device.
+      CALL PGQCOL( CI1, CI2 )
+
+*  For Xoverlay devices, the above call returns CI1=0 implying that 
+*  the background can be written to. This is technically correct because
+*  writing to the background causes any foreground graphics to be "rubbed
+*  out".  However, the pen representation for the background gives
+*  rgb=0,0,0 (i.e. it thinks the background is a black pen). This is not
+*  correct, therefore do not allow pen 0 to be used if CI1 is zero and
+*  CI2 = 1.
+      IF( CI1 .EQ. 0 .AND. CI2 .EQ. 1 ) THEN
+         COLIND = 1
+
+*  Otherwise,
+      ELSE
+
+*  Note the highest colour index in the palete.
+         HI = MIN( CI2, CTM__RSVPN - 1 )
+
 *  Inquire the palette colour indices.
-      DO  I = 0, CTM__RSVPN - 1
-         CALL PGQCR( I, PALETT( 1, I ), PALETT( 2, I ), PALETT( 3, I ) )
-      END DO
+         DO  I = CI1, HI
+            CALL PGQCR( I, PALETT( 1, I ), PALETT( 2, I ), 
+     :                  PALETT( 3, I ) )
+         END DO
 
 *  Loop to find a colour corresponding to the input colour's RGB.
 *  ==============================================================
@@ -106,31 +129,32 @@
 *  be exited.  For efficiency use a city-block metric to find the
 *  closest colour.  The closest match must therefore be less than
 *  three.
-      MATCH = .FALSE.
-      I = 0
-      CLOMET = 3.0
-      DO WHILE ( .NOT. MATCH .AND. I .LE. CTM__RSVPN - 1 )
-         METRIC = ABS( R - PALETT( 1, I ) ) +
-     :            ABS( G - PALETT( 2, I ) ) +
-     :            ABS( B - PALETT( 3, I ) )
+         MATCH = .FALSE.
+         I = CI1
+         CLOMET = 3.0
+         DO WHILE ( .NOT. MATCH .AND. I .LE. HI )
+            METRIC = ABS( R - PALETT( 1, I ) ) +
+     :               ABS( G - PALETT( 2, I ) ) +
+     :               ABS( B - PALETT( 3, I ) )
 
-         IF ( METRIC .LT. 3 * COPREC ) THEN
+            IF ( METRIC .LT. 3 * COPREC ) THEN
 
 *  A match is found so exit and record the colour index.
-            MATCH = .TRUE.
-            COLIND = I
+               MATCH = .TRUE.
+               COLIND = I
 
-         ELSE
+            ELSE
 
 *  Look to see whether or not the latest colour is nearer than the 
 *  previous nearest.
-            IF ( METRIC .LT. CLOMET ) THEN
-               CLOMET = METRIC
-               COLIND = I
+               IF ( METRIC .LT. CLOMET ) THEN
+                  CLOMET = METRIC
+                  COLIND = I
+               END IF
+   
+               I = I + 1
             END IF
-
-            I = I + 1
-         END IF
-      END DO
+         END DO
+      END IF
       
       END
