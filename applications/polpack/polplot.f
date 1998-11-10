6e90f6695ae796f1f452229100c03c5656a2f03d
David S. Berry <d.berry@jach.hawaii.edu>
1998-11-10 13:11:06 +0000

    *** empty log message ***
    svn path=/trunk/; revision=2673

--- applications/polpack/polplot.f
+++ applications/polpack/polplot.f
@@ -285,7 +285,8 @@
 *        the interpretation of parameters COLX and COLY accordingly. Defaults 
 *        for LBND and UBND changed.
 *     9-NOV-1998 (DSB):
-*        - Parameter COSYS replaced by FRAME andd EPOCH.
+*        Modified to bring the handling of WCS and graphics into line with
+*        KAPPA V0.13.
 *     {enter_further_changes_here}
 
 *  Bugs:
@@ -769,32 +770,8 @@
 
 *  Produce the plot.
 *  =================
-*  First draw the axes if required. Set a default height if none has been
-*  given. Otherwise, PGPLOT produces constant sizes text irrespective of
-*  the size of the picture. Setting a size ensures that the text drawing 
-*  routine in grf_kappa.c adjusts the size of the text to a constant
-*  fraction of the current picture size. If text heights are set, clear
-*  them after drawing the axes.
-      IF ( AXES ) THEN
-
-         F1 = AST_TEST( IPLOT, 'SIZE(NUMLAB)', STATUS )
-         F2 = AST_TEST( IPLOT, 'SIZE(TEXTLAB)', STATUS )
-         F3 = AST_TEST( IPLOT, 'SIZE(TITLE)', STATUS )
-
-         IF( .NOT. F1 ) CALL AST_SETR( IPLOT, 'SIZE(NUMLAB)', 1.5, 
-     :                                 STATUS )
-         IF( .NOT. F2 ) CALL AST_SETR( IPLOT, 'SIZE(TEXTLAB)', 1.5, 
-     :                                 STATUS )
-         IF( .NOT. F3 ) CALL AST_SETR( IPLOT, 'SIZE(TITLE)', 1.5, 
-     :                                 STATUS )
-
-         CALL AST_GRID( IPLOT, STATUS )
-
-         IF( .NOT. F1 ) CALL AST_CLEAR( IPLOT, 'SIZE(NUMLAB)', STATUS )
-         IF( .NOT. F2 ) CALL AST_CLEAR( IPLOT, 'SIZE(TEXTLAB)', STATUS )
-         IF( .NOT. F3 ) CALL AST_CLEAR( IPLOT, 'SIZE(TITLE)', STATUS )
-
-      END IF
+*  First draw the axes if required. 
+      IF ( AXES ) CALL KPG1_ASGRD( IPLOT, IPICF, .TRUE., STATUS )
 
 *  Get the Mapping from the Frame in which catalogue positions are stored,
 *  to graphics world coordinates.
