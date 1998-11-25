efac813b539991d28caf5e9d45810220206b8a68
Peter W. Draper <P.W.Draper@durham.ac.uk>
1998-11-25 17:19:04 +0000

    Removed "id" from headers. This isn't defined and isn't needed.
    svn path=/trunk/; revision=2808

--- applications/extractor/extract/src/sexheadsc.h
+++ applications/extractor/extract/src/sexheadsc.h
@@ -12,6 +12,8 @@
 *	Last modify:	02/09/97
 *                       23/10/98 (AJC):
 *                         Remove col headings from skycathead
+*                       23/11/98 (PWD):
+*                         Remove id from Skycat heading
 *
 *%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 */
@@ -21,7 +23,7 @@ char	skycathead[] = "QueryResult\n\n"
 	"serv_type: catalog\n"
 	"long_name: SExtractor catalog\n"
 	"short_name: SExCat\n"
-	"symbol: id diamond %4.1f\n"
+	"symbol: {} diamond %4.1f\n"
 	"# End config entry\n\n";
 
 char	skycattail[] = "[EOD]";
