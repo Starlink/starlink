1987bd222ba64bfe5ef054b1b27ab44f34068eb1
David S. Berry <d.berry@jach.hawaii.edu>
1999-04-13 11:40:11 +0000

    *** empty log message ***
    svn path=/trunk/; revision=3490

--- applications/polpack/pol1_blocr.f
+++ applications/polpack/pol1_blocr.f
@@ -1,44 +1,24 @@
-      SUBROUTINE POL1_BLOCR( BAD, SAMBAD, VAR, NX, NY, A, IBOXX,
-     :                         IBOXY, NLIM, B, BADOUT, ASUM, NSUM,
-     :                         STATUS )
+      SUBROUTINE POL1_BLOCR( NX, NY, A, IBOXX, IBOXY, WLIM, B, ASUM, 
+     :                       WSUM, STATUS )
 *+
 *  Name:
 *     POL1_BLOCR
  
 *  Purpose:
-*     Smooth a 2-dimensional image using a rectangular box filter.
+*     Smooth a 2-dimensional image using a weighted rectangular box filter.
  
 *  Language:
 *     Starlink Fortran 77
  
 *  Invocation:
-*     CALL POL1_BLOCR( BAD, SAMBAD, VAR, NX, NY, A, IBOXX,
-*                      IBOXY, NLIM, B, BADOUT, ASUM, NSUM, STATUS )
+*     CALL POL1_BLOCR( NX, NY, A, IBOXX, IBOXY, WLIM, B, ASUM, WSUM, STATUS )
  
 *  Description:
-*     The routine smooths a 2-dimensional image using a rectangular box
-*     filter; each pixel is replaced by the mean of those good
+*     The routine smooths a 2-dimensional image using a unweighted rectangular 
+*     box filter; each pixel is replaced by the unweighted mean of those good
 *     neighbours which lie within a box of specified size.
  
 *  Arguments:
-*     BAD = LOGICAL (Given)
-*        Whether it is necessary to check for bad pixels in the input
-*        image.
-*     SAMBAD = LOGICAL (Given)
-*        If a .TRUE. value is given for this argument, then bad input
-*        pixels will be propagated to the output image unchanged (a
-*        smoothed output value will be calculated for all other
-*        pixels). If a .FALSE. value is given, then the NLIM argument
-*        determines whether an output pixel is good or bad.  The value
-*        of SAMBAD is not relevant if BAD is .FALSE..
-*     VAR = LOGICAL (Given)
-*        If a .FALSE. value is given for this argument, then the
-*        smoothing applied will be appropriate to a data image. If a
-*        .TRUE. value is given, then the smoothing will be appropriate
-*        to an image containing variance values. In the latter case the
-*        output values will be (on average) smaller than the input
-*        values to take account of the variance-reducing effect which
-*        smoothing produces.
 *     NX = INTEGER (Given)
 *        First dimension of the image to be smoothed.
 *     NY = INTEGER (Given)
@@ -51,20 +31,15 @@
 *     IBOXY = INTEGER (Given)
 *        Half-size of the smoothing box in pixels in the Y direction
 *        (the actual size of the box used will be 2*IBOXY+1 pixels).
-*     NLIM = INTEGER (Given)
-*        Minimum number of good pixels which must be present in the
-*        smoothing box in order to calculate a smoothed output pixel.
-*        If this minimum number is not satisfied, then a bad output
-*        pixel will result. A value between 1 and the total number of
-*        pixels in the smoothing box should be supplied.
+*     WLIM = INTEGER (Given)
+*        The minimum number of contributing pixels required to produce 
+*        a good smoothed output pixel value. 
 *     B( NX, NY ) = REAL (Returned)
 *        The smoothed output image.
-*     BADOUT = LOGICAL (Returned)
-*        Whether bad pixels are present in the output image.
-*     ASUM( NX ) = REAL (Returned)
+*     ASUM( NX ) = DOUBLE PRECISION (Returned)
 *        Workspace for the pixel sums.
-*     NSUM( NX ) = INTEGER (Returned)
-*        Workspace for counting good pixels.
+*     WSUM( NX ) = INTEGER (Returned)
+*        Workspace for the weight sums.
 *     STATUS = INTEGER (Given and Returned)
 *        The global status.
  
@@ -74,12 +49,8 @@
 *     {enter_new_authors_here}
  
 *  History:
-*     18-SEP-1990 (RFWS):
-*        Original version.
-*     24-SEP-1990 (RFWS):
-*        Added the VAR argument to allow smoothing of variance images.
-*     1-MAR-1999 (DSB):
-*        Brought into POLPACK.
+*     3-MAR-1999 (DSB):
+*        Original version, based on KPG1_BLOCR by RFWS.
 *     {enter_further_changes_here}
  
 *  Bugs:
@@ -95,29 +66,22 @@
       INCLUDE 'PRM_PAR'          ! PRIMDAT primitive data constants
  
 *  Arguments Given:
-      LOGICAL BAD
-      LOGICAL SAMBAD
-      LOGICAL VAR
       INTEGER NX
       INTEGER NY
       REAL A( NX, NY )
       INTEGER IBOXX
       INTEGER IBOXY
-      INTEGER NLIM
+      INTEGER WLIM
  
 *  Arguments Returned:
       REAL B( NX, NY )
-      LOGICAL BADOUT
-      REAL ASUM( NX )
-      INTEGER NSUM( NX )
+      DOUBLE PRECISION ASUM( NX )
+      INTEGER WSUM( NX )
  
 *  Status:
       INTEGER STATUS             ! Global status
  
 *  Local Variables:
-      REAL ASUM0               ! Good pixel sum
-      REAL DENOM               ! Denominator for smoothed output value
-      REAL NPIX                ! Number of pixels in smoothing box
       INTEGER IIX                ! Loop counter for summing over pixels
       INTEGER IIY                ! Loop counter for summing over lines
       INTEGER ILINE              ! Contributing line number
@@ -126,24 +90,20 @@
       INTEGER IY                 ! Second array index
       INTEGER NEWX               ! X position of new pixel
       INTEGER NEWY               ! Y position of new pixel
-      INTEGER NSUM0              ! Good pixel count
       INTEGER OLDX               ! X position of old pixel
       INTEGER OLDY               ! Y position of old line
- 
+      DOUBLE PRECISION ASUM0     ! Good pixel sum
+      INTEGER WSUM0              ! Good pixel count
 *.
  
 *  Check inherited global status.
       IF ( STATUS .NE. SAI__OK ) RETURN
- 
-*  Initialise the bad output pixel flag and the workspace arrays.
-      BADOUT = .FALSE.
+
+*  Initialise the workspace arrays.
       DO 1 IX = 1, NX
-         ASUM( IX ) = 0.0E0
-         NSUM( IX ) = 0
+         ASUM( IX ) = 0.0D0
+         WSUM( IX ) = 0
  1    CONTINUE
- 
-*  Calculate the number of pixels in the smoothing box.
-      NPIX = ( 2 * IBOXX + 1 ) * ( 2 * IBOXY + 1 )
  
 *  Loop to initialise elements of the two workspace arrays so that they
 *  contain sum for the good pixels in a box of size 1x(2*IBOXY+1)
@@ -161,22 +121,13 @@
             ILINE = IIY
          END IF
  
-*  Accumulate sums for each box in the workspace arrays. If bad pixels
-*  are present, then test for them.
-         IF ( BAD ) THEN
-            DO 2 IX = 1, NX
-               IF ( A( IX, ILINE ) .NE. VAL__BADR ) THEN
-                  ASUM( IX ) = ASUM( IX ) + A( IX, ILINE )
-                  NSUM( IX ) = NSUM( IX ) + 1
-               END IF
- 2          CONTINUE
- 
-*  If no bad pixels are present, then omit the test for them.
-         ELSE
-            DO 3 IX = 1, NX
+*  Accumulate sums for each box in the workspace arrays. 
+         DO 2 IX = 1, NX
+            IF ( A( IX, ILINE ) .NE. VAL__BADR ) THEN
                ASUM( IX ) = ASUM( IX ) + A( IX, ILINE )
- 3          CONTINUE
-         END IF
+               WSUM( IX ) = WSUM( IX ) + 1
+            END IF
+ 2       CONTINUE
  4    CONTINUE
  
 *  Smooth down the image.
@@ -194,37 +145,24 @@
          IF ( NEWY .GT. NY ) NEWY = NY
  
 *  Remove the pixels in the old line from the accumulated sums and add
-*  the pixels in the new line.  If bad pixels are present, then test
-*  for them.
-         IF ( BAD ) THEN
-            DO 5 IX = 1, NX
-               IF ( A( IX, OLDY ) .NE. VAL__BADR ) THEN
-                  ASUM( IX ) = ASUM( IX ) - A( IX, OLDY )
-                  NSUM( IX ) = NSUM( IX ) - 1
-               END IF
-               IF ( A( IX, NEWY ) .NE. VAL__BADR ) THEN
-                  ASUM( IX ) = ASUM( IX ) + A( IX, NEWY )
-                  NSUM( IX ) = NSUM( IX ) + 1
-               END IF
- 5          CONTINUE
- 
-*  If bad pixels are absent, then there is no need to test for them.
-*  There is also no need to keep track of the number of good pixels in
-*  the box, as this will always be equal to the total number of pixels
-*  in the smoothing box.
-         ELSE
-            DO 6 IX = 1, NX
-               ASUM( IX ) = ASUM( IX ) + ( A( IX, NEWY ) -
-     :                                     A( IX, OLDY ) )
- 6          CONTINUE
-         END IF
+*  the pixels in the new line. 
+         DO 5 IX = 1, NX
+            IF ( A( IX, OLDY ) .NE. VAL__BADR ) THEN
+               ASUM( IX ) = ASUM( IX ) - A( IX, OLDY )
+               WSUM( IX ) = WSUM( IX ) - 1
+            END IF
+            IF ( A( IX, NEWY ) .NE. VAL__BADR ) THEN
+               ASUM( IX ) = ASUM( IX ) + A( IX, NEWY )
+               WSUM( IX ) = WSUM( IX ) + 1
+            END IF
+ 5       CONTINUE
  
 *  Smooth along each image line.
 *  ============================
 *  Form initial sums for a box of size (2*IBOXX+1)x1 centred on pixel
 *  (0,IY).
-         ASUM0 = 0.0E0
-         NSUM0 = 0
+         ASUM0 = 0.0D0
+         WSUM0 = 0
          DO 7 IIX = -IBOXX, IBOXX
  
 *  Allow for the image boundaries.
@@ -239,7 +177,7 @@
 *  Use the current values stored in the workspace arrays to form these
 *  initial sums.
             ASUM0 = ASUM0 + ASUM( IPIX )
-            IF ( BAD ) NSUM0 = NSUM0 + NSUM( IPIX )
+            WSUM0 = WSUM0 + WSUM( IPIX )
  7       CONTINUE
  
 *  Loop to move these sums along the current image line.
@@ -255,55 +193,18 @@
  
 *  Update the sums by subtracting the old pixel and adding the new one.
             ASUM0 = ASUM0 + ( ASUM( NEWX ) - ASUM( OLDX ) )
-            IF ( BAD ) NSUM0 = NSUM0 - NSUM( OLDX ) + NSUM( NEWX )
- 
-*  Form the denominator for calculating the smoothed output value. If
-*  there are no bad pixels present, then this is equal to the number of
-*  pixels in the smoothing box. Otherwise, use the accumulated sum of
-*  good pixels.
-            IF ( .NOT. BAD ) THEN
-               DENOM = NPIX
-            ELSE
-               DENOM = NSUM0
-            END IF
- 
-*  If smoothing a variance image, then use the square of the number of
-*  good pixels (this takes account of the variance-reducing effect that
-*  smoothing introduces).
-            IF ( VAR ) DENOM = DENOM * DENOM
+            WSUM0 = WSUM0 + ( WSUM( NEWX ) - WSUM( OLDX ) )
  
 *  Calculate the smoothed output value.
-*  ===================================
-*  This is valid if there are no bad pixels present.
-            IF ( .NOT. BAD ) THEN
-               B( IX, IY ) = ASUM0 / DENOM
+            IF ( WSUM0 .GE. WLIM ) THEN
+               B( IX, IY ) = ASUM0 / REAL( WSUM0 )
  
-*  It is also valid if SAMBAD is .TRUE. and the corresponding input
-*  pixel is not bad.
-            ELSE IF ( SAMBAD ) THEN
-               IF ( A( IX, IY ) .NE. VAL__BADR ) THEN
-                  B( IX, IY ) = ASUM0 / DENOM
- 
-*  It is bad if SAMBAD is .TRUE. and the corresponding input pixel is
-*  bad.  Note that bad pixels are present in the output image.
-               ELSE
-                  B( IX, IY ) = VAL__BADR
-                  BADOUT = .TRUE.
-               END IF
- 
-*  It is valid if SAMBAD is .FALSE. and the NLIM criterion is met.
+*  It is bad if the WLIM criterion is not met. 
             ELSE
-               IF ( NSUM0 .GE. NLIM ) THEN
-                  B( IX, IY ) = ASUM0 / DENOM
- 
-*  It is bad if the NLIM criterion is not met. Note that bad pixels are
-*  present in the output image.
-               ELSE
-                  B( IX, IY ) = VAL__BADR
-                  BADOUT = .TRUE.
-               END IF
+               B( IX, IY ) = VAL__BADR
             END IF
+
  8       CONTINUE
  9    CONTINUE
- 
+
       END
