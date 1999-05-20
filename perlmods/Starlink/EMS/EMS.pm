552ef684ba4fa6c2d03c1da410020f888a6d291c
Tim Jenness <t.jenness@jach.hawaii.edu>
1999-05-20 03:46:31 +0000

    Add new routines to export list. Still needs doc overhaul.
    svn path=/trunk/; revision=3864

--- perlmods/Starlink/EMS/EMS.pm
+++ perlmods/Starlink/EMS/EMS.pm
@@ -2,7 +2,7 @@ package Starlink::EMS;
 
 use strict;
 use Carp;
-use vars qw($VERSION @ISA @EXPORT );
+use vars qw($VERSION @ISA @EXPORT %EXPORT_TAGS);
 
 require Exporter;
 require DynaLoader;
@@ -14,7 +14,28 @@ require DynaLoader;
 	     ems1_get_facility_error
 	     SAI__OK SAI__ERROR SAI__WARN
 );
-$VERSION = '0.03';
+
+%EXPORT_TAGS = (
+		'ems' => [qw/
+			  ems_annul ems_begin ems_eload ems_end
+			  ems_errno ems_facer
+			  ems_fmtc ems_fmtd ems_fmti ems_fmtl ems_fmtr
+			  ems_level ems_mark ems_mload
+			  ems_renew ems_rep ems_rlse
+			  ems_setc ems_setd ems_seti ems_setl ems_setr
+			  ems_stat ems_syser
+			  /],
+		'emserr' => [qw/
+			     EMS__NOMSG
+			     /],
+		'sai'    => [qw/
+			     SAI__ERROR SAI__OK SAI__WARN
+			     /]
+	       );
+
+Exporter::export_tags('ems','emserr','sai');
+
+'$Revision$ ' =~ /.*:\s(.*)\s\$/ && ($VERSION = $1);
 
 
 bootstrap Starlink::EMS $VERSION;
@@ -37,15 +58,19 @@ Starlink::EMS - Perl extension for Starlink EMS library
 =head1 DESCRIPTION
 
 This module provides a simple interface to the Starlink EMS library.
-It provides a single function for converting Starlink error status
-integers into error texts and names.
-
-The only routine exported by this module is ems1_get_facility_error.
+All C routines are available plus an extra internal routien
+for converting a status code directly to the error mesage
+components (ems1_get_facility_error).
 
 =head1 CONSTANTS
 
 The following constants are available: SAI__OK, SAI__ERROR,
 SAI__WARN. They are exported by default.
+
+=head1 REVISION
+
+$Id$
+
 
 =head1 AUTHOR
 
