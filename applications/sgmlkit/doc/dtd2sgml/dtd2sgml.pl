2fbff81aae2ac43a5c2ccadff9876ee39b629742
Norman Gray <norman@ASTRO.GLA.AC.UK>
1999-05-24 11:06:45 +0000

    Rewritten, so that it generates an SGML document.
    svn path=/trunk/; revision=3896

--- applications/sgmlkit/doc/dtd2sgml/dtd2sgml.pl
+++ applications/sgmlkit/doc/dtd2sgml/dtd2sgml.pl
@@ -1,58 +1,38 @@
 #! /usr/bin/perl
 #
-# Parse a DTD and an element descriptions file, and produce a fragment
-# suitable for inclusion in a document marked up using the Starlink
-# General DTD.  The descriptions file has the format
+# Parse a DTD, and produce a
+# representation of the DTD as an SGML document.
 #
-#    element-description elementname
-#    <p>This is an element description, which refers to another
-#    element by the name of *otherelement*.
-#
-# Additionally, you can use `element-description elementname*attributename'.
-# Avoid using any other markup in the description, at present, as I
-# want to leave open the possibility of transforming the descriptions
-# file to other formats.  Perhaps it would be better to have the
-# descriptions file in SGML - that's nice, but it means that we
-# couldn't do the complete merge of the parsed DTD and the
-# descriptions just with Perl.
-#
-# The DTD parsing is due to Earl Hood's perlSGML
-# (http://www.oac.uci.edu/indiv/ehood/perlSGML.html), and the format of
-# the description file follows the description file for perlSGML's
-# dtd2html program.  The one can easily be converted into the other.
+# The DTD parsing is done by Earl Hood's perlSGML package
+# (http://www.oac.uci.edu/indiv/ehood/perlSGML.html).
 #    
 # A _much_ nicer way to do this whole thing would be to root around in a
 # document's property set.  Unfortunately, Jade doesn't support the
 # modules which are necessary (module "prolog sgml document string"
 # for class DocumentTypeDecl and friends).  Pity....
 
-$#ARGV == 1 || die "Usage: $0 dtd-file descriptions-file\n";
+$#ARGV == 0 || die "Usage: $0 dtd-file\n";
 
 $dtdfilename = $ARGV[0];
-$descripfile = $ARGV[1];
+
+$dtdecl = '
+<!element elementlist - - (element+)>
+<!element element - - (parents, content, tree?, attribute*)>
+<!element parents - - (elemref*)>
+<!element content - - (#pcdata | elemref)*>
+<!element tree - - (#pcdata)>
+<!element attribute - - (#pcdata)>
+<!element elemref - o empty>
+<!attlist elementlist sysid cdata #required>
+<!attlist element gi id #required>
+<!attlist attribute 
+  name cdata #required
+  default cdata #required
+  type cdata #implied>
+<!attlist elemref gi idref #required>
+';
 
 use SGML::DTD;
-
-open (DESCRIP, $descripfile) || die "Can't open file $descripfile to read\n";
-$entry = '';
-$currelem = '';
-while (defined($line = <DESCRIP>)) {
-    if ($line =~ /^#/) {
-	next;
-    } elsif ($line =~ /^element-description\s(\S+)/) {
-	if ($currelem ne '' && $entry ne '') {
-	    $description{$currelem} = $entry;
-	}
-	$currelem = $1;
-	$entry = '';
-    } else {
-	$entry .= $line;
-    }
-}
-if ($currelem ne '' && $entry ne '') {
-    $description{$currelem} = $entry;
-}
-close(DESCRIP);
 
 # Now parse the DTD and write the output
 open (DTDFILE, $dtdfilename) || die "Can't open DTD $dtdfilename to read\n";
@@ -62,32 +42,54 @@ $dtd->read_dtd(\*DTDFILE);
 
 @elements = $dtd->get_elements(0);
 
-foreach $e (@elements) {
-    print "<subsubsect id='elementlist.$e'>$e\n";
+print "<!doctype elementlist [$dtdecl]>\n<elementlist sysid='$dtdfilename'>\n";
 
-    $eldesc = $description{$e};
-    if (defined($eldesc)) {
-	$eldesc =~ s/\*(\S+)\*/<ref id='elementlist.$1'>/g;
-	print $eldesc;
+foreach $e (@elements) {
+    print "<element gi='$e'>\n";
+
+    @parents = $dtd->get_parents ($e);
+    print "<parents>\n";
+    foreach $pe (@parents) {
+	print "<elemref gi='", lc($pe), "'>\n";
     }
+    print "</parents>\n";
 
     @content = $dtd->get_base_children ($e, 1);
-    print "<p>Content: <code>@content</code>\n";
-    %atts = $dtd->get_elem_attr ($e);
-    $printattheader = 1;
-    foreach $a (keys(%atts)) {
-	if ($printattheader) {
-	    print "<p>Attributes:\n<dl>\n";
-	    $printattheader = 0;
-	}
-	$ar = $atts{$a};
-	print "<dt>$a<dd><px><code>@$ar</code></px>\n";
-	$attdesc = $description{$e.'*'.$a};
-	if (defined($attdesc)) {
-	    $attdesc =~ s/<p>/<px>/g;
-	    $attdesc =~ s/\*(\S+)\*/<ref id='elementlist.$1'>/g;
-	    print $attdesc;
+    print "<content>";
+    foreach $ce (@content) {
+	$ce = lc($ce);
+	if ($ce =~ /[a-z]+/) {
+	    print "<elemref gi='$ce'>";
+	} else {
+	    print "$ce ";
 	}
     }
-    print "</dl>\n" unless $printattheader;
+    print "</content>\n";
+
+    print "<tree>\n";
+    $dtd->print_tree($e, 2);
+    print "</tree>\n";
+
+    # FIXME -- following doesn't deal with notations properly
+    %atts = $dtd->get_elem_attr ($e);
+    foreach $a (keys(%atts)) {
+	$ar = $atts{$a};
+	@values = @$ar;
+	$def = lc($values[0]);
+	shift (@values);
+	print "<attribute name='$a' default='$def'";
+	if ($def eq '#fixed') {
+	    $type = $values[$#values];
+	    pop(@values);
+	    print " type='$type'";
+	} else {
+	    if (SGML::DTD->is_attr_keyword ($values[0])) {
+		print " type='", lc($values[0]), "'";
+		shift (@values);
+	    }
+	}
+	print ">@values</attribute>\n";
+    }
+    print "</element>\n";
 }
+print "</elementlist>\n";
