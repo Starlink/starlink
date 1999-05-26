026c80cdccc390973a059aacdf47851eea6175f8
Mark Taylor <m.b.taylor@bristol.ac.uk>
1999-05-26 12:06:47 +0000

    Changed format of AST file to contain modifiers.
    svn path=/trunk/; revision=3946

--- applications/ccdpack/tasks/astimp.f
+++ applications/ccdpack/tasks/astimp.f
@@ -20,22 +20,21 @@
 *        The global status.
 
 *  Description:
-*     This task reads AST FrameSet information from a suitable file 
+*     This task reads alignment information from an AST file
 *     and uses it to modify the WCS components of the given NDFs.
 *     A frame in a new domain is added (the same for each NDF) within
-*     which a set of NDFs can be aligned.  This alignment may be used to 
-*     assist FINDOFF in registering the images, or as a substititute
-*     for performing registration by matching objects.  The newly 
-*     added domain is made the Current domain of the WCS components.
-*
-*     AST files for use by this application are intended to be those 
-*     written by the ASTEXP application, but with care frameset files
-*     written from other applications or doctored by hand could be used.
+*     which a set of NDFs can be aligned.  The newly added domain
+*     is made the Current domain of the WCS components.
 *
 *     If the WCS component of the NDF has a frame whose Domain has
 *     the same value as that of the Current frame of the frameset 
 *     being imported, it will be overwritten, and a warning message
 *     issued.
+*
+*     AST files for use by this application will normally be those 
+*     written by the ASTEXP application, and may either be standard
+*     ones designed for use with a particular instrument, or prepared
+*     by the user.
 
 *  Usage:
 *     ASTIMP in astfile indomain
@@ -56,7 +55,9 @@
 *        The name of a FITS header whose value is an angle through 
 *        which all the imported frames should be rotated.  This 
 *        rotation is done after the mappings given in the AST file 
-*        itself have been applied.
+*        itself have been applied.  If this parameter is supplied it
+*        overrides any value given in the modification parts of the
+*        AST file.
 *        [!]
 *     IN = LITERAL (Read)
 *        A list of NDF names whose WCS components are to be modified 
@@ -70,7 +71,7 @@
 *        each NDF, what its index number is.  Thus if only one NDF is
 *        given in the IN list, and the value of INDICES is [3], then
 *        the frameset with the identifier 'INDEX 3' will be chosen. 
-*        If set null (!) a the NDFs will be considered in the order
+*        If set null (!) the NDFs will be considered in the order
 *        1,2,3,... which will be appropriate unless the NDFs are being 
 *        presented in a different order from that in which they were 
 *        presented to ASTEXP when generating the AST file.
@@ -118,11 +119,13 @@
 *        "camera.ast" has previously been written using ASTEXP with
 *        the parameter ASTFILE=camera.ast.  A new frame with a Domain 
 *        called "OBS1" is added to the WCS component of each NDF.
+*
 *     astimp "data3,data4" instrum.ast obs1 indices=[3,4]
 *        This imports frameset information from the AST file instrum.ast
 *        which was written by ASTEXP with the IDTYPE parameter set to
 *        INDEX.  In this case NDFs of only the third and fourth types
 *        described in that file are being modified.
+*
 *     astimp astfile=instrum.ast in=! logto=terminal accept
 *        This will simply report on the framesets contained within
 *        the AST file "instrum.ast", writing the ID of each to the 
@@ -139,30 +142,71 @@
 *        are to be undertaken.  The format of the file is explained
 *        here.
 *
-*        The AST file consists of a sequence of framesets.  Each
-*        frameset has an ID, and contains two frames (a Base frame and
-*        a Current frame) and a mapping between them.  The domains of
-*        all the Base frames should normally be the same, and likewise
-*        for all the Current frames.  For the NDFs to which the file
-*        will be applied by ASTIMP, their WCS components should contain
-*        frames in the same domain as the AST file's Base frame.
+*        The AST file consists of the following, in order:
 *
-*        The ID of each frameset is used to determine, for each NDF,
-*        which of the framesets in the file should be applied to it.
-*        This ID is a string which can assume one of the followin forms:
+*           <global modifiers>
+*           (blank line)
+*           <frameset 1>
+*           <frameset 1 modifiers>
+*           (blank line)
+*           <frameset 1>
+*           <frameset 2 modifiers>
+*           (blank line)
+*             ...
+*           (end of file)
 *
-*        -  "FITSID KEY VALUE"
-*              This will match an NDF if the first FITS header card
-*              with the keyword KEY has the value VALUE.  If the value
-*              is of type CHARACTER it must be in single quotes. KEY
-*              may be compound (of the form keyword1.keyword2 etc) to 
-*              permit reading of hierarchical keywords.
+*        Characters after a '#' character are normally ignored.  The
+*        constituent parts are composed as follows:
 *
-*        -  "INDEX N"
-*              This associates a frameset with an integer N.  Usually
-*              N will take the values 1,2,3,... for the framesets
-*              in the file.  Typically the N'th NDF in a list will
-*              match the one with an ID of "INDEX N".
+*        Blank line:
+*           A single blank line, which may contain spaces but no comments.
+*
+*        Frameset:
+*           The framesets are written in AST native format, as explained
+*           in SUN/210.
+*
+*           Each frameset has an ID, and contains two frames (a Base
+*           frame and a Current frame) and a mapping between them.
+*           The domains of all the Base frames should normally be the
+*           same, and likewise for all the Current frames.  For the
+*           NDFs to which the file will be applied by ASTIMP, their
+*           WCS components should contain frames in the same domain
+*           as the AST file's Base frame.
+*
+*           The ID of each frameset is used to determine, for each NDF,
+*           which of the framesets in the file should be applied to it.
+*           This ID is a string which can assume one of the following
+*           forms:
+*
+*           -  "FITSID KEY VALUE"
+*                 This will match an NDF if the first FITS header card
+*                 with the keyword KEY has the value VALUE.  If the
+*                 value is of type CHARACTER it must be in single
+*                 quotes. KEY may be compound (of the form
+*                 keyword1.keyword2 etc) to permit reading of
+*                 hierarchical keywords.
+*
+*           -  "INDEX N"
+*                 This associates a frameset with an integer N.
+*                 Usually N will take the values 1,2,3,... for the
+*                 framesets in the file.  Typically the N'th NDF in a
+*                 list will match the one with an ID of "INDEX N".
+*
+*        Modifiers:
+*           Modifiers describe additional modifications to be made
+*           to the framesets on import.  They are of the form
+*
+*              USE keyword arguments
+*
+*           Currently the only modifier defined is FITSROT, which
+*           defines the name of a FITS header which specifies how
+*           many degrees to rotate the image before use.  This
+*           rotation is carried out after the mapping defined by
+*           the frameset itself.
+*
+*           Global modifiers affect all NDFs processed with the AST
+*           file.  Frameset modifiers affect only those NDFs which
+*           correspond to their frameset.
 *
 *        Rigorous error checking of the AST file is not performed, so
 *        that unhelpful modifications to the WCS components of the
@@ -233,9 +277,12 @@
       CHARACTER * ( AST__SZCHR ) DMCUR ! Domain of Current frame
       CHARACTER * ( AST__SZCHR ) DMBAS1 ! Reference domain of Current frame
       CHARACTER * ( AST__SZCHR ) DMCUR1 ! Reference domain of Current frame
-      CHARACTER * ( CCD1__BLEN ) ID ! ID value of frameset
-      CHARACTER * ( CCD1__BLEN ) FITROT ! FITS keyword for rotation angle
       CHARACTER * ( AST__SZCHR ) INDOM ! Name to use for Current import frame
+      CHARACTER * ( CCD1__BLEN ) ID ! ID value of frameset
+      CHARACTER * ( CCD1__BLEN ) FITROT ! FITS rotation keyword
+      CHARACTER * ( CCD1__BLEN ) FITRT0 ! FITS rotation keyword global modifier
+      CHARACTER * ( CCD1__BLEN ) FITRTP ! FITS rotation keyword parameter value
+      CHARACTER * ( CCD1__BLEN ) FITRTS( MXFSET ) ! FITS rotation keyword frameset modifiers
       CHARACTER * ( DAT__SZLOC ) LOC ! HDS locator for FITS extension
       DOUBLE PRECISION ANGLR     ! Rotation angle in radians
       DOUBLE PRECISION DEGRA     ! Degrees - Radians conversion factor
@@ -246,10 +293,8 @@
       DOUBLE PRECISION ROTATE    ! Total additional angle to rotate frames
       INTEGER FCHAN              ! AST pointer to frameset file channel
       INTEGER FDAST              ! FIO file descriptor for frameset file
-      INTEGER FRCUR              ! AST pointer to Current frame of WCS frameset
       INTEGER FRMAT              ! AST pointer to Current frame of import frameset
       INTEGER FSET( MXFSET )     ! AST pointers to import framesets 
-      INTEGER FSMAP              ! AST pointer to mapping frameset
       INTEGER FSMAT              ! AST pointer to matched frameset
       INTEGER I                  ! Loop variable
       INTEGER IAT                ! Position in string
@@ -262,10 +307,8 @@
       INTEGER IWCS               ! AST pointer to WCS component of NDF
       INTEGER IX                 ! Index into frameset group
       INTEGER J                  ! Loop variable
-      INTEGER JDUP               ! Index of duplicate frame
-      INTEGER JFCUR              ! Index of current frame of import frameset
-      INTEGER JMBAS              ! Index of base frame of import frameset
-      INTEGER JWBAS              ! Index of base frame of WCS component
+      INTEGER JCUR               ! Index of current frame
+      INTEGER JMAT               ! Index of matched frameset
       INTEGER LENGTH             ! Length of FITS header cards
       INTEGER MAP                ! AST pointer to mapping frameset
       INTEGER MAPROT             ! AST pointer to rotational mapping
@@ -275,6 +318,7 @@
       LOGICAL DIFBAS             ! Base domains don't all match
       LOGICAL DIFCUR             ! Current domains don't all match
       LOGICAL DUPID              ! Duplicate ID value was generated
+      LOGICAL FITSEX             ! Does FITS extension exist
       LOGICAL MATCH              ! Whether NDF matches frameset ID
       LOGICAL THERE              ! Presence of requested item
 
@@ -307,7 +351,11 @@
 *  Create AST channel on open frameset file.
       CALL CCD1_ACHAN( FDAST, ' ', FCHAN, STATUS )
 
+*  Get global frameset modifiers from file.
+      CALL CCD1_AGTMD( FDAST, FITRT0, STATUS )
+
 *  Print header for per-frameset information.
+      CALL CCD1_MSG( ' ', ' ', STATUS )
       BUF = ' '
       BUF( 6: ) = 'N'
       BUF( 11: ) = 'Base domain'
@@ -332,6 +380,9 @@
          FSET( I ) = AST_READ( FCHAN, STATUS )
          IF ( FSET( I ) .EQ. AST__NULL ) GO TO 1
          NFSET = I
+
+*  Get frameset modifiers for this frameset from file.
+         CALL CCD1_AGTMD( FDAST, FITRTS( I ), STATUS )
 
 *  Get object ID string and frame domain names, and output to the user.
          ID = AST_GETC( FSET( I ), 'ID', STATUS )
@@ -425,7 +476,7 @@
          INDOM = ' '
       END IF
 
-*  Get list of NDF indexes for use if the frameset identifiers are of
+*  Get list of NDF indices for use if the frameset identifiers are of
 *  type 'INDEX'.  If null set to 1,2,3,...
       CALL CCD1_GISEQ( 1, 1, NNDF, INDXS, STATUS )
       CALL PAR_EXACI( 'INDICES', NNDF, INDXS, STATUS )
@@ -439,12 +490,12 @@
 
 *  Get FITS header for additional rotations.
       IF ( STATUS .NE. SAI__OK ) GO TO 99
-      CALL PAR_GET0C( 'FITSROT', FITROT, STATUS )
+      CALL PAR_GET0C( 'FITSROT', FITRTP, STATUS )
       IF ( STATUS .EQ. PAR__NULL ) THEN
-         FITROT = ' '
+         FITRTP = ' '
          CALL ERR_ANNUL( STATUS )
       END IF
-      CALL CHR_UCASE( FITROT )
+      CALL CHR_UCASE( FITRTP )
       
 *  Loop over NDFs
       DO 12 I = 1, NNDF
@@ -457,11 +508,13 @@
          
 *  Go through list of IDs to see if any match this NDF.
          MATCH = .FALSE.
+         JMAT = 0
          DO 13 J = 1, NFSET
             CALL GRP_GET( IFGRP, J, 1, ID, STATUS )
             CALL CCD1_NMID( INDF, ID, INDXS( I ), MATCH, STATUS )
             IF ( MATCH ) THEN
                FSMAT = FSET( J )
+               JMAT = J
                GO TO 2
             END IF
  13      CONTINUE
@@ -483,6 +536,15 @@
             CALL CCD1_MSG( ' ', '    Matched with frameset ID "^ID"',
      :                     STATUS )
 
+*  Map FITS extension if it can be found.
+            CALL NDF_XSTAT( INDF, 'FITS', FITSEX, STATUS )
+            IF ( FITSEX ) THEN
+               CALL NDF_XLOC( INDF, 'FITS', 'READ', LOC, STATUS )
+               CALL DAT_MAPV( LOC, '_CHAR*80', 'READ', IPFITS, NCARD,
+     :                           STATUS )
+               LENGTH = 80
+            END IF
+
 *  Get WCS component from NDF.
             CALL CCD1_GTWCS( INDF, IWCS, STATUS )
 
@@ -495,57 +557,59 @@
                DMCUR = INDOM
             END IF
 
-*  Check whether the WCS component has any frames with the same domain as
-*  the Current frame of the import frameset; if it does, remove them, 
-*  since we have to add another one of the same domain and we don't 
-*  want multiple frames with the same domain in the WCS component.
- 3          CONTINUE
-            CALL CCD1_FRDM( IWCS, DMCUR, JDUP, STATUS )
-            IF ( JDUP .NE. 0 ) THEN
-               CALL MSG_SETC( 'DOM', DMCUR )
-               CALL CCD1_MSG( ' ', 
-     :'    A frame in domain "^DOM" already exists - removing',
-     :                        STATUS )
-               CALL AST_REMOVEFRAME( IWCS, JDUP, STATUS )
-               GO TO 3
+*  Purge the WCS component of any frames in the same domain as the one
+*  which we're going to add.
+            CALL CCD1_DMPRG( IWCS, DMCUR, 0, STATUS )
+
+*  Set the Current frame of the WCS component to the one which is the
+*  Base frame of the import frameset.
+            IF ( STATUS .NE. SAI__OK ) GO TO 99
+            CALL AST_INVERT( FSMAT, STATUS )
+            DMBAS = AST_GETC( FSMAT, 'Domain', STATUS )
+            CALL AST_INVERT( FSMAT, STATUS )
+            CALL CCD1_FRDM( IWCS, DMBAS, JCUR, STATUS )
+            IF ( JCUR .EQ. 0 ) THEN
+               STATUS = SAI__ERROR
+               CALL MSG_SETC( 'DMBAS', DMBAS )
+               CALL ERR_REP( 'ASTIMP_NODMN', 
+     :         'ASTIMP: NDF does not contain frame in domain ^DMBAS',
+     :         STATUS )
             END IF
 
-*  Get a mapping between the current frame of the import frameset and 
-*  the current frame of the WCS component.  The base frames get reset
-*  by AST_CONVERT, so steps are taken to preserve them.
-            JMBAS = AST_GETI( FSMAT, 'Base', STATUS )
-            JWBAS = AST_GETI( IWCS, 'Base', STATUS )
-            FSMAP = AST_CONVERT( IWCS, FSMAT, ' ', STATUS )
-            MAP = AST_GETMAPPING( FSMAP, AST__BASE, AST__CURRENT, 
+*  Get the mapping represented by the import frameset.
+            MAP = AST_GETMAPPING( FSMAT, AST__BASE, AST__CURRENT, 
      :                            STATUS )
-            CALL AST_SETI( IWCS, 'Base', JWBAS, STATUS )
-            CALL AST_SETI( FSMAT, 'Base', JMBAS, STATUS )
+
+*  Use modifiers if they have been specified.  If a parameter value has
+*  been given use that, otherwise use a value specific to this frameset,
+*  otherwise use a global value for the AST file.  Otherwise use nothing.
+*  Currently only one modifier is implemented, FITSROT.
+            IF ( FITRTP .NE. ' ' ) THEN
+               FITROT = FITRTP
+            ELSE IF ( FITRTS( JMAT ) .NE. ' ' ) THEN
+               FITROT = FITRTS( JMAT )
+            ELSE IF ( FITRT0 .NE. ' ' ) THEN
+               FITROT = FITRT0
+            ELSE
+               FITROT = ' '
+            END IF
 
 *  Find additional rotation from FITS header if required.
             FROT = 0D0
             IF ( FITROT .NE. ' ' ) THEN
 
-*  Map FITS extension if it can be found.
-               CALL NDF_XSTAT( INDF, 'FITS', THERE, STATUS )
-               IF ( .NOT. THERE ) THEN
+*  Error if no FITS extension.
+               IF ( .NOT. FITSEX ) THEN
                   STATUS = SAI__ERROR
                   CALL NDF_MSG( 'NDF', INDF )
                   CALL ERR_REP( 'ASTIMP_NOFITS', 
      :                          '  No FITS exension in ^NDF', STATUS )
                   GO TO 99
                END IF
-               CALL NDF_XLOC( INDF, 'FITS', 'READ', LOC, STATUS )
-               CALL DAT_MAPV( LOC, '_CHAR*80', 'READ', IPFITS, NCARD,
-     :                        STATUS )
-               LENGTH = 80
 
 *  Get FITSROT key value.
                CALL FTS1_GKEYD( NCARD, %VAL( IPFITS ), 1, FITROT, THERE,
      :                          FROT, ICARD, STATUS, %VAL( LENGTH ) )
-
-*  Unmap array and release locator for FITS extension.
-               CALL DAT_UNMAP( LOC, STATUS )
-               CALL DAT_ANNUL( LOC, STATUS )
             END IF
 
 *  Add additional fixed rotation.
@@ -564,6 +628,12 @@
                CALL MSG_SETR( 'ANGLE', REAL( ROTATE ) )
                CALL CCD1_MSG( ' ', 
      :'    Rotating additional ^ANGLE degrees', STATUS )
+            END IF
+
+*  Unmap array and release locator for FITS extension if required.
+            IF ( FITSEX ) THEN
+               CALL DAT_UNMAP( LOC, STATUS )
+               CALL DAT_ANNUL( LOC, STATUS )
             END IF
 
 *  Error occurred - abort.
@@ -598,8 +668,7 @@
 
 *  Write the WCS component back to the NDF and inform the user.
                CALL NDF_PTWCS( IWCS, INDF, STATUS ) 
-               CALL MSG_SETC( 'DOM', 
-     :                        AST_GETC( FSMAT, 'Domain', STATUS ) )
+               CALL MSG_SETC( 'DOM', DMCUR )
                CALL CCD1_MSG( ' ',
      :         '    New frame in domain "^DOM" added', STATUS )
             END IF
