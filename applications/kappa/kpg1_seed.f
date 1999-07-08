967d0e019481b09c4bf1cf20a49c0c248ea9f027
David S. Berry <d.berry@jach.hawaii.edu>
1999-07-08 09:37:20 +0000

    *** empty log message ***
    svn path=/trunk/; revision=4379

--- applications/kappa/kpg1_seed.f
+++ applications/kappa/kpg1_seed.f
@@ -13,8 +13,8 @@
 *     RESULT = KPG1_SEED( STATUS )
 
 *  Description:
-*     This function uses the computer time from an arbitrary date to
-*     generate a non-repeatable seed.
+*     This function uses the computer time from an arbitrary date, plus
+*     the current process id., to generate a non-repeatable seed.
 
 *  Arguments:
 *     STATUS = INTEGER (Given and Returned)
@@ -28,11 +28,14 @@
 *  [optional_function_items]...
 *  Authors:
 *     MJC: Malcolm J. Currie (STARLINK)
+*     DSB: David S. Berry (STARLINK)
 *     {enter_new_authors_here}
 
 *  History:
 *     1992 March 17 (MJC):
 *        Original version.
+*     8-JUL-1999 (DSB):
+*        Include process ID in initial seed, as well as the time.
 *     {enter_changes_here}
 
 *  Bugs:
@@ -56,16 +59,49 @@
 
 *  Local Variables:
       INTEGER NTICKS             ! Number of computer clock ticks
+      INTEGER PID                ! Process id.
+      LOGICAL DONE               ! Has seed been set before?
+      REAL SEED                  ! The returned seed
 
+*  Ensure DONE is set false on the first call to this routine for 
+*  each process. Ensure the flag and seed values are retained for 
+*  future invocations.
+      DATA DONE /.FALSE./
+      SAVE DONE, SEED
 *.
 
 *  Check the inherited status.
       IF ( STATUS .NE. SAI__OK ) RETURN
 
-*  Obtain the computer time since an arbitrary epoch.
-      CALL PSX_TIME( NTICKS, STATUS )
+*  If the seed has already been set, just update the previous seed by a 
+*  random amount.
+      IF( DONE ) THEN
+         SEED = 2*SEED*SLA_RANDOM( SEED )
 
-*  Generate the seed as an odd number (of seconds since midnight).
-      KPG1_SEED = REAL( MOD( NTICKS, NORM ) * 2 + 1 )
+*  If this is the first time the seed has been set in this process...
+      ELSE
+
+*  Obtain the computer time since an arbitrary epoch, in seconds.
+         CALL PSX_TIME( NTICKS, STATUS )
+
+*  Obtain the process id as an integer.
+         CALL PSX_GETPID( PID, STATUS )
+
+*  Generate the seed as an odd number. The process id is included because
+*  the ticks are in seconds. On modern machines the applicatiom may be
+*  called several times each second, and so the same seed would be used
+*  if it depended on NTICKS alone. Including the PID causes different 
+*  seeds to be used each time because the PIDs will be different. If the
+*  application is run repeatedly in the same process (eg ICL), then the
+*  seed will be set in the above (DONE=.TRUE.) block.
+         SEED = REAL( ( MOD( NTICKS, NORM ) + PID )* 2 + 1 )
+
+*  Indicate the seed has been set.
+         DONE = .TRUE.
+
+      END IF
+
+*  Return the seed value
+      KPG1_SEED = SEED
 
       END
