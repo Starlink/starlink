6fb3a8a20447c18776946970ea517996c9f8715e
Tim Jenness <t.jenness@jach.hawaii.edu>
1999-07-29 21:52:29 +0000

    Rewrite header to Starlink standard. Add comments. Add status checking.
    svn path=/trunk/; revision=4453

--- applications/surf/sculib/sculib_ranged.f
+++ applications/surf/sculib/sculib_ranged.f
@@ -1,42 +1,85 @@
-C+
-      SUBROUTINE SCULIB_RANGED(ARRAY,IST,IEN,VMAX,VMIN)
-C
-C     S C U L I B _ R A N G E D
-C
-C     Finds the maximum and minimum values in a double precision array.
-C
-C     Parameters -  (">" input, "<" output)
-C
-C     (>) ARRAY     (Double precision array ARRAY(IEN) - or more) Array
-C                   containing the values to be checked.
-C     (>) IST       (Integer) The first element of ARRAY to be
-C                   examined.
-C     (>) IEN       (Integer) The last element of ARRAY to be
-C                   examined.
-C     (<) VMAX      (Double precision) The maximum value of those examined.
-C     (<) VMIN      (Double precision) The minimum value of those examined.
-C
-C                                      JFL / ROE 20 Sept 1991 - adapted from
-C                                      GEN_RANGEF by Keith Shortridge
-C+
-      IMPLICIT NONE
-C
-C     Parameters
-C
-      INTEGER IST,IEN
-      DOUBLE PRECISION ARRAY(IEN),VMIN,VMAX
-C
-C     Local variables
-C
+      SUBROUTINE SCULIB_RANGED(ARRAY,IST,IEN,VMAX,VMIN, STATUS)
+*+
+*  Name:
+*     SCULIB_RANGED
+
+*  Purpose:
+*     Finds the maximum and minimum values in a double precision array.
+
+*  Language:
+*     Starlink Fortran 77
+ 
+*  Invocation:
+*     CALL SCULIB_RANGED( ARRAY, IST, IEN, VMAX, VMIN, STATUS)
+
+*  Arguments:
+*     ARRAY( IEN ) = DOUBLE PRECISION (Given)
+*        Array containing the values to be checked.
+*     IST = INTEGER (Given)
+*        The first element of ARRAY to be examined.
+*     IEN = INTEGER (Given)
+*        The last element of ARRAY to be examined.
+*     VMAX = DOUBLE PRECISION (Returned)
+*        The maximum value of those examined
+*     VMIN = DOUBLE PRECISION (Returned)
+*        The minumum value of those examined
+*     STATUS = INTEGER (Given & Returned)
+*        Global status
+
+*  Authors:
+*     Keith Shortridge (AAO)
+*     John Lightfoot (ROE)
+*     Tim Jenness (JAC)
+
+*  History:
+*     $Log$
+*     Revision 1.2  1999/07/29 21:52:29  timj
+*     Rewrite header to Starlink standard. Add comments. Add status checking.
+*
+*     20 Sep 1991 (JFL)
+*        Adapt from GEN_RANGEF by Keith Shortridge
+
+*-
+
+*  Type Definitions:
+      IMPLICIT NONE                              ! No implicit typing
+ 
+*  Global Constants:
+      INCLUDE 'SAE_PAR'                          ! Standard SAE constants
+
+*  Arguments Given:
+      INTEGER IEN
+      INTEGER IST
+      DOUBLE PRECISION ARRAY(IEN)
+
+*  Arguments Returned:
+      DOUBLE PRECISION VMIN
+      DOUBLE PRECISION VMAX
+
+*  Global Status:
+      INTEGER STATUS
+
+*  Local Variables:
       INTEGER I
-C
+
+*.
+
+      IF (STATUS .NE. SAI__OK) RETURN
+
+*     Retrieve the first reference value
       VMIN=ARRAY(IST)
+
+*     Set the max to min
       VMAX=VMIN
+
+*     Check that the start position is less than the end position
       IF (IST.LT.IEN) THEN
+
+*     Loop over all elements, comparing
          DO I=IST+1,IEN
             VMIN=MIN(VMIN,ARRAY(I))
             VMAX=MAX(VMAX,ARRAY(I))
          END DO
       END IF
-C
+
       END
