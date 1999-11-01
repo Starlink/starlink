e905fe7ece813d038ee87eca25e4f152f9dad507
Mark Taylor <m.b.taylor@bristol.ac.uk>
1999-11-01 19:33:57 +0000

    Initial revision
    svn path=/trunk/; revision=5240

---
+++ applications/ccdpack/main/ccd1_proff.f
@@ -0,0 +1,136 @@
+      SUBROUTINE CCD1_PROFF( NN, USE, XOFF, YOFF, FRAMES, USEFRM, 
+     :                       STATUS )
+*+
+*  Name:
+*     CCD1_PROFF
+
+*  Purpose:
+*     Outputs X and Y offsets through the logging system.
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Invocation:
+*     CALL CCD1_PROFF( NN, USE, XOFF, YOFF, FRAMES, USEFRM, STATUS )
+
+*  Description:
+*     This routine prints out a set of X and Y offsets.  Output is only
+*     made for the pairs for which the logical array USE is true.  If
+*     USEFRM is set TRUE then output will be in a format determined by
+*     frame in question (using AST_FORMAT, so determined by the 
+*     Format(axis) attributes of the frame).  If the USEFRM
+*     argument is .FALSE. then output will simply be in pixels.
+
+*  Arguments:
+*     NN = INTEGER (Given)
+*        The number of offset pairs.
+*     USE( NN ) = LOGICAL (Given)
+*        Whether to use the offset pair of this index.
+*     XOFF( NN ) = DOUBLE PRECISION (Given)
+*        The X offsets to be output.
+*     YOFF( NN ) = DOUBLE PRECISION (Given)
+*        The Y offsets to be output.
+*     FRAMES( NN ) = INTEGER (Given)
+*        AST pointers to the Frame in which the offsets are given (not 
+*        accessed if USEFRM is FALSE).
+*     USEFRM = LOGICAL (Given)
+*        If TRUE, output coordinates as per FRAMES, otherwise output in 
+*        pixels.
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
+
+*  Notes:
+*     - this routine writes information using the CCDPACK logfile
+*     system, so this must be opened first.
+
+*  Authors:
+*     MBT: Mark Taylor (STARLINK)
+*     {enter_new_authors_here}
+
+*  History:
+*     1-NOV-1999 (MBT):
+*        Original version.
+*     {enter_further_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+      
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'PRM_PAR'          ! PRIMDAT constants
+      INCLUDE 'AST_PAR'          ! AST system constants
+      INCLUDE 'MSG_PAR'          ! Message system constants
+
+*  Arguments Given:
+      INTEGER NN
+      LOGICAL USE( NN )
+      DOUBLE PRECISION XOFF( NN )
+      DOUBLE PRECISION YOFF( NN )
+      INTEGER FRAMES( NN )
+      LOGICAL USEFRM
+
+*  Status:
+      INTEGER STATUS             ! Global status
+
+*  Local Variables:
+      CHARACTER * ( MSG__SZMSG ) BUFFER ! Output message string
+      CHARACTER * ( MSG__SZMSG ) XSTR ! Formatted X coordinate
+      CHARACTER * ( MSG__SZMSG ) YSTR ! Formatted Y coordinate
+      INTEGER FRM                ! AST frame to use for formatting
+      INTEGER I                  ! Loop variable
+      INTEGER IAT                ! Position in string
+      INTEGER XS                 ! X-offset position in o/p string
+      INTEGER YS                 ! Y-offset position in o/p string
+*.
+
+*  Check inherited global status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  Write a header about the units.
+      CALL CCD1_MSG( ' ', ' ', STATUS )
+      IF ( USEFRM ) THEN
+         CALL CCD1_MSG( ' ', 
+     :'  Offset units are in image Current coordinate system:', STATUS )
+      ELSE
+         CALL CCD1_MSG( ' ', 
+     :'  Offset units are in pixels:', STATUS )
+      END IF
+
+*  Construct a header string.
+      BUFFER = '  List'
+      IAT = VAL__SZI + 4
+      BUFFER( IAT: ) = '     X-offset '
+      XS = IAT + 2
+      IAT = IAT + VAL__SZD + 4
+      BUFFER( IAT: ) = '     Y-offset '
+      YS = IAT + 2
+      CALL CCD1_MSG( ' ', ' ', STATUS )
+      CALL CCD1_MSG( ' ', BUFFER, STATUS )
+
+*  Now write out the offsets for the selected pairs.
+       DO I = 1, NN
+         IF ( USE( I ) )  THEN
+            BUFFER = ' '
+            CALL CHR_ITOC( I, BUFFER( 5: ), IAT )
+            IAT = 5 + IAT
+            BUFFER( IAT : IAT ) = ')'
+            IF ( USEFRM ) THEN
+               FRM = FRAMES( I )
+            ELSE
+               FRM = AST__NULL
+            END IF
+            CALL CCD1_XYFMT( XOFF( I ), YOFF( I ), FRM, XSTR, YSTR,
+     :                       STATUS )
+            BUFFER( XS: ) = XSTR
+            BUFFER( YS: ) = YSTR
+            CALL CCD1_MSG( ' ', BUFFER, STATUS )
+         END IF
+      END DO 
+
+      END
+* $Id$
---
+++ applications/ccdpack/main/ccd1_xyfmt.f
@@ -0,0 +1,106 @@
+      SUBROUTINE CCD1_XYFMT( XC, YC, FRAME, XSTR, YSTR, STATUS )
+*+
+*  Name:
+*     CCD1_XYFMT
+
+*  Purpose:
+*     Formats an X and Y value according to a given AST frame.
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Invocation:
+*     CALL CCD1_XYFMT( XC, YC, FRAME, XSTR, YSTR, STATUS )
+
+*  Description:
+*     This routine returns strings representing the value of an X and
+*     a Y coordinate, in a format dicated by the FRAME parameter.  
+*     If FRAME is an AST pointer to a Frame object, then the
+*     characteristics of this frame will determine the formatting.
+*     If it is set to AST__NULL, the formatting will be done in a 
+*     default way.
+
+*  Arguments:
+*     XC = DOUBLE PRECISION (Given)
+*        X value.
+*     YC = DOUBLE PRECISION (Given)
+*        Y value.
+*     FRAME = INTEGER (Given)
+*        AST pointer to a Frame object which governs the transformation,
+*        or the value AST__NULL for default formatting.
+*     XSTR = CHARACTER * ( * ) (Returned)
+*        String containing the formatted X value.
+*     YSTR = CHARACTER * ( * ) (Returned)
+*        String containing the formatted Y value.
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
+
+*  Authors:
+*     MBT: Mark Taylor (STARLINK)
+*     {enter_new_authors_here}
+
+*  History:
+*     1-NOV-1999 (MBT):
+*        Original version.
+*     {enter_further_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+      
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'AST_PAR'          ! AST system constants
+
+*  Arguments Given:
+      DOUBLE PRECISION XC
+      DOUBLE PRECISION YC
+      INTEGER FRAME
+      CHARACTER * ( * ) XSTR
+      CHARACTER * ( * ) YSTR
+
+*  Status:
+      INTEGER STATUS             ! Global status
+
+*  Local Variables:
+      INTEGER FRM                ! AST frame to use for formatting
+*.
+
+*  Check inherited global status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  Begin a new AST context.
+      CALL AST_BEGIN( STATUS )
+
+*  Construct a suitable frame to control the formatting.
+
+*  If we did not get a valid frame in the first place, construct a
+*  default one.
+      IF ( FRAME .EQ. AST__NULL ) THEN
+         FRM = AST_FRAME( 2, 'Unit(1)=pixels,Unit(2)=pixels', STATUS )
+
+*  If we did get a frame, tweak it to give the output as we want.
+      ELSE
+         FRM = AST_COPY( FRAME, STATUS )
+         IF ( AST_ISASKYFRAME( FRM, STATUS ) ) THEN
+            CALL AST_SETC( FRM, 'Format(1)', '+zidmst.3', STATUS )
+            CALL AST_SETC( FRM, 'Format(2)', '+zidmst.3', STATUS )
+         ELSE 
+            CALL AST_SETC( FRM, 'Format(1)', '%+14.6f', STATUS )
+            CALL AST_SETC( FRM, 'Format(2)', '%+14.6f', STATUS )
+         END IF
+      END IF
+
+*  Now use the frame we have to do the actual formatting.
+      XSTR = AST_FORMAT( FRM, 1, XC, STATUS )
+      YSTR = AST_FORMAT( FRM, 2, YC, STATUS )
+
+*  Exit AST context.
+      CALL AST_END( STATUS )
+
+      END
+* $Id$
