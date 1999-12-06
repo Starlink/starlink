9eee1f34860e1c8dae2178372facad96b338dc28
Mark Taylor <m.b.taylor@bristol.ac.uk>
1999-12-06 19:53:12 +0000

    Added some globals.
    svn path=/trunk/; revision=5530

--- applications/scb/tag.h
+++ applications/scb/tag.h
@@ -5,6 +5,11 @@
       void uadd( char *item );
       char *ucontent();
 
+/* Functions for keeping track of whitespace-type characters to be put
+   into yylval but not yytext. */
+      void sappend( char *s );
+      void cappend( char c );
+
 /* Configuration flags. */
       int strict;
 
@@ -17,6 +22,30 @@
          struct element *next;
       };
       typedef struct element ELEMENT;
+
+/*
+*  These definitions set up variables to be used for passing text to
+*  the parser.  When each token is returned, as well as the matched
+*  characters an extra string is prepended to them as follows:
+*
+*     preval
+*        The text to precede the next yylval.
+*     preleng  
+*        The length of preval, excluding the terminating NULL.
+*     prealloc
+*        The space allocated for preval, excluding the terminating NULL.
+*
+*  The routines which modify these keep the amount of space allocated
+*  and the length values up to date.
+*/
+   char *preval;
+   int preleng;
+   int prealloc;
+
+#define BUFINC 1024      /* Amount by which to increase buffer each time.
+                            Affects only time/memory efficiency. */
+
+
 
 
 /* $Id$ */
