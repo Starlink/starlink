ff0ed7e58292bddb01dee61df6a61c5d30e2656d
Mark Taylor <m.b.taylor@bristol.ac.uk>
2000-01-05 15:01:45 +0000

    Modified somewhat. Probably working now apart from line breaks.
    svn path=/trunk/; revision=5618

--- applications/extreme/inscnf.c
+++ applications/extreme/inscnf.c
@@ -1,14 +1,14 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <stddef.h>
 
 #define MAXNEST 100
+#define LBUFSIZ 2000
 
 
 /* Local function prototypes. */
-   void outchar( char c, int careful );
-   void outstr( char *string, int careful );
-   void outinit();
+   void outbuf( char *buffer, char *interp[], int leng );
 
 
 /* Main function of program. */
@@ -17,22 +17,24 @@
 /* Declare local variables. */
       char *cpc;
       char *cpo;
+      char *interp[ LBUFSIZ ];
       char *name;
       char *usagef;
+      char buffer[ LBUFSIZ ];
       char c;
-      char cchar;
 
       int column;
       int hadval;
+      int leng;
       int level;
-      int scol[ MAXNEST ];
+      int spotcpv;
       int spotval;
       int valat[ MAXNEST ];
 
 /* Get name of program etc. */
       name = *(argv++);
       argc--;
-      usagef = "Usage: %s [-d[l][y]] [-s] [ in [ out ] ]\n";
+      usagef = "Usage: %s [-s] [ in [ out ] ]\n";
    
 /* Open standard input and output appropriately according to command line
    arguments, in the normal filter-type way. */
@@ -57,108 +59,175 @@
 /* Set up strings for output. */
       cpo = " CNF_PVAL(";
       cpc = " )";
-      cchar = ':';
 
 /* Initialise values for source code processing. */
+      leng = 0;
       level = 0;
-      hadval = 0;
-      scol[ 0 ] = 0;
       column = 0;
-
-/* Do any necessary setup for output. */
-      outinit();
+      spotval = 0;
+      spotcpv = 0;
 
 /* Cycle through characters of source file. */
-      while( ( c = getchar() ) != EOF ) {
+      while ( ( c = getchar() ) != EOF ) {
+
+/* Increment source column and buffer position. */
+         leng++;
          column++;
-         if ( column == 6 && ( c == ' ' || c == '0' ) ) {
+
+/* Copy character to output buffer. */
+         buffer[ leng - 1 ] = c;
+         interp[ leng - 1 ] = NULL;
+
+/* If a new line is beginning, or it's a comment, then the previous line
+   must have finished. */
+         if ( ( column == 6 && strchr( " 0", c ) ) 
+           || ( column == 1 && strchr( "*cCdD!", c ) ) ) {
+
+/* Flush the buffer. */
+            outbuf( buffer, interp, leng );
+
+/* Reset values. */
+            leng = 0;
             level = 0;
-            hadval = 0;
+            spotval = 0;
+            spotcpv = 0;
          }
-         if ( scol[ level ] <= 6 && column > 6 && ! isspace( c ) )
-            scol[ level ] = column;
 
-         outchar( c, hadval );
+/* Significant part of source card. */
+         else if ( column > 6 && column < 73 ) {
 
-/* Act appropriately for each character. */
-         switch( c ) {
-            case '(':
-               scol[ ++level ] = 0;
-               valat[ level ] = 0;
-               if ( spotval == 4 ) {
-                  hadval = 1;
-                  outstr( cpo, hadval );
-                  valat[ level ] = 1;
-               }
-               break;
-            case ')':
-               if ( valat[ level ] )
-                  outstr( cpc, hadval );
-               --level;
-               break;
-            case '%':
-               spotval = 1;
-               break;
-            case 'v':
-            case 'V':
-               if ( spotval == 1 ) spotval++;
-               break;
-            case 'a':
-            case 'A':
-               if ( spotval == 2 ) spotval++;
-               break;
-            case 'l':
-            case 'L':
-               if ( spotval == 3 ) spotval++;
-               break;
-            case ' ':
-            case '\n':
-            case '\t':
-               break;
-            default:
-               spotval = 0;
+/* Act appropriately for each significant character. */
+            switch( c ) {
+
+               case '(':
+                  level++;
+                  valat[ level ] = 0;
+                  if ( spotval == 4 ) valat[ level ] = leng;
+                  if ( spotcpv == 8 ) valat[ level - 1 ] = 0;
+                  spotval = spotcpv = 0;
+                  break;
+               case ')':
+                  if ( valat[ level ] ) {
+                     interp[ valat[ level ] - 1 ] = cpo;
+                     interp[ leng - 1 ] = cpc;
+                  }
+                  level--;
+                  spotval = spotcpv = 0;
+                  break;
+
+               case '%':
+                  spotval = 1;
+                  spotcpv = 0;
+                  break;
+               case 'v':
+               case 'V':
+                  spotval = ( spotval == 1 ) ? 2 : 0;
+                  spotcpv = ( spotcpv == 5 ) ? 6 : 0;
+                  break;
+               case 'a':
+               case 'A':
+                  spotval = ( spotval == 2 ) ? 3 : 0;
+                  spotcpv = ( spotcpv == 6 ) ? 7 : 0;
+                  break;
+               case 'l':
+               case 'L':
+                  spotval = ( spotval == 3 ) ? 4 : 0;
+                  spotcpv = ( spotcpv == 7 ) ? 8 : 0;
+                  break;
+
+               case 'c':
+               case 'C':
+                  spotcpv = ( spotcpv == 0 ) ? 1 : 0;
+                  spotval = 0;
+                  break;
+               case 'n':
+               case 'N':
+                  spotcpv = ( spotcpv == 1 ) ? 2 : 0;
+                  spotval = 0;
+                  break;
+               case 'f':
+               case 'F':
+                  spotcpv = ( spotcpv == 2 ) ? 3 : 0;
+                  spotval = 0;
+                  break;
+               case '_':
+                  spotcpv = ( spotcpv == 3 ) ? 4 : 0;
+                  spotval = 0;
+                  break;
+               case 'p':
+               case 'P':
+                  spotcpv = ( spotcpv == 4 ) ? 5 : 0;
+                  spotval = 0;
+                  break;
+            
+               case ' ':
+               case '\n':
+               case '\t':
+               case '\r':
+                  break;
+
+               default:
+                  spotval = spotcpv = 0;
+            }
          }
+         if ( c == '\n' || c == '\r' ) column = 0;
       }
 
+/* Flush any remaining text in buffer. */
+      outbuf( buffer, interp, leng );
+
+/* Finish. */
+      return 0;
    }
 
 
-   int outcol;
-   int softn;
+   void outbuf( char *buffer, char *interp[], int leng ) {
+      char c;
+      static int col = 0;
+      int done;
+      int fint;
+      int i;
+      int lint;
+      int pos;
 
-   void outinit() {
-      outcol = 0;
-      softn = 0;
-   }
-
-   void outstr( char *string, int careful ) {
-      for ( ; *string; string++ )
-         outchar( *string, careful );
-   }
+      fint = 0;
+      lint = 0;
+      done = -1;
+      for ( pos = 0; pos < leng; pos++ ) {
+         if ( interp[ pos ] != NULL ) {
+            fint = 1;
+            lint += strlen( interp[ pos ] );
+         }
       
-   void outchar( char c, int careful ) {
+/* End of input card, or end of buffer.  Output text up to this point. */
+         if ( buffer[ pos ] == '\n' || pos == leng - 1 ) {
 
-/* Declare local variables. */
-      static char linebuf[ 100 ];
+/* If there are no interpolations to make, output the line undisturbed. */
+            if ( ! fint ) {
+               for ( i = done + 1; i <= pos; i++ ) 
+                  putchar( buffer[ i ] );
+            }
 
-/* If no reformatting is to be done, simply output the character as read. */
-      if ( ! ( careful || softn ) ) {
-         putchar( c );
-         if ( c == '\n' )
-            outinit();
-         else
-            outcol++;
-      }
+/* If the interpolations can be made without overflowing the source card,
+   output the line with interpolations. */
+            else if ( col + lint + pos - done < 73 ) {
+               for ( i = done + 1; i <= pos; i++ ) {
+                  putchar( buffer[ i ] );
+                  if ( interp[ i ] != NULL ) printf( interp[ i ] );
+               }
+            }
 
-/* Reformatting may be required.  In this case, keep a check that we are 
-   not over the fortran column limit, and insert appropriate continuation
-   formatting if we are.  We also try to break lines at a suitable place
-   by buffering text until a break is found. */ 
-      else {
-         putchar( c );
-         if ( c == '\n' )
-            outinit();
-         else
-            outcol++;
+/* Interpolations would overflow the source card.  We have to break the
+   line somewhere sensible. */
+            else {
+               fprintf( stderr, "** Overflow **\n" );
+               exit( 1 );
+            }
+            done = pos;
+            fint = 0;
+            lint = 0;
+         }
       }
    }
+
+
