7846edc99a5a59b127e6390f2e70c29e5c7b1977
Mark Taylor <m.b.taylor@bristol.ac.uk>
2000-01-06 15:28:14 +0000

    Looking better.  Line breaks at the right places, but not with the right indents (and some safety features missing).
    svn path=/trunk/; revision=5619

--- applications/extreme/inscnf.c
+++ applications/extreme/inscnf.c
@@ -5,31 +5,20 @@
 
 #define MAXNEST 100
 #define LBUFSIZ 2000
+#define MAXCONT 20
 
 
 /* Local function prototypes. */
    void outbuf( char *buffer, char *interp[], int leng );
+   void inscnf();
 
 
 /* Main function of program. */
    int main( int argc, char **argv ) {
 
 /* Declare local variables. */
-      char *cpc;
-      char *cpo;
-      char *interp[ LBUFSIZ ];
       char *name;
       char *usagef;
-      char buffer[ LBUFSIZ ];
-      char c;
-
-      int column;
-      int hadval;
-      int leng;
-      int level;
-      int spotcpv;
-      int spotval;
-      int valat[ MAXNEST ];
 
 /* Get name of program etc. */
       name = *(argv++);
@@ -56,6 +45,33 @@
             exit( 1 );
       }
 
+/* Filter input to output. */
+      inscnf();
+
+/* Finish. */
+      return 0;
+   }
+
+
+/* Main data processing routine. */
+   void inscnf() {
+
+/* Declare local variables. */
+      char *cpc;
+      char *cpo;
+      char *interp[ LBUFSIZ ];
+      char buffer[ LBUFSIZ ];
+      char c;
+
+      int column;
+      int comment;
+      int flush;
+      int leng;
+      int level;
+      int spotcpv;
+      int spotval;
+      int valat[ MAXNEST ];
+
 /* Set up strings for output. */
       cpo = " CNF_PVAL(";
       cpc = " )";
@@ -64,6 +80,7 @@
       leng = 0;
       level = 0;
       column = 0;
+      comment = 0;
       spotval = 0;
       spotcpv = 0;
 
@@ -78,15 +95,28 @@
          buffer[ leng - 1 ] = c;
          interp[ leng - 1 ] = NULL;
 
-/* If a new line is beginning, or it's a comment, then the previous line
-   must have finished. */
-         if ( ( column == 6 && strchr( " 0", c ) ) 
-           || ( column == 1 && strchr( "*cCdD!", c ) ) ) {
+/* Work out whether this is a suitable time to process the current contents
+   of the buffer.  Suitable is when it contains a whole source line, give
+   or take some ignorable characters near the start or finish.  This will
+   be the case when a comment line has just started or finished, or when
+   we see the start of a source line which is not a continuation line. */
+         flush = 0;
+         if ( column == 1 && strchr( "*cCdD!", c ) ) { 
+            comment = 1;
+            flush = 1;
+         }
+         if ( comment && ( c == '\n' || c == '\r' ) ) {
+            comment = 0;
+            flush = 1;
+         }
+         if ( column == 6 && strchr( " 0", c ) ) {
+            flush = 1;
+         }
 
-/* Flush the buffer. */
+/* If the buffer is ready to be processed, then process it and reset 
+   some values appropriately. */
+         if ( flush ) {
             outbuf( buffer, interp, leng );
-
-/* Reset values. */
             leng = 0;
             level = 0;
             spotval = 0;
@@ -94,7 +124,7 @@
          }
 
 /* Significant part of source card. */
-         else if ( column > 6 && column < 73 ) {
+         if ( column > 6 && column < 73 && ! comment ) {
 
 /* Act appropriately for each significant character. */
             switch( c ) {
@@ -175,57 +205,136 @@
 
 /* Flush any remaining text in buffer. */
       outbuf( buffer, interp, leng );
-
-/* Finish. */
-      return 0;
    }
 
 
    void outbuf( char *buffer, char *interp[], int leng ) {
+/*
+*+
+*     If any interpolated text is present, this routine should be called 
+*     with a single line of source code (i.e. a set of continuation lines
+*     with matched brackets and so on).  Otherwise line breaks may get
+*     put in ugly (though not incorrect) places.  If there is
+*     no interpolation to be done, then output is identical to input,
+*     so it doesn't matter.
+*-
+*/
       char c;
+      char *ls[ MAXCONT ];
+      char *pc;
+      char *qc;
+      char *we;
+      char ebuf[ LBUFSIZ ];
       static int col = 0;
       int done;
-      int fint;
       int i;
-      int lint;
-      int pos;
+      int isinterp[ MAXCONT ];
+      int indent;
+      int j;
+      int line;
+      int lines;
+      int ll[ MAXCONT ];
+      int level;
+      int started;
 
-      fint = 0;
-      lint = 0;
-      done = -1;
-      for ( pos = 0; pos < leng; pos++ ) {
-         if ( interp[ pos ] != NULL ) {
-            fint = 1;
-            lint += strlen( interp[ pos ] );
+/* Start of source line, so it shouldn't be within the argument of a %VAL. */
+
+/* Copy the input buffer, with interpolations, into an expanded buffer.
+   Identify line starts, line lengths, and whether each line contains any 
+   interpolations. */
+      line = 0;
+      qc = ebuf;
+      isinterp[ line ] = 0;
+      ls[ line ] = ebuf;
+      ll[ line ] = 0;
+      for ( i = 0; i < leng; i++ ) {
+         *(qc++) = buffer[ i ];
+         if ( interp[ i ] != NULL ) {
+            isinterp[ line ] = 1;
+            for ( pc = interp[ i ]; *pc; pc++ )
+               *(qc++) = *pc;
          }
-      
-/* End of input card, or end of buffer.  Output text up to this point. */
-         if ( buffer[ pos ] == '\n' || pos == leng - 1 ) {
+         if ( buffer[ i ] == '\n' || i == leng - 1 ) {
+            ll[ line ] = qc - ls[ line ];
+            line++;
+            isinterp[ line ] = 0;
+            ls[ line ] = qc;
+            ll[ line ] = 0;
+         }
+      }
+      lines = line + 1;
 
-/* If there are no interpolations to make, output the line undisturbed. */
-            if ( ! fint ) {
-               for ( i = done + 1; i <= pos; i++ ) 
-                  putchar( buffer[ i ] );
+/* Process expanded buffer a line at a time. */
+      for ( line = 0; line < lines; line++ ) {
+
+/* If there are no interpolations in this line, or the line including 
+   interpolations can fit on the source card, then output it unchanged. */
+         if ( ! isinterp[ line ] || col + ll[ line ] < 73 ) {
+            for ( i = 0; i < ll[ line ]; i++ ) {
+               putchar( ls[ line ][ i ] );
+               if ( ls[ line ][ i ] == '\n' ) col = 0;
+               col++;
             }
+         }
 
-/* If the interpolations can be made without overflowing the source card,
-   output the line with interpolations. */
-            else if ( col + lint + pos - done < 73 ) {
-               for ( i = done + 1; i <= pos; i++ ) {
-                  putchar( buffer[ i ] );
-                  if ( interp[ i ] != NULL ) printf( interp[ i ] );
+/* The expanded line will not fit in 72 columns.  We need to add some 
+   line breaks. */
+         else {
+
+/* The procedure is to identify 'word boundaries'; if all text up to the
+   next boundary can be output on the current line do so, otherwise 
+   introduce a line break and continue with the rest of the input.
+   A word starts at a '%' and ends at the first character at the same
+   level of bracketing which is none of ')', ',' or ' '.  */
+            pc = ls[ line ];
+            while ( pc < ls[ line ] + ll[ line ] ) {
+
+/* Find the next place a line break could occur. */
+               done = 0;
+               level = 0;
+               started = 0;
+               for ( qc = pc; qc - ls[ line ] < ll[ line ] - 1; qc++ ) {
+                  if ( *pc == '%' ) {
+                     switch ( qc[ 1 ] ) {
+                        case '(':
+                           level++;
+                           started = 1;
+                           break;
+                        case ')':
+                           level--;
+                           break;
+                        case ' ':
+                        case ',':
+                        case '\n':
+                           break;
+                        default:
+                           done = ( started && level <= 0 );
+                     }
+                  }
+                  else {
+                     done = ( qc[ 1 ] == '%' );
+                  }
+                  if ( done ) break;
+               }
+               for ( we = qc; *we == ' ' && we >= pc; we-- );
+
+/* If this word won't fit on the current line, output a line break now. */
+               if ( we - pc + col > 73 ) {
+                  putchar( '\n' );
+                  indent = 6;
+                  for ( i = 1; i <= indent; i++ )
+                     putchar( i == 6 ? ':' : ' ' );
+                  col = indent;
+               }
+
+/* Now output the word. */
+               while ( pc <= qc ) {
+                  putchar( *pc );
+                  if ( *pc == '\n' ) col = 0;
+                  pc++;
+                  col++;
                }
             }
-
-/* Interpolations would overflow the source card.  We have to break the
-   line somewhere sensible. */
-            else {
-               fprintf( stderr, "** Overflow **\n" );
-               exit( 1 );
-            }
-            done = pos;
-            fint = 0;
-            lint = 0;
          }
       }
    }
