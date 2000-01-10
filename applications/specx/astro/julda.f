acb7949bd9dca393f014da3380bd1026ba167394
Tim Jenness <t.jenness@jach.hawaii.edu>
2000-01-10 21:10:16 +0000

    Add descriptive header
    svn path=/trunk/; revision=5635

--- applications/specx/astro/julda.f
+++ applications/specx/astro/julda.f
@@ -1,10 +1,56 @@
-C------------------------------------------------------------------------------
-
       INTEGER FUNCTION JULDA(NYR)
+*+
+*  Name:
+*     JULDA
 
-C   THIS FUNCTION COMPUTES THE JULIAN DAY NUMBER (MODIFIED) AT 12HRS U.T.
-C   ON JANUARY 0 OF THE YEAR NYR. JULDA IS AN INTEGER BECAUSE OF THIS
-C   DEFINITION.
+*  Purpose:
+*     Calculate Julian day number from year 1900
+
+*  Invocation:
+*     JULIAN_DAY = JULDA ( NYR )
+
+*  Description:
+*     This function computes the Julian day number (modified) at 12hrs U.T.
+*     on January 0 of the year nyr. JULDA is an integer because of this
+*     definition. It does not use standard Modified Julian Date.
+*     The reference date is actually 1900 January 0 0.5UT = Julian day
+*     2415020.0
+
+*  Arguments:
+*     NYR = INTEGER (Given)
+*       Year number. 2-digit years are treated as from 00-99 AD
+
+*  Returned Value:
+*     JULDA = INTEGER
+*        Number of days from Julian day 2415020.0
+
+*  Notes:
+*     To convert to true Modified Julian Date (MJD) add 15019.5
+
+*  Authors:
+*     Rachael Padman (MRAO, Cambridge)
+*     Tim Jenness (JAC, Hawaii)
+
+*  History:
+*     Pre-history (rp):
+*        Original version. Uses integer arithmetic
+*     30 Dec 1999 (timj):
+*        Add descriptive header
+
+*.
+
+*  Type Definitions:
+      IMPLICIT NONE
+
+*  Arguments Given:
+      INTEGER NYR
+
+*  Local Variables:
+      INTEGER IC                ! int(year / 100)
+      INTEGER JULDAT            ! Julian date
+      INTEGER NYRM1             ! Year minus 1
+
+*-
 
       NYRM1=NYR-1
       IC=NYRM1/100
@@ -13,5 +59,3 @@ C   DEFINITION.
 
       RETURN
       END
-
-C------------------------------------------------------------------------------
