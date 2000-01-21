b2cb7ddde5df3af179a86e547406a04d5890f02c
Mark Taylor <m.b.taylor@bristol.ac.uk>
2000-01-21 13:31:50 +0000

    Initial revision
    svn path=/trunk/; revision=5679

---
+++ applications/extreme/ygen.c
@@ -0,0 +1,175 @@
+/*
+*+
+*  Name:
+*     tag.c
+*
+*  Type of module:
+*     C source code.
+*
+*  Purpose:
+*     Common routines used by lex and yacc files for source code tagging.
+*
+*  Authors:
+*     MBT: Mark Taylor (STARLINK)
+*
+*  History:
+*     25-NOV-1999 (MBT):
+*        Intial version.
+*
+*-
+*/
+
+
+#include <stdio.h>
+#include <sys/file.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+#include "ygen.h"
+
+
+   void *memok( void *ptr ) {
+/*
+*+
+*  Name:
+*     memok
+*
+*  Purpose:
+*     Check that memory has been allocated successfully.
+*
+*  Description:
+*     This routine checks that a pointer does not point to NULL.  It 
+*     should be called on any pointer value which is got by a call to
+*     malloc, realloc or calloc.
+*
+*     If the argument is non-NULL then the routine returns without action.
+*     If it is NULL, the routine terminates the program with an error
+*     status.
+*
+*  Arguments:
+*     memok = void *
+*        A pointer, presumably a return value from malloc, realloc or calloc.
+*
+*  Return value:
+*     The same as the argument ptr.
+*
+*  Authors:
+*     MBT: Mark Taylor (STARLINK)
+*
+*  History:
+*     09-DEC-1999 (MBT):
+*        Initial revision.
+*-
+*/
+      if ( ptr == NULL ) {
+         fprintf( stderr, "Memory allocation failed.\n" );
+         exit( 1 );
+      }
+      return( ptr );
+   }
+
+
+
+   void sappend( char *s ) {
+/*
+*+
+*  Name:
+*     sappend
+*
+*  Purpose:
+*     Append a string to the preval string.
+*
+*  Description:
+*     This routine appends a string to the preval string.  If the preval
+*     string is not long enough to hold the new one, then more space
+*     is allocated for it.
+*
+*  Authors:
+*     MBT: Mark Taylor (STARLINK)
+*
+*  History:
+*     10-DEC-1999 (MBT):
+*        Initial revision.
+*-
+*/
+
+/* Local variables. */
+      int leng;
+
+/* See how many extra characters are to be added to preval. */
+      leng = strlen( s );
+
+/* Extend the allocated space if necessary. */
+      while ( preleng + leng > prealloc ) {
+         if ( prealloc == 0 ) {
+            preval = (char *) memok( malloc( BUFINC + 1 ) );
+            *preval = '\0';
+         }
+         else {
+            preval = (char *) memok( realloc( preval, prealloc + BUFINC + 1 ) );
+         }
+         prealloc += BUFINC;
+      }
+
+/* Append the string to preval. */
+      strcat( preval, s );
+      preleng += leng;
+   }
+
+
+   void cappend( char c ) {
+/*
+*+
+*  Name:
+*     cappend
+*
+*  Purpose:
+*     Append a character to the preval string.
+*
+*  Description:
+*     This routine appends a single character to the preval string.
+*     If the character is '<', '>' or '&', then it is replaced in the
+*     preval string by the appropriate HTML entity reference.
+*
+*  Authors:
+*     MBT: Mark Taylor (STARLINK)
+*
+*  History:
+*     10-DEC-1999 (MBT):
+*        Initial revision.
+*-
+*/
+
+/* Switch on the value of the character. */
+      switch( c ) {
+
+/* If it needs to be replaced by an entity reference, do so via sappend. */
+         case '<':
+            sappend( "&lt;" );
+            break;
+         case '>':
+            sappend( "&gt;" );
+            break;
+         case '&':
+            sappend( "&amp;" );
+            break;
+
+/* Otherwise it's just a single character: extend allocation if necessary
+   and add the new character. */
+         default:
+            if ( preleng + 1 > prealloc ) {
+               if ( prealloc == 0 )
+                  preval = (char *) memok( malloc( BUFINC + 1 ) );
+               else
+                  preval = (char *) memok( realloc( preval, 
+                                                    prealloc + BUFINC + 1 ) );
+               prealloc++;
+            }
+            preval[ preleng ] = c;
+            preval[ ++preleng ] = '\0';
+      }
+   }
+
+
+/* $Id$ */
---
+++ applications/extreme/ygen.h
@@ -0,0 +1,105 @@
+/*
+*+
+*  Name:
+*     tag.h
+*
+*  Purpose:
+*     Include file for tagging routines.
+*
+*  Description:
+*     This include file contains the shared declarations for the C code
+*     which forms the lex/yacc based source code parsers.
+*
+*  Authors:
+*     MBT: Mark Taylor (STARLINK)
+*
+*  History:
+*     10-DEC-1999 (MBT):
+*        Initial revision.
+*-
+*/
+
+/* Functions for keeping track of whitespace-type characters to be put
+   into yylval but not yytext. */
+      void sappend( char *s );
+      void cappend( char c );
+
+/* Utility functions. */
+      void *memok( void *ptr );
+
+/* Configuration flags. */
+      int strict;
+
+/* Generic linked list type. */
+      struct element {
+         char *text;
+         struct element *next;
+      };
+      typedef struct element ELEMENT;
+
+/*
+*  These definitions set up variables to be used for passing text to
+*  the parser.  When each token is returned, as well as the matched
+*  characters an extra string is prepended to them as follows:
+*
+*     preval
+*        The text to precede the next yylval.
+*     preleng  
+*        The length of preval, excluding the terminating NULL.
+*     prealloc
+*        The space allocated for preval, excluding the terminating NULL.
+*
+*  The routines which modify these keep the amount of space allocated
+*  and the length values up to date.
+*/
+   char *preval;
+   int preleng;
+   int prealloc;
+
+/* Amount by which to increase the preval buffer each time it needs
+   increasing.  Its value affects only time/memory efficiency. */
+
+#define BUFINC 4096
+
+typedef char * STRING;
+#define YYSTYPE STRING
+            
+   YYSTYPE yylval;
+
+
+#define LINE_START 257
+#define LINE_END 258
+#define BLANK_LINE 259
+#define COMMENT_LINE 260
+#define SUBROUTINE 261
+#define ENTRY 262
+#define BLOCKDATA 263
+#define PROGRAM 264
+#define FUNCTION 265
+#define INTEGER 266
+#define REAL 267
+#define DOUBLEPRECISION 268
+#define COMPLEX 269
+#define LOGICAL 270
+#define CHARACTER 271
+#define BYTE 272
+#define UBYTE 273
+#define WORD 274
+#define UWORD 275
+#define GENERIC_TYPE 276
+#define DIMENSION 277
+#define INCLUDE 278
+#define IF 279
+#define ELSEIF 280
+#define THEN 281
+#define CALL 282
+#define INTEGER_CONSTANT 283
+#define STRING_CONSTANT 284
+#define ILLEGAL_CHAR 285
+#define TOKEN 286
+#define VAL 287
+#define END 288
+#define CNF_PVAL 289
+
+
+/* $Id$ */
