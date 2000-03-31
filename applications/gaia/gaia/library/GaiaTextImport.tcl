7c8de85ee53ea5378a3d70952c08258ba264d54b
Peter W. Draper <P.W.Draper@durham.ac.uk>
2000-03-31 13:24:13 +0000

    *** empty log message ***
    svn path=/trunk/; revision=6033

--- applications/gaia/gaia/library/GaiaTextImport.tcl
+++ applications/gaia/gaia/library/GaiaTextImport.tcl
@@ -12,7 +12,7 @@
 #  Description:
 #     This class opens a text file and interprets its contents
 #     according to a series of configurable rules. The configuration
-#     covers what columns names to use, what separators are used and
+#     covers what columns names to use, what delimeters are used and
 #     which columns are coordinates. It also provides the means to
 #     convert these coordinates into those of a displayed rtdimage (if
 #     possible).
@@ -117,7 +117,30 @@ itcl::class gaia::GaiaTextImport {
       $itk_component(notebook) add -label Import
       #add_table_controls_
 
+      #  Add action buttons.
+      itk_component add actions {
+         frame $w_.actions
+      }
+
+      #  Update displayed table contents.
+      itk_component add update {
+         button $itk_component(actions).update -text Update \
+            -command [code $this update_table]
+      }
+      add_short_help $itk_component(update) \
+         {Update table to reflect current state}
+      pack $itk_component(actions) -side top -fill x 
+      pack $itk_component(update) -side left -expand 1 -pady 3 -padx 3
+
+      #  View the first page.
       $itk_component(notebook) view 0
+
+      #  Run-time initialisations
+      set_separated_
+      set values_($this,ra) -1
+      set values_($this,dec) -1
+      set values_($this,x) -1
+      set values_($this,y) -1
    }
 
    #  Destructor:
@@ -131,7 +154,7 @@ itcl::class gaia::GaiaTextImport {
    #  Set defaults for widget states.
    protected method set_defaults_ {} {
       set values_($this,separated) 1
-      set values_($this,separator) ","
+      set values_($this,delimeter) ","
       set values_($this,comment) "\#"
       set values_($this,fixwidths) {10,10,10,10,10}
    }
@@ -139,12 +162,12 @@ itcl::class gaia::GaiaTextImport {
 
    #  Add input file structure controls.
    protected method add_struct_controls_ {parent} {
-      set lwidth 17
+      set lwidth 10
 
-      #  Separator or fixed format.
+      #  Delimetered or fixed format.
       itk_component add separated {
          StarLabelCheck $parent.separated \
-            -text "Use separator (otherwise fixed width):" \
+            -text "Use delimeter (otherwise fixed width):" \
             -onvalue 1 \
             -offvalue 0 \
             -variable [scope values_($this,separated)] \
@@ -152,20 +175,25 @@ itcl::class gaia::GaiaTextImport {
       }
       pack $itk_component(separated) -side top -fill x -pady 3 -padx 3
       add_short_help $itk_component(separated) \
-         {Use a separator between fields, fixed width otherwise}
+         {Use a delimeter between fields, fixed width otherwise}
 
-      #  The separator.
-      itk_component add separator {
-         LabelEntry $parent.separator \
+      #  Set the delimeter.
+      itk_component add delimeter {
+         LabelMenu $parent.delimeter \
             -labelwidth $lwidth \
-            -text "Separator:" \
-            -textvariable [scope values_($this,separator)] \
-            -value $values_($this,separator)
+            -text "Delimeter:" \
+            -variable [scope values_($this,delimeter)]
       }
-      pack $itk_component(separator) -side top -fill x -ipadx 1m -ipady 1m
-      add_short_help $itk_component(separator) \
-         {The field separator character(s)}
-
+      pack $itk_component(delimeter) -side top -fill x -ipadx 1m -ipady 1m
+      add_short_help $itk_component(delimeter) \
+         {The field delimeter character(s)}
+      foreach {sname value} {space " " comma "," tab "\t" semicolon ";"} {
+         $itk_component(delimeter) add \
+            -label $sname \
+            -value $value \
+            -command [code $this set_delimeter_ $value]
+      }
+      set_delimeter_ { }
 
       #  Fixed width control.
       itk_component add fixwidths {
@@ -190,20 +218,29 @@ itcl::class gaia::GaiaTextImport {
       pack $itk_component(comment) -side top -fill x -ipadx 1m -ipady 1m
       add_short_help $itk_component(comment) \
          {The comment indicator character(s)}
+
+      #  How many lines to skip?
+
    }
 
 
-   #  Toggle the state of the separator fields.
+   #  Toggle the state of the delimeter fields.
    protected method set_separated_ {} {
       if { $values_($this,separated) } {
-         #  Using separator
-         $itk_component(separator) configure -state normal
+         #  Using delimeter
+         $itk_component(delimeter) configure -state normal
          $itk_component(fixwidths) configure -state disabled
       } else {
          #  Using fixed format
-         $itk_component(separator) configure -state disabled
+         $itk_component(delimeter) configure -state disabled
          $itk_component(fixwidths) configure -state normal
       }
+   }
+
+   #  Set the delimeter symbol.
+   protected method set_delimeter_ {value} {
+      set values_($this,delimeter) $value
+      puts "set delimeter to: '$value'"
    }
 
    #  Heading names and types controls.
@@ -217,40 +254,110 @@ itcl::class gaia::GaiaTextImport {
       set headparent_ [$itk_component(headregion) childsite]
    }
 
-   #  Add field for each column. Also add radio buttons to select
-   #  amongst RA/Dec and X/Y.
+   #  Add field for each column name. Also add radio buttons to select
+   #  amongst RA/Dec and X/Y. These all exist in a table.
    protected method add_headings_ {} {
-      for {set i 0} {$i < $ncolumns_} {incr i} {
+
+      itk_component add headtable {
+         frame $headparent_.headtable
+      }
+      set parent $itk_component(headtable)
+      itk_component add head0 {
+         label $parent.head0 -text "Column name"
+      }
+      itk_component add head1 {
+         label $parent.head1 -text "RA"
+      }
+      itk_component add head2 {
+         label $parent.head2 -text "Dec"
+      }
+      itk_component add head3 {
+         label $parent.head3 -text "X"
+      }
+      itk_component add head4 {
+         label $parent.head4 -text "Y"
+      }
+      table $itk_component(headtable) \
+         $itk_component(head0)  0,0 \
+         $itk_component(head1)  0,1 \
+         $itk_component(head2)  0,2 \
+         $itk_component(head3)  0,3 \
+         $itk_component(head4)  0,4
+
+
+      for {set i 0;set j 1} {$i < $ncolumns_} {incr i; incr j} {
          set values_($this,heading$i) "Col$i"
          itk_component add col$i {
-            LabelEntry $headparent_.col$i \
-               -labelwidth 17 \
-               -text "Name for column $i:" \
+            LabelEntry $parent.col$i \
+               -labelwidth 2 \
+               -text "$i:" \
                -textvariable [scope values_($this,heading$i)] \
                -value $values_($this,heading$i)
          }
-         pack $itk_component(col$i) -side top -fill x -ipadx 1m -ipady 1m
-         add_short_help $itk_component(col$i) \
-            "Name for data in column $i"
+         #add_short_help $itk_component(col$i) \
+         #   "Name for data in column $i"
+         
+         itk_component add ra$i {
+            radiobutton $parent.ra$i \
+               -variable [scope values_($this,ra)] \
+               -value $i
+         }
+         #add_short_help $itk_component(ra$i) \
+         #   "Column contains RA"
+
+         itk_component add dec$i {
+            radiobutton $parent.dec$i \
+               -variable [scope values_($this,dec)] \
+               -value $i
+         }
+         #add_short_help $itk_component(dec$i) \
+         #   "Column contains Dec"
+
+         itk_component add x$i {
+            radiobutton $parent.x$i \
+               -variable [scope values_($this,x)] \
+               -value $i
+         }
+         #add_short_help $itk_component(x$i) \
+         #   "Column contains X coordinates"
+
+         itk_component add y$i {
+            radiobutton $parent.y$i \
+               -variable [scope values_($this,y)] \
+               -value $i
+         }
+         #add_short_help $itk_component(y$i) \
+         #   "Column contains Y coordinates"
+
+         table $itk_component(headtable) \
+            $itk_component(col$i) $j,0 -fill x -pady 3 -padx 3 \
+            $itk_component(ra$i) $j,1 -fill x -pady 3 -padx 3 \
+            $itk_component(dec$i) $j,2 -fill x -pady 3 -padx 3 \
+            $itk_component(x$i) $j,3 -fill x -pady 3 -padx 3 \
+            $itk_component(y$i) $j,4 -fill x -pady 3 -padx 3
       }
+      pack $itk_component(headtable) -fill both -expand 1
+      add_short_help $itk_component(headtable) \
+         {Set column names and identify the RA/Dec, X/Y coordinates}
    }
 
    #  Remove existing headings controls.
    protected method clear_headings_ {} {
-      for {set i 0} {$i < $ncolumns_} {incr i} {
-         delete object $itk_component(col$i)
+      if { [info exists itk_component(headtable)] && 
+           [winfo exists $itk_component(headtable)] } {
+         catch {destroy $itk_component(headtable)}
       }
    }
 
    #  Start the decode cycle. Opens input file, reads first line and
-   #  decodes it (to count words), if using a separator. Otherwise
+   #  decodes it (to count words), if using a delimeter. Otherwise
    #  just counts the fixed width words.
    protected method start_process_ {args} {
 
       #  Remove old controls.
       clear_headings_
 
-      #  If using a separator.
+      #  If using a delimeter.
       if { $values_($this,separated) } {
 
          #  Open the file.
@@ -287,7 +394,7 @@ itcl::class gaia::GaiaTextImport {
          set ok 1
          set ncol 0
          while { $ok } {
-            set word [ctoken line $values_($this,separator)]
+            set word [ctoken line $values_($this,delimeter)]
             if { $word != {} } {
                incr ncol
                set words_($ncol) $word
