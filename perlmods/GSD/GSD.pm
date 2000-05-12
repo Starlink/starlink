22e26029b069c36f7db573ab7993b0cfc38e6bc0
Tim Jenness <t.jenness@jach.hawaii.edu>
2000-05-12 04:07:56 +0000

    - Add perl level gsdClose for linux problem - Slight pod fix
    svn path=/trunk/; revision=6287

--- perlmods/GSD/GSD.pm
+++ perlmods/GSD/GSD.pm
@@ -16,12 +16,48 @@ use vars qw/$VERSION @ISA @EXPORT/;
               gsdGet0d gsdGet0r gsdGet0i gsdGet0l gsdGet0c gsdGet0b
 	      gsdGet0w
 	      gsdGet1d gsdGet1r gsdGet1i gsdGet1c
-	      gsdGet1dp gsdGet1rp gsdGet1ip);
+	      gsdGet1dp gsdGet1rp gsdGet1ip
+	    );
 
 '$Revision$ ' =~ /.*:\s(.*)\s\$/ && ($VERSION = "$1");
 
 bootstrap GSD  $VERSION;
 
+
+# On linux, ran into a problem where the program segments
+# on exit during global cleanup. This occurs when it tries to
+# tidy up the GV created when the file is opened. When we run
+# gsdClose the file handle is closed but Perl doesnt know about
+# it. It tries to tidy up but gets all confused. This seems to be
+# fixed by simply closing the file before running gsdClose
+# but is not a wonderful solution.
+
+# We implement gsdClose at the perl level and then call
+# the C version. Note that we could implement this without the
+# actual gsdClose if we simply attached destructors to item_dsc,
+# file_dsc and data_ptr. For completeness though, we use gsdClose itself.
+
+# Input arguments are set to undef on exit
+
+# Return 0 if good status
+# Return -1 if there was a problem
+
+sub gsdClose ($$$$) {
+
+  # Close the file early
+  my $status = close($_[0]);
+
+  # Now shut down GSD
+  my $gsdstat = _gsdClose($_[0], $_[1], $_[2], $_[3]);
+
+  # Combine the status from the close and that from gsdClose
+  if ($status && $gsdstat == 0) {
+    return $gsdstat;
+  } else {
+    return -1;
+  }
+
+}
 
 1;
 __END__
@@ -69,21 +105,20 @@ gsdGet1dp gsdGet1rp gsdGet1ip
 
 
 =head1 NOTES
- 
-The gsdGet1xp routines return a packed string rather than an array.
+
+The C<gsdGet1xp> routines return a packed string rather than an array.
 This is far more convenient for packages such as PDL which need
 to manipulate the array without a perl array overhead.
 
 This library is read-only. 
 
 =head1 AUTHOR
- 
+
 This module was written by Tim Jenness, timj@jach.hawaii.edu.
- 
+
 =head1 ACKNOWLEDGMENTS
 
 This module would not have been possible without the help
 of Karl Glazebrook and Frossie Economou (frossie@jach.hawaii.edu).
-
 
 =cut
