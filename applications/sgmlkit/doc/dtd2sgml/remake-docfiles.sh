3b151f3956a5e070641e669411f6153f9c6be4d4
Norman Gray <norman@ASTRO.GLA.AC.UK>
2000-06-04 14:26:52 +0000

    The description+commentary->html magic has been stripped out of this file, into Makefile.sgml-mail, pending a better-designed long-term alternative.  The contents of this directory should now be buildable immediately after checkout form CVS, as long as you (a) override the directory paths, and (b) have perlSGML.
    svn path=/trunk/; revision=6500

--- applications/sgmlkit/doc/dtd2sgml/remake-docfiles.sh
+++ applications/sgmlkit/doc/dtd2sgml/remake-docfiles.sh
@@ -9,13 +9,13 @@ export STARLINK_SGML_DIR
 cd $BUILD_DIR
 
 # Don't produce any output unless the make fails
-make starlink-0.7.html >make.log 2>&1 || cat make.log
+make -f Makefile.sgml-mail starlink-0.7.html >make.log 2>&1 || cat make.log
 if test ! -e $TARGET_DIR/starlink-0.7.html -o \
 	starlink-0.7.html -nt $TARGET_DIR/starlink-0.7.html; then 
     cp starlink-0.7.html $TARGET_DIR
 fi
 
-make programcode-0.7.html > make.log 2>&1 || cat make.log
+make -f Makefile.sgml-mail programcode-0.7.html > make.log 2>&1 || cat make.log
 if test ! -e $TARGET_DIR/programcode-0.7.html -o \
 	programcode-0.7.html -nt $TARGET_DIR/programcode-0.7.html; then 
     cp programcode-0.7.html $TARGET_DIR
