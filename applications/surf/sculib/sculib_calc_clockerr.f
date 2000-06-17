13faa9e7b117ef4ddf2d76f5217a64e91c4c5d59
Tim Jenness <t.jenness@jach.hawaii.edu>
2000-06-17 00:54:39 +0000

    First version
    svn path=/trunk/; revision=6616

---
+++ applications/surf/sculib/sculib_calc_clockerr.f
@@ -0,0 +1,196 @@
+      SUBROUTINE SCULIB_CALC_CLOCKERR(N_FITS, FITS, RA_CEN,
+     :     LST_REF, CLOCK_ERR, LST_AZEL, STATUS)
+*+
+*  Name:
+*     SCULIB_CALC_CLOCKERR
+
+*  Purpose:
+*     Calculate start up time and error in system clock
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Invocation:
+*     CALL SCULIB_CALC_CLOCKERR( N_FITS, FITS, RA_CEN, LST_REF,
+*    :     CLOCK_ERR, LST_AZEL, STATUS )
+
+*  Description:
+*     Checks the self-consistency of the FITS headers by calculating
+*     the expected LST from the Azimuth and Elevation values stored
+*     in the header and comparing this with the supplied reference
+*     LST. Returns the difference between the two values and the 
+*     LST calculated from the headers.
+
+*  Arguments:
+*     N_FITS = INTEGER (Given)
+*        Number of items in the FITS array
+*     FITS() = CHAR*80 (Given)
+*        FITS array
+*     RA_CEN = DOUBLE (Given)
+*        Apparent RA corresponding to the Az/El defined in the FITS header.
+*        In radians.
+*     LST_REF = DOUBLE (Given)
+*        Local sidereal time at the start of data acquisition. This
+*        usually comes from the LST_STRT array. In radians.
+*     CLOCK_ERR = DOUBLE (Returned)
+*        The time difference (in radians) between the times stored
+*        in the data file (header items and LST_STRT) and the actual
+*        time of the observation derived from the azimuth and elevation 
+*        of the observed source. The value is the number that must be
+*        added to the supplied LST_REF in order to correct it.
+*     LST_AZEL = DOUBLE (Returned)
+*        Local Sidereal time calculated from the azimuth, elevation
+*        and apparent RA/Dec (radians)
+*     STATUS = INTEGER (Given & Returned)
+*        Global status. An error will be returned if the observation
+*        was not done in a tracking frame (eg NA or AZ) since those
+*        frames can not be used to determine the time from the telescope
+*        az and el.
+
+*  Notes:
+*     Requires the following FITS headers to be defined:
+*     - LAT-OBS
+*     - STRT_ELD
+*     - STRT_AZD
+*     - CENT_CRD
+*
+*     The clock error returned must be added to all times read from
+*     the data file in order to make those times correct.
+
+*     The centre coordinates of the observation must corresponde to
+*     an astronomical frame. AZ can not be used (for obvious reasons).
+
+*  Authors:
+*     TIMJ: Tim Jenness (JACH)
+
+*  Copyright:
+*     Copyright (C) 2000 Particle Physics and Astronomy
+*     Research Council. All Rights Reserved.
+
+*  History:
+*     $Log$
+*     Revision 1.1  2000/06/17 00:54:38  timj
+*     First version
+*
+
+*-
+      
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+
+*  Arguments Given:
+      INTEGER          N_FITS
+      CHARACTER *80    FITS ( N_FITS )
+      DOUBLE PRECISION RA_CEN
+      DOUBLE PRECISION LST_REF
+
+*  Arguments Returned:
+      DOUBLE PRECISION CLOCK_ERR
+      DOUBLE PRECISION LST_AZEL
+
+*  Status:
+      INTEGER          STATUS
+
+*  Local Constants:
+      DOUBLE PRECISION DD2R     ! Degrees to radians
+      PARAMETER ( DD2R = 0.017453292519943295769236907684886127134429 )
+      DOUBLE PRECISION DAS2R    ! Arcseconds to radians
+      PARAMETER ( DAS2R= 4.84813681109535993589914102357947975956D-6 )
+
+*  External References:
+      DOUBLE PRECISION SLA_DRANGE
+      DOUBLE PRECISION SLA_DRANRM
+
+*  Local Variables:
+      CHARACTER * 10   CENTRE_COORDS ! Centre coordinate frame
+      DOUBLE PRECISION DEC      ! Declination of observation (radians)
+      DOUBLE PRECISION HA       ! Hour angle of observation (radians)
+      INTEGER          IPOS     ! position in string
+      DOUBLE PRECISION LAT_OBS  ! Latitude of observatory (radians)
+      INTEGER          SLA_STATUS ! Status from SLA calls
+      DOUBLE PRECISION STRT_DAZ ! Eror in azimuth (converted to radians)
+      DOUBLE PRECISION STRT_DEL ! Eror in elevation (converted to radians)
+      DOUBLE PRECISION STRT_AZ  ! Azimuth from header (converted to radians)
+      DOUBLE PRECISION STRT_EL  ! elevation from header (cvted to radians)
+
+*.
+
+      IF (STATUS .NE. SAI__OK) RETURN
+
+*     Read the centre coordinate frame from the header and check that
+*     it is a tracking coordinate frame (not AZ or NA)
+      CALL SCULIB_GET_FITS_C ( N_FITS, N_FITS, FITS, 
+     :     'CENT_CRD', CENTRE_COORDS, STATUS)
+      CALL CHR_UCASE (CENTRE_COORDS)
+
+      IF (STATUS .EQ. SAI__OK) THEN
+         IF (CENTRE_COORDS .NE. 'RJ' .AND.
+     :        CENTRE_COORDS .NE. 'RB' .AND.
+     :        CENTRE_COORDS .NE. 'PLANET' .AND.
+     :        CENTRE_COORDS .NE. 'GA') THEN
+            STATUS = SAI__ERROR
+            CALL MSG_SETC('CNT', CENTRE_COORDS)
+            CALL ERR_REP( ' ','SCULIB_CALC_CLOCKERR: Can not '//
+     :           'calculate clock error when observing with frame ^CNT',
+     :           STATUS)
+         END IF
+
+      END IF
+
+*     Read the azimuth and elevation from the header
+*     and convert to radians
+
+      CALL SCULIB_GET_FITS_D (N_FITS, N_FITS, FITS,
+     :     'STRT_AZD', STRT_AZ, STATUS)
+      STRT_AZ = STRT_AZ * DD2R
+
+      CALL SCULIB_GET_FITS_D (N_FITS, N_FITS, FITS,
+     :     'STRT_ELD', STRT_EL, STATUS)
+      STRT_EL = STRT_EL * DD2R
+
+*     Read the error in azimuth and elevation (arcsec, convert to radians)
+
+      CALL SCULIB_GET_FITS_D (N_FITS, N_FITS, FITS,
+     :     'AZ_ERR', STRT_DAZ, STATUS)
+*      print *,' AZ error = ', STRT_DAZ
+      STRT_DAZ = STRT_DAZ * DAS2R
+
+      CALL SCULIB_GET_FITS_D (N_FITS, N_FITS, FITS,
+     :     'EL_ERR', STRT_DEL, STATUS)
+*      print *,' EL error = ', STRT_DEL
+      STRT_DEL = STRT_DEL * DAS2R
+
+*     Correct for the error
+      STRT_AZ = STRT_AZ + STRT_DAZ
+      STRT_EL = STRT_EL + STRT_DEL
+
+*     Get the latitude of the telescope
+      CALL SCULIB_GET_FITS_D (N_FITS, N_FITS, FITS,
+     :     'LAT-OBS', LAT_OBS, STATUS)
+      LAT_OBS = LAT_OBS * DD2R
+
+      IF (STATUS .EQ. SAI__OK) THEN
+
+*     Convert these values to hour angle
+         CALL SLA_DH2E( STRT_AZ, STRT_EL, LAT_OBS, HA, DEC)
+
+*     Add the hour angle to the RA to get the LST
+*     and put it into range 0 to 2PI
+         LST_AZEL = SLA_DRANRM( HA + RA_CEN )
+
+*     Now calculate the difference between this value and the reference
+*     value provided as an argument
+*     Have to be careful about day boundaries so put this into
+*     range -PI to PI
+
+         CLOCK_ERR = SLA_DRANGE( LST_AZEL - LST_REF )
+
+*         print *,' HA:', HA, RA_CEN, STRT_AZ, STRT_EL, DEC
+*         print *,'LST:', LST_AZEL, LST_REF
+
+      END IF
+
+      END
---
+++ applications/surf/sculib/sculib_rad2string.f
@@ -0,0 +1,164 @@
+      SUBROUTINE SCULIB_RAD2STRING ( ANGLE, NDP, ISTIME, RESLT, STATUS )
+*+
+*  Name:
+*     SCULIB_RAD2STRING
+
+*  Purpose:
+*     Translate an angle or time in radians to a nicely formatted string
+
+*  Invoation:
+*     CALL SCULIB_RAD2STRING( ANGLE, NDP, ISTIME, RESLT, STATUS ) 
+
+*  Description:
+*     Converts an angle or time in radians to a string that can be
+*     used for display. The sexagesimal format is colon separated.
+*     For time output, the output is format SHH:MM:SS.NDP.
+*     For angle output, the format used is SDD:MM:SS.NDP. Note
+*     that the sign is used for the first character but this can
+*     be either blank or a '-'.
+
+*  Arguments:
+*     ANGLE = DOUBLE PRECISION (Given)
+*        Angle to be translated (radians)
+*     NDP = INTEGER (Given)
+*        Number of decimal places to use for the seconds
+*        Can not be greater than 6.
+*     ISTIME = LOGICAL (Given)
+*        If true the angle will be assumed to be a time and will
+*        be converted to HH:MM:SS.NDP format. If false it will be
+*        assumed to be an angle and converted to SDDD:MM:SS.NDP.
+*     RESLT = CHARACTER (Returned)
+*        String containing the translated angle. Colon separated.
+*        Should be at least 11+NDP characters long.
+*     STATUS = INTEGER (Given & Returned)
+*        Global Status
+
+*  Notes:
+*     Essentially a wrapper around SLA_DR2AF and SLA_DR2TF
+
+*  Authors:
+*     Tim Jenness (TIMJ)
+*        Joint Astronomy Centre, Hilo, HI
+
+*  Copyright:
+*     Copyright (C) 2000 Particle Physics and Astronomy Research Council.
+*     All Rights Reserved.
+
+*  History:
+*     $Log$
+*     Revision 1.1  2000/06/17 00:54:39  timj
+*     First version
+*
+
+*-
+
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+
+*  Arguments Given:
+      DOUBLE PRECISION ANGLE
+      LOGICAL          ISTIME
+      INTEGER          NDP
+
+*  Arguments Returned:
+      CHARACTER *(*)   RESLT
+
+*  Status:
+      INTEGER          STATUS
+
+*  Local Constants:
+
+*  Local Variables:
+      INTEGER          FMTN     ! Format number to use for output
+      INTEGER          I        ! loop counter
+      INTEGER          IHMSF( 4 ) ! Hours, minutes, seconds and fraction
+      CHARACTER * 1    SIGN     ! Sign of angle
+
+*.
+
+      IF (STATUS .NE. SAI__OK) RETURN
+
+*     Check NDP
+      IF (NDP .GT. 6 .OR. NDP .LT. 0) THEN
+         STATUS = SAI__ERROR
+         CALL MSG_SETI('NDP', NDP)
+         CALL ERR_REP( ' ', 'SCULIB_RAD2STRING: NDP must be not be'//
+     :        ' greater than 6 or less than 0 (was ^NDP)', STATUS)
+         RETURN
+      END IF
+
+*     Translate the radians to sexagesimal
+*     and decide on the format statement to use
+      IF (ISTIME) THEN
+         CALL SLA_DR2TF( NDP, ANGLE, SIGN, IHMSF )
+      ELSE
+         CALL SLA_DR2AF( NDP, ANGLE, SIGN, IHMSF )
+      END IF
+
+*     Only put a SIGN if it is negative
+      IF (SIGN .EQ. '+') SIGN = ' '
+
+*     Create the formatted string. For some reason the format number
+*     can not be a variable so we need to select the format explicitly.
+*     This is a pain. All because I wanted NDP to be an argument....
+*     Should be using perl
+
+      IF (ISTIME) THEN
+         IF (NDP .EQ. 0) THEN
+            WRITE( RESLT, 20 ) SIGN, (IHMSF(I),I=1,3)
+         ELSE IF (NDP .EQ. 1) THEN
+            WRITE( RESLT, 21 ) SIGN, (IHMSF(I),I=1,4)
+         ELSE IF (NDP .EQ. 1) THEN
+            WRITE( RESLT, 22 ) SIGN, (IHMSF(I),I=1,4)
+         ELSE IF (NDP .EQ. 1) THEN
+            WRITE( RESLT, 23 ) SIGN, (IHMSF(I),I=1,4)
+         ELSE IF (NDP .EQ. 1) THEN
+            WRITE( RESLT, 24 ) SIGN, (IHMSF(I),I=1,4)
+         ELSE IF (NDP .EQ. 1) THEN
+            WRITE( RESLT, 25 ) SIGN, (IHMSF(I),I=1,4)
+         ELSE IF (NDP .EQ. 1) THEN
+            WRITE( RESLT, 26 ) SIGN, (IHMSF(I),I=1,4)
+         END IF
+      ELSE
+         IF (NDP .EQ. 0) THEN
+            WRITE( RESLT, 10 ) SIGN, (IHMSF(I),I=1,3)
+         ELSE IF (NDP .EQ. 1) THEN
+            WRITE( RESLT, 11 ) SIGN, (IHMSF(I),I=1,4)
+         ELSE IF (NDP .EQ. 1) THEN
+            WRITE( RESLT, 12 ) SIGN, (IHMSF(I),I=1,4)
+         ELSE IF (NDP .EQ. 1) THEN
+            WRITE( RESLT, 13 ) SIGN, (IHMSF(I),I=1,4)
+         ELSE IF (NDP .EQ. 1) THEN
+            WRITE( RESLT, 14 ) SIGN, (IHMSF(I),I=1,4)
+         ELSE IF (NDP .EQ. 1) THEN
+            WRITE( RESLT, 15 ) SIGN, (IHMSF(I),I=1,4)
+         ELSE IF (NDP .EQ. 1) THEN
+            WRITE( RESLT, 16 ) SIGN, (IHMSF(I),I=1,4)
+         END IF
+      END IF
+
+*     All the format statements since I cant seem to use
+*     a variable NDP to specify the width of the last field
+*     Since I want to make sure I get an 'INDP.NDP' format
+*     so that the last entry is zero-padded
+ 10   FORMAT ( A1, I3.3, ':', I2.2, ':', I2.2)
+ 11   FORMAT ( A1, I3.3, ':', I2.2, ':', I2.2, '.', I1)
+ 12   FORMAT ( A1, I3.3, ':', I2.2, ':', I2.2, '.', I2.2)
+ 13   FORMAT ( A1, I3.3, ':', I2.2, ':', I2.2, '.', I3.3)
+ 14   FORMAT ( A1, I3.3, ':', I2.2, ':', I2.2, '.', I4.4)
+ 15   FORMAT ( A1, I3.3, ':', I2.2, ':', I2.2, '.', I5.5)
+ 16   FORMAT ( A1, I3.3, ':', I2.2, ':', I2.2, '.', I6.6)
+
+ 20   FORMAT ( A1, I2.2, ':', I2.2, ':', I2.2)
+ 21   FORMAT ( A1, I2.2, ':', I2.2, ':', I2.2, '.', I1)
+ 22   FORMAT ( A1, I2.2, ':', I2.2, ':', I2.2, '.', I2.2)
+ 23   FORMAT ( A1, I2.2, ':', I2.2, ':', I2.2, '.', I3.3)
+ 24   FORMAT ( A1, I2.2, ':', I2.2, ':', I2.2, '.', I4.4)
+ 25   FORMAT ( A1, I2.2, ':', I2.2, ':', I2.2, '.', I5.5)
+ 26   FORMAT ( A1, I2.2, ':', I2.2, ':', I2.2, '.', I6.6)
+
+      END
+
