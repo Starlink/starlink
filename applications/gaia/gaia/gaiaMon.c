e4830fe2308ae19f90a9a1846a39ba2c41ff3c5b
Peter W. Draper <P.W.Draper@durham.ac.uk>
2000-07-19 15:11:46 +0000

    Top-level monolith for GAIA tasks.
    svn path=/trunk/; revision=6839

---
+++ applications/gaia/gaia/gaiaMon.c
@@ -0,0 +1,142 @@
+/*+
+ *  Name:
+ *     gaiaMon
+
+ *  Purpose:
+ *     Top-level ADAM monolith routine for the GAIA applications.
+
+ *  Language:
+ *     C
+
+ *  Synopsis:
+ *     void gaiaMon( int *status );
+
+ *  Description:
+ *     This routine obtains the name of the current action and calls
+ *     the appropriate routine to perform the specified operation. An
+ *     error will be reported and STATUS will be set if the action
+ *     name is not recognised.
+
+ *  Arguments:
+ *     status = pointer to integer (read and write)
+ *        The global status.
+
+ *  Notes:
+ *     -  Only contains GAIA internal applications.
+
+ *  Copyright:
+ *     Copyright (C) 2000 Central Laboratory of the Research Councils
+
+ *  Authors:
+ *     PWD: Peter W. Draper (Starlink - Durham University)
+ *     {enter_new_authors_here}
+
+ *  History:
+ *     19-JUL-2000 (PWD):
+ *        Original version.
+ *     {enter_further_changes_here}
+
+ *-
+ */
+
+/*  Include files */
+#include "sae_par.h"
+#include "par_par.h"
+#include "f77.h"
+#include "merswrap.h"
+
+/*  Define macro to generate functions for invoking any Fortran
+ *  actions. CNAME is the function name as will be called from C (add
+ *  these to the actionStruct) and FNAME is the Fortran subroutine
+ *  name.
+ */
+#define GENERATE_FORTRANCMD( CNAME, FNAME ) \
+   extern void F77_EXTERNAL_NAME( FNAME )( INTEGER( fstatus ) ); \
+   static void CNAME( int *status ) \
+{ \
+   DECLARE_INTEGER(fstatus); \
+   F77_EXPORT_INTEGER( *status, fstatus ); \
+   F77_CALL( FNAME )( INTEGER_ARG( &fstatus ) ); \
+   F77_IMPORT_INTEGER( fstatus, *status ); \
+   return; \
+}
+
+/*  ====================================== */
+/*  Generate Fortran stubs for any actions */
+/*  ====================================== */
+GENERATE_FORTRANCMD( ardstatCmd,  ardstat );
+GENERATE_FORTRANCMD( asc2tabCmd,  asc2tab );
+GENERATE_FORTRANCMD( autocropCmd, autocrop );
+GENERATE_FORTRANCMD( cat2tabCmd,  cat2tab );
+GENERATE_FORTRANCMD( tab2ascCmd,  tab2asc );
+GENERATE_FORTRANCMD( tab2catCmd,  tab2cat );
+
+/*  Define TASK subroutine that returns name */
+extern void F77_EXTERNAL_NAME( task_get_name )(
+    CHARACTER( fname ),
+    INTEGER( fstatus )
+    TRAIL( fpath ) );
+
+static void taskGetName( char *name, int name_length, int *status )
+{
+   DECLARE_CHARACTER_DYN(fname);
+   DECLARE_INTEGER(flevel);
+   DECLARE_INTEGER(fstatus);
+   F77_CREATE_CHARACTER( fname, name_length-1 );
+   F77_EXPORT_INTEGER( *status, fstatus );
+   F77_CALL( task_get_name )( CHARACTER_ARG( fname ),
+                              INTEGER_ARG( &fstatus )
+                              TRAIL_ARG( fname ) );
+   F77_IMPORT_CHARACTER( fname, fname_length, name );
+   F77_IMPORT_INTEGER( fstatus, *status );
+   return;
+}
+
+/*  ==============================================  */
+/*  Define the list of actions that we can invoke. */
+/*  ==============================================  */
+static struct actionStruct {
+    char const *name;              // Name of action
+    void (*fptr)( int *status );   // Ptr to invoking function
+} actions_[] = {
+    { "ardstat",  ardstatCmd  },
+    { "asc2tab",  asc2tabCmd  },
+    { "autocrop", autocropCmd },
+    { "cat2tab",  cat2tabCmd  },
+    { "tab2asc",  tab2ascCmd  },
+    { "tab2cat",  tab2catCmd  },
+};
+
+/*  ============= */
+/*  Main function */
+/*  ============= */
+void gaiaMon( int *status )
+{
+    /*  Local Variables: */
+    char name[PAR__SZNAM];
+    unsigned int i;
+    struct actionStruct *action;
+
+    /*  Check inherited global status. */
+    if ( *status != SAI__OK ) return;
+
+    /*  Get the action name. */
+    taskGetName( name, PAR__SZNAM, status );
+
+    /*  If the name is known then invoke the action */
+    for ( i = 0; i < sizeof( actions_ ) / sizeof( *actions_ ); i++ ) {
+        action = &actions_[i];
+        if ( strncasecmp( action->name, name, PAR__SZNAM ) == 0 ) {
+            ( action->fptr )( status );
+            return;
+        }
+    }
+
+    /*  If the action name is not recognised, so report an error. */
+    *status = SAI__ERROR;
+    msgSetc( "NAME", name );
+    errRep( "GAIA_MON_ERR",
+            "GAIA_MON: The action name '^NAME' is not recognised by "
+            "the GAIA monolith.", status );
+    return;
+}
