837f683c6a76906f547c53ba2d3aa1d27c37015c
Peter W. Draper <P.W.Draper@durham.ac.uk>
2000-10-05 13:25:52 +0000

    Fixed problem with standard complicant header card that has no equals.
    svn path=/trunk/; revision=7125

--- thirdparty/eso/skycat/skycat/astrotcl/libwcs/hget.c
+++ thirdparty/eso/skycat/skycat/astrotcl/libwcs/hget.c
@@ -34,6 +34,12 @@
  *              software for any purpose.  It is provided "as is" without
  *              express or implied warranty.
 
+ *** 5th October 2000: Peter W. Draper (PWD).
+ *   Changed hgetc to deal with the FITS standard possibility of a
+ *   keyword with no value (i.e KEYWORD               /Comment). This
+ *   is apparently a comment only keyword and is indicated by the
+ *   absence of an equals sign.
+
 */
 
 #include <string.h>		/* NULL, strlen, strstr, strcpy */
@@ -692,7 +698,15 @@ char *keyword0;	/* character string containing the name of the keyword
 	    c1 = strsrch (q2,"/");
 	    }
 	else {
-	    v1 = strsrch (line,"=") + 1;
+
+/*	    v1 = strsrch (line,"=")+1; */
+/*  PWD: change here. Equals may be missing, in which case keyword has */
+/*  no value and line is equivalent to a comment, just fudge the
+/*  equals position to the standard column 9, after all a "NULL+1" v1
+/*  isn't good for anything. */
+	    v1 = strsrch (line,"=");
+            if ( v1 == NULL ) v1 = line + 9;
+            v1++;
 	    c1 = strsrch (line,"/");
 	    if (c1 != NULL)
 		v2 = c1;
