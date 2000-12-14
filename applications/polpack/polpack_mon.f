1715ba912df0ed303e16809dd3d056a3e16d7963
David S. Berry <d.berry@jach.hawaii.edu>
2000-12-14 11:41:38 +0000

    File replaced by SDT
    svn path=/trunk/; revision=7632

--- applications/polpack/polpack_mon.f
+++ applications/polpack/polpack_mon.f
@@ -95,6 +95,9 @@
 *  is contructed.
       CALL NDF_HAPPN( NAME // ' (POLPACK 2.1-5)', STATUS )
 
+*  Re-read the polpack configuration file.
+      CALL POL1_CNFIG( STATUS )
+
 *  Check the string against valid A-task names---if matched then call
 *  the relevant A-task
 
