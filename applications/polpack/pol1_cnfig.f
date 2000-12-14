3423c9a707b4a3f0610f9d8ad89fd8272f88ebca
David S. Berry <d.berry@jach.hawaii.edu>
2000-12-14 11:45:09 +0000

    Initial version
    svn path=/trunk/; revision=7633

---
+++ applications/polpack/pol1_cnfig.f
@@ -0,0 +1,57 @@
+      SUBROUTINE POL1_CNFIG( STATUS )
+*+
+*  Name:
+*     POL1_CNFIG
+
+*  Purpose:
+*     Reads the polpack configuration file.
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Invocation:
+*     CALL POL1_CNFIG( STATUS )
+
+*  Description:
+*     This routine reads the polpack configuration file.
+
+*  Arguments:
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
+
+*  Copyright:
+*     Copyright (C) 1998 Central Laboratory of the Research Councils
+ 
+*  Authors:
+*     DSB: David Berry (STARLINK)
+*     {enter_new_authors_here}
+
+*  History:
+*     13-DEC-2000 (DSB):
+*        Original version.
+*     {enter_further_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+      
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+
+*  Status:
+      INTEGER STATUS             ! Global status
+
+*.
+
+*  Check the inherited global status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  At the moment, the config file only holds column definitions. Read
+*  these.
+      CALL POL1_COLNM( ' ', .FALSE., ' ', STATUS )
+
+      END
