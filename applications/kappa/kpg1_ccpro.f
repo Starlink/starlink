3caa03d7c41bca14afde1b55ab0e3daac2c46cf8
David S. Berry <d.berry@jach.hawaii.edu>
2001-01-09 13:57:35 +0000

    File replaced by SDT
    svn path=/trunk/; revision=7664

--- applications/kappa/kpg1_ccpro.f
+++ applications/kappa/kpg1_ccpro.f
@@ -17,8 +17,8 @@
 *     This routine uses the parameter sysyem to obtain a value for a
 *     selected character component of an output or updated NDF.  If the
 *     null value is supplied, the character component is copied from
-*     another NDF to the output NDF, unless the component is undefined,
-*     whereupon ablank string is used.
+*     an input NDF to the output NDF, unless the component is undefined,
+*     in the input, in which case it is left undefined in the output.
 
 *  Arguments:
 *     PNCOMP = CHARACTER * ( * ) (Given)
@@ -28,7 +28,7 @@
 *        The name of the character component.  It must be 'TITLE',
 *        'LABEL', or 'UNITS'.
 *     NDFI = INTEGER (Given)
-*        The identifier of the NDF from which a character component
+*        The identifier of the input NDF from which a character component
 *        is to be copied to the output NDF.
 *     NDFO = INTEGER (Given)
 *        The identifier of the output or updated NDF to which a
@@ -39,11 +39,16 @@
 *  [optional_subroutine_items]...
 *  Authors:
 *     MJC: Malcolm J. Currie (STARLINK)
+*     DSB: David S. Berry (STARLINK)
 *     {enter_new_authors_here}
 
 *  History:
 *     1994 September 27 (MJC):
 *        Original version.
+*     9-JAN-2001 (DSB):
+*        Changed behaviour so that the component is left undefined in the
+*        output NDF (rather than being set blank) if a null parameter value 
+*        is supplied, and no component exists in the input NDF.
 *     {enter_changes_here}
 
 *  Bugs:
@@ -68,26 +73,27 @@
 
 *  Local Variables:
       CHARACTER * ( 80 ) VALUE   ! Value of the character component
+      LOGICAL THERE              ! Is the component defined in the input NDF?
 
 *.
 
 *  Check the inherited global status.
       IF ( STATUS .NE. SAI__OK ) RETURN
 
-*  Initialise the value of the component.  This remains the value if
-*  there is no such component in the input NDF.
-      VALUE = ' '
-      
-*  Get the value of the character component from the 
-      CALL NDF_CGET( NDFI, COMP, VALUE, STATUS )
+*  See if the component is defined in the input NDF.
+      CALL NDF_STATE( NDFI, COMP, THERE, STATUS )
 
-*  Assign a default value for the character component in the output
-*  NDF.
-      CALL NDF_CPUT( VALUE, NDFO, COMP, STATUS )
+*  If so, use the current value as the parameter's dynamic default, and
+*  store it as an initial value in the output NDF.
+      IF( THERE ) THEN
+        CALL NDF_CGET( NDFI, COMP, VALUE, STATUS )
+        CALL PAR_DEF0C( PNCOMP, VALUE, STATUS )
+        CALL NDF_CPUT( VALUE, NDFO, COMP, STATUS )
+      END IF
 
 *  Obtain the value for the character component from the parameter
-*  system.  If a null value is supplied, the component takes the
-*  default value obtained above.
+*  system.  If a null value is supplied, any existing component is
+*  retained.
       CALL NDF_CINP( PNCOMP, NDFO, COMP, STATUS )
 
       END
