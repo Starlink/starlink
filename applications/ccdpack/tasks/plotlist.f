620a788e5e7df64778a12fc4fce1d166200d759d
Mark Taylor <m.b.taylor@bristol.ac.uk>
2001-01-12 15:13:59 +0000

    tweaked prologue.
    svn path=/trunk/; revision=7705

--- applications/ccdpack/tasks/plotlist.f
+++ applications/ccdpack/tasks/plotlist.f
@@ -24,7 +24,8 @@
 *     squares etc.) at positions specified in series of position lists.
 *     Before this application can be run an image (or other graphical
 *     output such as a contour image) must have been displayed using a
-*     suitable routine such as KAPPA DISPLAY (SUN/95).
+*     suitable routine such as KAPPA's DISPLAY (SUN/95) or CCDPACK's
+*     OUTLINE.
 *
 *     For a more interactive display of markers on an Xwindows display,
 *     you can use the IDICURS program instead.
