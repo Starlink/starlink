2b132e452ba049addac2b118c703cf0b154956c8
David S. Berry <d.berry@jach.hawaii.edu>
2001-01-17 15:36:04 +0000

    Initial version
    svn path=/trunk/; revision=7773

---
+++ applications/atools/removeframe.f
@@ -0,0 +1,103 @@
+      SUBROUTINE REMOVEFRAME( STATUS )
+*+
+*  Name:
+*     REMOVEFRAME
+
+*  Purpose:
+*     Remove a Frame from a FrameSet.
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Type of Module:
+*     ADAM A-task
+
+*  Invocation:
+*     CALL REMOVEFRAME( STATUS )
+
+*  Arguments:
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
+
+*  Description:
+*     This application  removes a Frame from a FrameSet. All other Frames
+*     in the FrameSet have their indices re-numbered from one (if necessary), 
+*     but are otherwise unchanged. 
+
+*  Usage:
+*     removeframe this iframe result
+
+*  ADAM Parameters:
+*     IFRAME = LITERAL (Read)
+*        The integer index or Domain name of the Frame within the
+*        FrameSet which is to be removed (the strings AST__BASE and 
+*        AST__CURRENT may also be supplied).
+*     RESULT = LITERAL (Read)
+*        An NDF or text file to receive the modified FrameSet. If an NDF
+*        is supplied, the WCS FrameSet within the NDF will be replaced by 
+*        the new FrameSet, if possible.
+*     THIS = LITERAL (Read)
+*        An NDF or text file holding the original FrameSet from which a 
+*        Frame is to be removed. If an NDF is supplied, the WCS 
+*        FrameSet will be used.
+
+*  Authors:
+*     DSB: David Berry (STARLINK)
+*     {enter_new_authors_here}
+
+*  History:
+*     12-JAN-2001 (DSB):
+*        Original version.
+*     {enter_further_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+*  Type Definitions:
+      IMPLICIT NONE              ! no default typing allowed
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'AST_PAR'          ! AST constants and function declarations
+
+*  Status:
+      INTEGER STATUS
+
+*  External References:
+      EXTERNAL AST_ISAFRAMESET
+
+*  Local Variables:
+      INTEGER IFRAME
+      INTEGER THIS
+*.
+
+*  Check inherited status.      
+      IF( STATUS .NE. SAI__OK ) RETURN
+
+*  Begin an AST context.
+      CALL AST_BEGIN( STATUS )
+
+*  Get a FrameSet.
+      CALL ATL1_GTOBJ( 'THIS', 'FrameSet', AST_ISAFRAMESET, THIS, 
+     :                 STATUS )
+
+*  Get the index of the Frame.
+      CALL ATL1_GTFRM( 'IFRAME', THIS, IFRAME, STATUS )
+
+*  Remove the Frame from the FrameSet.
+      CALL AST_REMOVEFRAME( THIS, IFRAME, STATUS ) 
+
+*  Write the modified FrameSet out.
+      CALL ATL1_PTOBJ( 'RESULT', 'THIS', THIS, STATUS )
+
+*  End the AST context.
+      CALL AST_END( STATUS )
+
+*  Give a context message if anything went wrong.
+      IF( STATUS .NE. SAI__OK ) THEN
+         CALL ERR_REP( 'REMOVEFRAME_ERR', 'Error removing a Frame '//
+     :                 'from a FrameSet.', STATUS )
+      END IF
+
+      END
