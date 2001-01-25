67d86e3c723c57b15638e3ef7d2ac7f66d95a371
David S. Berry <d.berry@jach.hawaii.edu>
2001-01-25 16:47:14 +0000

    Initial version
    svn path=/trunk/; revision=7789

---
+++ applications/kappa/kapsub/kps1_cntst.f
@@ -0,0 +1,89 @@
+      SUBROUTINE KPS1_CNTST( IPLOT, IGRP, IPEN, STATUS )
+*+
+*  Name:
+*     KPS1_CNTST
+
+*  Purpose:
+*     Establish a pen style from an attribute list in a group.
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Invocation:
+*     CALL KPS1_CNTST( IPLOT, IGRP, IPEN, STATUS )
+
+*  Description:
+*     This routine sets the plotting style according to a given element
+*     in the supplied group.
+
+*  Arguments:
+*     IPLOT = INTEGER (Given)
+*        An AST pointer to the Plot.
+*     IGRP = INTEGER (Given)
+*        A GRP identifier for a group containing descriptions of the pens
+*        to be used. Each element in the group should be a comma
+*        separated list of AST attribute settings to be established.
+*        Any attributes not specified default to their values in the Plot.
+*     IPEN = INTEGER (Given)
+*        The index withi the group of the style to be established.
+*     STATUS = INTEGER (Given)
+*        Global status value.
+
+*  Authors:
+*     DSB: David S. Berry (STARLINK)
+*     {enter_new_authors_here}
+
+*  History:
+*     25-JAN-2001 (DSB):
+*        Original version.
+*     {enter_further_changes_here}
+
+*  Bugs:
+*     {note_new_bugs_here}
+
+*-
+
+*  Type Definitions:
+      IMPLICIT NONE            
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'GRP_PAR'          ! GRP constants
+
+*  Arguments Given:
+      INTEGER IPLOT
+      INTEGER IGRP
+      INTEGER IPEN
+
+*  Status:
+      INTEGER STATUS             ! Global status
+
+*  Local Variables:
+      CHARACTER PENDEF*(GRP__SZNAM)! AST attribute settings for current pen
+      INTEGER J1                 ! Index at start of attribute setting
+      INTEGER J2                 ! Index of comma at end of attribute setting
+      LOGICAL BADAT              ! Was attribute setting string invalid?
+
+*.
+
+*  Check the global inherited status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  Get the next list of AST Attribute settings from the group. 
+      CALL GRP_GET( IGRP, IPEN, 1, PENDEF, STATUS )
+
+*  Loop round each comma-delimited attribute in the definitions, translating 
+*  colour names and any defined synonyms, and storing it in the Plot.
+      IF( PENDEF .NE. ' ' ) THEN
+         J1 = 1
+         DO WHILE( J1 .LE. GRP__SZNAM .AND. STATUS .EQ. SAI__OK )
+            J2 = J1
+            CALL CHR_FIND( PENDEF, ',', .TRUE., J2 )
+            CALL KPG1_ASSTS( PENDEF( J1 : J2 - 1 ), .TRUE., 
+     :                       .TRUE., IPLOT, BADAT, STATUS )
+            J1 = J2 + 1
+         END DO
+
+      END IF
+
+      END
