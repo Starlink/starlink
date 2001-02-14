61401ab1041bc912ebacb1df4282d3abd61bec4b
David S. Berry <d.berry@jach.hawaii.edu>
2001-02-14 13:04:08 +0000

    Initial version
    svn path=/trunk/; revision=7903

---
+++ applications/atools/astsimplify.f
@@ -0,0 +1,95 @@
+      SUBROUTINE ASTSIMPLIFY( STATUS )
+*+
+*  Name:
+*     ASTSIMPLIFY
+
+*  Purpose:
+*     Simplify a Mapping.
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Type of Module:
+*     ADAM A-task
+
+*  Invocation:
+*     CALL ASTSIMPLIFY( STATUS )
+
+*  Arguments:
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
+
+*  Description:
+*     This application simplifies a Mapping (which may be a compound
+*     Mapping such as a CmpMap) to eliminate redundant computational
+*     steps, or to merge separate steps which can be performed more
+*     efficiently in a single operation.
+
+*  Usage:
+*     astsimplify this result
+
+*  ADAM Parameters:
+*     RESULT = LITERAL (Read)
+*        An text file to receive the simplified Mapping.
+*     THIS = LITERAL (Read)
+*        An NDF or text file holding the Mapping. If an NDF is supplied, 
+*        the Mapping from the base Frame of the WCS FrameSet to the
+*        current Frame will be used.
+
+*  Authors:
+*     DSB: David Berry (STARLINK)
+*     {enter_new_authors_here}
+
+*  History:
+*     14-FEB-2001 (DSB):
+*        Original version.
+*     {enter_further_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+*  Type Definitions:
+      IMPLICIT NONE              ! no default typing allowed
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'AST_PAR'          ! AST constants and function declarations
+
+*  External References:
+      EXTERNAL AST_ISAMAPPING
+
+*  Status:
+      INTEGER STATUS
+
+*  Local Variables:
+      INTEGER THIS
+      INTEGER RESULT
+*.
+
+*  Check inherited status.      
+      IF( STATUS .NE. SAI__OK ) RETURN
+
+*  Begin an AST context.
+      CALL AST_BEGIN( STATUS )
+
+*  Get the Mapping.
+      CALL ATL1_GTOBJ( 'THIS', 'Mapping', AST_ISAMAPPING, THIS,
+     :                 STATUS )
+
+*  Get the required simplified Mapping.
+      RESULT = AST_SIMPLIFY( THIS, STATUS )
+
+*  Write this Mapping out to a text file.
+      CALL ATL1_PTOBJ( 'RESULT', ' ', RESULT, STATUS )
+
+*  End the AST context.
+      CALL AST_END( STATUS )
+
+*  Give a context message if anything went wrong.
+      IF( STATUS .NE. SAI__OK ) THEN
+         CALL ERR_REP( 'ASTSIMPLIFY_ERR', 'Error simplifying a '//
+     :                 'Mapping.', STATUS )
+      END IF
+
+      END
