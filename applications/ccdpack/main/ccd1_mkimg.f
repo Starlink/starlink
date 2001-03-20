7d723ec2f4b6cb3ed0efec8bf4c454c7ce1830d9
Mark Taylor <m.b.taylor@bristol.ac.uk>
2001-03-20 13:26:33 +0000

    Changed the function fairly completely.
    svn path=/trunk/; revision=8088

--- applications/ccdpack/main/ccd1_mkimg.f
+++ applications/ccdpack/main/ccd1_mkimg.f
@@ -1,5 +1,5 @@
-      SUBROUTINE CCD1_MKIMG( IPIN, ITYPE, IXDIM, IYDIM, IMAP, OXBASE,
-     :                       OYBASE, OXDIM, OYDIM, ILO, IHI, OLO, OHI, 
+      SUBROUTINE CCD1_MKIMG( IPIN, ITYPE, IXDIM, IYDIM, IMAP,
+     :                       OXDIM, OYDIM, ILO, IHI, OLO, OHI, 
      :                       OBAD, INTSCH, INTPAR, OUT, STATUS )
 *+
 *  Name:
@@ -12,15 +12,18 @@
 *     Starlink Fortran 77
 
 *  Invocation:
-*     CALL CCD1_MKIMG( IPIN, ITYPE, IXDIM, IYDIM, IMAP, OXBASE, OYBASE,
+*     CALL CCD1_MKIMG( IPIN, ITYPE, IXDIM, IYDIM, IMAP,
 *                      OXDIM, OYDIM, ILO, IHI, OLO, OHI, OBAD, INTSCH,
 *                      INTPAR, OUT, STATUS )
 
 *  Description:
 *     This routine resamples and rescales an array of data into a
 *     integer array with a given data range.  The data is resampled 
-*     using a given AST map and then rescaled.  The output array is
-*     then an image suitable for displaying using PGPLOT.
+*     using a given AST map, rescaled to fall within a given range,
+*     and then the good pixels are written into an output integer 
+*     array suitable for displaying using PGPLOT.  The output array
+*     should therefore be initialised to a background colour before
+*     use.
 
 *  Arguments:
 *     IPIN = INTEGER (Given)
@@ -37,10 +40,6 @@
 *     IMAP = INTEGER (Given)
 *        AST pointer to the mapping to use for resampling from the input
 *        array to the output array.
-*     OXBASE = INTEGER (Given)
-*        The first coordinate of the first pixel in the output array.
-*     OYBASE = INTEGER (Given)
-*        The second coordinate of the first pixel in the output array.
 *     OXDIM = INTEGER (Given)
 *        The first dimension of the output array.
 *     OYDIM = INTEGER (Given)
@@ -61,12 +60,7 @@
 *        The value to be assigned to bad pixels in the scaled array.
 *     INTSCH = INTEGER (Given)
 *        The INTERP parameter to be passed to the AST_RESAMPLE routine.
-*        This determines the subpixel resampling scheme.  If set to 
-*        AST__UINTERP then the resampling will be done using the 
-*        CCG1_IBLK<X> routines, which average over a cube in the input
-*        array.  This option should not be used as long as you have 
-*        a sufficiently recent version of AST; in that case you can
-*        use the value AST__BLOCKAVE instead.
+*        This determines the subpixel resampling scheme. 
 *     INTPAR( * ) = DOUBLE PRECISION (Given)
 *        The PARAMS vector to be passed to the AST_RESAMPLE routine.
 *        For certain values of INTSCH, it gives additional information
@@ -106,8 +100,6 @@
       INTEGER IXDIM
       INTEGER IYDIM
       INTEGER IMAP
-      INTEGER OXBASE
-      INTEGER OYBASE
       INTEGER OXDIM
       INTEGER OYDIM
       DOUBLE PRECISION ILO
@@ -127,6 +119,7 @@
 *  Local Constants:
 
 *  Local Variables:
+      INTEGER I                  ! Loop variable
       INTEGER ILBND( 2 )         ! Lower bounds of input array
       INTEGER IPIV               ! Pointer to dummy input variance array
       INTEGER IPOV               ! Pointer to dummy output variance array
@@ -136,18 +129,18 @@
       INTEGER NBAD               ! Number of bad pixels in resampled array
       INTEGER OLBND( 2 )         ! Lower bounds of output array
       INTEGER OUBND( 2 )         ! Upper bounds of output array
+      INTEGER OXB                ! X base of intermediate array
+      INTEGER OXD                ! X dimension of intermediate array
+      INTEGER OYB                ! Y base of intermediate array
+      INTEGER OYD                ! Y dimension of intermediate array
       INTEGER RFLAGS             ! Flags to pass to AST_RESAMPLE
+      DOUBLE PRECISION LPOS( 2 ) ! Dummy
+      DOUBLE PRECISION DOLBND    ! Output lower bound
+      DOUBLE PRECISION DOUBND    ! Output upper bound
+      DOUBLE PRECISION DILBND( 2 ) ! Lower bounds as a double
+      DOUBLE PRECISION DIUBND( 2 ) ! Upper bounds as a double
       DOUBLE PRECISION TOL       ! Pixel tolerance for resampling
-      LOGICAL BADTMP             ! Are there bad values in the resampled array?
-
-*  Internal References:
-      EXTERNAL CCG1_IBLKB        ! Block averaging interpolation routine
-      EXTERNAL CCG1_IBLKUB       ! Block averaging interpolation routine
-      EXTERNAL CCG1_IBLKW        ! Block averaging interpolation routine
-      EXTERNAL CCG1_IBLKUW       ! Block averaging interpolation routine
-      EXTERNAL CCG1_IBLKI        ! Block averaging interpolation routine
-      EXTERNAL CCG1_IBLKR        ! Block averaging interpolation routine
-      EXTERNAL CCG1_IBLKD        ! Block averaging interpolation routine
+      DOUBLE PRECISION UPOS( 2 ) ! Dummy
 
 *.
 
@@ -159,10 +152,6 @@
       ILBND( 2 ) = 1
       IUBND( 1 ) = IXDIM
       IUBND( 2 ) = IYDIM
-      OLBND( 1 ) = OXBASE
-      OLBND( 2 ) = OYBASE
-      OUBND( 1 ) = OXBASE + OXDIM - 1
-      OUBND( 2 ) = OYBASE + OYDIM - 1
       RFLAGS = AST__USEBAD
       TOL = 0.5D0 * MIN( FLOAT( OXDIM ) / FLOAT( IXDIM ),
      :                   FLOAT( OYDIM ) / FLOAT( IYDIM ) )
@@ -173,58 +162,78 @@
 *  not like passing invalid memory references.
       CALL CCD1_MALL( 1, ITYPE, IPIV, STATUS )
       CALL CCD1_MALL( 1, ITYPE, IPOV, STATUS )
+      IPIV = 0
+      IPOV = 0
+
+*  Work out how big an array we need to do the initial resampling.
+      DILBND( 1 ) = 1D0
+      DILBND( 2 ) = 1D0
+      DIUBND( 1 ) = DBLE( IXDIM )
+      DIUBND( 2 ) = DBLE( IYDIM )
+      CALL AST_MAPBOX( IMAP, DILBND, DIUBND, .TRUE., 1, DOLBND, DOUBND,
+     :                 LPOS, UPOS, STATUS )
+      OLBND( 1 ) = NINT( DOLBND ) - 1
+      OUBND( 1 ) = NINT( DOUBND ) + 1
+      OXB = OLBND( 1 )
+      OXD = OUBND( 1 ) - OLBND( 1 ) + 1
+      CALL AST_MAPBOX( IMAP, DILBND, DIUBND, .TRUE., 2, DOLBND, DOUBND,
+     :                 LPOS, UPOS, STATUS )
+      OLBND( 2 ) = NINT( DOLBND ) - 1
+      OUBND( 2 ) = NINT( DOUBND ) + 1
+      OYB = OLBND( 2 )
+      OYD = OUBND( 2 ) - OLBND( 2 ) + 1
 
 *  Get workspace for intermediate image (of type ITYPE).
-      CALL CCD1_MALL( OXDIM * OYDIM, ITYPE, IPTEMP, STATUS )
+      CALL CCD1_MALL( OXD * OYD, ITYPE, IPTEMP, STATUS )
 
 *  Now resample the data array into the output array.  Note that some of
 *  the arguments in the following calls change between types, as well
 *  as the names of the routines.
       IF ( ITYPE .EQ. '_BYTE' ) THEN
          NBAD = AST_RESAMPLEB( IMAP, 2, ILBND, IUBND, %VAL( IPIN ),
-     :                         %VAL( IPIV ), INTSCH, CCG1_IBLKB, 
+     :                         %VAL( IPIV ), INTSCH, AST_NULL, 
      :                         INTPAR, RFLAGS, TOL, MAXPIX, VAL__BADB,
      :                         2, OLBND, OUBND, OLBND, OUBND,
      :                         %VAL( IPTEMP ), %VAL( IPOV ), STATUS )
 
       ELSE IF ( ITYPE .EQ. '_UBYTE' ) THEN
          NBAD = AST_RESAMPLEUB( IMAP, 2, ILBND, IUBND, %VAL( IPIN ),
-     :                          %VAL( IPIV ), INTSCH, CCG1_IBLKUB, 
+     :                          %VAL( IPIV ), INTSCH, AST_NULL, 
      :                          INTPAR, RFLAGS, TOL, MAXPIX, VAL__BADUB,
      :                          2, OLBND, OUBND, OLBND, OUBND,
      :                          %VAL( IPTEMP ), %VAL( IPOV ), STATUS )
 
       ELSE IF ( ITYPE .EQ. '_WORD' ) THEN
          NBAD = AST_RESAMPLEW( IMAP, 2, ILBND, IUBND, %VAL( IPIN ),
-     :                         %VAL( IPIV ), INTSCH, CCG1_IBLKW, 
+     :                         %VAL( IPIV ), INTSCH, AST_NULL, 
      :                         INTPAR, RFLAGS, TOL, MAXPIX, VAL__BADW,
      :                         2, OLBND, OUBND, OLBND, OUBND,
      :                         %VAL( IPTEMP ), %VAL( IPOV ), STATUS )
 
       ELSE IF ( ITYPE .EQ. '_UWORD' ) THEN
          NBAD = AST_RESAMPLEUW( IMAP, 2, ILBND, IUBND, %VAL( IPIN ),
-     :                          %VAL( IPIV ), INTSCH, CCG1_IBLKUW, 
+     :                          %VAL( IPIV ), INTSCH, AST_NULL, 
      :                          INTPAR, RFLAGS, TOL, MAXPIX, VAL__BADUW,
      :                          2, OLBND, OUBND, OLBND, OUBND,
      :                          %VAL( IPTEMP ), %VAL( IPOV ), STATUS )
 
       ELSE IF ( ITYPE .EQ. '_INTEGER' ) THEN
          NBAD = AST_RESAMPLEI( IMAP, 2, ILBND, IUBND, %VAL( IPIN ),
-     :                         %VAL( IPIV ), INTSCH, CCG1_IBLKI, 
+     :                         %VAL( IPIV ), INTSCH, AST_NULL, 
      :                         INTPAR, RFLAGS, TOL, MAXPIX, VAL__BADI,
      :                         2, OLBND, OUBND, OLBND, OUBND,
      :                         %VAL( IPTEMP ), %VAL( IPOV ), STATUS )
 
       ELSE IF ( ITYPE .EQ. '_REAL' ) THEN
          NBAD = AST_RESAMPLER( IMAP, 2, ILBND, IUBND, %VAL( IPIN ),
-     :                         %VAL( IPIV ), INTSCH, CCG1_IBLKR, 
+     :                         %VAL( IPIV ), INTSCH, AST_NULL, 
      :                         INTPAR, RFLAGS, TOL, MAXPIX, VAL__BADR,
      :                         2, OLBND, OUBND, OLBND, OUBND,
      :                         %VAL( IPTEMP ), %VAL( IPOV ), STATUS )
 
       ELSE IF ( ITYPE .EQ. '_DOUBLE' ) THEN
          NBAD = AST_RESAMPLED( IMAP, 2, ILBND, IUBND, %VAL( IPIN ),
-     :                         %VAL( IPIV ), INTSCH, CCG1_IBLKD, 
+     :                         %VAL( IPIV ), INTSCH, AST_NULL, 
      :                         INTPAR, RFLAGS, TOL, MAXPIX, VAL__BADD,
      :                         2, OLBND, OUBND, OLBND, OUBND,
      :                         %VAL( IPTEMP ), %VAL( IPOV ), STATUS )
@@ -232,48 +241,47 @@
       END IF
 
 *  And finally rescale it.
-      BADTMP = NBAD .GT. 0
       IF ( ITYPE .EQ. '_BYTE' ) THEN
-         CALL KPG1_ISCLB( BADTMP, OXDIM, OYDIM, %VAL( IPTEMP ),
-     :                    .FALSE., ILO, IHI, OLO, OHI, OBAD, OUT,
-     :                    STATUS )
+         CALL CCG1_ISCLB( OXD, OYD, %VAL( IPTEMP ), ILO, IHI, OLO, OHI,
+     :                    OBAD, OXDIM, OYDIM, OLBND( 1 ) - 1, 
+     :                    OLBND( 2 ) - 1, OUT, STATUS )
 
       ELSE IF ( ITYPE .EQ. '_UBYTE' ) THEN
-         CALL KPG1_ISCLUB( BADTMP, OXDIM, OYDIM, %VAL( IPTEMP ),
-     :                     .FALSE., ILO, IHI, OLO, OHI, OBAD, OUT,
-     :                    STATUS )
+         CALL CCG1_ISCLUB( OXD, OYD, %VAL( IPTEMP ), ILO, IHI, OLO, OHI,
+     :                     OBAD, OXDIM, OYDIM, OLBND( 1 ) - 1, 
+     :                     OLBND( 2 ) - 1, OUT, STATUS )
 
       ELSE IF ( ITYPE .EQ. '_WORD' ) THEN
-         CALL KPG1_ISCLW( BADTMP, OXDIM, OYDIM, %VAL( IPTEMP ),
-     :                    .FALSE., ILO, IHI, OLO, OHI, OBAD, OUT,
-     :                    STATUS )
+         CALL CCG1_ISCLW( OXD, OYD, %VAL( IPTEMP ), ILO, IHI, OLO, OHI,
+     :                    OBAD, OXDIM, OYDIM, OLBND( 1 ) - 1, 
+     :                    OLBND( 2 ) - 1, OUT, STATUS )
 
       ELSE IF ( ITYPE .EQ. '_UWORD' ) THEN
-         CALL KPG1_ISCLUW( BADTMP, OXDIM, OYDIM, %VAL( IPTEMP ),
-     :                     .FALSE., ILO, IHI, OLO, OHI, OBAD, OUT,
-     :                    STATUS )
+         CALL CCG1_ISCLUW( OXD, OYD, %VAL( IPTEMP ), ILO, IHI, OLO, OHI,
+     :                     OBAD, OXDIM, OYDIM, OLBND( 1 ) - 1, 
+     :                     OLBND( 2 ) - 1, OUT, STATUS )
 
       ELSE IF ( ITYPE .EQ. '_INTEGER' ) THEN
-         CALL KPG1_ISCLI( BADTMP, OXDIM, OYDIM, %VAL( IPTEMP ),
-     :                    .FALSE., ILO, IHI, OLO, OHI, OBAD, OUT,
-     :                    STATUS )
+         CALL CCG1_ISCLI( OXD, OYD, %VAL( IPTEMP ), ILO, IHI, OLO, OHI,
+     :                    OBAD, OXDIM, OYDIM, OLBND( 1 ) - 1, 
+     :                    OLBND( 2 ) - 1, OUT, STATUS )
 
       ELSE IF ( ITYPE .EQ. '_REAL' ) THEN
-         CALL KPG1_ISCLR( BADTMP, OXDIM, OYDIM, %VAL( IPTEMP ),
-     :                    .FALSE., ILO, IHI, OLO, OHI, OBAD, OUT,
-     :                    STATUS )
+         CALL CCG1_ISCLR( OXD, OYD, %VAL( IPTEMP ), ILO, IHI, OLO, OHI,
+     :                    OBAD, OXDIM, OYDIM, OLBND( 1 ) - 1, 
+     :                    OLBND( 2 ) - 1, OUT, STATUS )
 
       ELSE IF ( ITYPE .EQ. '_DOUBLE' ) THEN
-         CALL KPG1_ISCLD( BADTMP, OXDIM, OYDIM, %VAL( IPTEMP ),
-     :                    .FALSE., ILO, IHI, OLO, OHI, OBAD, OUT,
-     :                    STATUS )
+         CALL CCG1_ISCLD( OXD, OYD, %VAL( IPTEMP ), ILO, IHI, OLO, OHI,
+     :                    OBAD, OXDIM, OYDIM, OLBND( 1 ) - 1, 
+     :                    OLBND( 2 ) - 1, OUT, STATUS )
 
       END IF
 
 *  Release the temporary workspace.
-      CALL CCD1_MFREE( IPTEMP, STATUS )
       CALL CCD1_MFREE( IPIV, STATUS )
       CALL CCD1_MFREE( IPOV, STATUS )
+      CALL CCD1_MFREE( IPTEMP, STATUS )
 
       END
 * $Id$
