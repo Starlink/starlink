c60db311aee4ad648c251b9307d1f8a068df0e94
Mark Taylor <m.b.taylor@bristol.ac.uk>
2001-04-10 10:25:06 +0000

    Upgraded for use with Sets, and improved the way that the memory allocation is done.
    svn path=/trunk/; revision=8242

--- applications/ccdpack/main/ccd1_algn.c
+++ applications/ccdpack/main/ccd1_algn.c
@@ -6,54 +6,58 @@
 #include <stdlib.h>
 #include <string.h>
 #include "ccdtcl.h"
+#include "ccdaux.h"
+#include "grp_par.h"
 
 #define BUFLENG 200
 
-   F77_SUBROUTINE(ccd1_algn)( CHARACTER_ARRAY(ndfnms), INTEGER(nndf),
-                              INTEGER(refpos), INTEGER(maxpos), 
+   F77_SUBROUTINE(ccd1_algn)( INTEGER(ndfgr), INTEGER(nndf), INTEGER(nset), 
+                              INTEGER_ARRAY(imem), INTEGER_ARRAY(imemof),
+                              INTEGER(snamgr), INTEGER(refset),
                               DOUBLE_ARRAY(percnt), DOUBLE(zoom), 
                               INTEGER(maxcanv), INTEGER_ARRAY(windim), 
-                              CHARACTER(mstyle),
-                              INTEGER(npoint), DOUBLE(xpos), DOUBLE(ypos), 
-                              INTEGER(index), INTEGER(status)
-                              TRAIL(ndfnms) TRAIL(mstyle) ) {
-   
+                              CHARACTER(mstyle), INTEGER(npoint), 
+                              POINTER_ARRAY(ipxpos), POINTER_ARRAY(ipypos), 
+                              POINTER_ARRAY(ipind), INTEGER(status)
+                              TRAIL(mstyle) ) {
+
 /*
 *+
 *  Name:
 *     CCD1_ALGN
-*
+
 *  Purpose:
 *     Get position lists by user interaction for a set of NDFs.
-*
+
 *  Language:
 *     ANSI C.
-*
+
 *  Invocation:
-*     CALL CCD1_ALGN( NDFNMS, NNDF, REFPOS, MAXPOS, PERCNT, ZOOM, 
-*                     MAXCNV, WINDIM, MSTYLE, NPOINT, XPOS, YPOS, 
-*                     INDEX, STATUS )
-*
+*     CALL CCD1_ALGN( NDFGR, NNDF, NSET, IMEM, IMEMOF, SNAMGR, REFSET,
+*                     PERCNT, ZOOM, MAXCNV, WINDIM, MSTYLE,
+*                     NPOINT, IPXPOS, IPYPOS, IPIND, STATUS )
+
 *  Description:
 *     This routine calls a Tcl script which displays a number of NDFs
 *     and allows the user to select a matching set of positions on
 *     each member of the set.  It returns the lists of points to 
 *     the calling routine.
-*     
-*     If the user attempts to select more positions than MAXPOS, the
-*     GUI part of the application will not allow it, and will force
-*     an early exit.
-*
+
 *  Arguments:
-*     NDFNMS = CHARACTER( NNDF ) * ( * ) (Given)
-*        Names of the NDFs which are to be matched.
+*     NDFGR = INTEGER (Given)
+*        A GRP identifier for the names of the NDFs to be processed.
 *     NNDF = INTEGER (Given)
-*        The number of NDFs to be matched.
-*     REFPOS = INTEGER (Given)
-*        The index of the NDF in NDFNMS which is to be the reference image.
-*        The first NDF in NDFNMS has the index 1.
-*     MAXPOS = INTEGER (Given)
-*        The maximum number of points to be marked on any of the NDFs.
+*        The number of members of NDFGR.
+*     NSET = INTEGER (Given)
+*        The number of Sets represented in the NDFGR group.
+*     IMEM( NNDF ) = INTEGER (Given)
+*        Indexes of the members of NDFGR in Set order.
+*     IMEMOF( NSET + 1 ) = INTEGER (Given)
+*        Array of pointers into the IMEM array.
+*     SNAMGR = INTEGER (Given)
+*        GRP identifier for a group giving the Set Names of the Sets.
+*     REFSET = INTEGER (Given)
+*        The index of the Set which represetns the reference image.
 *     PERCNT( 2 ) = DOUBLE PRECISION (Given and Returned)
 *        Lower and higher percentiles to use in displaying the images.
 *        They should satisfy 0 <= PERCNT( 0 ) <= PERCNT( 1 ) <= 100.
@@ -65,17 +69,28 @@
 *        Dimensions of the window used for display.
 *     MSTYLE = CHARACTER * ( * ) (Given and Returned)
 *        A string indicating how markers are to be plotted on the image.
-*     NPOINT = INTEGER( NNDF ) (Returned)
+*     NPOINT = INTEGER( NSET ) (Returned)
 *        The number of points contained in each of the output position
-*        lists in the array POINTS.
-*     XPOS = DOUBLE PRECISION( MAXPOS, NNDF ) (Returned)
-*        The X coordinates of the marked positions for each NDF.
-*     YPOS = DOUBLE PRECISION( MAXPOS, NNDF ) (Returned)
-*        The Y coordinates of the marked positions for each NDF.
-*     INDEX = INTEGER( MAXPOS, NNDF ) (Returned)
-*        The index label of the marked positions for each NDF.
+*        lists.
+*     IPXPOS( NSET ) = INTEGER (Returned)
+*        Pointers to arrays of the X coordinates of the marked positions
+*        for each Set.  The array at position I has NPOINT( I ) elements
+*        of type DOUBLE PRECISION.
+*     IPYPOS( NSET ) = INTEGER (Returned)
+*        Pointers to arrays of the Y coordinates of the marked positions
+*        for each Set.  The array at position I has NPOINT( I ) elements
+*        of type DOUBLE PRECISION.
+*     IPIND( NSET ) = INTEGER (Returned)
+*        Pointers to arrays of the index label of the marked positions 
+*        for each Set.  The array at position I has NPOINT( I ) elements
+*        of type INTEGER.
 *     STATUS = INTEGER (Given and Returned)
 *        The global status.
+*
+*  Notes:
+*     The pointers in the arrays IPXPOS, IPYPOS and IPIND are allocated
+*     using CCD1_MALL in this routine, and should be freed by the calling
+*     routine using CCD1_MFREE.
 *
 *  Copyright:
 *     Copyright (C) 1999 Central Laboratory of the Research Councils
@@ -86,23 +101,34 @@
 *  History:
 *     21-JUL-2000 (MBT):
 *        Original version.
+*     5-APR-2001 (MBT):
+*        Upgraded for use with Sets.
 *-
 */
 
-/* Arguments. */
-      GENPTR_CHARACTER_ARRAY(ndfnms)
+/* Arguments Given. */
+      GENPTR_INTEGER(ndfgr)
       GENPTR_INTEGER(nndf)
-      GENPTR_INTEGER(refpos)
-      GENPTR_INTEGER(maxpos)
+      GENPTR_INTEGER(nset)
+      GENPTR_INTEGER_ARRAY(imem)
+      GENPTR_INTEGER_ARRAY(imemof)
+      GENPTR_INTEGER(snamgr)
+      GENPTR_INTEGER(refset)
+
+/* Arguments Given and Returned. */
       GENPTR_DOUBLE_ARRAY(percnt)
       GENPTR_DOUBLE(zoom)
       GENPTR_INTEGER(maxcanv)
       GENPTR_INTEGER_ARRAY(windim)
       GENPTR_CHARACTER(mstyle)
+
+/* Arguments Returned. */
       GENPTR_INTEGER_ARRAY(npoint)
-      GENPTR_DOUBLE_ARRAY(xpos)
-      GENPTR_DOUBLE_ARRAY(ypos)
-      GENPTR_INTEGER_ARRAY(index)
+      GENPTR_POINTER_ARRAY(ipxpos)
+      GENPTR_POINTER_ARRAY(ipypos)
+      GENPTR_POINTER_ARRAY(ipind)
+
+/* Global Status. */
       GENPTR_INTEGER(status)
 
 /* Local Variables. */
@@ -114,6 +140,14 @@
       int ix;
       int j;
       int leng;
+      F77_INTEGER_TYPE *pi;
+      F77_DOUBLE_TYPE *px;
+      F77_DOUBLE_TYPE *py;
+      const int one = 1;
+      DECLARE_CHARACTER( fndfname, GRP__SZNAM );
+      DECLARE_CHARACTER( fsetname, GRP__SZNAM );
+      char ndfname[ GRP__SZNAM + 1 ];
+      char setname[ GRP__SZNAM + 1 ];
 
 /* Check the inherited status. */
       if ( *status != SAI__OK ) return;
@@ -121,20 +155,41 @@
 /* Initialise the Tcl interpreter. */
       cinterp = ccdTclStart( status );
 
-/* Set the value of Tcl variables to be passed into the script. */
+/* Construct a list of NDF Set constructor lists available to the Tcl 
+   interpreter as the value of the NDFSETS variable. */
+      for ( i = 1; i <= *nset; i++ ) {
+         F77_CALL(grp_get)( INTEGER_ARG(snamgr), INTEGER_ARG(&i),
+                            INTEGER_ARG(&one), CHARACTER_ARG(fsetname),
+                            INTEGER_ARG(status)
+                            TRAIL_ARG(fsetname) );
+         cnfImprt( fsetname, GRP__SZNAM, setname );
+         if ( *setname == '\0' || imemof[ i ] - imemof[ i - 1 ] == 1 ) {
+            strcpy( buffer, "{}" );
+         }
+         else {
+            sprintf( buffer, "{set:%s}", setname );
+         }
+         ccdTclSetC( cinterp, "tmplist", buffer, status );
+         for ( j = imemof[ i - 1 ]; j <= imemof[ i ] - 1; j++ ) {
+            F77_CALL(grp_get)( INTEGER_ARG(ndfgr), INTEGER_ARG(&imem[ j - 1 ]),
+                               INTEGER_ARG(&one), CHARACTER_ARG(fndfname),
+                               INTEGER_ARG(status)
+                               TRAIL_ARG(fndfname) );
+            cnfImprt( fndfname, GRP__SZNAM, ndfname );
+            ccdTclAppC( cinterp, "tmplist", ndfname, status );
+         }
+         ccdTclDo( cinterp, "lappend NDFSETS $tmplist", status );
+      }
+
+/* Set the value of other Tcl variables to be passed into the script. */
       if ( ( cmstyle = malloc( mstyle_length + 1 ) ) == NULL ) {
          *status = SAI__ERROR;
          errRep( " ", "Memory allocation failed", status );
          return;
       }
-      for ( i = 0; i < *nndf; i++ ) {
-         cnfImpn( ndfnms + i * ndfnms_length, ndfnms_length, BUFLENG - 1, 
-                  buffer );
-         ccdTclAppC( cinterp, "NDFNAMES", buffer, status );
-      }
       cnfImprt( mstyle, mstyle_length, cmstyle );
-      ccdTclSetI( cinterp, "REFPOS", *refpos - 1, status );
-      ccdTclSetI( cinterp, "MAXPOS", *maxpos, status );
+      ccdTclSetI( cinterp, "REFSET", *refset - 1, status );
+      ccdTclSetI( cinterp, "MAXPOS", 0, status );
       ccdTclSetD( cinterp, "PERCLO", percnt[ 0 ], status );
       ccdTclSetD( cinterp, "PERCHI", percnt[ 1 ], status );
       ccdTclSetD( cinterp, "ZOOM", *zoom, status );
@@ -149,17 +204,22 @@
 /* Retrieve the variables set by the script. */
       if ( *status == SAI__OK ) {
          fmt = "lindex [ lindex [ lindex $POINTS %d ] %d ] %d";
-         for ( i = 0; i < *nndf; i++ ) {
+         for ( i = 0; i < *nset; i++ ) {
             sprintf( buffer, "lindex $NPOINT %d", i );
-            ccdTclGetI( cinterp, buffer, npoint + i, status );
-            for ( j = 0; j < npoint[ i ] && j < *maxpos; j++ ) {
-               ix = i * *maxpos + j;
+            ccdTclGetI( cinterp, buffer, &npoint[ i ], status );
+            px = (double *) ccdMall( "_DOUBLE", npoint[ i ], status );
+            py = (double *) ccdMall( "_DOUBLE", npoint[ i ], status );
+            pi = (int *) ccdMall( "_INTEGER", npoint[ i ], status );
+            ipxpos[ i ] = cnfFptr( px );
+            ipypos[ i ] = cnfFptr( py );
+            ipind[ i ] = cnfFptr( pi );
+            for ( j = 0; j < npoint[ i ]; j++ ) {
                sprintf( buffer, fmt, i, j, 0 );
-               ccdTclGetI( cinterp, buffer, index + ix, status );
+               ccdTclGetI( cinterp, buffer, &pi[ j ], status );
                sprintf( buffer, fmt, i, j, 1 );
-               ccdTclGetD( cinterp, buffer, xpos + ix, status );
+               ccdTclGetD( cinterp, buffer, &px[ j ], status );
                sprintf( buffer, fmt, i, j, 2 );
-               ccdTclGetD( cinterp, buffer, ypos + ix, status );
+               ccdTclGetD( cinterp, buffer, &py[ j ], status );
             }
          }
          ccdTclGetD( cinterp, "set ZOOM", zoom, status );
