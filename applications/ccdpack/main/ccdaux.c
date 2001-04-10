2b977a1859969aed5cfd7a4e245506bf5d77849e
Mark Taylor <m.b.taylor@bristol.ac.uk>
2001-04-10 10:26:25 +0000

    Initial revision
    svn path=/trunk/; revision=8244

---
+++ applications/ccdpack/main/ccdaux.c
@@ -0,0 +1,45 @@
+#include "cnf.h"
+#include "dat_par.h"
+#include "sae_par.h"
+
+   void *ccdMall( char *type, int size, int *status ) {
+/*
+*+
+*  Name:
+*     ccdMall
+*
+*  Purpose:
+*     C wrapper for fortran CCD1_MALL routine.
+*
+*  Arguments:
+*     type = char *
+*        HDS type of memory to allocate, as a null-terminated string.
+*     size = int
+*        Number of elements of type type to allocate.
+*     status = int
+*        The global status.
+*
+*  Return Value:
+*     A pointer to a block of memory which will hold size elements of
+*     type type.  This pointer has been registered with the CCDPACK
+*     memory allocation machinery (and a fortiori the CNF memory 
+*     allocation machinery) and so must be deallocated using CCD1_MFREE.
+*     The pointer returned is a C pointer, and thus suitable for direct
+*     use by C code.  If it is to be used by Fortran code it must
+*     be processed with the function cnfFptr.
+*-
+*/
+      DECLARE_CHARACTER( ftype, DAT__SZTYP );
+      F77_POINTER_TYPE ptr;
+
+      if ( *status != SAI__OK ) return (void *) NULL;
+
+      cnfExprt( type, ftype, DAT__SZTYP );
+      F77_CALL(ccd1_mall)( INTEGER_ARG(&size), CHARACTER_ARG(ftype),
+                           POINTER_ARG(&ptr), INTEGER_ARG(status)
+                           TRAIL_ARG(ftype) );
+      return cnfCptr( ptr );
+   }
+                           
+
+/* $Id$ */
---
+++ applications/ccdpack/main/ccdaux.h
@@ -0,0 +1,28 @@
+/*
+*+
+*  Name:
+*     ccdaux.h
+
+*  Type of Module:
+*     C header file.
+
+*  Purpose:
+*     Include file for general purpose CCDPACK C functions.
+
+*  Description:
+*     This include file declares some miscellaneous routines used by
+*     CCDPACK C code.
+
+*  Authors:
+*     MBT: Mark Taylor (STARLINK)
+
+*  History:
+*     5-APR-2001 (MBT):
+*        Original version.
+*-
+*/
+
+/* Declare function prototypes. */
+      void *ccdMall( char *type, int size, int *status );
+
+/* $Id$ */
