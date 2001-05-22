28be774346ab2e5778cc93cb90528a9a409e60ca
David S. Berry <d.berry@jach.hawaii.edu>
2001-05-22 13:14:08 +0000

    Initial version
    svn path=/trunk/; revision=8574

---
+++ applications/kaprh/kaprh_mon.f
@@ -0,0 +1,127 @@
+      SUBROUTINE KAPRH_MON( STATUS )
+*+
+*  Name:
+*     KAPRH_MON
+
+*  Purpose:
+*     Top-level KAPRH subroutine for A-task monolith on UNIX.
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Type of Module:
+*     ADAM A-task
+
+*  Invocation:
+*     CALL KAPRH_MON( STATUS )
+
+*  Arguments:
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
+
+*  Description:
+*     This is the top-level A-task monolith subroutine for the KAPRH
+*     suite of A-tasks.  Each KAPRH command is an alias to a softlink
+*     that points to this monolith.  The chosen command is obtained
+*     from the ADAM routine TASK_GET_NAME.  The command may be specified
+*     from the shell or ICL.  Given the command, the requested A-task
+*     is called after a successful matching of the input string with a
+*     valid task name.  If there is no match, an error report is made.
+
+*  Implementation Deficiencies:
+*     The input string has to be forced to upper-case.
+
+*  Authors:
+*     DSB: David S. Berry (STARLINK)
+*     {enter_new_authors_here}
+
+*  History:
+*     22-MAY-2001 (DSB):
+*        Original version.
+*     {enter_further_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+
+*  Type Definitions:
+      IMPLICIT  NONE              ! no implicit typing allowed
+
+*  Global Constants:
+      INCLUDE  'SAE_PAR'          ! SSE global definitions
+
+*  Status:
+      INTEGER  STATUS
+
+*  External References:
+      LOGICAL LPG_AGAIN           ! Invoke the application again?
+
+*  Local Variables:
+      CHARACTER NAME * ( 15 )     ! Task name from the command
+      LOGICAL DISAB               ! Disable looping?
+      LOGICAL VERB                ! Run in verbose mode?
+      REAL DELAY                  ! Delay between loops in seconds
+
+*.
+
+*  Check the inherited status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  Obtain the command from the environment.  This returns uppercase
+*  names.
+      CALL TASK_GET_NAME( NAME, STATUS )
+
+*  Identify and execute the task.
+*  ==============================
+
+*  Make CAT run in quiet mode.
+      CALL CAT_TUNES( 'QUIET', 'YES', STATUS )
+
+*  Define the current application name for history. The package version
+*  number gets substituted in here when the KAPPA release source tar file 
+*  is contructed.
+      CALL NDF_HAPPN( NAME // ' (KAPPA PKG_VERS)', STATUS )
+
+*  See if NDF names should be reported when looping.
+      CALL KPG1_ENVDF( 'KAPPA_REPORT_NAMES', VERB, STATUS )
+
+*  If not, see if verbose reporting is required.
+      IF( .NOT. VERB ) CALL KPG1_VERB( VERB, 'KAPPA', STATUS )      
+
+*  See if looping should be disabled.
+      CALL KPG1_ENVDF( 'KAPPA_LOOP_DISABLE', DISAB, STATUS )
+
+*  See if a delay should be included between invocations.
+      DELAY = 0.0
+      CALL KPG1_ENV0R( 'KAPPA_LOOP_DELAY', DELAY, STATUS )
+
+*  Initialise the common blocks used to control multiple invokation of
+*  applications to process lists of NDFs.
+      CALL LPG_START( VERB, DELAY, DISAB, STATUS )
+
+*  Loop round invoking the task for each set of NDFs specified by the user.
+      DO WHILE( LPG_AGAIN( STATUS ) )
+
+*  Check the string against valid A-task names---if matched then call
+*  the relevant A-task
+
+*  Produces a greyscale plot of a 2-d NDF.
+         IF ( NAME .EQ. 'GREYPLOT' ) THEN
+            CALL GREYPLOT ( STATUS )
+
+*  No such option exists.
+         ELSE
+            STATUS = SAI__ERROR
+            CALL MSG_SETC( 'CMD', NAME )
+            CALL ERR_REP( 'KAPRH_MON_NOCOM',
+     :     'KAPRH: No such option ^CMD.', STATUS )
+
+         END IF
+
+      END DO
+
+*  End and return.
+
+      END
+
---
+++ applications/kaprh/kps1_imzbo.f
@@ -0,0 +1,100 @@
+      SUBROUTINE KPS1_IMZBO( BOUNDS, ZONE, STATUS )
+*+
+*  Name:
+*     KPS1_IMZBO
+
+*  Purpose:
+*     Creates a smaller image zone after making allowances for a border.
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Invocation:
+*     CALL KPS1_IMZBO( BOUNDS, ZONE, STATUS )
+
+*  Description:
+*     This routine takes fractional bounds in an SGS zone to define a
+*     new SGS zone preserving the world-coordinate system.
+
+*  Arguments:
+*     BOUNDS( 4 ) = REAL (Given)
+*        The fractional bounds in the unit zone where the new image
+*        zone is to be located.  The order is x_lower, x_upper, y_lower,
+*        and y_upper.  These are not validated as this is called by
+*        display applications that have these in parameter statements.
+*     ZONE = INTEGER (Returned)
+*        The new image zone after clipping.
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
+
+*  Prior Requirements:
+*     -  SGS must be active.
+
+*  Authors:
+*     MJC: Malcolm J. Currie (STARLINK)
+*     {enter_new_authors_here}
+
+*  History:
+*     1991 March 5 (MJC):
+*        Original version.
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+      
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+
+*  Arguments Given:
+      REAL
+     :  BOUNDS( 4 )
+
+*  Arguments Returned:
+      INTEGER
+     :  ZONE
+
+*  Status:
+      INTEGER STATUS             ! Global status
+
+*  Local Variables:
+      REAL
+     :  T1, T2,                  ! Dummy variables for rescaling image
+     :  X1, X2, Y1, Y2,          ! Zone size in world co-ordinates
+     :  XM, YM                   ! Zone size in metres (not used)
+
+*.
+
+*    Check the inherited global status.
+
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*    Obtain the bounds of the current zone.
+
+      CALL SGS_IZONE( X1, X2, Y1, Y2, XM, YM )
+
+*    Redefine the bounds of the image zone in terms of device pixels,
+*    otherwise the centering and scaling will be wrong.  Dummy variables 
+*    must be used as the limits are changing during the calculations.
+
+      T1 = X1
+      T2 = X2
+      X1 = T1 + BOUNDS( 1 ) * ( T2 - T1 )
+      X2 = T1 + BOUNDS( 2 ) * ( T2 - T1 )
+
+      T1 = Y1
+      T2 = Y2
+      Y1 = T1 + BOUNDS( 3 ) * ( T2 - T1 )
+      Y2 = T1 + BOUNDS( 4 ) * ( T2 - T1 )
+
+*    This defines the new "initial" image zone and the world
+*    co-ordinates.
+
+      CALL SGS_ZONE( X1, X2, Y1, Y2, ZONE, STATUS )
+      CALL SGS_SW( X1, X2, Y1, Y2, STATUS )
+
+      END
