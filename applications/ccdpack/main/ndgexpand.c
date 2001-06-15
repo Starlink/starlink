25345e8b6ca67766d448ddf7fd8be0ed7cd4ddf8
Mark Taylor <m.b.taylor@bristol.ac.uk>
2001-06-15 14:36:29 +0000

    added -sup flag.
    svn path=/trunk/; revision=8689

--- applications/ccdpack/main/ndgexpand.c
+++ applications/ccdpack/main/ndgexpand.c
@@ -13,11 +13,32 @@
 *     Expands an NDG group expression to a list of names.
 
 *  Usage:
-*     ndgexpand group
+*     ndgexpand ?-sup? group-expression
 
 *  Description:
 *     An NDG group expression is expanded to give a list of the NDF 
-*     structures to which it refers.
+*     structures to which it refers.  Either the NDF name alone or
+*     the full list of NDG supplementary data can be returned 
+*     according to the absence or presence of the -sup flag.
+
+*  Flags:
+*     -sup
+*        If the -sup flag is absent, then the return value is a list of
+*        the fully qualified NDF names found in the group expression.
+*
+*        If the -sup flag is present, then for each NDF a list containing
+*        the NDG supplementary information is returned instead.
+*        This is the data returned from NDG_GTSUP; for each NDF the
+*        list contains six elements:
+*           0 - NDF slice specification (if any) 
+*           1 - HDS path (if any) 
+*           2 - File type 
+*           3 - Base file name 
+*           4 - Directory path 
+*           5 - Full NDF specification 
+*
+*        Note that the final element of this list is the same as the 
+*        single value returned with no -sup flag.
 
 *  Arguments:
 *     group = string
@@ -50,10 +71,21 @@
                      Tcl_Obj *CONST objv[] ) {
 /**********************************************************************/
 
+/* Local constants. */
+#define NFIELD 6                    /* Number of fields returned by NDG_GTSUP */
+      const int nfield = NFIELD;    /* Number of fields returned by NDG_GTSUP */
+      const F77_INTEGER_TYPE one = 1; /* Unity */
+      const F77_LOGICAL_TYPE false = F77_FALSE; /* Logical false */
+
 /* Local variables. */
       char *grpex;                  /* Input group expression */
       char name[ GRP__SZNAM + 1 ];  /* Name of an expanded item */
+      char field[ GRP__SZNAM + 1 ]; /* Supplementary data field */
+      int gtsup;                    /* Get supplementary information? */
       int i;                        /* Loop variable */
+      int j;                        /* Loop variable */
+      int nflag;                    /* Number of flags supplied */
+      Tcl_Obj *ob;                  /* Element of result list */
       Tcl_Obj *result;              /* Result returned to Tcl caller */
       F77_INTEGER_TYPE errgid;      /* GRP identifier for inaccessible group */
       F77_INTEGER_TYPE outgid;      /* GRP identifier for output group */       
@@ -61,19 +93,26 @@
       F77_LOGICAL_TYPE flag;        /* NDG trailing character flag */
       DECLARE_CHARACTER( fgrpex, GRP__SZNAM ); /* Fortran version of grpex */
       DECLARE_CHARACTER( fname, GRP__SZNAM); /* Fortran version of name */
+      char ffields[ GRP__SZNAM * NFIELD ]; /* Fortran supplementary data */
+      int ffields_length = GRP__SZNAM; /* Length of ffields strings */
 
-/* Local constants. */
-      const F77_INTEGER_TYPE one = 1; /* Unity */
-      const F77_LOGICAL_TYPE false = F77_FALSE; /* Logical false */
+/* Process flags. */
+      gtsup = 0;
+      nflag = 0;
+      if ( objc > 1 + nflag &&
+           ! strncmp( Tcl_GetString( objv[ 1 + nflag ] ), "-sup", 3 ) ) {
+         gtsup = 1;
+         nflag++;
+      }
 
 /* Check syntax. */
-      if ( objc != 2 ) {
-         Tcl_WrongNumArgs( interp, 1, objv, "group-expression" );
+      if ( objc != 2 + nflag ) {
+         Tcl_WrongNumArgs( interp, 1, objv, "?-sup? group-expression" );
          return TCL_ERROR;
       }
 
 /* Get arguments. */
-      grpex = Tcl_GetString( objv[ 1 ] );
+      grpex = Tcl_GetString( objv[ 1 + nflag ] );
 
 /* Prepare arguments for passing to NDG. */
       cnfExprt( grpex, fgrpex, fgrpex_length );
@@ -93,18 +132,41 @@
       result = Tcl_NewListObj( 0, (Tcl_Obj **) NULL );
 
 /* Unpack the names from the group into the Tcl result object. */
-      STARCALL(
-         for ( i = 1; i <= size; i++ ) {
-            F77_CALL(grp_get)( INTEGER_ARG(&outgid), INTEGER_ARG(&i),
-                               INTEGER_ARG(&one), CHARACTER_ARG(fname),
-                               INTEGER_ARG(status)
-                               TRAIL_ARG(fname) );
+      for ( i = 1; i <= size; i++ ) {
+
+/* Only the name is required. */
+         if ( ! gtsup ) {
+            STARCALL(
+               F77_CALL(grp_get)( INTEGER_ARG(&outgid), INTEGER_ARG(&i),
+                                  INTEGER_ARG(&one), 
+                                  CHARACTER_ARG(fname), INTEGER_ARG(status)
+                                  TRAIL_ARG(fname) );
+            )
             cnfImprt( fname, fname_length, name );
-            Tcl_ListObjAppendElement( interp, result, 
-                                      Tcl_NewStringObj( name, -1 ) );
+            ob = Tcl_NewStringObj( name, -1 );
          }
 
+/* Supplementary information is required. */
+         else {
+            STARCALL(
+               F77_CALL(ndg_gtsup)( INTEGER_ARG(&outgid), INTEGER_ARG(&i),
+                                    CHARACTER_ARG(ffields), INTEGER_ARG(status)
+                                    TRAIL_ARG(ffields) );
+            )
+            ob = Tcl_NewListObj( 0, (Tcl_Obj **) NULL );
+            for ( j = 0; j < NFIELD; j++ ) {
+               cnfImprt( ffields + GRP__SZNAM * j, GRP__SZNAM, field );
+               Tcl_ListObjAppendElement( interp, ob, 
+                                         Tcl_NewStringObj( field, -1 ) );
+            }
+         }
+
+/* Append this element of the answer to the result object. */
+         Tcl_ListObjAppendElement( interp, result, ob );
+      }
+
 /* Annul the group. */
+      STARCALL(
          F77_CALL(grp_delet)( INTEGER_ARG(&outgid), INTEGER_ARG(status) );
       )
 
