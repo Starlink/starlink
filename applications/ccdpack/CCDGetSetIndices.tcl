2320441bebf87da4e73e91c58303b79a3de6d311
Mark Taylor <m.b.taylor@bristol.ac.uk>
2001-07-06 09:55:50 +0000

    Added force parameter and help tags.
    svn path=/trunk/; revision=8794

--- applications/ccdpack/CCDGetSetIndices.tcl
+++ applications/ccdpack/CCDGetSetIndices.tcl
@@ -1,4 +1,4 @@
-   proc CCDGetSetIndices { Topwin } {
+   proc CCDGetSetIndices { Topwin {force 0} } {
 #+
 #  Name:
 #     CCDGetSetIndices
@@ -16,6 +16,11 @@
 #  Arguments:
 #     Topwin = window (read)
 #        The command name of a top-level widget to contain this form.
+#     force = boolean (read)
+#        If force is supplied and true, then this will definitely result
+#        in a widget being posted.  Otherwise the widget will only
+#        be posted if the user has not seen it before (i.e. if there
+#        is not currently a valid CCDsetindices value).
 
 #  Global Variables:
 #     CCDsetindices = list of integers (read and write)
@@ -46,64 +51,79 @@
       global setsize
 
 #  Initialise set indices variable list if it does not already exist.
-      if { ! [info exists CCDsetindices] } { 
+      if { ! [info exists CCDsetindices] || \
+           ! [info exists CCDsetindicesvalid] } { 
          set CCDsetindices {} 
+         set CCDsetindicesvalid 0
       }
       set setsize [llength $CCDsetindices]
 
+#  Only bother posting the widget if we need to.
+      if { $force || ! $CCDsetindicesvalid } {
+
 #  Create the widget components.
-      CCDCcdWidget Top top Ccd_toplevel $Topwin -title "Size of each Set"
-      CCDTkWidget Frame frame frame $top.center
-      CCDTkWidget Value value scale $frame.slider \
-         -variable setsize \
-         -orient horizontal \
-         -from 0 -to 9 \
-         -label "Number of NDFs per Set" \
-         -showvalue 1 \
-         -tickinterval 1 \
-         -length 256
-      CCDCcdWidget Menu menu Ccd_helpmenubar $Top.menubar
-      CCDCcdWidget Choice choice Ccd_choice $Top.choice -standard 1
+         CCDCcdWidget Top top Ccd_toplevel $Topwin -title "Size of each Set"
+         CCDTkWidget Frame frame frame $top.center
+         CCDTkWidget Value value scale $frame.slider \
+            -variable setsize \
+            -orient horizontal \
+            -from 1 -to 9 \
+            -label "Number of NDFs per Set" \
+            -showvalue 1 \
+            -tickinterval 1 \
+            -length 256
+         CCDCcdWidget Menu menu Ccd_helpmenubar $Top.menubar
+         CCDCcdWidget Choice choice Ccd_choice $Top.choice -standard 1
 
 #  Configure the choice buttons.
-      $Choice addcommand OK "
-         if { \[llength \$CCDsetindices\] != \$setsize } {
-            set CCDsetindices {}
-            for { set i 1 } { \$i <= \$setsize } { incr i } {
-               lappend CCDsetindices \$i
+         $Choice addcommand OK "
+            if { \[llength \$CCDsetindices\] != \$setsize } {
+               set CCDsetindices {}
+               for { set i 1 } { \$i <= \$setsize } { incr i } {
+                  lappend CCDsetindices \$i
+               }
             }
-         }
-         set CCDsetindicesvalid 1
-         $Top kill $Top
-      "
-      $Choice addcommand Cancel "
-         set CCDsetindicesvalid 0
-         $Top kill $Top
-      "
+            set CCDsetindicesvalid 1
+            $Top kill $Top
+         "
+         $Choice addcommand Cancel "
+            set CCDsetindicesvalid 0
+            $Top kill $Top
+         "
 
 #  Set window acceptance as the default button if the current value of 
 #  CCDsetindices is sensible.
-      if { [llength $CCDsetindices] > 1 } {
-         $Choice focus OK
-      }
+         if { [llength $CCDsetindices] > 1 } {
+            $Choice focus OK
+         }
 
 #  Add menu options.
-      $Menu addcommand File {Accept Window} "$Choice invoke OK"
-      $Menu addcommand File {Close Window} "$Choice invoke Cancel"
-      $Menu addcommand File {Exit} CCDExit
+         $Menu addcommand File {Accept Window} "$Choice invoke OK"
+         $Menu addcommand File {Close Window} "$Choice invoke Cancel"
+         $Menu addcommand File {Exit} CCDExit
+         $Menu addcommand Options {No Sets} "
+            set CCDglobalpars(USESET) \"FALSE\"
+            $Choice invoke Cancel
+         "
 
 #  Pack the widget.
-      pack $menu -fill x -side top
-      pack $frame -fill both -expand 1
-      pack $value -fill x -expand 1
-      pack $choice -side bottom -fill x 
+         pack $menu -fill x -side top
+         pack $frame -fill both -expand 1
+         pack $value -fill x -expand 1
+         pack $choice -side bottom -fill x 
+
+#  Add contextual help.
+         $Top sethelp ccdpack CCDGetSetIndicesWindow
+         $Menu sethelpitem {On Window} ccdpack CCDGetSetIndicesWindow
+         $Menu sethelp all ccdpack CCDGetSetIndicesMenu
 
 #  Position the window.
-      wm withdraw $top
-      CCDCentreWindow $Top [winfo parent $top]
-      wm deiconify $top
+         wm withdraw $top
+         CCDCentreWindow $Top [winfo parent $top]
+         wm deiconify $top
 
 #  Wait for interaction to finish.
-      CCDWindowWait $Top
+         CCDWindowWait $Top
+      }
    }
 # $Id$
