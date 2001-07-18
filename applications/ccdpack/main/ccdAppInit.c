d437b92c3df6b317795f0376a6d2f735f8f97fa2
Mark Taylor <m.b.taylor@bristol.ac.uk>
2001-07-18 18:16:02 +0000

    Removed the ndfcentroffset command from ccdwish (no longer needed).
    svn path=/trunk/; revision=8862

--- applications/ccdpack/main/ccdAppInit.c
+++ applications/ccdpack/main/ccdAppInit.c
@@ -170,8 +170,6 @@ Tcl_AppInit(interp)
 
     Tcl_CreateObjCommand( interp, "ndfdrawpair", NdfDrawpair,
                           (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL );
-    Tcl_CreateObjCommand( interp, "ndfcentroffset", NdfCentroffset,
-                          (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL );
     Tcl_CreateObjCommand( interp, "ccdputs", CcdputsCmd,
                           (ClientData) NULL, (Tcl_CmdDeleteProc *) NULL );
     Tcl_CreateObjCommand( interp, "ndgexpand", NdgexpandCmd,
