b470849659f9d559c69d9490726b328669a692e8
Mark Taylor <m.b.taylor@bristol.ac.uk>
2001-07-25 14:41:11 +0000

    Redefined ccdTclDo - it was misdefined before, but in any case I haev rewritten it.
    svn path=/trunk/; revision=8908

--- applications/ccdpack/main/tcltalk.h
+++ applications/ccdpack/main/tcltalk.h
@@ -40,9 +40,9 @@
 
 
    ccdTcl_Interp *ccdTclStart( int *status );
-   void ccdTclDo( ccdTcl_Interp *cinterp, int callret, int *status );
    void ccdTclStop( ccdTcl_Interp *cinterp, int *status );
    void ccdTclRun( ccdTcl_Interp *cinterp, char *filename, int *status );
+   void ccdTclDo( ccdTcl_Interp *cinterp, char *script, int *status );
    void ccdTclAppC( ccdTcl_Interp *cinterp, char *name, char *value, 
                     int *status );
    void ccdTclSetI( ccdTcl_Interp *cinterp, char *name, int value, 
@@ -56,8 +56,6 @@
    void ccdTclGetD( ccdTcl_Interp *cinterp, char *script, double *value, 
                     int *status );
    char *ccdTclGetC( ccdTcl_Interp *cinterp, char *script, int *status );
-#define ccdTclDo(cinterp,script,status) \
-   (void) ccdTclGetC( cinterp, script, status )
 
 #endif  /* CCD_TCLTALK_DEFINED */
 
