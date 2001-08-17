0bbc8f30f7e318bd7d6833bfd32fe73f3383c8f1
David S. Berry <d.berry@jach.hawaii.edu>
2001-08-17 15:50:44 +0000

    Initial version
    svn path=/trunk/; revision=8990

---
+++ libraries/ard/ard1_snkta.f
@@ -0,0 +1,133 @@
+      SUBROUTINE ARD1_SNKTA( STATUS )
+*+
+*  Name:
+*     ARD1_SNKTA
+
+*  Purpose:
+*     Write AST_ data as text to a GRP group.
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Invocation:
+*     CALL ARD1_SNKTA( STATUS )
+
+*  Description:
+*     This is a service routine to be provided as a "sink" routine for
+*     the AST_CHANNEL function. It takes data in the form of text (in
+*     response to writing an AST_ object to a Channel) and delivers it
+*     to a GRP group for storage.
+
+*  Arguments:
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
+
+*  Authors:
+*     DSB: David S. Berry (STARLINK)
+*     {enter_new_authors_here}
+
+*  History:
+*     17-AUG-2001 (DSB):
+*        Original version.
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'GRP_PAR'          ! GRP constants 
+      INCLUDE 'ARD_ERR'          ! ARD error constants 
+      INCLUDE 'ARD_CONST'        ! ARD private constants 
+      
+*  Global Variables:
+      INCLUDE 'ARD_COM'          ! ARD common blocks
+*        CMN_AGRP = INTEGER (Read)
+*           GRP identifier for group holding AST_ data.
+*        CMN_ASTLN = INTEGER (Read and Write)
+*           Next element to use in the group holding AST_ data.
+      
+*  Global Status:
+      INTEGER STATUS             
+
+*  External References:
+      INTEGER CHR_LEN            ! Significant length of a string
+
+*  Local Constants:
+      INTEGER SZTEXT             ! Size of text buffer
+      PARAMETER ( SZTEXT = ( GRP__SZNAM - 1 ) * ( ARD__MXACL + 1 ) )
+
+      INTEGER ASTTSZ             ! Max. length of text which can be stored
+      PARAMETER ( ASTTSZ = GRP__SZNAM - 4 ) ! in a single element
+
+*  Local Variables:
+      CHARACTER FLAG*1           ! Flag character
+      CHARACTER TEXT*( SZTEXT + 1 ) ! Buffer for AST_ text
+      INTEGER I1                 ! Index of first character to write
+      INTEGER I2                 ! Index of last character to write
+      INTEGER L                  ! Number of characters in AST_ text
+*.
+
+*  Check inherited global status.
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  Obtain the text to be written.
+      IF ( STATUS .EQ. SAI__OK ) THEN
+         CALL AST_GETLINE( TEXT, L, STATUS )
+
+*  If the text was obtained successfully, then check its length. If
+*  this exceeds what can safely be handled, report an error.
+         IF ( ( STATUS .EQ. SAI__OK ) .AND. ( L .GT. SZTEXT ) ) THEN
+            STATUS = ARD__BADAR
+            CALL MSG_SETI( 'ACL', ARD__MXACL )
+            CALL ERR_REP( 'ARD1_SNKTA_XS', 'Maximum number of '//
+     :                    'continuation lines (^ACL) exceeded - '//
+     :                    'output text is too long.', STATUS )
+         END IF
+      END IF
+
+*  Remove any leading and trailing blanks (this removes any indentation).
+      IF ( STATUS .EQ. SAI__OK ) THEN
+         IF ( L .GT. 0 ) THEN
+            CALL CHR_LDBLK( TEXT( : L ) )
+            L = CHR_LEN( TEXT( : L ) )
+         END IF
+
+*  Initialise the flag character for continuation lines.
+         IF ( L .GT. 0 ) THEN
+            FLAG = ' '
+
+*  Loop to write the text into the group as a sequence of lines of length 
+*  one less than the max. number of characters in each group element (the 
+*  first character of each line is reserved for use as a flag character).
+            DO 1 I1 = 1, L, ASTTSZ - 1
+
+*  Find the last character to be included in the current line.
+               IF ( STATUS .EQ. SAI__OK ) THEN
+                  I2 = MIN( I1 + ASTTSZ - 2, L )
+
+*  Write the line, prefixed by the flag character.
+                  CALL GRP_PUT( CMN_AGRP, 1, FLAG // TEXT( I1 : I2 ),
+     :                          CMN_ASTLN, STATUS )
+
+*  Increment the line number to be used next, and note that the next
+*  line (if any) will be a continuation line.
+                  IF ( STATUS .EQ. SAI__OK ) THEN
+                     CMN_ASTLN = CMN_ASTLN + 1
+                     FLAG = '+'
+                  END IF
+               END IF
+
+*  Quit looping if an error occurs.
+               IF ( STATUS .NE. SAI__OK ) GO TO 2
+ 1          CONTINUE
+ 2          CONTINUE
+         END IF
+      END IF
+
+      END
---
+++ libraries/ard/ard_ptwcs.f
@@ -0,0 +1,132 @@
+      SUBROUTINE ARD_PTWCS( IWCS, IGRP, STATUS )
+*+
+*  Name:
+*     ARD_PTWCS
+
+*  Purpose:
+*     Construct an ARD WCS statement and append it to a GRP group.
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Invocation:
+*     CALL ARD_PTWCS( IWCS, IGRP, STATUS )
+
+*  Description:
+*     This routine creates a WCS statement describing the supplied
+*     FrameSet, and appends the statement to the end of the supplied GRP
+*     group.
+
+*  Arguments:
+*     IWCS = INTEGER (Given)
+*        An AST pointer to a FrameSet.
+*     IGRP = INTEGER (Given and Returned)
+*        A GRP group identifier. If GRP__NOID is supplied, a new group is
+*        created and its identifier returned.
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
+
+*  Authors:
+*     DSB: David Berry (STARLINK)
+*     {enter_new_authors_here}
+
+*  History:
+*     17-JUL-2001 (DSB):
+*        Original version.
+*     {enter_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+      
+*  Type Definitions:
+      IMPLICIT NONE              ! No implicit typing
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'GRP_PAR'          ! GRP constants 
+      INCLUDE 'AST_PAR'          ! AST constants and function declarations
+      INCLUDE 'ARD_ERR'          ! ARD error constants
+      INCLUDE 'ARD_CONST'        ! ARD private constants
+
+*  Global Constants:
+      INCLUDE 'ARD_COM'          ! ARD common blocks
+*        CMN_AGRP= INTEGER (Write)
+*           GRP identifier for the group holding AST_ data.
+*        CMN_ASTLN = INTEGER (Write)
+*           Next element to use in the group holding AST_ data.
+
+*  Arguments Given:
+      INTEGER IWCS
+      INTEGER IGRP
+
+*  Status:
+      INTEGER STATUS             ! Global status
+
+*  External References:
+      EXTERNAL ARD1_SNKTA
+
+*  Local Variables:
+      INTEGER CHAN               ! Pointer to an AST Channel
+      INTEGER SIZE0              ! Initial size of the supplied group
+      LOGICAL NEWGRP             ! Create a new GRP group?
+*.
+
+*  Check the inherited global status.
+      IF( STATUS .NE. SAI__OK ) RETURN
+
+*  If no group was supplied, create one now. Otherwise, note the 
+*  original size of the group.
+      NEWGRP = ( IGRP .EQ. GRP__NOID )
+      IF( NEWGRP ) THEN
+         CALL GRP_NEW( ' ', IGRP, STATUS )
+         SIZE0 = 0
+      ELSE
+         CALL GRP_GRPSZ( IGRP, SIZE0, STATUS )
+      END IF
+
+*  Append the opening "WCS(" string.
+      CALL GRP_PUT( IGRP, 1, 'WCS(', 0, STATUS ) 
+
+*  Create an AST_ Channel to write the supplied Object to the group.
+*  Supply the ARD1_SNKTA routine as the "sink" routine for storing the 
+*  data, and specify that only essential information be included. 
+      CHAN = AST_CHANNEL( AST_NULL, ARD1_SNKTA, 'Full=-1,Comment=0',
+     :                    STATUS )
+
+*  Initialise the index of the first element in the group to be
+*  used by the sink function.
+      CMN_ASTLN = SIZE0 + 2
+
+*  Store the group identifier in common.
+      CMN_AGRP = IGRP 
+
+*  Write the copy of the supplied AST_ object to the Channel, thus
+*  transferring the data to the catalogue. Report an error if the Object 
+*  could not be written.
+      IF( AST_WRITE( CHAN, IWCS, STATUS ) .EQ. 0 .AND. 
+     :    STATUS .EQ. SAI__OK ) THEN
+         STATUS = ARD__BADAR
+         CALL ERR_REP( 'ARD_PTWCS_ERR1', 'ARD_PTWCS: No Objects '//
+     :                 'written.', STATUS )
+      END IF
+
+*  Annul the Channel pointer, thus deleting the Channel.
+      CALL AST_ANNUL( CHAN, STATUS )
+
+*  Finish off with the final ")" string.
+      CALL GRP_PUT( IGRP, 1, ')', 0, STATUS ) 
+
+*  If an error occurred, tidy up.
+      IF( STATUS .NE. SAI__OK ) THEN
+         IF( NEWGRP ) THEN
+            CALL GRP_DELET( IGRP, STATUS )
+         ELSE
+            CALL ERR_BEGIN( STATUS )
+            CALL GRP_SETSZ( IGRP, SIZE0, STATUS ) 
+            CALL ERR_END( STATUS )
+         END IF
+      END IF
+
+      END
