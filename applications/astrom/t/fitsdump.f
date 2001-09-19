faf62893620ea0ca5968b4ef867ad11ec66e27f8
Norman Gray <norman@ASTRO.GLA.AC.UK>
2001-09-19 16:43:05 +0000

    Rationalised (and incidentally updated) crazy regression test
    svn path=/trunk/; revision=9164

--- applications/astrom/t/fitsdump.f
+++ applications/astrom/t/fitsdump.f
@@ -17,16 +17,26 @@
       call ftgiou (ftunit, ftstat)
       call ftopen (ftunit, s, 0, blocksize, ftstat)
       
+      if (ftstat.gt.0) then
+         write (*,'("Can''t open file ",a)') s
+         goto 998
+      endif
+
       keepreading = .true.
       i = 1
-      do while (keepreading)
+      do while (keepreading .and. ftstat.eq.0)
          call ftgrec (ftunit, i, s, ftstat)
-         write (*, '(a)'), s
+*      Omit date record (to make diffing reasonable)
+         if (s(1:4).ne.'DATE') write (*, '(a)'), s
          i = i+1
          if (s(1:3).eq.'END') keepreading = .false.
       enddo
 
+ 998  continue
+
       call ftclos (ftunit, ftstat)
+
+      call ftrprt ('STDERR', ftstat)
 
  999  continue
       end
