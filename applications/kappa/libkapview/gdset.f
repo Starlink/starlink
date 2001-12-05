cff543618be78b59408c33325b802044f62e4bbe
David S. Berry <d.berry@jach.hawaii.edu>
2001-12-05 12:08:51 +0000

    File replaced by SDT
    svn path=/trunk/; revision=9640

--- applications/kappa/libkapview/gdset.f
+++ applications/kappa/libkapview/gdset.f
@@ -37,6 +37,7 @@
 
 *  Authors:
 *     MJC: Malcolm J. Currie  (STARLINK)
+*     DSB: David Berry (STARLINK)
 *     {enter_new_authors_here}
 
 *  History:
@@ -45,6 +46,8 @@
 *     1991 October 17 (MJC):
 *        Added an AGI_ANNUL to ensure that the global parameter is
 *        updated every invocation.
+*     5-DEC-2001 (DSB):
+*        Report output file when using PS devices.
 *     {enter_further_changes_here}
 
 *  Bugs:
@@ -62,21 +65,53 @@
       INTEGER STATUS
 
 *  Local Variables:
-      INTEGER
-     :  PICID                  ! AGI input picture identifier
-
+      CHARACTER STRING*80     ! PGPLOT information
+      INTEGER LENSTR          ! Used length of STRING
+      INTEGER PICID           ! AGI input picture identifier
 *.
 
-*    Check the inherited status on entry.
+*  Check the inherited status on entry.
+      IF( STATUS .NE. SAI__OK ) RETURN
 
-      IF ( STATUS .NE. SAI__OK ) RETURN
-
-*    Open the AGI workstation to read the device.
-
+*  Open the AGI workstation to read the device.
       CALL AGI_ASSOC( 'DEVICE', 'READ', PICID, STATUS )
 
-*    Annul the workstation.
+*  If a postscript device has been opedn, tell the user the name of the
+*  output file, and warn them that this file will be over-writen by each
+*  subsequent graphics command.
+      CALL PGQINF( 'STATE', STRING, LENSTR )
+      IF( STRING .EQ. 'OPEN' ) THEN
 
+         CALL PGQINF( 'TYPE', STRING, LENSTR )
+         IF( INDEX( STRING, 'PS' ) .GT. 0 ) THEN
+
+            CALL PGQINF( 'HARDCOPY', STRING, LENSTR )
+            IF( STRING .EQ. 'YES' ) THEN
+
+               CALL PGQINF( 'FILE', STRING, LENSTR )
+               IF( STRING .NE. ' ' ) THEN 
+                  CALL MSG_BLANK( STATUS )
+                  CALL MSG_SETC( 'F', STRING )
+                  CALL MSG_OUT( ' ', 'All output will go to file ^F. '//
+     :                          'Each subsequent graphics command '//
+     :                          'will overwrite any existing file '//
+     :                          'with this name.', STATUS )
+                  CALL MSG_BLANK( STATUS )
+               END IF
+
+            END IF
+
+         END IF
+
+      END IF
+
+*  Annul the workstation.
       CALL AGI_ANNUL( PICID, STATUS )
+
+*  Issue a context message if an error has occurred.
+      IF( STATUS .NE. SAI__OK ) THEN
+         CALL ERR_REP( 'GDSET_ERR', 'GDSET: Error selecting a '//
+     :                 'graphics device.', STATUS )
+      END IF
 
       END
