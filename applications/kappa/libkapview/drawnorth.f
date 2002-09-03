13c85ffc923ded7c8ed8e0296a2ad7a5792fee2b
David S. Berry <d.berry@jach.hawaii.edu>
2002-09-03 12:14:16 +0000

    File replaced by SDT
    svn path=/trunk/; revision=10110

--- applications/kappa/libkapview/drawnorth.f
+++ applications/kappa/libkapview/drawnorth.f
@@ -38,7 +38,7 @@
 *        The size of the arrow heads are specified by this parameter. Simple 
 *        lines can be drawn by setting the arrow head size to zero. The value 
 *        should be expressed as a fraction of the largest dimension of the 
-*        underlying picture. [current value]
+*        underlying DATA picture. [current value]
 *     BLANK = _LOGICAL (Read)
 *        If TRUE, then the area behind the arrows is blanked before the
 *        arrows are drawn. This is done by drawing a rectangle filled with 
@@ -75,9 +75,9 @@
 *        will determine the 2 axes which are to be used. [!]
 *     LENGTH( 2 ) = _REAL (Read)
 *        The lengths of the arrows, expressed as fractions of the largest 
-*        dimension of the underlying picture. If only one value is supplied, 
-*        both arrows will be drawn with the given length. One of the
-*        supplied values can be set to zero if only a single arrow is 
+*        dimension of the underlying DATA picture. If only one value is 
+*        supplied, both arrows will be drawn with the given length. One of 
+*        the supplied values can be set to zero if only a single arrow is 
 *        required. [current value]
 *     OFRAME = LITERAL (Read)
 *        Specifies the co-ordinate Frame in which the position of the arrows 
@@ -116,7 +116,8 @@
 *        The co-ordinates at which to place the origin of the arrows, in the 
 *        Frame specified by parameter OFRAME. If a null (!) value is
 *        supplied, OFRAME is ignored and the arrows are positions at a
-*        default position near one of the corners, or at the centre. An
+*        default position near one of the corners, or at the centre. The
+*        supplied position can be anywhere within the current picture. An 
 *        error is reported if the arrows and labels cannot be drawn at any 
 *        of these positions. [!]
 *     STYLE = GROUP (Read)
@@ -194,8 +195,8 @@
 *        the default size for the label for the first axis.
 
 *  Notes:
-*     -  An error is reported if there is no existing DATA picture on the
-*     selected graphics device.
+*     -  An error is reported if there is no existing DATA picture within
+*     the current picture on the selected graphics device.
 *     -  The application stores a picture in the graphics database with name 
 *     KEY which contains the two arrows. On exit the current database picture 
 *     for the chosen device reverts to the input picture.
@@ -207,6 +208,9 @@
 *  History:
 *     13-AUG-2002 (DSB):
 *        Original version.
+*     3-SEP-2002 (DSB):
+*        Modified to allow arrows to be palced anywhere within the
+*        current picture.
 *     {enter_further_changes_here}
 
 *-
@@ -225,24 +229,35 @@
       INTEGER STATUS
 
 *  Local Variables:
+      CHARACTER DOM*30           ! Domain name
       DOUBLE PRECISION BBOX( 4 ) ! Box bounds 
       DOUBLE PRECISION CC0( 2 )  ! Current Frame position of origin
       DOUBLE PRECISION GC0( 2 )  ! GRAPHICS Frame position of origin
       DOUBLE PRECISION GC1( 2 )  ! GRAPHICS Frame position of new origin
+      INTEGER CURFRM             ! Pointer to original current Frame
+      INTEGER FRM                ! Frame pointer
+      INTEGER FS                 ! Results FrameSet
+      INTEGER I                  ! Loop count
+      INTEGER IAXIS( 2 )         ! Axes to pick from the old Current Frame
+      INTEGER IBPFRM             ! Index of BASEPIC Frame
       INTEGER ICURR              ! Index of original current Frame
       INTEGER IPIC0              ! AGI id for the original current picture
       INTEGER IPICD0             ! AGI id for the existing DATA picture
       INTEGER IPICK              ! AGI id for the new KEY picture
       INTEGER IPLOT              ! Pointer to AST Plot for DATA picture
+      INTEGER IPLOTC             ! Plot from current picture
       INTEGER NAX                ! No. of current Frame axes
-      INTEGER NLENS              ! No. of LENGTH values supplied
-      LOGICAL BLANK              ! Blank background?
-      INTEGER PMAP               ! AST pointer to a PermMap
       INTEGER NEWCUR             ! Pointer to the new Current Frame
-      INTEGER IAXIS( 2 )         ! Axes to pick from the old Current Frame
+      INTEGER NLENS              ! No. of LENGTH values supplied
+      INTEGER PMAP               ! AST pointer to a PermMap
+      LOGICAL BLANK              ! Blank background?
       LOGICAL GUESS              ! Find a default origin position?
       REAL ARROW                 ! Size of arrow heads
       REAL BLSIZE                ! Relative size of picture area
+      REAL DX1                   ! Lower GRAPHICS X value of DATA picture
+      REAL DX2                   ! Upper GRAPHICS X value of DATA picture
+      REAL DY1                   ! Lower GRAPHICS Y value of DATA picture
+      REAL DY2                   ! Upper GRAPHICS Y value of DATA picture
       REAL GBOX( 4 )             ! Box bounds 
       REAL LENS( 2 )             ! Size of arrows
       REAL MARGIN                ! Amount to extend area by at each edge
@@ -257,6 +272,7 @@
       REAL Y2                    ! Upper GRAPHICS Y value enclosing arrows
       REAL YY1                   ! Lower GRAPHICS Y value enclosing arrows
       REAL YY2                   ! Upper GRAPHICS Y value enclosing arrows
+
 *.
 
 *  Check the inherited global status.
@@ -271,15 +287,56 @@
       CALL KPG1_PLOTA( AST__NULL, 'OLD', ' ', IPIC0, IPICD0, IPLOT, 
      :                 STATUS )
 
+* Save the bounds of this picture.
+      CALL PGQWIN( DX1, DX2, DY1, DY2 )
+
+*  In order to allow the arrows to be drawn anywhere within the original
+*  current picture (as opposed to merely anywhere within the DATA picture),
+*  we get the Plot fro the current picture and into it the Frames from the 
+*  DATA picture Plot, aligning the FrameSets in the BASEPIC Frame....
+
+*  Get the Plot for the original current picture.
+      CALL KPG1_GDGET( IPIC0, AST__NULL, .FALSE., IPLOTC, STATUS )
+
+*  Remove all Frames except the GRAPHICS Frame and the BASEPIC Frame.
+      I = 1
+      DO WHILE( I .LE. AST_GETI( IPLOTC, 'NFRAME', STATUS ) ) 
+         FRM = AST_GETFRAME( IPLOTC, I, STATUS )
+         DOM = AST_GETC( FRM, 'DOMAIN', STATUS )
+         IF( DOM .NE. 'BASEPIC' .AND. DOM .NE. 'GRAPHICS' ) THEN
+            CALL AST_REMOVEFRAME( IPLOTC, I, STATUS )
+            I = I - 1
+         END IF            
+         CALL AST_ANNUL( FRM, STATUS )
+	 I = I + 1
+      END DO
+
+*  Delete the Base (GRAPHICS) Frame from the DATA picture Plot.
+      CALL AST_REMOVEFRAME( IPLOT, AST_GETI( IPLOT, 'BASE', STATUS ),
+     :                      STATUS )
+
+*  Get a pointer to the original current Frame in the DATA picture plot,
+*  and then make the BASEPIC Frame the current Frame.
+      CURFRM = AST_GETFRAME( IPLOT, AST__CURRENT, STATUS )
+      CALL KPG1_ASFFR( IPLOT, 'BASEPIC', IBPFRM, STATUS )
+      CALL AST_SETI( IPLOT, 'CURRENT', IBPFRM, STATUS )
+
+*  Add the Frames from the DATA picture plot into the current picture Plot,
+*  aligning them in the BASEPIC Frame.
+      CALL KPG1_ASMRG( IPLOTC, IPLOT, ' ', .TRUE., 0, STATUS )
+
+*  Reinstate the original current Frame (set to BASPIC by the above call).
+      FS = AST_FINDFRAME( IPLOTC, CURFRM, ' ', STATUS ) 
+
 *  Select the Frame for which arrows are required. The selected Frame
 *  becomes the current FRAME.
       CALL MSG_SETC( 'PIC', 'picture' )
-      CALL KPG1_ASFRM( 'FRAME', 'EPOCH', IPLOT, ' ', ' ', .TRUE.,
+      CALL KPG1_ASFRM( 'FRAME', 'EPOCH', IPLOTC, ' ', ' ', .TRUE.,
      :                 '^PIC', STATUS )
       
 *  If the selected Frame has more than 2 axes, get the user to indicate
 *  which two axes are to be used.
-      NAX = AST_GETI( IPLOT, 'NAXES', STATUS ) 
+      NAX = AST_GETI( IPLOTC, 'NAXES', STATUS ) 
       IF( NAX .LT. 2 .AND. STATUS .EQ. SAI__OK ) THEN
          STATUS = SAI__ERROR
          CALL MSG_SETI( 'NAX', NAX )
@@ -294,34 +351,34 @@
 *  default.
          IAXIS( 1 ) = 1
          IAXIS( 2 ) = 2
-         CALL KPG1_GTAXI( 'USEAXIS', IPLOT, 2, IAXIS, STATUS )
+         CALL KPG1_GTAXI( 'USEAXIS', IPLOTC, 2, IAXIS, STATUS )
 
 *  Create a new Frame by picking the selected axes from the original
 *  Current Frame. This also returns a PermMap which goes from the 
 *  original Frame to the new one, using AST__BAD values for the
 *  un-selected axes.
-         NEWCUR = AST_PICKAXES( IPLOT, 2, IAXIS, PMAP, STATUS )
+         NEWCUR = AST_PICKAXES( IPLOTC, 2, IAXIS, PMAP, STATUS )
 
 *  Add this new Frame into the FrameSet. It becomes the Current Frame.
-         CALL AST_ADDFRAME( IPLOT, AST__CURRENT, PMAP, NEWCUR, 
+         CALL AST_ADDFRAME( IPLOTC, AST__CURRENT, PMAP, NEWCUR, 
      :                      STATUS )
 
       END IF
 
 *  Note the index of the current Frame in the Plot.
-      ICURR = AST_GETI( IPLOT, 'CURRENT', STATUS )
+      ICURR = AST_GETI( IPLOTC, 'CURRENT', STATUS )
 
 *  Allow the user to specify the Frame in which ORIGIN is to be given. 
 *  Any Frame in the Plot can be chosen. The selected Frame becomes the 
 *  Current Frame in the Plot.
       CALL MSG_SETC( 'PIC', 'picture' )
-      CALL KPG1_ASFRM( 'OFRAME', 'EPOCH', IPLOT, ' ', ' ', .TRUE.,
+      CALL KPG1_ASFRM( 'OFRAME', 'EPOCH', IPLOTC, ' ', ' ', .TRUE.,
      :                 '^PIC', STATUS )
       
 *  Get the position of the arrow origin. Check first that no error has
 *  occurred.
       IF( STATUS .NE. SAI__OK ) GOTO 999
-      CALL KPG1_GTPOS( 'ORIGIN', IPLOT, .FALSE., CC0, GC0, STATUS )
+      CALL KPG1_GTPOS( 'ORIGIN', IPLOTC, .FALSE., CC0, GC0, STATUS )
 
 *  If a null value was supplied for ORIGIN, annull the error and pass on
 *  bad values for the origin. These cause a default origin to be found.
@@ -335,20 +392,22 @@
       END IF
 
 *  Reinstate the original current Frame.
-      CALL AST_SETI( IPLOT, 'CURRENT', ICURR, STATUS )
+      CALL AST_SETI( IPLOTC, 'CURRENT', ICURR, STATUS )
 
 *  Get the other parameters defining the arrows.
       CALL PAR_GET0R( 'ARROW', ARROW, STATUS )
       CALL PAR_GET1R( 'LENGTH', 2, LENS, NLENS, STATUS ) 
       IF( NLENS .LT. 2 ) LENS( 2 ) = LENS( 1 )
 
-*  Convert relative values into absolute values (in millimetres).
-      CALL PGQWIN( X1, X2, Y1, Y2 )
-      MXSIZE = MAX( ABS( X2 - X1 ), ABS( Y2 - Y1 ) )
-      MNSIZE = MIN( ABS( X2 - X1 ), ABS( Y2 - Y1 ) )
+* COnvert relative sizes to absolute sizes.
+      MXSIZE = MAX( ABS( DX2 - DX1 ), ABS( DY2 - DY1 ) )
+      MNSIZE = MIN( ABS( DX2 - DX1 ), ABS( DY2 - DY1 ) )
       ARROW = ARROW*MXSIZE
       LENS( 1 ) = LENS( 1 )*MXSIZE
       LENS( 2 ) = LENS( 2 )*MXSIZE
+
+*  Get the bounds of the original current picture.
+      CALL PGQWIN( X1, X2, Y1, Y2 )
 
 *  Create a new Plot from the existing one. All the Plot attributes of
 *  the new Plot are initialised to the "unset" defaults (i.e. any Plot
@@ -365,7 +424,7 @@
       BBOX( 3 ) = DBLE( X2 )
       BBOX( 4 ) = DBLE( Y2 )
 
-      IPLOT = AST_PLOT( IPLOT, GBOX, BBOX, ' ', STATUS )
+      IPLOT = AST_PLOT( IPLOTC, GBOX, BBOX, ' ', STATUS )
 
 *  Establish synonyms for AST graphical element names to be recognised
 *  during the following call to KPG1_ASSET.
@@ -374,7 +433,7 @@
       CALL KPG1_ASPSY( '(ARROW2)', '(AXIS2)', STATUS )
 
 *  Set the attributes of the Plot to give the required plotting style.     
-      CALL KPG1_ASSET( 'KAPPA_DRAWNORTH', 'STYLE', IPLOT, STATUS )
+      CALL KPG1_ASSET( 'KAPPA_DRAWNORTH', 'STYLE', IPLOTC, STATUS )
 
 *  If we are to find a default origin, first try a position near the top
 *  left corner of the underlying DATA picture. This first guess will 
@@ -382,8 +441,8 @@
 *  not be.
       IF( GUESS ) THEN 
          R = 1.3*MAX( LENS( 1 ), LENS( 2 ) )
-         GC0( 1 ) = X1 + R
-         GC0( 2 ) = Y2 - R
+         GC0( 1 ) = DX1 + R
+         GC0( 2 ) = DY2 - R
 
 *  Attempt to draw the arrows and labels in invisible ink, using this
 *  origin. The bouding box of the plotted items is returned.
@@ -396,8 +455,8 @@
 
 *  Modify the origin so that the bounding box does not extend outside the
 *  DATA picture. Leave a 3mm margin.
-            GC1( 1 ) = GC0( 1 ) - XX1 + X1 + 3.0
-            GC1( 2 ) = GC0( 2 ) - YY2 + Y2 - 3.0
+            GC1( 1 ) = GC0( 1 ) - XX1 + DX1 + 3.0
+            GC1( 2 ) = GC0( 2 ) - YY2 + DY2 - 3.0
 
 *  Attempt to draw the arrows and labels in invisible ink, using this
 *  modified origin. 
@@ -417,8 +476,8 @@
 *  If we still need to find a default origin, try a position near the
 *  bottom left corner of the underlying DATA picture. 
       IF( GUESS ) THEN 
-         GC0( 1 ) = X1 + R
-         GC0( 2 ) = Y1 + R
+         GC0( 1 ) = DX1 + R
+         GC0( 2 ) = DY1 + R
 
 *  Attempt to draw the arrows and labels in invisible ink, using this
 *  origin. The bouding box of the plotted items is returned.
@@ -431,8 +490,8 @@
 
 *  Modify the origin so that the bounding box does not extend outside the
 *  DATA picture.
-            GC1( 1 ) = GC0( 1 ) - XX1 + X1 + 3.0
-            GC1( 2 ) = GC0( 2 ) - YY1 + Y1 + 3.0
+            GC1( 1 ) = GC0( 1 ) - XX1 + DX1 + 3.0
+            GC1( 2 ) = GC0( 2 ) - YY1 + DY1 + 3.0
 
 *  Attempt to draw the arrows and labels in invisible ink, using this
 *  modified origin. 
@@ -452,8 +511,8 @@
 *  If we still need to find a default origin, try a position near the
 *  top right corner of the underlying DATA picture. 
       IF( GUESS ) THEN 
-         GC0( 1 ) = X2 - R
-         GC0( 2 ) = Y2 - R
+         GC0( 1 ) = DX2 - R
+         GC0( 2 ) = DY2 - R
 
 *  Attempt to draw the arrows and labels in invisible ink, using this
 *  origin. The bouding box of the plotted items is returned.
@@ -466,8 +525,8 @@
 
 *  Modify the origin so that the bounding box does not extend outside the
 *  DATA picture.
-            GC1( 1 ) = GC0( 1 ) - XX2 + X2 - 3.0
-            GC1( 2 ) = GC0( 2 ) - YY2 + Y2 - 3.0
+            GC1( 1 ) = GC0( 1 ) - XX2 + DX2 - 3.0
+            GC1( 2 ) = GC0( 2 ) - YY2 + DY2 - 3.0
 
 *  Attempt to draw the arrows and labels in invisible ink, using this
 *  modified origin. 
@@ -487,8 +546,8 @@
 *  If we still need to find a default origin, try a position near the
 *  bottom right corner of the underlying DATA picture. 
       IF( GUESS ) THEN 
-         GC0( 1 ) = X2 - R
-         GC0( 2 ) = Y1 + R
+         GC0( 1 ) = DX2 - R
+         GC0( 2 ) = DY1 + R
 
 *  Attempt to draw the arrows and labels in invisible ink, using this
 *  origin. The bouding box of the plotted items is returned.
@@ -501,8 +560,8 @@
 
 *  Modify the origin so that the bounding box does not extend outside the
 *  DATA picture.
-            GC1( 1 ) = GC0( 1 ) + X2 - XX2 - 3.0
-            GC1( 2 ) = GC0( 2 ) + Y1 - YY1 + 3.0
+            GC1( 1 ) = GC0( 1 ) + DX2 - XX2 - 3.0
+            GC1( 2 ) = GC0( 2 ) + DY1 - YY1 + 3.0
 
 *  Attempt to draw the arrows and labels in invisible ink, using this
 *  modified origin. 
@@ -522,8 +581,8 @@
 *  If we still need to find a default origin, try the centre of the 
 *  underlying DATA picture. 
       IF( GUESS ) THEN 
-         GC0( 1 ) = 0.5*( X1 + X2 )
-         GC0( 2 ) = 0.5*( Y1 + Y2 )
+         GC0( 1 ) = 0.5*( DX1 + DX2 )
+         GC0( 2 ) = 0.5*( DY1 + DY2 )
 
 *  Attempt to draw the arrows and labels in invisible ink, using this
 *  origin. The bouding box of the plotted items is returned.
@@ -546,7 +605,7 @@
 
 *  Find the bounds of the area just enclosing the arrows and labels.
 *  These are returned as positions in GRAPHICS coords.
-      CALL KPS1_DNRTH( .FALSE., IPLOT, LENS, ARROW, GC0, MNSIZE,
+      CALL KPS1_DNRTH( .FALSE., IPLOTC, LENS, ARROW, GC0, MNSIZE,
      :                 XX1, XX2, YY1, YY2, STATUS )
 
 *  Report an error if the supplied origin cannot be used.
@@ -585,7 +644,7 @@
       IF( BLANK ) CALL KPG1_PGCLR( STATUS )
 
 *  Draw the required arrows and labels.
-      CALL KPS1_DNRTH( .TRUE., IPLOT, LENS, ARROW, GC0, MNSIZE, 
+      CALL KPS1_DNRTH( .TRUE., IPLOTC, LENS, ARROW, GC0, MNSIZE, 
      :                 XX1, XX2, YY1, YY2, STATUS )
 
 *  Report an error if the supplied origin cannot be used.
@@ -598,7 +657,7 @@
       END IF
 
 *  Save the Plot with the new AGI picture.
-      CALL KPG1_GDPUT( IPICK, ' ', ' ', IPLOT, STATUS )
+      CALL KPG1_GDPUT( IPICK, ' ', ' ', IPLOTC, STATUS )
 
 *  Arrive here if an error occurs.
  999  CONTINUE
