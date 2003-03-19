15d4fb10f26f24da6fde99ee1e02dc8d8b0fd1bd
Norman Gray <norman@ASTRO.GLA.AC.UK>
2003-03-19 20:37:31 +0000

    Added more method documentation; changed DviFileEvent so charno is a method rather than a field
    svn path=/trunk/; revision=10601

--- applications/dvi2bitmap/DviFile.h
+++ applications/dvi2bitmap/DviFile.h
@@ -57,6 +57,9 @@ using std::map;
 class DviFileEvent;
 class DviFilePreamble;
 
+/**
+ * Represents a DVI file.
+ */
 class DviFile {
 public:
     // magmag is a factor by which the file's internal
@@ -66,24 +69,66 @@ public:
     bool eof();
     DviFileEvent *getEvent();
     DviFileEvent *getEndOfPage();
+    /**
+     * Sets the verbosity of this module.
+     * @param level the required verbosity
+     */
     static void verbosity (const verbosities level) { verbosity_ = level; }
     // currH and currY are current horiz and vert positions in pixel
     // units, including possible drift corrections
+    /**
+     * Obtains the current horizontal position in pixel units,
+     * including any drift corrections.
+     * @return the horizontal position
+     */
     int currH() const { return hh_; }	// device units
+    /**
+     * Obtains the current vertical position in pixel units,
+     * including any drift corrections.
+     * @return the vertical position
+     */
     int currV() const { return vv_; }
-    // hSize is the `width of the widest page' and vSize is the
-    // `height plus depth of the tallest page' in pixels.  Note that 
-    // this isn't the same as the max values of currH and currV, any more
-    // than 0 is the minimum, but if the origin is set `appropriately' 
-    // (ie, at (1in,1in)?), then everything should fit on.
+    /**
+     * Obtains the `width of the widest page'.  This figure is that
+     * obtained from the postamble of the DVI file, and is written
+     * there as information by TeX.  Note that 
+     * this isn't the same as the maximum value of {@link #currH}, any more
+     * than 0 is the minimum, but if the origin is set `appropriately' 
+     * (ie, at (1in,1in)), then everything should fit on.
+     *
+     * @return the horizontal size of the largest `page', in pixels
+     */
     int hSize() const { return static_cast<int>(postamble_.u * px_per_dviu_); }
+    /**
+     * Obtains the `height plus depth of the tallest page'.  This figure is that
+     * obtained from the postamble of the DVI file, and is written
+     * there as information by TeX.  Note that 
+     * this isn't the same as the maximum value of {@link #currV}, any more
+     * than 0 is the minimum, but if the origin is set `appropriately' 
+     * (ie, at (1in,1in)), then everything should fit on.
+     *
+     * @return the vertical size of the largest `page', in pixels
+     */
     int vSize() const { return static_cast<int>(postamble_.l * px_per_dviu_); }
-    // Return the net magnification factor for the DVI file
+    /**
+     * Return the net magnification factor for the DVI file
+     * @return the overall magnification factor applied to lengths in
+     * the DVI file.  A value of 1.0 implies no magnification at all.
+     */
     double magnification() const { return magfactor_; }
-    // Convert a length in points to one in pixels,
-    // using the current magnifications, etc
+    /**
+     * Converts a length in points to one in pixels, using the current
+     * magnifications and any other relevant parameters.
+     *
+     * @param npt a length in points
+     * @return the given length, in pixels
+     */
     int pt2px (double npt) const
 	    { return static_cast<int>(px_per_dviu_*dviu_per_pt_*magfactor_*npt+0.5); };
+    /**
+     * Gets the name of this DVI file.
+     * @return the open file name as a string
+     */
     const string *filename () const { return &fileName_; }
 
 private:
@@ -194,9 +239,22 @@ private:
 	bool finished_;
 	friend class DviFile;
     };
+    /**
+     * Creates an initialised <code>PkFont</code> iterator.  This
+     * allows you to step through the collection of
+     * <code>PkFont</code> instances which are contained within this
+     * <code>DviFile</code>.
+     * @return initialised iterator
+     */
     const_iterator begin() {
 	return const_iterator(fontMap_.begin(), fontMap_.end());
     }
+    /**
+     * Represents the end of the <code>PkFont</code> sequence begun by
+     * <code>begin()</code>
+     * @return iterator representing the end of sequence
+     * @see #begin
+     */
     const_iterator end() const { return const_iterator(); };
     friend class const_iterator;
 };
@@ -209,28 +267,65 @@ private:
  * above.
  * DviFileEvent is a virtual class, so these derived classes should have
  * non-virtual destructors.
+ * XXX these shouldn't be subclasses of DviFile, I don't think, but
+ * they should have DviFile as a friend (correct?), so that only
+ * DviFile can create instances
+ */
+/**
+ * Abstracts the contents of a DVI file.  All the features of a DVI
+ * file which calling code might be interested in are represented by
+ * one of the subclasses of this, and these are obtained in order by
+ * calling the {@link DviFile#getEvent} method on <code>DviFile</code>.
  */
 class DviFileEvent {
  public:
     enum eventTypes { setchar, setrule, fontchange, special,
 		      page, preamble, postamble };
+    /**
+     * Writes a representation of this event on cerr
+     */
+    virtual void debug() const;
+
+    /**
+     * Gets the type of this event
+     *
+     * @return the type
+     */
+    eventTypes type() const { return type_; }
+
+ protected:
+    /**
+     * Creates a new event.
+     *
+     * @param t the type of this event
+     * @param dp the <code>DviFile</code> it is associated with
+     */
     DviFileEvent(eventTypes t, DviFile *dp=0)
 	: dviFile_(dp), type_(t) { }
     virtual ~DviFileEvent () { };
-    virtual void debug() const;
-    eventTypes type() const { return type_; }
-    unsigned char opcode;
  private:
+    unsigned char opcode;
     DviFile *dviFile_;
     const eventTypes type_;
 };
 class DviFileSetChar : public DviFileEvent {
  public:
+    /**
+     * A request that a given character be set
+     * @param charno the character to be set
+     * @param dptr the <code>DviFile</code> associated with this character
+     */
     DviFileSetChar(int charno, DviFile *dptr)
-	: DviFileEvent(setchar,dptr), charno(charno) { }
+	: DviFileEvent(setchar,dptr), charno_(charno) { }
     //~DviFileSetChar () { };
     void debug() const;
-    const int charno;
+    /**
+     * Obtains the character requested.
+     * @return the character as an integer
+     */
+    const int charno() const { return charno_; }
+ private:
+    const int charno_;
 };
 class DviFileSetRule: public DviFileEvent {
  public:
