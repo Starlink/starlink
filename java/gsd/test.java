f2ddbfb829f1830cc097b6b1c36bf2b5ee3d307f
Tim Jenness <t.jenness@jach.hawaii.edu>
2003-04-13 09:11:16 +0000

    test long names
    svn path=/trunk/; revision=10693

--- java/gsd/test.java
+++ java/gsd/test.java
@@ -1,26 +1,33 @@
-
-import java.io.*;
-import edu.hawaii.jach.gsd.*;
-
+import edu.hawaii.jach.gsd.GSDItem;
+import edu.hawaii.jach.gsd.GSDObject;
 
 class TestGSD {
 
-    public static void main( String[] args) {
-	int item = 2;
-	try {
-	    GSDObject gsd = new GSDObject( "/stardev/bin/specx/obs_das_0011.dat" );
+	public static void main(String[] args) {
+		int item = 2;
+		try {
+			GSDObject gsd =
+               // new GSDObject("/stardev/bin/specx/obs_das_0011.dat");
+               new GSDObject("/home/timj/dev/scuba/jcmtdr/rxa_146.dat");
+            
+			gsd.print();
+			System.out.println(gsd);
+			System.out.println(gsd.itemByName("SCAN_VARS2"));
 
-	    gsd.print();
-            System.out.println( gsd );
-            System.out.println( gsd.itemByName("C1PID") );
+			// Get the items
+			GSDItem[] allitems = gsd.items();
+			System.out.println(allitems[20]);
 
-	} catch ( edu.hawaii.jach.gsd.GSDException e ) {
-	    System.out.println(e);
-	} catch ( java.io.FileNotFoundException e ) {
-	    System.out.println(e);
-	} catch ( java.io.IOException e ) {
-	    System.out.println(e);	
+            // This is designed to cause a problem
+			// System.out.println(gsd.itemByNum(0));
+
+		} catch (edu.hawaii.jach.gsd.GSDException e) {
+			System.out.println(e);
+		} catch (java.io.FileNotFoundException e) {
+			System.out.println(e);
+		} catch (java.io.IOException e) {
+			System.out.println(e);
+		}
 	}
-    }
 
 }
