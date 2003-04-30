9e6ffaa6a13933076dc8979ee325581abe116d4a
Norman Gray <norman@ASTRO.GLA.AC.UK>
2003-04-30 15:59:37 +0000

    Copied in slaDcmpf_old, so I can generated failing test results if necessary.  Added a DEBUG preprocessor branch, and a couple of extra comments.  No change necessary to t2.correct.
    svn path=/trunk/; revision=10743

--- applications/astrom/t/t2.c
+++ applications/astrom/t/t2.c
@@ -1,11 +1,38 @@
 #include <stdio.h>
 #include <math.h>
 
+/*
+** Use flags OLD_DCMPF and FORTRAN_DCMPF to control which dcmpf
+** routine is actually tested.  The regression test case is
+** -DOLD_DCMPF=0 -DFORTRAN_DCMPF=1, and this is the default.
+**
+** If you're not linking against the SLALIB library (which is the case
+** if you're using -DFORTRAN_DCMPF=0, then you need to find a
+** slaDrange to link against, probably from drange.c of the SLALIB C
+** distribution.
+*/
+
+/* If OLD_DCMPF is true, then use the old, broken, C version of
+** slaDcmpf, to verify cases that used to fail.
+*/
+#ifndef OLD_DCMPF
+#define OLD_DCMPF 0
+#endif
+
 /* If FORTRAN_DCMPF is true, then link this against the Fortran version of 
 ** this routine
 */
 #ifndef FORTRAN_DCMPF
-#define FORTRAN_DCMPF 1
+#  if OLD_DCMPF
+#    define FORTRAN_DCMPF 0
+#  else
+#    define FORTRAN_DCMPF 1
+#  endif
+#endif
+
+/* If DEBUG is true, then chatter */
+#ifndef DEBUG
+#define DEBUG 0
 #endif
 
 #if !FORTRAN_DCMPF
@@ -139,6 +166,136 @@ void slaDcmpf ( double coeffs[6],
    *perp = p;
    *orient = or;
 }
+
+/* Old version of slaDcmpf, priort to Pat's fix (here to allow me to
+** check test cases that used to fail)
+*/
+void slaDcmpf_old ( double coeffs[6],
+                double *xz, double *yz, double *xs,
+                double *ys, double *perp, double *orient )
+/*
+**  - - - - - - - - -
+**   s l a D c m p f
+**  - - - - - - - - -
+**
+**  Decompose an [x,y] linear fit into its constituent parameters:
+**  zero points, scales, nonperpendicularity and orientation.
+**
+**  Given:
+**     coeffs    double[6]     transformation coefficients (see note)
+**
+**  Returned:
+**     *xz       double        x zero point
+**     *yz       double        y zero point
+**     *xs       double        x scale
+**     *ys       double        y scale
+**     *perp     double        nonperpendicularity (radians)
+**     *orient   double        orientation (radians)
+**
+**  The model relates two sets of [x,y] coordinates as follows.
+**  Naming the elements of coeffs:
+**
+**     coeffs[0] = a
+**     coeffs[1] = b
+**     coeffs[2] = c
+**     coeffs[3] = d
+**     coeffs[4] = e
+**     coeffs[5] = f
+**
+**  The model transforms coordinates [x1,y1] into coordinates
+**  [x2,y2] as follows:
+**
+**     x2 = a + b*x1 + c*y1
+**     y2 = d + e*x1 + f*y1
+**
+**  The transformation can be decomposed into four steps:
+**
+**     1)  Zero points:
+**
+**             x' = xz + x1
+**             y' = yz + y1
+**
+**     2)  Scales:
+**
+**             x'' = xs*x'
+**             y'' = ys*y'
+**
+**     3)  Nonperpendicularity:
+**
+**             x''' = cos(perp/2)*x'' + sin(perp/2)*y''
+**             y''' = sin(perp/2)*x'' + cos(perp/2)*y''
+**
+**     4)  Orientation:
+**
+**             x2 = cos(orient)*x''' + sin(orient)*y'''
+**             y2 =-sin(orient)*y''' + cos(orient)*y'''
+**
+**  See also slaFitxy, slaPxy, slaInvf, slaXy2xy
+**
+**  Last revision:   22 September 1995
+**
+**  Copyright P.T.Wallace.  All rights reserved.
+*/
+{
+   double a, b, c, d, e, f, rb2e2, rc2f2, xsc, ysc, p,
+          ws, wc, or, hp, shp, chp, sor, cor, det, x0, y0;
+
+/* Copy the six coefficients */
+   a = coeffs[0];
+   b = coeffs[1];
+   c = coeffs[2];
+   d = coeffs[3];
+   e = coeffs[4];
+   f = coeffs[5];
+
+/* Scales */
+   rb2e2 = sqrt ( b * b + e * e );
+   rc2f2 = sqrt ( c * c + f * f );
+   if ( ( b * f - c * e ) >= 0.0 )
+     xsc = rb2e2;
+   else {
+     b = -b;
+     c = -c;
+     xsc = -rb2e2;
+   }
+   ysc = rc2f2;
+
+/* Non-perpendicularity */
+   p = ( ( c != 0.0 || f != 0.0 ) ? atan2 ( c, f ) : 0.0 ) +
+       ( ( e != 0.0 || b != 0.0 ) ? atan2 ( e, b ) : 0.0 );
+
+/* Orientation */
+   ws = ( c * rb2e2 ) - ( e * rc2f2 );
+   wc = ( b * rc2f2 ) + ( f * rb2e2 );
+   or = ( ws != 0.0 || wc != 0.0 ) ? atan2 ( ws, wc ) : 0.0;
+
+/* Zero corrections */
+   hp = p / 2.0;
+   shp = sin ( hp );
+   chp = cos ( hp );
+   sor = sin ( or );
+   cor = cos ( or );
+   det = xsc * ysc * ( chp + shp ) * ( chp - shp );
+   if ( fabs ( det ) > 0.0 ) {
+     x0 = ysc * ( a * ( ( chp * cor ) - ( shp * sor ) )
+                - d * ( ( chp * sor ) + ( shp * cor ) ) ) / det;
+     y0 = xsc * ( a * ( ( chp * sor ) - ( shp * cor ) )
+                + d * ( ( chp * cor ) + ( shp * sor ) ) ) / det;
+   }
+   else {
+     x0 = 0.0;
+     y0 = 0.0;
+   }
+
+/* Results */
+   *xz = x0;
+   *yz = y0;
+   *xs = xsc;
+   *ys = ysc;
+   *perp = p;
+   *orient = or;
+}
+
 #endif
 
 
@@ -194,7 +351,8 @@ int main (int argc, char **argv)
 #endif
 
     /* pars[] is an array of groups of six parameter values, and their
-       expected results. */
+       expected results.  The expected values are the same as the
+       input values, except where noted. */
     double pars[][12] = {
 	{ 0.00, 0.00,  1.00,  1.00,   0,   60,
           0.00, 0.00,  1.00,  1.00,   0,   60  },
@@ -212,12 +370,14 @@ int main (int argc, char **argv)
           -0.00, 0.00, -1.00,  1.00, -60, -90  },
 	{ 0.00, 0.00,  1.00,  1.00,  60,  -90,
           0.00, 0.00,  1.00,  1.00,  60,  -90  },
+	{ 0.00, 0.00,  1.00,  1.00, -60,  -90,
+          0.00, 0.00,  1.00,  1.00, -60,  -90  },
 	{ 0.00, 0.00,  1.00,  1.00,  60,   90,
           0.00, 0.00,  1.00,  1.00,  60,   90  },
 	{ 0,    0,    -1,    -1,      0,    0,
-          0.00, 0.00,  1.00,  1.00,  -0,  180  },
+          0.00, 0.00,  1.00,  1.00,  -0,  180  }, /* different */
 	{ 0,    0,    +1,    +1,   -360,  180,
-          0.00, 0.00,  1.00,  1.00,   0,   -0  },
+          0.00, 0.00,  1.00,  1.00,   0,   -0  }, /* different */
 	{ 0,    0,    +2,    +2,      0,    0,
           0.00, 0.00,  2.00,  2.00,   0,    0  },
 	{ 10,   5,    +1,    +2,      0,   45,
@@ -229,13 +389,13 @@ int main (int argc, char **argv)
         { 0.00, 0.00,  1.00,  1.00, -10,   0,
           0.00, 0.00,  1.00,  1.00, -10,   0  },
         { 0.00, 0.00,  1.00, -1.00,  10,   0,
-          -0.00, 0.00, -1.00,  1.00,  10, 180  },
+          -0.00, 0.00, -1.00,  1.00,  10, 180  }, /* different */
         { 0.00, 0.00,  1.00, -1.00, -10,   0,
-          -0.00, 0.00, -1.00,  1.00, -10, 180  },
+          -0.00, 0.00, -1.00,  1.00, -10, 180  }, /* different */
         { 0.00, 0.00, -1.00, -1.00,  10,   0,
-          0.00, 0.00,  1.00,  1.00,  10, 180  },
+          0.00, 0.00,  1.00,  1.00,  10, 180  }, /* different */
         { 0.00, 0.00, -1.00, -1.00, -10,   0,
-          -0.00,-0.00,  1.00,  1.00, -10, 180  },
+          -0.00,-0.00,  1.00,  1.00, -10, 180  }, /* different */
     };
     int npars = sizeof(pars)/(sizeof(pars[0]));
     int i;
@@ -255,13 +415,28 @@ int main (int argc, char **argv)
             mat[j] = pars[i][j];
 	make_cpts (mat);
 
+#if DEBUG
+        printf ("     %5s %5s %6s %6s %4s %4s      %5s %5s %5s %5s %5s %5s      %5s %5s %6s %6s %4s %4s \n",
+                "xz", "yz", "xs", "ys", "perp", "ori",
+                "a", "b", "c", "d", "e", "f",
+                "xz", "yz", "xs", "ys", "perp", "ori");
+        headerdone = 1;
+        printf ("%2d: (%g,%g,%g,%g,%g,%g) -> (%g,%g,%g,%g,%g,%g)\n",
+                i,
+                pars[i][0], pars[i][1], pars[i][2],
+                pars[i][3], pars[i][4], pars[i][5],
+                mat[0], mat[1], mat[2], mat[3], mat[4], mat[5]);
+#endif
+
 	/* Do the inversion -- transform the matrix elements to
 	   parameters, returning the parameters in xz, yz, xs, ys,
 	   perp and orient. */
 #if FORTRAN_DCMPF
-	sla_dcmpf_ (mat, &xz, &yz, &xs, &ys, &perp, &orient);
+	sla_dcmpf_   (mat, &xz, &yz, &xs, &ys, &perp, &orient);
+#elif OLD_DCMPF
+	slaDcmpf_old (mat, &xz, &yz, &xs, &ys, &perp, &orient);
 #else
-	slaDcmpf   (mat, &xz, &yz, &xs, &ys, &perp, &orient);
+	slaDcmpf     (mat, &xz, &yz, &xs, &ys, &perp, &orient);
 #endif
 
         badflag = 0;
@@ -281,7 +456,7 @@ int main (int argc, char **argv)
                 headerdone = 1;
             }
 
-            printf ("BAD: %2d: (%5.2f,%5.2f,%6.2f,%6.2f,%4.0f,%4.0f) -> (%5.2f,%5.2f,%5.2f,%5.2f,%5.2f,%5.2f) -> (%5.2f,%5.2f,%6.2f,%6.2f,%4.0f,%4.0f) not (%5.2f,%5.2f,%6.2f,%6.2f,%4.2f,%4.2f)\n",
+            printf ("BAD: %2d: (%5.2f,%5.2f,%6.2f,%6.2f,%4.0f,%4.0f) -> (%5.2f,%5.2f,%5.2f,%5.2f,%5.2f,%5.2f) -> (%5.2f,%5.2f,%6.2f,%6.2f,%4.0f,%4.0f) not (%5.2f,%5.2f,%6.2f,%6.2f,%4.0f,%4.0f)\n",
                     i,
                     pars[i][0],
                     pars[i][1],
