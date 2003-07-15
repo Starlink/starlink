a3e52faefce0b3d4b00f2504e62f4a924b3688e0
Norman Gray <norman@ASTRO.GLA.AC.UK>
2003-07-15 22:17:32 +0000

    Extensive tests of (modified) runCommandPipe
    svn path=/trunk/; revision=10973

--- applications/dvi2bitmap/test/t1.cc
+++ applications/dvi2bitmap/test/t1.cc
@@ -19,15 +19,81 @@ using std::endl;
 #include "verbosity.h"
 #include "Util.h"
 
+
 struct {
     string testcase;
+    string envlist;
     string expected;
 } tests[] = {
-    { "ls t1.cc", "t1.cc", },
-    { "echo HeLlO ThErE | tr '[A-Z]' '[a-z]'", "hello there", },
-    { "squorrocks", "" },
+    { "./t1.sh HOME LOGNAME BOINK ANOTHER",
+      "PATH=.  LOGNAME=blarfl TERM=nothing LOGNAME=norman BOINK=whee",
+      "HOME=!LOGNAME=norman!BOINK=whee!ANOTHER=!"
+    },
+    { "./t1.sh HOME LOGNAME",
+      "+ HOME=/root LOGNAME=",
+      "HOME=/root!LOGNAME=!"
+    },
+    { "/bin/ls t1.cc", "", "t1.cc", },
+    { "ls t1.cc", "", "", },	// no path
+    { "squorrocks", "", "" },
 };
 int ntests = sizeof(tests)/sizeof(tests[0]);
+
+int nfails = 0;
+
+void compare_string_list(string_list& expected, string_list& actual);
+
+void compare_string_list(string_list& expected, string_list& actual)
+{
+    string_list::const_iterator cie = expected.begin();
+    string_list::const_iterator cia = actual.begin();
+
+    while (! (cie == expected.end() || cia == actual.end())) {
+	if (*cie != *cia) {
+	    nfails++;
+	    cerr << "(list) Expected " << *cie
+		 << ", got " << *cia
+		 << endl;
+	}
+	cie++;
+	cia++;
+    }
+
+    if (cia != actual.end()) {
+	nfails++;
+	cerr << "(list) more actual (" << *cia << ") than expected" << endl;
+    }
+    if (cie != expected.end()) {
+	nfails++;
+	cerr << "(list) more expected (" << *cia << ") than actual" << endl;
+    }
+}
+
+void compare_string_array(string_list& expected, char** actual)
+{
+    string_list::const_iterator cie = expected.begin();
+    char** ap = actual;
+
+    while (! (cie == expected.end() || *ap == 0)) {
+	if (*cie != *ap) {
+	    nfails++;
+	    cerr << "(array) Expected " << *cie
+		 << ", got " << *ap
+		 << endl;
+	}
+	cie++;
+	ap++;
+    }
+
+    if (*ap != 0) {
+	nfails++;
+	cerr << "(array) more actual (" << *ap << ") than expected" << endl;
+    }
+    if (cie != expected.end()) {
+	nfails++;
+	cerr << "(array) more expected (" << *cie << ") than actual" << endl;
+    }
+}
 
 
 int main (int argc, char **argv)
@@ -35,23 +101,77 @@ int main (int argc, char **argv)
     Util::verbosity (debug);
 
     int i;
-    int nfails = 0;
+    
+    string_list res;
+
+    res.push_back("one");
+    res.push_back("two");
+    res.push_back("three");
+    string_list sl = Util::tokenise_string("one two three");
+    char **sa = Util::string_list_to_array(sl);
+    compare_string_list(res, sl);
+    compare_string_array(res, sa);
+    Util::delete_string_array(sa);
+
+    res.clear();
+    res.push_back("hello");
+    res.push_back("there");
+    sl = Util::tokenise_string("    hello there  ");
+    sa = Util::string_list_to_array(sl);
+    compare_string_list(res, sl);
+    compare_string_array(res, sa);
+    Util::delete_string_array(sa);
+
 
     for (i=0; i<ntests; i++) {
-	string ret = Util::runCommandPipe (tests[i].testcase);
-	if (tests[i].expected.length() == 0) {
-	    // this command was expected to fail
-	    if (ret.length() != 0) {
-		// ...but didn't
+	try {
+	    string ret;
+	    cerr << "===== Test " << i << endl;
+	    if (tests[i].envlist.length() == 0)
+		ret = Util::runCommandPipe (tests[i].testcase);
+	    else
+		ret = Util::runCommandPipe (tests[i].testcase, tests[i].envlist);
+	    if (tests[i].expected.length() == 0) {
+		// this command was expected to fail
+		if (ret.length() != 0) {
+		    // ...but didn't
+		    nfails++;
+		    cerr << "Test " << i << " did not fail as expected, returned <"
+			 << ret << ">" << endl;
+		}
+	    } else if (ret != tests[i].expected) {
 		nfails++;
-		cerr << "Test " << i << " did not fail as expected, returned <"
-		     << ret << ">" << endl;
+		cerr << "Test " << i << ": expected <" << tests[i].expected
+		     << ">, got <" << ret << ">" << endl;
 	    }
-	} else if (ret != tests[i].expected) {
+	} catch (DviError& e) {
+	    cerr << "Caught exception " << e.problem() << endl;
 	    nfails++;
-	    cerr << "Test " << i << ": expected <" << tests[i].expected
-		 << ">, got <" << ret << ">" << endl;
 	}
+    }
+
+    try {
+	string cmd = "./t1.sh LOGNAME HOME T TT";
+	setenv("TT", "test", 1);
+	string envs = "LOGNAME=blarfl HOME=blarfl T=t + LOGNAME=you TT + LOGNAME=me";
+	string expected = "LOGNAME=me!HOME=";
+	char* h = getenv("HOME");
+	expected += (h != 0 ? h : "");
+	expected += "!T=t!TT=test!";
+	int status;
+	string res = Util::runCommandPipe(cmd, envs, &status);
+	if (status != 0) {
+	    nfails++;
+	    cerr << "end: got non-zero status " << status << endl;
+	}
+	if (res != expected) {
+	    nfails++;
+	    cerr << "end: expected <" << expected
+		 << ", got <" << res << ">" << endl;
+	}
+	
+    } catch (DviError& e) {
+	cerr << "Caught exception " << e.problem() << endl;
     }
 
     exit (nfails);
