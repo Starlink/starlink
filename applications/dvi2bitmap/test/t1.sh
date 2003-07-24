0964ebcb69f61718d6ce020626d940298685ccdc
Norman Gray <norman@ASTRO.GLA.AC.UK>
2003-07-24 15:41:41 +0000

    Modified to avoid using `echo -n' (Solaris doesn't have -n option)
    svn path=/trunk/; revision=11043

--- applications/dvi2bitmap/test/t1.sh
+++ applications/dvi2bitmap/test/t1.sh
@@ -1,12 +1,12 @@
 #! /bin/sh -
 if test $# -eq 0; then
-    echo SHELL=SHELL
+    echo SHELL=$SHELL
 else
     while test $# -gt 0; do
-	eval echo -n "$1=\${$1}!"
+        eval RET="${RET}$1=\${$1}!"
 	shift
     done
-    echo
+    echo $RET
 fi
 echo "Don't see this"
 
