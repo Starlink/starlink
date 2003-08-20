19bb9ebd2a5ea33c7ceb94f39dd1327c12f4c438
Norman Gray <norman@ASTRO.GLA.AC.UK>
2003-08-20 14:31:04 +0000

    Added -trim and -notrim options; general tidyup.
    svn path=/trunk/; revision=11166

--- applications/autoastrom/t/fitsdump.f
+++ applications/autoastrom/t/fitsdump.f
@@ -3,24 +3,38 @@
       implicit none
       integer ftstat,ftunit,blocksize,i
       character*(80) s
+      character*(80) ffname
       logical keepreading
       logical printline
+      logical trimlines
       
       integer iargc
       
-      if (iargc().ne.1) then
-         write (*,'("Usage: fitsdump fitsfile")')
-         goto 999
-      endif
-      call getarg (1, s)
+      trimlines = .true.
+      ffname = ''
+
+      do i=1,iargc()
+         call getarg(i, s)
+         if (s(1:5).eq.'-trim') then
+            trimlines = .true.
+         else if (s(1:7).eq.'-notrim') then
+            trimlines = .false.
+         else if (s(1:1).eq.'-') then
+            goto 991
+         else
+            ffname = s
+         endif
+      enddo
+
+      if (ffname.eq.'') goto 991
 
       ftstat = 0
       call ftgiou (ftunit, ftstat)
-      call ftopen (ftunit, s, 0, blocksize, ftstat)
+      call ftopen (ftunit, ffname, 0, blocksize, ftstat)
       
       if (ftstat.gt.0) then
-         write (*,'("Can''t open file ",a)') s
-         goto 998
+         write (*,'("Can''t open file ",a)') ffname
+         goto 990
       endif
 
       keepreading = .true.
@@ -37,16 +51,21 @@
 *      the library is going to change the `this is FITS' comment cards
 *      (see thread
 *      <http://groups.google.com/groups?threadm=3BC7232C.C4E3A9C1%40tetra.gsfc.nasa.gov>)
-         if (s(1:4).eq.'DATE') then
-            printline = .false.
-         else if (s(1:7).eq.'HISTORY') then
-            printline = .false.
-         else if (s(1:7).eq.'COMMENT') then
-            printline = .false.
+         if (trimlines) then
+            if (s(1:4).eq.'DATE') then
+               printline = .false.
+            else if (s(1:7).eq.'HISTORY') then
+               printline = .false.
+            else if (s(1:7).eq.'COMMENT') then
+               printline = .false.
+            else
+*            It's OK -- print this line
+               printline = .true.
+            endif
          else
-*         It's OK -- print this line
             printline = .true.
          endif
+
          if (printline) write (*, '(a)'), s
          i = i+1
          if (s(1:3).eq.'END') then
@@ -57,11 +76,15 @@
          endif
       enddo
 
- 998  continue
+ 990  continue
 
       call ftclos (ftunit, ftstat)
-
       call ftrprt ('STDERR', ftstat)
+      goto 999
+
+ 991  continue
+      write(*,'("Usage: fitsdump [-[no]trim] fitsfile")')
+      goto 999
 
  999  continue
       end
