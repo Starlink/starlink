fa09ba6929f459c8a3915a64db142eb8caef5439
David S. Berry <d.berry@jach.hawaii.edu>
2003-09-17 08:36:55 +0000

    Initial version
    svn path=/trunk/; revision=11283

---
+++ applications/atools/astformat.f
@@ -0,0 +1,177 @@
+      SUBROUTINE ASTFORMAT( STATUS )
+*+
+*  Name:
+*     ASTFORMAT
+
+*  Purpose:
+*     Format coordinate values for a Frame axis
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Type of Module:
+*     ADAM A-task
+
+*  Invocation:
+*     CALL ASTFORMAT( STATUS )
+
+*  Arguments:
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
+
+*  Description:
+*     This application displays character strings containing the formatted 
+*     (character) versions of coordinate values for a Frame axis. The 
+*     formatting applied is determined by the Frame's attributes and, in 
+*     particular, by any Format attribute string that has been set for the 
+*     axis. A suitable default format (based on the Digits attribute value) 
+*     will be applied if necessary.
+
+*  Usage:
+*     astformat this axis value result
+
+*  ADAM Parameters:
+*     THIS = LITERAL (Read)
+*        An NDF, FITS file or text file holding the Frame. If an NDF is 
+*        supplied, the current Frame of the WCS FrameSet will be used. If a 
+*        FITS file is supplied, the Frame corresponding to the primary axis 
+*        descriptions will be used.
+*     AXIS = INTEGER (Read)
+*         The number of the Frame axis for which formatting is to be
+*         performed (axis numbering starts at 1 for the first axis).
+*     VALUE = GROUP (Read)
+*        A comma-separated list of floating point values to be formatted.
+*        A text file may be specified by preceeding the name of the file 
+*        with an up arrow character "^". If the supplied value ends with a 
+*        minus sign, the user is re-prompted for additional values.
+*     RESULT = LITERAL (Read)
+*        The name of a text file in which to put the formatted axis
+*        values. No file is produced if a null (!) value is supplied. One
+*        axis value is stored on each line of the file. [!]
+
+*  Authors:
+*     DSB: David Berry (STARLINK)
+*     {enter_new_authors_here}
+
+*  History:
+*     6-JUN-2003 (DSB):
+*        Original version.
+*     {enter_further_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+*  Type Definitions:
+      IMPLICIT NONE              ! no default typing allowed
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'NDF_PAR'          ! NDF constants
+      INCLUDE 'AST_PAR'          ! AST constants and function declarations
+      INCLUDE 'GRP_PAR'          ! GRP constants 
+      INCLUDE 'PAR_ERR'          ! PAR error constants 
+
+*  External References:
+      EXTERNAL AST_ISAFRAME
+      INTEGER CHR_LEN
+
+*  Status:
+      INTEGER STATUS
+
+*  Local Variables:
+      INTEGER THIS, IGRP, NP
+      CHARACTER FNAME*255, BUF*255
+      INTEGER I, FD, NC, AXIS
+      DOUBLE PRECISION VALUE
+      LOGICAL LOG
+*.
+
+*  Check inherited status.      
+      IF( STATUS .NE. SAI__OK ) RETURN
+
+*  Begin an AST context.
+      CALL AST_BEGIN( STATUS )
+
+*  Get the Frame.
+      CALL ATL1_GTOBJ( 'THIS', 'Frame', AST_ISAFRAME, THIS,
+     :                 STATUS )
+
+*  Get the axis index.
+      CALL PAR_GDR0I( 'AXIS', 1, 1, AST_GETI( THIS, 'NAXES', STATUS ), 
+     :                .FALSE., AXIS, STATUS ) 
+
+*  Get a group holding the input axis values.
+      IGRP = GRP__NOID
+      CALL KPG1_GTGRP( 'VALUE', IGRP, NP, STATUS )
+
+*  If required, open an output text file.
+      LOG = .FALSE.
+      CALL PAR_GET0C( 'RESULT', FNAME, STATUS )
+      IF( STATUS .EQ. PAR__NULL ) THEN
+         CALL ERR_ANNUL( STATUS )
+      ELSE
+
+*  We delete any pre-existing file first. 
+         CALL ATL1_RM( FNAME, STATUS )
+
+*  Open a new file and get an FIO identifier for it.
+         CALL FIO_OPEN( FNAME, 'WRITE', 'LIST', 132, FD, STATUS )
+         IF( STATUS .EQ. SAI__OK ) LOG = .TRUE.
+
+      END IF
+
+*  Loop round the group.
+      DO I = 1, NP
+
+*  Get this element.
+         CALL GRP_GET( IGRP, I, 1, BUF, STATUS ) 
+
+*  Attempt to convert it to a double.
+         CALL CHR_CTOD( BUF, VALUE, STATUS ) 
+         IF( STATUS .NE. SAI__OK ) THEN
+
+            CALL ERR_BEGIN( STATUS )               
+            CALL GRP_INFOC( IGRP, I, 'NAME', FNAME, STATUS ) 
+            CALL ERR_END( STATUS )               
+
+            CALL MSG_SETC( 'C', BUF )
+            IF( FNAME .EQ. ' ' ) THEN 
+               CALL ERR_REP( 'ASTFORMAT_ERR1', 'Error reading '//
+     :                       'string ''^C''.', STATUS )
+            ELSE
+               CALL MSG_SETC( 'F', FNAME )
+               CALL ERR_REP( 'ASTFORMATL_ERR2', 'Error reading '//
+     :                       'string ''^C'' in file ^F.', STATUS )
+            END IF
+
+         END IF
+
+*  Format the value.
+         BUF = AST_FORMAT( THIS, AXIS, VALUE, STATUS )  
+
+*  Display it to the screen.
+         CALL MSG_SETC( 'V', BUF )
+         CALL MSG_OUT( ' ', '  ^V', STATUS )
+
+*  Write it to the text file if necessary.
+         IF( LOG ) CALL FIO_WRITE( FD, BUF( : CHR_LEN( BUF ) ), STATUS )
+
+      END DO
+
+*  Free resources.
+      CALL GRP_DELET( IGRP, STATUS )
+
+*  Close the file.
+      CALL FIO_ANNUL( FD, STATUS )
+
+*  End the AST context.
+      CALL AST_END( STATUS )
+
+*  Give a context message if anything went wrong.
+      IF( STATUS .NE. SAI__OK ) THEN
+         CALL ERR_REP( 'ASTFORMAT_ERR', 'Error formatting an axis '//
+     :                 'value.', STATUS )
+      END IF
+
+      END
---
+++ applications/atools/asttran2.f
@@ -0,0 +1,180 @@
+      SUBROUTINE ASTTRAN2( STATUS )
+*+
+*  Name:
+*     ASTTRAN2
+
+*  Purpose:
+*     Use a Mapping to transform a set of position in two dimensions. 
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Type of Module:
+*     ADAM A-task
+
+*  Invocation:
+*     CALL ASTTRAN2( STATUS )
+
+*  Arguments:
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
+
+*  Description:
+*     This application applies a Mapping to transform the coordinates of a
+*     set of points in two dimensions. The input positions may be supplied
+*     either directly or in a text file. The output positions are listed
+*     on the screen ( an X value and a Y value on each line, separated by
+*     a space) and may optionally be stored in output text files.
+
+*  Usage:
+*     asttran2 this xin yin forward xout yout
+
+*  ADAM Parameters:
+*     THIS = LITERAL (Read)
+*        An NDF, FITS file or text file holding the Mapping. If an NDF is 
+*        supplied, the Mapping from the base Frame of the WCS FrameSet to the
+*        current Frame will be used. If a FITS file is supplied, the Mapping 
+*        from the pixel grid coordinates to the primary axis descriptions
+*        will be used.
+*     XIN = GROUP (Read)
+*        A comma-separated list of floating point values to be used as the 
+*        input X axis value. A text file may be specified by preceeding the 
+*        name of the file with an up arrow character "^". If the supplied value
+*        ends with a minus sign, the user is re-prompted for additional
+*        values.
+*     YIN = GROUP (Read)
+*        A comma-separated list of floating point values to be used as the 
+*        input Y axis value. A text file may be specified by preceeding the 
+*        name of the file with an up arrow character "^". If the supplied value
+*        ends with a minus sign, the user is re-prompted for additional
+*        values.
+*     FORWARD = _LOGICAL (Read)
+*        If this value is TRUE, then the Mapping's forward
+*        transformation will be used to transform the input positions.
+*        Otherwise, its inverse transformation will be used.
+*     XOUT = LITERAL (Read)
+*        The name of a text file in which to put the transformed X axis
+*        values. No file is produced if a null (!) value is supplied. One
+*        axis value is stored on each line of the file. [!]
+*     YOUT = LITERAL (Read)
+*        The name of a text file in which to put the transformed Y axis
+*        values. No file is produced if a null (!) value is supplied. One
+*        axis value is stored on each line of the file. [!]
+
+*  Authors:
+*     DSB: David Berry (STARLINK)
+*     {enter_new_authors_here}
+
+*  History:
+*     6-JUN-2003 (DSB):
+*        Original version.
+*     {enter_further_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+*  Type Definitions:
+      IMPLICIT NONE              ! no default typing allowed
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'NDF_PAR'          ! NDF constants
+      INCLUDE 'AST_PAR'          ! AST constants and function declarations
+      INCLUDE 'GRP_PAR'          ! GRP constants 
+
+*  External References:
+      EXTERNAL AST_ISAMAPPING
+
+*  Status:
+      INTEGER STATUS
+
+*  Local Variables:
+      INTEGER THIS, IGRP1, IGRP2, IPXIN, IPYIN, IPXOUT, IPYOUT
+      INTEGER NP, NP2
+      LOGICAL FORWRD
+
+*.
+
+*  Check inherited status.      
+      IF( STATUS .NE. SAI__OK ) RETURN
+
+*  Begin an AST context.
+      CALL AST_BEGIN( STATUS )
+
+*  Get the Mapping.
+      CALL ATL1_GTOBJ( 'THIS', 'Mapping', AST_ISAMAPPING, THIS,
+     :                 STATUS )
+
+*  Check that the the Nin and Nout attributes of the Mapping are both 2.
+      NP = AST_GETI( THIS, 'Nin', STATUS) 
+      IF( NP .NE. 2 .AND. STATUS .EQ. SAI__OK ) THEN
+         STATUS = SAI__ERROR
+         CALL MSG_SETI( 'NI', NP )
+         CALL ERR_REP( 'ASTTRAN2_ERR1', 'The supplied Mapping has '//
+     :                 '^NI input axes (should be 2).', STATUS)
+      END IF
+
+      NP = AST_GETI( THIS, 'Nout', STATUS) 
+      IF( NP .NE. 2 .AND. STATUS .EQ. SAI__OK ) THEN
+         STATUS = SAI__ERROR
+         CALL MSG_SETI( 'NI', NP )
+         CALL ERR_REP( 'ASTTRAN2_ERR2', 'The supplied Mapping has '//
+     :                 '^NI output axes (should be 2).', STATUS)
+      END IF
+
+*  Get the FORWARD parameter.
+      CALL PAR_GET0L( 'FORWARD', FORWRD, STATUS )
+
+*  Get two groups holding the input axis values.
+      IGRP1 = GRP__NOID
+      CALL KPG1_GTGRP( 'XIN', IGRP1, NP, STATUS )
+      IGRP2 = GRP__NOID
+      CALL KPG1_GTGRP( 'YIN', IGRP2, NP2, STATUS )
+
+      IF( NP .NE. NP2 .AND. STATUS .EQ. SAI__OK ) THEN
+         STATUS = SAI__ERROR
+         CALL MSG_SETI( 'NX', NP )
+         CALL MSG_SETI( 'NY', NP2 )
+         CALL ERR_REP( 'ASTTRAN2_ERR3', ' Number of input X positions'//
+     :                 ' (^NX) and Y positions (^NY) differ.', STATUS )
+      END IF
+
+*  Allocate memory for the input positions.
+      CALL PSX_CALLOC( NP, '_DOUBLE', IPXIN, STATUS )
+      CALL PSX_CALLOC( NP, '_DOUBLE', IPYIN, STATUS )
+
+*  Allocate memory for the output positions.
+      CALL PSX_CALLOC( NP, '_DOUBLE', IPXOUT, STATUS )
+      CALL PSX_CALLOC( NP, '_DOUBLE', IPYOUT, STATUS )
+
+*  Read the values from the group into the memory.
+      CALL ATL1_GTOFL( IGRP1, NP, %VAL( IPXIN ), STATUS )
+      CALL ATL1_GTOFL( IGRP2, NP, %VAL( IPYIN ), STATUS )
+
+* Transform the positions.
+      CALL AST_TRAN2( THIS, NP, %VAL( IPXIN ), %VAL( IPYIN ), FORWRD, 
+     :                %VAL( IPXOUT ), %VAL( IPYOUT ), STATUS )
+
+*  Output the results.
+      CALL ATL1_PRNT2( NP, %VAL( IPXOUT ), %VAL( IPYOUT ), 'XOUT', 
+     :                 'YOUT', STATUS )
+
+*  Free resources.
+      CALL GRP_DELET( IGRP1, STATUS )
+      CALL GRP_DELET( IGRP2, STATUS )
+      CALL PSX_FREE( IPXIN, STATUS )
+      CALL PSX_FREE( IPYIN, STATUS )
+      CALL PSX_FREE( IPXOUT, STATUS )
+      CALL PSX_FREE( IPYOUT, STATUS )
+
+*  End the AST context.
+      CALL AST_END( STATUS )
+
+*  Give a context message if anything went wrong.
+      IF( STATUS .NE. SAI__OK ) THEN
+         CALL ERR_REP( 'ASTTRAN2_ERR', 'Error transforming 2-'//
+     :                 'dimensional positions.', STATUS )
+      END IF
+
+      END
