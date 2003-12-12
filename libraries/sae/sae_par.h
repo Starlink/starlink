ebc15ad9fa9913bbdf6def42c82c80af56ffcaf9
Norman Gray <norman@ASTRO.GLA.AC.UK>
2003-12-12 20:06:43 +0000

    Initial version; autoconfed
    svn path=/trunk/; revision=11581

---
+++ libraries/sae/sae_par.h
@@ -0,0 +1,35 @@
+/*+
+ *  Name:
+ *     SAE_PAR.H
+
+ *  Purpose:
+ *     Define the Starlink ADAM Environment public constants.
+
+ *  Language:
+ *     Starlink ANSI C
+
+ *  Type of module:
+ *     Global constants header file.
+
+ *  Description:
+ *     This file defines the ADAM status values for non-ADAM users.
+
+ *  Authors:
+ *     PCTR: P.C.T. Rees (STARLINK)
+
+ *  History:
+ *     12-JUN-1990 (PCTR):
+ *        Original version.
+
+ *  Bugs:
+
+ *- */
+
+/* OK Status. */
+# define   SAI__OK      0
+
+/* Warning. */
+# define   SAI__WARN    148013859
+ 
+/* Error. */
+# define   SAI__ERROR   148013867
