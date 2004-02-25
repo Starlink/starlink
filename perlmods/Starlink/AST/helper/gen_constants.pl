39d521267e4588bb0f5e7579d3c3759626e5316a
Alasdair Allan <aa@astro.ex.ac.uk>
2004-02-25 06:25:04 +0000

    Modified for use with grf.h
    svn path=/trunk/; revision=12121

--- perlmods/Starlink/AST/helper/gen_constants.pl
+++ perlmods/Starlink/AST/helper/gen_constants.pl
@@ -1,17 +1,24 @@
 #!perl
 
+use strict;
+
 # Extract constants from ast.h
 # call as
 #  grep 'define AST__' $STARLINK/include/ast.h | perl gen_constants.pl
-
+# or
+#  grep 'define GRF__' grf.h | perl gen_constants.pl
+# 
 my $switch;
 $switch = 'STATUS'; # Also allow WCSMAP
 $switch = 'WCSMAP';
+$switch = 'ASTGRF';
 
 if ($switch eq 'STATUS') {
   print "MODULE = Starlink::AST  PACKAGE = Starlink::AST::Status\n\n";
 } elsif ($switch eq 'WCSMAP') {
   print "MODULE = Starlink::AST  PACKAGE = Starlink::AST::WcsMap\n\n";
+} elsif ($switch eq 'ASTGRF' ) {
+  print "MODULE = Starlink::AST  PACKAGE = Starlink::AST::Grf\n\n";
 }
 
 my $keep = 0;
@@ -27,6 +34,9 @@ while (<>) {
     $keep = 1 if $x[1] eq 'AST__AZP';
     $keep = 0 if $x[1] eq 'AST__NPID';
     next unless $keep;
+  } elsif ($switch eq 'ASTGRF' ) {
+    $keep = 1;
+    next unless $keep; 
   } else {
     next;
   }
