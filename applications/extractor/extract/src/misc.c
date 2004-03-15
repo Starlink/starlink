b87a853866637e6e3ef02d3b4368e75bd2e2660d
Peter W. Draper <P.W.Draper@durham.ac.uk>
2004-03-15 16:17:31 +0000

    Now SExtractor version. Functions moved to fits/fitsmisc.c.
    svn path=/trunk/; revision=12490

--- applications/extractor/extract/src/misc.c
+++ applications/extractor/extract/src/misc.c
@@ -5,72 +5,21 @@
 *
 *	Part of:	SExtractor
 *
-*	Author:		E.BERTIN, IAP/ESO
+*	Author:		E.BERTIN (IAP)
 *
 *	Contents:	miscellaneous functions.
 *
-*	Last modify:	31/05/97
-*                       28/10/98 (AJC)
-*                         ADAM version of error
-*                         Use AFPRINTF in warning
+*	Last modify:	13/12/2002
 *
 *%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 */
 
-#include	<ctype.h>
-#include	<stdio.h>
-#include	<stdlib.h>
-#include        <setjmp.h>
+#ifdef HAVE_CONFIG_H
+#include        "config.h"
+#endif
 
 #include	"define.h"
 #include	"globals.h"
-#include 	"merswrap.h"
-#include        "sae_par.h"
-
-extern jmp_buf		env;
-
-/********************************* error ************************************/
-/*
-Version for ADAM
-
-I hope it will never be used!
-*/
-
-void	error(int num, char *msg1, char *msg2)
-  {
-  int status;
-
-  status = SAI__ERROR;
-  msgSetc( "TOK1", msg1 );
-  msgSetc( "TOK2", msg2 );
-  errRep( " ", "^TOK1 ^TOK2", &status );
-  longjmp( env, num );
-  }
-
-/******************************* cistrcmp ***********************************/
-/*
-case-insensitive strcmp.
-*/
-int	cistrcmp(char *cs, char *ct, int mode)
-
-  {
-   int	diff;
-
-  if (mode)
-    {
-    while (*cs && *ct)
-      if (diff=tolower((int)*(cs++))-tolower((int)*(ct++)))
-        return diff;
-    }
-  else
-    {
-    while (*cs || *ct)
-      if (diff=tolower((int)*(cs++))-tolower((int)*(ct++)))
-        return diff;
-    }
-
-  return 0;
-  }
 
 
 /******************************** hmedian ***********************************/
@@ -120,63 +69,4 @@ float	hmedian(float *ra, int n)
 /* (the 'return' is inside the loop!!) */
   }
 
-
-/********************************* warning **********************************/
-/*
-Print a warning message on screen.
-*/
-void	warning(char *msg1, char *msg2)
-  {
-  AFPRINTF(OUTPUT, "\n> WARNING: %s%s.\n\n",msg1,msg2);
-  return;
-  }
-
-
-/******************************* swapbytes **********************************/
-/*
-Swap bytes for doubles, longs and shorts (for DEC machines or PC for inst.).
-*/
-void	swapbytes(void *ptr, int nb, int n)
-  {
-   char	*cp;
-   int	j;
-
-  cp = (char *)ptr;
-
-  if (nb&4)
-    {
-    for (j=n; j--; cp+=4)
-      {
-      cp[0] ^= (cp[3]^=(cp[0]^=cp[3]));
-      cp[1] ^= (cp[2]^=(cp[1]^=cp[2]));
-      }
-    return;
-    }
-
-  if (nb&2)
-    {
-    for (j=n; j--; cp+=2)
-      cp[0] ^= (cp[1]^=(cp[0]^=cp[1]));
-    return;
-    }
-
-  if (nb&1)
-    return;
-
-  if (nb&8)
-    {
-    for (j=n; j--; cp+=8)
-      {
-      cp[0] ^= (cp[7]^=(cp[0]^=cp[7]));
-      cp[1] ^= (cp[6]^=(cp[1]^=cp[6]));
-      cp[2] ^= (cp[5]^=(cp[2]^=cp[5]));
-      cp[3] ^= (cp[4]^=(cp[3]^=cp[4]));
-      }
-    return;
-    }
-
-  error(EXIT_FAILURE, "*Internal Error*: Unknown size in ", "swapbytes()");
-
-  return;
-  }
 
