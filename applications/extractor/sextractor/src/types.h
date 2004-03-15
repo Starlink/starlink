4483cf239276742331b50055ea9b258859ff56c2
Peter W. Draper <P.W.Draper@durham.ac.uk>
2004-03-15 13:58:55 +0000

    Merged changes for 2.3. The preferences section is now in prefs.h.
    svn path=/trunk/; revision=12473

--- applications/extractor/sextractor/src/types.h
+++ applications/extractor/sextractor/src/types.h
@@ -15,11 +15,17 @@
 *                                       support userradii function.
 *                       20/02/02 (PWD): Added ndfposx and ndfposy
 *                                       members.
+*	Last modify:	16/12/2002
+*                                (EB): 2.3.
 *
 *%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 */
 
 #include <stdio.h>
+
+#ifndef _FITSCAT_H_
+#include "fits/fitscat.h"
+#endif
 
 /*-------------------------------- flags ------------------------------------*/
 
@@ -41,17 +47,11 @@
 /*--------------------------------- typedefs --------------------------------*/
 typedef	unsigned char	BYTE;			/* a byte */
 typedef	unsigned short	USHORT;			/* 0 to 65535 integers */
-typedef	float		PIXTYPE;		/* pixel type */
 typedef unsigned int	FLAGTYPE;		/* flag type */
 typedef	char		pliststruct;		/* Dummy type for plist */
 
-#ifdef INT64
 typedef	int		LONG;
 typedef	unsigned int	ULONG;
-#else
-typedef	long		LONG;
-typedef	unsigned long	ULONG;
-#endif
 
 typedef  enum {BACK_RELATIVE, BACK_ABSOLUTE}
 		backenum;				/* BACK_TYPE */
@@ -149,7 +149,7 @@ typedef struct
   double	posx,posy;			/* "FITS" pos. in pixels */
   double	mamaposx,mamaposy;		/* "MAMA" pos. in pixels */
   float		sposx,sposy;			/* single precision pos. */
-  float		ndfposx,ndfposy;		/* single precision pos. 
+  float		ndfposx,ndfposy;		/* PWD: single precision pos. 
 						   NDF Pixel coordinates */
   float		poserr_a, poserr_b,
 		poserr_theta;			/* Error ellips parameters */
@@ -248,6 +248,8 @@ typedef struct
   float		fluxerr_galfit;			/* RMS error on galfit flux */
   float		mag_galfit;			/* Galaxy tot. mag from fit */
   float		magerr_galfit;			/* RMS error on galfit mag */
+/* ---- MEF */
+  short		ext_number;			/* FITS extension number */
   }	obj2struct;
 
 /*----------------------------- lists of objects ----------------------------*/
@@ -276,7 +278,7 @@ typedef struct pic
   int		bitpix, bytepix;	/* nb of bits and bytes per pixel */
   int		bitsgn;			/* non-zero if signed integer data */
   int		width, height;		/* x,y size of the field */
-  size_t	npix;			/* total number of pixels */
+  KINGSIZE_T	npix;			/* total number of pixels */
   double	bscale, bzero;		/* FITS scale and offset */
   double	ngamma;			/* normalized photo gamma */
   int		nlevels;		/* nb of quantification levels */
@@ -294,7 +296,7 @@ typedef struct pic
   int		stripylim;		/* y limit in buffer */
   int		stripysclim;		/* y scroll limit in buffer */
 /* ---- image (de-)compression */
-  enum {COMPRESS_NONE, COMPRESS_BASEBYTE, COMPRESS_PREVPIX}
+  enum {ICOMPRESS_NONE, ICOMPRESS_BASEBYTE, ICOMPRESS_PREVPIX}
 		compress_type;		/* image compression type */
   char		*compress_buf;		/* de-compression buffer */
   char		*compress_bufptr;	/* present pixel in buffer */
@@ -333,178 +335,8 @@ typedef struct pic
   int		interp_xtimeout;	/* interpolation timeout value in x */
   int		interp_ytimeout;	/* interpolation timeout value in y */
   struct pic	*reffield;	       	/* pointer to a reference field */
+  OFF_T		mefpos;			/* Position in a MEF file */
   }	picstruct;
-
-
-/*------------------------------- preferences -------------------------------*/
-typedef struct
-  {
-  char		prefs_name[MAXCHAR];			/* prefs filename*/
-  char		*(image_name[2]);			/* image filenames */
-  int		nimage_name;				/* nb of params */
-  char		cat_name[MAXCHAR];			/* catalog filename*/
-/*----- thresholding */
-  double	dthresh[2];				/* detect. threshold */
-  int		ndthresh;				/* (1 or 2 entries) */
-  double	thresh[2];				/* analysis thresh. */
-  int		nthresh;				/* (1 or 2 entries) */
-  enum	{THRESH_RELATIVE, THRESH_ABSOLUTE}
-					thresh_type[2];	/* bkgnd type */
-  int		nthresh_type;				/* nb of params */
-/*----- extraction */
-  int		dimage_flag;				/* detect. image ? */
-  int		ext_minarea;				/* min area in pix. */
-  int		deb_maxarea;				/* max deblend. area */
-  int		filter_flag;				/* smoothing on/off */
-  char		filter_name[MAXCHAR];			/* mask filename */
-  double	filter_thresh[2];			/* Filter thresholds */
-  int		nfilter_thresh;				/* nb of params */
-  int		deblend_nthresh;			/* threshold number */
-  double	deblend_mincont;			/* minimum contrast */
-  double	satur_level;				/* saturation level */
-  enum	{CCD, PHOTO}			detect_type;	/* detection type */
-/*----- Flagging */
-  char		*(fimage_name[MAXFLAG]);		/* flagmap filenames */
-  int		nfimage_name;				/* nb of params */
-  enum	{FLAG_OR, FLAG_AND, FLAG_MIN, FLAG_MAX, FLAG_MOST}
-				flag_type[MAXFLAG];	/* flag combination */
-  int		imaflag_size;				/* requested iso nb1 */
-  int		imanflag_size;				/* requested iso nb2 */
-  int		nimaisoflag;				/* effective iso nb */
-  int		nimaflag;				/* effective ima nb */
-/*----- cleaning */
-  int		clean_flag;				/* allow cleaning ? */
-  double	clean_param;				/* cleaning effic. */
-/*----- Weighting */
-  char		*(wimage_name[2]);       		/* weight filenames */
-  int		nwimage_name;				/* nb of params */
-  weightenum	weight_type[2];				/* weighting scheme */
-  int		nweight_type;				/* nb of params */
-  int		weight_flag;				/* do we weight ? */
-  int		dweight_flag;				/* detection weight? */
-  int		weightgain_flag;			/* weight gain? */
-/*----- photometry */
-  enum	{CAT_NONE, ASCII, ASCII_HEAD, ASCII_SKYCAT,
-	FITS_LDAC, FITS_BINIMHEAD, FITS_NOIMHEAD, FITS_10}
-		cat_type;				/* type of catalog */
-  enum	{PNONE, FIXED, AUTO}		apert_type;	/* type of aperture */
-  double	apert[MAXNAPER];			/* apert size (pix) */
-  int		naper;					/* effective apert. */
-  int		flux_apersize, fluxerr_apersize;	/* requested apert. */
-  int		mag_apersize, magerr_apersize;		/* requested apert. */
-  double	autoparam[2];				/* Kron parameters */
-  int		nautoparam;				/* nb of Kron params */
-  double	autoaper[2];				/* minimum apertures */
-  int		nautoaper;				/* nb of min. aperts */
-  double	mag_zeropoint;				/* magnitude offsets */
-  double	mag_gamma;				/* for emulsions */
-  double	gain;					/* only for CCD */
-/*----- S/G separation */
-  double	pixel_scale;				/* in arcsec */
-  double	seeing_fwhm;				/* in arcsec */
-  char		nnw_name[MAXCHAR];			/* nnw filename */
-/*----- background */
-  enum	{IMAGE, AFILE}			back_origin;	/* origin of bkgnd */
-  char		back_name[MAXCHAR];			/* bkgnd filename */
-  backenum	back_type[2];				/* bkgnd type */
-  int		nback_type;				/* nb of params */
-  double	back_val[2];				/* user-def. bkg */
-  int		nback_val;				/* nb of params */
-  int		backsize[2];				/* bkgnd mesh size */
-  int		nbacksize;				/* nb of params */
-  int		backfsize[2];				/* bkgnd filt. size */
-  int		nbackfsize;				/* nb of params */
-  double	backfthresh;				/* bkgnd fil. thresh */
-  enum	{GLOBAL, LOCAL}			pback_type;	/* phot. bkgnd type */
-  int		pback_size;				/* rect. ann. width */
-/*----- memory */
-  int		clean_stacksize;			/* size of buffer */
-  int		mem_pixstack;				/* pixel stack size */
-  int		mem_bufsize;				/* strip height */
-/*----- catalog output */
-  char		param_name[MAXCHAR];			/* param. filename */
-/*----- miscellaneous */
-  int		pipe_flag;				/* allow piping ? */
-  enum	{QUIET, NORM, WARN, FULL}      	verbose_type;	/* display type */
-/*----- CHECK-images */
-  int		check_flag;				/* CHECK-image flag */
-  checkenum    	check_type[MAXCHECK];		       	/* check-image types */
-  int		ncheck_type;				/* nb of params */
-  char		*(check_name[MAXCHECK]);       		/* check-image names */
-  int		ncheck_name;				/* nb of params */
-  struct structcheck	*(check[MAXCHECK]);		/* check-image ptrs */
-/*----- ASSOCiation */
-  int		assoc_flag;				/* ASSOCiation flag */
-  char		assoc_name[MAXCHAR];			/* ASSOC-list name */
-  int		assoc_param[3];				/* ASSOC param cols */
-  int		nassoc_param;				/* nb of params */
-  int		assoc_data[MAXNASSOC];		       	/* ASSOC data cols */
-  int		nassoc_data;				/* nb of params */
-  enum	{ASSOC_FIRST, ASSOC_NEAREST, ASSOC_MEAN, ASSOC_MAGMEAN,
-	 ASSOC_SUM, ASSOC_MAGSUM, ASSOC_MIN, ASSOC_MAX}
-		assoc_type;				/* type of assoc. */
-  enum	{ASSOCSELEC_ALL, ASSOCSELEC_MATCHED, ASSOCSELEC_NOMATCHED}
-		assocselec_type;		       	/* type of assoc. */
-  double	assoc_radius;				/* ASSOC range */
-  int		assoc_size;				/* nb of parameters */
-  char		retina_name[MAXCHAR];			/* retina filename */
-  int		vignetsize[2];				/* vignet size */
-  int		vigshiftsize[2];			/* shift-vignet size */
-  int		cleanmargin;				/* CLEANing margin */
-  char		som_name[MAXCHAR];			/* SOM filename */
-  int		somfit_flag;				/* ASSOCiation flag */
-  int		somfit_vectorsize;			/* SOMfit vec. size */
-/*----- masking */
-  enum {MASK_NONE, MASK_BLANK, MASK_CORRECT}
-		mask_type;				/* type of masking */
-  int		blank_flag;				/* BLANKing flag */
-  double	weight_thresh[2];      			/* weight threshlds */
-  int		nweight_thresh;				/* nb of params */
-/*----- interpolation */
-  enum	{INTERP_NONE, INTERP_VARONLY, INTERP_ALL}
-		interp_type[2];				/* interpolat. type */
-  int		ninterp_type;				/* nb of params */
-  int		interp_xtimeout[2];   			/* interp. x timeout */
-  int		ninterp_xtimeout;       	        /* nb of params */
-  int		interp_ytimeout[2];   			/* interp. y timeout */
-  int		ninterp_ytimeout;       		/* nb of params */
-/*----- astrometry */
-  int		world_flag;				/* WORLD required */
-/*----- growth curve */
-  int		growth_flag;				/* gr. curve needed */
-  int		flux_growthsize;       			/* number of elem. */
-  int		mag_growthsize;       			/* number of elem. */
-  int		flux_radiussize;       			/* number of elem. */
-  double	growth_step;				/* step size (pix) */
-  double	flux_frac[MAXNAPER];			/* for FLUX_RADIUS */
-  int		nflux_frac;       			/* number of elem. */
-/*----- PSF-fitting */
-  int		psf_flag;				/* PSF-fit needed */
-  char		psf_name[MAXCHAR];			/* PSF filename */
-  int		psf_npsfmax;				/* Max # of PSFs */
-  enum	{PSFDISPLAY_SPLIT, PSFDISPLAY_VECTOR}
-		psfdisplay_type;			/* PSF display type */
-  int		psf_xsize,psf_ysize;			/* nb of params */
-  int		psf_xwsize,psf_ywsize;			/* nb of params */
-  int		psf_alphassize,psf_deltassize;		/* nb of params */
-  int		psf_alpha2000size,psf_delta2000size;	/* nb of params */
-  int		psf_alpha1950size,psf_delta1950size;	/* nb of params */
-  int		psf_fluxsize;				/* nb of params */
-  int		psf_fluxerrsize;			/* nb of params */
-  int		psf_magsize;				/* nb of params */
-  int		psf_magerrsize;				/* nb of params */
-  int		pc_flag;				/* PC-fit needed */
-  int		pc_vectorsize;				/* nb of params */
-/*----- customize */
-  double	mama_corflex;
-  int		fitsunsigned_flag;			/* Force unsign FITS */
-
-  enum	{RAD_SB, RAD_INT}
-		rad_type;				/* PWD: radii units */
-  int           nrad_type;
-  double        rad[3];                                 /* PWD: radii specs */
-  int           nrad;                                   /* PWD: nb of params */
-  }	prefstruct;
 
 
 /*-------------------------------- catalog  ---------------------------------*/
@@ -519,9 +351,12 @@ typedef struct
   char		image_name[MAXCHAR];			/* image filename*/
   char		nnw_name[MAXCHAR];			/* NNW name */
   char		filter_name[MAXCHAR];			/* Filter name */
-  char		soft_name[MAXCHAR];			/* Sextractor version */
+  char		soft_name[MAXCHAR];			/* Sextractor version*/
 /*----- time */
   char		ext_date[12],ext_time[12];		/* date and time */
   double	ext_elapsed;				/* processing time */
+/*----- MEF */
+  int		currext;				/* current extension */
+  int		next;					/* Nb of extensions */
   }		sexcatstruct;
 
