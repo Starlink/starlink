5146dc0f462523b71c9a7c2b2cc3e909c4e38731
Peter W. Draper <P.W.Draper@durham.ac.uk>
2004-03-17 13:34:36 +0000

    Moved functions to same positions as SExtractor (better for comparison).
    svn path=/trunk/; revision=12521

--- applications/extractor/extract/src/readimage.c
+++ applications/extractor/extract/src/readimage.c
@@ -49,136 +49,6 @@
 #include        "ndf.h"
 #include        "merswrap.h"
 
-/******************************* readimagehead *******************************/
-/*
-extract some data from the FITS-file header
-*/
-void	readimagehead(picstruct *field)
-  {
-   int          status = SAI__OK;
-   int		i,j,l, n;
-   int          ndims, dims[NDF__MXDIM];
-   int          there;
-   char		*buf, st[80], str[80], *point;
-   char         type[20];
-   void         *pntr[3];
-   int          nel;
-   int          placehldr;
-   int          lbnd[NDF__MXDIM];
-   int          ubnd[NDF__MXDIM];
-
-/* Open the file */
-  field->file = 0;
-  ndfOpen( DAT__ROOT, field->filename, "READ", "OLD",
-           &field->ndf, &placehldr, &status );
-
-  if (status != SAI__OK)
-    error(EXIT_FAILURE,"*Error*: Failed to open ", field->filename);
-
-/*---------------------------- Basic keywords ------------------------------*/
-  ndfType( field->ndf, "DATA", type, 20, &status );
-  if (!(status == SAI__OK))
-    error(EXIT_FAILURE,"*Error*: Failed to get data type. ", field->filename);
-
-  field->bitsgn = 1;
-  if (!strcmp(type,"_BYTE")) {
-    field->bitpix = BP_BYTE;
-  } else if (!strcmp(type,"_UBYTE")) {
-    field->bitpix = BP_BYTE;
-    field->bitsgn = 0;
-  } else if (!strcmp(type,"_WORD")) {
-    field->bitpix = BP_SHORT;
-  } else if (!strcmp(type,"_UWORD")) {
-    field->bitpix = BP_SHORT;
-    field->bitsgn = 0;
-  } else if (!strcmp(type,"_INTEGER")) {
-    field->bitpix = BP_LONG;
-  } else if (!strcmp(type,"_REAL")) {
-    field->bitpix = BP_FLOAT;
-  } else if (!strcmp(type,"_DOUBLE")) {
-    field->bitpix = BP_DOUBLE;
-  } else error(EXIT_FAILURE, "Sorry, I don't know that kind of data.", "");
-
-/*  field->bytepix = (field->bitpix>0?field->bitpix:-field->bitpix)>>3;*/
-  field->bytepix = 4;
-
-  ndfDim( field->ndf, NDF__MXDIM, dims, &ndims, &status );
-  if ( !(ndims==2))
-    error( EXIT_FAILURE, field->filename, " does NOT contain 2D data." );
-
-  field->width = dims[0];
-  field->height = dims[1];
-  field->npix = (KINGSIZE_T)field->width*field->height;
-
-  field->bscale = 1.0;
-  field->bzero = 0.0;
-  if (field->bitsgn && prefs.fitsunsigned_flag)
-    field->bitsgn = 0;
-
-  ndfCget( field->ndf, "TITLE", field->ident, MAXCHAR, &status );
-
-  ndfBound( field->ndf, NDF__MXDIM, lbnd, ubnd, &ndims, &status );
-  field->origin[0] = lbnd[0];
-  field->origin[1] = lbnd[1];
-
-/*----------------------------- Astrometry ---------------------------------*/
-/* Presently, astrometry is done only on the measurement and detect images */
-  if ( field->flags & ( MEASURE_FIELD | DETECT_FIELD ) ) {
-    astromstruct *as;
-    AstFrameSet  *wcsinfo;
-    AstFrame     *frame;
-    int          exists;
-    int          issky;
-
-    QCALLOC(as, astromstruct, 1);
-    field->astrom = as;
-
-    /* See if the NDF has a WCS component */
-    status = SAI__OK;
-    field->astwcs = NULL;
-    ndfState( field->ndf, "WCS", &exists, &status );
-    if ( exists ) {
-
-      /* Access it and record the identifier */
-      ndfGtwcs( field->ndf, &wcsinfo, &status );
-      as->naxis = 2;
-      field->astwcs = wcsinfo;
-
-      /* Make sure the WCS represents a celestial coordinate system
-         (that's all we can deal with). */
-      frame = astGetFrame( wcsinfo, AST__CURRENT );
-      issky = astIsASkyFrame( frame );
-      frame = astAnnul( frame );
-      if ( ! issky ) {
-        /*  No WCS */
-        as->wcs_flag = 0;
-      } else {
-        as->wcs_flag = 1;
-      }
-      if ( status != SAI__OK ) errAnnul( &status );
-    }
-    else {
-      /*  No WCS */
-      as->wcs_flag = 0;
-    }
-
-    /*-----------------------------------------------------------------------*/
-  } /* end if MEASURE or DETECT field */
-
-  /*-------------------------------------------------------------------------*/
-
-  /* Map the NDF in the appropriate type */
-    if (field->flags ^ FLAG_FIELD)
-      ndfMap( field->ndf, "DATA", "_REAL", "READ", pntr, &nel, &status );
-    else
-      ndfMap( field->ndf, "DATA", "_REAL", "READ", pntr, &nel, &status );
-    field->map = pntr[0];
-    field->file = 0;
-    field->nel = nel;
-
-  return;
-  }
-
 /******************************* loadstrip ***********************************/
 /*
 Load a new strip of pixel data into the buffer.
@@ -371,8 +241,9 @@ void	readdata(picstruct *field, PIXTYPE *ptr, int size)
   bz = (PIXTYPE)field->bzero;
 
   left = field->nel - field->file;
-  for (i=0; i<(size<left?size:left); i++)
+  for ( i=0; i< (size < left ? size : left ); i++ ) {
      *(ptr++) = *((PIXTYPE *)field->map + field->file++)*bs + bz;
+  }
 
 /* Reset field->file if at end */
 /* This prevents endfield from crashing if field->file is used for NDF */
@@ -401,5 +272,135 @@ void	readidata(picstruct *field, FLAGTYPE *ptr, int size)
 /* Reset field->file if at end */
 /* This prevents endfield from crashing if field->file is used for NDF */
   field->file = (field->file >= field->nel)?0:field->file;
+  return;
+  }
+
+/******************************* readimagehead *******************************/
+/*
+extract some data from the FITS-file header
+*/
+void	readimagehead(picstruct *field)
+  {
+   int          status = SAI__OK;
+   int		i,j,l, n;
+   int          ndims, dims[NDF__MXDIM];
+   int          there;
+   char		*buf, st[80], str[80], *point;
+   char         type[20];
+   void         *pntr[3];
+   int          nel;
+   int          placehldr;
+   int          lbnd[NDF__MXDIM];
+   int          ubnd[NDF__MXDIM];
+
+/* Open the file */
+  field->file = 0;
+  ndfOpen( DAT__ROOT, field->filename, "READ", "OLD",
+           &field->ndf, &placehldr, &status );
+
+  if (status != SAI__OK)
+    error(EXIT_FAILURE,"*Error*: Failed to open ", field->filename);
+
+/*---------------------------- Basic keywords ------------------------------*/
+  ndfType( field->ndf, "DATA", type, 20, &status );
+  if (!(status == SAI__OK))
+    error(EXIT_FAILURE,"*Error*: Failed to get data type. ", field->filename);
+
+  field->bitsgn = 1;
+  if (!strcmp(type,"_BYTE")) {
+    field->bitpix = BP_BYTE;
+  } else if (!strcmp(type,"_UBYTE")) {
+    field->bitpix = BP_BYTE;
+    field->bitsgn = 0;
+  } else if (!strcmp(type,"_WORD")) {
+    field->bitpix = BP_SHORT;
+  } else if (!strcmp(type,"_UWORD")) {
+    field->bitpix = BP_SHORT;
+    field->bitsgn = 0;
+  } else if (!strcmp(type,"_INTEGER")) {
+    field->bitpix = BP_LONG;
+  } else if (!strcmp(type,"_REAL")) {
+    field->bitpix = BP_FLOAT;
+  } else if (!strcmp(type,"_DOUBLE")) {
+    field->bitpix = BP_DOUBLE;
+  } else error(EXIT_FAILURE, "Sorry, I don't know that kind of data.", "");
+
+/*  field->bytepix = (field->bitpix>0?field->bitpix:-field->bitpix)>>3;*/
+  field->bytepix = 4;
+
+  ndfDim( field->ndf, NDF__MXDIM, dims, &ndims, &status );
+  if ( !(ndims==2))
+    error( EXIT_FAILURE, field->filename, " does NOT contain 2D data." );
+
+  field->width = dims[0];
+  field->height = dims[1];
+  field->npix = (KINGSIZE_T)field->width*field->height;
+
+  field->bscale = 1.0;
+  field->bzero = 0.0;
+  if (field->bitsgn && prefs.fitsunsigned_flag)
+    field->bitsgn = 0;
+
+  ndfCget( field->ndf, "TITLE", field->ident, MAXCHAR, &status );
+
+  ndfBound( field->ndf, NDF__MXDIM, lbnd, ubnd, &ndims, &status );
+  field->origin[0] = lbnd[0];
+  field->origin[1] = lbnd[1];
+
+/*----------------------------- Astrometry ---------------------------------*/
+/* Presently, astrometry is done only on the measurement and detect images */
+  if ( field->flags & ( MEASURE_FIELD | DETECT_FIELD ) ) {
+    astromstruct *as;
+    AstFrameSet  *wcsinfo;
+    AstFrame     *frame;
+    int          exists;
+    int          issky;
+
+    QCALLOC(as, astromstruct, 1);
+    field->astrom = as;
+
+    /* See if the NDF has a WCS component */
+    status = SAI__OK;
+    field->astwcs = NULL;
+    ndfState( field->ndf, "WCS", &exists, &status );
+    if ( exists ) {
+
+      /* Access it and record the identifier */
+      ndfGtwcs( field->ndf, &wcsinfo, &status );
+      as->naxis = 2;
+      field->astwcs = wcsinfo;
+
+      /* Make sure the WCS represents a celestial coordinate system
+         (that's all we can deal with). */
+      frame = astGetFrame( wcsinfo, AST__CURRENT );
+      issky = astIsASkyFrame( frame );
+      frame = astAnnul( frame );
+      if ( ! issky ) {
+        /*  No WCS */
+        as->wcs_flag = 0;
+      } else {
+        as->wcs_flag = 1;
+      }
+      if ( status != SAI__OK ) errAnnul( &status );
+    }
+    else {
+      /*  No WCS */
+      as->wcs_flag = 0;
+    }
+
+    /*-----------------------------------------------------------------------*/
+  } /* end if MEASURE or DETECT field */
+
+  /*-------------------------------------------------------------------------*/
+
+  /* Map the NDF in the appropriate type */
+    if (field->flags ^ FLAG_FIELD)
+      ndfMap( field->ndf, "DATA", "_REAL", "READ", pntr, &nel, &status );
+    else
+      ndfMap( field->ndf, "DATA", "_REAL", "READ", pntr, &nel, &status );
+    field->map = pntr[0];
+    field->file = 0;
+    field->nel = nel;
+
   return;
   }
