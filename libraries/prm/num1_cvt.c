a606cfb48ce1f2181ec648dccba43c25027a417e
Norman Gray <norman@ASTRO.GLA.AC.UK>
2004-03-21 13:38:24 +0000

    _ix86_Linux file made into a configured, platform-independent, file
    svn path=/trunk/; revision=12559

---
+++ libraries/prm/num1_cvt.c
@@ -0,0 +1,372 @@
+/*
+ * Low level numerical conversion functions for primdat, to fill
+ * the gap on systems where the Fortran compiler doesn't support the
+ * intrinsics IZEXT and friends.
+ *
+ * The functions below are to be invoked from Fortran, hence the
+ * trailing underscores.  We might want to use the CNF macros at some
+ * point, and we certainly should do so if we require anything more
+ * involved than the single trailing underscore below.
+ */
+
+/* 
+ * Config.h defines WORDS_BIGENDIAN if the current platform stores
+ * words with the most significant byte first (Motorola, Sparc), and
+ * doesn't define if on platforms with the least significant byte
+ * first (Intel, VAX).
+ */
+#include <config.h>
+
+/*
+ * We don't have to do anything if the Fortran compiler has the IZEXT
+ * intrinsic, because then the functions below will have been defined
+ * as Fortran statement functions within NUM_DEV_CVT.
+ */
+#if !HAVE_INTRINSIC_IZEXT
+
+/*
+ * The bigendian code below relies on integers being 4 bytes, and words
+ * being 2, and will lose badly if this is not the case.  So check
+ * this.  I think we could get around this by looking at the contents
+ * of float.h: that doesn't actually give the number of bytes in a
+ * float, but it would allow you to work it out.
+ */
+#if WORDS_BIGENDIAN
+#  if sizeof(int) != 4*sizeof(char)
+#    error "num1_cvt.c is specific to 4-byte integers"
+#  endif
+#  if sizeof(short int) != 2*sizeof(char)
+#    error "num1_cvt.c is specific to 2-byte short integers"
+#  endif
+#endif
+
+int num1_ubtoi_( unsigned char *num_argub )
+{
+/*
+*+
+*  Name:
+*     NUM1_UBTOI
+
+*  Purpose:
+*     Convert an unsigned byte to an integer.
+
+*  Language:
+*     C
+
+*  Authors:
+*     BKM: B.K. McIlwrath (STARLINK, RAL)
+*     NG: Norman Gray (Starlink, Glasgow)
+*     {enter_new_authors_here}
+
+*  History:
+*     04-OCT-1995 (BKM):
+*        Original version.
+*     16-MAR-2004 (NG):
+*        Bytesex-agnostic version
+*     {enter_changes_here}
+
+*  Bugs:
+*     This implementation involves a function call overhead.
+*     {note_any_bugs_here}
+*-
+*/
+
+#if WORDS_BIGENDIAN
+    union {
+        int ret_val;
+        unsigned char[4] ubyte;
+    } un;
+
+    un.ret_val = 0;
+    un.ubyte[3] = *num_argub;
+
+#else
+
+    union {
+	int ret_val;
+        unsigned char ubyte;
+    } un;
+
+    un.ret_val = 0;
+    un.ubyte = *num_argub;
+#endif
+
+    return un.ret_val;
+}
+
+
+unsigned short int num1_ubtouw_( unsigned char *num_argub )
+{
+/*
+*+
+*  Name:
+*     NUM1_UBTOUW
+
+*  Purpose:
+*     Convert an unsigned byte to an unsigned word.
+
+*  Language:
+*     C
+
+*  Authors:
+*     BKM: B.K. McIlwrath (STARLINK, RAL)
+*     NG: Norman Gray (Starlink, Glasgow)
+*     {enter_new_authors_here}
+
+*  History:
+*     04-OCT-1995 (BKM):
+*        Original version.
+*     {enter_changes_here}
+
+*  Bugs:
+*     This implementation involves a function call overhead.
+*     {note_any_bugs_here}
+*-
+*/
+
+#if WORDS_BIGENDIAN
+    union {
+        unsigned short ret_val;
+        unsigned char[2] ubyte;
+    } un;
+
+    un.ret_val = 0;
+    un.ubyte[1] = *num_argub;
+
+#else
+
+    union {
+	unsigned short int ret_val;
+        unsigned char ubyte;
+    } un;
+
+    un.ret_val = 0;
+    un.ubyte = *num_argub;
+
+#endif
+
+    return un.ret_val;
+}
+
+
+short int num1_ubtow_( unsigned char *num_argub )
+{
+/*
+*+
+*  Name:
+*     NUM1_UBTOW
+
+*  Purpose:
+*     Convert an unsigned byte to a word.
+
+*  Language:
+*     C
+
+*  Authors:
+*     BKM: B.K. McIlwrath (STARLINK, RAL)
+*     NG: Norman Gray (Starlink, Glasgow)
+*     {enter_new_authors_here}
+
+*  History:
+*     04-OCT-1995 (BKM):
+*        Original version.
+*     16-MAR-2004 (NG):
+*        Bytesex-agnostic version
+*     {enter_changes_here}
+
+*  Bugs:
+*     This implementation involves a function call overhead.
+*     {note_any_bugs_here}
+*-
+*/
+
+#if WORDS_BIGENDIAN
+
+    union {
+        short ret_val;
+        unsigned char[2] ubyte;
+    } un;
+
+    un.ret_val = 0;
+    un.ubyte[1] = *num_argub;
+
+#else
+
+    union {
+	short ret_val;
+        unsigned char ubyte;
+    } un;
+
+    un.ret_val = 0;
+    un.ubyte = *num_argub;
+
+#endif
+
+    return un.ret_val;
+}
+
+int num1_uwtoi_( unsigned short int *num_arguw )
+{
+/*
+*+
+*  Name:
+*     NUM1_UWTOI
+
+*  Purpose:
+*     Convert an unsigned word to an integer.
+
+*  Language:
+*     C
+
+*  Authors:
+*     BKM: B.K. McIlwrath (STARLINK, RAL)
+*     NG: Norman Gray (Starlink, Glasgow)
+*     {enter_new_authors_here}
+
+*  History:
+*     04-OCT-1995 (BKM):
+*        Original version.
+*     16-MAR-2004 (NG):
+*        Bytesex-agnostic version
+*     {enter_changes_here}
+
+*  Bugs:
+*     This implementation involves a function call overhead.
+*     {note_any_bugs_here}
+*-
+*/
+
+#if WORDS_BIGENDIAN
+
+    union {
+        int ret_val;
+        unsigned short[2] uword;
+    } un;
+
+    un.ret_val = 0;
+    uword[1] = *num_arguw;
+
+#else
+
+    union {
+	int ret_val;
+        unsigned short uword;
+    } un;
+
+    un.ret_val = 0;
+    un.uword = *num_arguw;
+
+#endif
+
+    return un.ret_val;
+}
+
+unsigned char num1_wtoub_( short int *num_argw )
+{
+/*
+*+
+*  Name:
+*     NUM1_WTOU
+
+*  Purpose:
+*     Convert a word to an unsigned byte.
+
+*  Language:
+*     C
+
+*  Authors:
+*     BKM: B.K. McIlwrath (STARLINK, RAL)
+*     NG: Norman Gray (Starlink, Glasgow)
+*     {enter_new_authors_here}
+
+*  History:
+*     04-OCT-1995 (BKM):
+*        Original version.
+*     16-MAR-2004 (NG):
+*        Bytesex-agnostic version
+*     {enter_changes_here}
+
+*  Bugs:
+*     This implementation involves a function call overhead.
+*     {note_any_bugs_here}
+*-
+*/
+
+#if WORDS_BIGENDIAN
+
+    union {
+        unsigned char[2] ret_val;
+        short word;
+    } un;
+
+    un.word = *num_argw;
+    return ret_val[1];
+
+#else
+
+    union {
+	unsigned char ret_val;
+        short word;
+    } un;
+
+    un.word = *num_argw;
+
+    return un.ret_val;
+#endif
+}
+
+unsigned short int num1_itouw_( int *num_argi )
+{
+/*
+*+
+*  Name:
+*     NUM1_ITOUW
+
+*  Purpose:
+*     Convert an integer to an unsigned word.
+
+*  Language:
+*     C
+
+*  Authors:
+*     BKM: B.K. McIlwrath (STARLINK, RAL)
+*     NG: Norman Gray (Starlink, Glasgow)
+*     {enter_new_authors_here}
+
+*  History:
+*     04-OCT-1995 (BKM):
+*        Original version.
+*     16-MAR-2004 (NG):
+*        Bytesex-agnostic version
+*     {enter_changes_here}
+
+*  Bugs:
+*     This implementation involves a function call overhead.
+*     {note_any_bugs_here}
+*-
+*/
+
+#if WORDS_BIGENDIAN
+
+    union {
+        unsigned short[2] ret_val;
+        int i;
+    } un;
+
+    un.i = *num_argi;
+    return un.ret_val[1];
+
+#else
+           
+    union {
+	unsigned short ret_val;
+        int i;
+    } un;
+
+    un.i = *num_argi;
+
+    return un.ret_val;
+#endif
+}
+
+#endif /* !HAVE_INTRINSIC_IZEXT */
