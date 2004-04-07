596c4c4c329c29c85aa1b98124d1a394534b2c06
Tim Jenness <t.jenness@jach.hawaii.edu>
2004-04-07 07:47:20 +0000

    missed gks_deact
    svn path=/trunk/; revision=12715

--- libraries/graphpar/gkspar/gkspar_test.f
+++ libraries/graphpar/gkspar/gkspar_test.f
@@ -2,7 +2,7 @@
       INTEGER STATUS
       STATUS = 0
 
-*  Simple test to see that the MAG routine are available to be linked against.
+*  Simple test to see that the GKS routine are available to be linked against.
 
       PRINT *,'This test program only links against the GKSPAR routines'
       PRINT *,'It does not actually call them.'
@@ -10,6 +10,7 @@
       IF( STATUS .NE. -12345 ) GOTO 999
       CALL GKS_ANNUL
       CALL GKS_ASSOC
+      CALL GKS_DEACT
       CALL GKS_CANCL
       CALL GKS_GSTAT
       CALL GKS_RESET
