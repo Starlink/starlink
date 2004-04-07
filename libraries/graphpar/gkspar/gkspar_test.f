bb0f93ac40ec9712632af65758b85f7c3eb05862
Tim Jenness <t.jenness@jach.hawaii.edu>
2004-04-07 07:44:35 +0000

    tests now work with make check
    svn path=/trunk/; revision=12714

---
+++ libraries/graphpar/gkspar/gkspar_test.f
@@ -0,0 +1,18 @@
+      PROGRAM GKSPAR_TEST
+      INTEGER STATUS
+      STATUS = 0
+
+*  Simple test to see that the MAG routine are available to be linked against.
+
+      PRINT *,'This test program only links against the GKSPAR routines'
+      PRINT *,'It does not actually call them.'
+     ://' not needed.'
+      IF( STATUS .NE. -12345 ) GOTO 999
+      CALL GKS_ANNUL
+      CALL GKS_ASSOC
+      CALL GKS_CANCL
+      CALL GKS_GSTAT
+      CALL GKS_RESET
+  999 CONTINUE
+      print *,'ending'
+      END
