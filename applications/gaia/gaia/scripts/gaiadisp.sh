ad19db34899a2359a7527249f354d168cbac02ac
Peter W. Draper <P.W.Draper@durham.ac.uk>
2004-04-15 15:38:46 +0000

    Intergrated gaiadispmany into this command.
    svn path=/trunk/; revision=12767

--- applications/gaia/gaia/scripts/gaiadisp.sh
+++ applications/gaia/gaia/scripts/gaiadisp.sh
@@ -1,4 +1,4 @@
-#!/bin/sh    
+#!/bin/sh
 # The next line is executed by /bin/sh, but not Tcl \
 exec $GAIA_DIR/gaia_stcl $0 ${1+"$@"}
 #+
@@ -6,15 +6,19 @@ exec $GAIA_DIR/gaia_stcl $0 ${1+"$@"}
 #      gaiadisp
 #
 #   Purpose:
-#      Displays an image in a GAIA tool.
+#      Display images in a GAIA tool.
 #
 #   Usage:
-#      gaiadisp image [clone_number]
+#         gaiadisp image
+#      or
+#         gaiadisp image clone_number
+#      or
+#         gaiadisp image1 image2 image3 ...
 #
 #   Description:
-#      This command displays a given image in GAIA. The image can 
-#      be directed into a specified "clone". If the clone or GAIA 
-#      do not exist then they are created. 
+#      This command displays images in GAIA. It has two basic modes
+#      display a single image into a specified "clone", or display
+#      a list of images into a series of automatically created clones.
 #
 #      Clones are specified by an integer number less than 1000.
 #      The special number -1 indicates that a number should be generated.
@@ -22,51 +26,51 @@ exec $GAIA_DIR/gaia_stcl $0 ${1+"$@"}
 #   Notes:
 #
 #   Authors:
-#      Peter W. Draper (PDRAPER):
+#      Peter W. Draper (PWD):
 #
 #   History:
-#      25-NOV-1996 (PDRAPER):
+#      25-NOV-1996 (PWD):
 #         Original version.
-#      09-MAR-1998 (PDRAPER):
+#      09-MAR-1998 (PWD):
 #         Now uses the remote control interface, rather than send
 #         mechanism (less X security complaints).
-#      18-MAY-1999 (PDRAPER):
+#      18-MAY-1999 (PWD):
 #         Converted to use GAIA single binary gaia_stcl.
+#      15-APR-2004 (PWD):
+#         Merged with the gaiadispmany script that was never released.
 #-
 #.
 
 #  Check the command-line arguments.
-set clone ""
-if { $argc >= 1 } { 
-   set image [lindex $argv 0]
-   if { $argc >= 2 } {
-      set clone [lindex $argv 1]
+set clone -1
+if { $argc == 1 } {
+   set clone ""
+} elseif { $argc == 2 } {
+   set clone [lindex $argv 1]
+   if { [catch { expr $clone+1 }] } {
+      set clone -1
+   } else {
+      set argv [lindex $argv 0]
    }
+} elseif { $argc > 2 } {
+   set clone -1
 } else {
    puts stderr {Usage: gaiadisp filename [clone_number]}
+   puts stderr {   or: gaiadisp filename1 filename2 filename3 ...}
    exit
 }
 
 #  Add GAIA_DIR to autopath for some GAIA classes.
 lappend auto_path $env(GAIA_DIR)
 
-#  Now parse name.
-set namer [::gaia::GaiaImageName .namer -imagename $image]
-if { ! [$namer exists] } { 
-   puts stderr "Cannot read image: $image"
-   exit 1
-}
-
-#  Make it absolute (also stripping off tmp_mnt, if present).
-$namer absolute
-
-#  If it has a FITS extension then we need to protect the [].
-$namer protect
+#  Useful to know when instance is created.
+set created_instance 0
 
 #  Open a socket to a GAIA application and return the file descriptor
 #  for remote commands. If a GAIA isn't found then start one up.
 proc connect_to_gaia {} {
    global env
+   global created_instance
 
    #  Get the hostname and port info from the file ~/.rtd-remote,
    #  which is created by rtdimage when the remote subcommand is
@@ -97,6 +101,7 @@ proc connect_to_gaia {} {
       #  loop already, then start a new GAIA.
       if { $needed && $tries == 0 } {
          puts stderr "Failed to connect to GAIA, starting new instance..."
+         set created_instance 1
          exec $env(GAIA_DIR)/gaia.sh &
          #exec $env(GAIA_DIR)/tgaia &
       }
@@ -136,25 +141,48 @@ set gaia_fd [connect_to_gaia]
 set cmd "skycat::SkyCat::get_skycat_images"
 set images [send_to_gaia remotetcl $cmd]
 
-#  Got list so select first and ask about the parent (should be 
+#  Got list so select first and ask about the parent (should be
 #  top-level GAIA).
 set ctrlwidget [lindex $images 0]
 set cmd "winfo parent $ctrlwidget"
 set gaia [send_to_gaia remotetcl $cmd]
 
-#  Construct the command needed to display the image.
-if { $clone != "" } { 
-   set cmd "$gaia noblock_clone $clone [$namer fullname]"
-} else {
-   set cmd "$gaia open [$namer fullname]"
-}
- 
-#  And send the command.
-set ret [send_to_gaia remotetcl $cmd]
-if { $ret == "" || $ret == ".gaia$clone" } { 
-   puts stderr "Displayed image: $image."
-} else {
-   puts stderr "Failed to display image: ($ret)"
+#  Create a name parser.
+set namer [::gaia::GaiaImageName .namer]
+
+#  Loop over all images.
+foreach image $argv {
+
+   # Parse name.
+   $namer configure -imagename $image
+   if { ! [$namer exists] } {
+      puts stderr "Cannot read image: $image"
+      continue
+   }
+
+   #  Make it absolute (also stripping off tmp_mnt, if present).
+   $namer absolute
+
+   #  If it has a FITS extension then we need to protect the [].
+   $namer protect
+
+   #  Construct the command needed to display the image. Note
+   #  complication when we create GAIA, need to display into the first
+   #  clone.
+   if { $created_instance } {
+      set cmd "$gaia open [$namer fullname]"
+      set created_instance 0
+   } else {
+      set cmd "$gaia noblock_clone $clone [$namer fullname]"
+   }
+
+   #  And send the command.
+   set ret [send_to_gaia remotetcl $cmd]
+   if { $ret == "" || [string match ".gaia*" $ret] } {
+      puts stderr "Displayed image: $image."
+   } else {
+      puts stderr "Failed to display image: ($ret)"
+   }
 }
 close $gaia_fd
 exit
