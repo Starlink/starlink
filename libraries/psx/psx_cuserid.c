93ba2bc4132e16dd3c8d2fbc03c1c01be744b092
Tim Jenness <t.jenness@jach.hawaii.edu>
2004-04-23 02:00:18 +0000

    complete rewrite to use configure tests to find best way of getting user name
    svn path=/trunk/; revision=12838

--- libraries/psx/psx_cuserid.c
+++ libraries/psx/psx_cuserid.c
@@ -29,20 +29,18 @@
 *        associated with a user ID, there is no guarantee that the value
 *        returned will be unique.
 *     -  The Unix function cuserid is no longer in the IEEE 1003.1-1990 
-*        standard, so an alternative to this routine should be used.
+*        standard, so an alternative to this routine is used if available.
+*        with getpwuid being preferred over getlogin over cuserid.
+*     -  If the system call can not obtain a user ID, the value of LOGNAME
+*        environment variable is returned as a last resort. And if that is
+*        not there we use $USER. Only relevant currently if getenv() function
+*        exists.
 *     -  If the first attempt to get the username fails, one more attempt
 *        is made. This overcomes an occasional (timing?) problem on Linux.
 
 *  External Routines Used:
 *     cnf: cnfExprt
 
-*  Known Changes Required:
-*     There is a workaround for a bug in the include file stdio.h on
-*     Ultrix. This should be removed when the bug is fixed. The bug
-*     only shows up when using the c89 compiler.
-*     Since cuserid is no longer in the IEEE 1003.1-1990 standard, this
-*     routine will have to be revised in the future.
-      
 *  References:
 *     -  POSIX standard (1988), section 4.2.4
 *     -  POSIX standard (1990), section B.4.2.4
@@ -76,6 +74,10 @@
 *      9-JAN-2002 (AJC):
 *        #define cuserid directly if defined _needs_cuserid
 *        (formerly #included <unistd.h> but that didn't define it).
+*     22-APR-2004 (TIMJ):
+*        Use configure to decide which low level routine to use to obtain
+*        the userid. Prefer getpwuid over getlogin over cuserid.
+*        Use LOGNAME and USER env vars if we can not get a value.
 *     {enter_further_changes_here}
 
 *  Bugs:
@@ -89,19 +91,34 @@
 /* Global Constants:							    */
 
 #include <config.h>
+#include <stdlib.h>
 #include <stdio.h>		 /* Standard I/O routines		    */
 #include "f77.h"		 /* C - Fortran interface		    */
 #include "sae_par.h"		 /* ADAM constants			    */
 
 /* Prefer getpwuid */
 
-#if HAVE_CUSERID
-# if !HAVE_DECL_CUSERID
-/* cuserid is no longer in 1003.1-1990	                                    */
+#if HAVE_GETPWUID && HAVE_GETEUID
+#   define USE_GETPWUID
+#   include <pwd.h>
+#   include <unistd.h>
+#   include <sys/types.h>
+#else
+#  if HAVE_GETLOGIN
+#     define USE_GETLOGIN
+#     include <unistd.h>
+#  else
+#     if HAVE_CUSERID
+#        define USE_CUSERID
+#        if !HAVE_DECL_CUSERID
+/* cuserid is no longer in 1003.1-1990 */
 extern char *cuserid( const char *); 
-# endif
+#        endif
+#     else
+#        error "No supported cuserid equivalent on this system"
+#     endif
+#  endif
 #endif
-
 
 F77_SUBROUTINE(psx_cuserid)( CHARACTER(user), INTEGER(status) TRAIL(user) )
 {
@@ -113,8 +130,15 @@ F77_SUBROUTINE(psx_cuserid)( CHARACTER(user), INTEGER(status) TRAIL(user) )
 
 /* Local Variables:							    */
 
+#ifdef USE_CUSERID
    char tempuser[L_cuserid];	 /* Array to hold username		    */
+#else
+   char tempuser[2];             /* Array to hold defaulted string */
+#endif
    char *p_tempuser;		 /* Return value of calling cuserid	    */
+#ifdef USE_GETPWUID
+   struct passwd * pw;
+#endif
 
 /* Check the inherited global status.					    */
 
@@ -122,16 +146,50 @@ F77_SUBROUTINE(psx_cuserid)( CHARACTER(user), INTEGER(status) TRAIL(user) )
 
 /* Get the username.							    */
 
+/* Use getpwuid(geteuid) if we have it, else use getlogin, else use cuserid.
+   Try using LOGNAME environment variable as last resort.
+ */
+
+#ifdef USE_GETPWUID
+   pw = getpwuid( geteuid() );
+   p_tempuser = pw->pw_name;
+#endif
+
+#ifdef USE_GETLOGIN
+   p_tempuser = getlogin();
+#endif
+
+#ifdef USE_CUSERID
+/* Try twice with cuserid since on some linux system uses NIS+ there
+   can be a delay which will cause an initial failure */
    p_tempuser = cuserid( tempuser );
    if ( p_tempuser == NULL ) {
       p_tempuser = cuserid( tempuser );
-      if ( p_tempuser == NULL ) {
-         tempuser[0] = '\0';
-         p_tempuser = tempuser;
-      }
+   }
+#endif
+
+#if HAVE_GETENV
+   /* Use LOGNAME then USER env var if we do not have a string */
+   if (p_tempuser == NULL) {
+     /* do not use PSX_GETENV since we only want to use a simple
+	command without lots of temp char arrays */
+     p_tempuser = getenv( "LOGNAME");
+     if (p_tempuser == NULL) {
+       p_tempuser = getenv( "USER" );
+     }
+   }
+#endif
+
+   /* Last gasp protection from NULL pointer */
+   /* If we got a NULL value, just copy in empty string */
+   if (p_tempuser == NULL ) {
+     tempuser[0] = '\0';
+     p_tempuser = tempuser;
    }
 
-/* Export the username to the Fortran string user.			    */
+/* Export the username to the Fortran string user.
+   if we have a value.
+*/
 
    cnfExprt( p_tempuser, user, user_length );
 
