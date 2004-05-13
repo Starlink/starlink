363c3b35b67246cd1754612909e0c252a71d17cf
Mark Taylor <m.b.taylor@bristol.ac.uk>
2004-05-13 17:30:05 +0000

    Added working check target.
    svn path=/trunk/; revision=12928

---
+++ applications/startcl/tester.sh
@@ -0,0 +1,22 @@
+
+#+
+#  Name:
+#     tester.sh
+
+#  Purpose:
+#     Test script for STARTCL package
+
+#  Description:
+#     Short test of STARTCL functionality.  It's not exhaustive, but it 
+#     checks that at atclsh shell runs up satisfactorily and uses the 
+#     ADAM extension to exchange messages.
+
+#  Author:
+#     MBT: Mark Taylor (Starlink)
+
+#  History:
+#     13-MAY-2004 (MBT):
+#        Initial version.
+#-
+
+./atclsh startcl_test2 & ./atclsh startcl_test
