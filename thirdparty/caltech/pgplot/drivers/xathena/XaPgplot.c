923fb4b5face04492eeba0b8194863f8e7f9d2e9
Alasdair Allan <aa@astro.ex.ac.uk>
2004-06-30 16:53:21 +0000

    Updated to compile against Xaw3d rather than Xaw
    svn path=/trunk/; revision=13589

--- thirdparty/caltech/pgplot/drivers/xathena/XaPgplot.c
+++ thirdparty/caltech/pgplot/drivers/xathena/XaPgplot.c
@@ -7,8 +7,8 @@
 #include <X11/StringDefs.h>
 #include <X11/keysym.h>
 #include <X11/IntrinsicP.h>
-#include <X11/Xaw/SimpleP.h>
-#include <X11/Shell.h>
+#include <X11/Xaw3d/SimpleP.h>   /* Modified to use Xaw3d rather than   */
+#include <X11/Shell.h>           /* Xaw -- Alasdair Allan (30-JUN-2004) */
 
 #include <stdio.h>
 #include <stdlib.h>
