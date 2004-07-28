bced3a5922e32b975311b406c1f1d11d26adac55
Tim Jenness <t.jenness@jach.hawaii.edu>
2004-07-28 21:14:20 +0000

    use SHL for help
    svn path=/trunk/; revision=14547

--- applications/specx/src/helpm.c
+++ applications/specx/src/helpm.c
@@ -1,7 +1,8 @@
-void main( argc, argv )
-   int    argc;
-   char **argv;
+#include "shl.h"
+#include <stdlib.h>
+
+int main( int argc, char ** argv )
 {
-   void helpc();
-   (void) helpc( argc, argv );
+   (void)  shl_standalone("SPECX", argc, argv );
+   return EXIT_SUCCESS;
 }
