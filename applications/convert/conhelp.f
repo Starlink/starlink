8412a1b6c0965003e90117c6ea3121c7cff10a3f
Tim Jenness <t.jenness@jach.hawaii.edu>
2004-07-29 08:13:15 +0000

    switch to using SHL library
    svn path=/trunk/; revision=14578

--- applications/convert/conhelp.f
+++ applications/convert/conhelp.f
@@ -68,11 +68,14 @@
 
 *  Authors:
 *     MJC: Malcolm J. Currie (STARLINK)
+*     TIMJ: Tim Jenness (JAC, Hawaii)
 *     {enter_new_authors_here}
 
 *  History:
 *     1993 July 30 (MJC):
 *        Original based on KAPHELP.
+*     2004 July 28 (TIMJ):
+*        Switch to SHL library
 *     {enter_any_changes_here}
 
 *  Bugs:
@@ -85,72 +88,24 @@
 
 *  Global Constants:
       INCLUDE 'SAE_PAR'          ! SSE global definitions
-      INCLUDE 'PAR_ERR'          ! Parameter-system errors
 
 *  Status:
       INTEGER STATUS
 
 *  External References:
-      INTEGER CHR_LEN            ! Length of character strings ignoring
-                                 ! trailing blanks
 
 *  Local Constants:
       CHARACTER * ( 12 ) LIBNAM  ! Name of the CONVERT help library
       PARAMETER ( LIBNAM = 'CONVERT_HELP' )
-      INTEGER MAXLEV             ! Maximum number of help levels
-      PARAMETER ( MAXLEV = 4 )
 
 *  Local Variables:
-      CHARACTER HLPTXT * ( 80 )  ! Composite command
-      CHARACTER * ( 132 ) LIBRAY ! Library name and path
-      CHARACTER * ( 122 ) PATH   ! Library path
-      CHARACTER * ( 19 ) TOPIC( MAXLEV ) ! Name of the topic required
 
-      INTEGER I                  ! Loop counter
-      INTEGER NC                 ! Number of characters in the help
-                                 ! string less trailing blanks
 *.
 
 *  Check the inherited status.
       IF ( STATUS .NE. SAI__OK ) RETURN
 
-*  Translate the environment variable/logical name.
-      CALL PSX_GETENV( LIBNAM, PATH, STATUS )
-      IF ( STATUS .EQ. SAI__OK ) THEN
-
-*  Form the full library name.
-         NC = CHR_LEN( PATH )
-         LIBRAY = PATH( :NC )//'.shl'
-
-*  Get topic and subtopics.
-         CALL PAR_GET0C( 'TOPIC', TOPIC( 1 ), STATUS )
-         CALL PAR_CANCL( 'TOPIC', STATUS )
-
-         CALL PAR_GET0C( 'SUBTOPIC', TOPIC( 2 ), STATUS )
-         CALL PAR_CANCL( 'SUBTOPIC', STATUS )
-
-         CALL PAR_GET0C( 'SUBSUBTOPIC', TOPIC( 3 ), STATUS )
-         CALL PAR_CANCL( 'SUBSUBTOPIC', STATUS )
-
-         CALL PAR_GET0C( 'SUBSUBSUBTOPIC', TOPIC( 4 ), STATUS )
-         CALL PAR_CANCL( 'SUBSUBSUBTOPIC', STATUS )
-
-*  Concatenate the help topics into a single string
-         HLPTXT = TOPIC( 1 )
-         NC = CHR_LEN( TOPIC( 1 ) ) + 1
-         DO I = 2, MAXLEV
-            CALL CHR_APPND( ' '//TOPIC( I ), HLPTXT, NC )
-         END DO
-
-*  Use a null string when something has gone wrong obtaining the
-*  topics and sub-topics.
-         IF ( STATUS .NE. SAI__OK ) THEN
-            CALL ERR_ANNUL( STATUS )
-            HLPTXT = '         '
-         END IF
-
-*  Get help text.
-         CALL CON_GHELP( LIBRAY, HLPTXT, STATUS )
-      END IF
+*  Open the help library application layer
+      CALL SHL_ADAM( LIBNAM, .TRUE., STATUS)
 
       END
