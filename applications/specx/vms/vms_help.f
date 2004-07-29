788b56c2184658c3ace98f7bbd688a6802ea7db7
Tim Jenness <t.jenness@jach.hawaii.edu>
2004-07-29 01:26:59 +0000

    replace private specdre-derived help application with SHL version
    svn path=/trunk/; revision=14563

--- applications/specx/vms/vms_help.f
+++ applications/specx/vms/vms_help.f
@@ -50,11 +50,14 @@
 
 *  Authors:
 *     hme: Horst Meyerdierks (UoE, Starlink)
+*     timj: Tim Jenness (JAC, Hawaii)
 *     {enter_new_authors_here}
 
 *  History:
 *     09 Dec 1993 (hme):
 *        Original version, adapted from Specdre's SPE_HELP.
+*     28 Jul 2004 (timj):
+*        Now uses SHL library (simplifies this a lot)
 *     {enter_further_changes_here}
 
 *  Bugs:
@@ -75,24 +78,12 @@
       INTEGER IFAIL              ! Global status
 
 *  External References:
-      EXTERNAL SPACK             ! Input routine for help system
-      INTEGER SPACK
-      EXTERNAL SPACL             ! Output routine for help system
-      INTEGER SPACL
-      EXTERNAL HLP_NAMETR        ! Library name translation routine
-      INTEGER HLP_HELP           ! Help system retrieval routine
 
 *  Local Variables:
       INTEGER STATUS             ! Global status
-      INTEGER PAGE, WIDTH        ! Terminal size
       CHARACTER * ( 132 ) LIBRA  ! Help library name
-      INTEGER I                  ! Temporary integer
-      INTEGER HSTAT              ! Help status
-      INTEGER FU                 ! I/O unit
-      CHARACTER * ( 64 ) MESSAG  ! An error message
 
 *  Internal References:
-      INTEGER CHR_LEN            ! Used length of a string
 
 *.
 
@@ -103,55 +94,13 @@
       STATUS = SAI__OK
       CALL ERR_MARK
 
-*  Get page length and width.
-*  If spaef finds no information at all, it will return page and
-*  with unchanged, but status will be bad.  That status can be simply
-*  set good again, since no ERR calls are made by spaef.
-*  If spaef finds only partial information (say a non-zero page but
-*  a zero width), we must bring it into the range acceptable.
-*  Finally, if the width is allegedly more than 132, we restrict it to
-*  132 anyway.
-      PAGE  = 24
-      WIDTH = 80
-      CALL SPAEF( PAGE, WIDTH, IFAIL )
-      IF ( IFAIL .NE. 0 ) IFAIL = 0
-      IF ( PAGE  .LE. 0 ) PAGE  = 24
-      IF ( WIDTH .LE. 0 ) WIDTH = 80
-      WIDTH = MIN( WIDTH, 132 )
+*  Call SHL routine to determine help library
+      CALL SHL_TRNVAR( 'SPECX', .TRUE., LIBRA, STATUS )
 
-*  Get library name from environment variable.
-      CALL PSX_GETENV( 'SPECX_HELP', LIBRA, STATUS )
-      IF ( STATUS .NE. SAI__OK ) GO TO 500
-      I = CHR_LEN( LIBRA ) + 1
-      LIBRA(I:) = '.shl'
+*  Call SHL routine for help browser
+      CALL SHL_GETHLP( LIBRA, TOPIC, STATUS )
 
-*  Get a free Fortran unit for the library.
-      CALL FIO_GUNIT( FU, STATUS )
-      IF ( STATUS .NE. SAI__OK ) GO TO 500
-
-*  Store the page length/width and reset the line counter.
-      CALL SPAEG( 3, PAGE, WIDTH, 0, STATUS )
-
-*  Call the help system. This uses a good status of 1.
-      HSTAT = HLP_HELP( SPACL, WIDTH, TOPIC, FU, LIBRA, 1, SPACK,
-     :   HLP_NAMETR )
-      CLOSE( FU )
-
-*  If there was an error detected by the help system, make sure it is
-*  reported. The exception are errors -12 and -13 line output and line
-*  input failure. These are caused by our I/O routines, and a failure is
-*  equivalent to requesting an immediate exit from the application. It
-*  should not constitute an error condition and not cause an error
-*  report.
-      IF ( HSTAT .NE. 1 .AND. HSTAT .NE. -12 .AND. HSTAT .NE. -13 ) THEN
-         CALL HLP_ERRMES( HSTAT, MESSAG )
-         WRITE( *, * ) 'VMS_HELP: ' // MESSAG
-         GO TO 500
-      END IF
-
-*  Tidy up.
- 500  CONTINUE
-      CALL FIO_PUNIT( FU, STATUS )
+*  Tidy up error context
       IF ( STATUS .NE. SAI__OK ) CALL ERR_FLUSH( STATUS )
       CALL ERR_RLSE
 
