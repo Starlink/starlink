8cf1ece834f7b651d78063b36fb7c53936131b9a
Tim Jenness <t.jenness@jach.hawaii.edu>
2004-08-12 06:43:24 +0000

    initial version
    svn path=/trunk/; revision=14893

---
+++ applications/obsolete/ircamdr/io2ro_f/reduce.f
@@ -0,0 +1,336 @@
+*+  REDUCE - Actual conversion routine
+      SUBROUTINE REDUCE( STATUS )
+*    Description:
+*     This calls HDS to convert the data from I+O to RO files
+*     In UNIX the case of the filenames might be a problem.  The
+*     routine starts by assuming a name like:
+*       directory name(from parameter system)/
+*       o(or i or ro)date_obsno
+*       .sdf 
+*     If the file isn't found then it puts everything but the '.sdf'
+*     in upper case and tries again.  If it still isn't found then
+*     the '.sdf' is also put into upper case.  If the data is chopped,
+*     the output RO files are created with a lower case 'a(or b).sdf'
+*     independent of the case of the rest of the name.
+*    Invocation :
+*     CALL REDUCE( STATUS )
+*    Authors :
+*     P. N. Daly (PND@JACH.HAWAII.EDU)
+*    History :
+*     18-May-1994: Original version                                      (PND)
+*     01-Aug-1994: Added error catches, more comments, changed copying from 
+*                  VMS specific IO_COPYFILE to HDS_COPY, filename now passed 
+*                  as parameter, reduced number of times O file opened (SKL) 
+*     23-Sept-1994 Added case sensitivity for UNIX                (SKL@JACH)
+*    endhistory
+*    Type Definitions :
+      IMPLICIT NONE
+*    Global constants : 
+      INCLUDE 'SAE_PAR'              ! Defines SAI__OK etc
+      INCLUDE 'DAT_PAR'              ! Necessary for non-VMS
+      INCLUDE 'CHR_ERR'
+*    Status :
+      INTEGER STATUS                 ! Inherited global ADAM status
+*    Import-Export :
+*    External functions :
+      INTEGER CHR_LEN
+*    Local variables :
+      INTEGER 
+     :  CLEN,                  ! Length of string
+     :  CASE,                  ! Flag for upper/lower case chop filename
+     :  L1, CHR_LEN
+      CHARACTER*80 
+     :  DATE_OBS,                    ! UT date and obs number string for name
+     :  NAME,                        ! Object name
+     :  ANAME,                       ! Object name
+     :  BNAME,                       ! Object name
+     :  INAME,                       ! Integration name
+     :  ONAME,                       ! Observation name
+     :  RNAME                        ! Reduced observation name
+      CHARACTER*(DAT__SZLOC)
+     :  ILOC,                        ! Locator to top-level of I-file
+     :  ILOCA,                       ! Locator to top-level of I-file, A chop
+     :  ILOCB,                       ! Locator to top-level of I-file  B chop
+     :  RLOC,                        ! Locator to top-level of RO-file
+     :  OLOC,                        ! Locator to top-level of O-file
+     :  MRLOC,                       ! Locator to MORE level of RO-file
+     :  MOLOC,                       ! Locator to MORE level of O-file
+     :  MFRLOC,                      ! Locator to MORE.FITS level of RO-file
+     :  MFOLOC                       ! Locator to MORE.FITS level of O-file
+      LOGICAL 
+     :  O_OK,                        ! T observation file exits
+     :  INT_OK,                      ! T if integration exists
+     :  CHOP_MODE                    ! T if chop beams A and B exist
+*-
+
+*   Return if status on entry is not SAI__OK
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*   Get the observation name in three formats
+
+      CALL PAR_GET0C( 'DATE_OBS', DATE_OBS, STATUS )
+
+      CALL RONAME( DATE_OBS, INAME, ONAME, RNAME, STATUS )
+
+      IF (STATUS .EQ. SAI__OK) THEN
+
+        CALL MSG_SETC( 'RNAME', RNAME )
+        CALL MSG_OUT( ' ', 
+     :             'Attempting to create reduced observation ^RNAME', 
+     :                STATUS )
+ 
+        CHOP_MODE = .FALSE.
+        INT_OK    = .FALSE.
+        O_OK    = .FALSE.
+
+*       see if O file exists
+
+	L1 = CHR_LEN(ONAME)
+	type *, 'search name = ', oname(1:l1)//'.sdf'
+        INQUIRE( FILE=ONAME(1:L1)//'.sdf', EXIST=O_OK )
+    
+*     If file isn't found allow for possibility of upper case or
+*     mixed case filenames
+
+        IF ( O_OK .EQ. .FALSE. ) THEN
+
+*          CALL CHR_UCASE( ONAME )
+	  L1 = CHR_LEN(ONAME)
+          INQUIRE( FILE=ONAME(1:L1)//'.sdf', EXIST=O_OK )
+         
+          IF ( O_OK .EQ. .FALSE. ) THEN 
+	    L1 = CHR_LEN(ONAME)
+            INQUIRE( FILE=ONAME(1:L1)//'.SDF', EXIST=O_OK )
+          END IF
+
+        END IF
+
+        IF ( O_OK ) THEN
+
+*       get locators to O file and its FITS array
+
+          CALL HDS_OPEN( ONAME, 'READ', OLOC, STATUS )
+          CALL DAT_FIND( OLOC, 'MORE', MOLOC, STATUS )
+          CALL DAT_FIND( MOLOC, 'FITS', MFOLOC, STATUS )
+
+          IF (STATUS .EQ. SAI__OK) THEN
+
+*         see if I file exits
+
+	    L1 = CHR_LEN(INAME)
+            INQUIRE( FILE=INAME(1:L1)//'.sdf', EXIST=INT_OK )
+
+*         If file isn't found allow for possibility of upper case or
+*         mixed case filenames
+
+            IF ( INT_OK .EQ. .FALSE. ) THEN
+
+*              CALL CHR_UCASE( INAME )
+	      L1 = CHR_LEN(INAME)
+              INQUIRE( FILE=INAME(1:L1)//'.sdf', EXIST=INT_OK )
+
+              IF ( INT_OK .EQ. .FALSE. ) THEN
+	        L1 = CHR_LEN(INAME)
+                INQUIRE( FILE=INAME(1:L1)//'.SDF', EXIST=INT_OK )
+              END IF
+
+            END IF
+
+            IF ( INT_OK ) THEN
+      
+*           copy I file to RO file
+
+              CALL HDS_OPEN( INAME, 'READ', ILOC, STATUS )
+              CALL DAT_NAME( ILOC, NAME, STATUS )
+              CALL HDS_COPY( ILOC, RNAME//'.sdf', NAME, STATUS )   
+              CALL DAT_ANNUL( ILOC, STATUS )       
+
+              IF (STATUS .NE. SAI__OK) THEN
+                CALL ERR_REP('ERR', 
+     :          'Error copying I into RO file', STATUS )
+                CALL DAT_ANNUL( OLOC, STATUS )
+                RETURN
+              END IF
+
+            ELSE
+
+	      L1 = CHR_LEN(INAME)
+              INQUIRE( FILE=INAME(1:L1)//'a.sdf', EXIST=INT_OK )
+
+*            try to allow for upper/lower case varieties
+
+              IF ( INT_OK ) THEN
+                CASE = 1
+              ELSE 
+	        L1 = CHR_LEN(INAME)
+                INQUIRE( FILE=INAME(1:L1)//'A.sdf', EXIST=INT_OK )
+                IF ( INT_OK ) THEN
+                  CASE = 2 
+                ELSE
+	          L1 = CHR_LEN(INAME)
+                  INQUIRE( FILE=INAME(1:L1)//'A.SDF', EXIST=INT_OK )
+                  IF ( INT_OK ) THEN
+                    CASE = 3
+                  END IF
+                END IF
+              END IF
+     
+              IF ( INT_OK ) THEN
+
+                CHOP_MODE = .TRUE.
+
+*               copy I file to RO file for both  A and B beams
+
+                IF ( CASE .EQ. 1 ) THEN
+                  CALL HDS_OPEN( INAME//'a.sdf', 'READ', ILOCA, 
+     :                           STATUS )
+                  CALL HDS_OPEN( INAME//'b.sdf', 'READ', ILOCB,
+     :                           STATUS ) 
+                ELSE IF ( CASE .EQ. 2 ) THEN
+                  CALL HDS_OPEN( INAME//'A.sdf', 'READ', ILOCA, 
+     :                           STATUS )
+                  CALL HDS_OPEN( INAME//'B.sdf', 'READ', ILOCB,
+     :                           STATUS )
+                ELSE IF ( CASE .EQ. 3 ) THEN
+                  CALL HDS_OPEN( INAME//'A.SDF', 'READ', ILOCA,
+     :                           STATUS )
+                  CALL HDS_OPEN( INAME//'B.SDF', 'READ', ILOCB,
+     :                           STATUS )
+                END IF
+              
+                CALL DAT_NAME( ILOCA, ANAME, STATUS )
+                CALL DAT_NAME( ILOCB, BNAME, STATUS )
+
+*              The RO files will be given a lower case 'a' and 'b' '.sdf'
+                
+                CALL HDS_COPY( ILOCA, RNAME//'a.sdf', ANAME, STATUS )
+                CALL HDS_COPY( ILOCB, RNAME//'b.sdf', BNAME, STATUS )
+                          
+                CALL DAT_ANNUL( ILOCA, STATUS )       
+                CALL DAT_ANNUL( ILOCB, STATUS )       
+
+                IF (STATUS .NE. SAI__OK) THEN
+                  CALL ERR_REP('ERR', 
+     :            'Error copying I into RO files', STATUS )
+                  CALL DAT_ANNUL( OLOC, STATUS )
+                  RETURN
+                END IF
+
+              ELSE
+                CALL MSG_SETC( 'INAME', INAME )
+                CALL MSG_OUT('ERR', 
+     :                       'Unable to find integration file ^INAME',
+     :                        STATUS )
+                CALL DAT_ANNUL( OLOC, STATUS )
+                RETURN
+              END IF
+            END IF
+          ELSE
+            CALL MSG_SETC( 'ONAME', ONAME )
+            CALL ERR_REP('ERR', 
+     :          'Error getting FITS info from observation file ^ONAME',
+     :                    STATUS )
+            CALL DAT_ANNUL( OLOC, STATUS )
+            RETURN
+          END IF
+        ELSE
+          CALL MSG_SETC( 'ONAME', ONAME )
+          CALL MSG_OUT('ERR', 'Unable to find observation file ^ONAME',
+     :                  STATUS )
+          RETURN
+        END IF    
+      ELSE
+        CALL ERR_REP('ERR', 'Error getting file names', STATUS )
+        RETURN
+      END IF
+
+
+*   Complete conversion of RO file by moving the I file FITS array to
+*   an INT_FITS extension, and copy O file FITS to the RO FITS extension
+
+*   If in chop mode, deal with both beams else straight conversion
+
+      IF ( CHOP_MODE ) THEN
+
+*     Do beam A 
+
+        CALL HDS_OPEN( RNAME//'a', 'UPDATE', RLOC, STATUS )
+        CALL DAT_FIND( RLOC, 'MORE', MRLOC, STATUS )
+        CALL DAT_FIND( MRLOC, 'FITS', MFRLOC, STATUS )
+        CALL DAT_MOVE( MFRLOC, MRLOC, 'INT_FITS', STATUS )
+        CALL DAT_COPY( MFOLOC, MRLOC, 'FITS', STATUS )
+
+        CALL DAT_ANNUL( RLOC, STATUS )
+
+        IF ( STATUS .NE. SAI__OK ) THEN
+          CALL ERR_ANNUL( STATUS )
+          CLEN = CHR_LEN( ONAME )
+          CALL MSG_SETC( 'ONAME', ONAME(1:CLEN) )
+          CLEN = CHR_LEN( RNAME )
+          CALL MSG_SETC( 'RNAME', RNAME(1:CLEN)//'a' )
+          CALL MSG_OUT( ' ',  
+     :       '*** FAILED to copy FITS info from ^ONAME to ^RNAME', 
+     :                  STATUS )
+        ELSE
+          CLEN = CHR_LEN( INAME )
+          CALL MSG_SETC( 'INAME', INAME(1:CLEN)//'a' )
+          CLEN = CHR_LEN( RNAME )
+          CALL MSG_SETC( 'RNAME', RNAME(1:CLEN)//'a' )
+          CALL MSG_OUT( ' ', 'Converted ^INAME to ^RNAME OK', STATUS )
+        END IF
+
+*     Do beam B
+
+        CALL HDS_OPEN( RNAME//'b', 'UPDATE', RLOC, STATUS )
+        CALL DAT_FIND( RLOC, 'MORE', MRLOC, STATUS )
+        CALL DAT_FIND( MRLOC, 'FITS', MFRLOC, STATUS )
+        CALL DAT_MOVE( MFRLOC, MRLOC, 'INT_FITS', STATUS )
+        CALL DAT_COPY( MFOLOC, MRLOC, 'FITS', STATUS )
+
+        CALL DAT_ANNUL( RLOC, STATUS )
+        CALL DAT_ANNUL( OLOC, STATUS )
+
+        IF ( STATUS .NE. SAI__OK ) THEN
+          CALL ERR_ANNUL( STATUS )
+          CLEN = CHR_LEN( ONAME )
+          CALL MSG_SETC( 'ONAME', ONAME(1:CLEN) )
+          CLEN = CHR_LEN( RNAME )
+          CALL MSG_SETC( 'RNAME', RNAME(1:CLEN)//'b' )
+          CALL MSG_OUT( ' ', 
+     :      '*** FAILED to copy FITS info from ^ONAME to ^RNAME', 
+     :      STATUS )
+        ELSE
+          CLEN = CHR_LEN( INAME )
+          CALL MSG_SETC( 'INAME', INAME(1:CLEN)//'b' )
+          CLEN = CHR_LEN( RNAME )
+          CALL MSG_SETC( 'RNAME', RNAME(1:CLEN)//'b' )
+          CALL MSG_OUT( ' ', 'Converted ^INAME to ^RNAME OK', STATUS )
+        END IF
+
+      ELSE
+
+        CALL HDS_OPEN( RNAME, 'UPDATE', RLOC, STATUS )
+        CALL DAT_FIND( RLOC, 'MORE', MRLOC, STATUS )
+        CALL DAT_FIND( MRLOC, 'FITS', MFRLOC, STATUS )
+        CALL DAT_MOVE( MFRLOC, MRLOC, 'INT_FITS', STATUS )
+        CALL DAT_COPY( MFOLOC, MRLOC, 'FITS', STATUS )
+
+        CALL DAT_ANNUL( RLOC, STATUS )
+        CALL DAT_ANNUL( OLOC, STATUS )
+
+        IF ( STATUS .NE. SAI__OK ) THEN
+          CALL ERR_ANNUL( STATUS )
+          CALL MSG_SETC( 'ONAME', ONAME )
+          CALL MSG_SETC( 'RNAME', RNAME )
+          CALL MSG_OUT( ' ', 
+     :      '*** FAILED to copy FITS info from ^ONAME to ^RNAME', 
+     :      STATUS )
+        ELSE
+          CALL MSG_SETC( 'INAME', INAME )
+          CALL MSG_SETC( 'RNAME', RNAME )
+          CALL MSG_OUT( ' ', 'Converted ^INAME to ^RNAME OK', STATUS )
+        END IF
+      END IF
+
+*   Exit this subroutine
+      END
---
+++ applications/obsolete/ircamdr/io2ro_f/roname.f
@@ -0,0 +1,150 @@
+*+  RONAME  - Converts filename to various forms
+      SUBROUTINE RONAME( DATE_OBS, INAME, ONAME, RNAME, STATUS )
+*    Description :
+*     Converts filename to various forms
+*    Invocation :
+*     CALL ACVT_CNAME( DATE_OBS, INAME, ONAME, RNAME, STATUS )
+*    Authors :
+*     P. N. Daly (PND@JACH.HAWAII.EDU)
+*     
+*    History :
+*     18-May-1994: Original version                                      (PND)
+*     02-Aug-1994: Changed input string and manipulation slightly (SKL@JACH)
+*     23-Sept-1994 Changed slightly for UNIX (']',':' -> '/') (SKL@JACH)
+*    endhistory
+*    Type Definitions :
+      IMPLICIT NONE
+*    Global constants :
+      INCLUDE 'SAE_PAR'             ! Defines SAI__OK etc
+      INCLUDE 'CHR_ERR'
+*    Status :
+      INTEGER STATUS                ! Inherited global ADAM status
+*    External references :
+      INTEGER CHR_LEN               ! String length finding function
+*    Import-Export :
+      CHARACTER*(*) DATE_OBS        ! The input filename
+      CHARACTER*(*) INAME           ! The output integration filename
+      CHARACTER*(*) ONAME           ! The output observation filename
+      CHARACTER*(*) RNAME           ! The output reduced observation filename
+*    Local Constants :
+*    Local variables :
+      CHARACTER*80 IDIR            ! I image directory name 
+      CHARACTER*80 ODIR            ! O image directory name 
+      CHARACTER*80 RODIR           ! RO image directory name 
+      INTEGER DASH                 ! location of '_' in DATE_OBS string
+      INTEGER LEN                  ! Length of string
+      INTEGER LENI                 ! Length of dir string 
+      INTEGER LENO                 ! Length of dir string 
+      INTEGER LENR                 ! Length of dir string 
+*-
+
+*   Return if status on entry is not SAI__OK
+
+      IF ( STATUS .NE. SAI__OK ) RETURN
+
+*   Initialise strings
+
+      INAME = ' '
+      ONAME = ' '
+      RNAME = ' '
+      LEN = 0
+      LENI = 0
+      LENO = 0      
+      LENR = 0
+
+
+*   Remove leading blanks and get length of DATE_OBS string
+
+      CALL CHR_LDBLK( DATE_OBS )
+      LEN  = CHR_LEN( DATE_OBS )
+
+*   Check that string is likely to be OK by looking for the presence
+*   of '_' 
+
+      DASH = INDEX( DATE_OBS, '_' )
+
+D     write (6,*) 'RONAME:date_obs, len, dash: ', date_obs, len, dash
+
+*   Check further that the string is in the required format YYMMDD_OBS
+*   by looking for a reasonable decade value 
+*   (from the 80's to the start of the next century!)
+
+      IF ( DASH .LE. 0 .OR. 
+     :     ( DATE_OBS(1:1) .NE. '8' .AND.
+     :     DATE_OBS(1:1) .NE. '9' .AND.
+     :     DATE_OBS(1:1) .NE. '0'     )  ) THEN
+
+        CALL MSG_SETC('DATE_OBS', DATE_OBS )
+        CALL MSG_OUT('ERR', 
+     :    'Error, format for observation date should be YYMMDD_OBS', 
+     :               STATUS )
+        CALL MSG_OUT('ERR', 'Character string read as ^DATE_OBS',
+     :               STATUS )
+        STATUS = SAI__ERROR
+        RETURN
+
+      ELSE
+
+*     get names of directories and do some checking -
+*     if the directory name doesn't end in a '/' add one 
+
+        CALL PAR_GET0C( 'IDIR', IDIR, STATUS )
+        CALL CHR_LDBLK( IDIR )
+        LENI  = CHR_LEN( IDIR )
+        IF (LENI .EQ. 0) THEN
+          CALL MSG_OUT('ERR', 'Error, IDIR string has 0 length', 
+     :                  STATUS )
+          STATUS = SAI__ERROR
+          RETURN
+        END IF
+        IF ( IDIR(LENI:LENI) .NE. '/'  ) 
+     :   THEN
+          LENI = LENI + 1
+          IDIR(LENI:LENI) = '/'
+        END IF
+
+        CALL PAR_GET0C( 'ODIR', ODIR, STATUS )
+        CALL CHR_LDBLK( ODIR )
+        LENO  = CHR_LEN( ODIR )
+        IF (LENO .EQ. 0 ) THEN
+          CALL MSG_OUT('ERR', 'Error, ODIR string has 0 length', 
+     :                  STATUS )
+          STATUS = SAI__ERROR
+          RETURN
+        END IF
+        IF ( ODIR(LENO:LENO) .NE. '/'  ) 
+     :   THEN
+          LENO = LENO + 1
+          ODIR(LENO:LENO) = '/'
+        END IF
+
+        CALL PAR_GET0C( 'RODIR', RODIR, STATUS )
+        CALL CHR_LDBLK( RODIR )
+        LENR  = CHR_LEN( RODIR )
+        IF (LENR .EQ. 0) THEN
+          CALL MSG_OUT('ERR', 'Error, RODIR string has 0 length', 
+     :                  STATUS )
+          STATUS = SAI__ERROR
+          RETURN
+        END IF
+        IF ( RODIR(LENR:LENR) .NE. '/' ) 
+     :   THEN
+          LENR = LENR + 1
+          RODIR(LENR:LENR) = '/'
+        END IF
+
+D       write (6,*) 'IDIR, ODIR, RODIR', idir, odir, rodir
+
+
+*     create filenames 
+
+        INAME = IDIR(1:LENI) // 'i' // DATE_OBS(1:LEN) // '_1'
+        ONAME = ODIR(1:LENO) // 'o' // DATE_OBS(1:LEN)
+        RNAME = RODIR(1:LENR) // 'ro' // DATE_OBS(1:LEN)
+
+D       write (6,*) 'INAME, ONAME, RNAME', iname, oname, rname
+
+      END IF
+
+*   Exit this subroutine
+      END
