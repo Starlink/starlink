5971b514402cbcd12cdbe5e2d90e65ad012f5484
Tim Jenness <t.jenness@jach.hawaii.edu>
2004-10-01 21:00:02 +0000

    Use configure to determine when we can use atexit
    svn path=/trunk/; revision=15675

--- applications/pongo/subs/pon_atexit.c
+++ applications/pongo/subs/pon_atexit.c
@@ -1,3 +1,7 @@
+#if HAVE_CONFIG_H
+# include <config.h>
+#endif
+
 #include <stdlib.h>
 #include <stdio.h>
 #include "f77.h"
@@ -14,6 +18,10 @@
  *     This file may need updating on systems that do not support
  *     atexit().
 
+ *  History:
+ *    1-OCT-2004 (TIMJ):
+ *       Use configure to determine when we can use atexit
+
  */
 
 extern F77_SUBROUTINE(pon_exit)();
@@ -25,9 +33,13 @@ void ponExit()
 
 F77_SUBROUTINE(pon_atexit)( INTEGER( status ) )
 {
-#if defined( sun4 )
-  (void) on_exit( &ponExit );
-#else
+#if HAVE_ATEXIT
   (void) atexit( &ponExit );
+#else
+#  if HAVE_ON_EXIT
+  (void) on_exit( &ponExit );
+#  else
+#     error Unable to register exit handler
+#  endif
 #endif
 }
