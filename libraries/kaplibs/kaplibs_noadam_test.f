b69469ae5e29d1101f7a79a2544aba99ccaddc2c
Tim Jenness <t.jenness@jach.hawaii.edu>
2004-10-04 00:55:35 +0000

    first attempt at non-ADAM linking. Now creates _adam variants of libraries
    svn path=/trunk/; revision=15715

---
+++ libraries/kaplibs/kaplibs_noadam_test.f
@@ -0,0 +1,21 @@
+      PROGRAM KAPLIBS_NOADAM
+
+*   Exists mainly to test linking of non-ADAM kaplibs
+      INTEGER DIMS( 1 )
+      INTEGER PNTR
+      INTEGER WKLOC
+      INTEGER STATUS
+      STATUS = 0
+
+      CALL ERR_BEGIN( STATUS )
+      CALL KPG1_PSEED( STATUS )
+
+      DIMS(1) = 1
+      CALL AIF_GETVM('_INTEGER',1,DIMS,PNTR,WKLOC, STATUS)
+      CALL AIF_ANTMP( WKLOC, STATUS )
+
+      CALL IRA_INIT( STATUS )
+
+      CALL ERR_ANNUL( STATUS )
+
+      END
