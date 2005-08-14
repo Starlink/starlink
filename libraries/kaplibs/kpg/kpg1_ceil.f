2cecf42f4de4c983755217049db4c9cbf3b8b686
Tim Jenness <t.jenness@jach.hawaii.edu>
2005-08-14 08:40:08 +0000

    use the NUM_ macros for type conversion rather than relying on implicit compiler conversion of (possible) BOZ constants which may in fact be typeless
    svn path=/trunk/; revision=17797

--- libraries/kaplibs/kpg/kpg1_ceil.f
+++ libraries/kaplibs/kpg/kpg1_ceil.f
@@ -26,11 +26,18 @@
 
 *  Authors:
 *     DSB: David S. Berry (STARLINK)
+*     TIMJ: Tim Jenness (JAC, Hawaii)
 *     {enter_new_authors_here}
 
 *  History:
 *     20-AUG-1998 (DSB):
 *        Original version.
+*     13-AUG-2005 (TIMJ):
+*        On some compilers comparing a REAL value with an INTEGER
+*        derived from a BOZ (HEX) definition, may result in the
+*        bit pattern being treated as a REAL rather than the INTEGER
+*        value being converted to REAL. To protect against PRM_PAR changing
+*        its notation, use the NUM_ macros which force a type conversion.
 *     {enter_changes_here}
 
 *  Bugs:
@@ -42,22 +49,28 @@
       IMPLICIT NONE              ! No implicit typing
 
 *  Global Constants:
-      INCLUDE 'SAE_PAR'          ! Standard SAE constants
       INCLUDE 'PRM_PAR'          ! VAL__ constants
 
 *  Arguments Given:
       REAL VALUE
 
+*  Internal References:
+      INCLUDE 'NUM_DEC_CVT'    ! Declarations of conversion routines
+      INCLUDE 'NUM_DEF_CVT'    ! Definitions of conversion routines
+
 *  Do it.
-      IF( VALUE .GT. VAL__MAXI ) THEN
+
+      IF( VALUE .GT. NUM_ITOR( VAL__MAXI ) ) THEN
          KPG1_CEIL = VAL__MAXI
 
-      ELSE IF( VALUE .LT. VAL__MINI ) THEN
+      ELSE IF( VALUE .LT. NUM_ITOR( VAL__MINI ) ) THEN
          KPG1_CEIL = VAL__MINI
 
       ELSE
-         KPG1_CEIL = INT( VALUE )
-         IF( REAL( KPG1_CEIL ) .LT. VALUE ) KPG1_CEIL = KPG1_CEIL + 1.0
+         KPG1_CEIL = NUM_RTOI( VALUE )
+         IF( NUM_ITOR( KPG1_CEIL ) .LT. VALUE ) THEN
+            KPG1_CEIL = KPG1_CEIL + 1
+         END IF
       END IF
 
       END
--- libraries/kaplibs/kpg/kpg1_floor.f
+++ libraries/kaplibs/kpg/kpg1_floor.f
@@ -26,11 +26,18 @@
 
 *  Authors:
 *     DSB: David S. Berry (STARLINK)
+*     TIMJ: Tim Jenness (JAC, Hawaii)
 *     {enter_new_authors_here}
 
 *  History:
 *     20-AUG-1998 (DSB):
 *        Original version.
+*     13-AUG-2005 (TIMJ):
+*        On some compilers comparing a REAL value with an INTEGER
+*        derived from a BOZ (HEX) definition, may result in the
+*        bit pattern being treated as a REAL rather than the INTEGER
+*        value being converted to REAL. To protect against PRM_PAR changing
+*        its notation, use the NUM_ macros which force a type conversion.
 *     {enter_changes_here}
 
 *  Bugs:
@@ -42,23 +49,28 @@
       IMPLICIT NONE              ! No implicit typing
 
 *  Global Constants:
-      INCLUDE 'SAE_PAR'          ! Standard SAE constants
       INCLUDE 'PRM_PAR'          ! VAL__ constants
 
 *  Arguments Given:
       REAL VALUE
 
+*  Internal References:
+      INCLUDE 'NUM_DEC_CVT'    ! Declarations of conversion routines
+      INCLUDE 'NUM_DEF_CVT'    ! Definitions of conversion routines
+
 *  Do it.
-      IF( VALUE .GT. VAL__MAXI ) THEN
+
+      IF( VALUE .GT. NUM_ITOR( VAL__MAXI ) ) THEN
          KPG1_FLOOR = VAL__MAXI
 
-      ELSE IF( VALUE .LT. VAL__MINI ) THEN
+      ELSE IF( VALUE .LT. NUM_ITOR( VAL__MINI ) ) THEN
          KPG1_FLOOR = VAL__MINI
 
       ELSE
-         KPG1_FLOOR = INT( VALUE )
-         IF( REAL( KPG1_FLOOR ) .GT. VALUE ) KPG1_FLOOR = KPG1_FLOOR 
-     :                                                    - 1.0
+         KPG1_FLOOR = NUM_RTOI( VALUE )
+         IF( NUM_ITOR( KPG1_FLOOR ) .GT. VALUE ) THEN
+            KPG1_FLOOR = KPG1_FLOOR - 1
+         END IF
       END IF
 
       END
