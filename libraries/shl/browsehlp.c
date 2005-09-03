529e512fa6450af664bc390f4b5987570298db05
Tim Jenness <t.jenness@jach.hawaii.edu>
2005-09-03 01:48:21 +0000

    C programs now initialise Fortran runtime and define a dummy fortran main if necessary
    svn path=/trunk/; revision=18151

--- libraries/shl/browsehlp.c
+++ libraries/shl/browsehlp.c
@@ -2,15 +2,21 @@
 # include <config.h>
 #endif
 
-#ifdef FC_MAIN
+/* If a Fortran main is defined, provide a dummy entry point to
+   satisfy potential linker problems */
+#if HAVE_FC_MAIN
 void FC_MAIN () {}
 #endif
 
 #include <stdlib.h>
 #include "shl.h"
+#include "cnf.h"
 
 int main( int argc, char **argv )
 {
+  /* Make sure fortran is ready for us */
+  cnfInitRTL( argc, argv );
+
   /* Really need to prompt for a library if -l has not been specified */
   return shl_standalone( NULL, 0, argc, argv );
 }
--- libraries/shl/ctesthelp.c
+++ libraries/shl/ctesthelp.c
@@ -4,16 +4,21 @@
 # include <config.h>
 #endif
 
-#ifdef FC_MAIN
+/* If a Fortran main is defined, provide a dummy entry point to
+   satisfy potential linker problems */
+#if HAVE_FC_MAIN
 void FC_MAIN () {}
 #endif
 
-
 #include <stdlib.h>
 #include "shl.h"
+#include "cnf.h"
 
 int main( int argc, char **argv )
 {
+
+  /* Make sure Fortran is ready for us */
+  cnfInitRTL( argc, argv );
 
   /* Really need to prompt for a library if -l has not been specified */
   return shl_standalone( "./demo", 0, argc, argv );
