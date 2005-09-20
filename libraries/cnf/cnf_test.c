f8d55957dcdce1ce6049b1c511c47dd2a077aaa4
Tim Jenness <t.jenness@jach.hawaii.edu>
2005-09-20 00:35:08 +0000

    add more functions in test
    svn path=/trunk/; revision=18373

--- libraries/cnf/cnf_test.c
+++ libraries/cnf/cnf_test.c
@@ -5,19 +5,39 @@
 *  Also we include both files to check the #ifdef mechanism.
 */
 #include <stdio.h>
+#include <string.h>
 #include <f77.h>                 /* CNF macros and prototypes               */
 #include  <cnf.h>                 /* CNF macros and prototypes               */
 
 int main()
 {
-   char *string;
-   int length;
+   /* include trailing spaces */
+   char input[] = "Twenty six characters long     ";
+   char *string, *string2;
+   int clength,flength;
    void * dummy;
 
-   string = "Twenty six characters long";
-   length = cnf_lenc( string );
+   /* Make sure that we include trailing blanks to properly test cnf_lenc */
+   string = cnfMalloc( strlen( input ) + 1 );
+   cnfImprt( input, strlen( input ), string ); 
+   clength = cnfLenc( input );
+   flength = cnfLenf( input, strlen(input) );
+   printf( "The string \"%s\" is %d [%d] characters long\n", string, clength,
+	   flength);
 
-   printf( "The string \"%s\" is %d characters long\n", string, length );
+
+   /* Check C to fortran to C */
+   string2 = cnfMalloc( strlen( input ) + 1 );
+   cnfExprt( string, string2, strlen(input) );
+   cnfImprt( string2, strlen(input), string );
+   printf("Imprt, Exprt, Imprt: \"%s\"\n", string );
+
+   /* Export/Import to itself */
+   cnfExprt( string, string, strlen(input) );
+   cnfImprt( string, strlen(input), string );
+
+   cnfFree( string );
+   cnfFree( string2 );
 
    /* Make sure we can alloc and free */
    dummy = cnfMalloc( 10 );
