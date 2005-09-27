e360b0106361359fdb603e0d061d0ab9ce1aa657
Andy Gibb <agg@phas.ubc.ca>
2005-09-27 23:46:00 +0000

    First version
    svn path=/trunk/; revision=18441

---
+++ applications/smurf/smurf_typ.h
@@ -0,0 +1,61 @@
+/*
+*+
+*  Name:
+*     smurf_typ.h
+
+*  Purpose:
+*     type definitions for the smurf application
+
+*  Language:
+*     Starlink ANSI C
+
+*  Type of Module:
+*     Header File
+
+*  Invocation:
+*     #include "smurf_typ.h"
+
+*  Description:
+*     Data types used by the smurf functions.
+
+*  Authors:
+*     Andy Gibb (UBC)
+*     {enter_new_authors_here}
+
+*  History:
+*     2005-09-27 (AGG):
+*        Initial test version
+*     {enter_further_changes_here}
+
+*  Copyright:
+*     Copyright (C) 2005 Particle Physics and Astronomy Research Council.
+*     University of British Columbia.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful,but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public
+*     License along with this program; if not, write to the Free
+*     Software Foundation, Inc., 59 Temple Place,Suite 330, Boston,
+*     MA 02111-1307, USA
+
+*  Bugs:
+*     {note_any_bugs_here}
+*-
+*/
+#ifndef SMURF_TYP_DEFINED
+#define SMURF_TYP_DEFINED
+
+#define DIM_T_FMT "lud"
+typedef unsigned long dim_t;
+
+#endif /* SMURF_TYP_DEFINED */
