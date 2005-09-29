4e5bf314464e15cae8a8b9965d209f286b41c35d
David S. Berry <d.berry@jach.hawaii.edu>
2005-09-29 13:42:31 +0000

    Start a minimal C interface for KAPLIBS
    svn path=/trunk/; revision=18465

---
+++ libraries/kaplibs/kpg/kaplibs.h
@@ -0,0 +1,35 @@
+#if !defined( KAPLIBS_INCLUDED )  /* Include this file only once */
+#define KAPLIBS_INCLUDED
+/*
+*  Name:
+*     kaplibs.h
+
+*  Purpose:
+*     Define the C interface to the KAPLIBS library.
+
+*  Description:
+*     This module defines the C interface to the functions of the KAPLIBS
+*     library. The file kaplibs.c contains C wrappers for the Fortran 
+*     KAPLIBS routines.
+
+*  Notes:
+*     - Given the size of the KAPLIBS library, providing a complete C
+*     interface is probably not worth the effort. Instead, I suggest that 
+*     people who want to use KAPLIBS from C extend this file (and
+*     kaplibs.c) to include any functions which they need but which are
+*     not already included.
+
+*  Authors:
+*     DSB: David .S. Berry
+
+*  History:
+*     29-SEP-2005 (DSB):
+*        Original version.
+*/
+
+#include "ast.h"            
+
+void kpg1Asget( int, int, int, int, int, int *, int *, int *, 
+                AstFrameSet **, int * );
+
+#endif
