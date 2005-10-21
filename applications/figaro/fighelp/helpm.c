6b2d79f15489a16ee008e367af1814fb4ddc9e3a
Tim Jenness <t.jenness@jach.hawaii.edu>
2005-10-21 18:50:50 +0000

    make sure we use fortran main and initialise runtime. Also moved location of shl.h
    svn path=/trunk/; revision=18624

--- applications/figaro/fighelp/helpm.c
+++ applications/figaro/fighelp/helpm.c
@@ -1,7 +1,22 @@
-#include "shl.h"
+#include "star/shl.h"
+#include "f77.h"
 #include <stdlib.h>
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+/* If a Fortran main is defined, provide a dummy entry point to
+   satisfy potential linker problems */
+#if HAVE_FC_MAIN
+void FC_MAIN () {}
+#endif
+
 
 int main( int argc, char ** argv )
 {
+  /* Make sure fortran is ready for use */
+  cnfInitRTL( argc, argv );
+
    return shl_standalone("FIG", 1, argc, argv );
 }
