8ae8fb1bf5785e1b9343fcd893a3272e1569de0f
Tim Jenness <t.jenness@jach.hawaii.edu>
2005-10-22 20:10:12 +0000

    synchronize with PTW version (removed CBRD since it was never used + reformatting)
    svn path=/trunk/; revision=18652

--- libraries/sla/cs2c6.f
+++ libraries/sla/cs2c6.f
@@ -1,4 +1,4 @@
-      SUBROUTINE sla_CS2C6 (A, B, R, AD, BD, RD, V)
+      SUBROUTINE sla_CS2C6 ( A, B, R, AD, BD, RD, V )
 *+
 *     - - - - - -
 *      C S 2 C 6
@@ -18,9 +18,9 @@
 *  Returned:
 *     V     r(6)   Cartesian position & velocity vector
 *
-*  P.T.Wallace   Starlink   November 1984
+*  P.T.Wallace   Starlink   11 September 2005
 *
-*  Copyright (C) 1995 Rutherford Appleton Laboratory
+*  Copyright (C) 2005 Rutherford Appleton Laboratory
 *
 *  License:
 *    This program is free software; you can redistribute it and/or modify
@@ -42,32 +42,31 @@
 
       IMPLICIT NONE
 
-      REAL A,B,R,AD,BD,RD,V(6)
+      REAL A, B, R, AD, BD, RD, V(6)
 
-      REAL SA,CA,SB,CB,RCB,X,Y,RBD,CBRD,W
+      REAL SA, CA, SB, CB, RCB, X, Y, RBD, W
 
 
 
-*  Useful functions
-      SA=SIN(A)
-      CA=COS(A)
-      SB=SIN(B)
-      CB=COS(B)
-      RCB=R*CB
-      X=RCB*CA
-      Y=RCB*SA
-      RBD=R*BD
-      CBRD=CB*RD
-      W=RBD*SB-CB*RD
+*  Useful functions.
+      SA = SIN(A)
+      CA = COS(A)
+      SB = SIN(B)
+      CB = COS(B)
+      RCB = R*CB
+      X = RCB*CA
+      Y = RCB*SA
+      RBD = R*BD
+      W = RBD*SB-CB*RD
 
-*  Position
-      V(1)=X
-      V(2)=Y
-      V(3)=R*SB
+*  Position.
+      V(1) = X
+      V(2) = Y
+      V(3) = R*SB
 
-*  Velocity
-      V(4)=-Y*AD-W*CA
-      V(5)=X*AD-W*SA
-      V(6)=RBD*CB+SB*RD
+*  Velocity.
+      V(4) = -Y*AD-W*CA
+      V(5) = X*AD-W*SA
+      V(6) = RBD*CB+SB*RD
 
       END
