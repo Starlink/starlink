e29614a056017406a4b29929d3e21387607de6f0
David S. Berry <d.berry@jach.hawaii.edu>
2005-10-25 18:02:25 +0000

    Added CGEN_HDS_TYPE ("_DOUBLE", "_REAL", etc)
    svn path=/trunk/; revision=18677

--- applications/generic/cgeneric_defs.h
+++ applications/generic/cgeneric_defs.h
@@ -17,6 +17,7 @@
 *     "cgenerics_defs.h". This include file defines the macros:
 * 
 *        CGEN_TYPE
+*        CGEN_HDS_TYPE
 *        CGEN_CODE
 *        CGEN_PRM_TYPE
 *
@@ -25,11 +26,14 @@
 
 *  Authors:
 *     PWD: Peter W. Draper (JAC, Durham University)
+*     DSB: David S. Berry (JAC, UCLan)
 *     {enter_new_authors_here}
 
 *  History:
 *     10-OCT-2005 (PWD):
 *        Original version.
+*     25-OCT-2005 (DSB):
+*        Added CGEN_HDS_TYPE.
 *     {enter_further_changes_here}
 *        
 
@@ -52,6 +56,9 @@
 #ifdef CGEN_PRM_TYPE
 #undef CGEN_PRM_TYPE
 #endif
+#ifdef CGEN_HDS_TYPE
+#undef CGEN_HDS_TYPE
+#endif
 
 /* double */
 #if CGEN_CODE_TYPE == CGEN_DOUBLE_TYPE
@@ -59,6 +66,7 @@
 #define CGEN_TYPE double
 #define CGEN_CODE D
 #define CGEN_PRM_TYPE D
+#define CGEN_HDS_TYPE "_DOUBLE"
 
 /* float */
 #elif CGEN_CODE_TYPE == CGEN_FLOAT_TYPE
@@ -66,6 +74,7 @@
 #define CGEN_TYPE float
 #define CGEN_CODE F
 #define CGEN_PRM_TYPE R
+#define CGEN_HDS_TYPE "_REAL"
 
 /* int */
 #elif CGEN_CODE_TYPE == CGEN_INT_TYPE
@@ -73,6 +82,7 @@
 #define CGEN_TYPE int
 #define CGEN_CODE I
 #define CGEN_PRM_TYPE I
+#define CGEN_HDS_TYPE "_INTEGER"
 
 /* short int */
 #elif CGEN_CODE_TYPE == CGEN_WORD_TYPE
@@ -80,6 +90,7 @@
 #define CGEN_TYPE short int
 #define CGEN_CODE W
 #define CGEN_PRM_TYPE W
+#define CGEN_HDS_TYPE "_WORD"
 
 /* unsigned short int */
 #elif CGEN_CODE_TYPE == CGEN_UWORD_TYPE
@@ -87,6 +98,7 @@
 #define CGEN_TYPE unsigned short int
 #define CGEN_CODE UW
 #define CGEN_PRM_TYPE UW
+#define CGEN_HDS_TYPE "_UWORD"
 
 /* signed char */
 #elif CGEN_CODE_TYPE == CGEN_BYTE_TYPE
@@ -94,6 +106,7 @@
 #define CGEN_TYPE char
 #define CGEN_CODE B
 #define CGEN_PRM_TYPE B
+#define CGEN_HDS_TYPE "_BYTE"
 
 /* unsigned char */
 #elif CGEN_CODE_TYPE == CGEN_UBYTE_TYPE
@@ -101,5 +114,6 @@
 #define CGEN_TYPE unsigned char
 #define CGEN_CODE UB
 #define CGEN_PRM_TYPE UB
+#define CGEN_HDS_TYPE "_UBYTE"
 
 #endif
