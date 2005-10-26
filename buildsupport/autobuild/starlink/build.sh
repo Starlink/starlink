70a3ed28bef0266e11abe5e6e22af949fb1a6f56
Starlink Software Librarian <ussc@star.rl.ac.uk>
2005-10-26 16:17:05 +0000

    Updated to change version numbers of tar.gz files for RPM and add support for STAR_SUPPRESS_BUILD.
    svn path=/trunk/; revision=18691

--- buildsupport/autobuild/starlink/build.sh
+++ buildsupport/autobuild/starlink/build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
- 
+
+#HOSTNAME
+hostname -f
+
 NAME="Starlink CVS"
  
 # Exit immediately if command fails
@@ -8,6 +11,9 @@ set -e
 # Print command executed to stdout
 set -v
 # Make env for build.
+
+#Set the DISPLAY env, yes we do need this.
+DISPLAY=EXPORT_DISPLAY
 
 BUILD_SYSTEM=BUILDDIR
 export BUILD_SYSTEM
@@ -41,7 +47,48 @@ make
 make install
 
 # Make a distribution
+
+# Make a distribution
+
+#First test for STAR_SUPPRESS_BUILD
+if test -f STAR_SUPPRESS_BUILD; then
+
+echo "No need to make dist as STAR_SUPPRESS_BUILD is set"
+
+else
+
 make dist
 
-#Move any tar.gz files to the package dir
-mv *.tar.gz $BUILD_SYSTEM/packages/tars
+#Change version on tar.gz file for RPM.
+
+TARGZ_FILE=`ls -1 *.tar.gz`
+export TARGZ_FILE
+
+package=`echo ${TARGZ_FILE} | cut -d "-" -f1`
+version=`echo ${TARGZ_FILE} | cut -d "-" -f2- | sed -e 's#.tar.gz##g'`
+export version
+export package
+
+version2=`echo ${version} | sed -e 's#-#.#g'`
+export version2
+
+if test "${package}-${version}" = "${package}-${version2}"; then
+
+ #Move any tar.gz files to the package dir
+ mv ${package}-${version}.tar.gz $BUILD_SYSTEM/packages/tars
+
+else
+
+ tar -xzf ${package}-${version}.tar.gz
+ mv ${package}-${version} ${package}-${version2}
+
+ rm -f ${package}-${version}.tar.gz
+
+ tar -czf ${package}-${version}.tar.gz ${package}-${version2}
+
+ #Move any tar.gz files to the package dir
+ mv ${package}-${version}.tar.gz $BUILD_SYSTEM/packages/tars
+
+fi
+
+fi
