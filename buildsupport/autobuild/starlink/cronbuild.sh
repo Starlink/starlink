aa09adb091f2600f25ab1000821c23589cfb58e6
Starlink Software Librarian <ussc@star.rl.ac.uk>
2005-10-27 11:46:54 +0000

    Updated to latest version, added Normans java stuff for flatdeps.xml.
    svn path=/trunk/; revision=18707

--- buildsupport/autobuild/starlink/cronbuild.sh
+++ buildsupport/autobuild/starlink/cronbuild.sh
@@ -10,31 +10,62 @@ export PERL5LIB
 #do not rely on the CVSROOT environment variable being picked up
 #and set when this script is run.
 
+#We need java to run Normans stuff.
+JAVA=JDK_HOME/bin/java
+export JAVA
+
 MYCVSROOT=$CVSROOT
+
+BUILD_MYTOP=PREFIX
+export BUILD_MYTOP
 
 #Checkout componentset.xml so that we can create autobuild.conf.
 
-cd PREFIX/starlink
+cd ${BUILD_MYTOP}/starlink
+rm -fr componentset.xml CVS flatdeps.xml
 
 cvs -d $MYCVSROOT co componentset.xml
 
 #Use XSLT script to create autobuild.conf and remove
 #the starting XML line.
 
-xsltproc autobuild.xslt componentset.xml > autobuild.conf
-sed '1,1d' autobuild.conf > autobuild.conf-tmp
-rm -f autobuild.conf
-mv autobuild.conf-tmp autobuild.conf
+#Use Normans java stuff to calculate the flatdeps.xml
+cd ${BUILD_MYTOP}/starlink/javacalc/java
+rm -f componentset.xml
+cp ${BUILD_MYTOP}/starlink/componentset.xml .
+${JAVA} GenerateDependencies --flatdeps=flatdeps.xml componentset.xml
+cp -f flatdeps.xml ../../
+cd ../../
+
+#Now build the main part of the autobuild.conf file.
+xsltproc autobuild.xslt componentset.xml > top.conf
+
+#Use if you want to run the tests.
+##xsltproc test.xsl componentset.xml > test.conf
+##cat top.conf test.conf tail.txt > autobuild.conf
+
+#Use if you do not want to run the tests
+cat top.conf tail.txt > autobuild.conf
+
+#Make a backup of autobuild.conf
+cp autobuild.conf autobuild.conf-`date +%d%m%y`
+chmod 644 autobuild.conf-`date +%d%m%y`
+
 
 #Remove the files from the previous build so we start a fresh
 
-rm -fr PREFIX/build/build-cache/*
-rm -fr PREFIX/build/build-home/*
-rm -fr PREFIX/build/build-root/*
-rm -fr PREFIX/build/install/*
-rm -fr PREFIX/build/temp/*
-rm -fr PREFIX/build/packages/tars/*
+rm -fr ${BUILD_MYTOP}/build/build-cache/*
+rm -fr ${BUILD_MYTOP}/build/build-home/*
+rm -fr ${BUILD_MYTOP}/build/build-root/*
+rm -fr ${BUILD_MYTOP}/build/install/*
+rm -fr ${BUILD_MYTOP}/build/install-comp/*
+rm -fr ${BUILD_MYTOP}/build/temp/*
+rm -fr ${BUILD_MYTOP}/build/packages/tars/*
+rm -fr ${BUILD_MYTOP}/build/public_html/webstart/*
+rm -fr ${BUILD_MYTOP}/build/public_html/webstart15/*
+mkdir ${BUILD_MYTOP}/build/packages/tars/starjava15
 
 #Run the build
 
-PREFIX/bin/auto-build.pl --conf PREFIX/starlink/autobuild.conf
+${BUILD_MYTOP}/bin/auto-build.pl --conf ${BUILD_MYTOP}/starlink/autobuild.conf
+
