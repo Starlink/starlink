7625780d523966d372496051208fad060cab9230
Peter W. Draper <P.W.Draper@durham.ac.uk>
2005-11-07 11:26:53 +0000

    Merged
    svn path=/trunk/; revision=18800

--- applications/gaia/tcl/blt/library/bltDnd.tcl
+++
@@ -1,7 +0,0 @@
-
-proc blt::InitDrag&DropBindings { widget button cmd } {
-    bind $widget <ButtonPress-$button> [list $cmd drag %W %X %Y]
-    bind $widget <B$button-Motion> [list $cmd drag %W %X %Y]
-    bind $widget <ButtonRelease-$button> [list $cmd drop %W %X %Y]
-}
-
