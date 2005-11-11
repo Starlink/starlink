6cd2b4b1fe19a2f687d8c869cc158b84c8a66084
Peter W. Draper <P.W.Draper@durham.ac.uk>
2005-11-11 10:54:58 +0000

    Initial revision
    svn path=/trunk/; revision=18884

---
+++ applications/gaia/tcl/tclx/compat/rename.c
@@ -0,0 +1,20 @@
+/* rename.c -- file renaming routine for systems without rename(2)
+ *
+ * Written by reading the System V Interface Definition, not the code.
+ *
+ * Totally public domain.  (Author unknown)
+ *
+ */
+
+int rename(from,to)
+register char *from, *to;
+{
+    (void) unlink(to);
+    if (link(from, to) < 0)
+	return(-1);
+
+    (void) unlink(from);
+    return(0);
+}
+
+
