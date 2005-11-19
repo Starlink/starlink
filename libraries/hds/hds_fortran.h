412fbda1a50241e943f4114f67671bf06f79b9c8
Tim Jenness <t.jenness@jach.hawaii.edu>
2005-11-19 01:25:10 +0000

    + Fortran Locator in datImportFloc now marked const + datImportFloc now does not convert arg to pointer. Assumes it already is. + datExportFloc casts to HDSLoc** to overcome problems with const HDSLoc*
    svn path=/trunk/; revision=19011

--- libraries/hds/hds_fortran.h
+++ libraries/hds/hds_fortran.h
@@ -43,13 +43,16 @@
 
 /* This function converts an HDSLoc to a Fortran string buffer */
 
+/* The optional freeing of memory is problematic since it stops
+   us declaring clocator as const */
+
 void datExportFloc ( HDSLoc **clocator, int free, int loc_length, 
 		     char flocator[DAT__SZLOC], int * status);
 
 /* Convert a Fortran locator to a C HDSLoc struct. Memory is allocated
    by this routine that will be freed when the locator is annulled. */
 
-void datImportFloc( char flocator[DAT__SZLOC], int loc_length, 
+void datImportFloc( const char flocator[DAT__SZLOC], int loc_length, 
 		    HDSLoc **clocator, int * status);
 
 
@@ -60,14 +63,16 @@ void datImportFloc( char flocator[DAT__SZLOC], int loc_length,
    Note that we do not free the memory assoicated with the locator
    and we assume DAT__SZLOC for the Fortran locator */
 
+/* This really should be able to take a const pointer but currently
+   datExportFloc can either free or not free so it can't be const */
 
 #define HDS_EXPORT_CLOCATOR( cloc, floc, status )	\
-  datExportFloc( &cloc, 0, DAT__SZLOC, floc, status )
+  datExportFloc( (HDSLoc**)&cloc, 0, DAT__SZLOC, floc, status )
 
 /* Convert a Fortran locator to a C locator */
 
 #define HDS_IMPORT_FLOCATOR( floc, cloc, status ) \
-  datImportFloc( floc, DAT__SZLOC, &cloc, status )
+  datImportFloc( floc, DAT__SZLOC, cloc, status )
 
 #endif /* _INCLUDED */
 
