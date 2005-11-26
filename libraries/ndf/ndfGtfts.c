de1d3fa80552f3c9f9f7e723317d87b51c67e0af
Tim Jenness <t.jenness@jach.hawaii.edu>
2005-11-26 03:37:17 +0000

    now use datMapV rather than datCell when reading .FITS
    svn path=/trunk/; revision=19093

--- libraries/ndf/ndfGtfts.c
+++ libraries/ndf/ndfGtfts.c
@@ -11,6 +11,9 @@
 #include "ndf_err.h"
 #include "dat_err.h"
 
+const int SZFITSCARD = 80; /* Size of a FITS header card */
+#define FITSSTR "80"       /* string representation of size of FITS */
+
 /*
 *+
 *  Name:
@@ -76,16 +79,17 @@
 
 int ndfGtfts( int indf, AstFitsChan ** fchan, int * status ) {
 
-  char   card[81];            /* Space for a single FITS card */ 
+  char   *card;               /* Pointer to start of current card */
   HDSLoc *fitsloc = NULL;     /* FITS HDS Locator in extension */
   hdsdim fitsdim[DAT__MXDIM]; /* Dimensionality of FITS extension */
+  char   *fpntr;              /* Pointer to the mapped FITS header */
   int    i;                   /* Loop counter */
   size_t ncards;              /* Number of header cards in extension */
+  size_t nchars;              /* Number of characters in extension */
+  size_t nmapcards;           /* Number of cards mapped */
   int    ndim;                /* Number of dimensions in FITS array */
   int    *oldstat;            /* Current status watched by AST */
-  hdsdim subs[1];             /* subset of array to locate */
   int    there = 0;           /* Is FITS extension there? */
-  HDSLoc *tmploc = NULL;      /* Temporary HDS locator */
   char   type[DAT__SZTYP+1];  /* Data type of the FITS extension */
 
   /* make sure the fits chan is set to NULL on exit with bad status */
@@ -111,10 +115,10 @@ int ndfGtfts( int indf, AstFitsChan ** fchan, int * status ) {
   datType( fitsloc, type, status );
 
   if ( *status == SAI__OK ) {
-    if (strcmp(type, "_CHAR*80") != 0 ) {
+    if (strcmp(type, "_CHAR*" FITSSTR) != 0 ) {
       *status = DAT__TYPIN;
       msgSetc( "TYP", type );
-      errRep( "NDF_GTFTS_TYP", "Data type of FITS extension is '^TYP' not '_CHAR*80'", status );
+      errRep( "NDF_GTFTS_TYP", "Data type of FITS extension is '^TYP' not '_CHAR*" FITSSTR "'", status );
     }
   }
 
@@ -134,23 +138,31 @@ int ndfGtfts( int indf, AstFitsChan ** fchan, int * status ) {
 
   if ( *status == SAI__OK ) {
     if ( ncards != fitsdim[0] ) {
-      *status = DAT__WEIRD;
+      *status = DAT__DIMIN;
       msgSeti( "DM", (int)fitsdim[0] );
       msgSeti( "SZ", (int)ncards );
       errRep( "NDF_GTFTS_SIZ","Bizarre error whereby the first dimension of the FITS extension (^DM) does not equal the size of the extension (^SZ)", status);
     }
   }
 
+  /* Use datMapV to map the entire FITS array, then step through
+     it 80 characters at a time until we have done all the cards.
+     Note that there is no nul-terminator so we can not use
+     astPutCards directly */
+    
+  datMapV( fitsloc, "_CHAR*" FITSSTR, "READ", &fpntr, &nchars, status );
+
+  if ( *status == SAI__OK ) {
+    if ( ncards != nchars ) {
+      *status = DAT__DIMIN;
+      msgSeti( "DM", (int)nchars);
+      msgSeti( "SZ", (int)ncards );
+      errRep( "NDF_GTFTS_SIZ2","Bizarre error whereby the number of elements mapped in the FITS extension (^DM) does not equal the size of the extension (^SZ)", status);
+    }
+  }
+
   /* Do not bother with AST stuff if status is bad */
   if ( *status == SAI__OK ) {
-
-    /* We can now either read the entire chunk into a single buffer
-       (which me must malloc and then step through) or use 
-       datCell to step through the array. 
-       Use datCell since we then get a header card at a time
-       and can stuff it directly into the FitsChan. Swings and
-       roundabouts....since we have to malloc a new locator each time
-    */
 
     /* Associate AST status with our status */
     oldstat = astWatch( status );
@@ -158,13 +170,12 @@ int ndfGtfts( int indf, AstFitsChan ** fchan, int * status ) {
     /* Create a new FitsChan */
     *fchan = astFitsChan( NULL, NULL, "" );
   
-    for (i = 1; i <= ncards; i++ ) {
-      subs[0] = i;
-      /* datCell starts counting at 1 */
-      datCell( fitsloc, 1, subs, &tmploc, status ); 
-      datGet0C( tmploc, card, 81, status );
-      datAnnul( &tmploc, status );
+    /* Extract headers 80 characters at a time. No nul-termination
+       but astPutFits guarantees to only read 80 characters */
+    card = fpntr;
+    for (i = 0; i < ncards; i++ ) {
       astPutFits( *fchan, card, 0 );
+      card += SZFITSCARD;
     }
 
     /* Rewind the FitsChan */
@@ -179,7 +190,10 @@ int ndfGtfts( int indf, AstFitsChan ** fchan, int * status ) {
   }
 
   /* Clean up */
+  datUnmap( fitsloc, status );
   datAnnul( &fitsloc, status );
+
+  printf("Status = %d\n", *status );
 
   /* Report wrapper error message */
   if ( *status != SAI__OK ) {
