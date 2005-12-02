a68879d6928121c44ddef03992879698af004402
Tim Jenness <t.jenness@jach.hawaii.edu>
2005-12-02 21:27:10 +0000

    add GPL
    svn path=/trunk/; revision=19202

--- libraries/hds/datMsg.c
+++ libraries/hds/datMsg.c
@@ -74,8 +74,28 @@
 *     24-SEP-1991 (RFWS):
 *        Changed to call EMS_ routines instead of ERR_ routines.
 *     01-DEC-2005 (TIMJ):
-*        Rewrite in C
+*        Rewrite in C.
 *     {enter_further_changes_here}
+
+*  Copyright:
+*     Copyright (C) 2005 Particle Physics and Astronomy Research Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public
+*     License along with this program; if not, write to the Free
+*     Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+*     MA 02111-1307, USA
 
 *  Bugs:
 *     {note_any_bugs_here}
