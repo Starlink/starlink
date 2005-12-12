4b34ba1b1dc2cc91d594410fa4a063c9eb611841
Andy Gibb <agg@phas.ubc.ca>
2005-12-12 19:07:27 +0000

    Initial version: doesn't do anything yet
    svn path=/trunk/; revision=19308

---
+++ applications/smurf/libsmf/smf_clone_data.c
@@ -0,0 +1,91 @@
+/*
+*+
+*  Name:
+*     smf_clone_data
+
+*  Purpose:
+*     Low-level clone of an input smfData struct
+
+*  Language:
+*     Starlink ANSI C
+
+*  Type of Module:
+*     SMURF subroutine
+
+*  Invocation:
+*     smf_clone_data( smfData *data, int *status );
+
+*  Arguments:
+*     data = smfData* (Given)
+*        Pointer to a smfData struct
+*     status = int* (Given and Returned)
+*        Pointer to global status.
+
+*  Description:
+*     This routine returns a pointer to a cloned version of the input.
+
+*  Authors:
+*     Andy Gibb (UBC)
+*     {enter_new_authors_here}
+
+*  History:
+*     2005-12-09 (AGG):
+*        Initial test version.
+*     {enter_further_changes_here}
+
+*  Copyright:
+*     Copyright (C) 2005 Particle Physics and Astronomy Research Council.
+*     University of British Columbia.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public
+*     License along with this program; if not, write to the Free
+*     Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+*     MA 02111-1307, USA
+
+*  Bugs:
+*     {note_any_bugs_here}
+*-
+*/
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <string.h>
+#include <stdio.h>
+
+#include "mers.h"
+#include "prm_par.h"
+#include "sae_par.h"
+#include "msg_par.h"
+
+#include "smurf_par.h"
+#include "libsmf/smf.h"
+#include "libsmf/smf_err.h"
+
+void smf_clone_data ( const smfData *idata, smfData **odata, int *status ) {
+
+  smfHead *hdr;
+  smfFile *file;
+
+  void *ipntr[3];
+  void *opntr[3];
+
+  /* Set the output pointer */
+
+  /* Update the reference counter */
+
+
+}
