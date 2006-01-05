66af0b9931fe47805121318ecdc7d5a605c65ffb
David S. Berry <d.berry@jach.hawaii.edu>
2006-01-05 10:57:05 +0000

    Added error code DAT__NAMIN
    svn path=/trunk/; revision=19498

--- libraries/ndf/ndf1_absnt.f
+++ libraries/ndf/ndf1_absnt.f
@@ -28,19 +28,39 @@
 
 *  Algorithm:
 *     Currently, the routine tests for the status values: DAT__FILNF,
-*     DAT__OBJNF, NDF__CNMIN and NDF__FILNF. It returns .TRUE. if any of
-*     these is matched.
+*     DAT__OBJNF, DAT__NAMIN, NDF__CNMIN and NDF__FILNF. It returns .TRUE. 
+*     if any of these is matched.
 
 *  Copyright:
-*     Copyright (C) 1994 Particle Physics & Astronomy Research Council
+*     Copyright (C) 2006 Particle Physics and Astronomy Research Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public
+*     License along with this program; if not, write to the Free
+*     Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+*     MA 02111-1307, USA
 
 *  Authors:
 *     RFWS: R.F. Warren-Smith (STARLINK, RAL)
+*     DSB: David S Berry (JAC, UCLan)
 *     {enter_new_authors_here}
 
 *  History:
 *     15-NOV-1994 (RFWS):
 *        Original version.
+*     5-JAN-2006 (DSB):
+*        Added DAT__NAMIN.
 *     {enter_changes_here}
 
 *  Bugs:
@@ -64,6 +84,7 @@
 *  structure or component.
       NDF1_ABSNT = ( ( ISTAT .EQ. DAT__FILNF ) .OR.
      :               ( ISTAT .EQ. DAT__OBJNF ) .OR.
+     :               ( ISTAT .EQ. DAT__NAMIN ) .OR.
      :               ( ISTAT .EQ. NDF__CNMIN ) .OR.
      :               ( ISTAT .EQ. NDF__FILNF ) )
 
