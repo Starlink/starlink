2b0ee80976436e31120bce845b9241c580bd82e1
Peter W. Draper <P.W.Draper@durham.ac.uk>
2006-01-26 14:33:10 +0000

    Switch to using NDG to access the image list. Avoids any local knowledge.
    svn path=/trunk/; revision=19717

--- applications/extractor/extract/src/extractor.c
+++ applications/extractor/extract/src/extractor.c
@@ -113,6 +113,9 @@
  *        comma in parentheses)
  *     10-MAR-2004 (PWD):
  *        Increased size of filename buffers from 20 to 256.
+ *     25-JAN-2006 (PWD):
+ *        Change NDF slice handling so that it works for higher dimensions. 
+ *        Previously only worked for 2D images (by chance).
  *     {enter_changes_here}
  
  *  Bugs:
@@ -137,6 +140,7 @@
 #include        "sae_par.h"
 #include        "par.h"
 #include        "ndf.h"
+#include        "star/ndg.h"
 #include        "merswrap.h"
 #include        "prefs.h"
 #include        "adam_defs.h"
@@ -153,104 +157,103 @@ jmp_buf env;          /*  Program environment when setjmp called */
 
 void real_extractor( int *status ) {
 
-  /* Declarations */
-  int keywords=0;          /*  Prompt for interactive parameters */
-  char *argkey[MAXARGS];   /*  Pointers to parameter names */
-  char *argval[MAXARGS];   /*  Pointers to parameter values */
-  char argstr[MAXARGS*VALUELEN]; /*  Storage space for name & values */
-  int narg;                /*  Number of parameters */
-  int nim;                 /*  Number of images given (max=2) */
-  char *str;
-  char *ptr;
-  
-  if ( setjmp( env ) == 0 ) {
-    
-    /* Run program. */
-    
-    /* Default parameters */
-    memset( &prefs, '\0', sizeof(prefstruct) );
-    prefs.nimage_name = 1;
-    prefs.image_name[0] = "image";
-    
-    /* See if any KEYWORDS to be specified as parameters */
-    parGet0l( "KEYWORDS", &keywords, status );
-    
-    /*   keywords = 0;*/
-    if ( keywords ) {
-      
-      /*  KEYWORDS to be given - 
-       *   set pointers to names in argkey
-       *   and pointers to value strings in argval
-       *   Use argstr as temporary buffer
-       */
-      argkey[0] = argstr;
-      for ( narg=0; (*status == SAI__OK) && (narg < MAXARGS); narg++ ) {
-        
-        /*     Get KEYWORD name */
-        parGet0c( "NAME", argkey[narg], VALUELEN, status );
-        argval[narg] = argkey[narg] + strlen(argkey[narg]) + 1;
-        
-        /*     Get value */
-        parGet0c( "VALUE", argval[narg], VALUELEN, status );
-        argkey[narg+1] = argval[narg] + strlen(argval[narg]) + 1;
+    /* Declarations */
+    int keywords=0;          /*  Prompt for interactive parameters */
+    char *argkey[MAXARGS];   /*  Pointers to parameter names */
+    char *argval[MAXARGS];   /*  Pointers to parameter values */
+    char argstr[MAXARGS*VALUELEN]; /*  Storage space for name & values */
+    int narg;                /*  Number of parameters */
+    int nim;                 /*  Number of images given (max=2) */
+    Grp *igrp;               /*  Group identifier for input NDFs */
+    int size;                /*  Number of NDFs returned */
+    int flag;                /*  Status of NDFs returned */
+    int i;                   /*  Loop variable */
 
-        parCancl( "NAME", status );
-        parCancl( "VALUE", status );
-      }
-      if ( *status == PAR__NULL ) errAnnul( status );
-      narg--;
-    } else {
-      narg = 0;
-    }
-    
-    /* Get configuration file name */
-    parGet0c( "CONFIG", prefs.prefs_name, VALUELEN, status );
-    if ( *status == SAI__OK ) {
-        /* And read it, using our NAME, VALUE pairs in preference to those in
-         * the file */
-        readprefs(prefs.prefs_name, argkey, argval, narg);
-    }
-    
-    /* and image name(s) - again use argstr as buffer */
-    parGet0c( "IMAGE", argstr, VALUELEN, status );
-    if ( *status == SAI__OK ) {
-      for (nim = 0; (str=strtok( nim?NULL:argstr, notokstr ))!=NULL; nim++) {
-        if (nim<MAXIMAGE) {
-          /*  Check for trailing slice. This is indicated by a 
-              "(" in the string */
-          if ( strstr( str, "(" ) != NULL ) {
-            /*  Access up to next token and use this as final part 
-                of name */
-            ptr = strtok( NULL, notokstr );
-            *(--ptr) = ',';
-          }
-          prefs.image_name[nim] = str;
+    if ( setjmp( env ) == 0 ) {
+        
+        /* Run program. */
+        
+        /* Default parameters */
+        memset( &prefs, '\0', sizeof(prefstruct) );
+        prefs.nimage_name = 1;
+        prefs.image_name[0] = "image";
+        
+        /* See if any KEYWORDS to be specified as parameters */
+        parGet0l( "KEYWORDS", &keywords, status );
+        
+        /*   keywords = 0;*/
+        if ( keywords ) {
+            
+            /*  KEYWORDS to be given - 
+             *   set pointers to names in argkey
+             *   and pointers to value strings in argval
+             *   Use argstr as temporary buffer
+             */
+            argkey[0] = argstr;
+            for ( narg=0; (*status == SAI__OK) && (narg < MAXARGS); narg++ ) {
+                
+                /*     Get KEYWORD name */
+                parGet0c( "NAME", argkey[narg], VALUELEN, status );
+                argval[narg] = argkey[narg] + strlen(argkey[narg]) + 1;
+                
+                /*     Get value */
+                parGet0c( "VALUE", argval[narg], VALUELEN, status );
+                argkey[narg+1] = argval[narg] + strlen(argval[narg]) + 1;
+                
+                parCancl( "NAME", status );
+                parCancl( "VALUE", status );
+            }
+            if ( *status == PAR__NULL ) errAnnul( status );
+            narg--;
         } else {
-          error(EXIT_FAILURE, "*Error*: Too many input images: ", str);
+            narg = 0;
         }
-      }
-      prefs.nimage_name = nim;
-      
-      /* Now do the business */
-      errStat( status );
-      if ( *status == SAI__OK ) { 
-        ndfBegin();
-        srand( 1 ); /* Needed for repeatable measurements as rand
-                       function is used in gatherup */
-        makeit();
+        
+        /* Get configuration file name */
+        parGet0c( "CONFIG", prefs.prefs_name, VALUELEN, status );
+        if ( *status == SAI__OK ) {
+            /* And read it, using our NAME, VALUE pairs in preference to those
+             * in the file */
+            readprefs( prefs.prefs_name, argkey, argval, narg );
+        }
+        
+        /* and image name(s), obtain these as an NDG group */
+        igrp = NULL;
+        ndgAssoc( "IMAGE", 0, &igrp, &size, &flag, status );
+
+        if ( *status == SAI__OK ) {
+
+            //  Only pick first two images, if more are given.
+            nim = ( size > 1 ) ? 2: 1;
+            argval[0] = argstr;
+            grpGet( igrp, 1, nim, argval, VALUELEN, status );
+            prefs.image_name[0] = argval[0];
+            if ( nim == 2 ) {
+                prefs.image_name[1] = argval[1];
+            }
+            prefs.nimage_name = nim;
+            grpDelet( &igrp, status );
+            
+            /* Now do the business */
+            errStat( status );
+            if ( *status == SAI__OK ) { 
+                ndfBegin();
+                srand( 1 ); /* Needed for repeatable measurements as rand
+                               function is used in gatherup */
+                makeit();
+                errStat( status );
+                ndfEnd( status );
+            }
+            if ( *status == SAI__OK ) {
+                NFPRINTF (OUTPUT, "All done" );
+                NPRINTF( OUTPUT, "\n" );
+            }
+        }
+    } else {
+        /* Return from longjmp in error() -- would be nice to close any open 
+           files at this point.... */
         errStat( status );
-        ndfEnd( status );
-      }
-      if ( *status == SAI__OK ) {
-        NFPRINTF(OUTPUT, "All done");
-        NPRINTF(OUTPUT, "\n");
-      }
     }
-  } else {
-    /* Return from longjmp in error() -- would be nice to close any open 
-       files at this point.... */
-    errStat( status );
-  }
 }
 
 /* Atask wrapper */
