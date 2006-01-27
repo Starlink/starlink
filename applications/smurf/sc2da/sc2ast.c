758f3402e2a98e49e4a8aaf1924e4fa5b1a6dadf
Tim Jenness <t.jenness@jach.hawaii.edu>
2006-01-27 20:01:30 +0000

    integrate simualator version
    svn path=/trunk/; revision=19768

--- applications/smurf/sc2da/sc2ast.c
+++ applications/smurf/sc2da/sc2ast.c
@@ -8,6 +8,23 @@
 #include "sc2ast.h"
 #include "Ers.h"
 
+/* -----------------------------------------------------------------------*/
+/* Temporary kludge required for sc2ast_createwcs_compat   EC 05Dec2005   */
+/*                       and for sc2ast_kludgemodjuldate                  */
+#ifndef PI
+#define PI 3.14159265358979            /* math constant */
+#endif
+
+#ifndef dmod
+#define dmod(A,B) ((B)!=0.0?((A)*(B)>0.0?(A)-(B)*floor((A)/(B))\
+                                        :(A)+(B)*floor(-(A)/(B))):(A))
+#endif
+
+#ifndef DS2R
+#define DS2R 7.2722052166430399038487115353692196393452995355905e-5
+#endif
+/* -----------------------------------------------------------------------*/
+
 static char errmess[132];              /* For DRAMA error messages */
 
 #define MM2RAD 2.4945e-5               /* scale at array in radians */
@@ -572,3 +589,203 @@ int *status             /* global status (given and returned) */
    astAddFrame ( *fset, AST__CURRENT, cmpmap, polframe );
 
 }
+
+/*+ sc2ast_kludgemodjuldate - kludge for the modified julian date */
+
+double sc2ast_kludgemodjuldate
+( 
+double ra,           /* Right Ascension in radians (given) */
+int *status          /* global status (given and returned) */
+)
+
+/* Description :
+    Until the simulator runs on a single clock, this kludge will calculate
+    a modified julian date that is consistent with the astronomical source
+    being on the meridian (i.e. lst=ra as it is assumed everywhere in the
+    simulator). This routine is needed to calculate the time fields
+    for TAI/RTS.
+
+   Authors :
+    Ed Chapin (echapin@phas.ubc.ca)
+
+   History :
+    05Dec2005 : Original version (echapin)
+*/
+
+{
+  double date;          /* ref. modified julian date */
+  double delta_st;      /* How much does reflst have to change? radians */
+  double lat;           /* JCMT latitude in radians */
+  double lon;           /* JCMT west longitude in radians */
+  double obsdate;       /* The modified julian date of the observation */
+  double refmjdate;     /* date + ut */
+  double refgst;        /* ref. gst in radians */
+  double reflst;        /* ref. lst in radians */
+  double t;             /* Julian centuries since J2000 */
+  double ut;            /* ref. universal time as day fraction */
+
+
+  /*if ( !StatusOkP(status) ) return; */
+
+  /* JCMT coordinates in radians */
+
+  /*lat = ( 19.0 + (49.0/60.0) + (33.0/3600.0) ) / RAD2DEG;*/
+  lon = ( 155.0 + (28.0/60.0) + (0.0/3600.0) ) / RAD2DEG;
+
+  /* Define an arbitrary modified julian date + UT time */
+
+  date = 51544;        /* Jan 1, 2000 */
+  ut = 0.5;            /* noon */
+
+  /* Use slalib slaGmsta algorithm to calculate greenwich LST at this date */
+
+  t = ( ut + ( date - 51544.5 ) ) / 36525.0;
+
+  refgst = DS2R * ( 24110.54841
+                           + ( 8640184.812866
+                           + ( 0.093104
+                             - 6.2e-6 * t ) * t ) * t
+                             + 86400.0 * ( dmod ( ut, 1.0 ) +
+                                           dmod ( date, 1.0 ) ) ); 
+
+  /* Calculate the reference LST from the GST using the telescope longitude */
+
+  reflst = refgst - lon;
+
+  /* If we want ra = lst, how far do we have to move in ST?
+     i.e. what is the hour angle for the reference lst */
+ 
+  delta_st = ra - reflst;
+
+  /* Calculate the obsdate. Note that delta_st is multiplied by
+     the ratio of a civil time interval to a sidereal time interval */
+
+  obsdate = date + ut + delta_st/(2.*PI*1.002737909);
+
+  return obsdate;
+}
+
+/*+ sc2ast_telpos - get telescope position and orientation */
+
+void sc2ast_telpos
+( 
+double ra,           /* Right Ascension in radians (given) */
+double dec,          /* Declination in radians (given) */
+double lst,          /* local sidereal time in radians (given) */
+double *az,          /* Azimuth in radians (returned) */
+double *el,          /* Elevation in radians (returned) */
+double *p            /* Parallactic angle in radians (returned) */
+)
+
+/* Description :
+    Copy of dsim_telpos to avoid a libdsim dependency
+    Use slalib algorithms to get from equatorial to horizontal coordinates.
+
+   Authors :
+    Ed Chapin (echapin@phas.ubc.ca)
+    B.D.Kelly (bdk@roe.ac.uk)
+
+   History :
+    12May2005 : Original version (bdk)
+    05Dec2005 : Copied into sc2ast.c (EC)
+*/
+
+{
+   double cosp;      /* intermediate result */
+   double phi;       /* latitude of telescope in radians */
+   double ha;        /* hour angle in radians */
+   double r;         /* intermediate result */
+   double sinp;      /* intermediate result */
+   double x;         /* cartesian coordinates */
+   double y;         /* cartesian coordinates */
+   double z;         /* cartesian coordinates */
+
+/* JCMT is 19:49:33 N */
+
+   phi = ( 19.0 + (49.0/60.0) + (33.0/3600.0) ) / RAD2DEG;
+   ha = lst - ra;
+
+/*
+   Equivalent to slaDe2h ( ha, dec, phi, az, el );
+*/
+
+/* Az,El as x,y,z */
+
+   x = - cos(ha) * cos(dec) * sin(phi) + sin(dec) * cos(phi);
+   y = - sin(ha) * cos(dec);
+   z = cos(ha) * cos(dec) * cos(phi) + sin(dec) * sin(phi);
+
+/* To spherical */
+
+   r = sqrt ( x*x + y*y );
+
+   if ( r < 1.0e-20 )
+   {
+      *az = 0.0;
+   }
+   else
+   {
+      *az = atan2 ( y, x );
+   }
+
+   if ( *az < 0.0 )
+   {
+      *az += 2.0 * PI;
+   }
+
+   *el = atan2 ( z, r );
+
+/*
+   *p = slaPa ( ha, dec, phi ); 
+*/
+   sinp = cos ( phi ) * sin ( ha );
+   cosp = sin ( phi ) * cos ( dec) - cos ( phi ) * sin ( dec) * cos ( ha );
+
+   if ( sinp != 0.0 || cosp != 0.0 )
+   {
+      *p = atan2 ( sinp, cosp );
+   }
+   else
+   {
+      *p = 0.0;
+   }
+
+}
+
+/*+ sc2ast_createwcs_compat - create WCS descriptionusing old parameters */
+
+void sc2ast_createwcs_compat
+(
+int subnum,             /* subarray number, 0-7 (given) */
+double ra,              /* Right Ascension of the telescope boresight */
+double dec,             /* Declination of the telescope boresight */
+double el,              /* Boresight elevation in radians (given) */
+double p,               /* No longer used (pass any dummy value) */
+AstFrameSet **fset,     /* constructed frameset (returned) */
+int *status             /* global status (given and returned) */
+)
+/* Description : 
+     This is a temporary routine meant to replace calls to
+     sc2ast_createwcs that were written before TimJ changed the
+     API. It is simply a wrapper for sc2ast_createwcs which generates
+     an appropriate Modified Julian date so that LST=RA for the
+     calculation of TAI using the old parameters for sc2ast_createwcs
+     Authors : 
+      Ed Chapin (echapin@phas.ubc.ca) 
+     History : 
+      05Dec2005 : original (EC)
+*/
+
+{
+  double tai;
+  double temp_az, temp_el, temp_lst, temp_p;
+
+  temp_lst = ra;
+  sc2ast_telpos ( ra, dec, temp_lst, &temp_az, &temp_el, &temp_p );
+
+  tai = sc2ast_kludgemodjuldate( ra, status );
+  
+  sc2ast_createwcs( subnum, temp_az, temp_el, tai, fset, status );
+}
+
+
