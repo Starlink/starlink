f21a5346dbb846dfe7a12864300e9842f9c80583
Peter W. Draper <P.W.Draper@durham.ac.uk>
2006-03-07 12:02:14 +0000

    Add type and cget support.
    svn path=/trunk/; revision=20245

--- applications/gaia/gaia/generic/gaiaNDFTcl.c
+++ applications/gaia/gaia/generic/gaiaNDFTcl.c
@@ -22,6 +22,7 @@
 #include <stdlib.h>
 
 #include <tcl.h>
+#include <ndf.h>
 #include <gaiaNDF.h>
 
 static int gaiaNdfOpen( ClientData clientData, Tcl_Interp *interp,
@@ -33,6 +34,9 @@ static int gaiaNdfMap( ClientData clientData, Tcl_Interp *interp,
 
 static int gaiaNdfGtWcs( ClientData clientData, Tcl_Interp *interp,
                          int objc, Tcl_Obj *CONST objv[] );
+
+static int gaiaNdfCGet( ClientData clientData, Tcl_Interp *interp,
+                        int objc, Tcl_Obj *CONST objv[] );
 
 /**
  * Register all the NDF access commands.
@@ -54,6 +58,11 @@ int Ndf_Init( Tcl_Interp *interp )
     Tcl_CreateObjCommand( interp, "ndf::getwcs", gaiaNdfGtWcs,
                           (ClientData) NULL,
                           (Tcl_CmdDeleteProc *) NULL );
+
+    Tcl_CreateObjCommand( interp, "ndf::getc", gaiaNdfCGet,
+                          (ClientData) NULL,
+                          (Tcl_CmdDeleteProc *) NULL );
+
     return TCL_OK;
 }
 
@@ -117,14 +126,15 @@ static int gaiaNdfClose( ClientData clientData, Tcl_Interp *interp,
 }
 
 /**
- * Map the NDF data array in double precision. The result is a memory address
- * (long int) and the number of elements mapped.
+ * Map the NDF data array in the NDF's data type. The result is a memory 
+ * address (long int), the number of elements mapped and the data type.
  */
 static int gaiaNdfMap( ClientData clientData, Tcl_Interp *interp,
                        int objc, Tcl_Obj *CONST objv[] )
 {
     Tcl_Obj *resultObj;
     char *error_mess;
+    char type[NDF__SZTYP+1];
     int el;
     int indf;
     int result;
@@ -139,14 +149,21 @@ static int gaiaNdfMap( ClientData clientData, Tcl_Interp *interp,
     /* Get the identifier */
     resultObj = Tcl_GetObjResult( interp );
     if ( Tcl_GetIntFromObj( interp, objv[1], &indf ) == TCL_OK ) {
-        result = gaiaSimpleMapNDF( indf, "_DOUBLE", "DATA", &dataPtr, 
-                                   &el, &error_mess  );
+        result = gaiaSimpleTypeNDF( indf, "DATA", type, NDF__SZTYP+1,
+                                    &error_mess );
         if ( result == TCL_OK ) {
-            Tcl_ListObjAppendElement( interp, resultObj, 
-                                      Tcl_NewLongObj( (long) dataPtr ) );
-            Tcl_ListObjAppendElement( interp, resultObj, Tcl_NewIntObj( el ) );
+            result = gaiaSimpleMapNDF( indf, "_DOUBLE", "DATA", &dataPtr, 
+                                       &el, &error_mess  );
+            if ( result == TCL_OK ) {
+                Tcl_ListObjAppendElement( interp, resultObj, 
+                                          Tcl_NewLongObj( (long) dataPtr ) );
+                Tcl_ListObjAppendElement( interp, resultObj, 
+                                          Tcl_NewIntObj( el ) );
+                Tcl_ListObjAppendElement( interp, resultObj,
+                                          Tcl_NewStringObj( type, -1 ) );
+            }
         }
-        else {
+        if ( result != TCL_OK ) {
             Tcl_SetStringObj( resultObj, error_mess, -1 );
             free( error_mess );
         }
@@ -212,6 +229,46 @@ static int gaiaNdfGtWcs( ClientData clientData, Tcl_Interp *interp,
     else {
         Tcl_SetStringObj( resultObj, error_mess, -1 );
         free( error_mess );
+    }
+    return result;
+}
+
+/**
+ * Return the value of an NDF character component.
+ */
+static int gaiaNdfCGet( ClientData clientData, Tcl_Interp *interp,
+                        int objc, Tcl_Obj *CONST objv[] )
+{
+    Tcl_Obj *resultObj;
+    char *error_mess;
+    char value[NDF__SZHIS+1];
+    int el;
+    int indf;
+    int result;
+    void *dataPtr;
+
+    /* Check arguments, need 2 the ndf identifier and the component */
+    if ( objc != 3 ) {
+        Tcl_WrongNumArgs( interp, objc, objv, "ndf_identifier component" );
+        return TCL_ERROR;
+    }
+
+    /* Get the identifier */
+    resultObj = Tcl_GetObjResult( interp );
+    if ( Tcl_GetIntFromObj( interp, objv[1], &indf ) == TCL_OK ) {
+        result = gaiaSimpleCGetNDF( indf, Tcl_GetString( objv[2] ),
+                                    value, NDF__SZHIS+1, &error_mess );
+        if ( result == TCL_OK ) {
+            Tcl_SetStringObj( resultObj, value, -1 );
+        }
+        else {
+            Tcl_SetStringObj( resultObj, error_mess, -1 );
+            free( error_mess );
+        }
+    }
+    else {
+        Tcl_SetStringObj( resultObj, "not an integer" , -1 );
+        result = TCL_ERROR;
     }
     return result;
 }
