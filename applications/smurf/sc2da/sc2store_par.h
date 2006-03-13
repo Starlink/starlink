5f3b75e1fdb3db971439c7c0bad9cd685048c0ff
Andy Gibb <agg@phas.ubc.ca>
2006-03-13 22:44:06 +0000

    Revert to original...
    svn path=/trunk/; revision=20323

--- applications/smurf/sc2da/sc2store_par.h
+++ applications/smurf/sc2da/sc2store_par.h
@@ -4,11 +4,10 @@
     13Aug2004 : original (bdk)
     17Feb2005 : add SC2_HEAT (bdk)
     02Oct2005 : add SC2STORE_FLATLEN (bdk)
-    02Feb2006 : use an enum for the indices (bdk)
-    09Feb2006 : make SC2STORE_NUM last item in the enum (bdk)
 */
 
 
+#define SC2STORE_NUM 41      /* number of per-frame data items */
 #define SC2STORE_FLATLEN 17  /* maximum length of flatfield algorithm name */
 #define RTS_TASKS_LEN 80     /* length of string for rts report */
 #define RTS_ERRS_LEN 80      /* length of string for rts report */
@@ -20,48 +19,45 @@
 
 /* Named indices into the per-frame header arrays */
 
-typedef enum
-{
-   FTS_POS,
-   POL_ANG,
-   RTS_NUM,
-   RTS_STEP,
-   RTS_END,
-   RTS_TASKS,
-   RTS_ERRS,
-   SC2_HEAT,
-   SMU_AZ_OFF_X,
-   SMU_AZ_OFF_Y,
-   SMU_X,
-   SMU_Y,
-   SMU_Z,
-   SMU_TR_OFF_X,
-   SMU_TR_OFF_Y,
-   TCS_AIRMASS,
-   TCS_AZ_SYS,
-   TCS_AZ_ANG,
-   TCS_AZ_AC1,
-   TCS_AZ_AC2,
-   TCS_AZ_DC1,
-   TCS_AZ_DC2,
-   TCS_AZ_BC1,
-   TCS_AZ_BC2,
-   TCS_INDEX,
-   TCS_SOURCE,
-   TCS_TR_SYS,
-   TCS_TR_ANG,
-   TCS_TR_AC1,
-   TCS_TR_AC2,
-   TCS_TR_DC1,
-   TCS_TR_DC2,
-   TCS_TR_BC1,
-   TCS_TR_BC2,
-   WVM_TH,
-   WVM_T12,
-   WVM_T42,
-   WVM_T78,
-   WVM_TW,
-   WVM_QUAL,
-   WVM_TIME,
-   SC2STORE_NUM
-} SC2STORE_HEADLIST;
+#define FTS_POS 0
+#define POL_ANG 1
+#define RTS_NUM 2
+#define RTS_STEP 3
+#define RTS_END 4
+#define RTS_TASKS 5
+#define RTS_ERRS 6
+#define SC2_HEAT 7
+#define SMU_AZ_OFF_X 8
+#define SMU_AZ_OFF_Y 9
+#define SMU_X 10
+#define SMU_Y 11
+#define SMU_Z 12
+#define SMU_TR_OFF_X 13
+#define SMU_TR_OFF_Y 14
+#define TCS_AIRMASS 15
+#define TCS_AZ_SYS 16
+#define TCS_AZ_ANG 17
+#define TCS_AZ_AC1 18
+#define TCS_AZ_AC2 19
+#define TCS_AZ_DC1 20
+#define TCS_AZ_DC2 21
+#define TCS_AZ_BC1 22
+#define TCS_AZ_BC2 23
+#define TCS_INDEX 24
+#define TCS_SOURCE 25
+#define TCS_TR_SYS 26
+#define TCS_TR_ANG 27
+#define TCS_TR_AC1 28
+#define TCS_TR_AC2 29
+#define TCS_TR_DC1 30
+#define TCS_TR_DC2 31
+#define TCS_TR_BC1 32
+#define TCS_TR_BC2 33
+#define WVM_TH 34
+#define WVM_T12 35
+#define WVM_T42 36
+#define WVM_T78 37
+#define WVM_TW 38
+#define WVM_QUAL 39
+#define WVM_TIME 40
+
