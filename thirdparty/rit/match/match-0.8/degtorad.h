a655ae914ef03e96fc69c10e60ec7c87a2be64cd
Peter W. Draper <P.W.Draper@durham.ac.uk>
2006-03-16 15:33:56 +0000

    The match program used by AUTOASTROM.
    svn path=/trunk/; revision=20356

---
+++ thirdparty/rit/match/match-0.8/degtorad.h
@@ -0,0 +1,11 @@
+#if !defined(APPLY_MATCH_H)
+#define APPLY_MATCH_H
+
+
+   /* multiply by this to convert degrees to radians */
+#ifndef PI
+#define PI 3.14159265359
+#endif
+#define DEGTORAD  (PI/180.0)
+
+#endif   /* APPLY_MATCH_H */
