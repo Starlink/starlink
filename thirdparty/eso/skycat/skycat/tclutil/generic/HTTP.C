1f732ee2b5b193ddd7979997f9df75f78cfd0647
Peter W. Draper <P.W.Draper@durham.ac.uk>
2006-03-24 12:18:39 +0000

    Fix valgrind warning
    svn path=/trunk/; revision=20460

--- thirdparty/eso/skycat/skycat/tclutil/generic/HTTP.C
+++ thirdparty/eso/skycat/skycat/tclutil/generic/HTTP.C
@@ -109,7 +109,7 @@ HTTP::~HTTP()
     if (fd_ >= 0) 
 	close(fd_);
     if (resultGC_) {
-	delete[] resultGC_;
+	free( resultGC_ );
         resultGC_ = NULL;
     }
     reset();
@@ -762,7 +762,7 @@ int HTTP::get(const char* url)
 char* HTTP::get(const char* url, int& nlines, int freeFlag)
 {
     if (resultGC_) {
-	delete[] resultGC_;
+	free( resultGC_ );
 	resultGC_ = resultBuf_ = resultPtr_ = NULL;
     }
 
