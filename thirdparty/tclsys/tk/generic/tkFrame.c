5357ba8f51d406f369bfeec6355c9a16c4065696
Peter W. Draper <P.W.Draper@durham.ac.uk>
2006-03-27 09:11:20 +0000

    Pesky GNUisms
    svn path=/trunk/; revision=20479

--- thirdparty/tclsys/tk/generic/tkFrame.c
+++ thirdparty/tclsys/tk/generic/tkFrame.c
@@ -785,8 +785,9 @@ FrameWidgetObjCmd(clientData, interp, objc, objv)
 	     */
 
 	    for (i = 2; i < objc; i++) {
+                char *arg;
                 length = 0;
-		char *arg = Tcl_GetStringFromObj(objv[i], (int *) &length);
+		arg = Tcl_GetStringFromObj(objv[i], (int *) &length);
 		if (length < 2) {
 		    continue;
 		}
