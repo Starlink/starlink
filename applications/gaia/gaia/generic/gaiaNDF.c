232e2a442b9c2af70502c3bb617f0fe707154379
Peter W. Draper <P.W.Draper@durham.ac.uk>
2006-04-12 11:15:54 +0000

    Refactor general FrameSet handling to util code.
    svn path=/trunk/; revision=20650

--- applications/gaia/gaia/generic/gaiaNDF.c
+++ applications/gaia/gaia/generic/gaiaNDF.c
@@ -1254,75 +1254,15 @@ int gaiaNDFGtWcs( int ndfid, AstFrameSet **iwcs, char **error_mess )
 int gaiaNDFGtAxisWcs( int ndfid, int axis, int offset, AstFrameSet **iwcs,
                       char **error_mess )
 {
-   AstFrame *tmpframe;
    AstFrameSet *fullwcs;
-   AstMapping *joined;
-   int iaxes[1];
-   int nin;
-   int nout;
+   int result;
 
-   astBegin;
    if ( gaiaNDFGtWcs( ndfid, &fullwcs, error_mess ) == 1 ) {
-       astEnd;
        return TCL_ERROR;
    }
-
-   /* Determine the current number of input and output axes and
-    * take a copy of the full frameset. */
-   nin = astGetI( fullwcs, "Nin" );
-   nout = astGetI( fullwcs, "Nout" );
-   *iwcs = astCopy( fullwcs );
-
-   /* The requested axis must be valid, if not we adopt the
-    * default of axis 1. */
-   iaxes[0] = axis;
-   if ( iaxes[0] > nin ) {
-       iaxes[0] = 1;
-   }
-   else if ( iaxes[0] < 1 ) {
-       iaxes[0] = 1;
-   }
-
-   /* If the base frame has more than one axis then select the given one.
-    * This is easy, just pick a frame with the appropriate axes and put it
-    * back, note that we have to pick the current frame, so swap things
-    * around a little. */
-   if ( nin != 1 ) {
-       astInvert( *iwcs );
-       joined = NULL;
-       tmpframe = astPickAxes( *iwcs, 1, iaxes, &joined );
-       astAddFrame( *iwcs, AST__CURRENT, joined, tmpframe );
-       astInvert( *iwcs );
-
-       /* If we have an offset to apply to the GRID coordinates (these will be
-        * the base frame). Then add in a ShiftMap. */
-       if ( offset != 0 ) {
-           double shift[1];
-           AstShiftMap *map;
-           shift[0] = (double) -offset;
-           map = astShiftMap( 1, shift, "" );
-           astRemapFrame( *iwcs, AST__BASE, map );
-           astAnnul( map );
-       }
-   }
-
-   /* Select an axis in the current frame and tack this onto the
-    * end. Same procedure as above, just no inversion. */
-   if ( nout != 1 ) {
-       joined = NULL;
-       tmpframe = astPickAxes( *iwcs, 1, iaxes, &joined );
-       astAddFrame( *iwcs, AST__CURRENT, joined, tmpframe );
-   }
-
-   /* Export the WCS and report if any errors occurred. */
-   astExport( *iwcs );
-   astEnd;
-   if ( ! astOK ) {
-       *error_mess = strdup( "Failed to extract AST frameset for an axis" );
-       astClearStatus;
-       return TCL_ERROR;
-   }
-   return TCL_OK;
+   result = gaiaUtilsGtAxisWcs( fullwcs, axis, offset, iwcs, error_mess );
+   astAnnul( fullwcs );
+   return result;
 }
 
 /**
@@ -1382,29 +1322,18 @@ int gaiaNDFQueryCoord( int ndfid, int axis, double *coords, int trailed,
                        char **error_mess )
 {
     AstFrameSet *frameSet = NULL;
-    static char buf[256];              /* Static as may be returned */
     char *domain;
-    char *label;
-    char *units;
-    static char lcoord[30];            /* Static as may be returned */
-    double static_coords_in[7];
-    double *coords_in;
-    double diff;
-    double out1[7];
-    double out2[7];
-    double tmp;
-    int base;
-    int caxis;
     int current;
+    int base;
     int i;
-    int ncoords_in;
-    int ncoords_out;
     int nframes;
     int pixel;
+    int result;
     int status = SAI__OK;
 
     emsMark();
 
+    /*  Get the NDF FrameSet */
     ndfGtwcs( ndfid, &frameSet, &status );
     if ( status != SAI__OK ) {
         *error_mess = gaiaUtilsErrMessage();
@@ -1413,27 +1342,11 @@ int gaiaNDFQueryCoord( int ndfid, int axis, double *coords, int trailed,
         return TCL_ERROR;
     }
 
-    /* Check dimensionality, if more than ncoords then pad up, if less them
-     * drop them off. */
+    /*  Find the PIXEL domain. */
     current = astGetI( frameSet, "Current" );
     base = astGetI( frameSet, "Base" );
-    astSetI( frameSet, "Current", base );
-    ncoords_in = astGetI( frameSet, "naxes" );
-    astSetI( frameSet, "Current", current );
-    if ( ncoords_in == ncoords ) {
-        coords_in = coords;
-    }
-    else if ( ncoords_in > ncoords ) {
-        coords_in = static_coords_in;
-        memcpy( coords_in, coords, ncoords * sizeof( double ) );
-        for ( i = ncoords; i < ncoords_in; i++ ) {
-            coords_in[i] = AST__BAD;
-        }
-    }
-
-    /*  Find the PIXEL domain. */
-    pixel = 0;
     nframes = astGetI( frameSet, "Nframe" );
+    pixel = 0;
     for ( i = 1; i <= nframes; i++ ) {
         astSetI( frameSet, "Current", i );
         domain = (char *)astGetC( frameSet, "Domain" );
@@ -1454,62 +1367,16 @@ int gaiaNDFQueryCoord( int ndfid, int axis, double *coords, int trailed,
     /*  This becomes the BASE frame. */
     astSetI( frameSet, "Base", pixel );
 
-    /*  Transform the position from pixel coordinates to world coordinates for
-     *  this position, and a position one pixel offset along the given
-     *  axis. */
-    ncoords_out = astGetI( frameSet, "naxes" );
-
-    coords_in[axis-1] += 1.0;
-    astTranN( frameSet, 1, ncoords_in, 1, coords_in, 1, ncoords_out, 1, out1 );
-    coords_in[axis-1] -= 1.0;
-    astTranN( frameSet, 1, ncoords_in, 1, coords_in, 1, ncoords_out, 1, out2 );
-
-    /*  Select the axis with the largest shift as equivalent in world
-     *  coordinates. */
-    caxis = axis;
-    diff = 0.0;
-    for ( i = 0; i < ncoords_out; i++ ) {
-        tmp = fabs( out1[i] - out2[i] );
-        if ( tmp > diff ) {
-            caxis = i + 1;
-            diff = tmp;
-        }
-    }
-
-    /*  Format the value along that axis, if requested, otherwise just write
-     *  out the double. */
-    if ( formatted ) {
-        *coord = (char *) astFormat( frameSet, caxis, out2[caxis - 1] );
-    }
-    else {
-        sprintf( lcoord, "%.17g", out2[caxis -1] );
-        *coord = lcoord;
-    }
-
-    if ( ! astOK ) {
-        astClearStatus;
-        *coord = NULL;
-        *error_mess =
-            strdup( "Failed to convert pixel index to world coordinate" );
-        astSetI( frameSet, "Base", base );
-        frameSet = (AstFrameSet *) astAnnul( frameSet );
-        errRlse();
-        return TCL_ERROR;
-    }
-
-    /*  Add the axis units and label if requested. */
-    if ( trailed ) {
-        sprintf( buf, "unit(%d)", caxis );
-        units = (char *) astGetC( frameSet, buf );
-        sprintf( buf, "label(%d)", caxis );
-        label = (char *) astGetC( frameSet, buf );
-        sprintf( buf, "%s %s (%s)", *coord, label, units );
-        *coord = buf;
-    }
+    /* Do the transformation */
+    result = gaiaUtilsQueryCoord( frameSet, axis, coords, trailed, formatted,
+                                  ncoords, coord, error_mess );
 
     /*  Restore the base frame. */
     astSetI( frameSet, "Base", base );
     frameSet = (AstFrameSet *) astAnnul( frameSet );
     emsRlse();
-    return TCL_OK;
+    if ( !astOK ) {
+        astClearStatus;
+    }
+    return result;
 }
--- applications/gaia/gaia/generic/gaiaUtils.c
+++ applications/gaia/gaia/generic/gaiaUtils.c
@@ -21,12 +21,17 @@
  *-
  */
 
+#include <stdio.h>
 #include <string.h>
+#include <math.h>
 
 #include <gaiaUtils.h>
 #include <sae_par.h>
 #include <ems.h>
 #include <ems_par.h>
+
+#define TCL_OK 0
+#define TCL_ERROR 1
 
 /*
  *  Name:
@@ -94,4 +99,189 @@ char *gaiaUtilsErrMessage()
         }
     }
     return result_message;
+}
+
+/**
+ * Get an AST frameset that describes the coordinates of a given axis.
+ * Axes are in the AST sense, i.e. start at 1. The offset value is a shift
+ * that should be applied to the GRID coordinates (useful when an NDF data
+ * component has been sectioned outside of the NDF library, this is the
+ * origin).
+ */
+int gaiaUtilsGtAxisWcs( AstFrameSet *fullwcs, int axis, int offset,
+                        AstFrameSet **iwcs, char **error_mess )
+{
+   AstFrame *tmpframe;
+   AstMapping *joined;
+   int iaxes[1];
+   int nin;
+   int nout;
+
+   astBegin;
+
+   /* Determine the current number of input and output axes and
+    * take a copy of the full frameset. */
+   nin = astGetI( fullwcs, "Nin" );
+   nout = astGetI( fullwcs, "Nout" );
+   *iwcs = astCopy( fullwcs );
+
+   /* The requested axis must be valid, if not we adopt the
+    * default of axis 1. */
+   iaxes[0] = axis;
+   if ( iaxes[0] > nin ) {
+       iaxes[0] = 1;
+   }
+   else if ( iaxes[0] < 1 ) {
+       iaxes[0] = 1;
+   }
+   
+   /* If the base frame has more than one axis then select the given one.
+    * This is easy, just pick a frame with the appropriate axes and put it
+    * back, note that we have to pick the current frame, so swap things 
+    * around a little. */
+   if ( nin != 1 ) {
+       astInvert( *iwcs );
+       joined = NULL;
+       tmpframe = astPickAxes( *iwcs, 1, iaxes, &joined );
+       astAddFrame( *iwcs, AST__CURRENT, joined, tmpframe );
+       astInvert( *iwcs );
+
+       /* If we have an offset to apply to the GRID coordinates (these will be
+        * the base frame). Then add in a ShiftMap. */
+       if ( offset != 0 ) {
+           double shift[1];
+           AstShiftMap *map;
+           shift[0] = (double) -offset;
+           map = astShiftMap( 1, shift, "" );
+           astRemapFrame( *iwcs, AST__BASE, map );
+           astAnnul( map );
+       }
+   }
+
+   /* Select an axis in the current frame and tack this onto the
+    * end. Same procedure as above, just no inversion. */
+   if ( nout != 1 ) {
+       joined = NULL;
+       tmpframe = astPickAxes( *iwcs, 1, iaxes, &joined );
+       astAddFrame( *iwcs, AST__CURRENT, joined, tmpframe );
+   }
+
+   /* Export the WCS and report if any errors occurred. */
+   astExport( *iwcs );
+   astEnd;
+   if ( ! astOK ) {
+       *error_mess = strdup( "Failed to extract AST frameset for an axis" );
+       astClearStatus;
+       return TCL_ERROR;
+   }
+   return TCL_OK;
+}
+
+/**
+ * Query the equivalent current coordinate of a base coordinate along the
+ * given axis.
+ *
+ * Returns a coordinate value in *coord that may be formatted and trailed by
+ * the axis units and label. The position along the axis is identified using
+ * ncoords pixel coordinates, where ncoords must match the dimensionality of
+ * the FrameSet (at least up to ncoords axes, axes beyond that are handled as
+ * dummies). The coordinate returned is matched to the requested axis by being
+ * the coordinate with the largest change wrt to a single pixel step. We need
+ * to use this logic as the relationship between the base frame axes and the
+ * current frame ones may not be straight-forward. Note that the value
+ * returned by the coord argument should be immediately copied.
+ */
+int gaiaUtilsQueryCoord( AstFrameSet *frameset, int axis, double *coords, 
+                         int trailed, int formatted, int ncoords, char **coord,
+                         char **error_mess )
+{
+    char *label;
+    char *units;
+    double *coords_in;
+    double diff;
+    double out1[7];
+    double out2[7];
+    double static_coords_in[7];
+    double tmp;
+    int base;
+    int caxis;
+    int current;
+    int i;
+    int ncoords_in;
+    int ncoords_out;
+    static char buf[256];              /* Static as may be returned */
+    static char lcoord[30];            /* Static as may be returned */
+
+
+    /* 
+     * Check dimensionality, if more than ncoords then pad up, if less 
+     * then drop them off. 
+     */
+    current = astGetI( frameset, "Current" );
+    base = astGetI( frameset, "Base" );
+    astSetI( frameset, "Current", base );
+    ncoords_in = astGetI( frameset, "naxes" );
+    astSetI( frameset, "Current", current );
+    if ( ncoords_in == ncoords ) {
+        coords_in = coords;
+    }
+    else if ( ncoords_in > ncoords ) {
+        coords_in = static_coords_in;
+        memcpy( coords_in, coords, ncoords * sizeof( double ) );
+        for ( i = ncoords; i < ncoords_in; i++ ) {
+            coords_in[i] = AST__BAD;
+        }
+    }
+
+    /*  Transform the position from base coordinates to current coordinates
+     *  for this position, and a position one units offset along the given
+     *  axis. */
+    ncoords_out = astGetI( frameset, "naxes" );
+
+    coords_in[axis-1] += 1.0;
+    astTranN( frameset, 1, ncoords_in, 1, coords_in, 1, ncoords_out, 1, out1 );
+    coords_in[axis-1] -= 1.0;
+    astTranN( frameset, 1, ncoords_in, 1, coords_in, 1, ncoords_out, 1, out2 );
+
+    /*  Select the axis with the largest shift as equivalent */
+    caxis = axis;
+    diff = 0.0;
+    for ( i = 0; i < ncoords_out; i++ ) {
+        tmp = fabs( out1[i] - out2[i] );
+        if ( tmp > diff ) {
+            caxis = i + 1;
+            diff = tmp;
+        }
+    }
+
+    /*  Format the value along that axis, if requested, otherwise just write
+     *  out the double. */
+    if ( formatted ) {
+        *coord = (char *) astFormat( frameset, caxis, out2[caxis - 1] );
+    }
+    else {
+        sprintf( lcoord, "%.17g", out2[caxis -1] );
+        *coord = lcoord;
+    }
+
+    if ( ! astOK ) {
+        astClearStatus;
+        *coord = NULL;
+        *error_mess = strdup( "Failed to convert to world coordinate" );
+        return TCL_ERROR;
+    }
+
+    /*  Add the axis units and label if requested. */
+    if ( trailed ) {
+        sprintf( buf, "unit(%d)", caxis );
+        units = (char *) astGetC( frameset, buf );
+        sprintf( buf, "label(%d)", caxis );
+        label = (char *) astGetC( frameset, buf );
+        sprintf( buf, "%s %s (%s)", *coord, label, units );
+        *coord = buf;
+    }
+    if ( ! astOK ) {
+        astClearStatus;
+    }
+    return TCL_OK;
 }
