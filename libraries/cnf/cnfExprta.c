2cf8b3fa99d9c35589587b899a4fc7a7728fe129
Tim Jenness <t.jenness@jach.hawaii.edu>
2006-04-13 03:46:56 +0000

    Add GPL
    svn path=/trunk/; revision=20669

--- libraries/cnf/cnfExprta.c
+++ libraries/cnf/cnfExprta.c
@@ -52,6 +52,22 @@ void cnfExprta( const char *source_c, int source_len, char *dest_f,
 *     Copyright (C) 1996 Council for the Central Laboratory of the Research
 *     Councils.
 
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*     
+*     This program is distributed in the hope that it will be
+*     useful,but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*     
+*     You should have received a copy of the GNU General Public License
+*     along with this program; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+*     02111-1307, USA
+
 *  Authors:
 *     AJC: Alan Chipperfield (Starlink, RAL)
 *     {enter_new_authors_here}
