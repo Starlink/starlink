2113610cf7e16f535ae0ba1929284cbb02849378
Tim Jenness <t.jenness@jach.hawaii.edu>
2006-04-13 23:36:36 +0000

    add GPL
    svn path=/trunk/; revision=20685

--- libraries/ary/ary_newp.f
+++ libraries/ary/ary_newp.f
@@ -62,6 +62,27 @@
 *     -  Restore the error context, reporting context information if
 *     appropriate.
 
+*  Copyright:
+*     Copyright (C) 1990 Science & Engineering Research Council.
+*     Copyright (C) 1996 Central Laboratory of the Research Councls.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*     
+*     This program is distributed in the hope that it will be
+*     useful,but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*     
+*     You should have received a copy of the GNU General Public License
+*     along with this program; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+*     02111-1307, USA
+
 *  Authors:
 *     RFWS: R.F. Warren-Smith (STARLINK)
 *     BKM:  B.K. McIlwrath    (STARLINK)
--- libraries/ary/ary_test.f
+++ libraries/ary/ary_test.f
@@ -14,6 +14,26 @@
 *     order to test for correct installation. Note that this is not an
 *     exhaustive test of ARY, but only of its installation.
 
+*  Copyright:
+*     Copyright (C) 1991, 1992 Science & Engineering Research Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*     
+*     This program is distributed in the hope that it will be
+*     useful,but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*     
+*     You should have received a copy of the GNU General Public License
+*     along with this program; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+*     02111-1307, USA
+
 *  Authors:
 *     RFWS: R.F. Warren-Smith (STARLINK, RAL)
 *     {enter_new_authors_here}
