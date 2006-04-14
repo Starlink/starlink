6fb440de8c2a2bcf5386ac612b828c69ad1eacc8
Tim Jenness <t.jenness@jach.hawaii.edu>
2006-04-14 04:09:54 +0000

    add GPL
    svn path=/trunk/; revision=20706

--- libraries/graphpar/gkspar/gks1_ass.f
+++ libraries/graphpar/gkspar/gks1_ass.f
@@ -32,6 +32,26 @@
 *     type and connection id isn't already open, pick an unused workstation
 *     id and open and activate the workstation.
 
+*  Copyright:
+*     Copyright (C) 1983, 1985, 1986, 1990, 1992 Science & Engineering Research Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*     
+*     This program is distributed in the hope that it will be
+*     useful,but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*     
+*     You should have received a copy of the GNU General Public License
+*     along with this program; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+*     02111-1307, USA
+
 *  Authors:
 *     SLW: Sid Wright (UCL)
 *     BDK: Dennis Kelly (ROE)
@@ -40,7 +60,7 @@
 *     DLT: David Terrett (Starlink, RAL)
 *     {enter_new_authors_here}
 
-*    History :
+*    History:
 *     11-OCT-1983: (SLW):
 *         Starlink Version.
 *     07-MAR-1985: (BDK):
--- libraries/graphpar/gkspar/gkspar_test.f
+++ libraries/graphpar/gkspar/gkspar_test.f
@@ -1,8 +1,38 @@
       PROGRAM GKSPAR_TEST
+*+
+*  Name:
+*    gkspar_test
+
+*  Purpose:
+*    Simple test to see that the GKS routine are available to be linked 
+*    against.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*     
+*     This program is distributed in the hope that it will be
+*     useful,but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*     
+*     You should have received a copy of the GNU General Public License
+*     along with this program; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+*     02111-1307, USA
+
+*  Copyright:
+*     Copyright (C) 2006 Particle Physics & Engineering Research Council.
+*     All Rights Reserved.
+
+*  Authors:
+*    {enter_authors_here}
+
+*-
       INTEGER STATUS
       STATUS = 0
-
-*  Simple test to see that the GKS routine are available to be linked against.
 
       PRINT *,'This test program only links against the GKSPAR routines'
       PRINT *,'It does not actually call them.'
