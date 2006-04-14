72ea9a3e1a6c70ff2bb4ad12fdc9f6d1261a5f4d
Tim Jenness <t.jenness@jach.hawaii.edu>
2006-04-14 04:01:29 +0000

    fix typo
    svn path=/trunk/; revision=20705

--- libraries/fary/ary_newp.f
+++ libraries/fary/ary_newp.f
@@ -64,7 +64,7 @@
 
 *  Copyright:
 *     Copyright (C) 1990 Science & Engineering Research Council.
-*     Copyright (C) 1996 Central Laboratory of the Research Councls.
+*     Copyright (C) 1996 Central Laboratory of the Research Councils.
 *     All Rights Reserved.
 
 *  Licence:
