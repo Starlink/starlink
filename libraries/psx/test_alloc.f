3f74aaecad17a091238c0156fbb3dc77ad3a02a7
Tim Jenness <t.jenness@jach.hawaii.edu>
2006-04-18 02:04:04 +0000

    add GPL
    svn path=/trunk/; revision=20766

--- libraries/psx/test_alloc.f
+++ libraries/psx/test_alloc.f
@@ -1,6 +1,39 @@
       PROGRAM TEST_ALLOC
+*+
+*  Name:
+*     TEST_ALLOC
 
-* Test subroutines PSX_MALLOC, PSX_REALLOC, PSX_CALLOC and PSX_FREE.
+*  Purpose:
+*     Test subroutines PSX_MALLOC, PSX_REALLOC, PSX_CALLOC and PSX_FREE.
+
+*  Copyright:
+*     Copyright (C) 2006 Particle Physics & Engineering Research Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*     
+*     This program is distributed in the hope that it will be
+*     useful,but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*     
+*     You should have received a copy of the GNU General Public License
+*     along with this program; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+*     02111-1307, USA
+
+*  Authors:
+*     TIMJ: Tim Jenness (JAC, Hawaii)
+
+*  History:
+*     17-APR-2006 (TIMJ):
+*         Add prologue.
+
+*-
 
       IMPLICIT NONE
       INCLUDE 'CNF_PAR'
--- libraries/psx/test_cuserid.f
+++ libraries/psx/test_cuserid.f
@@ -1,6 +1,39 @@
       PROGRAM TEST_CUSERID
+*+
+*  Name:
+*     TEST_CUSERID
 
-* Test the subroutine PSX_CUSERID
+*  Purpose:
+*     Test the subroutine PSX_CUSERID
+
+*  Copyright:
+*     Copyright (C) 2006 Particle Physics & Engineering Research Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*     
+*     This program is distributed in the hope that it will be
+*     useful,but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*     
+*     You should have received a copy of the GNU General Public License
+*     along with this program; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+*     02111-1307, USA
+
+*  Authors:
+*     TIMJ: Tim Jenness (JAC, Hawaii)
+
+*  History:
+*     17-APR-2006 (TIMJ):
+*         Add prologue.
+
+*-
 
       IMPLICIT NONE
       INCLUDE 'SAE_PAR'
