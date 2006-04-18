a4a3f8b22554b16a34195577e6db42ab8238b4c4
Tim Jenness <t.jenness@jach.hawaii.edu>
2006-04-18 23:25:55 +0000

    modern prologue
    svn path=/trunk/; revision=20782

--- libraries/pcs/adam/adam_defns.h
+++ libraries/pcs/adam/adam_defns.h
@@ -1,21 +1,36 @@
-/* ***************************************************************************
- *
- * 	A D A M _ D E F N S . H
- *
- *	Include file for messys layer
- *	The sequence g(et)s(et)o(bey)c(ancel) => gsoc used as part
- *	of messys protocols
- *
- *	History :	Created: irj 15/6/92
- *			Tidied : irj, skr 16/6/92
- *                      Add CONTROL : ajc 6//7/94
- *
- * **************************************************************************** */
+/*
+*+
+*  Name:
+*     adam_defns.h
 
+*  Purpose:
+*     Include file for MESSYS layer
 
-#define NAMELEN 	32			/* length of character strings holding names */
-#define SET		1			/* set a task parameter */
-#define GET		2			/* get a task parameter */
-#define OBEY		3			/* obey a task action */
-#define CANCEL		4			/* cancel a task action in progress */
-#define CONTROL         5                       /* control message for task */
+*  Description:
+*     The sequence g(et)s(et)o(bey)c(ancel) => gsoc used as part
+*     of messys protocols
+
+*  Authors:
+*     IRJ:
+*     SKR:
+*     AJC: Alan Chipperfield (Starlink)
+
+*  History:
+*     15-JUN-1992 (IRJ):
+*        Original
+*     16-JUN-1992 (IRJ):
+*        Tidied
+*     16-JUN-1992 (SKR):
+*        Tidied
+*     07-JUL-1994 (AJC):
+*        Add CONTROL
+
+*-
+*/
+
+#define NAMELEN 	32	/* length of character strings holding names */
+#define SET		1	/* set a task parameter */
+#define GET		2	/* get a task parameter */
+#define OBEY		3	/* obey a task action */
+#define CANCEL		4	/* cancel a task action in progress */
+#define CONTROL         5       /* control message for task */
