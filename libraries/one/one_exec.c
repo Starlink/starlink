372808a60fe92d84911f1e25f3840707bddbb70c
Tim Jenness <t.jenness@jach.hawaii.edu>
2006-04-18 00:47:25 +0000

    add GPL
    svn path=/trunk/; revision=20763

--- libraries/one/one_exec.c
+++ libraries/one/one_exec.c
@@ -34,6 +34,27 @@ F77_SUBROUTINE(one_exec)( CHARACTER(command), INTEGER(status) TRAIL(command) )
 *      STATUS = INTEGER (Given and Returned)
 *         The global status. Set to SAI__ERROR if command fails.
 *
+*  Copyright:
+*     Copyright (C) 1995, 2000, 2004 Central Laboratory of the Research Councils.
+*     Copyright (C) 2006 Particle Physics & Engineering Research Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*     
+*     This program is distributed in the hope that it will be
+*     useful,but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*     
+*     You should have received a copy of the GNU General Public License
+*     along with this program; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+*     02111-1307, USA
+
 *   Authors:
 *      PDRAPER: P.W. Draper (STARLINK - Durham University)
 *      RTP: Roy Platon (STARLINK)
--- libraries/par/par.h
+++ libraries/par/par.h
@@ -1,3 +1,42 @@
+/*
+*+
+*  Name:
+*     par.h
+
+*  Purpose:
+*     C interface to PAR library
+
+*  Copyright:
+*     Copyright (C) 2006 Particle Physics & Engineering Research Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*     
+*     This program is distributed in the hope that it will be
+*     useful,but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*     
+*     You should have received a copy of the GNU General Public License
+*     along with this program; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+*     02111-1307, USA
+
+*  Authors:
+*     TIMJ: Tim Jenness (JAC, Hawaii)
+*     {enter_authors_here}
+
+*  History:
+*     17-APR-2006 (TIMJ):
+*         Add prolog.
+
+*-
+*/
+
 #include "parwrap.h"
 #include "par_par.h"
 #include "par_err.h"
