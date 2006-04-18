f5d5141191e9bfda344b97337eee3864d65d2384
Tim Jenness <t.jenness@jach.hawaii.edu>
2006-04-18 23:16:30 +0000

    add GPL
    svn path=/trunk/; revision=20781

--- libraries/pcs/adam/adm_sendt.f
+++ libraries/pcs/adam/adm_sendt.f
@@ -51,6 +51,26 @@
 *     given timeout to wait for a message from PATH with the same
 *     MESSID.
 
+*  Copyright:
+*     Copyright (C) 1984, 1985, 1986, 1992, 1993 Science & Engineering Research Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*     
+*     This program is distributed in the hope that it will be
+*     useful,but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*     
+*     You should have received a copy of the GNU General Public License
+*     along with this program; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+*     02111-1307, USA
+
 *  Authors:
 *     John Cooke (REVAD::JAC) <date>
 *     Charlie Richardson (REVA::DCR) 5-Mar-85
