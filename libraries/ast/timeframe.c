07cd3c978fca62b6d3c16d4293dc24b4d08f4301
David S. Berry <d.berry@jach.hawaii.edu>
2006-04-19 09:11:00 +0000

    Added GNU Licence section to prologue
    svn path=/trunk/; revision=20789

--- libraries/ast/timeframe.c
+++ libraries/ast/timeframe.c
@@ -70,6 +70,22 @@ f     - AST_CURRENTTIME: Return the current system time
 *  Copyright:
 *     <COPYRIGHT_STATEMENT>
 
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public Licence as
+*     published by the Free Software Foundation; either version 2 of
+*     the Licence, or (at your option) any later version.
+*     
+*     This program is distributed in the hope that it will be
+*     useful,but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public Licence for more details.
+*     
+*     You should have received a copy of the GNU General Public Licence
+*     along with this program; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+*     02111-1307, USA
+
 *  Authors:
 *     NG: Norman Gray (Starlink)
 *     DSB: David Berry (Starlink)
