9d89e59b0ec40690ca76424146837fa8f6c1cb48
Tim Jenness <t.jenness@jach.hawaii.edu>
2006-05-02 23:56:59 +0000

    update prologue
    svn path=/trunk/; revision=21057

--- libraries/nbs/nbs_par.h
+++ libraries/nbs/nbs_par.h
@@ -1,20 +1,25 @@
 /*
- *                              N B S _ P A R . H
- *
- *  Function:
- *    Parameter definitions for the noticeboard system.
+ *+
+ *  Name:
+ *     nbs_par.h
+
+ *  Purpose:
+ *     Parameter definitions for the noticeboard system.
  *
  *  Description:
  *
  *  Language:
- *    C
+ *     C
  *
- *  Support: William Lupton, AAO
+ *  Authors:
+ *     WFL: William Lupton (AAO)
  *
- *  Version date: 02-Mar-1988
- *-
  *  History:
- *    12-Apr-88 - WFL - Use NBS_K_ prefix
+ *    02-MAR-1988 (WFL):
+ *       Original.
+ *    12-Apr-88 (WFL):
+ *       Use NBS_K_ prefix
+ *-
  */
       
 /* Constant parameters */
