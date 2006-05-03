61035e04b1390b553e3934752373c286e0311d51
Tim Jenness <t.jenness@jach.hawaii.edu>
2006-05-03 06:11:55 +0000

    add prologue
    svn path=/trunk/; revision=21067

--- libraries/nbs/nbs_gbl.c
+++ libraries/nbs/nbs_gbl.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
 #include "nbs_typ.h"
 
-/* Modified 28-Jun-04 (Alasdair Allan, Norman Gray, Tim Jenness)
-   Symbols were undefined under Mac OSX and GCC 3.3, defining the pointer
-   to NULL fixed this (int's set to zero just in case). Looks to be from the
-   ISO-9899:1999 standard (section 6.9.2) (commonly refered to as C99) which 
-   declares that:
-   
-    "A declaration of an identifier for an object that has file scope 
-    without an initializer, and without a storage-class specified for with 
-    the storage-class specifier static, constitutes a tentative definition. 
-    If a translation unit contains one or more tentative definitions for an
-    identifier, and the translation unit contains no external definitions for
-    that identifier, then the behaviour is exactly as if the translation unit
-    contains a file scope declaration of that identifier, with the composite
-    type as of the end of the translation unit, with an initializer equal to 0.
+/*
+ *+
+ *  Name:
+ *     nbs_gbl.c
+
+ *  Purpose:
+ *     Define global variables
+
+ *  Authors:
+ *     WFL: William Lupton (RGO)
+ *     AA: Alasdair Allan (Starlink)
+ *     TIMJ: Tim Jenness (JAC, Hawaii)
+ *     NXG: Norman Gray (Starlink)
+
+ *  History:
+ *     XX-XX-19XX (WFL):
+ *        Original.
+ *     28-JUN-2004 (AA/TIMJ/NXG):
+ *        Symbols were undefined under Mac OSX and GCC 3.3, defining the pointer
+ *        to NULL fixed this (int's set to zero just in case). Looks to be from the
+ *        ISO-9899:1999 standard (section 6.9.2) (commonly refered to as C99) which 
+ *        declares that:
+ *   
+ *        "A declaration of an identifier for an object that has file scope 
+ *        without an initializer, and without a storage-class specified for with 
+ *        the storage-class specifier static, constitutes a tentative definition. 
+ *        If a translation unit contains one or more tentative definitions for an
+ *        identifier, and the translation unit contains no external definitions for
+ *        that identifier, then the behaviour is exactly as if the translation unit
+ *        contains a file scope declaration of that identifier, with the composite
+ *        type as of the end of the translation unit, with an initializer equal to 0."
+ *-
 */   
 
 /* External definitions   */
