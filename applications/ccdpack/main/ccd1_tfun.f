dc8d662fc538b66970bbf60cc62e77e5ec859e5e
Tim Jenness <t.jenness@jach.hawaii.edu>
2006-05-06 23:58:50 +0000

    add GPL and prologue
    svn path=/trunk/; revision=21156

--- applications/ccdpack/main/ccd1_tfun.f
+++ applications/ccdpack/main/ccd1_tfun.f
@@ -1,9 +1,60 @@
-***********
-*  Associated routine ccd1_tfun, all parameters passed through common.
-*  This routine basically returns the value which when multiplied by the
-*  integrated intensity of the object gives the intensity at specified
-*  radius squared
-***********
+*+
+*  Name:
+*     CCD1_TFUN
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Invocation:
+*     RES = CCD1_TFUN( RADSQ )
+
+*  Description:
+*     Associated routine ccd1_tfun, all parameters passed through common.
+*     This routine basically returns the value which when multiplied by the
+*     integrated intensity of the object gives the intensity at specified
+*     radius squared
+
+*  Arguments:
+*     RADSQ = INTEGER (Givne)
+*        Radius squared at which the calculation should be performed.
+
+*  Returned Value:
+*     CCD1_TFUN = REAL
+*        Result.
+
+*  Copyright:
+*     Copyright (C) 1997 Central Laboratory of the Research Councils.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public License
+*     along with this program; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+*     02111-1307, USA
+
+*  Authors:
+*     PWD: Peter Draper (Starlink)
+*     {enter_new_authors_here}
+
+*  History:
+*     27-JUN-1997 (PWD):
+*        Original.
+*     {enter_further_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
       REAL FUNCTION CCD1_TFUN( RADSQ )
       IMPLICIT NONE              ! no implicit typing
 
