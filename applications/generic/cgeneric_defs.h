1f28b15f57f4b4ebb44b042a1db500b43bfe8d7a
Tim Jenness <t.jenness@jach.hawaii.edu>
2006-05-12 06:08:04 +0000

    add GPL
    svn path=/trunk/; revision=21250

--- applications/generic/cgeneric_defs.h
+++ applications/generic/cgeneric_defs.h
@@ -15,7 +15,7 @@
 *  Description:
 *     See the description in "cgeneric.h". This is the file 
 *     "cgenerics_defs.h". This include file defines the macros:
-* 
+*
 *        CGEN_TYPE
 *        CGEN_HDS_TYPE
 *        CGEN_CODE
@@ -23,6 +23,26 @@
 *
 *     To match the type declared by the current value of CGEN_CODE_TYPE.
 *     See "cgeneric.h" for the possible values.
+
+*  Copyright:
+*     Copyright (C) 2005 Particle Physics & Astronomy Research Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public License
+*     along with this program; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+*     02111-1307, USA
 
 *  Authors:
 *     PWD: Peter W. Draper (JAC, Durham University)
@@ -35,10 +55,6 @@
 *     25-OCT-2005 (DSB):
 *        Added CGEN_HDS_TYPE.
 *     {enter_further_changes_here}
-*        
-
-*  Bugs:
-*     {note_any_bugs_here}
 
 *-
 ------------------------------------------------------------------------------
