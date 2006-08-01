09fc9704a3c6d5e684e23d6fe7162f12a986c58d
Jen Balfour <j.balfour@jach.hawaii.edu>
2006-08-01 18:30:07 +0000

    From dsim_addpnoise
    svn path=/trunk/; revision=22219

---
+++ applications/smurf/libsc2sim/sc2sim_addpnoise.c
@@ -0,0 +1,116 @@
+/*
+*+
+*  Name:
+*     sc2sim_addpnoise
+
+*  Purpose:
+*     Calculate photon noise and add to a flux.
+
+*  Language:
+*     Starlink ANSI C
+
+*  Type of Module:
+*     Subroutine
+
+*  Invocation:
+*     sc2sim_addpnoise( double lambda, double bandGHz, double aomega, 
+*                       double integ_time, double *flux, int *status )
+
+*  Arguments:
+*     lambda = double (Given)
+*        Wavelength in metres
+*     bandGHz = double (Given)
+*        Bandwidth in GHz (Given)
+*     aomega = double (Given)
+*        Geometrical optical factor
+*     integ_time = double (Given)
+*        Effective integration time in sec
+*     flux = double* (Given and Returned)
+*        Flux value in pW
+*     status = int* (Given and Returned)
+*        Pointer to global status.
+
+*  Description:
+*     Calculate the photon equivalent of the given flux allowing the
+*     corresponding photon noise sigma to be calculated. Use this to
+*     scale a generated random number.
+
+*  Authors:
+*     B.D.Kelly (ROE)
+*     {enter_new_authors_here}
+
+*  History :
+*     2001-07-26 (BDK):  
+*        Original
+*     2002-08-19 (BDK):  
+*        C version
+*     2003-05-22 (BDK):
+*        Multiply photon noise by 1.5
+*     2004-01-23 (BDK):  
+*        Use dsim_getsigma 
+*     2006-07-19 (JB):
+*        Split from dsim.c
+
+*  Copyright:
+*     Copyright (C) 2005-2006 Particle Physics and Astronomy Research
+*     Council. University of British Columbia. All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public
+*     License along with this program; if not, write to the Free
+*     Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+*     MA 02111-1307, USA
+
+*  Bugs:
+*     {note_any_bugs_here}
+*-
+*/
+
+/* Standard includes */
+#include <math.h>
+
+/* SC2SIM includes */
+#include "sc2sim.h"
+#include "dream.h"
+#include "dream_par.h"
+
+void sc2sim_addpnoise 
+(
+double lambda,       /* wavelength in metres (given) */
+double bandGHz,      /* bandwidth in GHZ (given) */
+double aomega,       /* geometrical optical factor (given) */
+double integ_time,   /* Effective integration time in sec (given) */
+double *flux,        /* Flux value in pW (given and returned) */
+int *status          /* global status (given and returned) */
+)
+
+{
+
+   double err;                  /* error offset */
+   double sigma;                /* NEP in pW */
+
+   /* Check status */
+   if ( !StatusOkP(status) ) return;
+
+   /* Calculate the dispersion due to photon noise */
+   sc2sim_getsigma ( lambda, bandGHz, aomega, *flux, 
+                     &sigma, status );
+ 
+   /* Calculate a random number and scale it to the required sigma */
+   err = sc2sim_drand ( sigma );
+
+   /* Correct for the integration time - factor 2 because sigma is per root
+      Hz */
+   *flux = *flux + err / sqrt(2.0*integ_time);
+
+}//sc2sim_addpnoise
