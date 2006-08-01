fd3ff252024ea1284895c7d5ec3b156ea98a3af3
Jen Balfour <j.balfour@jach.hawaii.edu>
2006-08-01 18:48:14 +0000

    From dsim_par.h
    svn path=/trunk/; revision=22259

---
+++ applications/smurf/libsc2sim/sc2sim_par.h
@@ -0,0 +1,9 @@
+/*  sc2sim_par.h - constants for DREAM simulator */
+
+/*  History :
+     16Aug2002: original (bdk)
+     16Jun2003: move NWEIGHTS to dream_par.h (bdk)
+     04Feb2005: remove TELEMISSION, ASTSCALE, ASTSIZE, ATMSCALE, ATMSIZE (bdk)
+*/
+
+#define NCOEFFS 6         /* no. of coeffs for bolometer response */
