8e459926a9f2d9a88180e968e13435bc71b7828f
Jen Balfour <j.balfour@jach.hawaii.edu>
2006-08-01 18:32:06 +0000

    From dsim_calctime
    svn path=/trunk/; revision=22224

---
+++ applications/smurf/libsc2sim/sc2sim_calctime.c
@@ -0,0 +1,130 @@
+/*
+*+
+*  Name:
+*     sc2sim_calctime
+
+*  Purpose:
+*     Calculte UT + LST arrays given a start time
+
+*  Language:
+*     Starlink ANSI C
+
+*  Type of Module:
+*     Subroutine
+
+*  Invocation:
+*     sc2sim_calctime ( double mjdaystart, double samptime, int nsamp, 
+                        double *ut, double *lst, int *status )
+
+*  Arguments:
+*     mjdaystart = double (Given)
+*        Start time as amodified juldate 
+*     samptime = double (Given)
+*        Length of sample in seconds 
+*     nsamp = int (Given)
+*        Number of samples
+*     ut = double* (Returned)
+*        UT at each sample (mod. juldate)
+*     lst = double* (Returned)
+*        LST at each sample (radians)
+*     status = int* (Given and Returned)
+*        Pointer to global status.
+
+*  Description:
+*     Given a start time and number of samples, calculate the UT and LST at
+*     each sample.
+
+*  Authors:
+*     E.Chapin (UBC)
+*     {enter_new_authors_here}
+
+*  History :
+*     2006-02-23 (EC):
+*        Original
+*     2006-07-20 (JB):
+*        Split from dsim.c
+
+*  Copyright:
+*     Copyright (C) 2005-2006 Particle Physics and Astronomy Research
+*     Council. University of British Columbia. All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public
+*     License along with this program; if not, write to the Free
+*     Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+*     MA 02111-1307, USA
+
+*  Bugs:
+*     {note_any_bugs_here}
+*-
+*/
+
+/* Standard includes */
+#include <math.h>
+
+/* Starlink includes */
+#include "ast.h"
+
+/* SC2SIM includes */
+#include "sc2sim.h"
+
+void sc2sim_calctime
+( 
+double mjdaystart,   /* start time as modified juldate */
+double samptime,     /* length of a sample in seconds */
+int nsamp,           /* number of samples */
+double *ut,          /* returned UT at each sample (mod. juldate) */
+double *lst,         /* returned LST at each sample (radians) */
+int *status          /* global status (given and returned) */
+)
+
+{
+
+   /* Local variables */
+   double gst;
+   int i;
+   double lon;
+   double tu;
+   double sampday;
+
+   /* Check status */
+   if ( !StatusOkP(status) ) return;
+  
+   /* JCMT longitude in radians */
+   lon = ( 155.0 + (28.0/60.0) + (0.0/3600.0) ) / AST__DR2D;
+
+   /* Length of a single sample in days */
+   sampday = samptime/(3600. * 24.);
+ 
+   /* Loop over each time step, calculate UT and then calculate 
+      Greenwich sidereal time using slalib routine slaGmst and convert
+      to local sidereal time */
+  
+   for(i=0; i<nsamp; i++) {
+      ut[i] = mjdaystart + ((double) i)*sampday;
+
+      /* Julian centuries from fundamental epoch J2000 to this UT */
+      tu = ( ut[i] - 51544.5 ) / 36525.0;
+
+      /* GMST at this UT */
+      gst = dmod ( ut[i], 1.0 ) * D2PI +
+         ( 24110.54841 +
+	    ( 8640184.812866 +
+	  (      0.093104 - 6.2e-6 * tu ) * tu ) * tu ) * DS2R;
+    
+      /* Calculate LST from GMST using telescope longitude */
+      lst[i] = fmod(gst - lon + D2PI,D2PI);
+   }//for
+
+}//sc2sim_calctime
+
