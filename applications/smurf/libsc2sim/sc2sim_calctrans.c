486b454904a7e41560114c540971fa50bc4dd6a6
Jen Balfour <j.balfour@jach.hawaii.edu>
2006-08-01 18:32:22 +0000

    From dsim_calctrans
    svn path=/trunk/; revision=22225

---
+++ applications/smurf/libsc2sim/sc2sim_calctrans.c
@@ -0,0 +1,98 @@
+/*
+*+
+*  Name:
+*     sc2sim_calctrans
+
+*  Purpose:
+*     Calculate sky zenith % atmospheric transmission
+
+*  Language:
+*     Starlink ANSI C
+
+*  Type of Module:
+*     Subroutine
+
+*  Invocation:
+*     sc2sim_calctrans ( double lambda, double *trans, double tauCSO, 
+*                        int *status )
+
+*  Arguments:
+*     lambda = double (Given)
+*        Wavelength in metres
+*     trans = double* (Returned)
+*        % Atmospheric transmission
+*     tauCSO = double (Given)
+*        CSO optical depth
+*     status = int* (Given and Returned)
+*        Pointer to global status.
+
+*  Description:
+*     Calculate the atmospheric transmission equivalent to the given 
+*     optical depth for this wavelength band.  This is essentially the
+*     opposite of dsim_calctau.
+
+
+*  Authors:
+*     J.Balfour (UBC) 
+*     {enter_new_authors_here}
+
+*  History :
+*     2006-06-19 (JB):
+*        Original
+*     2006-07-20 (JB):
+*        Split from dsim.c
+
+*  Copyright:
+*     Copyright (C) 2005-2006 Particle Physics and Astronomy Research
+*     Council. University of British Columbia. All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public
+*     License along with this program; if not, write to the Free
+*     Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+*     MA 02111-1307, USA
+
+*  Bugs:
+*     {note_any_bugs_here}
+*-
+*/
+
+/* Standard includes */
+#include <math.h>
+
+/* Starlink includes */
+#include "ast.h"
+
+/* SC2SIM includes */
+#include "sc2sim.h"
+
+void sc2sim_calctrans
+(
+double lambda,        /* wavelength in metres (given) */
+double *trans,        /* % atmospheric transmission (returned) */
+double tauCSO,        /* CSO optical depth (given) */
+int *status           /* global status (given and returned) */
+)
+
+{
+   /* Check status */
+   if ( !StatusOkP(status) ) return;
+
+   if ( fabs ( lambda - 0.45e-3 ) < 0.1e-3 ) {
+      *trans = 100 / exp ( 26.2 * ( tauCSO - 0.014 ) );
+   } else {
+      *trans = 100 / exp ( 4.02 * ( tauCSO - 0.001 ) );
+   }//if-else
+
+}//sc2sim_calctrans
+
