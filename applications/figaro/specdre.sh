da22a08f92607ac6a33dea0d02d3f17aa7901a3f
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2006-08-31 02:12:00 +0000

    initial addition
    svn path=/branches/keoe/; revision=22517

---
+++ applications/figaro/specdre.sh
@@ -0,0 +1,43 @@
+#+
+#  Name:
+#     specdre.sh
+#
+#  Purpose:
+#     Start the Specdre package from the sh shell.
+#
+#  Type of Module:
+#     sh shell command list
+#
+#  Invocation:
+#     source specdre.ssh
+#
+#  Description:
+#     This procedure used to start the Specdre package for use from the 
+#     sh shell by defining the aliases needed to execute each application.
+#     Since Specdre has been merged with Figaro, this procedure now tells
+#     the user to invoke Figaro.
+#
+#  Authors:
+#     BC: Brad Cavanagh (JAC, Hawaii)
+#     {enter_new_authors_here}
+#
+#  History:
+#     30 AUG 2006 (BC):
+#         Original version, as adapted from specdre.csh
+#     {enter_changes_here}
+#
+#-
+#.
+echo " "
+echo "        Specdre has been merged with Figaro"
+echo " "
+echo "  In order to use the Specdre commands, use Figaro"
+echo " "
+echo "        "Type \"fighelp specdre\" for help
+echo " "
+#
+# end
+#
+
+
+
