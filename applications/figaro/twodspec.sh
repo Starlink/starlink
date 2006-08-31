7592f70b69d18abbe01f15ff5104f269b72ba037
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2006-08-31 02:12:00 +0000

    initial addition
    svn path=/trunk/; revision=22517

---
+++ applications/figaro/twodspec.sh
@@ -0,0 +1,40 @@
+#+
+#  Name:
+#     twodspec.sh
+#
+#  Purpose:
+#     Define the TWODSPEC commands from the sh shell.
+#
+#  Type of Module:
+#     sh shell script.
+#
+#  Invocation:
+#     source ${TWODSPEC_DIR}/twodspec.sh
+#
+#  Description:
+#     This procedure defines the TWODSPEC commands for use from the 
+#     sh shell by defining the aliases for each command.
+#
+#  Authors:
+#     BC: Brad Cavanagh (JAC, Hawaii)
+#     {enter_new_authors_here}
+#
+#  History:
+#     30 AUG 2006 (BC):
+#        Original version, adapted from twodspec.csh.
+#     {enter_changes_here}
+#-
+#
+echo " "
+echo "        Twodspec has been merged with Figaro"
+echo " "
+echo "  In order to use the Twodspec commands, use Figaro"
+echo " "
+echo "        "Type \"fighelp twodspec\" for help
+echo " "
+#
+# end
+#
+
+
+
