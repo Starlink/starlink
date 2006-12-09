3315a64b21f4496f3b1fef21e6b2aa960c4b8deb
Jen Balfour <j.balfour@jach.hawaii.edu>
2006-12-09 00:59:41 +0000

    Time is already for beginning of this file
    svn path=/trunk/; revision=23174

--- applications/smurf/libsc2sim/sc2sim_dateobs.c
+++ applications/smurf/libsc2sim/sc2sim_dateobs.c
@@ -13,18 +13,11 @@
 *     SC2SIM subroutine
 
 *  Invocation:
-*     sc2sim_dateobs ( double mjdaystart, int maxwrite, double sample_t,
-*                      int outscan, char *dateobs, int *status);
+*     sc2sim_dateobs ( double mjdaystart, char *dateobs, int *status);
 
 *  Arguments:
 *     mjdaystart = double (Given)
 *        MJD corresponding to first sample in file (TAI)
-*     maxwrite = int (Given)
-*        Maximum number of samples to write per output file
-*     sample_t = double (Given)
-*        Sample integration time in milliseconds
-*     outscan = int (Given)
-*        Sequence number for current output file
 *     dateobs = char* (Given and Returned)
 *        Dateobs string
 *     status = int* (Given and Returned)
@@ -32,9 +25,8 @@
 
 *  Description:
 *      This routine constructs the DATE-OBS string for the FITS header
-*      in the output files. The necessary inputs are the MJD at the
-*      start of the file, the integration time per sample and the
-*      sequence number of the current file. Note that the MJD is on
+*      in the output files. The necessary input is the MJD at the
+*      start of the file. Note that the MJD is on
 *      the TAI time scale.
 
 *  Notes:
@@ -51,6 +43,8 @@
 *        Original version 
 *     2006-12-01 (AGG):
 *        String written with a single sprintf
+*     2006-12-08 (JB):
+*        Date for current chunk is precalculated by calling function.
 *     {enter_further_changes_here}
 
 *  Copyright:
@@ -101,10 +95,8 @@
 #define FUNC_NAME "sc2sim_dateobs"
 #define LEN__METHOD 20
 
-void sc2sim_dateobs ( double mjdaystart, int maxwrite, double sample_t, 
-		      int outscan, char *dateobs, int *status ) {
+void sc2sim_dateobs ( double mjdaystart, char *dateobs, int *status ) {
 
-  double filestarttime;       /* MJD corresponding to first sample in output file */
   int yy;                     /* Year for output file start time */
   int mm;                     /* Month for output file start time */
   int dd;                     /* Day for output file start time */
@@ -115,14 +107,8 @@ void sc2sim_dateobs ( double mjdaystart, int maxwrite, double sample_t,
 
   if ( *status != SAI__OK) return;
 
-  /* Calculate DATE-OBS for current file */
-  /* Remember sample_t is in milliseconds - hence the 1e-3
-     factor - and convert to a day fraction by dividing by the
-     number of seconds in 24 hours */
-  filestarttime = mjdaystart + (double)(maxwrite*sample_t*outscan*1.0e-3)/86400.0;
-
   /* Convert this MJD to something more readable */
-  slaDjcl( filestarttime, &yy, &mm, &dd, &df, &date_status );
+  slaDjcl( mjdaystart, &yy, &mm, &dd, &df, &date_status );
 
   /* Convert day fraction to hh:mm:ss */
   slaDd2tf( 3, df, sign, ihmsf );
