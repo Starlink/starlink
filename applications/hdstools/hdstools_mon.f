c2d11d454f1babbec7864617639514f9e823554a
Brad Cavanagh <b.cavanagh@jach.hawaii.edu>
2007-02-03 00:22:59 +0000

    Add GRP and HDS locator tracking code.
    svn path=/trunk/; revision=23541

--- applications/hdstools/hdstools_mon.f
+++ applications/hdstools/hdstools_mon.f
@@ -27,11 +27,14 @@
 
 *  Authors:
 *     AJC: A.J.Chipperfield (Starlink, RAL)
+*     BC:  Brad Cavanagh (Joint Astronomy Centre, Hawaii)
 *     {enter_new_authors_here}
 
 *  History:
 *     18-SEP-2001 (AJC):
 *        Original version.
+*     02-FEB-2007 (BC):
+*        Add GRP and HDS locator tracking code.
 *     {date} ({author_identifier}):
 *        {changes}
 *     {enter_further_changes_here}
@@ -47,21 +50,48 @@
 
 *  Global Constants:
       INCLUDE 'SAE_PAR'         ! Standard SAE constants
+      INCLUDE 'GRP_PAR'         ! GRP definitions
+      INCLUDE 'DAT_PAR'         ! DAT definitions
       INCLUDE 'PAR_PAR'         ! PAR_ public constants
 
 *  Status:
       INTEGER STATUS            ! Global status
 
+*  External References:
+      INTEGER CHR_LEN             ! Length of string
+
 *  Local Variables:
       CHARACTER * ( PAR__SZNAM ) NAME ! Action name
+      CHARACTER FILTER*( 60 )     ! HDS_INFOI filter string
+      INTEGER IPOSN               ! Position in string
+      INTEGER LSTAT               ! Local status
+      INTEGER NGRP0               ! Number of active GRP groups on entry
+      INTEGER NGRP1               ! Number of active GRP groups on exit
+      INTEGER NLOC0               ! Number of active locators on entry
+      INTEGER NLOC1               ! Number of active locators on exit
 
 *.
 
 *  Check inherited global status.
       IF ( STATUS .NE. SAI__OK ) RETURN
 
+*  Note the current number of active GRP identifiers.
+      CALL GRP_INFOI( GRP__NOID, 0, 'NGRP', NGRP0, STATUS )
+
 *  Get the action name.
       CALL TASK_GET_NAME( NAME, STATUS )
+
+*  Need to construct a string to filter out parameter locators
+*  from the list of active locators. Need monolith and taskname
+*  excluded. Also seem to need HDS_SCRATCH excluded.
+      FILTER = '!HDSTOOLS_MON,!HDS_SCRATCH,!GLOBAL,!'
+      IPOSN = CHR_LEN( FILTER )
+      CALL CHR_APPND(NAME, FILTER, IPOSN)
+
+*  Note the current number of active locators (excluding parameter
+*  system
+      CALL HDS_INFOI( DAT__NOLOC, 'LOCATORS', FILTER,
+     :     NLOC0, STATUS )
 
 *  Test the action name against each valid value in turn, calling the
 *  appropriate routine...
@@ -124,5 +154,52 @@
      :        'not recognised by the {routine_name} monolith.',
      :        STATUS )
       END IF
+
+*  Note the current number of active GRP identifiers. Do this in a new
+*  error reporting context so that we get the correct value even if an
+*  error has occurred.
+      CALL ERR_BEGIN( STATUS )
+      CALL GRP_INFOI( GRP__NOID, 0, 'NGRP', NGRP1, STATUS )
+
+*  If there are more active groups now than there were on entry, there
+*  must be a problem (GRP identifiers are not being freed somewhere). So
+*  report it.
+      IF( STATUS .EQ. SAI__OK .AND. NGRP1 .GT. NGRP0 ) THEN
+         CALL MSG_BLANK( STATUS )
+         CALL MSG_SETC( 'NAME', NAME )
+         CALL MSG_SETI( 'NGRP0', NGRP0 )
+         CALL MSG_SETI( 'NGRP1', NGRP1 )
+         CALL MSG_OUT( 'HDS_NGRP', 'WARNING: The number of active '//
+     :             'GRP identifiers increased from ^NGRP0 to ^NGRP1 '//
+     :             'during execution of ^NAME (HDSTOOLS programming '//
+     :             'error).', STATUS )
+         CALL MSG_BLANK( STATUS )
+      END IF
+
+*  Note the current number of active locators. Do this in a new
+*  error reporting context so that we get the correct value even if an
+*  error has occurred.
+      CALL ERR_MARK
+      LSTAT = SAI__OK
+      CALL HDS_INFOI( DAT__NOLOC, 'LOCATORS', FILTER,
+     :     NLOC1, LSTAT )
+
+*  If there are more active locators now than there were on entry, there
+*  must be a problem (HDS locators are not being freed somewhere). So
+*  report it.
+      IF( LSTAT .EQ. SAI__OK .AND. NLOC1 .GT. NLOC0 ) THEN
+         CALL MSG_BLANK( LSTAT )
+         CALL MSG_SETC( 'NAME', NAME )
+         CALL MSG_SETI( 'NLOC0', NLOC0 )
+         CALL MSG_SETI( 'NLOC1', NLOC1 )
+         CALL MSG_OUT( 'KAPPA_NLOC', 'WARNING: The number of active '//
+     :             'HDS locators increased from ^NLOC0 to ^NLOC1 '//
+     :             'during execution of ^NAME (HDSTOOLS programming '//
+     :             'error).', LSTAT )
+         CALL MSG_BLANK( LSTAT )
+      END IF
+      IF (LSTAT .NE. SAI__OK) CALL ERR_ANNUL( LSTAT )
+
+      CALL ERR_RLSE
 
       END
