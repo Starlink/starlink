6a0165442bf83db81c307aca2e6fa8a7c6f26068
Peter W. Draper <P.W.Draper@durham.ac.uk>
2007-03-21 14:43:04 +0000

    Update to include latest CADC catalogues, JCMT archive and fix CADC NED access problem
    svn path=/trunk/; revision=23845

--- applications/gaia/gaia/library/Gaia.tcl
+++ applications/gaia/gaia/library/Gaia.tcl
@@ -1865,7 +1865,7 @@ itcl::class gaia::Gaia {
    #  The match string should be set to something new in the
    #  default file.
    public proc check_config_file { config_file } {
-      set newmatch "*UCAC2*"
+      set newmatch "*JCMT*"
 
       #  Search the file for the string match.
       set ok 0
