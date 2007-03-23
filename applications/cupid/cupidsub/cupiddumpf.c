6363dbf697c7d880e2c1b18b27c65cf873ef310b
David S. Berry <d.berry@jach.hawaii.edu>
2007-03-23 10:25:06 +0000

    Add cupidDumpF diagnostic function
    svn path=/trunk/; revision=23865

---
+++ applications/cupid/cupidsub/cupiddumpf.c
@@ -0,0 +1,99 @@
+#include "sae_par.h"
+#include "prm_par.h"
+#include "ndf.h"
+#include "ast.h"
+#include "dat_par.h"
+#include "cupid.h"
+#include <stdio.h>
+
+void cupidDumpF( float *array, int ndim, int *dims, int *slbnd,
+         int *status ){
+/*
+*+
+*  Name:
+*     cupidDumpF
+
+*  Purpose:
+*     Dump a float array.
+
+*  Language:
+*     Starlink C
+
+*  Synopsis:
+*     void cupidDumpF( float *array, int ndim, int *dims, int *slbnd,
+*        int *status )
+
+*  Description:
+*     This function is a diagnostic function which dumps the supplied
+*     array.
+
+*  Parameters:
+*     array
+*        Pointer to the array to be dumped. 
+*     ndim
+*        The number of pixel axes.
+*     dims
+*        Pointer to the size of each pixel axis.
+*     slbnd
+*        Pointer to the lower pixel bounds of each pixel axis.
+*     status
+*        Pointer to the inherited status value.
+
+*  Copyright:
+*     Copyright (C) 2005 Particle Physics & Astronomy Research Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public License
+*     along with this program; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+*     02111-1307, USA
+
+*  Authors:
+*     DSB: David S. Berry
+*     {enter_new_authors_here}
+
+*  History:
+*     7-DEC-2005 (DSB):
+*        Original version.
+*     {enter_further_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+*/
+
+/* Local Variables: */
+
+   float *adata;
+   int indf, place, i, el, lbnd[3], ubnd[3];
+   static int jj = 0;
+   char name[ 100 ];
+
+   for( i = 0; i < ndim; i++ ) {
+      lbnd[ i ] = slbnd[ i ];
+      ubnd[ i ] = dims[ i ] + slbnd[ i ] - 1;
+   }
+
+   jj++;
+   sprintf( name, "ddata%d", jj );
+   printf("   Dumping %s\n", name );
+   ndfOpen( NULL, name, "WRITE", "NEW", &indf, &place, status );
+   ndfNew( "_REAL", ndim, lbnd, ubnd, &place, &indf, status );
+   ndfMap( indf, "DATA", "_REAL", "WRITE", (void *) &adata, &el, status );
+   if( adata ) {
+      for( i = 0; i < el; i++ ) adata[ i ] = array[ i ];
+   }
+   ndfAnnul( &indf, status );
+}
