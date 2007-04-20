bdad88cda15545b58bf8ccabbb4bbeaab4613c0f
Peter W. Draper <P.W.Draper@durham.ac.uk>
2007-04-20 11:46:26 +0000

    Add test to check C and Fortran D1MACH values.
    svn path=/trunk/; revision=24068

---
+++ libraries/pda/d1machtest.f
@@ -0,0 +1,23 @@
+      PROGRAM D1MACHTEST
+      
+      DOUBLE PRECISION PDA_D1MACH
+      DOUBLE PRECISION PDA_D1MACH_C
+      INTEGER I
+      LOGICAL PASS
+
+      PRINT *, 'Test of D1MACH C replacement'
+      PASS = .TRUE.
+      DO I = 1, 5
+         PRINT *,' Fortran:', PDA_D1MACH( I ), ' -> ', 
+     :           'C:', PDA_D1MACH_C( I )
+         IF ( PDA_D1MACH( I ) .NE. PDA_D1MACH_C( I ) ) THEN
+            PASS = .FALSE.
+            PRINT *, 'failed'
+         END IF
+      END DO
+      IF ( .NOT. PASS ) THEN 
+         PRINT *, 'FAILED'
+      ELSE
+         PRINT *, 'PASSED'
+      END IF
+      END
