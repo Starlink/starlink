85bc2cd1294f911b5e5b611481e6bd6a42c09664
Peter W. Draper <P.W.Draper@durham.ac.uk>
2008-03-04 15:51:20 +0000

    gaia: remove checks for cubes from control image level, refactored to Gaia level (more efficient access)
    svn path=/trunk/; revision=26235

--- applications/gaia/gaia/library/GaiaHduChooser.tcl
+++ applications/gaia/gaia/library/GaiaHduChooser.tcl
@@ -202,29 +202,8 @@ itcl::class gaia::GaiaHduChooser {
       }
    }
 
-   #  Override to trap cubes and direct these to the correct handler.
-   protected method select_image_hdu {hdu} {
-
-      #  Move to the HDU.
-      RtdImageHduChooser::select_image_hdu $hdu
-
-      #  Issue check for cubes command, if given and applicable.
-      if { $itk_option(-cube_cmd) != {} } {
-         set headings [$image_ hdu listheadings]
-         eval lassign [list [lindex [$image_ hdu list] [expr {$hdu -1}] ]] $headings
-         if { "$Type" == "image" && "$NAXIS" >= 3 } {
-            #  Make sure this is syncronised.
-            ::update
-            eval $itk_option(-cube_cmd)
-         }
-      }
-   }
-
    #  Configuration options: (public variables)
    #  ----------------------
-
-   #  Command to execute if a cube is found. Issued after loading.
-   itk_option define -cube_cmd cube_cmd Cube_Cmd {}
 
    #  Protected variables: (available to instance)
    #  --------------------
