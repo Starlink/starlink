0783dd60513612e218aeb665d380722a105cffe0
Malcolm J. Currie <mjc@star.rl.ac.uk>
2008-04-07 20:55:56 +0000

    Wrap long lines.
    svn path=/trunk/; revision=26587

--- applications/kappa/libkapview/pictrans.f
+++ applications/kappa/libkapview/pictrans.f
@@ -4,8 +4,8 @@
 *     PICTRANS
 
 *  Purpose:
-*     Transform a graphics position from one picture co-ordinate Frame to 
-*     another.
+*     Transforms a graphics position from one picture co-ordinate Frame 
+*     to another.
 
 *  Language:
 *     Starlink Fortran 77
@@ -21,77 +21,79 @@
 *        The global status.
 
 *  Description:
-*     This application transforms a position on a graphics device from one 
-*     co-ordinate Frame to another. The input and output Frames may be chosen 
-*     freely from the Frames available in the WCS information stored with
-*     the current picture in the AGI graphics database. The transformed 
-*     position is formatted for display and written to the screen and 
-*     also to an output parameter.
+*     This application transforms a position on a graphics device from 
+*     one co-ordinate Frame to another.  The input and output Frames may
+*     be chosen freely from the Frames available in the WCS information
+*     stored with the current picture in the AGI graphics database.  The
+*     transformed position is formatted for display and written to the 
+*     screen and also to an output parameter.
 
 *  Usage:
 *     pictrans posin framein [frameout] [device]
 
 *  ADAM Parameters:
 *     BOUND = _LOGICAL (Write)
-*        BOUND is TRUE when the supplied point lies within the bounds of 
+*        BOUND is TRUE when the supplied point lies within the bounds of
 *        the current picture.
 *     DEVICE = DEVICE (Read)
-*        The graphics workstation. [The current graphics device]
+*        The graphics workstation.  [The current graphics device]
 *     EPOCHIN = _DOUBLE (Read)
 *        If a "Sky Co-ordinate System" specification is supplied (using 
-*        parameter FRAMEIN) for a celestial co-ordinate system, then an epoch 
-*        value is needed to qualify it. This is the epoch at which the 
-*        supplied sky position was determined. It should be given as a 
-*        decimal years value, with or without decimal places  ("1996.8" for 
-*        example). Such values are interpreted as a Besselian epoch if less 
-*        than 1984.0 and as a Julian epoch otherwise. 
+*        parameter FRAMEIN) for a celestial co-ordinate system, then an 
+*        epoch value is needed to qualify it.  This is the epoch at 
+*        which the supplied sky position was determined.  It should be 
+*        given as a decimal years value, with or without decimal places 
+*        ("1996.8" for example).  Such values are interpreted as a 
+*        Besselian epoch if less than 1984.0 and as a Julian epoch 
+*        otherwise. 
 *     EPOCHOUT = _DOUBLE (Read)
 *        If a "Sky Co-ordinate System" specification is supplied (using 
-*        parameter FRAMEOUT) for a celestial co-ordinate system, then an epoch 
-*        value is needed to qualify it. This is the epoch at which the 
-*        transformed sky position is required. It should be given as a 
-*        decimal years value, with or without decimal places  ("1996.8" for 
-*        example). Such values are interpreted as a Besselian epoch if less 
-*        than 1984.0 and as a Julian epoch otherwise. 
+*        parameter FRAMEOUT) for a celestial co-ordinate system, then an
+*        epoch value is needed to qualify it.  This is the epoch at
+*        which the transformed sky position is required.  It should be 
+*        given as a decimal years value, with or without decimal places 
+*        ("1996.8" for example).  Such values are interpreted as a 
+*        Besselian epoch if less than 1984.0 and as a Julian epoch
+*        otherwise. 
 *     FRAMEIN = LITERAL (Read)
 *        A string specifying the co-ordinate Frame in which the input
-*        position is supplied (see parameter POSIN). The string can be 
+*        position is supplied (see parameter POSIN).  The string can be 
 *        one of the following:
 *
-*        - A domain name such as SKY, AXIS, PIXEL, GRAPHICS, NDC, CURPIC, 
-*        BASEPIC, CURNDC, etc. 
+*        - A domain name such as SKY, AXIS, PIXEL, GRAPHICS, NDC, 
+*        CURPIC, BASEPIC, CURNDC, etc. 
 *
-*        - An integer value giving the index of the required Frame within
-*        the WCS component.
+*        - An integer value giving the index of the required Frame 
+*        within the WCS component.
 *
-*        - A "Sky Co-ordinate System" (SCS) value such as EQUAT(J2000) (see 
-*        section "Sky Co-ordinate Systems" in SUN/95).
+*        - A "Sky Co-ordinate System" (SCS) value such as EQUAT(J2000) 
+*        (see  section "Sky Co-ordinate Systems" in SUN/95).
 *
-*        If a null parameter value is supplied, then the current Frame in 
-*        the current picture is used. [!]
+*        If a null parameter value is supplied, then the current Frame 
+*        in the current picture is used.  [!]
 *     FRAMEOUT = LITERAL (Read)
-*        A string specifying the co-ordinate Frame in which the transformed
-*        position is required. If a null parameter value is supplied, then 
-*        the current Frame in the picture is used. The string can be one of the 
-*        following:
+*        A string specifying the co-ordinate Frame in which the 
+*        transformed position is required.  If a null parameter value is
+*        supplied, then the current Frame in the picture is used.  The 
+*        string can be one of the following options.
 *
-*        - A domain name such as SKY, AXIS, PIXEL, GRAPHICS, NDC, CURPIC, 
-*        BASEPIC, CURNDC, etc. 
+*        - A domain name such as SKY, AXIS, PIXEL, GRAPHICS, NDC,
+*        CURPIC, BASEPIC, CURNDC, etc. 
 *
-*        - An integer value giving the index of the required Frame within
-*        the WCS component.
+*        - An integer value giving the index of the required Frame
+*        within the WCS component.
 *
-*        - A "Sky Co-ordinate System" (SCS) value such as EQUAT(J2000) (see 
-*        section "Sky Co-ordinate Systems" in SUN/95). 
+*        - A "Sky Co-ordinate System" (SCS) value such as EQUAT(J2000) 
+*        (see section "Sky Co-ordinate Systems" in SUN/95). 
 *
-*        If a null parameter value is supplied, then the BASEPIC Frame is 
-*        used. ["BASEPIC"]
+*        If a null parameter value is supplied, then the BASEPIC Frame 
+*        is used.  ["BASEPIC"]
 *     POSIN = LITERAL (Read)
 *        The co-ordinates of the position to be transformed, in the 
 *        co-ordinate Frame specified by parameter FRAMEIN (supplying 
-*        a colon ":" will display details of the required co-ordinate Frame). 
-*        The position should be supplied as a list of formatted axis values 
-*        separated by spaces or commas. 
+*        a colon ":" will display details of the required co-ordinate 
+*        Frame).  The position should be supplied as a list of 
+*        formatted axis values separated by spaces or commas. 
 *     POSOUT = LITERAL (Write)
 *        The formatted co-ordinates of the transformed position, in the 
 *        co-ordinate Frame specified by parameter FRAMEOUT. The position
@@ -99,42 +101,44 @@
 *        spaces. 
 *     QUIET = _LOGICAL (Read)
 *        If TRUE, the transformed position is not written to the screen
-*        (it is still written to the output parameter OUTPOS). [FALSE]
+*        (it is still written to the output parameter OUTPOS).  [FALSE]
 
 *  Examples:
 *     pictrans "100.3,-20.1" framein=pixel
 *        This converts the position (100.3,-20.1), in pixel co-ordinates
-*        within the current picture of the current graphics device, to the
-*        BASEPIC co-ordinates of that point in the BASE picture.  
+*        within the current picture of the current graphics device, to 
+*        the BASEPIC co-ordinates of that point in the BASE picture.  
 *     pictrans "100.3,-20.1" framein=pixel frameout=graphics
 *        This converts the position (100.3,-20.1), in pixel co-ordinates
-*        within the current picture of the current graphics device, to the
-*        GRAPHICS co-ordinates of that point (i.e. millimetres from the
-*        bottom left corner of the graphics device).
+*        within the current picture of the current graphics device, to
+*        the GRAPHICS co-ordinates of that point (i.e. millimetres from
+*        the bottom-left corner of the graphics device).
 *     pictrans "10 10" framein=graphics frameout=basepic
 *        This converts the position (10 10), in graphics co-ordinates
 *        (i.e. the point which is 10mm above and to the right of the
-*        lower left corner of the graphics device), into BASEPIC
+*        lower-left corner of the graphics device), into BASEPIC
 *        co-ordinates.
 
 *  Notes:
-*     -  BASEPIC co-ordinates locate a position within the entire graphics 
-*     device. The bottom left corner of the device screen has BASEPIC 
-*     co-ordinates of (0,0). The shorter dimension of the screen has 
-*     length 1.0, and the other axis has a length greater than 1.0. 
+*     -  BASEPIC co-ordinates locate a position within the entire 
+*     graphics device.  The bottom-left corner of the device screen has 
+*     BASEPIC co-ordinates of (0,0).  The shorter dimension of the 
+*     screen has length 1.0, and the other axis has a length greater 
+*     than 1.0. 
 *
-*     -  NDC co-ordinates also locate a position within the entire graphics 
-*     device. The bottom left corner of the device screen has NDC 
-*     co-ordinates of (0,0), and the top right corner has NDC
+*     -  NDC co-ordinates also locate a position within the entire
+*     graphics device.  The bottom-left corner of the device screen has 
+*     NDC co-ordinates of (0,0), and the top-right corner has NDC
 *     co-ordinates (1,1).
 *
 *     -  GRAPHICS co-ordinates also span the entire graphics device but
 *     are measured in millimetres from the bottom left corner.
 *
-*     -  CURPIC co-ordinates locate a point within the current picture. The 
-*     bottom left corner of the current picture has CURPIC co-ordinates of 
-*     (0,0). The shorter dimension of the current picture has length 1.0, and 
-*     the other axis has a length greater than 1.0. 
+*     -  CURPIC co-ordinates locate a point within the current picture. 
+*     The bottom-left corner of the current picture has CURPIC 
+*     co-ordinates of (0,0).  The shorter dimension of the current 
+*     picture has length 1.0, and the other axis has a length greater 
+*     than 1.0. 
 *
 *     -  CURNDC co-ordinates also locate a position within the current
 *     picture. The bottom left corner of the current picture has CURNDC 
@@ -152,7 +156,7 @@
 *  Licence:
 *     This program is free software; you can redistribute it and/or
 *     modify it under the terms of the GNU General Public License as
-*     published by the Free Software Foundation; either version 2 of
+*     published by the Free Software Foundation; either Version 2 of
 *     the License, or (at your option) any later version.
 *
 *     This program is distributed in the hope that it will be
@@ -162,8 +166,8 @@
 *
 *     You should have received a copy of the GNU General Public License
 *     along with this program; if not, write to the Free Software
-*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
-*     02111-1307, USA
+*     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+*     02111-1307, USA.
 
 *  Authors:
 *     MJC: Malcolm J. Currie (STARLINK)
@@ -178,7 +182,7 @@
 *     13-AUG-2002 (DSB):
 *        Added CURNDC Frame.
 *     2004 September 3 (TIMJ):
-*        Use CNF_PVAL
+*        Use CNF_PVAL.
 *     {enter_further_changes_here}
 
 *-
@@ -188,7 +192,7 @@
 *  Global Constants:
       INCLUDE 'SAE_PAR'          ! Standard SAE constants
       INCLUDE 'NDF_PAR'          ! NDF constants 
-      INCLUDE 'AST_PAR'          ! AST constants and function declarations
+      INCLUDE 'AST_PAR'          ! AST constants and functions
 
 *  Status:
       INTEGER STATUS
@@ -200,16 +204,17 @@
       DOUBLE PRECISION POSOUT( NDF__MXDIM )! Output position
       INTEGER FRMIN              ! Pointer to requested input Frame
       INTEGER FRMOUT             ! Pointer to requested output Frame
-      INTEGER IAT                ! No. of characters in the TEXT variable
+      INTEGER IAT                ! No. of characters in TEXT variable
       INTEGER ICUR               ! Index of CURPIC Frame
       INTEGER IFRMIN             ! Index of input Frame
       INTEGER IPIC               ! AGI identifier for current picture
       INTEGER IPLOT              ! AST pointer for the Plot
       INTEGER MAP                ! Point er to AST Mapping
-      INTEGER NIN                ! No. of input axes
-      INTEGER NOUT               ! No. of output axes
+      INTEGER NIN                ! Number of input axes
+      INTEGER NOUT               ! Number of output axes
       LOGICAL BOUND              ! Point is within the picture's bounds
       LOGICAL QUIET              ! Suppress screen output?
+
 *.
 
 *  Check the inherited global status.
@@ -221,13 +226,13 @@
 *  Associate image display and start database activity.  
       CALL KPG1_PGOPN( 'DEVICE', 'UPDATE', IPIC, STATUS )
 
-*  Set the PGPLOT viewport to match this picture, and get an AST Plot for
-*  the picture.
+*  Set the PGPLOT viewport to match this picture, and get an AST Plot 
+*  for the picture.
       CALL KPG1_GDGET( IPIC, AST__NULL, .FALSE., IPLOT, STATUS )
 
 *  Set the current Frame to be the required input Frame specified by 
-*  parameter FRAMEIN. If "WORLD" co-ordinates are requested, use AGI_WORLD. 
-*  If "DATA" co-ordinates are requested, use "AGI_DATA".
+*  parameter FRAMEIN. If "WORLD" co-ordinates are requested, use 
+*  AGI_WORLD.  If "DATA" co-ordinates are requested, use "AGI_DATA".
       CALL MSG_SETC( 'PIC', 'picture' )
       CALL KPG1_ASFRM( 'FRAMEIN', 'EPOCHIN', IPLOT, 'AGI_WORLD', 
      :                 'AGI_DATA', .TRUE., '^PIC', STATUS )
@@ -281,7 +286,7 @@
          IAT = IAT + 2
 
 *  Format the transformed position, including axis symbols.
-         CALL KPG1_ASPTP( FRMOUT, NOUT, POSOUT, .TRUE., '  ', TEXT, IAT, 
+         CALL KPG1_ASPTP( FRMOUT, NOUT, POSOUT, .TRUE., '  ', TEXT, IAT,
      :                    STATUS )
 
 *  Display the text, between blank lines.
@@ -292,7 +297,8 @@
 
       END IF
 
-*  Format the transformed position again, this time without axis symbols.
+*  Format the transformed position again, this time without axis
+*  symbols.
       TEXT = ' '
       IAT = 0
       CALL KPG1_ASPTP( FRMOUT, NOUT, POSOUT, .FALSE., '  ', TEXT, IAT, 
@@ -328,8 +334,8 @@
 
 *  If an error occurred, then report a contextual message.
       IF ( STATUS .NE. SAI__OK ) THEN
-         CALL ERR_REP( 'PICTRANS_ERR', 'PICTRANS: Failed to transform'//
-     :                 ' a position between two picture co-ordinate '//
+         CALL ERR_REP( 'PICTRANS_ERR', 'PICTRANS: Failed to transform '/
+     :                 /'a position between two picture co-ordinate '//
      :                 'Frames.', STATUS )
       END IF
 
