612125967c175c532380080a73027a2535cbf559
Malcolm J. Currie <mjc@star.rl.ac.uk>
2008-04-16 17:42:02 +0000

    Documentation update and version increment for WCSSLIDE.
    svn path=/trunk/; revision=26745

--- applications/kappa/libkappa/wcsslide.f
+++ applications/kappa/libkappa/wcsslide.f
@@ -34,46 +34,48 @@
 
 *  ADAM Parameters:
 *     ABS( ) = _DOUBLE (Read)
-*        Absolute shift for each WCS axis.  The number of values supplied 
-*        must match the number of WCS axes in the NDF.  It is only used if 
-*        STYPE="Absolute". Offsets for celestial longitude and latitude
-*        axes should be specified in arc-seconds. Offsets for all other
-*        types of axes should be given directly in the units of the axis.
+*        Absolute shift for each WCS axis.  The number of values 
+*        supplied must match the number of WCS axes in the NDF.  It is 
+*        only used if STYPE="Absolute".  Offsets for celestial longitude
+*        and latitude axes should be specified in arcseconds.  Offsets 
+*        for all other types of axes should be given directly in the 
+*        units of the axis.
 *     FID = LITERAL (Read)
-*        A comma separated list of formatted axis values giving the position
-*        of the fiducial point in WCS co-ordinates.  The number of values 
-*        supplied must match the number of WCS axes in the NDF.  It is only 
-*        used if STYPE="Relative".
+*        A comma-separated list of formatted axis values giving the 
+*        position of the fiducial point in WCS co-ordinates.  The number
+*        of values supplied must match the number of WCS axes in the 
+*        NDF.  It is only used if STYPE="Relative".
 *     NDF = NDF (Update)
 *        The NDF to be translated.
 *     OBJ = LITERAL (Read)
-*        A comma separated list of formatted axis values giving the position
-*        of the standard object in WCS co-ordinates.  The number of values 
-*        supplied must match the number of WCS axes in the NDF.  It is only 
-*        used if STYPE="Relative".
+*        A comma-separated list of formatted axis values giving the 
+*        position of the standard object in WCS co-ordinates.  The 
+*        number of values supplied must match the number of WCS axes in 
+*        the NDF.  It is only used if STYPE="Relative".
 *     STYPE = LITERAL (Read)
 *        The sort of shift to be used.  The choice is "Relative" or
-*        "Absolute". ["Absolute"]
+*        "Absolute".  ["Absolute"]
 
 *  Examples:
 *     wcsslide m31 [32,23]
-*        The (RA,Dec) axes in the NDF m31 are shifted by 32 arc-seconds 
-*        in RA and 23 arc-seconds in Dec.
+*        The (RA,Dec) axes in the NDF m31 are shifted by 32 arcseconds 
+*        in right ascension and 23 arcseconds in declination.
 *     wcsslide speca stype=rel fid=211.2 obj=211.7
 *        The spectral axis in the NDF speca (which measures frequency in
-*        GHz), is shifted by 0.5 GHz (i.e. 211.7 - 211.2). 
+*        GHz), is shifted by 0.5 GHz (i.e. 211.7--211.2). 
 *     wcsslide speca stype=abs abs=0.5
 *        This does just the same as the previous example.
 
 *  Notes:
 *     -  The correction is affected by translating pixel co-ordinates by
 *     a constant amount before projection them into WCS co-ordinates.
-*     Therefore, whilst the translation will be constant across the array
-*     in pixel co-ordinates, it may vary in WCS co-ordinates depending on
-*     the nature of the pixel->WCS transformation. The size of the
-*     translation in pixel co-ordinates is chosen in order to produce the 
-*     required shift in WCS co-ordinates at the OBJ position (if STYPE is
-*     "relative") or at the array centre (if STYPE is "absolute").
+*     Therefore, whilst the translation will be constant across the 
+*     array in pixel co-ordinates, it may vary in WCS co-ordinates 
+*     depending on the nature of the pixel->WCS transformation.  The 
+*     size of the translation in pixel co-ordinates is chosen in order 
+*     to produce the required shift in WCS co-ordinates at the OBJ 
+*     position (if STYPE is "Relative"), or at the array centre (if 
+*     STYPE is "Absolute").
 
 *  Related Applications:
 *     KAPPA: SLIDE.
@@ -88,7 +90,7 @@
 *  Licence:
 *     This program is free software; you can redistribute it and/or
 *     modify it under the terms of the GNU General Public License as
-*     published by the Free Software Foundation; either version 2 of
+*     published by the Free Software Foundation; either Version 2 of
 *     the License, or (at your option) any later version.
 *
 *     This program is distributed in the hope that it will be
@@ -98,8 +100,8 @@
 *
 *     You should have received a copy of the GNU General Public License
 *     along with this program; if not, write to the Free Software
-*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
-*     02111-1307, USA
+*     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+*     02111-1307, USA.
 
 *  Authors:
 *     DSB: David Berry (JAC, UCLan)
