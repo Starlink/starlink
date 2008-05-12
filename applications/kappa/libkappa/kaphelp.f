5ed775fd49e27ac934b4a1829bad237255e0e4ed
Malcolm J. Currie <mjc@star.rl.ac.uk>
2008-05-12 21:06:27 +0000

    Prologue tidying.
    svn path=/trunk/; revision=27071

--- applications/kappa/libkappa/kaphelp.f
+++ applications/kappa/libkappa/kaphelp.f
@@ -135,7 +135,7 @@
 *  Licence:
 *     This program is free software; you can redistribute it and/or
 *     modify it under the terms of the GNU General Public License as
-*     published by the Free Software Foundation; either version 2 of
+*     published by the Free Software Foundation; either Version 2 of
 *     the License, or (at your option) any later version.
 *
 *     This program is distributed in the hope that it will be
@@ -145,8 +145,8 @@
 *
 *     You should have received a copy of the GNU General Public License
 *     along with this program; if not, write to the Free Software
-*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
-*     02111-1307, USA
+*     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+*     02111-1307, USA.
 
 *  Authors:
 *     MJC: Malcolm J. Currie (STARLINK)
@@ -169,7 +169,7 @@
 *     1995 November 9 (MJC):
 *        Modified for UNIX and added the Topic on Navigation.
 *     2004 July 27 (TIMJ):
-*        Switch to SHL library
+*        Switch to SHL library.
 *     {enter_further_changes_here}
 
 *-
@@ -183,14 +183,10 @@
 *  Status:
       INTEGER STATUS
 
-*  External References:
-
 *  Local Constants:
       CHARACTER*10 LIBNAM      ! Name of the KAPPA help library
       PARAMETER ( LIBNAM = 'KAPPA_HELP' )
 
-*  Local Variables:
-                               ! less trailing blanks
 *.
 
 *  Check the inherited status.
