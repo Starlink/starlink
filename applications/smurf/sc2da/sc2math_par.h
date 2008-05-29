134701977eff59723e5cf3e88c35cacfbcaf400f
Andy Gibb <agg@phas.ubc.ca>
2008-05-29 20:55:04 +0000

    New files for sc2math.c
    svn path=/trunk/; revision=27177

---
+++ applications/smurf/sc2da/sc2math_par.h
@@ -0,0 +1,12 @@
+/* sc2math_par.h - parameters for sc2math library
+
+   Authors :
+    B.D.Kelly (bdk@roe.ac.uk)
+   History :
+    06May2008 : original (bdk)
+*/
+
+#define SC2MATH__GAUS 0
+#define SC2MATH__SINC 1
+#define SC2MATH__SINCTAP 2
+#define SC2MATH__SINCDEL 3
