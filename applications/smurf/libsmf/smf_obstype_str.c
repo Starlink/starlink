bf215a2d9af98a204c92fb0b5a94e0bc15dfc5cd
Tim Jenness <t.jenness@jach.hawaii.edu>
2008-07-25 19:25:29 +0000

    remove debug print
    svn path=/trunk/; revision=27735

--- applications/smurf/libsmf/smf_obstype_str.c
+++ applications/smurf/libsmf/smf_obstype_str.c
@@ -90,7 +90,6 @@ const char * smf_obstype_str( smf_obstype type, int * status ) {
   /* Check entry status */
   if (*status != SAI__OK) return retval;
 
-  printf("Checking type %d\n",type);
   /* now switch on data type */
   switch( type ) {
   case SMF__TYP_NULL:
