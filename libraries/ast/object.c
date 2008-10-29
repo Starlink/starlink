c4a1b9b3663bd4805ad396a033079df68f32a2bd
David S. Berry <d.berry@jach.hawaii.edu>
2008-10-29 18:07:10 +0000

    ast: The CheckThreads tuning parameter was a bad idea. Remove it.
    svn path=/trunk/; revision=28306

--- libraries/ast/object.c
+++ libraries/ast/object.c
@@ -258,11 +258,6 @@ static AstObject *zero_ptr;
    caching is switched off via the astTune function. */
 static int object_caching = 0;
 
-/* A flag that indicates if a check should be made by each public AST
-   function to ensure that any supplied Objects have been locked for use
-   by the calling thread. */
-static int check_threads = 1;
-
 /* Set up global data access, mutexes, etc, needed for thread safety. */
 #ifdef THREAD_SAFE
 
@@ -2234,9 +2229,8 @@ AstObject *astCheckLock_( AstObject *this, int *status ) {
 /* This function does nothing in the non-threads version of libast. */
 #if defined(THREAD_SAFE)
 
-/* Check the supplied pointer. Also check that lock checking has not been
-   disabled via the CheckThreads tuning parameter (see astTune). */
-   if( this && check_threads ) {
+/* Check the supplied pointer. */
+   if( this ) {
 
 /* We use the private ManageLock function rather than the virtual 
    astManageLock method to avoid the unnecessary overhead of checking 
@@ -3416,15 +3410,6 @@ f     AST_DELETE).
 *     that it controls caching of all memory blocks of less than 300 bytes
 *     allocated by AST (whether for internal or external use), not just 
 *     memory used to store AST Objects. 
-c     - CheckThreads: A boolean flag which indicates whether or not a
-c     check should be made by each AST function to ensure that any
-c     supplied Objects are locked for use by the calling thread. If
-c     this flag is non-zero (the default), such checks are made. See 
-c     See functions astLock and astUnlock. Note, this flag should only be
-c     set to zero if it is known that multiple threads will never try to
-c     modify the same AST Object at the same time. Any sort of strange
-c     behaviour is possible if different threads modify an Object at the
-c     same time.
 
 *  Notes:
 c     - This function attempts to execute even if the AST error
@@ -3448,10 +3433,6 @@ f     error value
             object_caching = value;
             if( !object_caching ) EmptyObjectCache( status );
          }
-         
-      } else if( astChrMatch( name, "CheckThreads" ) ) {
-         result = check_threads;
-         if( value != AST__TUNULL ) check_threads = value;
          
       } else if( astChrMatch( name, "MemoryCaching" ) ) {
          result = astMemCaching( value );
