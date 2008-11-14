11e976daf8a5d353bdf0e048dfc58f3109238738
Tim Jenness <t.jenness@jach.hawaii.edu>
2008-11-14 22:56:50 +0000

    use SEQCOUNT to decide whether a dark can be used
    svn path=/trunk/; revision=28393

--- applications/smurf/libsmf/smf_choose_darks.c
+++ applications/smurf/libsmf/smf_choose_darks.c
@@ -43,6 +43,8 @@
 *        Initial version.
 *     2008-07-25 (TIMJ):
 *        Be more lenient with time gaps for scan mode. Use SMF__BADIDX
+*     2008-11-14 (TIMJ):
+*        Use SEQCOUNT to decide relatedness rather than time.
 
 *  Copyright:
 *     Copyright (C) 2008 Science and Technology Facilities Council.
@@ -79,123 +81,62 @@
 /* SMURF includes */
 #include "libsmf/smf.h"
 
-typedef struct {
-  size_t index;
-  double diff;
-} smf_timediff;
-
 void smf_choose_darks( const smfArray *darks, const smfData *indata,
                        size_t *dark1, size_t *dark2, int * status ) {
   size_t i;          /* loop counter */
-  const double maxdiffstare = 15.0; /* maximum gap between dark and science*/
-  const double maxdiffscan = 30.0 * 60.0; /* scan can be more lenient */
-  double maxdiff = 0.0;  /* actual difference allowed */
-  double reftime;    /* MJD of input science data */
+  int refseq;        /* Sequence count of input science data */
   int refsubnum;     /* Subarray number of science data */
-
-  smf_timediff prev; /* information on closest previous */
-  smf_timediff next; /* information on closest next */
 
   *dark1 = SMF__BADIDX;
   *dark2 = SMF__BADIDX;
 
   if (*status  != SAI__OK) return;
 
-  /* get reference MJD and subarray number */
-  reftime = (indata->hdr->allState)[0].rts_end;
-  smf_find_subarray( indata->hdr, NULL, 0, &refsubnum, status );
+  /* get reference sequence counter and subarray number */
+  smf_find_seqcount( indata->hdr, &refseq, status );
+  smf_find_subarray( indata->hdr, NULL, (size_t)0, &refsubnum, status );
 
-  /* initialise the diff structs */
-  prev.diff = VAL__MAXD;
-  prev.index = SMF__BADIDX;
-  next.diff = VAL__MAXD;
-  next.index = SMF__BADIDX;
-
-  /* loop through all the darks finding the ones closest in time
-     with the correct subarray number */
+  /* Loop through all the darks looking for ones that only differ
+     from the reference sequence counter by 1 */
   for (i=0; i< darks->ndat; i++) {
     smfData *thisdark = (darks->sdata)[i];
     int thissubnum;
-    smf_find_subarray( thisdark->hdr, NULL, 0, &thissubnum, status );
+    smf_find_subarray( thisdark->hdr, NULL, (size_t)0, &thissubnum, status );
 
-    /* see if we even need to look at the time */
+    /* see if we even need to look at the sequence counter */
     if (thissubnum == refsubnum) {
-      double thistime = (thisdark->hdr->allState)[0].rts_end;
-      double diff = reftime - thistime;
-      if (diff > 0) {
-        if (prev.diff > diff) {
-          prev.diff = diff;
-          prev.index = i;
-        }
-      } else if (diff < 0) {
-        diff = fabs(diff);
-        if (next.diff > diff) {
-          next.diff = diff;
-          next.index = i;
-        }
-      } else {
+      int thisseq;
+      int seqdiff;
+      smf_find_seqcount( thisdark->hdr, &thisseq, status );
+      seqdiff = refseq - thisseq;
+
+      if ( seqdiff == 1 ) {
+        /* Valid previous dark */
+        *dark1 = i;
+      } else if (seqdiff == -1 ) {
+        /* Valid next dark */
+        *dark2 = i;
+      } else if (seqdiff == 0) {
         /* should not be possible */
         if (*status == SAI__OK) {
           *status = SAI__ERROR;
           errRep(" ","Should not be possible for dark and science "
-                 "observation to have identical MJD times", status );
+                 "observation to have identical sequence counter.", status );
           return;
         }
       }
     }
-  }
 
-  /* max allowed diff depends on observing mode */
-  if ( indata->hdr->obsmode == SMF__OBS_SCAN ) {
-    maxdiff = maxdiffscan;
-  } else {
-    maxdiff = maxdiffstare;
-  }
-
-  /* if we found a previous, see how close it really was
-      - reduced darks still have all JCMTState if processed by
-      SMURF internally.
-  */
-  if (prev.index != SMF__BADIDX) {
-    smfData *thisdark = (darks->sdata)[prev.index];
-    size_t endframe = thisdark->hdr->nframes - 1;
-    double endtime = (thisdark->hdr->allState)[endframe].rts_end;
-    double difftime = reftime - endtime;
-
-    /* convert to seconds */
-    difftime *= SPD;
-    if ( difftime > maxdiff ) {
-      /* dark is no good */
-      prev.index = SMF__BADIDX;
-    }
+    /* finish if we have everything */
+    if (*dark1 != SMF__BADIDX && *dark2 != SMF__BADIDX) break;
 
   }
 
-  if (next.index != SMF__BADIDX) {
-    smfData *thisdark = (darks->sdata)[next.index];
-    size_t endframe = indata->hdr->nframes - 1;
-    double starttime = (thisdark->hdr->allState)[0].rts_end;
-    double refendtime = (indata->hdr->allState)[endframe].rts_end;
-    double difftime = starttime - refendtime;
-
-    /* convert to seconds */
-    difftime *= SPD;
-    if ( difftime > maxdiff ) {
-      /* dark is no good */
-      next.index = SMF__BADIDX;
-    }
-
-  }
-
-  if (prev.index == SMF__BADIDX) {
+  if (*dark1 == SMF__BADIDX) {
     msgOutif( MSG__VERB, " ","Unable to find any prior dark", status );
   }
-  if (next.index == SMF__BADIDX) {
+  if (*dark2 == SMF__BADIDX) {
     msgOutif( MSG__VERB, " ","Unable to find any following dark", status );
   }
-
-  /* store return values */
-  *dark1 = prev.index;
-  *dark2 = next.index;
 
 }
