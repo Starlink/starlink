9be60e2c7e2fde34e50ae891cdc41d73fc86684c
Andy Gibb <agg@phas.ubc.ca>
2008-11-14 00:39:02 +0000

    Deal with bad values so that if all the input values are bad, the output is set bad as well
    svn path=/trunk/; revision=28372

--- applications/smurf/libsmf/smf_average_dataD.c
+++ applications/smurf/libsmf/smf_average_dataD.c
@@ -65,11 +65,14 @@
 *        Update to use new smf_free behaviour
 *     2008-05-29 (TIMJ):
 *        Clarify that someone needs to free this memory.
+*     2008-11-13 (AGG):
+*        Deal with bad values so that if all the input values are bad,
+*        the output is set bad as well.
 *     {enter_further_changes_here}
 
 *  Copyright:
 *     Copyright (C) 2008 Science and Technology Facilities Council.
-*     Copyright (C) 2006-2007 University of British Columbia. All
+*     Copyright (C) 2006-2008 University of British Columbia. All
 *     Rights Reserved.
 
 *  Licence:
@@ -120,6 +123,7 @@ void smf_average_dataD( const smfData *data, int start,  int nslice,
   int j;                    /* Loop counter */
   int k;                    /* Loop counter */
   int lastslice;            /* Index of the last time slice in the current sum */
+  int nbad;		    /* Number of data points with a bad value */
   int nbol;                 /* Number of data points per timeslice (bolometers) */
   int nframes;              /* Number of time slices in input data */
   int nsamples;             /* Number of time slices in the average */
@@ -249,6 +253,7 @@ void smf_average_dataD( const smfData *data, int start,  int nslice,
       for ( j=0; j<nbol; j++ ) {
         sum = 0.0;
         nsamples = 0;
+	nbad = 0;
         /* Sum all the contributions from that element in the desired
            range of time slices */
         for ( k=base; k<lastslice; k++ ) {
@@ -256,11 +261,17 @@ void smf_average_dataD( const smfData *data, int start,  int nslice,
           if ( currentdata != VAL__BADD ) {
             sum += currentdata;
             nsamples++;
-          }
+          } else {
+	    nbad++;
+	  }
         }
         /* Calculate the average and store it in the output array */
-        sum /= nsamples;
-        (*avdata)[j + offset] = sum;
+	if ( nbad == (lastslice - base) ) {
+	  (*avdata)[j + offset] = VAL__BADD;
+	} else {
+	  sum /= nsamples;
+	  (*avdata)[j + offset] = sum;
+	}
       }
     }
   } else {
