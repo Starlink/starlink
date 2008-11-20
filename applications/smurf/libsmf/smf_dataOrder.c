d13ca33844af760e0e7e06351c629f99129543c3
Tim Jenness <t.jenness@jach.hawaii.edu>
2008-11-20 21:53:23 +0000

    issue a warning when reordering an NDF
    svn path=/trunk/; revision=28434

--- applications/smurf/libsmf/smf_dataOrder.c
+++ applications/smurf/libsmf/smf_dataOrder.c
@@ -342,7 +342,8 @@ void smf_dataOrder( smfData *data, int isTordered, int *status ) {
   /* If NDF associated with data, modify dimensions of the data */
   if( data->file ) {
     if( data->file->ndfid != NDF__NOID ) {
-
+      fprintf(stderr,
+              "Do not yet handle changing dimensions of NDF on reorder\n");
     }
   }
 
