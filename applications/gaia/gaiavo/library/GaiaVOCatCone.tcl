d0114eb69fea9ca5aeae26e130fb9ec552af0ab1
Peter W. Draper <P.W.Draper@durham.ac.uk>
2009-01-16 15:06:50 +0000

    r28879: gaiavo: add simple plotting symbol default and full dialog control

--- applications/gaia/gaiavo/library/GaiaVOCatCone.tcl
+++ applications/gaia/gaiavo/library/GaiaVOCatCone.tcl
@@ -93,6 +93,12 @@ itcl::class gaiavo::GaiaVOCatCone {
 
       set m [add_menubutton Options "Options menu"]
 
+      #  Change the plot symbols. XXX how to associate this with the
+      #  catalogue?
+      add_menuitem $m command "Set plot symbols..." \
+         {Set the symbol (color, size, etc.) to use to plot objects} \
+         -command [code $this set_plot_symbols]
+
       set ns_menu [menu $m.ns]
       add_menuitem $m cascade "Set Name Server" \
          {Select the name server used to resolve astronomical object names} \
@@ -127,7 +133,24 @@ itcl::class gaiavo::GaiaVOCatCone {
 
       pack $itk_component(plot) -side left -expand 1 -pady 2m
       add_short_help $itk_component(plot) {Plot positions over image}
+
+      #  Canvas tag is used for all symbols.
+      set tag_ $w_.cat
+      set object_tag_ $tag_.objects
+      set label_tag_ $tag_.labels
+
+      #  Add bindings for symbols. XXX need to implement.
+      $canvas_ bind $object_tag_  <1> [code $this select_symbol current 0]
+      $canvas_ bind $object_tag_  <Shift-1> \
+         [code $this select_symbol current 1]
+      $canvas_ bind $object_tag_  <Control-1> \
+         [code $this select_symbol current 1]
+      #$canvas_ bind $object_tag_  <Any-Enter> "$canvas_ config -cursor tcross"
+      #$canvas_ bind $object_tag_  <Any-Leave> "$draw_ reset_cursor"
    }
+
+
+
 
    #  Set the name server used, pass to other components.
    public method set_namesvr {name} {
@@ -157,6 +180,12 @@ itcl::class gaiavo::GaiaVOCatCone {
          {Controls for querying Cone Search server}
 
       set query_component_ $itk_component(cone)
+   }
+
+   #  New catalogue, set default plot symbol.
+   public method query_done {status result} {
+      GaiaVOCat::query_done $status $result
+      set_default_plot_symbol_
    }
 
    #  Open a service, "args" is a list of values from a row of the current
@@ -189,24 +218,43 @@ itcl::class gaiavo::GaiaVOCatCone {
       return {}
    }
 
-   #  Plot the RA and Dec positions on the image.
+   #  Plot the RA and Dec positions on the image using the defined symbols.
    public method plot {} {
-      set rtdctrl [$itk_option(-gaia) get_image]
-      set rtdimage [$rtdctrl get_image]
-      set equinox "J2000"; # Must be true for all catalogues.
 
-      #  Do the plot... 
-      #  XXX uses the plotting symbols set in the astrocat for this catalogue,
-      #  need to set a value. 
-      set symbol [list {} [list circle red {} {} {} {}] [list {4.0} {}]]
-      $w_.cat symbol $symbol
+      #  Clear any existing positions.
+      clear_plot
 
-      if {[catch {$w_.cat imgplot $rtdimage $info_ $equinox $headings_} msg]} {
+      #  Do the plot.
+      if {[catch {$w_.cat imgplot $rtdimage_ $info_ $equinox_ $headings_} msg]} {
          error_dialog $msg
       }
    }
 
+   #  Clear the plot of any existing symbols.
+   public method clear_plot {} {
+      catch {$canvas_ delete $tag_}
+   }
 
+   #  Set the catalogue plotting symbol to the default.
+   protected method set_default_plot_symbol_ {} {
+      set symbol [list {} [list circle red {} {} {} {}] [list {4.0} {}]]
+      $w_.cat symbol $symbol
+   }
+
+   #  Pop up a dialog to set the plot symbols to use for this catalogue.
+   public method set_plot_symbols {} {
+      set columns $headings_
+      if {[llength $columns] == 0} {
+         info_dialog \
+            "Please make a query first so that the column names are known" $w_
+         return
+      }
+      utilReUseWidget cat::SymbolConfig $w_.symconf \
+         -catalog $itk_option(-catalog) \
+         -astrocat [code $w_.cat] \
+         -columns $columns \
+         -command [code $this plot]
+   }
 
    #  Configuration options: (public variables)
    #  ----------------------
@@ -218,13 +266,31 @@ itcl::class gaiavo::GaiaVOCatCone {
    itk_option define -accessURL accessURL AccessURL {}
 
    #  Instance of GAIA to display the catalogue.
-   itk_option define -gaia gaia Gaia {}
+   itk_option define -gaia gaia Gaia {} {
+      set rtdctrl_ [$itk_option(-gaia) get_image]
+      set rtdimage_ [$rtdctrl_ get_image]
+      set canvas_ [$rtdctrl_ get_canvas]
+   }
 
    #  The name server.
    itk_option define -namesvr namesvr NameSvr {}
 
    #  Protected variables: (available to instance)
    #  --------------------
+
+   #  Various elements from the GAIA instance.
+   protected variable rtdctrl_ {}
+   protected variable rtdimage_ {}
+   protected variable canvas_ {}
+
+   #  Equinox for VO catalogues. Really ICRS but J2000 (=FK5/J2000).
+   protected variable equinox_ "J2000"
+
+   #  Various tags associated with the positions when plotted. These
+   #  are defined by imgplot method.
+   protected variable tag_ {}
+   protected variable object_tag_ {}
+   protected variable label_tag_ {}
 
    #  Common variables: (shared by all instances)
    #  -----------------
