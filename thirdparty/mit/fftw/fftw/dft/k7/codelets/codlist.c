61feee31713b232b9d21efdbdc488ff9f2f3baa4
Peter W. Draper <p.w.draper@durham.ac.uk>
2009-02-18 15:41:08 +0000

    Replace FFTW with a submodule

--- thirdparty/mit/fftw/fftw/dft/k7/codelets/codlist.c
+++
@@ -1,8 +0,0 @@
-#include "ifftw.h"
-
-
-
-extern const solvtab X(solvtab_dft_k7);
-const solvtab X(solvtab_dft_k7) = {
-   SOLVTAB_END
-};
--- thirdparty/mit/fftw/fftw/dft/simd/codelets/codlist.c
+++
@@ -1,8 +0,0 @@
-#include "ifftw.h"
-
-
-
-extern const solvtab X(solvtab_dft_simd);
-const solvtab X(solvtab_dft_simd) = {
-   SOLVTAB_END
-};
