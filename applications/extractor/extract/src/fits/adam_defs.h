005cc39dfdb11cc781084bbb393e7f1984097947
Peter W. Draper <p.w.draper@durham.ac.uk>
2009-02-23 14:35:44 +0000

    Replace EXTRACTOR with a submodule
    Only the src directory of SExtractor is tracked for this part

--- applications/extractor/extract/src/fits/adam_defs.h
+++
@@ -1,8 +0,0 @@
-#ifndef _ADAM__H_INCLUDED_
-#define _ADAM_H_INCLUDED_ 1
-
-/* Prototypes for extension functions, like adamprint */
-void adamprint( FILE *file, char *fmt, ... );
-
-
-#endif
--- applications/extractor/extract/src/mydefs.h
+++
@@ -1,4 +0,0 @@
-#define	QFSEEK(afile, offset, pos, fname) \
-		{if (fseek(afile, (long)(offset), pos)) \
-		  error(EXIT_FAILURE,"*Error*: File positioning failed in ", \
-			fname);;}
