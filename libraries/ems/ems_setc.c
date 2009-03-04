ae148b9c51848e71aa7154ff6ec86d45770dbc77
Tim Jenness <t.jenness@jach.hawaii.edu>
2009-03-04 13:22:07 -1000

    ems: whitespace tweaks in ems_setc.c

--- libraries/ems/ems_setc.c
+++ libraries/ems/ems_setc.c
@@ -1,9 +1,9 @@
 /*+
  *  Name:
- *     emsSetc
+ *     ems_setc
  *
  *  Purpose:
-*  Fortran callable routine
+ *     Fortran callable implementation of emsSetc
 
  *  Authors:
  *    AJC: Alan Chipperfield (Starlink)
@@ -20,31 +20,31 @@
  *      Fortran string will result in a single byte being allocated
  *      holding '\0'.
 
-*  Licence:
-*     This program is free software; you can redistribute it and/or
-*     modify it under the terms of the GNU General Public License as
-*     published by the Free Software Foundation; either version 2 of
-*     the License, or (at your option) any later version.
-*     
-*     This program is distributed in the hope that it will be
-*     useful,but WITHOUT ANY WARRANTY; without even the implied
-*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
-*     PURPOSE. See the GNU General Public License for more details.
-*     
-*     You should have received a copy of the GNU General Public License
-*     along with this program; if not, write to the Free Software
-*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
-*     02111-1307, USA
+ *  Licence:
+ *     This program is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU General Public License as
+ *     published by the Free Software Foundation; either version 2 of
+ *     the License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be
+ *     useful,but WITHOUT ANY WARRANTY; without even the implied
+ *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *     PURPOSE. See the GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+ *     02111-1307, USA
 
-*  Copyright:
-*     Copyright (C) 2009 Science and Technology Faciltiies Council.
-*     Copyright (C) 2006 Particle Physics & Astronomy Research Council.
-*     All Rights Reserved.
+ *  Copyright:
+ *     Copyright (C) 2009 Science and Technology Faciltiies Council.
+ *     Copyright (C) 2006 Particle Physics & Astronomy Research Council.
+ *     All Rights Reserved.
 
  *  Authors:
 
  *-
-*/
+ */
 #include <string.h>
 #include "ems.h"                       /* ems_ function prototypes */
 #include "ems_par.h"                   /* EMS constants */
