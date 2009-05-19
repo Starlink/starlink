52c99a926fe9e59555c2b85786e0377369704532
David Berry <d.berry@jach.hawaii.edu>
2009-05-19 10:32:01 +0100

    atools: Add astremoveregion

---
+++ applications/atools/astremoveregion.f
@@ -0,0 +1,118 @@
+      SUBROUTINE ASTREMOVEREGION( STATUS )
+*+
+*  Name:
+*     ASTREMOVEREGION
+
+*  Purpose:
+*     Remove any Regions from a Mapping.
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Type of Module:
+*     ADAM A-task
+
+*  Invocation:
+*     CALL ASTREMOVEREGION( STATUS )
+
+*  Arguments:
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
+
+*  Description:
+*     This application searches the supplied Mapping (which may be a 
+*     compound Mapping such as a CmpMap) for any component Mappings 
+*     that are instances of the AST Region class. It then creates a new
+*     Mapping from which all Regions have been removed. If a Region
+*     cannot simply be removed (for instance, if it is a component of a
+*     parallel CmpMap), then it is replaced with an equivalent UnitMap 
+*     in the returned Mapping.
+
+*  Usage:
+*     astremoveregion this result
+
+*  ADAM Parameters:
+*     RESULT = LITERAL (Read)
+*        A text file to receive the modified Mapping.
+*     THIS = LITERAL (Read)
+*        An NDF or text file holding the Mapping. If an NDF is supplied, 
+*        the Mapping from the base Frame of the WCS FrameSet to the
+*        current Frame will be used.
+
+*  Copyright:
+*     Copyright (C) 2009 Science & Technology Facilities Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public License
+*     along with this program; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+*     02111-1307, USA
+
+*  Authors:
+*     DSB: David Berry (JAC, Hawaii)
+*     {enter_new_authors_here}
+
+*  History:
+*     7-MAY-2009 (DSB):
+*        Original version.
+*     {enter_further_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+*  Type Definitions:
+      IMPLICIT NONE              ! no default typing allowed
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'AST_PAR'          ! AST constants and function declarations
+
+*  External References:
+      EXTERNAL AST_ISAMAPPING
+
+*  Status:
+      INTEGER STATUS
+
+*  Local Variables:
+      INTEGER THIS
+      INTEGER RESULT
+*.
+
+*  Check inherited status.      
+      IF( STATUS .NE. SAI__OK ) RETURN
+
+*  Begin an AST context.
+      CALL AST_BEGIN( STATUS )
+
+*  Get the Mapping.
+      CALL KPG1_GTOBJ( 'THIS', 'Mapping', AST_ISAMAPPING, THIS,
+     :                 STATUS )
+
+*  Get the required modified Mapping.
+      RESULT = AST_REMOVEREGIONS( THIS, STATUS )
+
+*  Write this Mapping out to a text file.
+      CALL ATL1_PTOBJ( 'RESULT', ' ', RESULT, STATUS )
+
+*  End the AST context.
+      CALL AST_END( STATUS )
+
+*  Give a context message if anything went wrong.
+      IF( STATUS .NE. SAI__OK ) THEN
+         CALL ERR_REP( 'ASTREMOVEREGION_ERR', 'Error removing Regions'//
+     :                 ' from a Mapping.', STATUS )
+      END IF
+
+      END
