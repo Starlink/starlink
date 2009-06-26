a500cecfa60533ed30ae815b504c305b2fc8d580
Tim Jenness <t.jenness@jach.hawaii.edu>
2009-06-25 17:06:41 -1000

    convert: Add licence and copyright to cof_whead.f

--- applications/convert/libcof/cof_whead.f
+++ applications/convert/libcof/cof_whead.f
@@ -135,6 +135,29 @@
 *        PRVPn, PRVIn, PRVDn, PRVCn, PRVMn ---  provenance headers are
 *          removed.
 
+*  Copyright:
+*     Copyright (C) 1994 Science & Engineering Research Council.
+*     Copyright (C) 1995-2000, 2006 Central Laboratory of the Research
+*     Councils.
+*     Copyright (C) 2008 Science & Technology Facilities Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either Version 2 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public License
+*     along with this program; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+*     02111-1307, USA.
+
 *  Authors:
 *     MJC: Malcolm J. Currie (STARLINK)
 *     DSB: David S. Berry (STARLINK)
