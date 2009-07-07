b87c5a7ae2e2f0dbf9062daf247b59b24dc6ea98
Peter W. Draper <p.w.draper@durham.ac.uk>
2009-07-07 16:13:46 +0100

    tcllibs: correct package version

--- thirdparty/tclsys/tcllibs/tcllibs/Tclxml3.1/pkgIndex.tcl
+++ thirdparty/tclsys/tcllibs/tcllibs/Tclxml3.1/pkgIndex.tcl
@@ -4,7 +4,7 @@
 
 package ifneeded xml::c       3.1 [list load   [file join $dir libTclxml3.1.so]]
 package ifneeded xml::tcl     3.1 [list source [file join $dir xml__tcl.tcl]]
-package ifneeded sgmlparser   1.1       [list source [file join $dir sgmlparser.tcl]]
+package ifneeded sgmlparser   1.0       [list source [file join $dir sgmlparser.tcl]]
 package ifneeded xpath        1.0       [list source [file join $dir xpath.tcl]]
 package ifneeded xmldep       1.0       [list source [file join $dir xmldep.tcl]]
 
