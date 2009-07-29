2fde8be70509b4ed2922d044f4023d57b9fa319d
Tim Jenness <t.jenness@jach.hawaii.edu>
2009-07-29 12:58:23 -1000

    pcs: Add C wrapper to TASK_GET_NAME function

---
+++ libraries/pcs/task/task_adam.c
@@ -0,0 +1,70 @@
+/*
+ *+
+ *  Name:
+ *     task_adam.c
+
+ *  Purpose:
+ *     C interface to Fortran PCS TASK routines.
+
+ *  Language:
+ *     Starlink ANSI C
+
+ *  Copyright:
+ *     Copyright (C) 2009 Science and Technology Facilities Council.
+ *     All Rights Reserved.
+
+ *  Licence:
+ *     This program is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU General Public License as
+ *     published by the Free Software Foundation; either version 2 of
+ *     the License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be
+ *     useful,but WITHOUT ANY WARRANTY; without even the implied
+ *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *     PURPOSE. See the GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+ *     02111-1307, USA
+
+ *  Authors:
+ *     TIMJ: Tim Jenness (JAC, Hawaii)
+ *     {enter_new_authors_here}
+
+ *  History:
+ *     29-JUL-2009 (TIMJ):
+ *        Initial version. Don't worry about MUTEXes.
+
+ *  Bugs:
+ *     {note_any_bugs_here}
+
+ *-
+*/
+
+#include "f77.h"
+
+#include "task_adam.h"
+
+
+/* Allocated buffer length is given not returned. Should be at least 16. */
+
+F77_SUBROUTINE(task_get_name)( CHARACTER(name), INTEGER(status) TRAIL(name) );
+
+
+void taskGetName( char * taskname, size_t buflen, int * status ) {
+
+  DECLARE_CHARACTER_DYN( TASKNAME );
+  DECLARE_INTEGER(STATUS);
+
+  F77_EXPORT_INTEGER( *status, STATUS );
+  F77_CREATE_CHARACTER( TASKNAME, buflen - 1 );
+
+  taskname[0] = '\0';
+  F77_CALL(task_get_name) ( CHARACTER_ARG(TASKNAME), INTEGER_ARG(&STATUS)
+                            TRAIL_ARG(TASKNAME) );
+  cnfImprt( TASKNAME, TASKNAME_length, taskname );
+  F77_FREE_CHARACTER(TASKNAME);
+  F77_IMPORT_INTEGER( &STATUS, status );
+}
---
+++ libraries/pcs/task/task_adam.h
@@ -0,0 +1,57 @@
+
+#if !defined( _TASK_ADAM_INCLUDED ) /* Protect against multiple inclusion       */
+#define _TASK_ADAM_INCLUDED 1
+
+/*
+ *+
+ *  Name:
+ *     task_adam.h
+
+ *  Purpose:
+ *     C interface to PCS TASK routines.
+
+ *  Language:
+ *     Starlink ANSI C
+
+ *  Copyright:
+ *     Copyright (C) 2008 Science and Technology Facilities Council.
+ *     All Rights Reserved.
+
+ *  Licence:
+ *     This program is free software; you can redistribute it and/or
+ *     modify it under the terms of the GNU General Public License as
+ *     published by the Free Software Foundation; either version 2 of
+ *     the License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be
+ *     useful,but WITHOUT ANY WARRANTY; without even the implied
+ *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *     PURPOSE. See the GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program; if not, write to the Free Software
+ *     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+ *     02111-1307, USA
+
+ *  Authors:
+ *     TIMJ: Tim Jenness (JAC, Hawaii)
+ *     {enter_new_authors_here}
+
+ *  History:
+ *     19-JUL-2008 (TIMJ):
+ *        Initial version.
+
+ *  Bugs:
+ *     {note_any_bugs_here}
+
+ *-
+*/
+/* we need size_t */
+#include <stdlib.h>
+
+void taskGetName( char * taskname, size_t buflen, int * status );
+
+#endif
+
+
+
