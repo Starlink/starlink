b0a7af9be8e06eb34bf060a631d245822e00bec4
David Berry <d.berry@jach.hawaii.edu>
2009-10-13 11:16:47 +0100

    atools: Add astgetregframe

---
+++ applications/atools/astgetregframe.f
@@ -0,0 +1,111 @@
+      SUBROUTINE ASTGETREGFRAME( STATUS )
+*+
+*  Name:
+*     ASTGETREGFRAME
+
+*  Purpose:
+*     Obtain the Frame represented by a Region.
+
+*  Language:
+*     Starlink Fortran 77
+
+*  Type of Module:
+*     ADAM A-task
+
+*  Invocation:
+*     CALL ASTGETREGFRAME( STATUS )
+
+*  Arguments:
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
+
+*  Description:
+*     This application returns a copy of the Frame represented by a
+*     Region.
+
+*  Usage:
+*     astgetregframe this result
+
+*  ADAM Parameters:
+*     RESULT = LITERAL (Read)
+*        An text file to receive the Frame.
+*     THIS = LITERAL (Read)
+*        A text file holding the Region.
+
+*  Copyright:
+*     Copyright (C) 2009 Science & Technology Facilities Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 2 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public License
+*     along with this program; if not, write to the Free Software
+*     Foundation, Inc., 59 Temple Place,Suite 330, Boston, MA
+*     02111-1307, USA
+
+*  Authors:
+*     DSB: David Berry (STARLINK)
+*     {enter_new_authors_here}
+
+*  History:
+*     13-OCT-2009 (DSB):
+*        Original version.
+*     {enter_further_changes_here}
+
+*  Bugs:
+*     {note_any_bugs_here}
+
+*-
+*  Type Definitions:
+      IMPLICIT NONE              ! no default typing allowed
+
+*  Global Constants:
+      INCLUDE 'SAE_PAR'          ! Standard SAE constants
+      INCLUDE 'AST_PAR'          ! AST constants and function declarations
+
+*  Status:
+      INTEGER STATUS
+
+*  External References:
+      EXTERNAL AST_ISAREGION
+
+*  Local Variables:
+      INTEGER IAST
+      INTEGER RESULT
+*.
+
+*  Check inherited status.      
+      IF( STATUS .NE. SAI__OK ) RETURN
+
+*  Begin an AST context.
+      CALL AST_BEGIN( STATUS )
+
+*  Get an AST Object.
+      CALL KPG1_GTOBJ( 'THIS', 'Region', AST_ISAREGION, IAST, 
+     :                 STATUS )
+
+*  Get the required Frame.
+      RESULT = AST_GETREGIONFRAME( IAST, STATUS )
+
+*  Write the results out to a text file.
+      CALL ATL1_PTOBJ( 'RESULT', ' ', RESULT, STATUS )
+
+*  End the AST context.
+      CALL AST_END( STATUS )
+
+*  Give a context message if anything went wrong.
+      IF( STATUS .NE. SAI__OK ) THEN
+         CALL ERR_REP( 'ASTGETREGFRAME_ERR', 'Error getting the '//
+     :                 'Frame represented by a Region.', STATUS )
+      END IF
+
+      END
