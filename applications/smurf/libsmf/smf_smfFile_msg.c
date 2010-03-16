7ed4cf6ead1b0181f87fadc8537f1453fa99c1ea
Tim Jenness <t.jenness@jach.hawaii.edu>
2010-03-16 11:13:55 -1000

    smurf: Add routine to copy filename of smfFile into a message token
    Will optional strip the directory path and can work out the filename
    from the NDF identifier.

---
+++ applications/smurf/libsmf/smf_smfFile_msg.c
@@ -0,0 +1,161 @@
+/*
+*+
+*  Name:
+*     smf_smfFile_msg
+
+*  Purpose:
+*     Convert a smfFile to a message token
+
+*  Language:
+*     Starlink ANSI C
+
+*  Type of Module:
+*     SMURF subroutine
+
+*  Invocation:
+*     smf_smfFile_msg( const smfFile * file, const char token[],
+*                      int strip, const char fallback[], int * status );
+
+*  Arguments:
+*     file = const smfFile * (Given)
+*        smfFile to be read.
+*     token = const char [] (Given)
+*        Name of message token.
+*     strip = int (Given)
+*        If true path information will be stripped.
+*     fallback = const char [] (Given)
+*        Default value for token if a filename can not be obtained.
+*        Token will not be set if this is a null pointer.
+*     status = int* (Given and Returned)
+*        Pointer to global status. If status is bad on entry the token
+*        will not be set.
+
+*  Description:
+*     Store the filename associated with a smfFile into a message token.
+*     Preference is given to the "name" component of the struct. If that is
+*     empty the NDF identifier will be used. Can optionally strip path information.
+
+*  Notes:
+*     Not thread-safe if the ndfMsg path is used.
+
+*  Authors:
+*     Tim Jenness (JAC, Hawaii)
+*     {enter_new_authors_here}
+
+*  History:
+*     2010-03-16 (TIMJ):
+*        Initial version.
+
+*  Copyright:
+*     Copyright (C) 2010 Science & Technology Facilities Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 3 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public
+*     License along with this program; if not, write to the Free
+*     Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+*     MA 02111-1307, USA
+
+*  Bugs:
+*     {note_any_bugs_here}
+*-
+*/
+
+
+#if HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <string.h>
+#include <stdio.h>
+
+#include "ndf.h"
+#include "mers.h"
+#include "sae_par.h"
+#include "msg_par.h"
+#include "star/one.h"
+
+#include "smf.h"
+#include "smurf_par.h"
+
+void
+smf_smfFile_msg( const smfFile * file, const char token[],
+                 int strip, const char fallback[], int * status ) {
+
+  int have_set = 0;         /* have we set the token yet? */
+  char path[GRP__SZNAM+1];  /* Full path information */
+
+  if (*status != SAI__OK) return;
+  if (!file) {
+    if (fallback) msgSetc( token, fallback );
+    return;
+  }
+
+  path[0] = '\0';
+
+  if (file->name && strlen(file->name) > 0 ) {
+    if (strip) {
+      one_strlcpy( path, file->name, sizeof(path), status );
+    } else {
+      /* set directly */
+      msgSetc( token, file->name );
+      have_set = 1;
+    }
+  } else if ( file->ndfid != NDF__NOID ) {
+    if (strip) {
+      const char localtok[] = "_MYTOK_";
+      int oplen = 0;
+      /* we need to get the value from the NDF subsystem
+         and we need to do it without clearing tokens */
+      errMark();
+      ndfMsg( localtok, file->ndfid );
+      msgLoad( "", localtok, path, sizeof(path), &oplen, status );
+      errRlse();
+
+    } else {
+      /* set directly */
+      ndfMsg( token, file->ndfid );
+      have_set = 1;
+    }
+  } else {
+    msgSetc( token, fallback );
+  }
+
+  /* if "path" has stuff in it we are meant to strip it and we have not
+     set the token yet */
+  if (strlen(path) > 0 ) {
+    size_t length = strlen(path);
+    char *endp = &path[length-1];
+
+    /* Do not use the POSIX basename() function here because it does not
+       seem to be thread safe (but then again ndfMsg is not thread safe above).
+       Could do with a one_basename based on the GNU basename implementation. Does
+       not handle all the cases handled by the generic routines. Very naive. */
+    while ( *endp != '/' && endp != path) endp--;
+
+    /* gone one too far */
+    if (endp != path) endp++;
+
+    /* set the token */
+    msgSetc( token, endp);
+    have_set = 1;
+
+  }
+
+  if (!have_set) {
+    msgSetc(token, fallback );
+  }
+
+  return;
+
+}
