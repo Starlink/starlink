14e29943866cb0c26c718d4bc955ac3ab4ac47b0
Ed Chapin <echapin@phas.ubc.ca>
2010-05-27 16:25:07 -0700

    smurf: factor out container init for model expansion
    Every time we expand a model we will need to generate a container
    of the correct dimensions, so factor this out into smf_expmodel_init.

--- applications/smurf/libsmf/smf_expmodel_dks.c
+++ applications/smurf/libsmf/smf_expmodel_dks.c
@@ -13,19 +13,15 @@
 *     Library routine
 
 *  Invocation:
-*     pntr = smf_expmodel_dks( const smfData *indata, smfData **outdata,
-*                              int *status)
+*     smf_expmodel_dks( const smfData *indata, smfData **outdata, int *status)
 
 *  Arguments:
 *     indata = smfData* (Given)
-*        Pointer to the input smfData
+*        Pointer to the input smfData containing a DIMM model.
 *     outdata = smfData** (Returned)
 *        Container for pointer to newly created smfData.
 *     status = int* (Given and Returned)
 *        Pointer to global status.
-
-*  Return Value:
-*     Pointer to newly created smfData containing the expanded data.
 
 *  Description:
 *     Calculate the full time-series representation of a model. A new smfData
@@ -41,6 +37,8 @@
 *  History:
 *     2010-05-19 (EC)
 *        Initial version.
+*     2010-05-27 (EC)
+*        Factored generation of empty container smfData into smf_expmodel_init
 *     {enter_further_changes_here}
 
 *  Copyright:
@@ -82,11 +80,8 @@
 
 #define FUNC_NAME "smf_expmodel_dks"
 
-void smf_expmodel_dks( const smfData *indata, smfData **outdata,
-                           int *status) {
-
+void smf_expmodel_dks( const smfData *indata, smfData **outdata, int *status) {
   /* Local Variables */
-  AstFrameSet *tswcs=NULL;      /* time series wcs for expanded data cube */
   size_t bolo;                  /* bolo number */
   size_t bstride;               /* bolo stride */
   double *d=NULL;               /* pointer to data array */
@@ -94,84 +89,33 @@ void smf_expmodel_dks( const smfData *indata, smfData **outdata,
   double *dksquid=NULL;         /* Pointer to current dark squid */
   double *gainbuf=NULL;         /* Array of gains for all bolos in this col */
   size_t i;                     /* Loop counter */
-  smfHead *inhdr=NULL;          /* input smfHead pointer */
   size_t j;                     /* Loop counter */
   size_t k;                     /* Loop counter */
-  int lbnd_c;
-  int lbnd_r;
-  int lbnd_t;
   dim_t nbolo;                  /* Number of bolometers */
   dim_t ncol;                   /* Number of columns */
-  int ncols;
-  dim_t ndata;                  /* Total number of data points */
-  smfHead *outhdr=NULL;         /* output smfHead pointer */
   dim_t nrow;                   /* Number of rows */
-  int nrows;
   dim_t ntslice;                /* Number of time slices */
-  int ntslices;
   double *offsetbuf=NULL;       /* Array of offsets for all bolos in this col */
   size_t tstride;               /* time stride */
 
   /* Main routine */
   if (*status != SAI__OK) return;
 
-  /* Get the dimensions of the expanded data */
-  inhdr = indata->hdr;
+  /* Create an empty container initialized to VAL__BADD */
+  smf_expmodel_init( indata, &data, status );
+  if( *status != SAI__OK ) return;
 
-  if( !inhdr ) {
+  if( !indata->pntr[0] || !data->pntr[0] ) {
     *status = SAI__ERROR;
-    errRep( "", FUNC_NAME ": model container does not have a header.", status );
-    return;
+    errRep( "", FUNC_NAME ": NULL DATA pointer encountered", status );
   }
 
-  smf_getfitsi( inhdr, "SMFDIMR", &nrows, status );
-  smf_getfitsi( inhdr, "SMFDIMC", &ncols, status );
-  smf_getfitsi( inhdr, "SMFDIMT", &ntslices, status );
-  smf_getfitsi( inhdr, "SMFLBNDR", &lbnd_r, status );
-  smf_getfitsi( inhdr, "SMFLBNDC", &lbnd_c, status );
-  smf_getfitsi( inhdr, "SMFLBNDT", &lbnd_t, status );
+  /* Pointer to output data buffer */
+  d = data->pntr[0];
 
-  nrow = (dim_t) nrows;
-  ncol = (dim_t) ncols;
-  ntslice = (dim_t) ntslices;
-  ndata = nrow*ncol*ntslice;
-
-  /* Create a deep copy of input data exluding the main data arrays or associated
-     file information. Also replace the tswcs with one appropriate for a full
-     time series cube. */
-
-  data = smf_deepcopy_smfData( indata, 0, SMF__NOCREATE_DATA |
-                               SMF__NOCREATE_VARIANCE | SMF__NOCREATE_QUALITY |
-                               SMF__NOCREATE_FILE, status );
-
-  if (!data) return;
-
-  d = astCalloc( ndata, sizeof(*d), 0 );
-  if( *status == SAI__OK ) {
-    for( i=0; i<ndata; i++ ) {
-      d[i] = VAL__BADD;
-    }
-  }
-
-  data->pntr[0] = d;
-  data->ndims = 3;
-  data->dims[SC2STORE__ROW_INDEX] = nrow;
-  data->dims[SC2STORE__COL_INDEX] = ncol;
-  data->dims[2] = ntslice;
-  data->lbnd[SC2STORE__ROW_INDEX] = lbnd_r;
-  data->lbnd[SC2STORE__COL_INDEX] = lbnd_c;
-  data->lbnd[2] = lbnd_t;
-
-  outhdr = data->hdr;
-  smf_create_tswcs( outhdr, &tswcs, status );
-
-  if( outhdr->tswcs ) {
-    outhdr->tswcs = astAnnul( outhdr->tswcs );
-  }
-  outhdr->tswcs = tswcs;
-
-  /* Figure out remaining dimensions information */
-  smf_get_dims( data, NULL, NULL, &nbolo, NULL, NULL, &bstride, &tstride,status);
+  /* Get array dimensions*/
+  smf_get_dims( data, &nrow, &ncol, &nbolo, &ntslice, NULL, &bstride, &tstride,
+                status);
 
   /* Loop over column */
   for( i=0; (*status==SAI__OK)&&(i<ncol); i++ ) {
@@ -201,29 +145,6 @@ void smf_expmodel_dks( const smfData *indata, smfData **outdata,
         }
       }
     }
-  }
-
-  /* Once expanded these are no longer model files, so delete relevant FITS
-     headers */
-  if( (*status==SAI__OK) && (outhdr->fitshdr) ) {
-    AstFitsChan *fits = outhdr->fitshdr;
-
-    astClear( fits, "Card" );
-    if( astFindFits(fits, "SMFMODEL", NULL, 0) ) astDelFits( fits );
-    astClear( fits, "Card" );
-    if( astFindFits(fits, "SMFLBNDR", NULL, 0) ) astDelFits( fits );
-    astClear( fits, "Card" );
-    if( astFindFits(fits, "SMFDIMR", NULL, 0) ) astDelFits( fits );
-    astClear( fits, "Card" );
-    if( astFindFits(fits, "SMFDIMC", NULL, 0) ) astDelFits( fits );
-    astClear( fits, "Card" );
-    if( astFindFits(fits, "SMFDIMT", NULL, 0) ) astDelFits( fits );
-    astClear( fits, "Card" );
-    if( astFindFits(fits, "SMFLBNDR", NULL, 0) ) astDelFits( fits );
-    astClear( fits, "Card" );
-    if( astFindFits(fits, "SMFLBNDC", NULL, 0) ) astDelFits( fits );
-    astClear( fits, "Card" );
-    if( astFindFits(fits, "SMFLBNDT", NULL, 0) ) astDelFits( fits );
   }
 
   /* Return pointer to new data */
---
+++ applications/smurf/libsmf/smf_expmodel_init.c
@@ -0,0 +1,192 @@
+/*
+*+
+*  Name:
+*     smf_expmodel_init
+
+*  Purpose:
+*     Create an empty expanded model container
+
+*  Language:
+*     Starlink ANSI C
+
+*  Type of Module:
+*     Library routine
+
+*  Invocation:
+*     pntr = smf_expmodel_init( const smfData *indata, smfData **outdata,
+*                               int *status)
+
+*  Arguments:
+*     indata = smfData* (Given)
+*        Pointer to the input smfData containing a DIMM model
+*     outdata = smfData** (Returned)
+*        Container for pointer to newly created smfData, initialized to VAL__BADD
+*     status = int* (Given and Returned)
+*        Pointer to global status.
+
+*  Description:
+*     All model expansion routines require a container with the dimensions of
+*     the full bolometer data cube, and the time-series WCS information. This
+*     routine creates that container using information in the headers. Models
+*     FITS headers contain several entries to help: SMFDIM* and SMFLBND* which
+*     given the LBND and lengths along each axis respectively, They also contain
+*     a SMFMODEL keyword giving the 3-character model type. These are all
+*     deleted in the returned container to avoid confusing the expanded model
+*     with a compressed model.
+
+*  Notes:
+
+*  Authors:
+*     Edward Chapin (UBC)
+*     {enter_new_authors_here}
+
+*  History:
+*     2010-05-27 (EC)
+*        Initial version -- factored out of smf_expmodel_dks.c
+*     {enter_further_changes_here}
+
+*  Copyright:
+*     Copyright (C) 2010 University of British Columbia.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 3 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public
+*     License along with this program; if not, write to the Free
+*     Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+*     MA 02111-1307, USA
+
+*  Bugs:
+*     {note_any_bugs_here}
+*-
+*/
+
+/* Starlink includes */
+#include "mers.h"
+#include "ndf.h"
+#include "sae_par.h"
+#include "star/ndg.h"
+#include "prm_par.h"
+#include "par_par.h"
+
+/* SMURF includes */
+#include "libsmf/smf.h"
+#include "sc2da/sc2store.h"
+
+#define FUNC_NAME "smf_expmodel_init"
+
+void smf_expmodel_init( const smfData *indata, smfData **outdata, int *status) {
+
+  /* Local Variables */
+  double *d=NULL;               /* pointer to data array */
+  smfData *data=NULL;           /* newly created smfData */
+  size_t i;                     /* Loop counter */
+  smfHead *inhdr=NULL;          /* input smfHead pointer */
+  int lbnd_c;
+  int lbnd_r;
+  int lbnd_t;
+  dim_t ncol;                   /* Number of columns */
+  int ncols;
+  dim_t ndata;                  /* Total number of data points */
+  smfHead *outhdr=NULL;         /* output smfHead pointer */
+  dim_t nrow;                   /* Number of rows */
+  int nrows;
+  dim_t ntslice;                /* Number of time slices */
+  int ntslices;
+  AstFrameSet *tswcs=NULL;      /* time series wcs for expanded data cube */
+
+
+  /* Main routine */
+  if (*status != SAI__OK) return;
+
+  /* Get the dimensions of the expanded data */
+  inhdr = indata->hdr;
+
+  if( !inhdr ) {
+    *status = SAI__ERROR;
+    errRep( "", FUNC_NAME ": model container does not have a header.", status );
+    return;
+  }
+
+  smf_getfitsi( inhdr, "SMFDIMR", &nrows, status );
+  smf_getfitsi( inhdr, "SMFDIMC", &ncols, status );
+  smf_getfitsi( inhdr, "SMFDIMT", &ntslices, status );
+  smf_getfitsi( inhdr, "SMFLBNDR", &lbnd_r, status );
+  smf_getfitsi( inhdr, "SMFLBNDC", &lbnd_c, status );
+  smf_getfitsi( inhdr, "SMFLBNDT", &lbnd_t, status );
+
+  nrow = (dim_t) nrows;
+  ncol = (dim_t) ncols;
+  ntslice = (dim_t) ntslices;
+  ndata = nrow*ncol*ntslice;
+
+  /* Create a deep copy of input data excluding the main data arrays
+     or associated file information. Also replace the tswcs with one
+     appropriate for a full time series cube. */
+
+  data = smf_deepcopy_smfData( indata, 0, SMF__NOCREATE_DATA |
+                               SMF__NOCREATE_VARIANCE | SMF__NOCREATE_QUALITY |
+                               SMF__NOCREATE_FILE, status );
+
+  if (!data) return;
+
+  d = astCalloc( ndata, sizeof(*d), 0 );
+  if( *status == SAI__OK ) {
+    for( i=0; i<ndata; i++ ) {
+      d[i] = VAL__BADD;
+    }
+  }
+
+  data->pntr[0] = d;
+  data->ndims = 3;
+  data->dims[SC2STORE__ROW_INDEX] = nrow;
+  data->dims[SC2STORE__COL_INDEX] = ncol;
+  data->dims[2] = ntslice;
+  data->lbnd[SC2STORE__ROW_INDEX] = lbnd_r;
+  data->lbnd[SC2STORE__COL_INDEX] = lbnd_c;
+  data->lbnd[2] = lbnd_t;
+
+  outhdr = data->hdr;
+  smf_create_tswcs( outhdr, &tswcs, status );
+
+  if( outhdr->tswcs ) {
+    outhdr->tswcs = astAnnul( outhdr->tswcs );
+  }
+  outhdr->tswcs = tswcs;
+
+  /* Once expanded these are no longer model files, so delete relevant FITS
+     headers */
+  if( (*status==SAI__OK) && (outhdr->fitshdr) ) {
+    AstFitsChan *fits = outhdr->fitshdr;
+
+    astClear( fits, "Card" );
+    if( astFindFits(fits, "SMFMODEL", NULL, 0) ) astDelFits( fits );
+    astClear( fits, "Card" );
+    if( astFindFits(fits, "SMFLBNDR", NULL, 0) ) astDelFits( fits );
+    astClear( fits, "Card" );
+    if( astFindFits(fits, "SMFDIMR", NULL, 0) ) astDelFits( fits );
+    astClear( fits, "Card" );
+    if( astFindFits(fits, "SMFDIMC", NULL, 0) ) astDelFits( fits );
+    astClear( fits, "Card" );
+    if( astFindFits(fits, "SMFDIMT", NULL, 0) ) astDelFits( fits );
+    astClear( fits, "Card" );
+    if( astFindFits(fits, "SMFLBNDR", NULL, 0) ) astDelFits( fits );
+    astClear( fits, "Card" );
+    if( astFindFits(fits, "SMFLBNDC", NULL, 0) ) astDelFits( fits );
+    astClear( fits, "Card" );
+    if( astFindFits(fits, "SMFLBNDT", NULL, 0) ) astDelFits( fits );
+  }
+
+  /* Return pointer to new data */
+  if( outdata ) *outdata = data;
+
+}
