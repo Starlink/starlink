Starlink/starlink

commit 9219db02e471b56d2320bf513a5aa70b6e66cee1

smurf: add DKS cleaning to map-maker, quality arrays to DKS
The default dimmconfig now has cleandk parameters defined for use
when the DKS model is turned on. I also added quality to DKS
to make it easier to keep track of what is going on -- it is
generated by smf_open_file and smf_concat_smfGroup.

applications/smurf/libsmf/smf_concat_smfGroup.c
@@ -145,6 +145,8 @@
  *        Add flatramps argument.
  *     2010-05-27 (EC):
  *        Calculate tswcs properly when padding using smf_create_tswcs
+ *     2010-06-10 (EC):
+ *        Handle dark squid QUALITY
 
  *  Notes:
  *     If projection information supplied, pointing LUT will not be
@@ -472,17 +474,21 @@ void smf_concat_smfGroup( smfWorkForce *wf, const smfGroup *igrp,
           if( *status == SAI__OK ) {
             /* If first chunk initialize the concatenated array */
             if( j == firstpiece ) {
+              int cflags=0;
+
               /* Copy first data right after the initial padding */
               tchunk = padStart;
 
               /* Allocate memory for empty smfData with a smfHead. Create
-                 a DA struct only if the input file has one. Create it as
-                 a clone rather than creating an empty smfDa. */
-              data = smf_create_smfData( SMF__NOCREATE_DA, status );
-              if (refdata->da) {
-                da = smf_deepcopy_smfDA( refdata, status );
-                data->da = da;
+                 a DA struct only if the input file has dark squids. */
+
+              if( !(refdata->da && refdata->da->dksquid) ) {
+                cflags |= SMF__NOCREATE_DA;
               }
+
+              data = smf_create_smfData( cflags, status );
+              da = data->da;
+
               if (refdata->history) data->history = astCopy( refdata->history );
 
               if( *status == SAI__OK ) {
@@ -566,16 +572,38 @@ void smf_concat_smfGroup( smfWorkForce *wf, const smfGroup *igrp,
                               &bstr, &tstr, status );
 
                 /* Allocate space for enlarged dksquid array. */
-                if( da && da->dksquid) {
-                  da->dksquid->pntr[0] = astRealloc(da->dksquid->pntr[0],
-                                                    ncol*tlen *
-                                                    smf_dtype_size(da->dksquid,
-                                                                   status) );
+                if( da ) {
+                  da->dksquid = smf_create_smfData(SMF__NOCREATE_FILE |
+                                                   SMF__NOCREATE_HEAD |
+                                                   SMF__NOCREATE_DA, status );
 
-                  if( *status == SAI__OK ) {
-                    memset( da->dksquid->pntr[0], 0,
-                            ncol*tlen*smf_dtype_size(da->dksquid,status) );
+                  /* Dimensions */
+                   da->dksquid->dtype = SMF__DOUBLE;
+                   da->dksquid->isTordered = 1;
+                   da->dksquid->ndims = 3;
+
+                   da->dksquid->dims[0] = ncol;
+                   da->dksquid->dims[1] = 1;
+                   da->dksquid->dims[2] = tlen;
+                   da->dksquid->lbnd[0] = 0;
+                   da->dksquid->lbnd[1] = 0;
+                   da->dksquid->lbnd[2] = 1;
+
+                  da->dksquid->pntr[0] = astCalloc(ncol*tlen,
+                                                   smf_dtype_size(da->dksquid,
+                                                                  status), 1 );
+
+                  /* QUALITY too if requested */
+                  if( !(flags&SMF__NOCREATE_QUALITY) ) {
+                    da->dksquid->pntr[2] = astCalloc(ncol*tlen,
+                                                     smf_dtype_sz(SMF__UBYTE,
+                                                                  status), 1 );
+                    if( *status == SAI__OK ) {
+                      memset( da->dksquid->pntr[2], 0,
+                              ncol*tlen*smf_dtype_sz(SMF__UBYTE,status) );
+                    }
                   }
+
                 }
 
                 /* Un-set havearray values corresponding to flags */
@@ -639,9 +667,17 @@ void smf_concat_smfGroup( smfWorkForce *wf, const smfGroup *igrp,
               /* dark squids */
               if( da && da->dksquid && refdata->da && refdata->da->dksquid) {
                 double *ptr = da->dksquid->pntr[0];
+                unsigned char *qptr = da->dksquid->pntr[2];
+
                 ptr += tchunk*ncol;
                 memcpy( ptr, refdata->da->dksquid->pntr[0],
                         reftlen*ncol*smf_dtype_size(da->dksquid,status));
+
+                if( qptr && refdata->da->dksquid->pntr[2] ) {
+                  qptr += tchunk*ncol;
+                  memcpy( qptr, refdata->da->dksquid->pntr[2],
+                          reftlen*ncol*smf_dtype_sz(SMF__UBYTE,status));
+                }
               }
 
               /* Now do DATA/QUALITY/VARIANCE */
@@ -720,6 +756,30 @@ void smf_concat_smfGroup( smfWorkForce *wf, const smfGroup *igrp,
             /* Loop over relevant time slices at set quality */
             for( l=tstart; l<=tend; l++ ) {
               ((char *)data->pntr[2])[l*tstr+k*bstr] |=  qual;
+            }
+          }
+        }
+
+        /* Similarly handle QUALITY for dark squids */
+        if( da && da->dksquid && da->dksquid->pntr[2] ) {
+          dim_t dnbolo;
+          size_t dbstr, dtstr;
+
+          smf_get_dims( da->dksquid, NULL, NULL, &dnbolo, NULL, NULL, &dbstr,
+                        &dtstr, status );
+
+           /* Loop over bolometer (column for dark squids) */
+          for( k=0; k<dnbolo; k++ ) {
+            /* SMF__Q_PAD always set */
+            qual = SMF__Q_PAD;
+
+            /* Check for SMF__Q_BADB in first sample of this column */
+            qual |= ((char *)da->dksquid->pntr[2])[padStart*dtstr+k*dbstr] &
+              SMF__Q_BADB;
+
+            /* Loop over relevant time slices at set quality */
+            for( l=tstart; l<=tend; l++ ) {
+              ((char *)da->dksquid->pntr[2])[l*dtstr+k*dbstr] |=  qual;
             }
           }
         }

applications/smurf/examples/dimmconfig.lis
@@ -161,6 +161,29 @@ fillgaps = 1
 #filt_notchlow  = (25,55)
 #filt_notchhigh = (35,65)
 
+# ----------------------------------------------------------------------------
+# A number of analagous parameters to clean dark squid signals before fitting
+# them to the data (only relevant if DKS is specified as a model component).
+# Note that if padding has been added to the bolometer data, it is
+# automatically added to the dark squids as well (i.e. there is no
+# cleandk.padstart/padend).
+# ----------------------------------------------------------------------------
+
+cleandk.apod = 100
+cleandk.badfrac = 0.05
+cleandk.dcfitbox = 400
+cleandk.dcmaxsteps = 10
+cleandk.dcthresh = 3.0
+cleandk.dcmedianwidth = 40
+cleandk.dclimcorr = 10
+cleandk.fillgaps = 1
+#cleandk.filt_edgelow = 0
+#cleandk.filt_edgehigh = 0
+#cleandk.filt_notchlow = <undef>
+#cleandk.filt_notchhigh = <undef>
+cleandk.order = 0
+#cleandk.spikethresh = 0
+#cleandk.spikeiter = 0
 
 # ----------------------------------------------------------------------------
 # These parameters control the iterative model components

applications/smurf/libsmf/smf_clean_dksquid.c
@@ -88,6 +88,8 @@
 *        Report number of new BADBOL if replacebad=0
 *     2010-06-01 (EC):
 *        Only fit gain of DKS, not gain and offset (to avoid COM offset degen.)
+*     2010-06-10 (EC):
+*        Don't remove the means here since that is handled by smf_clean_smfData
 
 *  Copyright:
 *     Copyright (C) 2009 Science & Technology Facilities Council.
@@ -194,6 +196,9 @@ void smf_clean_dksquid( smfData *indata, unsigned char *quality,
               status);
       return;
     }
+
+    /* Assert the correct data order here */
+    smf_dataOrder( indata->da->dksquid, 1, status );
   }
 
   /* Check for 3-d data and get dimensions */
@@ -302,18 +307,8 @@ void smf_clean_dksquid( smfData *indata, unsigned char *quality,
 
         /* Do some dksquid initialization if requested  */
         if( (*status==SAI__OK) && needDA && calcdk && model && dkgood[i] ) {
-          double mean;
-
           /* Smooth the dark squid template */
           smf_boxcar1D( &dksquid[jt1], ntot, window, NULL, 0, status );
-
-          /* Also remove the mean because it isn't useful. */
-          smf_stats1D( &dksquid[jt1], 1, ntot, NULL, 0, 0, &mean, 0, NULL,
-                       status );
-
-          for( j=jt1; (*status==SAI__OK)&&(j<=jt2); j++ ) {
-            dksquid[j] -= mean;
-          }
         }
 
         /* Initialize fit coeffs to VAL__BADD */

applications/smurf/libsmf/smf_model_create.c
@@ -192,6 +192,8 @@
 *        Add SMO smoothing model
 *     2010-06-08 (EC):
 *        Add TWO component model
+*     2010-06-10 (EC):
+*        Add dark squid cleaning
 *     {enter_further_changes_here}
 
 *  Copyright:
@@ -374,8 +376,9 @@ void smf_model_create( smfWorkForce *wf, const smfGroup *igroup, smfArray **iarr
 
   oflag = 0;
 
-  /* Only map head if creating LUT, EXT or QUA */
-  if( (mtype != SMF__LUT) && (mtype != SMF__EXT) && (mtype != SMF__QUA) ) {
+  /* Only map head if creating LUT, EXT, QUA or DKS */
+  if( (mtype != SMF__LUT) && (mtype != SMF__EXT) && (mtype != SMF__QUA) &&
+      (mtype != SMF__DKS) ) {
     oflag |= SMF__NOCREATE_HEAD;
   }
 
@@ -953,7 +956,32 @@ void smf_model_create( smfWorkForce *wf, const smfGroup *igroup, smfArray **iarr
               /* First set the entire buffer to 0 */
               memset( dataptr, 0, datalen );
 
-              /* Check for special DKS initialzation parameters */
+              /* Apply basic DKS cleaning parameters */
+              if( idata->da && idata->da->dksquid ) {
+                smfData *dksquid = idata->da->dksquid;
+
+                msgOutif( MSG__VERB, "", FUNC_NAME ": cleaning dark squids",
+                          status);
+
+                /* fudge the header so that we can get at JCMTState */
+                dksquid->hdr = idata->hdr;
+
+                /* clean darks using cleandk.* parameters */
+                astMapGet0A( keymap, "CLEANDK", &kmap );
+                smf_clean_smfData( wf, dksquid, NULL, kmap, status );
+                if( kmap ) kmap = astAnnul( kmap );
+
+                /* Unset hdr pointer so that we don't accidentally close it */
+                dksquid->hdr = NULL;
+
+              } else {
+                *status = SAI__ERROR;
+                errRep( "", FUNC_NAME
+                        ": Reference smfData does not contain dark squids",
+                        status );
+              }
+
+              /* Check for additional special DKS initialzation parameters */
               astMapGet0A( keymap, "DKS", &kmap );
               astMapGet0I( kmap, "REPLACEBAD", &replacebad );
               kmap = astAnnul( kmap );

applications/smurf/libsmf/smf_open_file.c
@@ -206,6 +206,8 @@
  *        Store dark squids in a smfData instead of a bare buffer
  *     2010-06-03 (EC):
  *        Map the dark squids even if data were already flatfielded
+ *     2010-06-10 (EC):
+ *        Add quality to dark squids
  *     {enter_further_changes_here}
 
  *  Copyright:
@@ -558,8 +560,9 @@ void smf_open_file( const Grp * igrp, size_t index, const char * mode,
         int dkndims;
         int dkndf;
         int dkplace;
+        int dqexists;
         int nmap;
-        void *pntr[1];
+        void *dpntr[] = {NULL,NULL,NULL};
 
         ndfXstat( indf, "SCUBA2", &itexists, status );
         if( itexists ) {
@@ -581,12 +584,30 @@ void smf_open_file( const Grp * igrp, size_t index, const char * mode,
             errRepf("", FUNC_NAME ": DKSQUID has %i dimensions, should be 2",
                     status, dkndims);
           }
-          ndfMap( dkndf, "DATA", "_DOUBLE", mode, pntr, &nmap, status );
+          ndfMap( dkndf, "DATA", "_DOUBLE", mode, &dpntr[0], &nmap, status );
+
+          /* Also generically map/create a quality array unless we're
+             in READ mode and one didn't previously exist */
+          ndfState( dkndf, "QUALITY", &dqexists, status );
+          if( dqexists ) {
+            msgOutif( MSG__DEBUG, "", "Mapping existing DKSQUID QUALITY",
+                      status);
+            ndfMap( dkndf, "QUALITY", "_UBYTE", mode, &dpntr[2], &nmap,
+                    status );
+          } else if( !strncmp(mode,"READ",4) ) {
+            msgOutif( MSG__DEBUG, "", "Creating new DKSQUID QUALITY",
+                      status);
+            ndfMap( dkndf, "QUALITY", "_UBYTE", "WRITE/ZERO", &dpntr[2], &nmap,
+                    status );
+          }
+
+
           if(*status == SAI__OK) {
             da = (*data)->da;
             da->dksquid = smf_create_smfData( SMF__NOCREATE_HEAD |
                                               SMF__NOCREATE_DA, status );
-            da->dksquid->pntr[0] = pntr[0];
+            da->dksquid->pntr[0] = dpntr[0];
+            da->dksquid->pntr[2] = dpntr[2];
 
             da->dksquid->dtype = SMF__DOUBLE;
             da->dksquid->isTordered = 1;
@@ -851,6 +872,11 @@ void smf_open_file( const Grp * igrp, size_t index, const char * mode,
               ptr[i] = (double) dksquid[i];
             }
           }
+
+          /* Create an empty QUALITY array */
+          da->dksquid->pntr[2] = astCalloc( rowsize*nframes,
+                                            smf_dtype_sz(SMF__UBYTE,status),
+                                            1);
         }
 
         /* Create a FitsChan from the FITS headers */

applications/smurf/libsmf/smf_write_smfData.c
@@ -86,6 +86,8 @@
 *        Write out dark squids
 *     2010-01-29 (TIMJ):
 *        Fix writing of 3d variance for 3d data
+*     2010-06-10 (EC):
+*        Write dark squid quality
 *     {enter_further_changes_here}
 
 *  Copyright:
@@ -358,7 +360,8 @@ void smf_write_smfData( const smfData *data, const smfData *variance,
       HDSLoc *loc=NULL;
       int id;
       int nmap;
-      void *pntr[1];
+      void *pntr[]={NULL,NULL,NULL};
+      unsigned char *outdkqual=NULL;
       double *outdksquid=NULL;
 
       if( da && da->dksquid && da->dksquid->pntr[0] && outdata &&
@@ -376,12 +379,21 @@ void smf_write_smfData( const smfData *data, const smfData *variance,
         id = smf_get_ndfid( loc, "DKSQUID", "WRITE", "UNKNOWN", "_DOUBLE",
                             2, lbnd, ubnd, status );
 
-        ndfMap( id, "DATA", "_DOUBLE", "WRITE", pntr, &nmap, status );
+        ndfMap( id, "DATA", "_DOUBLE", "WRITE", &pntr[0], &nmap, status );
         outdksquid = pntr[0];
 
-        if( (*status == SAI__OK) && (outdksquid) ) {
-          memcpy( outdksquid, da->dksquid->pntr[0],
-                  nmap*sizeof(*outdksquid) );
+        if( (*status == SAI__OK) && outdksquid ) {
+          memcpy( outdksquid, da->dksquid->pntr[0], nmap*sizeof(*outdksquid) );
+        }
+
+        /* Also do the quality if it exists */
+        if( da->dksquid->pntr[2] ) {
+          ndfMap( id, "QUALITY", "_UBYTE", "WRITE", &pntr[2], &nmap, status );
+          outdkqual = pntr[2];
+
+          if( (*status==SAI__OK) && outdkqual ) {
+            memcpy( outdkqual, da->dksquid->pntr[2], nmap*sizeof(*outdkqual) );
+          }
         }
 
         ndfAnnul( &id, status );

applications/smurf/libsmurf/smurf_sc2clean.c
@@ -190,6 +190,8 @@
 *        Add sub-instrument support to config reading.
 *        Read the config file inside the loop so that the command will
 *        run properly with files that are from different sub-instruments.
+*     2010-06-10 (EC):
+*        Dark squids now have their own quality.
 *     {enter_further_changes_here}
 
 *  Copyright:
@@ -329,25 +331,18 @@ void smurf_sc2clean( int *status ) {
         smfData *dksquid = ffdata->da->dksquid;
         AstKeyMap *kmap=NULL;
         dim_t ndata;
-        unsigned char *dkqual=NULL;
 
         msgOut("", TASK_NAME ": cleaning dark squids", status);
 
-        /* Temporary quality buffer for dksquid */
-        smf_get_dims( dksquid, NULL, NULL, NULL, NULL, &ndata, NULL, NULL,
-                      status );
-        dkqual = astCalloc( ndata, sizeof(*dkqual), 1 );
-
-        /* also fudge the header so that we can get at JCMTState */
+        /* fudge the header so that we can get at JCMTState */
         dksquid->hdr = ffdata->hdr;
 
         /* clean darks using cleandk.* parameters */
         astMapGet0A( keymap, "CLEANDK", &kmap );
-        smf_clean_smfData( wf, dksquid, dkqual, kmap, status );
+        smf_clean_smfData( wf, dksquid, NULL, kmap, status );
         if( kmap ) kmap = astAnnul( kmap );
-        if (dkqual) dkqual = astFree( dkqual );
 
-        /* Set hdr pointer to NULL again so that we don't accidentally close it*/
+        /* Unset hdr pointer so that we don't accidentally close it */
         dksquid->hdr = NULL;
       }
 

commit 717f359794e91f2fbdde20a4b33bb666a7c1bb4e

smurf: we need to copy all of smfDA not just dksquid
Commit 9219db02e471b56d2320bf513a5aa70b6e66cee1 broke
smf_concat_smfGroup so that flatfield information was
ignored and only dark squid information was copied.

Revert to previous behaviour of copying all of the
smfDA struct.

Also set qfamily in dksquid smfData.

applications/smurf/libsmf/smf_concat_smfGroup.c
@@ -149,6 +149,9 @@
  *        Handle dark squid QUALITY
  *     2010-06-14 (TIMJ):
  *        Refactor loops for separate quality component in smfData
+ *     2010-07-01 (TIMJ):
+ *        Fix smfDA logic so that we again copy more than just
+ *        the dksquid item.
 
  *  Notes:
  *     If projection information supplied, pointing LUT will not be
@@ -475,20 +478,19 @@ void smf_concat_smfGroup( smfWorkForce *wf, const smfGroup *igrp,
           if( *status == SAI__OK ) {
             /* If first chunk initialize the concatenated array */
             if( j == firstpiece ) {
-              int cflags=0;
 
               /* Copy first data right after the initial padding */
               tchunk = padStart;
 
               /* Allocate memory for empty smfData with a smfHead. Create
-                 a DA struct only if the input file has dark squids. */
-
-              if( !(refdata->da && refdata->da->dksquid) ) {
-                cflags |= SMF__NOCREATE_DA;
+                 a DA struct only if the input file has one. Create it as
+                 a clone rather than creating an empty smfDa. */
+              data = smf_create_smfData( SMF__NOCREATE_DA, status );
+              if (refdata->da && data) {
+                /* do not copy dark squids. We do that below */
+                data->da = smf_deepcopy_smfDA( refdata, 0, status );
+                da = data->da;
               }
-
-              data = smf_create_smfData( cflags, status );
-              da = data->da;
 
               if (refdata->history) data->history = astCopy( refdata->history );
 
@@ -597,6 +599,7 @@ void smf_concat_smfGroup( smfWorkForce *wf, const smfGroup *igrp,
 
                   /* QUALITY too if requested */
                   if( !(flags&SMF__NOCREATE_QUALITY) ) {
+                    da->dksquid->qfamily = refdata->da->dksquid->qfamily;
                     da->dksquid->qual = astCalloc(ncol*tlen, sizeof(*(da->dksquid->qual)), 1);
                     if( *status == SAI__OK ) {
                       memset( da->dksquid->qual, 0,