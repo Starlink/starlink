2d18826af4d82a50441d5ad40417a8e94fb58f63
Malcolm J. Currie <mjc@star.rl.ac.uk>
2010-10-13 17:29:54 +0000

    KAPPA: Permit temporary style attributes in DRAWSIG.

--- applications/kappa/libkapview/drawsig.f
+++ applications/kappa/libkapview/drawsig.f
@@ -82,39 +82,47 @@
 *        for the lines.
 *
 *        A comma-separated list of strings should be given in which each
-*        string is either an attribute setting, or the name of a text file
-*        preceded by an up-arrow character "^". Such text files should
-*        contain further comma-separated lists which will be read and
-*        interpreted in the same manner. Attribute settings are applied in
-*        the order in which they occur within the list, with later settings
-*        over-riding any earlier settings given for the same attribute.
+*        string is either an attribute setting, or the name of a text
+*        file preceded by an up-arrow character "^".  Such text files
+*        should contain further comma-separated lists which will be read
+*        and interpreted in the same manner.  Attribute settings are
+*        applied in the order in which they occur within the list, with
+*        later settings overriding any earlier settings given for the
+*        same attribute.
 *
 *        Each individual attribute setting should be of the form:
 *
 *           <name>=<value>
 *
-*        where <name> is the name of a plotting attribute, and <value> is
-*        the value to assign to the attribute. Default values will be
-*        used for any unspecified attributes. All attributes will be
-*        defaulted if a null value (!) is supplied. See section "Plotting
-*        Attributes" in SUN/95 for a description of the available
-*        attributes. Any unrecognised attributes are ignored (no error is
-*        reported).
+*        where <name> is the name of a plotting attribute, and <value>
+*        is the value to assign to the attribute.  Default values will
+*        be used for any unspecified attributes.  All attributes will be
+*        defaulted if a null value (!)---the initial default---is
+*        supplied.  To apply changes of style to only the current
+*        invocation, begin these attributes with a plus sign.  A mixture
+*        of persistent and temporary style changes is achieved by
+*        listing all the persistent attributes followed by a plus sign
+*        then the list temporary attributes.
+*
+*        See section "Plotting Attributes" in SUN/95 for a description
+*        of the available attributes.  Any unrecognised attributes are
+*        ignored (no error is reported).
 *
 *        The attributes Colour(Curves), Width(Curves), etc, can be used
 *        to specify the style for the lines ("Lines" is recognised as a
-*        synonym for "Curves"). These values apply to all
-*        lines unless subsequent attributes over-ride them. Attributes for
-*        individual clipping levels can be given by replacing "Curves" above
-*        by a string of the form "Nsig<i>" where "<i>" is an integer
-*        index into the list of clipping levels supplied for parameter
-*        NSIGMA. Thus, "Colour(Nsig1)" will set the colour for the lines
-*        associated with the first clipping level, etc. The attribute
-*        settings can be restricted to one of the two lines by appending
-*        either a "+" or a "-" to the "Nsig<i>" string. Thus,
-*        "Width(Nsig2-)" sets the line width for the lower of the two
-*        lines associated with the second clipping level, and "Width(Nsig2+)"
-*        sets the width for the upper of the two lines. [current value]
+*        synonym for "Curves").  These values apply to all lines unless
+*        subsequent attributes over-ride them.  Attributes for
+*        individual clipping levels can be given by replacing "Curves"
+*        above by a string of the form "Nsig<i>" where "<i>" is an
+*        integer index into the list of clipping levels supplied for
+*        Parameter NSIGMA.  Thus, "Colour(Nsig1)" will set the colour
+*        for the lines associated with the first clipping level, etc.
+*        The attribute settings can be restricted to one of the two
+*        lines by appending either a "+" or a "-" to the "Nsig<i>"
+*        string. Thus, "Width(Nsig2-)" sets the line width for the
+*        lower of the two lines associated with the second clipping
+*        level, and "Width(Nsig2+)" sets the width for the upper of the
+*        two lines.  [current value]
 
 *  Examples:
 *     drawsig nsigma=3 style='style=1'
@@ -157,7 +165,9 @@
 
 *  Copyright:
 *     Copyright (C) 1996, 1998-1999, 2004 Central Laboratory of the
-*     Research Councils. All Rights Reserved.
+*     Research Councils.
+*     Copyright (C) 2010 Science & technology FDacilities Council.
+*     All Rights Reserved.
 
 *  Licence:
 *     This program is free software; you can redistribute it and/or
@@ -196,7 +206,9 @@
 *     27-OCT-1999 (DSB):
 *        Major changes to use AST/PGPLOT for the graphics.
 *     2004 September 3 (TIMJ):
-*        Use CNF_PVAL
+*        Use CNF_PVAL.
+*     2010 October 13 (MJC):
+*        Permit temporary style attributes.
 *     {enter_further_changes_here}
 
 *-
@@ -455,8 +467,15 @@
          CALL CHR_APPND( '+)', SYN, IAT )
          CALL KPG1_ASPSY( SYN( : IAT ), '(CURVE)', STATUS )
 
-*  Set the plotting style for the upper (positive) line.
-         CALL KPG1_ASSET( 'KAPPA_DRAWSIG', 'STYLE', IPLOT, STATUS )
+*  Set the plotting style for the upper (positive) line.  The plus
+*  prefix requests support of temporary attributes.  Also allow for
+*  more than one call to KPG1_ASSET for STYLE by using the delimiter
+*  suffix.
+         IF ( NCLIP .EQ. 1 ) THEN
+            CALL KPG1_ASSET( 'KAPPA_DRAWSIG', '+STYLE+', IPLOT, STATUS )
+         ELSE
+            CALL KPG1_ASSET( 'KAPPA_DRAWSIG', '+STYLE', IPLOT, STATUS )
+         END IF
 
 *  Draw the first line.
          START( DATAX ) = MEAN + STDEV*CLIP( I )
@@ -484,8 +503,9 @@
             CALL CHR_APPND( '-)', SYN, IAT )
             CALL KPG1_ASPSY( SYN( : IAT ), '(CURVE)', STATUS )
 
-*  Set the plotting style for the upper (positive) line.
-            CALL KPG1_ASSET( 'KAPPA_DRAWSIG', 'STYLE', IPLOT, STATUS )
+*  Set the plotting style for the lower (negative) line.  The plus
+*  requests support of temporary attributes.
+            CALL KPG1_ASSET( 'KAPPA_DRAWSIG', '+STYLE', IPLOT, STATUS )
 
 *  Draw the line.
             START( DATAX ) = MEAN - STDEV*CLIP( I )
