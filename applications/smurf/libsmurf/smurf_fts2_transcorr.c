51663e0879f16ceda9009a0d78fb74aebf284a79
Coskun Oba <josh.oba@uleth.ca>
2010-11-23 16:35:52 -0700

    smurf: Update to atmospheric transmission correction

--- applications/smurf/libsmurf/smurf_fts2_transcorr.c
+++ applications/smurf/libsmurf/smurf_fts2_transcorr.c
@@ -24,6 +24,17 @@
 *     given the current PWV and elevation. Transmission data is stored in the
 *     form of a wet and dry Tau vs frequency table in the calibration database.
 
+*  ADAM Parameters:
+*     IN = NDF (Read)
+*          Input files to be transformed.
+*     OUT = NDF (Write)
+*          Output files.
+*     TAU = NDF (Read)
+*          TAU files.
+*     DEBUG = LOGICAL (Read)
+*          If debug, does NOT include the dry component of the atnoshpheric
+*          transmission in the computation
+
 *  Authors:
 *     COBA: Coskun Oba (UoL)
 
@@ -88,156 +99,286 @@
 
 void smurf_fts2_transcorr(int* status)
 {
-  printf("FTS2TRANSCORR...: NOT IMPLEMENTED YET!\n");
-
   if( *status != SAI__OK ) { return; }
 
-/*
-  int i = 0;
-  int j = 0;
-  int k = 0;
-  int index = 0;
-  int pixelIndex = 0;
-  int width = 0;
-  int height = 0;
-  int pixelCount = 0;
-  double airMass = 0;
-  double PWV = 0;
-  double tauFactor = 0.0;
-  double* dryData = NULL;
-  size_t dryCount = 0;
-  size_t fIndex = 0;
+  char filename[GRP__SZNAM+1]; /* Filename */
+  char *pname        = NULL; /* Pointer to filename */
+  int bolCount       = 0;    /* Number of bolometers */
+  int bolIndex       = 0;    /* Bolometer index */
+  int count;
+  int dims[NDF__MXDIM];
+  int debug          = 0;    /* If not debug, include dry component */
+  int ftsExists      = 0;
+  int index          = 0;
+  int indf;                  /* NDF identifier for TAU file */
+  int KERNELLENGTH   = 101;
+  int nbolX          = 0;    /* Width of the source subarray */
+  int nbolY          = 0;    /* Height of the source subarray */
+  int ndfTau;
+  int ndim;
+  int nPWV           = 0;
+  int nWN            = 0;
+  int N              = 0;    /* Sample count */
+  int i              = 0;    /* Index */
+  int j              = 0;    /* Index */
+  int k              = 0;    /* Index */
+  int place;
+  double AM          = 0.0;  /* Airmass at ZPD */
+  double DELTAPWV    = 0.0;
+  double PWV0        = 0.0;
+  double PWV         = 0.0;  /* PWV at ZPD */
+  double wnFact      = 0.0;  /* Wave number factor */
+  double* inPntr     = NULL; /* Pointer to the input data */
+  double* outPntr    = NULL; /* Pointer to the output data */
+  double* wnScan     = NULL;
+  double* wnTau      = NULL;
+  double* TAtm       = NULL;
+  double* TAtmNew    = NULL;
+  double* GAUSSIANKERNEL = NULL;
+  double* PWVARR     = NULL;
+  double* PWVNEW     = NULL;
+  double* TAUNEW     = NULL;
+  double* TAUWET     = NULL;
+  double* TMPARR     = NULL;
+  Grp* inGrp         = NULL; /* Input group */
+  Grp* outGrp        = NULL; /* Output group */
+  Grp* tauGrp        = NULL; /* TAU WET group */
+  HDSLoc* loc        = NULL; /* HDS location */
+  size_t fIndex      = 0;    /* File loop counter */
+  size_t inSize      = 0;    /* Size of the input group */
+  size_t outSize     = 0;    /* Size of the output group */
+  size_t tauSize     = 0;    /* Size of the tau group */
+  smfData* inData    = NULL; /* Pointer to input data */
+  smfData* outData   = NULL; /* Pointer to output data */
+  smfData* tauData   = NULL; /* Pointer to tau dry data */
+  void* TAU[]        = {NULL, NULL}; /* {dry, wet} */
 
-  double* src = NULL;
-  double wnFactor = 0.0;
-  int nwn = 0;
-  double* tsm = NULL;
-  double* ftsWNRel = NULL;
+  const double SQRT2PI  = 2.50662827463100050242;
+  const double SQRT2LN2 = 1.17741002251547469101;
 
-  double* wetData = NULL;
-  size_t wetCount = 0;
+  // GET INPUT GROUP
+  kpg1Rgndf("IN", 0, 1, "", &inGrp, &inSize, status);
+  // GET OUTPUT GROUP
+  kpg1Wgndf("OUT", outGrp, inSize, inSize,
+            "Equal number of input and output files expected!",
+            &outGrp, &outSize, status);
+  // GET TAU GROUP
+  kpg1Gtgrp("TAU", &tauGrp, &tauSize, status);
 
-  double* tau = NULL;
-
-  HDSLoc* hdsLoc        = NULL;
-  HDSLoc* hdsLocFactor  = NULL;
-  HDSLoc* hdsLocMore    = NULL;
-
-  Grp* igrp = NULL;
-  Grp* ogrp = NULL;
-  Grp* taugrp = NULL;
-  size_t size;
-  size_t outsize;
-
-  smfData* tauData = NULL;
-  smfData* srcData = NULL;
-
-  // Get input group
-  kpg1Rgndf("IN", 0, 1, "", &igrp, &size, status);
-  // Get output group
-  kpg1Wgndf("OUT", ogrp, size, size, "Equal number of input and output files expected!", &ogrp, &outsize, status);
-  // Get TAU group
-  kpg1Gtgrp("TAU", &taugrp, &size, status);
-
-  parGet0d( "AM", &airMass, status ); // GET AIR MASS
-  parGet0d( "PWV", &PWV, status ); // GET PWV
+  parGet0l("DEBUG", &debug, status);
 
   ndfBegin();
 
-  // OPEN TAU
-  smf_open_file(taugrp, 1, "READ", SMF__NOCREATE_QUALITY, &tauData, status);
-  if(*status != SAI__OK)
-  {
-    errRep(FUNC_NAME, "Unable to open the TAU file!", status);
+  // ===========================================================================
+  // GET TAU INFORMATION
+  // ===========================================================================
+  int dryOK = 0;
+  int wetOK = 0;
+  pname = filename;
+  grpGet(tauGrp, 1, 1, &pname, sizeof(filename), status);
+  ndgNdfas(tauGrp, 1, "READ", &indf, status );
+  if (indf == NDF__NOID) {
+    *status = SAI__ERROR;
+    msgSetc("FILE", filename);
+    errRep("", FUNC_NAME ": Could not locate file ^FILE", status);
     return;
   }
-
-  //
-  // READ IN TAU INFO
-  //
-  hdsLocMore = smf_get_xloc(tauData, "MORE", "EXT", "READ", 0, 0, status);
-  // FACTOR
-  datFind(hdsLocMore, "FACTOR", &hdsLoc, status);
-  datGet0D(hdsLoc, &tauFactor, status);
-  // DRY DATA
-  datFind(hdsLocMore, "DRY", &hdsLoc, status);
-  datSize(hdsLoc, &dryCount, status);
-  dryData = (double*) astMalloc(dryCount * sizeof(double));
-  datGetVD(hdsLoc, dryCount, dryData, &dryCount, status);
-  // WET DATA
-  datFind(hdsLocMore, "WET", &hdsLoc, status);
-  datSize(hdsLoc, &wetCount, status);
-  wetData = (double*) astMalloc(wetCount * sizeof(double));
-  datGetVD(hdsLoc, wetCount, wetData, &wetCount, status);
-
-  // COMPUTE e^(-TAU)
-  tau = (double*) astMalloc(dryCount * sizeof(double));
-  for(i = 0; i < dryCount; i++)
-  {
-    tau[i] = exp(-(airMass * PWV * wetData[i] + dryData[i]));
-  }
-  // FREE RESOURCES
-  astFree(dryData);
-  astFree(wetData);
-  datAnnul(&hdsLocMore, status);
-  datAnnul(&hdsLoc, status);
-
-  // APPLY TRANSMISSION CORRECTION FOR EACH SPECTRUM FILE
-  for(fIndex = 1; fIndex <= size; fIndex++)
-  {
-    // OPEN SOURCE
-    smf_open_file(ogrp, fIndex, "UPDATE", SMF__NOCREATE_QUALITY, &srcData, status);
-    if(*status != SAI__OK)
-    {
-      errRep(FUNC_NAME, "Unable to open source file!", status);
-      break;
-    }
-    src = (double*) (srcData->pntr[0]);
-
-    hdsLoc = smf_get_xloc(srcData, "FTS2DR", "EXT", "READ", 0, 0, status);
-    datFind(hdsLoc, "FTS_WN_FACTOR", &hdsLocFactor, status);
-    datGet0D(hdsLocFactor, &wnFactor, status);
-    datAnnul(&hdsLocFactor, status);
-    datAnnul(&hdsLoc, status);
-
-    nwn = srcData->dims[2];
-    tsm = (double*) astMalloc(nwn * sizeof(double));
-    ftsWNRel = (double*) astMalloc(nwn * sizeof(double));
-    for(i = 0; i < nwn; i++)
-    {
-      ftsWNRel[i] = i * wnFactor / tauFactor;
-    }
-
-    // TODO Replace with fts2_naturalcubicsplineinterpolator
-    // csi_simplified(tau, dryCount, ftsWNRel, nwn, tsm);
-
-    width = srcData->dims[0];
-    height = srcData->dims[1];
-    pixelCount = width * height;
-
-    for(i = 0; i < height; i++)
-    {
-      for(j = 0; j < width; j++)
-      {
-        pixelIndex = i + j * height;
-        for(k = 0; k < nwn; k++)
-        {
-          index = pixelIndex + pixelCount * k;
-          src[index] /= tsm[k];
+  ndfXstat(indf, "FTS2", &ftsExists, status);
+  if(*status == SAI__OK && ftsExists) {
+    ndfXloc(indf, "FTS2", "READ", &loc, status);
+    if(*status == SAI__OK && loc != NULL) {
+      // DRY COMPONENT
+      ndfOpen(loc, "DRY", "READ", "UNKNOWN", &ndfTau, &place, status);
+      if(*status == SAI__OK && ndfTau != NDF__NOID) {
+        ndfDim(ndfTau, NDF__MXDIM, dims, &ndim, status);
+        if(*status == SAI__OK && ndim == 1) {
+          ndfMap(ndfTau, "DATA", "_DOUBLE", "READ", &TAU[0], &count, status);
+          dryOK = 1;
+        }
+      }
+      // WET COMPONENT
+      ndfOpen(loc, "WET", "READ", "UNKNOWN", &ndfTau, &place, status);
+      if(*status == SAI__OK && ndfTau != NDF__NOID) {
+        ndfDim(ndfTau, NDF__MXDIM, dims, &ndim, status);
+        if(*status == SAI__OK && ndim == 2) {
+          ndfMap(ndfTau, "DATA", "_DOUBLE", "READ", &TAU[1], &count, status);
+          wetOK = 1;
         }
       }
     }
-
-    // FREE RESOURCES
-    astFree(tsm);
-    astFree(ftsWNRel);
-    smf_close_file(&srcData, status);
+  }
+  if(loc) { datAnnul(&loc, status); }
+  if(!(dryOK && wetOK)) {
+    *status = SAI__ERROR;
+    errRep("", FUNC_NAME ": Unable to obtain TAU values!", status);
+    return;
   }
 
-  // FREE RESOURCES
-  smf_close_file(&tauData, status);
+  smf_open_file(tauGrp, 1, "READ", 0, &tauData, status);
+  smf_fits_getD(tauData->hdr, "PWV0", &PWV0, status);
+  smf_fits_getD(tauData->hdr, "DELTAPWV", &DELTAPWV, status);
+  if(*status != SAI__OK) {
+    *status = SAI__ERROR;
+    errRep("", FUNC_NAME ": Unable to obtain PWV value(s)!", status);
+    return;
+  }
 
-  // END
-  ndfEnd(status);
-  */
+  nWN  = dims[0];
+  nPWV = dims[1];
+  PWVARR = astMalloc(nPWV * sizeof(*PWVARR));
+  for(i = 0; i < nPWV; i++) {
+    PWVARR[i] = PWV0 + i * DELTAPWV;
+  }
+  PWVNEW = astMalloc(1 * sizeof(*PWVNEW));
+  TAUNEW = astMalloc(1 * sizeof(*TAUNEW));
+
+  // ===========================================================================
+  // LOOP THROUGH EACH NDF FILE IN THE INPUT GROUP
+  // ===========================================================================
+  for(fIndex = 1; fIndex <= inSize; fIndex++) {
+    // OPEN INPUT FILE
+    smf_open_file(inGrp, fIndex, "READ", 0, &inData, status);
+    if(*status != SAI__OK) {
+      *status = SAI__ERROR;
+      errRep(FUNC_NAME, "Unable to open source file!", status);
+      break;
+    }
+
+    outData = smf_deepcopy_smfData(inData, 0, SMF__NOCREATE_DATA, 0, 0, status);
+    if(*status == SAI__OK) {
+      inPntr   = inData->pntr[0];
+      nbolX    = inData->dims[0];
+      nbolY    = inData->dims[1];
+      N        = inData->dims[2];
+      bolCount = nbolX * nbolY;
+
+      outData->dtype   = SMF__DOUBLE;
+      outData->ndims   = 3;
+      outData->dims[0] = inData->dims[0];
+      outData->dims[1] = inData->dims[1];
+      outData->dims[2] = inData->dims[2];
+      outData->lbnd[0] = outData->lbnd[0];
+      outData->lbnd[1] = outData->lbnd[1];
+      outData->lbnd[2] = outData->lbnd[2];
+      outData->pntr[0] = (double*) astCalloc(N * bolCount, sizeof(double), 0);
+      outPntr          = outData->pntr[0];
+
+      // DETERMINE WAVENUMBER FACTOR FROM FITS
+      smf_fits_getD(inData->hdr, "WNFACT", &wnFact, status);
+      if(*status != SAI__OK) {
+        errRep(FUNC_NAME, "Unable to find wave number factor!", status);
+        smf_close_file(&inData, status);
+        break;
+      }
+
+      // TODO
+      // DETERMINE AIRMASS AT ZPD
+
+      // TODO
+      // DETERMINE PWV AT ZPD
+      PWVNEW[0] = PWV;
+
+      // GET TAU WET FOR CORRESPONDING PWV
+      TAUWET = astMalloc(nWN * sizeof(*TAUWET));
+      TMPARR = astMalloc(nWN * sizeof(*TMPARR));
+      for(k = 0; k < nWN; k++) {
+        for(j = 0; j < nPWV; j++) {
+          TMPARR[j] = *((double*) TAU[1] + j);
+        }
+        fts2_naturalcubicsplineinterpolator(PWVARR, TMPARR, nPWV, PWVNEW, TAUNEW, 1);
+        TAUWET[k] = TAUNEW[0];
+      }
+      astFree(TMPARR);
+
+      // COMPUTE ATMOSPHERIC TRANSMISSION
+      // TATM = EXP(-AIRMASS * (PWV * TAUWET + TAUDRY))
+      TAtm = astMalloc(nWN * sizeof(*TAtm));
+      if(!debug) {
+        for(i = 0; i < nWN; i++) {
+          TAtm[i] = exp(-AM * (PWV * TAUWET[i] + (*((double*) TAU[0] + i))));
+        }
+      } else {
+        for(i = 0; i < nWN; i++) {
+          TAtm[i] = exp(-AM * PWV * TAUWET[i]);
+        }
+      }
+
+      // SMOOTH ATMOSPHERIC TRANSMISSION VIA GAUSSIAN CONVOLUTION
+      // NEED TO TRIM FROM BOTH ENDS BY HALF OF (KERNELLENGTH - 1)
+      double OPDMAX = 1.0;
+      double FWHM   = 1.0 / (2.0 * OPDMAX);   // FWHM = 1 / (2 x OPDMAX)
+      double SDEV   = 0.5 * FWHM / SQRT2LN2;  // FWHM = 2 x SQRT(2ln2) x SDEV
+      double VAR    = SDEV * SDEV;
+      double VAR2   = 2.0 * VAR;
+      double NORM   = 1.0 / (SDEV * SQRT2PI);
+      double XMIN   = -6 * SDEV;
+      double XMAX   =  6 * SDEV;
+      double DX = (XMAX - XMIN) / (KERNELLENGTH - 1);
+      double X = XMIN;
+      for(i = 0; i < KERNELLENGTH; i++) {
+        X = XMIN + i * DX;
+        GAUSSIANKERNEL[i] = NORM * exp(-(X * X) / VAR2);
+      }
+      int M = nWN + KERNELLENGTH - 1;
+      TMPARR = astMalloc(M * sizeof(*TMPARR));
+      for(i = 0; i < nWN; i++) {
+        for(j = 0; j < KERNELLENGTH; j++) {
+          TMPARR[i + j] += (TAtm[i] * GAUSSIANKERNEL[j]);
+        }
+      }
+      int OFFSET = (KERNELLENGTH - 1) >> 1;
+      for(i = 0; i < nWN; i++) {
+        TAtm[i] = TMPARR[i + OFFSET];
+      }
+      astFree(TMPARR);
+
+      // INTERPOLATE ATMOSPHERIC TRANSMISSION ONTO SCAN RESOLUTION
+      wnTau = astMalloc(nWN * sizeof(*wnTau));
+      wnScan = astMalloc(N * sizeof(*wnScan));
+      TAtmNew = astMalloc(N * sizeof(*TAtmNew));
+      for(i = 0; i < N; i++) {
+        wnScan[i] = i * wnFact;
+      }
+      for(i = 0; i < nWN; i++) {
+        wnTau[i] = i;
+      }
+      fts2_naturalcubicsplineinterpolator(wnTau, TAtm, nWN, wnScan, TAtmNew, N);
+
+      // TSOURCE = TOBS / TATM
+      for(i = 0; i < nbolY; i++) {
+        for(j = 0; j < nbolX; j++) {
+          bolIndex = i + j * nbolY;
+          for(k = 0; k < N; k++) {
+            index = bolIndex + bolCount * k;
+            outPntr[index] = inPntr[index] / TAtmNew[k];
+          }
+        }
+      }
+      astFree(wnTau);
+      astFree(wnScan);
+      astFree(TAtm);
+      astFree(TAtmNew);
+
+      smf_write_smfData(outData, NULL, NULL, outGrp, fIndex, 0, status);
+      smf_close_file(&outData, status);
+      smf_close_file(&inData, status);
+    } else {
+      *status = SAI__ERROR;
+      errRep(FUNC_NAME, "Unable to deep copy!", status);
+      smf_close_file(&inData, status);
+      break;
+    }
+  }
+
+  CLEANUP:
+    ndfEnd(status);
+    astFree(PWVARR);
+    astFree(PWVNEW);
+    astFree(TAUNEW);
+    astFree(TAU[0]);
+    astFree(TAU[1]);
+    astFree(GAUSSIANKERNEL);
+    if(inGrp) { grpDelet(&inGrp, status); }
+    if(outGrp) { grpDelet(&outGrp, status); }
+    if(tauGrp) { grpDelet(&tauGrp, status); }
 }
