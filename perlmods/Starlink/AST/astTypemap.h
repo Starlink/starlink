95057020c9d76ffc0d63c2b6aa9d9776b8879952
Luca Rizzi <l.rizzi@jach.hawaii.edu>
2011-01-11 14:37:35 -1000

    Deleted most of the perlmods/Starlink modules, and stored in a separate repository
    These modules have been removed from this tree and stores in a separate repository:
    NDF
    ADAM
    AMS
    AST
    Astrom
    Autoastrom
    Config
    EMS
    Extractor
    HSDPACK
    Versions
    GSD
    
    These modules are still in the mail Starlink repository but appear to be unused:
    
    PSF
    NBS
    Prologue

--- perlmods/Starlink/AST/astTypemap.h
+++
@@ -1,9 +0,0 @@
-/* Prototypes for astTypemap.c helper functions */
-
-SV* createPerlObject( const char *, AstObject * );
-IV extractAstIntPointer( SV * );
-char * ntypeToClass( const char * );
-SV* getPerlObjectAttr( SV *, const char * );
-void setPerlObjectAttr( SV *, const char *, SV * );
-int ReportPerlError ( int );
-void setPerlAstObject( SV *, AstObject * );
