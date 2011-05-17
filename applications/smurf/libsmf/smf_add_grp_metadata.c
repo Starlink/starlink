541c5d9c9ad78023bee6e1c970b7ad81e1a5eb00
David Berry <d.berry@jach.hawaii.edu>
2011-05-17 12:17:55 +0100

    smurf: New functions to store and retrieve metadata from a group
    These functions allow named items of metadata to be associated with a GRP
    group. Each item is stored as a "name=value" string in a separate group
    that is associated with a supplied group via a GRP "owner-slave"
    relationship.

---
+++ applications/smurf/libsmf/smf_add_grp_metadata.c
@@ -0,0 +1,191 @@
+/*
+*+
+*  Name:
+*     smf_add_grp_metadata
+
+*  Purpose:
+*     Add items of metadata to a GRP group
+
+*  Language:
+*     Starlink ANSI C
+
+*  Type of Module:
+*     C function
+
+*  Invocation:
+*     void smf_add_grp_metadata( Grp *grp, const char *name,
+*                                const char *value, int *status )
+
+*  Arguments:
+*     grp = Grp * (Given)
+*        Pointer to the GRP to which metadata is to be added.
+*     name = const char * (Given)
+*        A string giving the name of the metadata item to add to the group.
+*     value = const char * (Given)
+*        A string giving the value of the metadata item to add to the group.
+*        If this is NULL, then the supplied "name" is assumed to be the name
+*        of an environment parameter, and the value of the metadata item
+*        is determined by getting a literal value for the parameter using
+*        function parGet0C.
+*     status = int* (Given and Returned)
+*        Pointer to global status.
+
+*  Description:
+*     This function allows global items of metadata to be associated with
+*     a specified GRP group. Each item is stored as  a "<name>=<value>"
+*     string in a separate GRP group, which is associated with the supplied
+*     group. Metadata items can be retrieved using "smf_get_grp_metadata".
+*
+*     The value for the metadata item can be supplied as a function
+*     argument ("value") or obtained through a speicfied environment
+*     parameter. If a null (!) value is supplied for the parameter, then
+*     the error is annulled and no metadata is added to the group.
+
+*  Notes:
+*     - Metadata items are stored in a separate group that is associated
+*     with the supplied group using the GRP "owner-slave" relationship.
+*     The metadata group is made a slave of the supplied group. This should
+*     not interfere with the supplemental groups used by NDG since NDG
+*     assumes that the main group (i.e. the group holding the NDF names -
+*     the group supplied to this function) is a slave of the supplemental
+*     groups, leaving us free to add a slave group to the main group.
+
+*  Authors:
+*     David S Berry (JAC, Hawaii)
+*     {enter_new_authors_here}
+
+*  History:
+*     17-MAY-2011 (DSB):
+*        Original version.
+*     {enter_further_changes_here}
+
+*  Copyright:
+*     Copyright (C) 2011 Science & Technology Facilities Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 3 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public
+*     License along with this program; if not, write to the Free
+*     Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+*     MA 02111-1307, USA
+
+*  Bugs:
+*     {note_any_bugs_here}
+*-
+*/
+
+/* Starlink includes */
+#include "ast.h"
+#include "star/grp.h"
+#include "mers.h"
+#include "sae_par.h"
+#include "par.h"
+
+/* SMURF includes */
+#include "libsmf/smf.h"
+
+void smf_add_grp_metadata( Grp *grp, const char *name, const char *value,
+                           int *status ){
+
+/* Local Variables: */
+   Grp *newslave;                /* New slave group */
+   Grp *slave;                   /* Slave group */
+   char *item;                   /* Buffer for formatted item */
+   char buff[ GRP__SZNAM  + 1 ]; /* Buffer for group element text */
+   char vbuff[ GRP__SZNAM ];     /* Buffer for user-supplied value */
+   int changed;                  /* Have slave group been changed? */
+   int nc;                       /* Length of item */
+   size_t i;                     /* Index of current group element */
+   size_t nlen;                  /* Length of supplied name */
+   size_t size;                  /* Size of slave group */
+
+/* Check the inherited status. */
+   if( *status != SAI__OK ) return;
+
+/* If no value was supplied, get a value from the environment using the
+   supplied name as a parameter name. */
+   if( !value ) {
+      parGet0c( name, vbuff, sizeof( vbuff ) - 1, status );
+
+/* If a null parameter value was supplied, annul the error and retain the
+   NULL value pointer. Otherwise, if no error occurred, use the
+   user-supplied value in future. */
+      if( *status == PAR__NULL ) {
+         errAnnul( status );
+      } else if( *status == SAI__OK ){
+         value = vbuff;
+      }
+   }
+
+/* Do nothing more if we have no value string. */
+   if( value ) {
+
+/* Get the slave group that is owned by the supplied group. */
+      slave = grpSlave( grp, status );
+
+/* If the supplied group does not have a slave, create a new group now
+   and make it the slave of the supplied group. */
+      if( !slave ) {
+         slave = grpNew( " ", status );
+         grpSown( slave, grp, status );
+
+/* If a slave group already exists, create a new slave group that excludes
+   any existing value for the specified name. */
+      } else {
+         changed = 0;
+         newslave = grpNew( " ", status );
+         nlen = strlen( name );
+         size = grpGrpsz( slave, status );
+         for( i = 1; i <= size; i++ ) {
+            grpInfoc( slave, i, "NAME", buff, sizeof( buff ), status );
+            if( strncmp( buff, name, nlen ) || buff[ nlen ] != '=' ) {
+               grpPut1( newslave, buff, 0, status );
+            } else {
+               changed = 1;
+            }
+         }
+
+         if( changed ) {
+            grpSown( slave, NULL, status );
+            grpDelet( &slave, status );
+            slave = newslave;
+            grpSown( slave, grp, status );
+         } else {
+            grpDelet( &newslave, status );
+         }
+      }
+
+/* Format the metadata item. */
+      item = NULL;
+      nc = 0;
+      item = astAppendString( item, &nc, name );
+      item = astAppendString( item, &nc, "=" );
+      item = astAppendString( item, &nc, value );
+
+/* Report an error if the item is too long */
+      if( nc > GRP__SZNAM && *status == SAI__OK ) {
+         *status = SAI__ERROR;
+         msgSetc( "I", item );
+         errRep( " ", "smf_add_grp_metadata: GRP metadata item is too "
+                 "long: '^I'", status );
+      }
+
+/* Free resources. */
+      item = astFree( item );
+
+/* Store the item in the slave group. */
+      grpPut1( grp, item, 0, status );
+
+   }
+}
+
---
+++ applications/smurf/libsmf/smf_get_grp_metadata.c
@@ -0,0 +1,130 @@
+/*
+*+
+*  Name:
+*     smf_get_grp_metadata
+
+*  Purpose:
+*     Get an item of metadata from a GRP group
+
+*  Language:
+*     Starlink ANSI C
+
+*  Type of Module:
+*     C function
+
+*  Invocation:
+*     void smf_get_grp_metadata( Grp *grp, const char *name,
+*                                char *value, int *status )
+
+*  Arguments:
+*     grp = Grp * (Given)
+*        Pointer to the GRP from which metadata is to be retrieved.
+*     name = const char * (Given)
+*        A string giving the name of the metadata item to retrieve.
+*     value = char * (Returned)
+*        Pointer to a buffer in which to return the value associated with
+*        the requested name. The buffer should be at least (GRP__SZNAM+1)
+*        characters long.
+*     status = int* (Given and Returned)
+*        Pointer to global status.
+
+*  Description:
+*     This function retrieves a named item of metadata from a specified
+*     GRP group. Such metadata can be stored in the group using
+*     smf_add_grp_metadata. If no value has been stored for the named item,
+*     the contents of the supplied buffer are returned unchanged, but no
+*     error is reported.
+
+*  Notes:
+*     - Metadata items are stored in a separate group that is associated
+*     with the supplied group using the GRP "owner-slave" relationship.
+*     The metadata group is made a slave of the supplied group. This should
+*     not interfere with the supplemental groups used by NDG since NDG
+*     assumes that the main group (i.e. the group holding the NDF names -
+*     the group supplied to this function) is a slave of the supplemental
+*     groups, leaving us free to add a slave group to the main group.
+
+*  Authors:
+*     David S Berry (JAC, Hawaii)
+*     {enter_new_authors_here}
+
+*  History:
+*     17-MAY-2011 (DSB):
+*        Original version.
+*     {enter_further_changes_here}
+
+*  Copyright:
+*     Copyright (C) 2011 Science & Technology Facilities Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 3 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public
+*     License along with this program; if not, write to the Free
+*     Software Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+*     MA 02111-1307, USA
+
+*  Bugs:
+*     {note_any_bugs_here}
+*-
+*/
+
+/* Starlink includes */
+#include "star/grp.h"
+#include "sae_par.h"
+
+/* SMURF includes */
+#include "libsmf/smf.h"
+
+void smf_get_grp_metadata( Grp *grp, const char *name, char *value,
+                           int *status ){
+
+/* Local Variables: */
+   Grp *slave;                   /* Slave group */
+   char buff[ GRP__SZNAM  + 1 ]; /* Buffer for group element text */
+   size_t size;                  /* Size of slave group */
+   size_t i;                     /* Index of current group element */
+   size_t nlen;                  /* Length of supplied name */
+
+/* Check the inherited status. */
+   if( *status != SAI__OK ) return;
+
+/* Get the slave group that is owned by the supplied group. */
+   slave = grpSlave( grp, status );
+
+/* Return the buffer unchanged if no slave group is available. */
+   if( slave ) {
+
+/* Record the length of the name of the metadata item. */
+      nlen = strlen( name );
+
+/* Sadly we need to check each element in the group to find any that
+   match the name. */
+      size = grpGrpsz( slave, status );
+      for( i = 1; i <= size; i++ ) {
+
+/* Get the text of the i'th element from the group. */
+         grpInfoc( slave, i, "NAME", buff, sizeof( buff ), status );
+
+/* If it is of the form "<name>=..." we have found the required item. */
+         if( !strncmp( buff, name, nlen ) && buff[ nlen ] == '=' ) {
+
+/* Copy the value into the returned buffer. */
+            strcpy( value, buff + nlen + 1 );
+
+/* Leave the loop. */
+            break;
+         }
+      }
+   }
+}
+
