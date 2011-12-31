ddc518e6fc3bcb439633ddecf881dece1391c69b
Malcolm J. Currie <mjc@star.rl.ac.uk>
2011-12-30 19:40:36 -1000

    PHOTOM: Partial tidy of OUTOPT.

--- applications/photom/subs/outopt.f
+++ applications/photom/subs/outopt.f
@@ -6,73 +6,69 @@
      :                    STATUS )
 
 *+
-*  Name :
+*  Name:
 *     OUTOPT
 *
-*  Purpose :
+*  Purpose:
 *     Output the results of the optimal-extraction measurement
 *
-*  Language :
+*  Language:
 *     FORTRAN
 *
-*  Invocation :
+*  Invocation:
 *     CALL OUTOPT ( FOUT, INDEX, XCEN, YCEN, ORIGIN, LPADU, LSTAR,
 *                   LAREA, LVSTAR, LSKY, LSKYARE, LSIGMA, LVSKY,
 *                   MAGS, LSKYMAG, PHOTON, LBIASLE, CODE, LETIME,
 *                   STATUS )
 *
-*  Description :
+*  Description:
 *     Output the results of the optimal-extraction measurement.
 *
-*  Arguments :
+*  Arguments:
 *     FOUT = INTEGER (Given)
-*        Identifier for results file used by FIO_
+*        Identifier for the results file used by FIO_.
 *     INDEX = INTEGER (Given)
-*        Index number of object
+*        Index number of the object.
 *     XCEN = REAL (Given)
-*        Centre of aperture in pixel coordinates
+*        X centre of the aperture in pixel coordinates.
 *     YCEN = REAL (Given)
-*        Centre of aperture in pixel coordinates
+*        Y centre of the aperture in pixel coordinates.
 *     ORIGIN( 2 ) = INTEGER (Given)
-*        Origin of NDF axes
+*        The origin of the NDF axes.
 *     LPADU = REAL (Given)
-*        Photons per data unit
+*        Photons per data unit.
 *     LSTAR = REAL (Given)
-*        Integrated signal in object aperture
+*        Integrated signal in the object aperture.
 *     LAREA = REAL (Given)
-*        Area of object aperture
+*        Area of the object aperture.
 *     LVSTAR = REAL (Given)
-*        Data variance in object aperture
+*        The data variance in the object aperture.
 *     LSKY = REAL (Given)
-*        Sky value per pixel
+*        Sky value per pixel.
 *     LSKYARE = REAL (Given)
-*        Area of sky aperture
+*        Area of the sky aperture in pixels.
 *     LSIGMA = REAL (Given)
-*        Deviation in sky aperture
+*        Standard deviation in the sky aperture.
 *     LVSKY = REAL (Given)
-*        Data variance in sky aperture per pixel
+*        Data variance per pixel in the sky aperture.
 *     MAGS = LOGICAL (Given)
-*        If TRUE then results are converted into magnitudes
+*        If TRUE then results are converted into magnitudes.
 *     LSKYMAG = REAL (Given)
-*        Magnitude of sky
+*        Magnitude of the sky when MAGS is TRUE.
 *     PHOTON = INTEGER (Given)
-*        Errors from photon noise, sky or data variance, gaussian sky
+*        How the errors are to be determined.  Allowed values are
+*        1: photon noise, 2: sky variance, 3: data variance,
+*        4: gaussian sky.
 *     LBIASLE = REAL (Given)
-*        Zero point for photon noise calculation per pixel
-*     CODE = CHARACTER (Given)
-*        Error code flag
+*        Zero point for the photon noise calculation per pixel.
+*     CODE = CHARACTER * ( 2 ) (Given)
+*        Error code flag.
 *     LETIME = REAL (Given)
-*        Exposure time
+*        Exposure time in seconds.
 *     STATUS = INTEGER (Given and Returned)
 *        The global status.
 *
-*  Algorithm :
-*     {algorithm_description}...
-*
-*  Deficiencies :
-*     {routine_deficiencies}...
-*
-*  Authors :
+*  Authors:
 *     NE: Nick Eaton (Durham University)
 *     PWD: Peter W. Draper (Durham University)
 *     AA: Alasdair Allan (Starlink, Keele University)
@@ -80,7 +76,7 @@
 *     MJC: Malcolm J. Currie (JAC)
 *     {enter_new_authors_here}
 *
-*  History :
+*  History:
 *     25-JAN-1999 (AA)
 *         Original version, heavily based on OUTRES
 *     21-FEB-2008 (PWD):
@@ -92,18 +88,18 @@
 *        signal.  Increase the format width for magnitudes.
 *     {enter_further_changes_here}
 *
-*  Bugs :
+*  Bugs:
 *     {note_any_bugs_here}
 *-
 
-*  Type Definitions :
+*  Type Definitions:
       IMPLICIT NONE
 
-*  Global Constants :
+*  Global Constants:
       INCLUDE 'SAE_PAR'
 
 
-*  Arguments Given :
+*  Arguments Given:
       INTEGER FOUT
       INTEGER INDEX
       REAL XCEN
@@ -127,10 +123,10 @@
       CHARACTER * ( 2 ) CODE
       REAL LETIME
 
-*  Status :
+*  Status:
       INTEGER STATUS
 
-*  Local Variables :
+*  Local Variables:
       LOGICAL ERFLAG
 
       DOUBLE PRECISION ERRMAG, ERRSIG, ERR2, FACTOR, MAG, PSKY, SIGNAL
@@ -141,12 +137,14 @@
       CHARACTER CSKY * 15, CSIG * 15, CA * 5, CE * 5, CT * 5
       CHARACTER TEXT * 80
       CHARACTER MAGFMT * 9
+
 *.
-*   Check status on entry
+
+*  Check inherited status on entry.
       IF ( STATUS .NE. SAI__OK ) RETURN
 
-*   Transform all input REAL variables into DOUBLE PRECISION for
-*   better accuracy.
+*  Transform all input REAL variables into DOUBLE PRECISION for
+*  better accuracy.
       PADU = DBLE( LPADU )
       STAR = DBLE( LSTAR )
       AREA = DBLE( LAREA )
@@ -159,22 +157,23 @@
       BIASLE = DBLE( LBIASLE )
       ETIME  = DBLE( LETIME )
 
-*   Initialise the error flag
+*  Initialise the error flag.
       IF ( SIGMA .LT. 0.0 ) THEN
          ERFLAG = .TRUE.
       ELSE
          ERFLAG = .FALSE.
-      ENDIF
+      END IF
 
-*   Unlike the aperture routines, for optimal extraction we pre-calculate
-*   the signal and error, but we still need to put the result into electrons
-      SIGNAL = PADU *STAR
-      ERRSIG = PADU *VSTAR
+*  Unlike the aperture routines, for optimal extraction we pre-calculate
+*  the signal and error, but we still need to put the result into
+*  electrons.
+      SIGNAL = PADU * STAR
+      ERRSIG = PADU * VSTAR
 
-*   The magnitude error = (2.5/ln(10)) * (errsig/signal) which comes from
-*   differentiating the magnitude calculation. Check that the signal is
-*   not zero, otherwise set an error condition. If MAGS is
-*   FALSE then we do not want a magnitude conversion.
+*  The magnitude error = (2.5/ln(10)) * (errsig/signal) which comes from
+*  differentiating the magnitude calculation. Check that the signal is
+*  not zero, otherwise set an error condition. If MAGS is
+*  FALSE then we do not want a magnitude conversion.
       FACTOR = 0.0D0
       IF ( MAGS ) THEN
          IF ( ABS( SIGNAL ) .GT. 1.0D-6 ) THEN
@@ -182,19 +181,19 @@
             ERRMAG = 1.08574D0 * FACTOR
          ELSE
             ERFLAG = .TRUE.
-         ENDIF
+         END IF
       ELSE
          ERRMAG = ERRSIG
       END IF
 
-*   Scale the signal according to the exposure time.
+*  Scale the signal according to the exposure time.
       IF ( ETIME .GT. 0.0D0 ) THEN
          SIGNAL = SIGNAL / ETIME
-      ENDIF
+      END IF
 
-*   Calculate the stars magnitude. A magnitude of skymag means that the
-*   star and sky are the same brightness. If the signal is negative then
-*   flag an error.
+*  Calculate the stars magnitude. A magnitude of skymag means that the
+*  star and sky are the same brightness. If the signal is negative then
+*  flag an error.
       IF ( MAGS ) THEN
          IF ( SIGNAL .GT. 0.0D0 ) THEN
             MAG = SKYMAG - 2.5D0 * LOG10( ABS( SIGNAL ) )
@@ -206,13 +205,14 @@
 	 ERRMAG = ERRSIG
       END IF
 
-*   If there is no signal or an error has occurred, give it a nominal value
+*  If there is no signal or an error has occurred, give it a nominal
+*  value.
       IF ( MAGS ) THEN
          IF ( ( FACTOR .GT. 1.0D0 ) .OR. ERFLAG ) THEN
             MAG = SKYMAG
             ERRMAG = 99.99999D0
             CODE = '?'
-         ENDIF
+         END IF
       ELSE
          IF ( ERFLAG ) THEN
             MAG = 0.0D0
@@ -221,7 +221,8 @@
          END IF
       END IF
 
-*   Code the output into seperate character strings
+*   Code the output into separate character strings.
+
 *   Index - limit to 5 characters
       WRITE( TEXT, '( I80 )' ) INDEX
       CINDEX = TEXT( 76:80 )
