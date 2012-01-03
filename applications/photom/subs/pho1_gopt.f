3c9dde778658647b66a4bc52cfed5787d2a3bdb5
Malcolm J. Currie <mjc@star.rl.ac.uk>
2012-01-03 13:08:47 -1000

    PHOTOM: Converted to standard prologue and tidied.
    Added argument descriptions.
    
    The history changes apply to some of the earlier commits, as the hunk
    of new history would not split.

--- applications/photom/subs/pho1_gopt.f
+++ applications/photom/subs/pho1_gopt.f
@@ -3,13 +3,13 @@
      :                      SEE, CODE, LETIME, BUFFER, STATUS )
 
 *+
-*  Name :
+*  Name:
 *     PHO1_GOPT
 *
-*  Purpose :
+*  Purpose:
 *     Calculates photometry measurements for optimal photometry
 *
-*  Language :
+*  Language:
 *     Starlink Fortran-77
 *
 *  Invocation:
@@ -17,35 +17,74 @@
 *    :                LSKY, LSIGMA, LSKYMG, CLIP, SEE, CODE, LETIME,
 *    :                BUFFER, STATUS )
 *
-*  Description :
+*  Description:
 *     Calculates the final measurements for an aperture and writes the
 *     results into a string buffer.
 *
-*  Arguments :
-*     {enter_new_arguments_here}
+*  Arguments:
+*     MAGS = LOGICAL (Given)
+*        If TRUE then results are converted into magnitudes.
+*     XCEN = REAL (Given)
+*        Centre of aperture in pixel coordinates.
+*     YCEN = REAL (Given)
+*        Centre of aperture in pixel coordinates.
+*     ORIGIN( 2 ) = INTEGER (Given)
+*        Origin of the NDF axes.
+*     LPADU = REAL (Given)
+*        Photons per data unit.
+*     LSTAR = REAL (Given)
+*        Integrated signal in object aperture.
+*     LVSTAR = REAL (Given)
+*        Data variance in object aperture.
+*     LSKY = REAL (Given)
+*        Sky value per pixel.
+*     LSIGMA = REAL (Given)
+*        Standard deviation in sky aperture.
+*     LSKYMG = REAL (Given)
+*        Magnitude of sky.
+*     CLIP = REAL (Given)
+*        Clipping radius for weight map.
+*     SEE = REAL (Given)
+*        Seeing in pixels.
+*     CODE = CHARACTER * ( 2 ) (Given)
+*        Error code flag
+*     LETIME = REAL (Given)
+*        Exposure time.
+*     BUFFER = CHARACTER * ( * ) (Returned)
+*        The formatted results.  This should accommodate at least 80
+*        characters.  The output contains the following fields:
+*           X Y MAG MAGERR SKY SIGNAL CODE
+*     STATUS = INTEGER (Given and Returned)
+*        The global status.
 *
-*  Authors :
+*  Authors:
 *     AA: Alasdair Allan (STARLINK - Keele University)
 *     MJC: Malcolm J. Currie (JAC)
 *     {enter_new_authors_here}
 *
-*  History :
+*  History:
 *     1-FEB-1999 (AA):
-*        Original version based on PWD's PHO1_GCALC
-*     {enter_changes_here}
+*        Original version based on PWD's PHO1_GCALC.
+*     2011 December 22 (MJC):
+*        Use G-format for counts mean and its error, and for sky and
+*        signal.  Increase the format width for magnitudes.
+*     2011 December 23 (MJC)
+*        Removed unused arguments and tidied.  Test for zero value by
+*        using VAL__SMD.
+*     {enter_further_changes_here}
 *
-*  Bugs :
+*  Bugs:
 *     {note_any_bugs_here}
 *-
 
-*  Type Definitions :
-      IMPLICIT NONE
+*  Type Definitions:
+      IMPLICIT NONE              ! no default rtyping allowed
 
 *  Global Constants:
       INCLUDE 'SAE_PAR'          ! Global SSE definitions
       INCLUDE 'PRM_PAR'          ! VAL__ public constants
 
-*  Arguments Given :
+*  Arguments Given:
       LOGICAL MAGS
       REAL XCEN
       REAL YCEN
@@ -60,10 +99,11 @@
       REAL SEE
       CHARACTER * ( 2 ) CODE
       REAL LETIME
-      CHARACTER * ( * ) BUFFER
-      REAL CLIP, SEE
 
-*  Status :
+*  Arguments Returned:
+      CHARACTER * ( * ) BUFFER
+
+*  Status:
       INTEGER STATUS
 
 *  Local Variables:
@@ -88,14 +128,12 @@
 
 *.
 
-*   Check status on entry
+*  Check inherited status on entry
       IF ( STATUS .NE. SAI__OK ) RETURN
 
-*   Transform all input REAL variables into DOUBLE PRECISION for
-*   better accuracy.
+*  Transform all input REAL variables into DOUBLE PRECISION for
+*  better accuracy.
       PADU = DBLE( LPADU )
-      STAR = DBLE( LSTAR )
-      VSTAR = DBLE( LVSTAR )
       SKY = DBLE( LSKY )
       SKYMAG = DBLE( LSKYMG )
       ETIME = DBLE( LETIME )
@@ -105,17 +143,18 @@
          ERFLAG = .TRUE.
       ELSE
          ERFLAG = .FALSE.
-      ENDIF
+      END IF
 
-*   Unlike the aperture routines, for optimal extraction we pre-calculate
-*   the signal and error, but we still need to put the result into electrons
-      SIGNAL = PADU*STAR
-      ERRSIG = PADU*VSTAR
+*  Unlike the aperture routines, for optimal extraction we
+*  pre-calculate the signal and error, but we still need to put the
+*  result into electrons.
+      SIGNAL = PADU * DBLE( LSTAR )
+      ERRSIG = PADU * DBLE( LVSTAR )
 
-*   The magnitude error = (2.5/ln(10)) * (errsig/signal) which comes
-*   from differentiating the magnitude calculation. Check that the
-*   signal is not zero, otherwise set an error condition. If MAGS is
-*   FALSE then we do not want a magnitude conversion.
+*  The magnitude error = (2.5/ln(10)) * (errsig/signal) which comes
+*  from differentiating the magnitude calculation. Check that the
+*  signal is not zero, otherwise set an error condition. If MAGS is
+*  FALSE then we do not want a magnitude conversion.
       FACTOR = 0.0
       IF ( MAGS ) THEN
          IF ( ABS( SIGNAL ) .GT. VAL__SMLD ) THEN
@@ -123,37 +162,38 @@
             ERRMAG = 1.08574D0 * FACTOR
          ELSE
             ERFLAG = .TRUE.
-         ENDIF
+         END IF
       ELSE
          ERRMAG = ERRSIG
       END IF
 
-*   Scale the signal according to the exposure time.
+*  Scale the signal according to the exposure time.
       IF ( ETIME .GT. 0.0D0 ) THEN
          SIGNAL = SIGNAL / ETIME
-      ENDIF
+      END IF
 
-*   Calculate the stars magnitude. A magnitude of skymag means that the
-*   star and sky are the same brightness. If the signal is negative then
-*   flag an error.
+*  Calculate the stars magnitude. A magnitude of skymag means that the
+*  star and sky are the same brightness. If the signal is negative then
+*  flag an error.
       IF ( MAGS ) THEN
          IF ( SIGNAL .GT. 0.0D0 ) THEN
             MAG = SKYMAG - 2.5D0 * LOG10( ABS( SIGNAL ) )
          ELSE
             ERFLAG = .TRUE.
-         ENDIF
+         END IF
       ELSE
          MAG = SIGNAL
 	 ERRMAG = ERRSIG
-      ENDIF
+      END IF
 
-*   If there is no signal or an error has occured, give it a nominal value
+*  If there is no signal or an error has occurred, give it a nominal
+*  value.
       IF ( MAGS ) THEN
          IF ( ( FACTOR .GT. 1.0D0 ) .OR. ERFLAG ) THEN
             MAG = SKYMAG
             ERRMAG = 99.999D0
             CODE = '?'
-         ENDIF
+         END IF
       ELSE
          IF ( ERFLAG ) THEN
             MAG = 0.0D0
@@ -162,11 +202,12 @@
          END IF
       END IF
 
-*   Code the output into separate character strings
-*   Xcen
+*  Code the output into separate character strings.
+
+*  Xcen
       WRITE( CXCEN, '( F9.2 )' ) XCEN + REAL( ORIGIN( 1 ) - 1 )
 
-*   Ycen
+*  Ycen
       WRITE( CYCEN, '( F9.2 )' ) YCEN + REAL( ORIGIN( 2 ) - 1 )
 
 *  Counts may cover a wide range of values so use G format.
@@ -193,10 +234,9 @@
 *  significant figures.
       WRITE( CSIG, '( G13.5 )' ) SIGNAL
 
-*   Concatenate these into the output string.
+*  Concatenate these into the output string.
       BUFFER = CXCEN//' '//CYCEN//' '//CMAG//' '//CERRMG//' '//CSKY//' '
      :         //CSIG//' '//' '//CODE
   99  CONTINUE
 
       END
-
