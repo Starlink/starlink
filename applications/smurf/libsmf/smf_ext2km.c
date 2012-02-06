1647e0c7513f9d07430e719d10bfb39922c43554
David Berry <d.berry@jach.hawaii.edu>
2012-02-06 10:43:10 +0000

    smurf: Allow timesort to work on an NDF section along the time axis

--- applications/smurf/libsmf/smf_ext2km.c
+++ applications/smurf/libsmf/smf_ext2km.c
@@ -21,9 +21,16 @@
 *        An NDF identifier for the NDF containing the extension items
 *        to be read. This should be a time series file spanned by
 *        (spectrum,receptor,time slice) axes. It should contain ACSIS and
-*        JCMTSTATE extensions.
+*        JCMTSTATE extensions. It is assumed that the first element of
+*        each time-ordered extension array corresponds to a pixel index
+*        of one on the third pixel axis of the NDF. Thus, a section of
+*        the original NDF may be supplied.
 *     xname = const char * (Given)
-*        The name of the NDF extension to read.
+*        The name of the NDF extension to read. Should be "JCMTSTATE" or
+*        "ACSIS".
+*     keymap = AstKeyMap * (Given)
+*        An AST keyMap to hold the primitive array values read from the
+*        NDF extension.
 *     mode = int (Given)
 *        Determines what data is copied from the NDF, and whether it
 *        over-writes any existing data in the KeyMap or is appended to
@@ -36,9 +43,6 @@
 *        KeyMap, appending the new values to the end of the existing
 *        values. An error is reported if new data cannot be found in the
 *        NDF for any of the existing entries in the KeyMap.
-*     keymap = AstKeyMap * (Given)
-*        An AST keyMap to hold the primitive array values read from the
-*        NDF extension.
 *     status = int * (Given and Returned)
 *        Inherited status value.
 
@@ -49,11 +53,14 @@
 *     vectorised list of the values read from the extension component.
 *
 *     The named extension is searched for primitive array components
-*     that have a final trailing axis length equal to the length of the
-*     NDFs 3rd pixel axis. An additional constraint is that if "xname" is
-*     ACSIS, only arrays with at least 2 axes are used. Any such arrays are
-*     vectorised and stored in the KeyMap, in the manner determined by the
-*     "mode" argument.
+*     that have a final trailing axis length equal to the length of
+*     the final trailing axis in a "prototype" component - if "xname" is
+*     "JCMTSTATE" then the prototype component is "RTS_NUM", and if "xname"
+*     is "ACSIS" the prototype component is "TSYS". An additional constraint
+*     is that if "xname" is ACSIS, only arrays with at least 2 axes are used.
+
+*     Any such arrays are vectorised and stored in the KeyMap, in the manner
+*     determined by the "mode" argument.
 
 *  Authors:
 *     David S Berry (JAC, UCLan)
@@ -66,6 +73,13 @@
 *        Only copy ACSIS arrays that have at least 2 axes. This guards
 *        against problems if the number of time slices is equal to the
 *        number of detectors.
+*     6-FEB-2012 (DSB):
+*        Use a prototype component in each extension to specify the size of
+*        the arrays to copy, rather than the length of the 3rd pixel axis in
+*        the supplied NDF. This is because the supplied NDF may now be a
+*        section, and so the length of the 3rd NDF pixel axis may not be
+*        the same as the number of time slices in the time-ordered extension
+*        arrays.
 *     {enter_further_changes_here}
 
 *  Copyright:
@@ -106,26 +120,62 @@ void smf_ext2km( int indf, const char *xname, AstKeyMap *keymap,
 
 /* Local Variables */
    HDSLoc *cloc = NULL;
+   HDSLoc *sloc = NULL;
    HDSLoc *xloc = NULL;
    const char *key = NULL;
+   hdsdim diml[NDF__MXDIM];
+   hdsdim dimu[NDF__MXDIM];
    int dim[ NDF__MXDIM ];
    int i;
+   int idim;
+   int lbnd[ NDF__MXDIM ];
    int ncomp;
    int ndim;
    int nentry;
    int ntime;
    int prim;
    int there;
+   int ubnd[ NDF__MXDIM ];
 
 /* Check the inherited status */
    if( *status != SAI__OK ) return;
 
-/* Get the shape of the NDF, and noe the length of the 3rd axis. */
-   ndfDim( indf, 3, dim, &ndim, status );
-   ntime = dim[ 2 ];
+/* Get the shape of the NDF. */
+   ndfBound( indf, 3, lbnd, ubnd, &ndim, status );
 
 /* Get a locator to the named extension. */
    ndfXloc( indf, xname, "READ", &xloc, status );
+
+/* Select the name of the prototype component that determines the length
+   of the arrays to be copied. */
+   if( !strcmp( xname, "JCMTSTATE" ) ) {
+      key = "RTS_NUM";
+   } else if( !strcmp( xname, "ACSIS" ) ) {
+      key = "TSYS";
+   } else {
+      cloc = NULL;
+      msgSetc( "X", xname );
+      errRep( "", "smf_ext2km: Unknown extension specified - '^X'. "
+              "Must be 'JCMTSTATE' or 'ACSIS'.", status );
+   }
+
+/* Check the prorotype component is present in the extension. Report an
+   error if not. */
+   datThere( xloc, key, &there, status );
+   if( !there && *status == SAI__OK ) {
+      msgSetc( "C", key );
+      msgSetc( "X", xname );
+      errRep( "", "smf_ext2km: Component '^C' not found in extension "
+              "^X", status );
+
+/* If it is present, get the length of its last pixel axis. This is the
+   length of the arrays to be copied. */
+   } else {
+      datFind( xloc, key, &cloc, status );
+      datShape( cloc, NDF__MXDIM, dim, &ndim, status );
+      ntime = dim[ ndim - 1 ];
+      datAnnul( &cloc, status );
+   }
 
 /* First deal with mode 1... */
 /* ========================= */
@@ -144,15 +194,28 @@ void smf_ext2km( int indf, const char *xname, AstKeyMap *keymap,
             datShape( cloc, NDF__MXDIM, dim, &ndim, status );
 
 /* Skip over this component if the length of its final axis is not equal
-   to the number of time slices. */
+   to the expected number of time slices. */
             if( ndim > 0 && dim[ ndim - 1 ] == ntime ) {
 
 /* Also skip if we are dealing with the ACSIS extension and the array has
    only 1 (or zero) axes. */
                if( ndim > 1 || strcmp( xname, "ACSIS" ) ) {
 
-/* If it is, store the values as a new entry in "keymap". */
-                  kpg1Hdsky( cloc, keymap, 2, 1, status );
+/* Cut a section from the HDS array so that it matches the pixel bounds of
+   the NDF . */
+                  for( idim = 0; idim < ndim - 1; idim++ ) {
+                     diml[ idim ] = 1;
+                     dimu[ idim ] = dim[ idim ];
+                  }
+                  diml[ idim ] = lbnd[ 2 ];
+                  dimu[ idim ] = ubnd[ 2 ];
+                  datSlice( cloc, ndim, diml, dimu, &sloc, status );
+
+/* Store the values as a new entry in "keymap". */
+                  kpg1Hdsky( sloc, keymap, 2, 1, status );
+
+/* Annul the locator for the slice. */
+                  datAnnul( &sloc, status );
                }
             }
          }
@@ -178,7 +241,7 @@ void smf_ext2km( int indf, const char *xname, AstKeyMap *keymap,
             if( prim ) {
 
 /* Check the final axis of the primitive array has a length equal to the
-   number of time slices. */
+   expected number of time slices. */
                datShape( cloc, NDF__MXDIM, dim, &ndim, status );
                if( ndim > 0 && dim[ ndim - 1 ] == ntime ) {
 
@@ -186,8 +249,22 @@ void smf_ext2km( int indf, const char *xname, AstKeyMap *keymap,
    only 1 (or zero) axes. */
                   if( ndim > 1 || strcmp( xname, "ACSIS" ) ) {
 
-/* If it is OK, append the values to the end of the existing KeyMap entry. */
-                     kpg1Hdsky( cloc, keymap, 1, 3, status );
+/* Cut a section from the HDS array so that it matches the pixel bounds of
+   the NDF . */
+                     for( idim = 0; idim < ndim - 1; idim++ ) {
+                        diml[ idim ] = 1;
+                        dimu[ idim ] = dim[ idim ];
+                     }
+                     diml[ idim ] = lbnd[ 2 ];
+                     dimu[ idim ] = ubnd[ 2 ];
+                     datSlice( cloc, ndim, diml, dimu, &sloc, status );
+
+/* Append the values to the end of the existing KeyMap entry. */
+                     kpg1Hdsky( sloc, keymap, 1, 3, status );
+
+/* Annul the locator for the slice. */
+                     datAnnul( &sloc, status );
+
                   }
 
                } else if( *status == SAI__OK ) {
