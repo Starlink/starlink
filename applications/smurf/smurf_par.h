e88a13136a087070f878bc542754c913af8ab8da
Tim Jenness <t.jenness@jach.hawaii.edu>
2012-03-06 15:24:24 -1000

    smurf: Replace slamac.h constants with sofam.h constants

--- applications/smurf/smurf_par.h
+++ applications/smurf/smurf_par.h
@@ -55,10 +55,12 @@
  *        Use correct DTOI value.
  *     2011-06-08 (EC):
  *        RAW2CURRENT is no longer a constant, moved to smf_raw2current.c
+ *     2012-03-06 (TIMJ):
+ *        Use sofam.h instead of slamac.h
  *     {enter_further_changes_here}
 
  *  Copyright:
- *     Copyright (C) 2008-2011 Science and Technology Facilities Council.
+ *     Copyright (C) 2008-2012 Science and Technology Facilities Council.
  *     Copyright (C) 2005-2006 Particle Physics and Astronomy Research Council.
  *     Copyright (C) 2005,2006,2011 University of British Columbia.
  *     All Rights Reserved.
@@ -110,65 +112,18 @@ error can not determine PI
 
 /* Assume we now have access to the standard POSIX definitions */
 
-/* These convenient definitions come from slamac.h - we prefer to use the
-   slamac.h header file directly. */
+/* Use the SOFA conversion constants since we know those are going
+   to be available */
 
-#if HAVE_SLAMAC_H
-# include "slamac.h"
-#else
+#include "sofam.h"
 
-/* pi */
-#define DPI 3.1415926535897932384626433832795028841971693993751
-
-/* 2pi */
-#define D2PI 6.2831853071795864769252867665590057683943387987502
-
-/* 1/(2pi) */
-#define D1B2PI 0.15915494309189533576888376337251436203445964574046
-
-/* 4pi */
-#define D4PI 12.566370614359172953850573533118011536788677597500
-
-/* 1/(4pi) */
-#define D1B4PI 0.079577471545947667884441881686257181017229822870228
-
-/* pi^2 */
-#define DPISQ 9.8696044010893586188344909998761511353136994072408
-
-/* sqrt(pi) */
-#define DSQRPI 1.7724538509055160272981674833411451827975494561224
-
-/* pi/2:  90 degrees in radians */
-#define DPIBY2 1.5707963267948966192313216916397514420985846996876
-
-/* pi/180:  degrees to radians */
-#define DD2R 0.017453292519943295769236907684886127134428718885417
+/* And some from slamac.h that SOFA does not define */
 
 /* 180/pi:  radians to degrees */
 #define DR2D 57.295779513082320876798154814105170332405472466564
 
-/* pi/(180*3600):  arcseconds to radians */
-#define DAS2R 4.8481368110953599358991410235794797595635330237270e-6
-
-/* 180*3600/pi :  radians to arcseconds */
-#define DR2AS 2.0626480624709635515647335733077861319665970087963e5
-
 /* pi/12:  hours to radians */
 #define DH2R 0.26179938779914943653855361527329190701643078328126
-
-/* 12/pi:  radians to hours */
-#define DR2H 3.8197186342054880584532103209403446888270314977709
-
-/* pi/(12*3600):  seconds of time to radians */
-#define DS2R 7.2722052166430399038487115353692196393452995355905e-5
-
-/* 12*3600/pi:  radians to seconds of time */
-#define DR2S 1.3750987083139757010431557155385240879777313391975e4
-
-/* 15/(2pi):  hours to degrees x radians to turns */
-#define D15B2P 2.3873241463784300365332564505877154305168946861068
-
-#endif /* HAVE_SLAMAC_H */
 
 /* Tidy up to prevent leakage of SLA symbols */
 #undef DPI
