60d270b057b7692b471a1b9b6dcac8c0495e6d50
Tim Jenness <t.jenness@jach.hawaii.edu>
2012-06-19 19:16:30 -0700

    perl: Add a first attempt at perl module building
    This patch includes a script in thirdparty/perlsys/perlmods
    for building all the perl modules needed by Starlink. The
    Starlink perl modules are attached as git submodules.
    
    This is not integrated into make world.

---
+++ thirdparty/perlsys/perlmods/build-modules.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# Installs cpanm or updates it
+# Then goes through each perl-* directory and installs the modules with cpanm
+
+# Location of Perl and support libraries
+perlroot=${STARLINK_DIR}/Perl/bin/
+export CFITSIO=${STARLINK_DIR}
+
+starperl=${perlroot}/perl
+cpanm=${perlroot}/cpanm
+
+# Make sure that CPANM is up to date
+if [ -x $cpanm ]
+then
+    ${cpanm} --self-upgrade
+else
+    curl -L http://cpanmin.us | $starperl - App::cpanminus
+fi
+
+# First do the CPAN dependencies required
+# to run Starlink perl applications
+for i in `cat ./cpan.deps`
+do
+    ${cpanm} $i
+    if (( $? ))
+    then
+        echo Failed to install $i
+        exit 1
+    fi
+done
+
+# Then do the local repositories. We have to be careful
+# about the install order so get the order from an external
+# file.
+
+# CPANM rebuilds all of these each time it runs
+
+for i in `cat ./perlmods-order.txt`
+do
+    if [ -d ./$i ]
+    then
+        ${cpanm} ./$i
+        if (( $? ))
+        then
+            echo Failed to install $i
+            exit 1
+        fi
+    fi
+done
+
