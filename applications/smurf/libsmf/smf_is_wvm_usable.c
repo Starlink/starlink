4640ee0b44fc3a0d0a4c8944956821204ab80091
Tim Jenness <tim.jenness@gmail.com>
2013-06-21 16:13:04 -0700

    smurf: Add routine that knows when the WVM is not working

---
+++ applications/smurf/libsmf/smf_is_wvm_usable.c
@@ -0,0 +1,134 @@
+/*
+*+
+*  Name:
+*     smf_is_wvm_usable
+
+*  Purpose:
+*     Is the WVM thought to be usable for this data set?
+
+*  Language:
+*     Starlink ANSI C
+
+*  Type of Module:
+*     Library routine
+
+*  Invocation:
+*     isusable = smf_is_wvm_usable( const smfHead* hdr, int *status );
+
+*  Arguments:
+*     hdr = const smfHead * (Given)
+*        Header to examine to determine relevant information (date)
+*     status = int* (Given and Returned)
+*        Pointer to global status.
+
+*  Description:
+*     Looks at the date in the header and compares that with a list of nights known
+*     for an unstable or missing WVM. Assumes that unlisted nights are good.
+
+*  Authors:
+*     TIMJ: Tim Jenness (JAC, Hawaii)
+*     {enter_new_authors_here}
+
+*  Notes:
+*     - Initially done with a static list of unstable nights hard-coded into source file.
+*     - Nights supplied by Daniel Berke 2013-06-19
+
+*  History:
+*     2013-06-21 (TIMJ):
+*        Initial version
+*     {enter_further_changes_here}
+
+*  Copyright:
+*     Copyright (C) 2013 Science and Technology Facilities Council.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 3 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public
+*     License along with this program; if not, write to the Free
+*     Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+*     MA 02110-1301, USA
+
+*  Bugs:
+*     {note_any_bugs_here}
+*-
+*/
+
+#include "smf.h"
+
+#include "sae_par.h"
+#include "ast.h"
+
+int
+smf_is_wvm_usable( const smfHead * hdr, int *status ) {
+
+  size_t i = 0;
+  int utdate;           /* UTDATE FITS header */
+  int isusable = 1;     /* Default to probably being usable */
+
+  /* Initially just have a static list in the source code */
+  const int badwvm[] = {
+    20130119,
+    20130120,
+    20130223,
+    20130224,
+    20130225,
+    20130308,
+    20130309,
+    20130315,
+    20130316,
+    20130319,
+    20130320,
+    20130321,
+    20130322,
+    20130323,
+    20130324,
+    20130325,
+    20130327,
+    20130328,
+    20130329,
+    20130330,
+    20130331,
+    20130401,
+    20130402,
+    20130403,
+    20130404,
+    20130405,
+    20130406,
+    20130407,
+    20130408,
+    20130422,
+    20130508,
+    20130509,
+    20130510,
+    20130511,
+    20130512,
+    20130513,
+    20130514,
+    20130523,
+    0    /* Sentinenl */
+  };
+
+  if (*status != SAI__OK) return 0;
+
+  /* Get the UT date from the header */
+  smf_getfitsi( hdr, "UTDATE", &utdate, status );
+
+  for (i = 0; badwvm[i] != 0; i++) {
+    if (utdate == badwvm[i]) {
+      isusable = 0;
+      break;
+    }
+  }
+
+  return isusable;
+}
