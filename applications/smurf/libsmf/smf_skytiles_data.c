9ca5d4674f3a7a0903fee63326ae2906f9681481
David Berry <d.berry@jach.hawaii.edu>
2013-07-15 13:37:46 +0100

    smurf: Check for errors before accsssing pointers
    Avoids seg faults

--- applications/smurf/libsmf/smf_skytiles_data.c
+++ applications/smurf/libsmf/smf_skytiles_data.c
@@ -88,7 +88,7 @@ int *smf_skytiles_data( Grp *igrp, size_t size, int *ntile, int *status ){
    JCMTState *state;
    const char *trsys;
    const char *trsys_last;
-   dim_t *hits;
+   dim_t *hits = NULL;
    dim_t *ph;
    dim_t iframe;
    double *gx = NULL;
@@ -112,7 +112,7 @@ int *smf_skytiles_data( Grp *igrp, size_t size, int *ntile, int *status ){
    smfData *data = NULL;
    smfHead *hdr = NULL;
    smfSkyTiling skytiling;
-   smf_inst_t inst;
+   smf_inst_t inst = SMF__INST_NONE;
    smf_inst_t instrument;
    smf_subinst_t subinst;
 
@@ -205,32 +205,36 @@ int *smf_skytiles_data( Grp *igrp, size_t size, int *ntile, int *status ){
       gx = astGrow( gx, 4*hdr->nframes, sizeof( *gx ) );
       gy = astGrow( gy, 4*hdr->nframes, sizeof( *gy ) );
 
+/* Check pointers can be used safely. */
+      if( *status == SAI__OK ) {
+
 /* Loop round all time slices, getting the tracking coords at the corners
    of a box that enclose the field of view. */
-      p1 = trac1;
-      p2 = trac2;
-      state = hdr->allState;
-      for( iframe = 0; iframe < hdr->nframes; iframe++,state++ ) {
-         point1[ 0 ] = state->tcs_tr_ac1;
-         point1[ 1 ] = state->tcs_tr_ac2;
-         for( i = 0; i < 4; i++ ) {
-            astOffset2( frm, point1, i*AST__DPIBY2, search, point2 );
-            *(p1++) = point2[ 0 ];
-            *(p2++) = point2[ 1 ];
+         p1 = trac1;
+         p2 = trac2;
+         state = hdr->allState;
+         for( iframe = 0; iframe < hdr->nframes; iframe++,state++ ) {
+            point1[ 0 ] = state->tcs_tr_ac1;
+            point1[ 1 ] = state->tcs_tr_ac2;
+            for( i = 0; i < 4; i++ ) {
+               astOffset2( frm, point1, i*AST__DPIBY2, search, point2 );
+               *(p1++) = point2[ 0 ];
+               *(p2++) = point2[ 1 ];
+            }
          }
-      }
 
 /* Convert them to grid coords in the hits map. */
-      astTran2( fs, 4*hdr->nframes, trac1, trac2, 0, gx, gy );
+         astTran2( fs, 4*hdr->nframes, trac1, trac2, 0, gx, gy );
 
 /* Loop round them all again. Update the hits map to indiucate how many
    points fall in each tile. */
-      px = gx;
-      py = gy;
-      for( iframe = 0; iframe < 4*hdr->nframes; iframe++,px++,py++ ) {
-         ix = (int)( *px + 0.5 ) - 1;
-         iy = (int)( *py + 0.5 ) - 1;
-         hits[ ix + iy*ubnd[ 0 ] ]++;
+         px = gx;
+         py = gy;
+         for( iframe = 0; iframe < 4*hdr->nframes; iframe++,px++,py++ ) {
+            ix = (int)( *px + 0.5 ) - 1;
+            iy = (int)( *py + 0.5 ) - 1;
+            hits[ ix + iy*ubnd[ 0 ] ]++;
+         }
       }
 
 /* Close the current input data file. */
