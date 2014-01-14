b69384474965466b35835f30413cb8a183fce41e
David Berry <d.berry@jach.hawaii.edu>
2014-01-14 13:10:20 +0000

    smurf: Multi-thread smf_import_array

--- applications/smurf/libsmf/smf_import_array.c
+++ applications/smurf/libsmf/smf_import_array.c
@@ -68,7 +68,9 @@
 *     7-JAN-2014 (DSB):
 *        Added support for SMF__INTEGER arrays.
 *     10-JAN-2014 (DSB):
-*        Added argument wf.
+*        Added argument wf (needed by smf_open_file).
+*     14-JAN-2014 (DSB):
+*        Multi-thread.
 *     {enter_further_changes_here}
 
 *  Copyright:
@@ -106,6 +108,24 @@
 #include "libsmf/smf.h"
 #include "libsmf/smf_typ.h"
 
+/* Prototypes for local static functions. */
+static void smf1_import_array( void *job_data_ptr, int *status );
+
+/* Local data types */
+typedef struct smfImportArrayData {
+   const char *name;
+   int operation;
+   size_t i1;
+   size_t i2;
+   size_t t1;
+   size_t t2;
+   size_t bstride;
+   size_t tstride;
+   dim_t nbolo;
+   smf_dtype type;
+   void *din;
+   void *dout;
+} SmfImportArrayData;
 
 void smf_import_array( ThrWorkForce *wf, smfData *refdata, const char *name, int bad,
                        int expand, smf_dtype type, void *dataptr,
@@ -113,20 +133,17 @@ void smf_import_array( ThrWorkForce *wf, smfData *refdata, const char *name, int
 
 /* Local Variables: */
    Grp *igrp;                  /* Group holding NDF name */
+   SmfImportArrayData *job_data = NULL;
+   SmfImportArrayData *pdata;
    dim_t nbolo;                /* Number of bolometers */
+   dim_t nel;                  /* Number of elements in array */
    dim_t ntslice;              /* Number of time slices */
-   double *pin;                /* Pointer to next input value */
-   double *pout;               /* Pointer to next output value */
-   int *ipin;                  /* Pointer to next input value */
-   int *ipout;                 /* Pointer to next output value */
-   double mean;                /* Mean value in the plane */
-   double vsum;                /* Sum of good data values */
-   int nbad;                   /* Number of bad data values */
-   int ngood;                  /* Number of good data values */
+   int iw;
+   int nw;
    size_t bstride;             /* Stride between bolometer values */
    size_t i;                   /* Loop count */
-   size_t j;                   /* Loop count */
-   dim_t nel;                  /* Number of elements in array */
+   size_t istep;
+   size_t tstep;
    size_t tstride;             /* Stride between time slices */
    smfData *data;              /* Model for one sub-array */
 
@@ -195,39 +212,63 @@ void smf_import_array( ThrWorkForce *wf, smfData *refdata, const char *name, int
                         name, data->lbnd[i], i + 1, refdata->lbnd[i] );
             }
          }
-
       }
 
 /* Get the smfData dimensions and strides. */
       smf_get_dims( refdata, NULL, NULL, &nbolo, &ntslice, &nel, &bstride,
                     &tstride, status );
 
+/* How many threads do we get to play with */
+      nw = wf ? wf->nworker : 1;
+
+/* Find how many elements and time slices to process in each worker thread. */
+      istep = nel/nw;
+      if( istep == 0 ) istep = 1;
+      tstep = ntslice/nw;
+      if( tstep == 0 ) tstep = 1;
+
+/* Allocate job data for threads, and store common values. Ensure that the
+   last thread picks up any left-over elements or time slices.  */
+      job_data = astCalloc( nw, sizeof(*job_data) );
+      if( *status == SAI__OK ) {
+         for( iw = 0; iw < nw; iw++ ) {
+            pdata = job_data + iw;
+            pdata->i1 = iw*istep;
+            pdata->t1 = iw*tstep;
+            if( iw < nw - 1 ) {
+               pdata->i2 = pdata->i1 + istep - 1;
+               pdata->t2 = pdata->t1 + tstep - 1;
+            } else {
+               pdata->i2 = nel - 1;
+               pdata->t2 = ntslice - 1;
+            }
+
+            pdata->din = data->pntr[0];
+            pdata->dout = dataptr;
+            pdata->type = type;
+            pdata->bstride = bstride;
+            pdata->tstride = tstride;
+            pdata->nbolo = nbolo;
+            pdata->name = name;
+         }
+      }
+
 /* Copy the values into the model array, replacing bad values as required. */
-      if( *status == SAI__OK && type == SMF__DOUBLE ) {
-         pin = data->pntr[0];
-         pout = (double *) dataptr;
+      if( *status == SAI__OK ) {
          if( data->ndims < 3 ) data->dims[2] = 1;
          if( data->ndims < 2 ) data->dims[1] = 1;
 
-         /* Copy the data into the returned array unchanged. */
+/* First copy the data into the returned array unchanged. */
          if( expand ) {
-
-            pin = (double *) data->pntr[0];
-            for( i = 0; i < ntslice; i++,pin++ ) {
-               pout = (double *) dataptr + i*tstride;
-
-               for( j = 0; j < nbolo; j++ ) {
-                  if( *pin != VAL__BADD ) {
-                     *pout = *pin;
-                  } else {
-                     *pout = VAL__BADD;
-                  }
-                  pout += bstride;
-               }
+            for( iw = 0; iw < nw; iw++ ) {
+               pdata = job_data + iw;
+               pdata->operation = 1;
+               thrAddJob( wf, 0, pdata, smf1_import_array, 0, NULL, status );
             }
+            thrWait( wf, status );
 
          } else {
-            memcpy( pout, pin, nel*sizeof(*pin) );
+            memcpy(  dataptr, data->pntr[0], nel*smf_dtype_sz( type, status ) );
          }
 
 /* Retain bad values. */
@@ -235,138 +276,227 @@ void smf_import_array( ThrWorkForce *wf, smfData *refdata, const char *name, int
 
 /* Replace bad values with zero. */
          } else if( bad == 1 )  {
-            pout = (double *) dataptr;
-            for( i = 0; i < nel; i++,pout++ ) {
-               if( *pout == VAL__BADD ) *pout = 0.0;
+            for( iw = 0; iw < nw; iw++ ) {
+               pdata = job_data + iw;
+               pdata->operation = 2;
+               thrAddJob( wf, 0, pdata, smf1_import_array, 0, NULL, status );
             }
+            thrWait( wf, status );
 
 /* Replace bad values with the mean value in the time slice. */
          } else if( bad == 2 )  {
-            pout = (double *) dataptr;
-            mean = VAL__BADD;
+            for( iw = 0; iw < nw; iw++ ) {
+               pdata = job_data + iw;
+               pdata->operation = 3;
+               thrAddJob( wf, 0, pdata, smf1_import_array, 0, NULL, status );
+            }
+            thrWait( wf, status );
+         }
+      }
 
-            for( i = 0; i < ntslice; i++ ) {
-               vsum = 0.0;
-               ngood = 0;
-               nbad = 0;
-               pout = (double *) dataptr + i*tstride;
+/* Free resources. */
+      job_data = astFree( job_data );
+      smf_close_file( wf, &data, status );
+   }
+}
 
-               for( j = 0; j < nbolo; j++ ) {
-                  if( *pout != VAL__BADD ) {
-                     vsum += *pout;
-                     ngood++;
-                  } else {
-                     nbad++;
-                  }
-                  pout += bstride;
+static void smf1_import_array( void *job_data_ptr, int *status ) {
+/*
+*  Name:
+*     smf1_import_array
+
+*  Purpose:
+*     Executed in a worker thread to do various calculations for
+*     smf_import_array.
+
+*  Invocation:
+*     smf1_import_array( void *job_data_ptr, int *status )
+
+*  Arguments:
+*     job_data_ptr = SmfImportArrayData * (Given)
+*        Data structure describing the job to be performed by the worker
+*        thread.
+*     status = int * (Given and Returned)
+*        Inherited status.
+
+*/
+
+/* Local Variables: */
+   SmfImportArrayData *pdata;
+   dim_t nbolo;
+   double *pin;
+   double *pout;
+   int *ipin;
+   int *ipout;
+   size_t bstride;
+   size_t i1;
+   size_t i2;
+   size_t i;
+   size_t j;
+   size_t t1;
+   size_t t2;
+   size_t tstride;
+
+/* Check inherited status */
+   if( *status != SAI__OK ) return;
+
+/* Get a pointer that can be used for accessing the required items in the
+   supplied structure. */
+   pdata = (SmfImportArrayData *) job_data_ptr;
+
+   i1 = pdata->i1;
+   i2 = pdata->i2;
+   t1 = pdata->t1;
+   t2 = pdata->t2;
+   tstride = pdata->tstride;
+   bstride = pdata->bstride;
+   nbolo = pdata->nbolo;
+
+   if( pdata->operation == 1 ){
+      if( pdata->type == SMF__DOUBLE ) {
+         pin = (double *) pdata->din + t1;
+         for( i = t1; i <= t2; i++,pin++ ) {
+            pout = (double *) pdata->dout + i*tstride;
+            for( j = 0; j < nbolo; j++ ) {
+               if( *pin != VAL__BADD ) {
+                  *pout = *pin;
+               } else {
+                  *pout = VAL__BADD;
                }
-
-               if( ngood > 0 ) mean = vsum/ngood;
-
-               if( nbad > 0 ) {
-                  if( mean != VAL__BADD ) {
-                     pout = (double *) dataptr + i*tstride;
-                     for( j = 0; j < nbolo; j++ ) {
-                        if( *pout == VAL__BADD ) *pout = mean;
-                        pout += bstride;
-                     }
-                  } else {
-                     *status = SAI__ERROR;
-                     errRepf( " ", "NDF '%s' has no good values in plane "
-                              "%zu.", status, name, i );
-                     break;
-                  }
-               }
+               pout += bstride;
             }
          }
 
-/* Now do the same for integer data. */
-      } else if( *status == SAI__OK && type == SMF__INTEGER ) {
-         ipin = data->pntr[0];
-         ipout = (int *) dataptr;
-         if( data->ndims < 3 ) data->dims[2] = 1;
-         if( data->ndims < 2 ) data->dims[1] = 1;
-
-         /* Copy the data into the returned array unchanged. */
-         if( expand ) {
-
-            ipin = (int *) data->pntr[0];
-            for( i = 0; i < ntslice; i++,ipin++ ) {
-               ipout = (int *) dataptr + i*tstride;
-
-               for( j = 0; j < nbolo; j++ ) {
-                  if( *ipin != VAL__BADI ) {
-                     *ipout = *ipin;
-                  } else {
-                     *ipout = VAL__BADI;
-                  }
-                  ipout += bstride;
+      } else if( pdata->type == SMF__INTEGER ) {
+         ipin = (int *) pdata->din + t1;
+         for( i = t1; i <= t2; i++,pin++ ) {
+            ipout = (int *) pdata->dout + i*tstride;
+            for( j = 0; j < nbolo; j++ ) {
+               if( *ipin != VAL__BADI ) {
+                  *ipout = *ipin;
+               } else {
+                  *ipout = VAL__BADI;
                }
-            }
-
-         } else {
-            memcpy( ipout, ipin, nel*sizeof(*ipin) );
-         }
-
-/* Retain bad values. */
-         if( bad == 0 )  {
-
-/* Replace bad values with zero. */
-         } else if( bad == 1 )  {
-            ipout = (int *) dataptr;
-            for( i = 0; i < nel; i++,ipout++ ) {
-               if( *ipout == VAL__BADI ) *ipout = 0;
-            }
-
-/* Replace bad values with the mean value in the time slice. */
-         } else if( bad == 2 )  {
-            ipout = (int *) dataptr;
-            mean = VAL__BADD;
-
-            for( i = 0; i < ntslice; i++ ) {
-               vsum = 0.0;
-               ngood = 0;
-               nbad = 0;
-               ipout = (int *) dataptr + i*tstride;
-
-               for( j = 0; j < nbolo; j++ ) {
-                  if( *ipout != VAL__BADI ) {
-                     vsum += *ipout;
-                     ngood++;
-                  } else {
-                     nbad++;
-                  }
-                  ipout += bstride;
-               }
-
-               if( ngood > 0 ) mean = vsum/ngood;
-
-               if( nbad > 0 ) {
-                  if( mean != VAL__BADD ) {
-                     ipout = (int *) dataptr + i*tstride;
-                     for( j = 0; j < nbolo; j++ ) {
-                        if( *ipout == VAL__BADI ) *ipout = mean;
-                        ipout += bstride;
-                     }
-                  } else {
-                     *status = SAI__ERROR;
-                     errRepf( " ", "NDF '%s' has no good values in plane "
-                              "%zu.", status, name, i );
-                     break;
-                  }
-               }
+               pout += bstride;
             }
          }
 
       } else if( *status == SAI__OK ) {
-         const char *stype = smf_dtype_str( type, status );
+         const char *stype = smf_dtype_str( pdata->type, status );
          *status = SAI__ERROR;
          errRepf( " ", "smf_import_array: Data type '%s' not supported "
                   "(programming error).", status, stype );
       }
 
-/* Close the NDF. */
-      smf_close_file( wf, &data, status );
+   } else if( pdata->operation == 2 ){
+      if( pdata->type == SMF__DOUBLE ) {
+         pout = (double *) pdata->dout + i1;
+         for( i = i1; i <= i2; i++,pout++ ) {
+            if( *pout == VAL__BADD ) *pout = 0.0;
+         }
+
+      } else if( pdata->type == SMF__INTEGER ) {
+         ipout = (int *) pdata->dout + i1;
+         for( i = i1; i <= i2; i++,ipout++ ) {
+            if( *ipout == VAL__BADI ) *ipout = 0.0;
+         }
+
+      } else if( *status == SAI__OK ) {
+         const char *stype = smf_dtype_str( pdata->type, status );
+         *status = SAI__ERROR;
+         errRepf( " ", "smf_import_array: Data type '%s' not supported "
+                  "(programming error).", status, stype );
+      }
+
+   } else if( pdata->operation == 3 ){
+      double mean = VAL__BADD;
+      double vsum;
+      size_t ngood;
+      size_t nbad;
+
+      if( pdata->type == SMF__DOUBLE ) {
+         for( i = t1; i <= t2; i++ ) {
+            vsum = 0.0;
+            ngood = 0;
+            nbad = 0;
+            pout = (double *) pdata->dout + i*tstride;
+
+            for( j = 0; j < nbolo; j++ ) {
+               if( *pout != VAL__BADD ) {
+                  vsum += *pout;
+                  ngood++;
+               } else {
+                  nbad++;
+               }
+               pout += bstride;
+            }
+
+            if( ngood > 0 ) mean = vsum/ngood;
+
+            if( nbad > 0 ) {
+               if( mean != VAL__BADD ) {
+                  pout = (double *) pdata->dout + i*tstride;
+                  for( j = 0; j < nbolo; j++ ) {
+                     if( *pout == VAL__BADD ) *pout = mean;
+                     pout += bstride;
+                  }
+               } else {
+                  *status = SAI__ERROR;
+                  errRepf( " ", "NDF '%s' has no good values in plane "
+                           "%zu.", status, pdata->name, i );
+                  break;
+               }
+            }
+         }
+
+      } else if( pdata->type == SMF__INTEGER ) {
+         for( i = t1; i <= t2; i++ ) {
+            vsum = 0.0;
+            ngood = 0;
+            nbad = 0;
+            ipout = (int *) pdata->dout + i*tstride;
+
+            for( j = 0; j < nbolo; j++ ) {
+               if( *ipout != VAL__BADI ) {
+                  vsum += *ipout;
+                  ngood++;
+               } else {
+                  nbad++;
+               }
+               ipout += bstride;
+            }
+
+            if( ngood > 0 ) mean = vsum/ngood;
+
+            if( nbad > 0 ) {
+               if( mean != VAL__BADD ) {
+                  ipout = (int *) pdata->dout + i*tstride;
+                  for( j = 0; j < nbolo; j++ ) {
+                     if( *ipout == VAL__BADI ) *ipout = mean;
+                     ipout += bstride;
+                  }
+               } else {
+                  *status = SAI__ERROR;
+                  errRepf( " ", "NDF '%s' has no good values in plane "
+                           "%zu.", status, pdata->name, i );
+                  break;
+               }
+            }
+         }
+
+      } else if( *status == SAI__OK ) {
+         const char *stype = smf_dtype_str( pdata->type, status );
+         *status = SAI__ERROR;
+         errRepf( " ", "smf_import_array: Data type '%s' not supported "
+                  "(programming error).", status, stype );
+      }
+
+   } else {
+      *status = SAI__ERROR;
+      errRepf( "", "smf1_import_array: Invalid operation (%d) supplied.",
+               status, pdata->operation );
    }
 }
+
+
 
