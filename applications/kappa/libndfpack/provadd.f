2ceebb18e2cff6490e8d8508c832745e0459f153
David Berry <d.berry@jach.hawaii.edu>
2014-05-06 08:56:12 +0100

    kappa: update provadd to use current API for NDG_PUTPROV.
    This has involved removing parameter MORE. But it cannot have been used
    by anyone since it did not work, so removing it will not cause a problem.

--- applications/kappa/libndfpack/provadd.f
+++ applications/kappa/libndfpack/provadd.f
@@ -42,20 +42,11 @@
 *        then any provenance information within PARENT is copied into
 *        the main NDF.  PARENT is then only a root NDF only if it
 *        contains no provenance information.  [FALSE]
-*     MORE = UNIV (Read)
-*        This is only accessed if a null value is supplied for parameter
-*        MORETEXT.  If supplied, it should be an HDS object containing
-*        arbitrary additional information about the parent NDF, and how
-*        it was used in the creation of the main NDF.  This information
-*        is stored with the provenance in the main NDF.  If a null (!)
-*        value is supplied no additional information is stored.  [!]
 *     MORETEXT = GROUP (Read)
 *        A group of "keyword=value" strings that give additional
 *        information about the parent NDF, and how it was used in the
 *        creation of the main NDF.  If supplied, this information is
-*        stored with the provenance in the main NDF.  If a null (!)
-*        value is supplied, then the MORE parameter will be used to
-*        obtain this extra information instead of MORETEXT.
+*        stored with the provenance in the main NDF.
 *
 *        The supplied value should be either a comma-separated list of
 *        strings, or the name of a text file preceded by an up-arrow
@@ -95,7 +86,7 @@
 *     KAPPA: PROVMOD, PROVSHOW, HISCOM.
 
 *  Copyright:
-*     Copyright (C) 2008 Science & Technology Facilities Council.
+*     Copyright (C) 2008-2014 Science & Technology Facilities Council.
 *     All Rights Reserved.
 
 *  Licence:
@@ -125,6 +116,9 @@
 *        Added parameter MORETEXT.
 *     25-JUN-2009 (DSB):
 *        Updated to use new provenance API.
+*     6-MAY-2014 (DSB):
+*        Remove the MORE parameter since NDG requires a KeyMap rather
+*        than an HDS structure.
 *     {enter_further_changes_here}
 
 *-
@@ -134,26 +128,30 @@
 
 *  Global Constants:
       INCLUDE 'SAE_PAR'          ! Standard SAE constants
-      INCLUDE 'DAT_PAR'          ! HDS public constants
       INCLUDE 'PAR_ERR'          ! PAR_ error codes
       INCLUDE 'AST_PAR'          ! AST constants and functions
       INCLUDE 'NDG_PAR'          ! NDG constants
+      INCLUDE 'GRP_PAR'          ! GRP constants
 
 *  Status:
       INTEGER STATUS             ! Global status
 
 *  Local Variables:
       CHARACTER CREATR*255       ! Name of creator
-      CHARACTER MORE*(DAT__SZLOC) ! Locator for MORE structure
+      INTEGER IGRP               ! Group holding extra info
       INTEGER INDF1              ! Identifier for NDF being modified
       INTEGER INDF2              ! Identifier for parent NDF
       INTEGER IPROV              ! Identifier for provenance info
-      LOGICAL CNCLMR             ! Cancel the MORE parameter on exit?
+      INTEGER MORE               ! Pointer to KeyMap holding extra info
+      INTEGER SIZE               ! No of line sof text in IGRP
       LOGICAL ISROOT             ! Is the parent an orphan?
 *.
 
 *  Check the inherited global status.
       IF ( STATUS .NE. SAI__OK ) RETURN
+
+*  Start an AST context.
+      CALL AST_BEGIN( STATUS )
 
 *  Begin an NDF context.
       CALL NDF_BEGIN
@@ -167,29 +165,23 @@
 *  ABort if an error has occurred.
       IF( STATUS .NE. SAI__OK ) GO TO 999
 
-*  Attempt to get additional information first as a set of text strings
+*  Attempt to get additional information as a GRP group of text strings
 *  using the MORETEXT parameter.
-      CALL KPG1_GTMOR( 'MORETEXT', MORE, STATUS )
+      IGRP = GRP__NOID
+      CALL KPG1_GTGRP( 'MORETEXT', IGRP, SIZE, STATUS )
 
-*  If a null value was supplied for MORETEXT, annul the error and
-*  attempt to get the additional information directly as an HDS object
-*  using parameter MORE.
+*  If a null value was supplied for MORETEXT, annul the error. Otherwise
+*  create a KeyMap from the group, then delete the group.
+      MORE = AST__NULL
+
       IF( STATUS .EQ. PAR__NULL ) THEN
          CALL ERR_ANNUL( STATUS )
 
-         MORE = DAT__NOLOC
-         CALL DAT_ASSOC( 'MORE', 'READ', MORE, STATUS )
-
-         IF( STATUS .EQ. PAR__NULL ) THEN
-            CALL ERR_ANNUL( STATUS )
-            MORE = DAT__NOLOC
-         ELSE
-            CNCLMR = .TRUE.
-         END IF
-
-      ELSE
-         CNCLMR = .FALSE.
+      ELSE IF( IGRP .NE. GRP__NOID .AND. SIZE .GT. 0 ) THEN
+         CALL KPG1_KYMAP( IGRP, MORE, STATUS )
       END IF
+
+      IF( IGRP .NE. GRP__NOID ) CALL GRP_DELET( IGRP, STATUS )
 
 *  Obtain the creator string.
       IF( STATUS .EQ. SAI__OK ) THEN
@@ -208,8 +200,7 @@
 
 *  Read provenance information from the parent NDF and add it into the
 *  structure holding provenance information read from the child NDF.
-      CALL NDG_PUTPROV( IPROV, INDF2, MORE, AST__NULL, ISROOT,
-     :                  STATUS )
+      CALL NDG_PUTPROV( IPROV, INDF2, MORE, ISROOT, STATUS )
 
 *  Store the modified provenance information in the child NDF.
       CALL NDG_WRITEPROV( IPROV, INDF1, .FALSE., STATUS )
@@ -220,17 +211,11 @@
 *  Free the Provenance information.
       CALL NDG_FREEPROV( IPROV, STATUS )
 
-*  Free the MORE structure locator.
-      IF( MORE .NE. DAT__NOLOC ) THEN
-         IF( CNCLMR ) THEN
-            CALL DAT_CANCL( 'MORE', STATUS )
-         ELSE
-            CALL DAT_ANNUL( MORE, STATUS )
-         END IF
-      END IF
-
 *  End the NDF context.
       CALL NDF_END( STATUS )
+
+*  End the AST context.
+      CALL AST_END( STATUS )
 
 *  If an error occurred, then report contextual information.
       IF ( STATUS .NE. SAI__OK ) THEN
