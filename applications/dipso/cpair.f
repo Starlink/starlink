be225022b0f6a38926d97c7b954c9904fa23b706
David Berry <d.berry@jach.hawaii.edu>
2014-06-13 11:04:52 +0100

    dipso: Prevent cursor position hopping around between clicks in cregs

--- applications/dipso/cpair.f
+++ applications/dipso/cpair.f
@@ -7,7 +7,16 @@
        FIRSTCURSOR=.TRUE.
        CALL SGSCURSE(HITVAL,XEND,YEND,FIRSTCURSOR)
        FIRSTCURSOR=.FALSE.
-       CALL SGS_SETCU(XEND,YEND)
+
+* The following call was commented out by DSB on 13/6/2014 becuase it
+* can cause the cursor to be moved to an incorrect position. XEND and
+* YEND are in spectral coords, but SGS_SETCU requires graphics coords.
+* Also, SGSCURSE leavs the graphics system in NCAR mode rather than SGS
+* mode. These issues could be fixed, but I cannot see why this call is
+* needed anyway, since the cursor will currently be in the position at
+* which it was left by SGSCURSE anyway.
+c       CALL SGS_SETCU(XEND,YEND)
+
        XSTART=XEND
        YSTART=YEND
        CALL SGSCURSE(HITVAL,XEND,YEND,FIRSTCURSOR)
