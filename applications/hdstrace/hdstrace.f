6ddf36e4b5f7d6d857b860b3ed3894f691e14087
Malcolm J. Currie <mjc@star.rl.ac.uk>
2014-11-16 01:03:59 -1000

    HDSTRACE: Prologue tidying.

--- applications/hdstrace/hdstrace.f
+++ applications/hdstrace/hdstrace.f
@@ -40,8 +40,8 @@
 *        nested structures.
 *
 *     3) X is an array of structures.  All elements will be listed
-*        if parameter FULL is set to TRUE; only the first element will
-*        be listed when parameter FULL is set to FALSE (default).
+*        if Parameter FULL is set to TRUE; only the first element will
+*        be listed when Parameter FULL is set to FALSE (default).
 *        Arrays of structures nested six deep can be listed.
 *
 *     Listings are in the following order: name; dimensions, if any;
@@ -172,7 +172,7 @@
 *  Licence:
 *     This program is free software; you can redistribute it and/or
 *     modify it under the terms of the GNU General Public License as
-*     published by the Free Software Foundation; either version 2 of
+*     published by the Free Software Foundation; either Version 2 of
 *     the License, or (at your option) any later version.
 *
 *     This program is distributed in the hope that it will be
@@ -182,8 +182,8 @@
 *
 *     You should have received a copy of the GNU General Public License
 *     along with this program; if not, write to the Free Software
-*     Foundation, Inc., 51 Franklin Street,Fifth Floor, Boston, MA
-*     02110-1301, USA
+*     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+*     02110-1301, USA.
 
 *  Authors:
 *     MJC: Malcolm J. Currie (STARLINK)
