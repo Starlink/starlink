0c485e71a83774ec2bfbd2e562ee769b5e4c9e66
Malcolm J. Currie <mjc@star.rl.ac.uk>
2015-07-16 19:08:00 +0100

    KAPPA: Standardise documentation for NDFCOMPARE.
    Also increment SUN/95 and KAPPA version numbers, and documentation
    date. Add NDFCOMPARE to classified lists.  Within NDFCOMPARE add
    Related Applications, albeit a little tenuous.  Use \texttt instead of
    \tt.  [Is PROLAT still generating the \tt?]

--- applications/kappa/libndfpack/ndfcompare.c
+++ applications/kappa/libndfpack/ndfcompare.c
@@ -58,25 +58,25 @@ F77_SUBROUTINE(ndfcompare)( INTEGER(status) ){
 
 *  Description:
 *     This application compares two supplied NDFs, and sets the
-*     ADAM parameter SIMILAR to "FALSE" if they are significantly
-*     different in any way, and to "TRUE" if they are not significantly
+*     Parameter SIMILAR to "FALSE" if they are significantly different
+*     in any way, and to "TRUE" if they are not significantly
 *     different.
 *
 *     If they are not similar, a textual description of the differences
 *     is written to standard output, and to any file specified by
-*     parameter REPORT.
+*     Parameter REPORT.
 *
 *     The two NDFS are compared in the following ways. Each test has
 *     an integer identifier, and the list of tests to be used can be
-*     controlled by parameters DOTESTS and SKIPTESTS. Tests that are
+*     controlled by Parameters DOTESTS and SKIPTESTS. Tests that are
 *     not included by default are indicated by the test number being in
 *     square brackets. Some tests have parameters that control the exact
-*     nature of the test - these are listed in parentheses at the end of
-*     the description test:
+*     nature of the test. These are listed in parentheses at the end of
+*     the description test listed below.
 *
 *        1   - The number of pixel axes are compared.
 *        2   - The pixel bounds are compared.
-*        3   - The list of coordinate systems in the WCS FrameSet are
+*        3   - The list of co-ordinate systems in the WCS FrameSet are
 *              compared.
 *        4   - The presence or absence of NDF components are compared (COMP).
 *        5   - The sky positions of a grid of pixels are compared (ACCPOS).
@@ -91,7 +91,7 @@ F77_SUBROUTINE(ndfcompare)( INTEGER(status) ){
 *        14  - The pixel VARIANCE values (if any) are compared (ACCVAR).
 *        15  - The pixel QUALITY values (if any) are compared (NBAD).
 *        16  - The QUALITY names (if any) are compared.
-*       [17] - The lists of root ancestor NDFs that were used to create
+*        17  - The lists of root ancestor NDFs that were used to create
 *              each NDF are compared.
 
 *  Usage:
@@ -100,51 +100,56 @@ F77_SUBROUTINE(ndfcompare)( INTEGER(status) ){
 *  ADAM Parameters:
 *     ACCDAT = LITERAL (Read)
 *        The maximum difference allowed between two pixel data values
-*        for them to be considered equivalent. The supplied string should
-*        contain a numerical value followed by a single character (case
-*        insensitive) indicating how the numerical value is to be used:
+*        for them to be considered equivalent. The supplied string
+*        should contain a numerical value followed by a single character
+*        (case insensitive) from the list below indicating how the
+*        numerical value is to be used.
 *
-*        - "V": The numerical value is a signal-to-noise value. The
-*        absolute difference in pixel data value is divided by the square
-*        root of the smaller of the two variances associated with the
-*        pixels (one from each input NDF). If the resulting ratio is
-*        smaller than the ACCDAT value, then the two pixel data values are
-*        considered to be equivalent. An error is reported if either NDF
-*        does not have a VARIANCE component.
+*        - "V" --- The numerical value is a signal-to-noise value. The
+*        absolute difference in pixel data value is divided by the
+*        square root of the smaller of the two variances associated with
+*        the pixels (one from each input NDF). If the resulting ratio is
+*        smaller than the ACCDAT value, then the two pixel data values
+*        are considered to be equivalent. An error is reported if either
+*        NDF does not have a VARIANCE component.
 *
-*        - "R": The numerical value is a relative error. The absolute
+*        - "R" --- The numerical value is a relative error. The absolute
 *        difference between the two pixel data values is divided by the
 *        absolute mean of the two data values. If the resulting ratio is
-*        smaller than the ACCDAT value, then the two pixel data values are
-*        considered to be equivalent. To avoid problems with pixels where
-*        the mean is close to zero, a lower limit equal to the the RMS of
-*        the data values is placed on the mean value used in the above ratio.
+*        smaller than the ACCDAT value, then the two pixel data values
+*        are considered to be equivalent. To avoid problems with pixels
+*        where the mean is close to zero, a lower limit equal to the RMS
+*        of the data values is placed on the mean value used in the
+*        above ratio.
 *
-*        - "A": The numerical value is an absolute error. If the absolute
-*        difference in pixel data value is smaller than the ACCDAT value,
-*        then the two pixel data values are considered to be equivalent.
+*        - "A" --- The numerical value is an absolute error. If the
+*        absolute difference in pixel data value is smaller than the
+*        ACCDAT value, then the two pixel data values are considered to
+*        be equivalent.
 *
-*        If no character is included in the ACCDAT string, "R" is assumed.
-*        ["1E-6 R"]
+*        If no character is included in the ACCDAT string, "R" is
+*        assumed.  ["1E-6 R"]
 *     ACCPOS = _DOUBLE (Read)
 *        The maximum difference allowed between two axis values for
 *        them to be considered equivalent, in units of pixels on the
 *        corresponding pixel axes. [0.2]
 *     ACCVAR = LITERAL (Read)
-*        The maximum difference allowed between two pixel variance values
-*        for them to be considered equivalent. The supplied string should
-*        contain a numerical value followed by a single character (case
-*        insensitive) indicating how the numerical value is to be used:
+*        The maximum difference allowed between two pixel variance
+*        values for them to be considered equivalent. The supplied
+*        string should contain a numerical value followed by a single
+*        character (case insensitive) from the list below indicating how
+*        the numerical value is to be used.
 *
-*        - "R": The numerical value is a relative error. The absolute
+*        - "R" --- The numerical value is a relative error. The absolute
 *        difference in variance value is divided by the absolute mean of
 *        the two variance values. If the resulting ratio is smaller than
 *        the ACCVAR value, then the two pixel variances are considered to
 *        be equivalent.
 *
-*        - "A": The numerical value is an absolute error. If the absolute
-*        difference in variance values is smaller than the ACCVAR value,
-*        then the two pixel variances are considered to be equivalent.
+*        - "A" --- The numerical value is an absolute error. If the
+*        absolute difference in variance values is smaller than the
+*        ACCVAR value, then the two pixel variances are considered to be
+*        equivalent.
 *
 *        If no character is included in the ACCVAR string, "R" is assumed.
 *        ["1E-6 R"]
@@ -159,7 +164,7 @@ F77_SUBROUTINE(ndfcompare)( INTEGER(status) ){
 *        An initial list of indices for the tests to be performed, or
 *        null (!) if all tests are to be included in the initial list.
 *        This initial list is modified by excluding any tests specified
-*        by parameter SKIPTESTS. [!]
+*        by Parameter SKIPTESTS. [!]
 *     IN1 = NDF (Read)
 *        The first NDF.
 *     IN2 = NDF (Read)
@@ -170,9 +175,10 @@ F77_SUBROUTINE(ndfcompare)( INTEGER(status) ){
 *        arrays. It is also used as the maximum number of pixel that can
 *        have different QUALITY values. The supplied string should contain
 *        a numerical value followed by a single character (case insensitive)
-*        indicating how the numerical value is to be used:
+*        from the list below indicating how the numerical value is to
+*        be used.
 *
-*        - "R": The numerical value is a relative error. The absolute
+*        - "R" --- The numerical value is a relative error. The absolute
 *        difference in the number of bad values is divided by the mean
 *        number of bad values in both NDFs (for the QUALITY array, the
 *        total number of pixels in the NDF is used as the denominator in
@@ -180,7 +186,7 @@ F77_SUBROUTINE(ndfcompare)( INTEGER(status) ){
 *        then the two NDFs are considered to be equivalent for the purposes
 *        of this test.
 *
-*        - "A": The numerical value is an absolute error. If the absolute
+*        - "A" --- The numerical value is an absolute error. If the absolute
 *        difference in the number of bad values is smaller than the NBAD
 *        value, then the two NDFs are considered to be equivalent for the
 *        purposes of this test.
@@ -192,7 +198,7 @@ F77_SUBROUTINE(ndfcompare)( INTEGER(status) ){
 *        differences found between the two NDFs will be store. [!]
 *     SKIPTESTS() = _INTEGER (Read)
 *        A list of indices for tests that are to removed from the initial
-*        list of tests specified by parameter DOTESTS. If a null (!) value
+*        list of tests specified by Parameter DOTESTS. If a null (!) value
 *        is supplied, the initial list is left unchanged. [15]
 *     SIMILAR = _LOGICAL (Write)
 *        Set to FALSE on exit if any of the used tests indicate that the
