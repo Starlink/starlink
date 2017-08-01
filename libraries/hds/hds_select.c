e372a8d0420872b41d9446e9eead92dd73d0482b
David Berry <d.berry@jach.hawaii.edu>
2017-08-01 09:33:08 +0100

    hds: All V4 objects are locked for read-write by the current thread.

--- libraries/hds/hds_select.c
+++ libraries/hds/hds_select.c
@@ -1153,11 +1153,8 @@ datLocked( const HDSLoc *locator, int *status) {
   if (isv5) {
     retval = datLocked_v5(locator, status);
   } else if( *status == SAI__OK && hds1V4LockError() ){
-    *status = DAT__VERMM;
-    datMsg( "O", locator );
-    emsRepf("","datLocked: supplied HDS object (^O) uses V4 data format", status );
-    emsRepf("","The running application is multi-threaded and so requires V5 data files.",
-            status );
+    retval = 1;  /* In V4, all HDS objects are considered to be locked by
+                    the current thread for read-write. */
   }
   HDS_CHECK_STATUS("datLocked","(v5)");
   return retval;
