e641a513819bc1349bc0b0d63c426d2ba19547c7
David Berry <d.berry@jach.hawaii.edu>
2017-08-29 15:48:47 +0100

    prm: Add C versions of vec type conversion functions
    This is for use by the C versions of ARY and NDF that are being
    developed. The new C type conversion functions are thread safe and can
    handle more than 2^32 elements. Further C functions will no doubt be
    needed eventually,  but this is just a start.

---
+++ libraries/prm/prm.h
@@ -0,0 +1,51 @@
+#if !defined( PRM_INCLUDED )   /* Protect against multiple inclusion*/
+#define PRM_INCLUDED 1
+
+/*
+*  Name:
+*     prm.h
+
+*  Purpose:
+*     Defines the public interface for the PRM library.
+
+*  Description:
+*     This file defines all the public function prototypes
+*     provided by the C version of PRM.
+
+*  Authors:
+*     DSB: David S Berry (EAO)
+
+*  History:
+*     29-AUG-2017 (DSB):
+*        Initial version.
+
+*  Copyright:
+*     Copyright (C) 2017 East Asian Observatory.
+*     All Rights Reserved.
+
+*  Licence:
+*     This program is free software; you can redistribute it and/or
+*     modify it under the terms of the GNU General Public License as
+*     published by the Free Software Foundation; either version 3 of
+*     the License, or (at your option) any later version.
+*
+*     This program is distributed in the hope that it will be
+*     useful, but WITHOUT ANY WARRANTY; without even the implied
+*     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+*     PURPOSE. See the GNU General Public License for more details.
+*
+*     You should have received a copy of the GNU General Public
+*     License along with this program; if not, write to the Free
+*     Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+*     MA 02110-1301, USA
+
+*/
+
+
+
+
+
+/* Include the expanded generic prototypes. */
+#include "prm_cgen.h"
+
+#endif
