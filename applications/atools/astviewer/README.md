8b9d98e18c6c4639bf23e0d04056c762e454f365
David Berry <d.berry@eaobservatory.org>
2019-01-29 08:55:36 +0000

    atools: add missing files and restructure astviewer files

---
+++ applications/atools/astviewer/README.md
@@ -0,0 +1,5 @@
+# astviewer
+A QT-based GUI for exploring Starlink AST objects
+
+See http://timj.github.io/starlink-pyast/astviewer.html.
+
---
+++ applications/atools/astviewer/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name='starlink-astviewer',
+      version='1.0.8',
+      packages=['astviewer'],
+      package_data={'':['*.html','*.png']},
+      eager_resources={'astviewer/astviewer.html','astviewer/example-frameset.png'},
+      description='A utility for exploring Starlink AST FrameSet objects',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author='David S. Berry',
+      author_email='davidstuartberry@gmail.com',
+      url="https://github.com/dsberry/astviewer",
+      scripts=['astviewer/astviewer'],
+      install_requires=['starlink-pyast>=3.11.0','numpy'],
+      classifiers=[
+          'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
+          'Programming Language :: Python',
+          'Topic :: Scientific/Engineering :: Astronomy'
+      ])
