ab0eee4b689d85af064f989359c4647e73c3ceff
David Berry <d.berry@eaobservatory.org>
2020-11-11 17:55:04 +0000

    atools: new pip setup an docs for astviewer

--- applications/atools/astviewer/setup.py
+++ applications/atools/astviewer/setup.py
@@ -5,10 +5,10 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
       name='starlink-astviewer',
-      version='1.0.9',
+      version='1.1.0',
       packages=['astviewer'],
       package_data={'':['*.html','*.png']},
-      eager_resources={'astviewer/astviewer.html','astviewer/example-frameset.png'},
+      eager_resources=['astviewer/astviewer.html','astviewer/example-frameset.png'],
       description='A utility for exploring Starlink AST FrameSet objects',
       long_description=long_description,
       long_description_content_type="text/markdown",
@@ -16,7 +16,7 @@ setuptools.setup(
       author_email='davidstuartberry@gmail.com',
       url="https://github.com/dsberry/astviewer",
       scripts=['astviewer/astviewer'],
-      install_requires=['starlink-pyast>=3.11.0','numpy'],
+      install_requires=['starlink-pyast>=3.11.0','numpy', 'pyqt5'],
       classifiers=[
           'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
           'Programming Language :: Python',
