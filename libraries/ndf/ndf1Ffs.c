d931500084e426760111666bb1043b7ac25d813f
David Berry <d.berry@eaobservatory.org>
2021-06-16 17:19:43 +0100

    ndf: Exlcude mslot memory from AST memory checks
    This is a block of memory that by design is never freed.

--- libraries/ndf/ndf1Ffs.c
+++ libraries/ndf/ndf1Ffs.c
@@ -207,7 +207,9 @@ void *ndf1Ffs( NdfBlockType type, int *status ) {
          } else {
             head = parray + oldsize;
             for( i = oldsize; i < *pn; i++ ) {
+               astBeginPM;
                result = *(head++) = astCalloc( 1, size );
+               astEndPM;
                if( *status == SAI__OK ) {
                   result->used = 0;
                   result->slot = i;
