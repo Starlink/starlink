13fdc0812e46312b5adf6aa5fe2320db51f87ccb
Graham Bell <g.bell@eaobservatory.org>
2022-05-13 15:43:55 -1000

    fluxes: Convert scripts to sh
    This is to allow usage without csh.

--- applications/fluxes/scripts/fluxnow.sh
+++ applications/fluxes/scripts/fluxnow.sh
@@ -1,40 +1,38 @@
-#!@csh@
+#!@sh@
 #+
 #  Name:
-#     fluxes.csh
+#     fluxnow.sh
 #
 #  Purpose:
-#     Start the FLUXES program from Unix shell.
+#     Start the FLUXES system from Unix shell in such
+#     a way that the current values are used.
 #
 #  Type of Module:
-#     C shell script.
+#     Bourne shell script.
 #
 #  Invocation:
-#     fluxes.csh
+#     fluxnow.sh
 #
 #  Description:
 #     This procedure starts the FLUXES system for use from Unix by
-#     defining some environmental variables appropriately.
+#     defining some environmental variables appropriately and then
+#     setting the parameters to those that generate the current values.
 #
 #  Authors:
 #     GJP: G.J. Privett (Starlink, Cardiff)
 #     ACC: A.C. Charles (Starlink, RAL)
 #     BLY: M.J. Bly (Starlink, RAL)
-#     RPT: R.P. Tilanus (JACH)
 #     TIMJ: Tim Jenness (JACH)
 #
 #  History:
 #     13-DEC-1996 (GJP):
-#       Original Version.
+#       Original Version
 #     03-FEB-1997 (ACC/BLY):
 #       Changed definition of FLUXES to INSTALL_BIN.
 #       Modified startup to use link to JPL ephemeris instead of
 #          requiring to be run from ephemeris directory.
 #     07-FEB-1997 (BLY):
 #       Modified setting of FLUXPWD since $PWD not guranteed on Dec-Unix.
-#     04-SEP-1998 (RPT)
-#       Forward arguments to fluxes executable and print fewer messages
-#       if the arglist contains screen=n.
 #     14-JUL-2004 (TIMJ):
 #       Uses @bindir@ rather than INSTALL_BIN
 #       Uses FLUXES_DIR and JPL_DIR environment variable if set
@@ -42,37 +40,22 @@
 #       Remove JPL logic.
 #-
 
-# Flag an interrupt for bugging out.
-onintr goto quit
-
-# No screen output?
-set silent = 0
-if (`echo "$argv" | fgrep -c 'screen=n'` != 0) then
-  set silent = 1
-endif
-
 # Set up the environmental variables.
 
 # Define the location of fluxes and its data files.
 # This is edited into this script during installation.
-if ($?FLUXES_DIR) then
-  if (-d $FLUXES_DIR) then
-    # FLUXES_DIR is fine
-  else
+if [[ -n "$FLUXES_DIR" ]]; then
+  if [[ ! -d "$FLUXES_DIR" ]]; then
     echo FLUXES_DIR environment variable not set to a directory.
     echo Using default fluxes location
-    setenv FLUXES_DIR @bindir@
-  endif
+    export FLUXES_DIR=@bindir@
+  fi
 else
-  setenv FLUXES_DIR @bindir@
-endif
+  export FLUXES_DIR=@bindir@
+fi
 
 # Run the main program.
-$FLUXES_DIR/fluxes $argv
-
-# Exit
-quit:
-exit
+$FLUXES_DIR/fluxes pos=y flu=y screen=y ofl=y outfile=fluxes.dat now=y apass=n planet=all
 
 # end
 #.
