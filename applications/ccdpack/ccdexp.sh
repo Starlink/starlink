afbfa1bbeafd16e4cc28007fa8128745e76b6234
Graham Bell <g.bell@eaobservatory.org>
2022-05-19 09:05:45 -1000

    ccdpack: Convert scripts to sh
    This is to allow usage without csh.

--- applications/ccdpack/ccdexp.sh
+++ applications/ccdpack/ccdexp.sh
@@ -1,8 +1,8 @@
-#!/bin/csh -f
+#!/bin/sh
 
 #  Wrapper routine for CCDEXP to unset the ICL_TASK_NAME variable
 #  so we can run from cl (and presumably Tcl).
-unsetenv ICL_TASK_NAME
+unset ICL_TASK_NAME
 $CCDPACK_DIR/ccdexp in=$1 table=$CCDPACK_DIR/export.table
 exit
 # $Id$
--- applications/ccdpack/ccdimp.sh
+++ applications/ccdpack/ccdimp.sh
@@ -1,8 +1,8 @@
-#!/bin/csh -f
+#!/bin/sh
 
 #  Wrapper routine for CCDIMP to unset ICL_TASK_NAME variable  so we
 #  can run from cl (and presumably Tcl).
-unsetenv ICL_TASK_NAME
+unset ICL_TASK_NAME
 $CCDPACK_DIR/ccdimp in=$1 table=$CCDPACK_DIR/import.table
 exit
 # $Id$
